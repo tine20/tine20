96f636a2eb2090fa5d4b31f168c1ea1c2669e5b0
Lars Kneschke <l.kneschke@metaways.de>
2007-08-12 19:53:51 +0000

    svn propset svn:keywords Id
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@31 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- egroupware20/Addressbook/Contacts/Sql.php
+++ egroupware20/Addressbook/Contacts/Sql.php
@@ -7,7 +7,7 @@
  * @license     http://www.gnu.org/license/gpl GPL
  * @author      Lars Kneschke <l.kneschke@metaways.de>
  * @copyright   Copyright (c) 2007-2007 Metaways Infosystems GmbH (http://www.metaways.de)
- * @version     $Id: DbTable.php 4246 2007-03-27 22:35:56Z ralph $
+ * @version     $Id$
  *
  */
 class Addressbook_Contacts_Sql extends Zend_Db_Table_Abstract implements Addressbook_Contacts_Interface
