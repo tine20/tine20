a30a45630c080b500a2806762d03d7000af09b86
Lars Kneschke <l.kneschke@metaways.de>
2007-09-15 20:25:20 +0000

    baseclass for list information
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@84 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ egroupware20/Addressbook/List.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * class to hold list data
+ * 
+ * @package     Addressbook
+ * @license     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2007-2007 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+class Addressbook_List
+{
+    /**
+     * list of zend inputfilter
+     * 
+     * this filter get used when validating user generated content with Zend_Input_Filter
+     *
+     * @var array
+     */
+    protected $_filters = array(
+        '*'                     => 'StringTrim',
+        'contact_email'         => array('StringTrim', 'StringToLower'),
+        'contact_email_home'    => array('StringTrim', 'StringToLower'),
+        'contact_url'           => array('StringTrim', 'StringToLower'),
+        'contact_url_home'      => array('StringTrim', 'StringToLower'),
+    );
+    
+    /**
+     * list of zend validator
+     * 
+     * this validators get used when validating user generated content with Zend_Input_Filter
+     *
+     * @var array
+     */
+    protected $_validators = array(
+		'contact_tid'			=> array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'adr_one_countryname'   => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'adr_one_locality'      => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'adr_one_postalcode'    => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'adr_one_region'        => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'adr_one_street'        => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'adr_one_street2'       => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'adr_two_countryname'   => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'adr_two_locality'      => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'adr_two_postalcode'    => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'adr_two_region'	=> array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'adr_two_street'	=> array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'adr_two_street2'	=> array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'contact_assistent'	=> array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'contact_bday'		=> array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'contact_email'		=> array('EmailAddress', Zend_Filter_Input::ALLOW_EMPTY => true),
+        'contact_email_home'	=> array('EmailAddress', Zend_Filter_Input::ALLOW_EMPTY => true),
+        'contact_note'          => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'contact_role'		=> array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'contact_room'		=> array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'contact_title'		=> array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'contact_url'		=> array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'contact_url_home'	=> array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'n_family'		=> array(),
+        'n_given'		=> array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'n_middle'		=> array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'n_prefix'		=> array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'n_suffix'		=> array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'org_name'              => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'org_unit'              => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'tel_assistent'         => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'tel_car'               => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'tel_cell'              => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'tel_cell_private'      => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'tel_fax'               => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'tel_fax_home'          => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'tel_home'              => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'tel_pager'             => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'tel_work'              => array(Zend_Filter_Input::ALLOW_EMPTY => true)
+    );
+    
+    protected $_validationErrors = array();
+    
+    public function __construct($_contactData = NULL)
+    {
+    	if(is_array($_contactData)) {
+    		$this->setFromUserData($_contactData);
+    	}
+    }
+    
+    /**
+     * returns array of fields with validation errors 
+     *
+     * @return array
+     */
+    public function getValidationErrors()
+    {
+    	return $this->_validationErrors;
+    }
+    
+    /**
+     * sets the contactdata from user generated input. data get filtered by Zend_Filter_Input
+     *
+     * @param array $_contactData
+     * @throws Execption when content contains invalid or missing data
+     */
+    public function setFromUserData(array $_contactData)
+    {
+    	$inputFilter = new Zend_Filter_Input($this->_filters, $this->_validators, $_contactData);
+    	
+    	if ($inputFilter->isValid()) {
+    		$contactData = $inputFilter->getUnescaped();
+    		foreach($contactData as $key => $value) {	
+    			$this->$key = $value;
+    		}
+    	} else {
+            foreach($inputFilter->getMessages() as $fieldName => $errorMessages) {
+                $this->_validationErrors[] = array('id'  => $fieldName,
+                                  'msg' => $errorMessages[0]);
+            }
+    		throw new Exception("some fields have invalid content");
+    	}
+    }
+    
+    /**
+     * return public fields as array
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+    	$resultArray = array();
+    	foreach($this->_validators as $key => $name) {
+    		if(isset($this->$key)) {
+    			$resultArray[$key] = $this->$key;
+    		}
+    	}
+    	
+    	return $resultArray;
+    }
+    
+    private function __set($_name, $_value)
+    {
+    	if(isset($this->_validators[$_name])) {
+    		$this->$_name = $_value;
+    	} else {
+    		throw new Exception("$_name is not a valid resource name");
+    	}
+    }
+}
