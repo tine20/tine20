2dc31fa6d6cfe72b10ecd2470d341ee04b967116
Lars Kneschke <l.kneschke@metaways.de>
2007-09-26 13:12:18 +0000

    commited file for Thomas
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@133 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- egroupware20/Egwbase/Http.php
+++ egroupware20/Egwbase/Http.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * HTTP interface to Egwbase
- * 
+ *
  * @author Lars Kneschke <l.kneschke@metaways.de>
  * @package Egwbase
  *
@@ -12,31 +12,31 @@ class Egwbase_Http
 	 * displays the login dialog
 	 *
 	 */
-	public function login() 
+	public function login()
 	{
 		$view = new Zend_View();
-		
+
 		$view->setScriptPath('Egwbase/views');
-		
+
 		header('Content-Type: text/html; charset=utf-8');
 		echo $view->render('login.php');
 	}
-	
+
 	public function mainScreen()
 	{
 		$userApplications = array('Addressbook');
 		$view = new Zend_View();
-		
+
 		$view->setScriptPath('Egwbase/views');
-		
-//		foreach(array('Felamimail', 'Addressbook', 'Asterisk') as $applicationName) {
+
+		//foreach(array('Felamimail', 'Addressbook', 'Asterisk') as $applicationName) {
 		foreach($userApplications as $applicationName) {
 			$className = "{$applicationName}_Json";
-			
+				
 			$application = new $className;
-			
+				
 			$applications[] = $application->getMainTree();
-			
+				
 			$jsIncludeFiles[] = $applicationName . '/Js/' . $applicationName . '.js';
 		}
 
