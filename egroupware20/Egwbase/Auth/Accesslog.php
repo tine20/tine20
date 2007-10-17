e3bf4069aaed1ba9db96194cb8496fbe0634027e
Cornelius Weiß <c.weiss@metaways.de>
2007-10-17 14:21:45 +0000

    add egw14compat mode for access log
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@202 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- egroupware20/Egwbase/Auth/Accesslog.php
+++ egroupware20/Egwbase/Auth/Accesslog.php
@@ -1,6 +1,26 @@
 <?php
+class Egwbase_Auth_Accesslog
+{
+	protected $_accesslogger = null;
+	
+	public function __construct()
+	{
+		if ( !Zend_Registry::get('dbConfig')->get('egw14compat') == 1 )
+		{
+			$this->_accesslogger = new _Egwbase_Auth_Accesslog();
+		}
+	}
+	
+	public function __call( $_functionname, $_arguments );
+	{
+		if ( $this->_accesslogger instanceof _Egwbase_Auth_Accesslog )
+		{
+			return call_user_func_array( array( $this->_accesslogger, $_functionname ), $_arguments );
+		}
+	}
+}
 
-class Egwbase_Auth_Accesslog extends Zend_Db_Table_Abstract
+class _Egwbase_Auth_Accesslog extends Zend_Db_Table_Abstract
 {
 	protected $_name = 'egw_access_log';
 	protected $_primary = 'log_id';
@@ -9,6 +29,7 @@ class Egwbase_Auth_Accesslog extends Zend_Db_Table_Abstract
 	{
 		return $this->getAdapter()->fetchOne('SELECT count(*) FROM '. $this->_name);
 	}
+	
 }
 
 ?>
