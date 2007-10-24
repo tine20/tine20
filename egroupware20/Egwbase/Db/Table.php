ce9c668c6c15ddf32166733338b2ac8958f54ebb
Lars Kneschke <l.kneschke@metaways.de>
2007-10-24 06:02:17 +0000

    egw db base class
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@209 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ egroupware20/Egwbase/Db/Table.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * the class provides functions to handle applications
+ * 
+ * @package     Egwbase
+ * @license     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2007-2007 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+
+class Egwbase_Db_Table extends Zend_Db_Table_Abstract
+{
+    /**
+     * wrapper around Zend_Db_Table_Abstract::fetchAll
+     *
+     * @param strin|array $_where OPTIONAL
+     * @param string $_order OPTIONAL
+     * @param string $_dir OPTIONAL
+     * @param int $_count OPTIONAL
+     * @param int $_offset OPTIONAL
+     * @throws Exception if $_dir is not ASC or DESC
+     * @return the row results per the Zend_Db_Adapter fetch mode.
+     */
+    public function fetchAll($_where = NULL, $_order = NULL, $_dir = 'ASC', $_count = NULL, $_offset = NULL)
+    {
+        if($_dir != 'ASC' && $_dir != 'DESC') {
+            throw new Exception('$_dir can be only ASC or DESC');
+        }
+        
+        $order = NULL;
+        if($_order !== NULL) {
+            $order = $_order . ' ' . $_dir;
+        }
+        
+        $rowSet = parent::fetchAll($_where, $order, $_count, $_offset);
+        
+        return $rowSet;
+    }
+}
