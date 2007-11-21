876018d70b43f37e87edcd1027ab7603afbc2362
Lars Kneschke <l.kneschke@metaways.de>
2007-11-21 19:10:58 +0000

    the new Egwbase_Container class
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@322 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ egroupware20/Egwbase/Record/Container.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * defines the datatype for one container
+ * 
+ * @package     Egwbase
+ * @license     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2007-2007 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id: Application.php 308 2007-11-20 16:06:07Z lkneschke $
+ *
+ */
+
+class Egwbase_Record_Container extends Egwbase_Record_Abstract
+{
+    /**
+     * list of zend inputfilter
+     * 
+     * this filter get used when validating user generated content with Zend_Input_Filter
+     *
+     * @var array
+     */
+    protected $_filters = array(
+        '*'      => 'StringTrim'
+    );
+    
+    /**
+     * list of zend validator
+     * 
+     * this validators get used when validating user generated content with Zend_Input_Filter
+     *
+     * @var array
+     */
+    protected $_validators = array(
+        'container_id'      => array('Digits', 'presence' => 'required'),
+        'container_name'    => array('presence' => 'required'),
+        'container_type'    => array('Digits', 'presence' => 'required'),
+        'container_backend' => array('Digits', 'presence' => 'required'),
+        'application_id'    => array('Digits', 'presence' => 'required')
+    );
+
+   /**
+     * key in $_validators/$_properties array for the filed which 
+     * represents the identifier
+     * 
+     * @var string
+     */    
+    protected $_identifier = 'container_id';
+}
