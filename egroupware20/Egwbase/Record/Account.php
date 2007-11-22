04df8a551a84efd664555227c936b827f1c236e3
Lars Kneschke <l.kneschke@metaways.de>
2007-11-22 09:30:57 +0000

    added new account record class
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@328 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ egroupware20/Egwbase/Record/Account.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * defines the datatype for one application
+ * 
+ * @package     Egwbase
+ * @license     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2007-2007 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id: Application.php 308 2007-11-20 16:06:07Z lkneschke $
+ *
+ */
+
+class Egwbase_Record_Account extends Egwbase_Record_Abstract
+{
+    /**
+     * list of zend inputfilter
+     * 
+     * this filter get used when validating user generated content with Zend_Input_Filter
+     *
+     * @var array
+     */
+    protected $_filters = array(
+        '*'      => 'StringTrim'
+    );
+    
+    /**
+     * list of zend validator
+     * 
+     * this validators get used when validating user generated content with Zend_Input_Filter
+     *
+     * @var array
+     */
+    protected $_validators = array(
+        'account_id'      => array('Digits', 'presence' => 'required'),
+        'account_loginid' => array('presence' => 'required'),
+        'account_name'    => array('allowEmpty' => true),
+    );
+
+   /**
+     * key in $_validators/$_properties array for the filed which 
+     * represents the identifier
+     * 
+     * @var string
+     */    
+    protected $_identifier = 'account_id';
+}
