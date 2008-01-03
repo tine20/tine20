58354f1210d6105ca13456dcde15fe55908a6de0
Cornelius Weiß <c.weiss@metaways.de>
2008-01-03 09:41:21 +0000

    add backend functions for addressbook again, they where removed in rev. 415 by accident
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@440 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Addressbook/Backend/Sql/Lists.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * egroupware 2.0
+ * 
+ * @package     Addressbook
+ * @subpackage  Backend
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2007-2007 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+
+/**
+ * this classes provides access to the sql table egw_addressbook_lists
+ * 
+ * @package     Addressbook
+ * @subpackage  Backend
+ */
+class Addressbook_Backend_Sql_Lists extends Zend_Db_Table_Abstract
+{
+    protected $_name = 'egw_addressbook_lists';
+}
