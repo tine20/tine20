02a05b30c5365b0b1bf53227b91b5f7b971d97d3
Lars Kneschke <l.kneschke@metaways.de>
2008-01-05 22:22:46 +0000

    added addressbook controller
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@450 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Addressbook/Controller.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * controller for Addressbook
+ *
+ * @package     Addressbook
+ * @license     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2007-2007 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id: Controller.php 273 2007-11-08 22:51:16Z lkneschke $
+ *
+ */
+class Addressbook_Controller
+{
+    /**
+     * the constructor
+     *
+     * don't use the constructor. use the singleton 
+     */
+    private function __construct() {}
+    
+    /**
+     * don't clone. Use the singleton.
+     *
+     */
+    private function __clone() {}
+
+    /**
+     * holdes the instance of the singleton
+     *
+     * @var Adressbook_Controller
+     */
+    private static $instance = NULL;
+    
+    /**
+     * the singleton pattern
+     *
+     * @return Adressbook_Controller
+     */
+    public static function getInstance() 
+    {
+        if (self::$instance === NULL) {
+            self::$instance = new Addressbook_Controller;
+        }
+        
+        return self::$instance;
+    }
+    
+    public function getGrants($_addressbookId)
+    {
+        $addressbookId = (int)$_addressbookId;
+        if($addressbookId != $_addressbookId) {
+            throw new InvalidArgumentException('$_addressbookId must be integer');
+        }
+        
+        $result = Egwbase_Container::getInstance()->getAllGrants($addressbookId);
+                
+        return $result;
+    }
+    
+    public function setGrants($_addressbookId, Egwbase_Record_RecordSet $_grants)
+    {
+        $addressbookId = (int)$_addressbookId;
+        if($addressbookId != $_addressbookId) {
+            throw new InvalidArgumentException('$_addressbookId must be integer');
+        }
+        
+        $result = Egwbase_Container::getInstance()->setAllGrants($addressbookId, $_grants);
+                
+        return $result;
+    }
+    
+    public function getOtherUsers() 
+    {
+        $rows = Egwbase_Container::getInstance()->getOtherUsers('addressbook');
+
+        $accountData = array();
+
+        $result = new Egwbase_Record_RecordSet(array(), 'Egwbase_Record_PublicAccountProperties');
+        
+        foreach($rows as $account) {
+            $result->addRecord($account->getPublicProperties());
+        }
+
+        return $result;
+    }
+        
+    
+}
