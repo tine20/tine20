6642a5032f5bee606144920d24fd1d3de6ff6ece
Lars Kneschke <l.kneschke@metaways.de>
2008-01-07 13:22:03 +0000

    fixed keywords
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@478 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Egwbase/Record/Account.php
+++ tine20/Egwbase/Record/Account.php
@@ -7,7 +7,7 @@
  * @license     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * @copyright   Copyright (c) 2007-2007 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Lars Kneschke <l.kneschke@metaways.de>
- * @version     $Id: $
+ * @version     $Id$
  */
 
 /**
--- tine20/Egwbase/Record/PublicAccountProperties.php
+++ tine20/Egwbase/Record/PublicAccountProperties.php
@@ -7,7 +7,7 @@
  * @license     http://www.gnu.org/licenses/agpl.html
  * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Lars Kneschke <l.kneschke@metaways.de>
- * @version     $Id: $
+ * @version     $Id$
  */
 
 /**
