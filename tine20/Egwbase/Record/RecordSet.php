dd4c65b475a00bec44e79a83112f8ef30b3991b4
Lars Kneschke <l.kneschke@metaways.de>
2008-01-07 13:20:48 +0000

    another try
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@477 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Egwbase/Record/RecordSet.php
+++ tine20/Egwbase/Record/RecordSet.php
@@ -7,7 +7,7 @@
  * @license     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * @copyright   Copyright (c) 2007-2007 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Cornelius Weiss <c.weiss@metaways.de>
- * @version     $Id: $
+ * @version     $Id$
  */
 
 /**
