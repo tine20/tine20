8b3616eeb913e0ea0f2946f3a6b7eef3e0dbf705
Lars Kneschke <l.kneschke@metaways.de>
2008-01-22 14:40:57 +0000

    renamed setup to Setup
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@556 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tasks/Setup/MigrateFromEgw14.php
@@ -0,0 +1,242 @@
+<?php
+/**
+ * egroupware 2.0
+ * 
+ * @package     Tasks
+ * @subpackage  Backend
+ * @license     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2007-2007 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id: $
+ *
+ */
+
+/**
+ * Migration from  egw 1.4 table layout to tasks 2.0 table layout
+ * 
+ * @package Tasks
+ * @subpackage Setup
+ * @todo maybe we should create a mapping table infolog_id -> taks_id to be able
+ * to megrate cut-off fields later?
+ * cut-off:
+ *   -info_type         ?
+ *   -info_used_time    pm interface -> later
+ *   -info_confirm      ?
+ *   -info_link_id      obsolete?
+ *   -pl_id             ?
+ *   -info_price        pm interface -> later
+ *   -info_custom_from  ?
+ *   -custom fields      defer for later
+ * transform:
+ *   -contact is info_from, if info_addr is not an email.
+ *   -duration is info_enddate - info_startdate
+ */
+class Tasks_Setup_MigrateFromEgw14 
+{
+
+    /**
+     * Mapping Tasks Model / Egw 1.4 Infolog Table
+     *
+     * @var array
+     */
+    protected static $_mapping = array(
+        // egw record fields
+        'container'            => '',
+        'created_by'           => 'info_owner',
+        'creation_time'        => 'info_datemodified',
+        'last_modified_by'     => 'info_modifier',
+        'last_modified_time'   => 'info_datemodified',
+        'is_deleted'           => '',
+        'deleted_time'         => NULL,
+        'deleted_by'           => '',
+        // task only fields
+        'identifier'           => 'info_id',
+        'percent'              => 'info_percent',
+        'completed'            => 'info_datecompleted',
+        'due'                  => 'info_enddate',
+        // ical common fields
+        'class'                => 'info_access',
+        'description'          => 'info_des',
+        'geo'                  => '',
+        'location'             => 'info_location',
+        'organizer'            => 'info_responsible',
+        'priority'             => 'info_priority',
+        'status'               => 'info_status',
+        'summaray'             => 'info_subject',
+        'url'                  => '',
+        // ical common fields with multiple appearance
+        'attach'                => '',
+        'attendee'              => '',
+        'categories'            => 'info_cat',
+        'comment'               => '',
+        'contact'               => 'info_from',
+        'related'               => 'info_id_parent',
+        'resources'             => '',
+        'rstatus'               => '',
+        // scheduleable interface fields
+        'dtstart'               => 'info_startdate',
+        'duration'              => 'info_planned_time',
+        'recurid'               => '',
+        // scheduleable interface fields with multiple appearance
+        'exdate'                => '',
+        'exrule'                => '',
+        'rdate'                 => '',
+        'rrule'                 => '',
+    );
+    
+    /**
+     * Migrates Infologs from egw 1.4 to Tasks.
+     * 
+     * @return void
+     */
+    public static function MigrateInfolog2Tasks()
+    {
+        $tasksBackend = Tasks_Backend_Factory::factory(Tasks_Backend_Factory::SQL);
+
+        $db = Zend_Registry::get('dbAdapter');
+        $stmt = $db->query($db->select()
+            ->from('egw_infolog')
+        );
+        
+        while ($infolog = $stmt->fetchObject()) {
+            $Task = self::$_mapping;
+            
+            foreach (array('info_datemodified', 'info_datecompleted', 'info_enddate', 'info_startdate' ) as $datefield) {
+                if ((int)$infolog->$datefield == 0) continue;
+                $infolog->$datefield = new Zend_Date($infolog->$datefield, Zend_Date::TIMESTAMP);
+            }
+            
+            foreach (self::$_mapping as $TaskKey => $InfoKey) {
+                if (!$InfoKey) continue;
+                
+                // Map fields
+                if (isset($infolog->$InfoKey)) {
+                    $Task[$TaskKey] = $infolog->$InfoKey;
+                }
+            }
+            
+            //$Task['identifier'] = self::id2uid($Task['identifier']);      // uid
+            unset($Task['identifier']);
+    
+            $Task['class']     = self::getClass($Task['class']);
+            $Task['status']    = self::getStatus($Task['status']);
+            $Task['container'] = self::getOwnersContainer($infolog->info_owner);
+            $Task['organizer'] = $Task['organizer'] ? $Task['organizer'] : $Task['created_by'];
+            
+            error_log(print_r($Task,true));
+            try {
+                $Task20 = new Tasks_Model_Task(NULL, true, true);
+                $Task20->setFromUserData($Task);
+                
+            } catch (Exception $e) {
+                $validation_errors = $Task20->getValidationErrors();
+                Zend_Registry::get('logger')->debug( 
+                    'Could not migrate Infolog with info_id ' . $infolog->info_id . "\n" . 
+                    'Tasks_Setup_MigrateFromEgw14::infolog2Task: ' . $e->getMessage() . "\n" .
+                    "Tasks_Model_Task::validation_errors: \n" .
+                    print_r($validation_errors,true));
+                    continue;
+            }
+            $tasksBackend->createTask($Task20);
+        }
+    }
+    
+    /**
+     * Converts info_id to an uid
+     *
+     * @param int $_id
+     * @return string uid
+     */
+    protected static function id2uid($_id)
+    {
+        return $_id;
+    }
+    
+    /**
+     * Returns Container we can migrate tasks to
+     * 
+     * @todo Make shure we use same containers in calendar and tasks!
+     * ??? what about loose coupling in this case???
+     * 
+     * @param int $_owner
+     * @return int Egwbase_Container::container_id
+     */
+    protected static function getOwnersContainer($_owner)
+    {
+        static $containers = array();
+        
+        if (!isset($containers[$_owner])) {
+            $containers[$_owner] = Egwbase_Container::getInstance()->addContainer(
+                'Tasks',
+                'My Tasks (from egw 1.4 migration)',
+                Egwbase_Container::TYPE_PERSONAL,
+                Tasks_Backend_Factory::SQL
+            );
+            Egwbase_Container::getInstance()->addGrants($containers[$_owner], $_owner, array(
+                Egwbase_Container::GRANT_ANY
+            ));
+        }
+        return $containers[$_owner];  
+    }
+    
+    /**
+     * Returns Class identifier
+     *
+     * @param string $_egw14Class
+     * @return id identifier
+     */
+    protected static function getClass($_egw14Class)
+    {
+        static $classes;
+        
+        $oldclass = strtoupper($_egw14Class);
+        
+        if (!isset($classes[$oldclass])) {
+            $classTable = new Egwbase_Db_Table(array('name' => SQL_TABLE_PREFIX . 'class'));
+            $class = $classTable->fetchRow($classTable->getAdapter()->quoteInto('class LIKE ?', $oldclass));
+            if (!$class) {
+                $identifier = $classTable->insert(array(
+                    'created_by'    => Zend_Registry::get('currentAccount')->account_id,
+                    'creation_time' => Zend_Date::now()->getIso(),
+                    'class'         => $oldclass
+                ));
+                $classes[$oldclass] = $identifier;
+            } else {
+                $classes[$oldclass] = $class->identifier;
+            }
+            
+        }
+        return $classes[$oldclass];
+    }
+    
+    /**
+     * Returns Status identifier
+     *
+     * @param string $_egw14Status
+     * @return id identifier
+     */
+    protected static function getStatus($_egw14Status)
+    {
+        static $stati;
+        
+        $oldstatus = strtoupper($_egw14Status);
+        
+        if (!isset($stati[$oldstatus])) {
+            $statusTable = new Egwbase_Db_Table(array('name' => SQL_TABLE_PREFIX . 'tasks_status'));
+            $status = $statusTable->fetchRow($statusTable->getAdapter()->quoteInto('status LIKE ?', $oldstatus));
+            if (!$status) {
+                $identifier = $statusTable->insert(array(
+                    'created_by'    => Zend_Registry::get('currentAccount')->account_id,
+                    'creation_time' => Zend_Date::now()->getIso(),
+                    'status'         => $oldstatus
+                ));
+                $stati[$oldstatus] = $identifier;
+            } else {
+                $stati[$oldstatus] = $status->identifier;
+            }
+            
+        }
+        return $stati[$oldstatus];
+    }
+    
+}
---
+++ tine20/Tasks/Setup/SetupSqlTables.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * egroupware 2.0
+ * 
+ * @package     Tasks
+ * @subpackage  Setup
+ * @license     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2007-2007 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id: $
+ *
+ */
+
+/**
+ * Tables setup for Tasks 2.0
+ * @package Tasks
+ * @subpackage  Setup
+ */
+class Tasks_Setup_SetupSqlTables
+{
+    /**
+     * Creates Tasks 2.0 tables
+     * @return void
+     */
+    public static function createTasksTables() {
+        $db = Zend_Registry::get('dbAdapter');
+        
+        try {
+            $tableData = $db->describeTable(SQL_TABLE_PREFIX . 'tasks');
+        } catch (Zend_Db_Statement_Exception $e) {
+            $db->getConnection()->exec("CREATE TABLE " . SQL_TABLE_PREFIX . "class (
+                `identifier` INT(11) NOT NULL auto_increment,
+                `created_by` INT(11) NOT NULL,
+                `creation_time` DATETIME NOT NULL,
+                `last_modified_by` INT(11),
+                `last_modified_time` DATETIME DEFAULT NULL,
+                `is_deleted` BOOLEAN NOT NULL DEFAULT FALSE,
+                `deleted_time` DATETIME DEFAULT NULL,
+                `deleted_by` INT(11),
+                `class` VARCHAR(64) NOT NULL,
+                PRIMARY KEY  (`identifier`),
+                UNIQUE (`class`)) ENGINE=InnoDB DEFAULT CHARSET=utf8"
+            );
+            
+            $db->getConnection()->exec("CREATE TABLE " . SQL_TABLE_PREFIX . "tasks_status (
+                `identifier` INT(11) NOT NULL auto_increment,
+                `created_by` INT(11) NOT NULL,
+                `creation_time` DATETIME NOT NULL,
+                `last_modified_by` INT(11),
+                `last_modified_time` DATETIME DEFAULT NULL,
+                `is_deleted` BOOLEAN NOT NULL DEFAULT FALSE,
+                `deleted_time` DATETIME DEFAULT NULL,
+                `deleted_by` INT(11),
+                `status` VARCHAR(64) NOT NULL,
+                PRIMARY KEY  (`identifier`),
+                UNIQUE (`status`)) ENGINE=InnoDB DEFAULT CHARSET=utf8"
+            );
+
+            $db->getConnection()->exec("CREATE TABLE " . SQL_TABLE_PREFIX . "tasks (
+                `identifier` INT(11) NOT NULL auto_increment,
+                `container` INT(11) NOT NULL,
+                `created_by` INT(11) NOT NULL,
+                `creation_time` DATETIME NOT NULL,
+                `last_modified_by` INT(11),
+                `last_modified_time` DATETIME DEFAULT NULL,
+                `is_deleted` BOOLEAN NOT NULL DEFAULT FALSE,
+                `deleted_time` DATETIME DEFAULT NULL,
+                `deleted_by` INT(11),
+                `percent` INT(3),
+                `completed` DATETIME DEFAULT NULL,
+                `due` DATETIME DEFAULT NULL,
+                `class` INT(11),
+                `description` LONGTEXT,
+                `geo` FLOAT,
+                `location` VARCHAR(256),
+                `organizer` INT(11),
+                `priority` INT(11),
+                `status` INT(11),
+                `summaray` VARCHAR(256),
+                `url` VARCHAR(256),
+                PRIMARY KEY  (`identifier`),
+                KEY `" . SQL_TABLE_PREFIX . "tasks_container` (`container`),
+                KEY `" . SQL_TABLE_PREFIX . "tasks_organizer` (`organizer`),
+                FOREIGN KEY (`class`) REFERENCES " . SQL_TABLE_PREFIX . "class(`identifier`) ON DELETE RESTRICT,
+                FOREIGN KEY (`status`) REFERENCES " . SQL_TABLE_PREFIX . "tasks_status(`identifier`) ON DELETE RESTRICT)
+                ENGINE=InnoDB DEFAULT CHARSET=utf8"
+            );
+            
+            $db->getConnection()->exec("CREATE TABLE " . SQL_TABLE_PREFIX . "tasks_tag (
+                `task_identifier` INT(11) NOT NULL,
+                `tag_identifier` INT(11) NOT NULL,
+                PRIMARY KEY  (`task_identifier`, `tag_identifier`),
+                FOREIGN KEY (`task_identifier`) REFERENCES " . SQL_TABLE_PREFIX . "tasks(`identifier`) ON DELETE CASCADE) ENGINE=InnoDB DEFAULT CHARSET=utf8"
+            );
+            
+            $db->getConnection()->exec("CREATE TABLE " . SQL_TABLE_PREFIX . "tasks_contact (
+                `task_identifier` INT(11) NOT NULL,
+                `contact_identifier` INT(11) NOT NULL,
+                PRIMARY KEY  (`task_identifier`, `contact_identifier`),
+                FOREIGN KEY (`task_identifier`) REFERENCES " . SQL_TABLE_PREFIX . "tasks(`identifier`) ON DELETE CASCADE) ENGINE=InnoDB DEFAULT CHARSET=utf8"
+            );
+            
+            $db->getConnection()->exec("CREATE TABLE " . SQL_TABLE_PREFIX . "tasks_related (
+                `task_identifier` INT(11) NOT NULL,
+                `related_identifier` INT(11) NOT NULL,
+                PRIMARY KEY  (`task_identifier`, `related_identifier`),
+                FOREIGN KEY (`task_identifier`) REFERENCES " . SQL_TABLE_PREFIX . "tasks(`identifier`) ON DELETE CASCADE,
+                FOREIGN KEY (`related_identifier`) REFERENCES " . SQL_TABLE_PREFIX . "tasks(`identifier`)) ENGINE=InnoDB DEFAULT CHARSET=utf8"
+            );
+        }
+    }
+    
+    /**
+     * inserts default records
+     * @return void
+     */
+    public static function insertDefaultRecords() {
+        $db = Zend_Registry::get('dbAdapter');
+        $accountId = Zend_Registry::get('currentAccount')->account_id;
+        $now = $db->quote(Zend_Date::now()->getIso());
+        
+        // egw_class
+        $db->getConnection()->exec("INSERT INTO `" . SQL_TABLE_PREFIX . "class` (
+            `created_by`, `creation_time`, `class` ) VALUES (
+            $accountId, $now, 'PUBLIC')"
+        );
+        
+        $db->getConnection()->exec("INSERT INTO `" . SQL_TABLE_PREFIX . "class` (
+            `created_by`, `creation_time`, `class` ) VALUES (
+            $accountId, $now ,'PRIVATE')"
+        );
+        
+        $db->getConnection()->exec("INSERT INTO `" . SQL_TABLE_PREFIX . "class` (
+            `created_by`, `creation_time`, `class` ) VALUES (
+            $accountId, $now ,'CONFIDENTIAL')"
+        );
+        
+        // egw_tasks_status
+        $db->getConnection()->exec("INSERT INTO `" . SQL_TABLE_PREFIX . "tasks_status` (
+            `created_by`, `creation_time`, `status` ) VALUES (
+            $accountId, $now, 'NEEDS-ACTION')"
+        );
+        
+        $db->getConnection()->exec("INSERT INTO `" . SQL_TABLE_PREFIX . "tasks_status` (
+            `created_by`, `creation_time`, `status` ) VALUES (
+            $accountId, $now, 'COMPLETED')"
+        );
+        
+        $db->getConnection()->exec("INSERT INTO `" . SQL_TABLE_PREFIX . "tasks_status` (
+            `created_by`, `creation_time`, `status` ) VALUES (
+            $accountId, $now, 'IN-PROCESS')"
+        );
+        
+        $db->getConnection()->exec("INSERT INTO `" . SQL_TABLE_PREFIX . "tasks_status` (
+            `created_by`, `creation_time`, `status` ) VALUES (
+            $accountId, $now, 'CANCELLED')"
+        );
+    }
+    
+}
