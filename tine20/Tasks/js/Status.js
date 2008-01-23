3802ca8342e3ec5a7c92b065af2600a6348a8ad0
Cornelius Weiß <c.weiss@metaways.de>
2008-01-23 15:40:44 +0000

    fit the reworked accounts api
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@569 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tasks/js/Status.js
+++ tine20/Tasks/js/Status.js
@@ -8,43 +8,64 @@
  * @version     $Id$
  *
  */
-Ext.namespace('Egw.Tasks', 'Egw.Tasks.Status');
+Ext.namespace('Egw.Tasks', 'Egw.Tasks.status');
 
-Egw.Tasks.Status = function() {
+Egw.Tasks.status.ComboBox = Ext.extend(Ext.form.ComboBox, {
+	fieldLabel: 'status',
+    name: 'status',
+    displayField: 'status',
+    valueField: 'identifier',
+    mode: 'local',
+    triggerAction: 'all',
+    emptyText: 'Status...',
+    typeAhead: true,
+    selectOnFocus: true,
+    editable: false,
+    lazyInit: false,
 	
-	_init = function() {
-	    // initial data from http request
-	    Egw.Tasks.Status.Store = new Ext.data.JsonStore({
-	        fields: [ 
-	            { name: 'identifier'                                        },
-	            { name: 'created_by'                                        }, 
-	            { name: 'creation_time',      type: 'date', dateFormat: 'c' },
-	            { name: 'last_modified_by'                                  },
-	            { name: 'last_modified_time', type: 'date', dateFormat: 'c' },
-	            { name: 'is_deleted'                                        }, 
-	            { name: 'deleted_time',       type: 'date', dateFormat: 'c' }, 
-	            { name: 'deleted_by'                                        },
-	            { name: 'status'                                            }
-	       ],
-	       data: Egw.Tasks.InitialData.Status,
-		   autoLoad: true,
-	       id: 'identifier'
-	    });
-	};
-	
-	_getStatusName = function(identifier) {
-	    var status = Egw.Tasks.Status.Store.getById(identifier);
-	    return status.data.status;
-	};
-	
-	_getStatusIcon = function(identifier) {
-	    var name = _getStatusName(identifier);
-	    return '<div class="TasksMainGridStatus-' + name + '" ext:qtip="' + name + '"></div>';
-	};
-	
-	return{
-		init          : _init,
-		getStatusName : _getStatusName,
-		getStatusIcon : _getStatusIcon
-	};
-}();
+	//private
+    initComponent: function(){
+		this.store = Egw.Tasks.status.getStore();
+		if (this.autoExpand) {
+			this.on('focus', function(){
+				this.lazyInit = false;
+                this.expand();
+            });
+		}
+	    Egw.widgets.Percent.ComboBox.superclass.initComponent.call(this);
+	}
+        
+});
+
+Egw.Tasks.status.getStore = function() {
+	if (!store) {
+		var store = new Ext.data.JsonStore({
+            fields: [ 
+                { name: 'identifier'                                        },
+                { name: 'created_by'                                        }, 
+                { name: 'creation_time',      type: 'date', dateFormat: 'c' },
+                { name: 'last_modified_by'                                  },
+                { name: 'last_modified_time', type: 'date', dateFormat: 'c' },
+                { name: 'is_deleted'                                        }, 
+                { name: 'deleted_time',       type: 'date', dateFormat: 'c' }, 
+                { name: 'deleted_by'                                        },
+                { name: 'status'                                            }
+           ],
+		   // initial data from http request
+           data: Egw.Tasks.InitialData.Status,
+           autoLoad: true,
+           id: 'identifier'
+       });
+	}
+	return store;
+}
+
+Egw.Tasks.status.getStatusName = function(identifier) {
+	var status = Egw.Tasks.status.getStore().getById(identifier);
+    return status.data.status;
+};
+
+Egw.Tasks.status.getStatusIcon = function(identifier) {
+    var name = Egw.Tasks.status.getStatusName(identifier);
+    return '<div class="TasksMainGridStatus-' + name + '" ext:qtip="' + name + '"></div>';
+};
