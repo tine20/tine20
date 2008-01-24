54561ce2c6d75ddfd7ff8530a186ee1401dc1690
Cornelius Weiß <c.weiss@metaways.de>
2008-01-24 16:53:23 +0000

    - add a shema for user/server time conversion - finish create api
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@598 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tasks/Json.php
+++ tine20/Tasks/Json.php
@@ -22,7 +22,8 @@ class Tasks_Json extends Egwbase_Application_Json_Abstract
      */
     protected $_controller;
     
-    protected $_timezone;
+    protected $_userTimezone;
+    protected $_serverTimezone;
     
     public function __construct()
     {
@@ -31,7 +32,8 @@ class Tasks_Json extends Egwbase_Application_Json_Abstract
         } catch (Exception $e) {
             //error_log($e);
         }
-        $this->_timezone = Zend_Registry::get('userTimeZone');
+        $this->_userTimezone = Zend_Registry::get('userTimeZone');
+        $this->_serverTimezone = date_default_timezone_get();
     }
 
     /**
@@ -46,7 +48,7 @@ class Tasks_Json extends Egwbase_Application_Json_Abstract
         //error_log(print_r($filter->toArray(),true));
         
         $tasks = $this->_controller->searchTasks($filter);
-        $tasks->setTimezone($this->_timezone);
+        $tasks->setTimezone($this->_userTimezone);
         
         return array(
             'results' => $tasks->toArray(array('part' => Zend_Date::ISO_8601)),
@@ -62,27 +64,36 @@ class Tasks_Json extends Egwbase_Application_Json_Abstract
      */
     public function getTask($uid)
     {
+        
         return $this->_backend->getTask($uid)->toArray();
     }
     
     /**
      * Create a new Task
      *
-     * @param Tasks_Model_Task $_task
-     * @return Tasks_Model_Task
+     * @param  $task
+     * @return array the created task
      */
-    public function createTask(Tasks_Model_Task $_task)
+    public function createTask($task)
     {
+        date_default_timezone_set($this->_userTimezone);
+        $newTask = new Tasks_Model_Task(Zend_Json::decode($task));
+        $newTask->setTimezone($this->_serverTimezone);
+        date_default_timezone_set($this->_serverTimezone);
         
+        //error_log(print_r($newTask->toArray(),true));
+        $createdTask = $this->_controller->createTask($newTask);
+        $createdTask->setTimezone($this->_userTimezone);
+        return $createdTask->toArray();
     }
     
     /**
      * Upate an existing Task
      *
-     * @param Tasks_Model_Task $_task
-     * @return Tasks_Model_Task
+     * @param  $task
+     * @return array the updated task
      */
-    public function updateTask(Tasks_Model_Task $_task)
+    public function updateTask($task)
     {
         
     }
