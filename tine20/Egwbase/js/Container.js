0940cf1494e8a688c209901e6a93a8f1691aec8f
Cornelius Weiß <c.weiss@metaways.de>
2008-01-31 20:32:56 +0000

    trubble with svnx client :-(
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@724 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Egwbase/js/Container.js
@@ -0,0 +1,76 @@
+/*
+ * egroupware 2.0
+ * 
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2007-2007 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+
+Ext.namespace('Egw.Egwbase.container');
+
+/**
+ * Egwbase container class
+ * 
+ * @todo add generic container model
+ * @todo internal cache (store)
+ */
+Egw.Egwbase.container = {
+    /**
+     * constant for no grants
+     */
+    GRANT_NONE: 0,
+    /**
+     * constant for read grant
+     */
+    GRANT_READ: 1,
+    /**
+     * constant for add grant
+     */
+    GRANT_ADD: 2,
+    /**
+     * constant for edit grant
+     */
+    GRANT_EDIT: 4,
+    /**
+     * constant for delete grant
+     */
+    GRANT_DELETE: 8,
+    /**
+     * constant for admin grant
+     */
+    GRANT_ADMIN: 16,
+    /**
+     * constant for all grants
+     */
+    GRANT_ANY: 31,
+    /** 
+     * type for internal contaier
+     * for example the internal addressbook
+     */
+    TYPE_INTERNAL: 'internal',
+    /**
+     * type for personal containers
+     */
+    TYPE_PERSONAL: 'personal',
+    /**
+     * type for shared container
+     */
+    TYPE_SHARED: 'shared',
+	
+	/**
+	 * Models of Egwbase container
+	 * @property {Object}
+	 */
+	models: {
+		containerGrant: Ext.data.Record.create([
+            {name: 'accountId'},
+            {name: 'accountName'},
+            {name: 'readGrant'},
+            {name: 'addGrant'},
+            {name: 'editGrant'},
+            {name: 'deleteGrant'}
+        ])
+	}
+};
