9857bc34eb5486f3ac856b164a3a5fa4dc3a9d56
Lars Kneschke <l.kneschke@metaways.de>
2008-02-15 17:42:41 +0000

    Event classes for new and updated accounts
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@815 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Admin/Event/UpdateAccount.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Admin
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * event class for updated account
+ *
+ * @package     Admin
+ */
+class Admin_Event_UpdateAccount extends Egwbase_Events_Abstract 
+{
+    /**
+     * the just added account
+     *
+     * @var Egwbase_Account_Model_FullAccount
+     */
+    public $account;
+}
