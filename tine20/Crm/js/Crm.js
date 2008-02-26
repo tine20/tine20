4e49a43659ac75c88d8d7de6909d8df57352787a
Cornelius Weiß <c.weiss@metaways.de>
2008-02-26 08:11:38 +0000

    make use of the new Ext.ux.grid.CheckColumn user extension
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@872 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Crm/js/Crm.js
+++ tine20/Crm/js/Crm.js
@@ -693,7 +693,7 @@ Egw.Crm.Main = function(){
             
             storeLeadstate.load();
             
-           var checkColumn = new Ext.grid.CheckColumn({
+           var checkColumn = new Ext.ux.grid.CheckColumn({
                header: "X Lead?",
                dataIndex: 'lead_leadstate_endslead',
                width: 50
@@ -821,39 +821,6 @@ Egw.Crm.Main = function(){
                     }]  
                 });
 
-
-            Ext.grid.CheckColumn = function(config){
-                Ext.apply(this, config);
-                if(!this.id){
-                    this.id = Ext.id();
-                }
-                this.renderer = this.renderer.createDelegate(this);
-            };
-            
-            Ext.grid.CheckColumn.prototype ={
-                init : function(grid){
-                    this.grid = grid;
-                    this.grid.on('render', function(){
-                        var view = this.grid.getView();
-                        view.mainBody.on('mousedown', this.onMouseDown, this);
-                    }, this);
-                },
-            
-                onMouseDown : function(e, t){
-                    if(t.className && t.className.indexOf('x-grid3-cc-'+this.id) != -1){
-                        e.stopEvent();
-                        var index = this.grid.getView().findRowIndex(t);
-                        var record = this.grid.store.getAt(index);
-                        record.set(this.dataIndex, !record.data[this.dataIndex]);
-                    }
-                },
-            
-                renderer : function(v, p, record){
-                    p.css += ' x-grid3-check-col-td'; 
-                    return '<div class="x-grid3-check-col'+(v?'-on':'')+' x-grid3-cc-'+this.id+'">&#160;</div>';
-                }
-            };                    
-                        
           Dialog.add(leadstateGridPanel);
           Dialog.show();          
         }
