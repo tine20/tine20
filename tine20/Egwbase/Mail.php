98d18442170d11ee63a36aa094ccfe5c9bb6f6db
Lars Kneschke <l.kneschke@metaways.de>
2008-02-27 14:35:14 +0000

    This class extends the Zend_Mail class
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@893 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Egwbase/Mail.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Egwbase
+ * @subpackage  Mail
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * This class extends the Zend_Mail class 
+ *
+ * @package     Egwbase
+ * @subpackage  Mail
+ */
+class Egwbase_Mail extends Zend_Mail
+{
+    /**
+     * Sender: address
+     * @var string
+     */
+    protected $_sender = null;
+    
+    /**
+     * Sets Sender-header and sender of the message
+     *
+     * @param  string    $email
+     * @param  string    $name
+     * @return Zend_Mail Provides fluent interface
+     * @throws Zend_Mail_Exception if called subsequent times
+     */
+    public function setSender($email, $name = '')
+    {
+        if ($this->_sender === null) {
+            $email = strtr($email,"\r\n\t",'???');
+            $this->_from = $email;
+            $this->_storeHeader('Sender', $this->_encodeHeader('"'.$name.'"').' <'.$email.'>', true);
+        } else {
+            throw new Zend_Mail_Exception('Sender Header set twice');
+        }
+        return $this;
+    }
+}
