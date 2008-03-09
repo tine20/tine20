33a223c4e9ab744cbd7725940566bb00f52b0dbf
Lars Kneschke <l.kneschke@metaways.de>
2008-03-09 07:50:46 +0000

    fixed formating
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@977 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Setup/Tables.php
+++ tine20/Setup/Tables.php
@@ -17,20 +17,20 @@
  */
 class Setup_Tables
 {
-	private $tineExtDb;
-	private $prefix; 
-	private $_config;
-	
-	public function __construct()
-	{
-		try {
-			$this->_config = new Zend_Config_Ini($_SERVER['DOCUMENT_ROOT'] . '/../config.ini');
-		} catch (Zend_Config_Exception $e) {
-			die ('central configuration file ' . $_SERVER['DOCUMENT_ROOT'] . '/../config.ini not found');
-		}
-		
-		$this->setupDatabaseConnection();
-	}
+    private $tineExtDb;
+    private $prefix;
+    private $_config;
+
+    public function __construct()
+    {
+        try {
+            $this->_config = new Zend_Config_Ini($_SERVER['DOCUMENT_ROOT'] . '/../config.ini');
+        } catch (Zend_Config_Exception $e) {
+            die ('central configuration file ' . $_SERVER['DOCUMENT_ROOT'] . '/../config.ini not found');
+        }
+
+        $this->setupDatabaseConnection();
+    }
 
     /**
      * initializes the database connection
@@ -40,11 +40,11 @@ class Setup_Tables
     {
         if(isset($this->_config->database)) {
             $dbConfig = $this->_config->database;
-            
+
             $this->prefix = $dbConfig->get('tableprefix') ? $dbConfig->get('tableprefix') : 'tine20_';
-            
+
             echo "setting table prefix to: {$this->prefix} <hr>";
-        
+
             $db = Zend_Db::factory('PDO_MYSQL', $dbConfig->toArray());
             Zend_Db_Table_Abstract::setDefaultAdapter($db);
 
@@ -53,51 +53,51 @@ class Setup_Tables
             die ('database section not found in central configuration file');
         }
     }
-    
-	public function createTable($_table)
-	{
-		$statement = "CREATE TABLE IF NOT EXISTS `" . $this->prefix . $_table['name'] . "` (\n";
-	
-		foreach ($_table->fields[0] as $field) {
-			if($field['name'] != '') {
-				$statement .= "`" . $field['name'] . "` " . $field['type'] . " " . $field['NULL'];
-				if (isset($field['extra'])) {
-					$statement .= " " . $field['extra'];
-				}
-				$statement .=",\n";
-			}
-		}
-	
-		foreach ($_table->keys[0] as $key) {
-			$statement .= " " . $key['type'] . " `" . $this->prefix . $key['name'] . "` (" ;
-				
-			foreach ($key->keyfield as $keyfield) {
-				$statement .= "`"  . (string)$keyfield . "`,";
-			}
-			
-			$statement = substr($statement, 0, (strlen($statement)-1)) . "),";	
-		}
-	
-		$statement = substr($statement, 0, (strlen($statement)-1)) ;
-		$statement .= ")";
-	
-		$statement .= 	"\n ENGINE=" . $_table->engine . " DEFAULT CHARSET=" . $_table->charset; 
-		
-		echo $statement . "<hr>";
-		
-		Zend_Registry::get('dbAdapter')->query($statement);
-	}
+
+    public function createTable($_table)
+    {
+        $statement = "CREATE TABLE IF NOT EXISTS `" . $this->prefix . $_table['name'] . "` (\n";
+
+        foreach ($_table->fields[0] as $field) {
+            if($field['name'] != '') {
+                $statement .= "`" . $field['name'] . "` " . $field['type'] . " " . $field['NULL'];
+                if (isset($field['extra'])) {
+                    $statement .= " " . $field['extra'];
+                }
+                $statement .=",\n";
+            }
+        }
+
+        foreach ($_table->keys[0] as $key) {
+            $statement .= " " . $key['type'] . " `" . $this->prefix . $key['name'] . "` (" ;
+
+            foreach ($key->keyfield as $keyfield) {
+                $statement .= "`"  . (string)$keyfield . "`,";
+            }
+            	
+            $statement = substr($statement, 0, (strlen($statement)-1)) . "),";
+        }
+
+        $statement = substr($statement, 0, (strlen($statement)-1)) ;
+        $statement .= ")";
+
+        $statement .= 	"\n ENGINE=" . $_table->engine . " DEFAULT CHARSET=" . $_table->charset;
+
+        //echo $statement . "<hr>";
+
+        Zend_Registry::get('dbAdapter')->query($statement);
+    }
 
 
 
-	public function parseFile($_file)
-	{
-		$xml = simplexml_load_file($_file);
-		
-		foreach ($xml->applications[0] as $application) {
-			foreach ($application->table as $table) {
-				$this->createTable($table);
-			}
-		}
-	}
+    public function parseFile($_file)
+    {
+        $xml = simplexml_load_file($_file);
+
+        foreach ($xml->applications[0] as $application) {
+            foreach ($application->table as $table) {
+                $this->createTable($table);
+            }
+        }
+    }
 }
