a893959279a500a6e06dc7a711c65f2ff6efc94c
Lars Kneschke <l.kneschke@metaways.de>
2008-03-12 08:46:59 +0000

    the group model
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1020 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/Group/Model/Group.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Tinebase
+ * @subpackage  Group
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * defines the datatype for the group object
+ * 
+ * @package     Tinebase
+ * @subpackage  Group
+ */
+class Tinebase_Group_Model_Group extends Tinebase_Record_Abstract
+{
+    /**
+     * list of zend inputfilter
+     * 
+     * this filter get used when validating user generated content with Zend_Input_Filter
+     *
+     * @var array
+     */
+    protected $_filters = array(
+        'id'            => 'Digits',
+        'name'          => 'StringTrim',
+        'description'   => 'StringTrim',
+    );
+    
+    /**
+     * list of zend validator
+     * 
+     * this validators get used when validating user generated content with Zend_Input_Filter
+     *
+     * @var array
+     */
+    protected $_validators = array(
+        'id'            => array('Digits', 'allowEmpty' => true),
+        'name'          => array('presence' => 'required'),
+        'description'   => array('allowEmpty' => true)
+    );
+    
+   /**
+     * key in $_validators/$_properties array for the filed which 
+     * represents the identifier
+     * 
+     * @var string
+     */    
+    protected $_identifier = 'id';    
+}
