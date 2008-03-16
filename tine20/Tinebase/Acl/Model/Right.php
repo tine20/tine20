e61bdeff6aae183aa9e033bc07dde31ee308467a
Lars Kneschke <l.kneschke@metaways.de>
2008-03-16 10:56:23 +0000

    added Right model
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1079 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/Acl/Model/Right.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * model to handle rights
+ * 
+ * @package     Tinebase
+ * @subpackage  Acl
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * defines the datatype for rights
+ * 
+ * @package     Tinebase
+ * @subpackage  Acl
+ *  */
+class Tinebase_Acl_Model_Right extends Tinebase_Record_Abstract
+{
+	/**
+     * key in $_validators/$_properties array for the filed which 
+     * represents the identifier
+     * 
+     * @var string
+     */    
+    protected $_identifier = 'id';
+    
+    /**
+     * application the record belongs to
+     *
+     * @var string
+     */
+    protected $_application = 'Tinebase';
+    
+    /**
+     * list of zend inputfilter
+     * 
+     * this filter get used when validating user generated content with Zend_Filter_Input
+     *
+     * @var array
+     */
+    protected $_filters = array(
+        //'*'      => 'StringTrim'
+    );
+
+    /**
+     * list of zend validator
+     * 
+     * this validators get used when validating user generated content with Zend_Filter_Input
+     *
+     * @var array
+     */
+    protected $_validators = array();
+
+    /**
+     * @see Tinebase_Record_Abstract
+     */
+    public function __construct($_data = NULL, $_bypassFilters = false, $_convertDates = NULL)
+    {
+        $this->_validators = array(
+            'id'                => array('presence' => 'required', 'allowEmpty' => true),
+            'application_id'    => array('presence' => 'required'),
+            'account_id'        => array('presence' => 'required'),
+            'account_type'      => array(
+                new Zend_Validate_InArray(array('account', 'group', 'anyone'), FALSE) 
+            ),
+            'right'             => array('presence' => 'required')
+        );
+        
+        return parent::__construct($_data, $_bypassFilters);
+    }
+}
