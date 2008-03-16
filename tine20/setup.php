4dd403b1cdeb3a935ce8aff645aaa891e6bd35a7
Lars Kneschke <l.kneschke@metaways.de>
2008-03-16 20:37:08 +0000

    improved setup again
    setup prepares now the environment as much as possible
    
    - it creates two groups Administrators and Users
    - it created one admin account: tine20admin
    - it enables all applications for the Users group and gives the Administrators group admins rights for all applications
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1091 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/setup.php
+++ tine20/setup.php
@@ -32,5 +32,56 @@ foreach ( new DirectoryIterator('./') as $item ) {
 	}
 }
 
+# either import data from eGroupWare 1.4
 #$import = new Setup_Import_Egw14();
 #$import->import();
+
+# or initialize the database ourself
+
+# add the admin group
+$adminGroup = new Tinebase_Group_Model_Group(array(
+    'name'          => 'Adminstrators',
+    'description'   => 'Group of administrative accounts'
+));
+Tinebase_Group_Sql::getInstance()->addGroup($adminGroup);
+
+# add the user group
+$userGroup = new Tinebase_Group_Model_Group(array(
+    'name'          => 'Users',
+    'description'   => 'Group of user accounts'
+));
+Tinebase_Group_Sql::getInstance()->addGroup($userGroup);
+
+# add the admin account
+$account = new Tinebase_Account_Model_FullAccount(array(
+    'accountLoginName'      => 'tine20admin',
+    'accountStatus'         => 'enabled',
+    'accountPrimaryGroup'   => $userGroup->id,
+    'accountLastName'       => 'Account',
+    'accountFirstName'      => 'Tine 2.0 Admin'
+));
+Tinebase_Account_Sql::getInstance()->addAccount($account);
+
+# set the password for the tine20admin account
+Tinebase_Auth::getInstance()->setPassword('tine20admin', 'lars', 'lars');
+
+Tinebase_Group::getInstance()->addGroupMember($adminGroup, $account);
+Tinebase_Group::getInstance()->addGroupMember($userGroup, $account);
+
+foreach(Tinebase_Application::getInstance()->getApplications() as $application) {
+    $right = new Tinebase_Acl_Model_Right(array(
+        'application_id'    => $application,
+        'account_id'        => $userGroup,
+        'account_type'      => 'group',
+        'right'             => Tinebase_Acl_Rights::RUN
+    ));
+    Tinebase_Acl_Rights::getInstance()->addRight($right);
+
+    $right = new Tinebase_Acl_Model_Right(array(
+        'application_id'    => $application,
+        'account_id'        => $adminGroup,
+        'account_type'      => 'group',
+        'right'             => Tinebase_Acl_Rights::ADMIN
+    ));
+    Tinebase_Acl_Rights::getInstance()->addRight($right);
+}
