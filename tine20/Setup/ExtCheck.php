f25dd7e01a3148d0157aae5ba878acae87ed1a4f
Matthias Greiling <m.greiling@metaways.de>
2008-03-19 20:40:50 +0000

    searchable output
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1265 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Setup/ExtCheck.php
+++ tine20/Setup/ExtCheck.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Tine 2.0
- * 
+ *
  * @package     Setup
  * @license     http://www.gnu.org/licenses/agpl.html
  * @author      Matthias Greiling <m.greiling@metaways.de>
  * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
- * @version     $Id: Tables.php 1063 2008-03-14 16:28:12Z lkneschke $ 
+ * @version     $Id: Tables.php 1063 2008-03-14 16:28:12Z lkneschke $
  *
  */
 
@@ -14,7 +14,7 @@
 * checks local PHP installation for needed configuration details defined in essentials.xml
 */
 
-		
+
 /*
 * Output factory
 */
@@ -31,17 +31,17 @@ interface TableFactory
 * abstract output classes
 */
 
-abstract class Table 
+abstract class Table
 {
 	protected $header = NULL;
 	protected $rows = array();
-	
-	public function setHeader(Header $header) 
+
+	public function setHeader(Header $header)
 	{
 		$this->header = $header;
 	}
 
-	public function addRow(Row $row) 
+	public function addRow(Row $row)
 	{
 		$this->rows[] = $row;
 	}
@@ -49,10 +49,10 @@ abstract class Table
 	abstract public function display();
 }
 
-abstract class Row 
+abstract class Row
 {
 	protected $cells = array();
-	
+
 	public function addCell(Cell $cell)
 	{
 		$this->cells[] = $cell;
@@ -64,17 +64,17 @@ abstract class Row
 abstract class Header extends Row {}
 
 
-abstract class Cell 
+abstract class Cell
 {
 	protected $content = NULL;
 	protected $color = NULL;
-	
+
 	public function __construct($content, $color = NULL)
 	{
 		$this->content = $content;
 		$this->color = $color;
 	}
-	
+
 	abstract public function display();
 }
 
@@ -82,9 +82,9 @@ abstract class Cell
 Text Output
 */
 
-class TextCell extends Cell 
+class TextCell extends Cell
 {
-	public function display() 
+	public function display()
 	{
 		echo "|" . str_pad($this->content, 20) ;
 	}
@@ -93,14 +93,14 @@ class TextCell extends Cell
 
 class TextRow extends Row
 {
-	public function display() 
-	{		
+	public function display()
+	{
 		echo "\n";
 		foreach ($this->cells as $cell)
 		{
-			$cell->display();	
+			$cell->display();
 		}
-		
+
 		echo "|";
 		echo "\n+" . str_repeat("-", (count($this->cells) * 21) -1) . "+";
 	}
@@ -108,15 +108,15 @@ class TextRow extends Row
 
 class TextHeader extends Header
 {
-	public function display() 
+	public function display()
 	{
 		echo "+" . str_repeat("-", (count($this->cells) * 21) -1) . "+\n";
-		
+
 		foreach ($this->cells as $cell)
 		{
-			$cell->display();	
+			$cell->display();
 		}
-		
+
 		echo "|";
 		echo "\n+" . str_repeat("-", (count($this->cells) * 21) -1) . "+";
 	}
@@ -127,31 +127,31 @@ class TextTable extends Table
 	public function display()
 	{
 		$this->header->display();
-		
+
 		foreach ($this->rows as $row)
 		{
-			$row->display();	
+			$row->display();
 		}
 		echo "\n";
-		
+
 	}
 
 }
 
 class TextTableFactory implements TableFactory
 {
-	public function createTable() 
+	public function createTable()
 	{
 		$table = new TextTable();
 		return $table;
 	}
-	
+
 	public function createRow()
 	{
 		$row = new TextRow();
 		return $row;
 	}
-	
+
 	public function createHeader()
 	{
 		$header = new TextHeader();
@@ -170,17 +170,17 @@ class TextTableFactory implements TableFactory
 HTML Output
 */
 
-class HTMLCell extends Cell 
+class HTMLCell extends Cell
 {
-	public function display() 
+	public function display()
 	{
 		if ($this->color)
 		{
-			echo "\n\t\t\t<td style=\"color:" . $this->color . "\">" . $this->content . "</td>";
+			return "\n\t\t\t<td style=\"color:" . $this->color . "\">" . $this->content . "</td>";
 		}
-		else 
+		else
 		{
-			echo "\n\t\t\t<td>" . $this->content . "</td>";
+			return "\n\t\t\t<td>" . $this->content . "</td>";
 		}
 	}
 
@@ -188,30 +188,30 @@ class HTMLCell extends Cell
 
 class HTMLRow extends Row
 {
-	public function display() 
+	public function display()
 	{
-		echo "\n\t\t<tr>";
-		
+		$buffer =  "\n\t\t<tr>";
+
 		foreach ($this->cells as $cell)
 		{
-			$cell->display();	
+			$buffer .= $cell->display();
 		}
-		echo "\n\t\t</tr>";
+		return $buffer . "\n\t\t</tr>";
 	}
 }
 
 class HTMLHeader extends Header
 {
-	public function display() 
+	public function display()
 	{
-		echo "\n\t\t<tr style=\"font-weight: bold;\">";
-		
+		$buffer = "\n\t\t<tr style=\"font-weight: bold;\">";
+
 		foreach ($this->cells as $cell)
 		{
-			$cell->display();	
+			$buffer .=$cell->display();
 		}
-		
-		echo "\n\t\t</tr>";
+
+		return $buffer .  "\n\t\t</tr>";
 	}
 }
 
@@ -219,34 +219,34 @@ class HTMLTable extends Table
 {
 	public function display()
 	{
-		echo "\n\t<table border>";
-		
+		$buffer = "\n\t<table border>";
+
 		$this->header->display();
-		
+
 		foreach ($this->rows as $row)
 		{
-			$row->display();	
+			$buffer .= $row->display();
 		}
-		
-		echo "\n\t</table>";
+
+		return $buffer . "\n\t</table>";
 	}
 
 }
 
 class HTMLTableFactory implements TableFactory
 {
-	public function createTable() 
+	public function createTable()
 	{
 		$table = new HtmlTable();
 		return $table;
 	}
-	
+
 	public function createRow()
 	{
 		$row = new HTMLRow();
 		return $row;
 	}
-	
+
 	public function createHeader()
 	{
 		$header = new HTMLHeader();
@@ -268,31 +268,31 @@ class HTMLTableFactory implements TableFactory
 class ExtensionList
 {
 	protected $tableFactory = NULL;
-	
+
 	public function __construct(TableFactory $tableFactory)
 	{
 		$this->tableFactory = $tableFactory;
 	}
-	
+
 	public function showTable($data)
 	{
 		$table = $this->tableFactory->createTable();
-		
+
 		$header = $this->tableFactory->createHeader();
 		$header->addCell($this->tableFactory->createCell('Item checked'));
 		$header->addCell($this->tableFactory->createCell('Status'));
-		
+
 		$table->setHeader($header);
-		
+
 		foreach ($data as $line)
 		{
 			$row = $this->tableFactory->createRow();
 			$table->addRow($row);
-		
+
 			foreach ($line as $field)
 			{
 				$cell = $this->tableFactory->createCell($field);
-				
+
 				if ($field == 'FAILURE')
 				{
 					$color = "#ff0000";
@@ -303,41 +303,42 @@ class ExtensionList
 					$color = "#00ff00";
 					$cell = $this->tableFactory->createCell($field, $color);
 				}
-				
-				
+
+
 				$row->addCell($cell);
 			}
-		
+
 		}
-		
-		$table->display();
+
+		return $table->display();
 	}
-	
+
 }
 
 
-class Setup_ExtCheck 
+class Setup_ExtCheck
 {
 	private $loadedExtensions = array();
-	
+
 	public $values = array();
-	
+
 	public $output = '';
 	/*
 	* * read configuration
 	*/
 	private function _getConfiguration($_file)
 	{
-		if ($fileHandle = fopen($_file, 'r')) 
+		if ($fileHandle = fopen($_file, 'r'))
 		{
+			$buffer = '';
 		    while (!feof($fileHandle))
 		    {
 		       $buffer .= fgets($fileHandle, 4096);
 		    }
 		    fclose($fileHandle);
-			
+
 			$values = array();
-			$parser = xml_parser_create();    
+			$parser = xml_parser_create();
 			xml_parse_into_struct($parser, $buffer, $values);
 			xml_parser_free($parser);
 		}
@@ -346,31 +347,31 @@ class Setup_ExtCheck
 
 	private function _check()
 	{
-		foreach ($this->values as $key => $value) 
+		foreach ($this->values as $key => $value)
 		{
 			if ($value['tag'] == 'ENVIROMENT')
 			{
-				if ($value['attributes']['NAME'] == 'Zend') 
+				if ($value['attributes']['NAME'] == 'Zend')
 				{
 					if (version_compare($value['attributes']['VERSION'], zend_version(), '<'))
 					{
 						$data[] = array($value['attributes']['NAME'], 'SUCCESS');
-					} 
+					}
 					else
 					{
 						$data[] = array($value['attributes']['NAME'], 'FAILURE');
-					}					
-				} 
-				else if ($value['attributes']['NAME'] == 'PHP') 
+					}
+				}
+				else if ($value['attributes']['NAME'] == 'PHP')
 				{
 					if (version_compare($value['attributes']['VERSION'], phpversion(), '<'))
 					{
 						$data[] = array($value['attributes']['NAME'], 'SUCCESS');
-					} 
+					}
 					else
 					{
 						$data[] = array($value['attributes']['NAME'], 'FAILURE');
-					}					
+					}
 				}
 			}
 
@@ -380,13 +381,13 @@ class Setup_ExtCheck
 				{
 					if (is_array($extensionArray))
 					{
-						$succeeded = false; 
-						
+						$succeeded = false;
+
 						if (in_array($extensionArray['NAME'], $this->loadedExtensions))
 						{
 							$passed[] = true;
 
-							if ($values[($key + 1)]['tag'] == 'INISET')
+							if ($this->values[($key + 1)]['tag'] == 'INISET')
 							{
 								$iniSettings = ini_get_all($extensionArray['NAME']);
 			//($iniSettings);
@@ -426,51 +427,51 @@ class Setup_ExtCheck
 									}
 									$i++;
 								}
-								
+
 							}
 						if (!in_array(false, $passed))
 							{
 								$succeeded = true;
 							}
-							else 
+							else
 							{
 								$succeeded = false;
 							}
 							unset($passed);
 							unset($iniSettings);
 						}
-						
+
 						if($succeeded)
 						{
 							$data[] = array($extensionArray['NAME'], 'SUCCESS');
 						}
-						else 
+						else
 						{
 							$data[] = array($extensionArray['NAME'], 'FAILURE');
-						}			
+						}
 					}
 				}
 			}
 		}
 		return $data;
 	}
-	
+
 	public function getOutput()
 	{
-		$this->output = $this->list->showTable($this->_check());
+		return $this->output = $this->list->showTable($this->_check());
 	}
-	
+
 	public function __construct($_file = NULL)
 	{
 		if (isset($_SERVER['SHELL']) || isset($_SERVER['ProgramFiles']))  // Unix-Shell; Windows-Kommandozeile
 		{
-			$this->list = new ExtensionList(new TextTableFactory()); 
+			$this->list = new ExtensionList(new TextTableFactory());
 		}
 		else
 		{
-			$this->list = new ExtensionList(new HTMLTableFactory()); 
+			$this->list = new ExtensionList(new HTMLTableFactory());
 		}
-		
+
 		/*
 		* fetch local server info
 		*/
@@ -480,4 +481,4 @@ class Setup_ExtCheck
 	}
 }
 
-	
+
