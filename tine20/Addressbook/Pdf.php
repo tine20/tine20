e195681f3dbefe0f3db1a84768d0085dd3974446
Lars Kneschke <l.kneschke@metaways.de>
2008-03-26 21:19:16 +0000

    fixed formatting
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1413 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Addressbook/Pdf.php
+++ tine20/Addressbook/Pdf.php
@@ -19,69 +19,66 @@
  */
 class Addressbook_Pdf extends Tinebase_Export_Pdf
 {
-	
-	
-	/**
+    /**
      * create contact pdf
      *
      * @param	Addressbook_Model_Contact contact data
-     * 
+     *
      * @return	string	the contact pdf
      */
-	public function contactPdf ( Addressbook_Model_Contact $_contact )
-	{
+    public function contactPdf ( Addressbook_Model_Contact $_contact )
+    {
         $translate = new Zend_Translate('gettext', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'translations', null, array('scan' => Zend_Translate::LOCALE_FILENAME));
         $translate->setLocale(Zend_Registry::get('locale'));
-	    
-		$contactFields = array ( 
-					'Business Contact Info' => 'separator',
-			        'org_name' => $translate->_('Organisation'),
-			        'org_unit' => $translate->_('Organisation Unit'),
-					'adr_one_street' => $translate->_('Street'),
-			        'adr_one_street2' => $translate->_('Street 2'),
-			        'adr_one_postalcode' => $translate->_('Postalcode'),
-					'adr_one_locality' => $translate->_('City'),
-			        'adr_one_region' => $translate->_('Region'),
-					'adr_one_countryname' => $translate->_('Country'),
-			        'email' => $translate->_('Email'),
-					'tel_work' => $translate->_('Telephone Work'),
-			        'tel_cell' => $translate->_('Telephone Cellphone'),
-					'tel_car' => $translate->_('Telephone Car'),
-					'tel_fax' => $translate->_('Telephone Fax'),
-					'tel_pager' => $translate->_('Telephone Page'),
-			        'url' => $translate->_('URL'),
-					'role' => $translate->_('Role'),
-					'assistent' => $translate->_('Assistant'),
-			        'tel_assistent' => $translate->_('Assistant Telephone'),
-		
-					'Private Contact Info' => 'separator',
-			        'adr_two_street' => 'Street',
-			        'adr_two_street2' => 'Street 2',
-			        'adr_two_postalcode' => 'Postalcode',
-					'adr_two_locality' => 'City',
-			        'adr_two_region' => 'Region',
-					'adr_two_countryname' => 'Country',
-			        'email_home' => 'Email Home',
-			        'tel_home' => 'Telephone Home',
-					'tel_cell_private' => 'Telephone Cellphone Private',
-			        'tel_fax_home' => 'Telephone Fax Home',
-			        'url_home' => 'URL Home',
-		
-			        'Other Infos' => 'separator',
-			        'bday' => 'Birthday',
-			        'title' => 'Title',
-					'id' => 'Contact ID',
-		
-			        //'owner' => 'Owner',
-			        //'n_prefix' => 'Name Prefix',
-			        //'n_suffix' => 'Name Suffix',
+         
+        $contactFields = array (
+            'Business Contact Info' => 'separator',
+            'org_name' => $translate->_('Organisation'),
+            'org_unit' => $translate->_('Organisation Unit'),
+            'adr_one_street' => $translate->_('Street'),
+            'adr_one_street2' => $translate->_('Street 2'),
+            'adr_one_postalcode' => $translate->_('Postalcode'),
+            'adr_one_locality' => $translate->_('City'),
+            'adr_one_region' => $translate->_('Region'),
+            'adr_one_countryname' => $translate->_('Country'),
+            'email' => $translate->_('Email'),
+            'tel_work' => $translate->_('Telephone Work'),
+            'tel_cell' => $translate->_('Telephone Cellphone'),
+            'tel_car' => $translate->_('Telephone Car'),
+            'tel_fax' => $translate->_('Telephone Fax'),
+            'tel_pager' => $translate->_('Telephone Page'),
+            'url' => $translate->_('URL'),
+            'role' => $translate->_('Role'),
+            'assistent' => $translate->_('Assistant'),
+            'tel_assistent' => $translate->_('Assistant Telephone'),
+            
+            'Private Contact Info' => 'separator',
+            'adr_two_street' => 'Street',
+            'adr_two_street2' => 'Street 2',
+            'adr_two_postalcode' => 'Postalcode',
+            'adr_two_locality' => 'City',
+            'adr_two_region' => 'Region',
+            'adr_two_countryname' => 'Country',
+            'email_home' => 'Email Home',
+            'tel_home' => 'Telephone Home',
+            'tel_cell_private' => 'Telephone Cellphone Private',
+            'tel_fax_home' => 'Telephone Fax Home',
+            'url_home' => 'URL Home',
+            
+            'Other Infos' => 'separator',
+            'bday' => 'Birthday',
+            'title' => 'Title',
+        
+            //'id' => 'Contact ID',    
+            //'owner' => 'Owner',
+            //'n_prefix' => 'Name Prefix',
+            //'n_suffix' => 'Name Suffix',
 		);
-
-		//@todo	include contact photo here
-		$contactPhoto = Zend_Pdf_Image::imageWithPath(dirname(dirname(__FILE__)).'/images/empty_photo.jpg');		
-		
-		return $this->generatePdf($_contact, $_contact->n_fn, $_contact->note, $contactFields, $contactPhoto );
-		
+        
+        //@todo	include contact photo here
+        $contactPhoto = Zend_Pdf_Image::imageWithPath(dirname(dirname(__FILE__)).'/images/empty_photo.jpg');		
+        
+        return $this->generatePdf($_contact, $_contact->n_fn, $_contact->note, $contactFields, $contactPhoto );        
 	}
 	
 
