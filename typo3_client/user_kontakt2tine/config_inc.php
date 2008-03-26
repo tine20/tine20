0e84e301ba277b7946fec0735bca4ae1d8827072
Matthias Greiling <m.greiling@metaways.de>
2008-03-26 16:01:32 +0000

    Stand-alone typo3-extension with own zend-directory (about 300 kB)
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1399 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- typo3_client/user_kontakt2tine/config_inc.php
+++ typo3_client/user_kontakt2tine/config_inc.php
@@ -4,17 +4,17 @@ $kontaktnumlinks = 5;
 function user_kontakt2tine_linkliste() {
   global $kontaktnumlinks;
   $linkliste = '';
-  for($i=0;$i<$kontaktnumlinks;$i++) {
+  for ($i = 1; $i < ($kontaktnumlinks + 1); $i++) {
     $linkliste .= '
-	<auswahl_'.($i+1).'>
+	<auswahl_'. $i . '>
       <TCEforms>
-        <label>Thema der Anfrage '.($i+1).'</label>
+        <label>Thema der Anfrage '.$i.'</label>
           <config>
             <type>input</type>
             <size>50</size>
           </config>
       </TCEforms>
-    </auswahl_'.($i+1).'>	
+    </auswahl_' . $i . '>	
 	';
   }
   return $linkliste;
