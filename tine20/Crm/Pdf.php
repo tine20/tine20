e521d35e7caad5f3296c353bd6a44db0946455ed
Philipp Schüle <p.schuele@metaways.de>
2008-03-26 16:03:14 +0000

    added pdf export class to crm
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1400 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Crm/Pdf.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * crm pdf generation class
+ *
+ * @package     Crm
+ * @subpackage	PDF
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ */
+
+
+/**
+ * crm pdf export class
+ * 
+ * @package     Crm
+ * @subpackage	PDF
+ */
+class Crm_Pdf extends Tinebase_Export_Pdf
+{
+	
+	
+	/**
+     * create lead pdf
+     *
+     * @param	Crm_Model_Lead lead data
+     * 
+     * @return	string	the contact pdf
+     */
+	public function leadPdf ( Crm_Model_Lead $_lead )
+	{
+		//@todo	add fields
+		$leadFields = array ();
+		
+		return $this->generatePdf($_lead, $_lead->lead_name, "", $leadFields );
+		
+	}
+	
+
+	/**
+     * create lead list pdf
+     *
+     * @param	array leads data
+     * 
+     * @return	string	the lead list pdf
+     * 
+     * @todo	implement
+     */
+	public function leadListPdf ( array $_leads )
+	{
+		return $this->generateListPdf($_leads);
+	}
+		
+
+}
