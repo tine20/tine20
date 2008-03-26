624a56f7c6b07040a75f6ca8ee02b2f4a7ce31f4
Philipp Schüle <p.schuele@metaways.de>
2008-03-26 16:20:11 +0000

    added test for lead pdf generation
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1403 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/Crm/PdfTest.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Crm
+ * @subpackage  PDF
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Addressbook_ControllerTest::main');
+}
+
+/**
+ * Test class for Tinebase_Group
+ */
+class Crm_PdfTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var array test objects
+     */
+    protected $objects = array();
+
+    /**
+     * Runs the test methods of this class.
+     *
+     * @access public
+     * @static
+     */
+    public static function main()
+    {
+		$suite  = new PHPUnit_Framework_TestSuite('Tine 2.0 Addressbook Controller Tests');
+        PHPUnit_TextUI_TestRunner::run($suite);
+	}
+
+    /**
+     * Sets up the fixture.
+     * This method is called before a test is executed.
+     *
+     * @access protected
+     */
+    protected function setUp()
+    {        
+       $personalContainer = Tinebase_Container::getInstance()->getPersonalContainer(
+            Zend_Registry::get('currentAccount'), 
+            'Crm', 
+            Zend_Registry::get('currentAccount'), 
+            Tinebase_Container::GRANT_EDIT
+        );
+        
+        if($personalContainer->count() === 0) {
+            $this->testContainer = Tinebase_Container::getInstance()->addPersonalContainer(Zend_Registry::get('currentAccount')->accountId, 'Crm', 'PHPUNIT');
+        } else {
+            $this->testContainer = $personalContainer[0];
+        }
+        
+        $this->objects['lead'] = new Crm_Model_Lead(array(
+            'id'            => 20,
+            'lead_name'     => 'PHPUnit',
+            'leadstate_id'  => 1,
+            'leadtype_id'   => 1,
+            'leadsource_id' => 1,
+            'container'     => $this->testContainer->id,
+            'start'         => Zend_Date::now(),
+            'description'   => 'Lead Description',
+            'end'           => Zend_Date::now(),
+            'turnover'      => '200000',
+            'probability'   => 70,
+            'end_scheduled' => Zend_Date::now(),
+        )); 
+        
+            	
+        return;
+        
+    }
+
+    /**
+     * Tears down the fixture
+     * This method is called after a test is executed.
+     *
+     * @access protected
+     */
+    protected function tearDown()
+    {
+	
+    }
+    
+    /**
+     * try to create a pdf
+     *
+     */
+    public function testLeadPdf()
+    {
+		$pdf = new Crm_Pdf();
+		$pdfOutput = $pdf->leadPdf($this->objects['lead']);
+		
+		$this->assertEquals(1, preg_match("/^%PDF-1.4/", $pdfOutput)); 
+		//$this->assertEquals(1, preg_match("/Lead Description/", $pdfOutput)); 
+		$this->assertEquals(1, preg_match("/PHPUnit/", $pdfOutput));
+				
+    }
+    
+}		
+	
+
+if (PHPUnit_MAIN_METHOD == 'Addressbook_ControllerTest::main') {
+    Addressbook_ControllerTest::main();
+}
