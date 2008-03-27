4b593f62a4d864e35060e7c56a4be38ef4df5ac2
Cornelius Weiß <c.weiss@metaways.de>
2008-03-27 15:53:29 +0000

    add Timemachine tests skeleton
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1435 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/Tinebase/Timemachine/AllTests.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Tasks
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+if (! defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Tinebase_Timemachine_AllTests::main');
+}
+
+class Tinebase_Timemachine_AllTests
+{
+    public static function main ()
+    {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+    
+    public static function suite ()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('Tine 2.0 Tinebase All Timemachine Tests');
+        $suite->addTestSuite('Tinebase_Timemachine_ModificationLogTest');
+        return $suite;
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'Tinebase_Timemachine_AllTests::main') {
+    Tinebase_Timemachine_AllTests::main();
+}
---
+++ tests/Tinebase/Timemachine/ModificationLogTest.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Tinebase
+ * @subpackage  Record
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Tinebase_Timemachine_ModificationLogTest::main');
+}
+
+/**
+ * Test class for Tinebase_Group
+ */
+class Tinebase_Timemachine_ModificationLogTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var array test objects
+     */
+    protected $objects = array();
+
+    /**
+     * Runs the test methods of this class.
+     *
+     * @access public
+     * @static
+     */
+    public static function main()
+    {
+		$suite  = new PHPUnit_Framework_TestSuite('Tinebase_Timemachine_ModificationLogTest');
+        PHPUnit_TextUI_TestRunner::run($suite);
+	}
+
+    /**
+     * Sets up the fixture.
+     * This method is called before a test is executed.
+     *
+     * @access protected
+     */
+    protected function setUp()
+    {
+        
+    }
+
+    /**
+     * Tears down the fixture
+     * This method is called after a test is executed.
+     *
+     * @access protected
+     */
+    protected function tearDown()
+    {
+	
+    }
+    
+    public function testTest()
+    {
+    	$this->assertTrue(true);
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'Tinebase_Timemachine_ModificationLogTest::main') {
+    Tinebase_Timemachine_ModificationLogTest::main();
+}
