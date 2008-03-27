3cbdf708439288eb5b94e6bd894b0f2cd4172af5
Cornelius Weiß <c.weiss@metaways.de>
2008-03-27 13:42:54 +0000

    tasks sql backend CRID tests
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1422 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/Tasks/Backend/SqlTest.php
+++ tests/Tasks/Backend/SqlTest.php
@@ -23,7 +23,107 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
  */
 class Tasks_Backend_SqlTest extends PHPUnit_Framework_TestCase
 {
-
+	/**
+	 * @var Tasks_Backend_Sql SQL Backend in test
+	 */
+	protected $_backend;
+	/**
+	 * @var Tasks_Model_Task test Task 1
+	 */
+	protected $_testTask1;
+	/**
+     * @var Tasks_Model_Task persistant (readout from db after persistant creation) test Task 1
+     */
+	protected $_persistantTestTask1;
+	/**
+	 * As the backend does not depend on the container classes, we define
+	 * two static containers here.
+	 * Moreover we generate static initial datas for two tasks which act 
+	 * as our test rabits.
+	 */
+	public function setUp()
+	{
+		$this->_backend = new Tasks_Backend_Sql();
+        
+		$this->_testTask1 = new Tasks_Model_Task(array(
+            // tine record fields
+	        'container_id'         => 5,
+	        'created_by'           => 6,
+	        'creation_time'        => Zend_Date::now(),
+	        'is_deleted'           => 0,
+	        'deleted_time'         => NULL,
+	        'deleted_by'           => NULL,
+	        // task only fields
+	        'percent'              => 70,
+	        'completed'            => NULL,
+	        'due'                  => Zend_Date::now()->addMonth(1),
+	        // ical common fields
+	        'class_id'             => 1,
+	        'description'          => str_pad('',1000,'.'),
+	        'geo'                  => 0.2345,
+	        'location'             => 'here and there',
+	        'organizer'            => 4,
+	        'priority'             => 2,
+	        'status_id'            => 1,
+	        'summary'              => 'our fist test task',
+	        'url'                  => 'http://www.testtask.com',
+        ),true, false);
+        $this->_persistantTestTask1 = $this->_backend->createTask($this->_testTask1);
+	}
+	public function tearDown()
+	{
+		// NOTE: cascading delete of dependend stuff due to sql schema
+        $db = new Tinebase_Db_Table(array('name' => SQL_TABLE_PREFIX . 'tasks'));
+        $db->delete($db->getAdapter()->quoteInto('id = ?', $this->_persistantTestTask1->getId() ));
+	}
+	/**
+	 * If $this->_testTask1 and $this->_persistantTestTask1 are equal, 
+	 * creation and single readout of task must have been successfull
+	 */
+    public function testCreateTask()
+    {
+    	foreach ($this->_testTask1 as $field => $value) {
+    		$pvalue = $this->_persistantTestTask1->$field;
+    		$this->assertEquals($value, $pvalue, "$field shoud be $value but is $pvalue");
+    	}
+    }
+    /**
+     * Note: delete sets is_deleted and does not delete records 
+     * from table!
+     */
+    public function testSearchDeletedTask()
+    {
+    	$testId = $this->_persistantTestTask1->getId();
+        $this->_backend->deleteTask($testId);
+        $filter = new Tasks_Model_Filter();
+        $filter->query = 'our fist test task';
+        $pagnition = new Tasks_Model_Pagnition();
+        $tasks = $this->_backend->searchTasks($filter, $pagnition);
+        foreach ($tasks as $task) {
+        	$this->assertNotEquals($testId, $task->getId());
+        }
+    }
+    /**
+     * Note: delete sets is_deleted and does not delete records 
+     * from table!
+     */
+    public function testGetDeletedTask()
+    {
+        $testId = $this->_persistantTestTask1->getId();
+        $this->_backend->deleteTask($testId);
+        try {
+        	$task = $this->_backend->getTask($testId);
+        	// this point should not be reached!
+        	if($task->is_deleted) {
+        		$this->fail('Entry getable although it\'s maked as deleted!');
+        	} else {
+        		$this->fail('Failed to delete (set is_deleted).');
+        	}
+        } catch (Exception $e) {
+        	$this->assertTrue(true);
+        }
+    }
+    
 }		
 	
 
