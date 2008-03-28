2de299bad8ff3aa928820ff258c7f95373aeb537
Lars Kneschke <l.kneschke@metaways.de>
2008-03-28 14:22:51 +0000

    added container tests
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1501 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/Tinebase/AllTests.php
+++ tests/Tinebase/AllTests.php
@@ -33,6 +33,7 @@ class Tinebase_AllTests
         $suite->addTestSuite('Tinebase_AccountTest');
         $suite->addTestSuite('Tinebase_GroupTest');
         $suite->addTestSuite('Tinebase_ZendFilterTest');
+        $suite->addTestSuite('Tinebase_ContainerTest');
         
         $suite->addTest(Tinebase_Account_AllTests::suite());
         $suite->addTest(Tinebase_Group_AllTests::suite());
