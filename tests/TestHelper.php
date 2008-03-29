af060a9c4d4591b8d3953235bc1c00df3619799f
Lars Kneschke <l.kneschke@metaways.de>
2008-03-29 11:58:27 +0000

    set Id
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1554 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -7,7 +7,7 @@
  * @license     http://www.gnu.org/licenses/agpl.html AGPL3
  * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Matthias Greiling <m.greiling@metaways.de>
- * @version     $$
+ * @version     $Id$
  */
 
 /*
