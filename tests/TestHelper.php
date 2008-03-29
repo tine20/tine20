21829447a132e4669a334fb5c12926c34830f0b8
Lars Kneschke <l.kneschke@metaways.de>
2008-03-29 11:57:48 +0000

    set correct license and properties
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1553 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -4,8 +4,8 @@
  * 
  * @package     tests
  * @subpackage  test root
- * @license     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL3
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Matthias Greiling <m.greiling@metaways.de>
  * @version     $$
  */
