cf0669c71adaa67ec2ee8afd6d3366a1c9f67358
Cornelius Weiß <c.weiss@metaways.de>
2008-03-31 12:39:41 +0000

    tests for creating modification logs
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1603 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/Tinebase/Timemachine/ModificationLogTest.php
+++ tests/Tinebase/Timemachine/ModificationLogTest.php
@@ -4,9 +4,9 @@
  * 
  * @package     Tinebase
  * @subpackage  Record
- * @license     http://www.gnu.org/licenses/agpl.html
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
- * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
  * @version     $Id$
  */
 
@@ -24,11 +24,22 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
  */
 class Tinebase_Timemachine_ModificationLogTest extends PHPUnit_Framework_TestCase
 {
+	/**
+	 * @var Tinebase_Timemachine_ModificationLog
+	 */
+	protected $_modLogClass;
+	
     /**
-     * @var array test objects
+     * @var Tinebase_Record_RecordSet
      */
-    protected $objects = array();
-
+    protected $_logEntries;
+    
+    /**
+     * @var Tinebase_Record_RecordSet
+     * Persistant Records we need to cleanup at tearDown()
+     */
+    protected $_persistantLogEntries;
+    
     /**
      * Runs the test methods of this class.
      *
@@ -42,25 +53,67 @@ class Tinebase_Timemachine_ModificationLogTest extends PHPUnit_Framework_TestCas
 	}
 
     /**
-     * Sets up the fixture.
-     * This method is called before a test is executed.
+     * Lets update a record tree times
      *
      * @access protected
      */
     protected function setUp()
     {
+    	$this->_modLogClass = Tinebase_Timemachine_ModificationLog::getInstance();
+    	$this->_persistantLogEntries = new Tinebase_Record_RecordSet('Tinebase_Timemachine_Model_ModificationLog');
+    	
+    	$this->_logEntries = new Tinebase_Record_RecordSet('Tinebase_Timemachine_Model_ModificationLog', array(
+        array(
+            'application_id'       => Tinebase_Application::getInstance()->getApplicationByName('Tinebase'),
+            'record_id'            => '5dea69be9c72ea3d263613277c3b02d529fbd8bc',
+            'record_type'          => 'TestType',
+            'record_backend'       => 'TestBackend',
+            'modification_time'    => Zend_Date::now()->addDay(-2),
+            'modification_account' => 7,
+            'modified_attribute'   => 'TestAttribute',
+            'old_value'            => 'InitialValue'
+        ),
+        array(
+            'application_id'       => Tinebase_Application::getInstance()->getApplicationByName('Tinebase')->getId(),
+            'record_id'            => '5dea69be9c72ea3d263613277c3b02d529fbd8bc',
+            'record_type'          => 'TestType',
+            'record_backend'       => 'TestBackend',
+            'modification_time'    => Zend_Date::now()->addDay(-1),
+            'modification_account' => 7,
+            'modified_attribute'   => 'TestAttribute',
+            'old_value'            => 'FirstUpdate'
+        ),
+        array(
+            'application_id'       => Tinebase_Application::getInstance()->getApplicationByName('Tinebase')->getId(),
+            'record_id'            => '5dea69be9c72ea3d263613277c3b02d529fbd8bc',
+            'record_type'          => 'TestType',
+            'record_backend'       => 'TestBackend',
+            'modification_time'    => Zend_Date::now(),
+            'modification_account' => 7,
+            'modified_attribute'   => 'TestAttribute',
+            'old_value'            => 'SecondUpdate'
+        )), true, false);
         
+        foreach ($this->_logEntries as $logEntry) {
+        	$id = $logEntry->generateUID();
+        	
+        	$logEntry->setId($id);
+        	$this->_modLogClass->setModification($logEntry);
+        	$this->_persistantLogEntries->addRecord($this->_modLogClass->getModification($id));
+        }
     }
 
     /**
-     * Tears down the fixture
-     * This method is called after a test is executed.
-     *
+     * cleanup database
      * @access protected
      */
     protected function tearDown()
     {
-	
+        $table = new Tinebase_Db_Table(array('name' => SQL_TABLE_PREFIX . 'timemachine_modificationlog'));
+        
+        foreach ($this->_persistantLogEntries as $persistantLogEntry) {
+             $table->delete($table->getAdapter()->quoteInto('id = ?', $persistantLogEntry->getId()));
+        }
     }
     
     public function testTest()
