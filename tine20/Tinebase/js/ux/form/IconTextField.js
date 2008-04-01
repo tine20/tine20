4104ae264b0639dc379c5aa729b81d2e18160906
Cornelius Weiß <c.weiss@metaways.de>
2008-04-01 16:52:51 +0000

    add ux for form fields with an icon in front of a label
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1642 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/js/ux/form/IconTextField.js
@@ -0,0 +1,40 @@
+/*
+ * Tine 2.0
+ * 
+ * @package     Ext
+ * @subpackage  ux
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+Ext.namespace('Ext.ux', 'Ext.ux.form');
+
+/**
+ * Class for creating text-fields with an icon in front of the label.
+ * <p>Example usage:</p>
+ * <pre><code>
+ var field =  new Ext.ux.form.IconTextField({
+     labelIcon: 'images/oxygen/16x16/actions/mail.png'
+     fieldLabel: 'email',
+     name: 'email',
+ });
+ * </code></pre>
+ */
+Ext.ux.form.IconTextField = Ext.extend(Ext.form.TextField, {
+    /**
+     * @cfg {String} LabelIcon icon to be displayed in front of the label
+     */
+    labelIcon: '',
+    /**
+     * @private
+     */
+    initComponent: function(){
+         Ext.ux.form.IconTextField.call(this);
+         if (this.labelIcon.length > 0){
+            this.fieldLabel = this.labelIcon + this.fieldLabel;
+         }
+    }
+});
+Ext.reg('icontextfield', Ext.ux.form.IconTextField);
