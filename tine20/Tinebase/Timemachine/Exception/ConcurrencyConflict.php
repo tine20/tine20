08beb2a69d8c0b61160ffd743d269189a3d57252
Cornelius Weiß <c.weiss@metaways.de>
2008-04-01 20:45:17 +0000

    add spechial exception for concurrency confilicts
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1649 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/Timemachine/Exception/ConcurrencyConflict.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Tinebase
+ * @subpackage  Timemachine
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Concurrency Conflict exception
+ * 
+ * @package     Tinebase
+ * @subpackage  Timemachine
+ */
+class Tinebase_Timemachine_Exception_ConcurrencyConflict extends Exception {
+
+}
