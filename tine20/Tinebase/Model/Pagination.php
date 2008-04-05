15245993069ece1e563077e24c55e45be9f13ad6
Lars Kneschke <l.kneschke@metaways.de>
2008-04-05 19:29:34 +0000

    added pagination model
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1733 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/Model/Pagination.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Tinebase
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+
+/**
+ * Pagination Class
+ * @package Tinebase
+ */
+class Tinebase_Model_Pagination extends Tinebase_Record_Abstract
+{
+	/**
+     * key in $_validators/$_properties array for the filed which 
+     * represents the identifier
+     * 
+     * @var string
+     */    
+    protected $_identifier = 'id';
+    
+    protected $_validators = array(
+        'id'                   => array('allowEmpty' => true,  'Int'   ),
+        
+        'start'                => array('allowEmpty' => true,  'Int'   ),
+        'limit'                => array('allowEmpty' => true,  'Int'   ),
+        'sort'                 => array('allowEmpty' => true,          ),
+        'dir'                  => array('presence'      => 'required',
+                                        'allowEmpty'    => false,
+                                        'InArray'       => array('ASC', 'DESC'),
+                                        'default'       => 'ASC'       )
+    );
+}
