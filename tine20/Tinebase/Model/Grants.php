775bf623d1425adc46a6fc67b08de4b8264c64bb
Lars Kneschke <l.kneschke@metaways.de>
2008-04-08 11:34:45 +0000

    set accountId to 0 as default
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1786 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Model/Grants.php
+++ tine20/Tinebase/Model/Grants.php
@@ -57,7 +57,7 @@ class Tinebase_Model_Grants extends Tinebase_Record_Abstract
     {
         $this->_validators = array(
             'id'          => array('Alnum', 'allowEmpty' => TRUE),
-            'accountId'   => array('presence' => 'required', 'allowEmpty' => TRUE),
+            'accountId'   => array('presence' => 'required', 'allowEmpty' => TRUE, 'default' => 0),
             'accountType' => array('presence' => 'required', 'InArray' => array('anyone','account','group')),
             'readGrant'   => array(
                 new Zend_Validate_InArray(array(TRUE, FALSE), TRUE), 
