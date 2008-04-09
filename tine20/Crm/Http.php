62b969f01507d50c2bc19d0e863b42205a249ed3
Lars Kneschke <l.kneschke@metaways.de>
2008-04-09 14:37:28 +0000

    fixed some minor problems
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1826 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Crm/Http.php
+++ tine20/Crm/Http.php
@@ -136,7 +136,7 @@ class Crm_Http extends Tinebase_Application_Http_Abstract
             $leadData['contacts'] = array();   
             $leadData['tasks'] = array();                                   
             
-            $personalFolders = $controller->getPersonalContainer($currentAccount, $currentAccount->accountId, Tinebase_Container::GRANT_READ);
+            $personalFolders = Zend_Registry::get('currentAccount')->getPersonalContainer('Crm', $currentAccount, Tinebase_Container::GRANT_READ);
             foreach($personalFolders as $folder) {
                 $leadData['container']     = $folder->toArray();
                 break;
--- tine20/Crm/Json.php
+++ tine20/Crm/Json.php
@@ -344,17 +344,17 @@ class Crm_Json extends Tinebase_Application_Json_Abstract
         //Zend_Registry::get('logger')->debug(__METHOD__ . '::' . __LINE__ . ' ' . print_r($leadData->toArray(), true));
         $responsible = Zend_Json::decode($linkedAccount);
         if(is_array($responsible)) {
-            $lead->responsible = $responsible;
+            $leadData->responsible = $responsible;
         }
         
         $customer = Zend_Json::decode($linkedCustomer);
         if(is_array($customer)) {
-            $lead->customer = $customer;
+            $leadData->customer = $customer;
         }
         
         $oartner = Zend_Json::decode($linkedPartner);
         if(is_array($partner)) {
-            $lead->partner = $partner;
+            $leadData->partner = $partner;
         }
         
         if(empty($leadData->id)) {
@@ -363,16 +363,6 @@ class Crm_Json extends Tinebase_Application_Json_Abstract
             $savedLead = Crm_Controller::getInstance()->updateLead($leadData);
         }
         
-        // set linked contacts
-        //$linkedCustomer = Zend_Json::decode($linkedCustomer);
-        //Crm_Controller::getInstance()->setLinkedCustomer($savedLead->id, $linkedCustomer);
-
-        //$linkedPartner = Zend_Json::decode($linkedPartner);
-        //Crm_Controller::getInstance()->setLinkedPartner($savedLead->id, $linkedPartner);
-
-        //$linkedAccount = Zend_Json::decode($linkedAccount);
-        //Crm_Controller::getInstance()->setLinkedAccount($savedLead->id, $linkedAccount);
-
         // set linked tasks
         $linkedTasks = Zend_Json::decode($linkedTasks);
         Crm_Controller::getInstance()->setLinkedTasks($savedLead->id, $linkedTasks);
