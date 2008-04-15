8fa09da5e6b1cbd5b865edbe1367259704a02c1f
Philipp Schüle <p.schuele@metaways.de>
2008-04-15 14:54:16 +0000

    added importContainer (and container acl)
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1922 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Setup/Import/TineRev949.php
+++ tine20/Setup/Import/TineRev949.php
@@ -3,6 +3,8 @@
  * Tine 2.0
  * class for migration from tine 2.0 revision 949
  * 
+ * the database setup is very similar to egw14 -> perhaps we can use these functions for egw14 import
+ * 
  * @package     Setup
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Philipp Schuele <p.schuele@metaways.de>
@@ -18,6 +20,21 @@
  */
 class Setup_Import_TineRev949
 {
+    /**
+     * is needed for container grants
+     * 
+     * @var groupAccountArray
+     */
+    protected $groupAccountArray = array();
+
+    /**
+     * is needed for container acl
+     * acl -> uid mapping
+     * 
+     * @var groupArray
+     */
+    protected $grantAclIdToUid = array();
+    
     
     /**
      * import main function
@@ -29,15 +46,16 @@ class Setup_Import_TineRev949
         $this->importGroups();
         $this->importAccounts();
         $this->importGroupMembers();
+        $this->importContainer();
+        
+        //@todo make it work
+        //$this->importAddressbook();
         
         //@todo write these functions (and add more?)
         
-//        $this->importAddressbook();
 //        $this->importCrm();
 //        $this->importAcl();
-//        $this->importXXX();
-        
-        //@todo add container?
+//        $this->importXXX();        
         
         //@todo delete old tables?
     }
@@ -106,6 +124,9 @@ class Setup_Import_TineRev949
                 'description'   => 'imported'
             ));
             
+            // add group for container import
+            $this->groupAccountArray[] = $group->account_id;
+            
             Tinebase_Group_Sql::getInstance()->addGroup($tineGroup);
         }
         echo "done! got ".sizeof($groups)." groups.<br>";
@@ -126,11 +147,159 @@ class Setup_Import_TineRev949
         
         $groupMembers = $aclTable->fetchAll($where);
         
-        echo "Import Groups from table sirona_accounts ... ";
+        echo "Import Groupmembers from table sirona_acl ... ";
         foreach($groupMembers as $member) {
             Tinebase_Group_Sql::getInstance()->addGroupMember(abs($member->acl_location), $member->acl_account);
         }
         echo "done! got ".sizeof($groupMembers)." group memberships.<br>";
         
     }
+    
+    /**
+     * import the containers (+acl) from revision 949
+     *
+     */
+    protected function importContainer()
+    {
+        
+        $what = "container";
+        $table = new Tinebase_Db_Table(array('name' => 'sirona_'.$what));
+        $mapping = array (  "8" => "2", 
+                            "12" => "5", 
+                            "13" => "4" );
+        $where = array();        
+
+        // delete old entries (contacts + container)      
+        $newContactsTable = new Tinebase_Db_Table(array('name' => 'sironanew_addressbook'));
+        $newContactsTable->delete( "1" );        
+        $newTable = new Tinebase_Db_Table(array('name' => 'sironanew_'.$what));
+        $newTable->delete( "1" );
+        
+        $rows = $table->fetchAll($where);
+        
+        echo "Import $what  ... ";
+        foreach($rows as $row) {
+            // old: container_id    container_name  container_type  container_backend   application_id
+            // new: id   name    type    backend     application_id
+            $tineModel = new Tinebase_Model_Container(array(
+                'id'                => $row->container_id,
+                'name'              => $row->container_name,
+                'type'              => $row->container_type,
+                'backend'           => strtolower($row->container_backend),
+                'application_id'    => ( isset($mapping[$row->application_id]) ) ? $mapping[$row->application_id] : $row->application_id,
+            ));
+            
+            // get grants
+            $grantsArray = array();
+            $aclTable = new Tinebase_Db_Table(array('name' => 'sirona_container_acl'));
+            $grants = $aclTable->fetchAll( array(Zend_Registry::get('dbAdapter')->quoteInto('container_id = ?', $row->container_id)) );
+            foreach ( $grants as $grant ) {
+                             
+                $type = 'user';
+                $accountId = $grant->account_id;
+                
+                if ( empty($accountId) or $accountId === NULL ) {
+                    $type = 'anyone';
+                    $accountId = 0;
+                }
+                if ( in_array($grant->account_id, $this->groupAccountArray) ) {
+                    $type = 'group';
+                }
+                
+                $grantsArray[] = array ( 'type' => $type, 'accountId' => $accountId, 'grant' => $grant->account_grant );
+                
+            }
+            Tinebase_Container::getInstance()->addContainer($tineModel, new Tinebase_Record_RecordSet('Tinebase_Model_Grants', array()), TRUE);
+
+            foreach ( $grantsArray as $grantData ) {
+                Tinebase_Container::getInstance()->addGrants(
+                    $row->container_id, 
+                    $grantData['type'],
+                    $grantData['accountId'], 
+                    array($grantData['grant']),
+                    TRUE
+                );
+            }
+            
+        }
+        echo "done! got ".sizeof($rows)." $what(s).<br>";
+        
+    }
+    
+    /**
+     * import the addressbook from revision 949
+     *
+     */
+    protected function importAddressbook()
+    {
+        $contactsTable = new Tinebase_Db_Table(array('name' => 'sirona_addressbook'));
+        
+        // get contacts
+        $contacts = $contactsTable->fetchAll();        
+        
+        echo "Import Contacts from table sirona_addressbook ... ";
+        foreach($contacts as $contact) {
+            
+            $tineContact = new Addressbook_Model_Contact ( array(
+                
+                'id'                    => $contact->contact_id,
+                'account_id'            => $contact->account_id,
+                'owner'                 => $contact->contact_owner,
+
+                'n_family'              => $contact->n_family,
+                'n_fileas'              => $contact->n_fileas,
+                'n_fn'                  => $contact->n_fn,
+            
+                'adr_one_countryname'   => $contact->adr_one_countryname,
+                'adr_one_locality'      => $contact->adr_one_locality,
+                'adr_one_postalcode'    => $contact->adr_one_postalcode,
+                'adr_one_region'        => $contact->adr_one_region,
+                'adr_one_street'        => $contact->adr_one_street,
+                'adr_one_street2'       => $contact->adr_one_street2,
+                'adr_two_countryname'   => $contact->adr_two_countryname,
+                'adr_two_locality'      => $contact->adr_two_locality,
+                'adr_two_postalcode'    => $contact->adr_two_postalcode,
+                'adr_two_region'        => $contact->adr_two_region,
+                'adr_two_street'        => $contact->adr_two_street,
+                'adr_two_street2'       => $contact->adr_two_street2,
+
+            /* @todo add more fields */
+            
+/*
+                'modified'              => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'assistent'             => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'bday'                  => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'email'                 => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'email_home'            => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'note'                  => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'role'                  => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'title'                 => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'url'                   => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'url_home'              => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'n_given'               => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'n_middle'              => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'n_prefix'              => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'n_suffix'              => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'org_name'              => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'org_unit'              => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'tel_assistent'         => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'tel_car'               => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'tel_cell'              => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'tel_cell_private'      => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'tel_fax'               => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'tel_fax_home'          => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'tel_home'              => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'tel_pager'             => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                'tel_work'              => array(Zend_Filter_Input::ALLOW_EMPTY => true)
+            */            
+            
+                ) 
+            );
+            
+            Addressbook_Backend_Sql::getInstance()->addContact($tineContact);
+            
+        }
+        echo "done! got ".sizeof($contacts)." contacts.<br>";
+        
+    }    
 }
