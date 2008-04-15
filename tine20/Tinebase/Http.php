2b3014b3f03525c85e6c1d8ff8e94058fd6f316a
Cornelius Weiß <c.weiss@metaways.de>
2008-04-15 09:27:24 +0000

    new ux: Ext.ux.form.MirrorTextField
    Class for creating equal text-fields multiple times in a form.
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1911 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Http.php
+++ tine20/Tinebase/Http.php
@@ -79,6 +79,7 @@ class Tinebase_Http extends Tinebase_Application_Http_Abstract
             self::_appendFileTime("Tinebase/js/ux/grid/CheckColumn.js"),
             self::_appendFileTime("Tinebase/js/ux/grid/QuickaddGridPanel.js"),
             self::_appendFileTime("Tinebase/js/ux/form/IconTextField.js"),
+            self::_appendFileTime("Tinebase/js/ux/form/MirrorTextField.js"),
             self::_appendFileTime("Tinebase/js/DatepickerRange.js"),
             // Tine 2.0 specific widgets
             self::_appendFileTime("Tinebase/js/Widgets.js"),
---
+++ tine20/Tinebase/js/ux/form/MirrorTextField.js
@@ -0,0 +1,104 @@
+/*
+ * Tine 2.0
+ * 
+ * @package     Ext
+ * @subpackage  ux
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+Ext.namespace('Ext.ux', 'Ext.ux.form');
+
+/**
+ * Class for creating equal text-fields multiple times in a form.
+ * If a value gets changed in one of the fields, all other will be updated
+ * <p>Example usage:</p>
+ * <pre><code>
+ var dialog =  new Ext.Panel({
+     layout: 'form',
+     items: [
+         {
+             fieldLabel: 'First occurrence',
+             xtype: 'mirrortextfield',
+             name: 'themirrorfiled'
+         },{
+             fieldLabel: 'Normal field',
+             xtype: 'textfield',
+             name: 'someotherfield'
+         },{
+             fieldLabel: 'Second occurrence',
+             xtype: 'mirrortextfield',
+             name: 'themirrorfiled'
+         },
+     ]
+ });
+ * </code></pre>
+ */
+Ext.ux.form.MirrorTextField = Ext.extend(Ext.ux.form.IconTextField, {
+    /**
+     * @private
+     */
+    initComponent: function(){
+         Ext.ux.form.MirrorTextField.superclass.initComponent.call(this);
+         Ext.ux.form.MirrorTextFieldManager.register(this);
+    },
+    /**
+     * @private
+     */
+    setValue: function(value){
+        Ext.ux.form.MirrorTextFieldManager.setAll(this, value);
+    },
+    /**
+     * @private
+     */
+    onDestroy : function(){
+        if(this.rendered){
+            Ext.ux.form.MirrorTextFieldManager.unregister(this);
+        }
+    },
+});
+Ext.reg('mirrortextfield', Ext.ux.form.MirrorTextField);
+
+/**
+ * Helper for Ext.ux.form.MirrorTextField
+ * @singleton
+ */
+Ext.ux.form.MirrorTextFieldManager = function() {
+    var MirrorTextFields = {};
+    
+    function MirrorField(field, newValue, oldValue) {
+        var m = MirrorTextFields[field.name];
+        for(var i = 0, l = m.length; i < l; i++){
+            m[i].setRawValue(newValue);
+        }
+        return true;
+    }
+    
+    return {
+        register: function(field) {
+            var m = MirrorTextFields[field.name];
+            if(!m){
+                m = MirrorTextFields[field.name] = [];
+            }
+            m.push(field);
+            field.on("change", MirrorField);
+        },
+        
+        unregister: function(field) {
+            var m = MirrorTextFields[field.name];
+            if(m){
+                m.remove(field);
+                field.un("change", toggleGroup);
+            }
+        },
+        
+        setAll: function(field, value) {
+            var m = MirrorTextFields[field.name];
+            if(m){
+                MirrorField(field, value);
+            }
+        }
+    };
+}();
