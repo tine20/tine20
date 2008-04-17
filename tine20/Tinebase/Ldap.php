56cbf54581544c347b689ef369e4866bc48c6650
Lars Kneschke <l.kneschke@metaways.de>
2008-04-17 22:24:52 +0000

    our extended version of Zend_Ldap
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1980 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/Ldap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Egwbase
+ * @subpackage  Server
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL3
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ */
+
+class Tinebase_Ldap extends Zend_Ldap
+{
+    
+}
