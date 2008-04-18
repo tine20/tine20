dcedb2c1145e2f1aab25f0d8bcb9d81bd0e1bd4f
Matthias Greiling <m.greiling@metaways.de>
2008-04-18 10:40:51 +0000

    Ready for new Setup & Update strategy
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1997 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Setup/Tables.php
+++ tine20/Setup/Tables.php
@@ -30,8 +30,21 @@ class Setup_Tables
 
         $this->setupLogger();
         $this->setupDatabaseConnection();
-
-        $this->_backend = new Setup_Backend_Mysql();
+		
+		switch ($this->_config->database->database)
+		{
+			case('mysql'):
+			{
+				$this->_backend = new Setup_Backend_Mysql();
+				break;
+			}
+			
+			default:
+			{
+				echo "you have to define a dbms = yourdbms (like mysql) in your config.ini file";
+			}
+		}
+		
     }
 
     /**
@@ -86,6 +99,26 @@ class Setup_Tables
         }
     }
 
+	public function applicationExists($_application)
+	{
+		if($this->tableExists(SQL_TABLE_PREFIX . 'applications'))
+		{
+			if($this->applicationVersionQuery($_application) != false)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	
     /**
      * parses the xml stream and creates the tables if needed
      *
@@ -94,14 +127,57 @@ class Setup_Tables
     public function parseFile($_file)
     {
         $createdTables = array();
-
+	
         $xml = simplexml_load_file($_file);
-
+		
+		if (false == $this->applicationExists($xml->name)) 
+		{
+			// just insert tables
+			if(isset($xml->tables)) 
+			{
+				foreach ($xml->tables[0] as $table) 
+				{
+					if (false == $this->tableExists(SQL_TABLE_PREFIX . $table->name)) 
+					{
+						$this->_backend->_createTable($table);
+						$createdTables[] = $table;
+					}
+				}
+			}
+		}
+		else
+		{
+		
+			switch($this->applicationNeedsUpdate($xml->name, $xml->version)) 
+			{
+				case (-1):
+				{
+					$this->applicationUpdate($xml->name, $xml->version);
+					
+					echo "<strong>" . $xml->name . " had different specifications. Now up-to-date to version " .  $xml->version . "</strong>";
+					break; 
+				}
+				case (0):
+				{
+					echo "<i>" . $xml->name . " no changes " . $xml->version . "</i>";
+					break;
+				}
+				case(1):
+				{
+					echo "<span style=color:#ff0000>database schema newer as your new definition - giving up</span>";
+					break;
+				}
+			}
+		
+		}
+		
+		/*
         if(isset($xml->tables)) {
             foreach ($xml->tables[0] as $table) {
-                  $tableName = SQL_TABLE_PREFIX . $table->name;
-                  if(!$this->_backend->tableExists($this->_config->database->dbname, $tableName)) {
-                    $this->_backend->createTable($table);
+				$tableName = SQL_TABLE_PREFIX . $table->name;
+                if(!$this->_backend->tableExists($this->_config->database->dbname, $tableName)) 
+				{
+					$this->_backend->createTable($table);
                     $createdTables[] = $table;
                     }
                     else
@@ -116,7 +192,7 @@ class Setup_Tables
 				}
             }
         }
-
+*/
         try {
             $application = Tinebase_Application::getInstance()->getApplicationByName($xml->name);
         } catch (Exception $e) {
@@ -139,6 +215,204 @@ class Setup_Tables
                 $this->_backend->execInsertStatement($record);
             }
         }
+    }
+
+
+
+    /**
+     * check's if a given table exists
+     *
+     * @param string $_tableSchema
+     * @param string $_tableName
+     * @return boolean return true if the table exists, otherwise false
+     */
+    public function tableExists($_tableName)
+    {
+	 	$select = Zend_Registry::get('dbAdapter')->select()
+          ->from('information_schema.tables')
+		  ->where('TABLE_SCHEMA = ?', $this->_config->database->dbname)
+          ->where('TABLE_NAME = ?', $_tableName);
+
+        $stmt = $select->query();
+
+        $table = $stmt->fetchObject();
+ 
+
+		if($table === false) {
+          return false;
+        }
+
+        return true; 
+    }
+	
+	/**
+     * check's a given database table version 
+     *
+     * @param string $_tableName
+     * @return boolean return string "version" if the table exists, otherwise false
+     */
+    
+	public function tableVersionQuery($_tableName)
+	{
+		$select = Zend_Registry::get('dbAdapter')->select()
+				->from( SQL_TABLE_PREFIX . 'application_tables')
+				->where('name = ?', $_tableName);
+
+        $stmt = $select->query();
+		$version = $stmt->fetchAll();
+		
+		return $version[0]['version'];
+	}
+	/**
+     * check's a given application version
+     *
+     * @param string $_application
+     * @return boolean return string "version" if the table exists, otherwise false
+     */
+    
+	public function applicationVersionQuery($_application)
+	{	
+		$select = Zend_Registry::get('dbAdapter')->select()
+				->from( SQL_TABLE_PREFIX . 'applications')
+				->where('name = ?', $_application);
+
+        $stmt = $select->query();
+		$version = $stmt->fetchAll();
+		if(empty($version))
+		{
+			return false;
+		}
+		else
+		{
+			return $version[0]['version'];
+		}
+	}
+
+	/**
+     * compare versions
+     *
+     * @param string $_tableName (database)
+	 * @param string $_tableVersion (xml-file)
+	 * @return 1,0,-1
+     */
+	public function applicationNeedsUpdate($_applicationName, $_applicationVersion)
+	{
+		return version_compare($this->applicationVersionQuery($_applicationName),  $_applicationVersion);
+	}
+	
+	
+	/**
+     * compare versions
+     *
+     * @param string $_tableName (database)
+	 * @param string $_tableVersion (xml-file)
+	 * @return 1,0,-1
+     */
+	public function tableNeedsUpdate($_tableName, $_tableVersion)
+	{
+		return version_compare($this->tableVersionQuery($_tableName),  $_tableVersion);
+	}
+
+	/**
+     * update database tables via including the right files
+     *
+     * @param string $_xmlVersion (numbers read from xml-file)
+	 * @param string $_application (name of updateable application)
+	 * @param string $_tableName (name of database table)
+	 * @return true
+     */
+	 
+	 /*
+	 hirnknoten: update ich eine tabelle oder eine application ... 
+	 
+	 */
+	 
+	public function tableUpdate($_xmlVersion, $_application, $_tableName)
+	{
+		$tableVersion = $this->tableVersionQuery($_tableName);
+		
+		$noGoArea = array('.', '..', 'setup.xml');
+		
+		$path = $_application . "/Setup/";
+		
+		foreach ( new DirectoryIterator( $path ) as $item ) 
+		{
+			if(! in_array($item->getFileName(), $noGoArea)) {
+				$fileBuffer[] = $item->getPathname();
+			}
+		}
+		
+		sort($fileBuffer);
+	
+		foreach ($fileBuffer as $file)
+		{
+			$fileVersion = substr($file, (strpos($file, 'up-') + 3), (strlen($file) - 4 - (strpos($file, 'up-') + 3)));
+			if ((version_compare($fileVersion, $tableVersion) == 1) && ((version_compare($_xmlVersion, $fileVersion ) == 0 || version_compare($_xmlVersion, $fileVersion) == 1)))
+			{
+				try
+				{
+					include ($file);
+				}
+				catch (Exception $e)
+				{
+					echo $e->getMessage();
+				}
+			}
+		}
+		exit;
+		
+	}
+	
+	public function applicationUpdate($_name, $_version)
+	{
+		$number = explode('.', $_version);
+		$className = $_name . '_Setup_Update_' . $number[0] . '_' . $number[1];
+		$update = new $className($this->_backend, $_name);
+		
+		$update->make();
+	}
+	
+	
+    public function execInsertStatement($_record)
+    {
+        $table = new Tinebase_Db_Table(array(
+           'name' => SQL_TABLE_PREFIX . $_record->table->name
+        ));
+
+        foreach ($_record->field as $field) {
+            if(isset($field->value['special'])) {
+                switch(strtolower($field->value['special'])) {
+                    case 'now':
+                    {
+                        $value = Zend_Date::now()->getIso();
+                        break;
+                    }
+                    case 'account_id':
+                    {
+                        break;
+                    }
+                    case 'application_id':
+                    {
+                        $application = Tinebase_Application::getInstance()->getApplicationByName($field->value);
+
+                        $value = $application->id;
+
+                        break;
+                    }
+                    default:
+                    {
+                        throw new Exception('unsupported special type ' . strtolower($field->value['special']));
+                        break;
+                    }
+                }
+            } else {
+                $value = $field->value;
+            }
+
+            $data[(string)$field->name] = $value;
+        }
+
+        $table->insert($data);
     }
 
     public function addTable(Tinebase_Model_Application $_application, $_name, $_version)
