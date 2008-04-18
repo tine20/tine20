a559a3917469176a43aa5f4e975dfba4e81a8741
Cornelius Weiß <c.weiss@metaways.de>
2008-04-18 09:29:07 +0000

    add first tests for tags
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@1996 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/Tinebase/TagsTests.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Tasks
+ * @subpackage  Tags
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    Tinebase_TagsTest::main();
+}
+
+/**
+ * Test class for Tinebase_Group
+ */
+class Tinebase_TagsTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * unit under test (UIT)
+     * @var Tinebase_Tags
+     */
+    protected $_instance;
+    
+    /**
+     * Runs the test methods of this class.
+     */
+    public static function main()
+    {
+        $suite  = new PHPUnit_Framework_TestSuite('Tinebase_TagsTest');
+        PHPUnit_TextUI_TestRunner::run($suite);
+    }
+    
+    public function setUp()
+    {
+        $this->_instance = Tinebase_Tags::getInstance();
+    }
+    
+    public function testCreateTags()
+    {
+        $sharedTag = new Tinebase_Tags_Model_Tag(array(
+            'type'  => Tinebase_Tags_Model_Tag::TYPE_SHARED,
+            'name'  => 'tag::shared',
+            'description' => 'this is a shared tag',
+            'color' => '#009B31',
+        ));
+        $savedSharedTag = $this->_instance->createTag($sharedTag);
+        $this->assertEquals($sharedTag->name, $savedSharedTag->name);
+        
+        $personalTag = new Tinebase_Tags_Model_Tag(array(
+            'type'  => Tinebase_Tags_Model_Tag::TYPE_PERSONAL,
+            'name'  => 'tag::personal',
+            'description' => 'this is a personal tag of account 1',
+            'color' => '#FF0000',
+        ));
+        $savedPersonalTag = $this->_instance->createTag($personalTag);
+        $this->assertEquals($personalTag->description, $savedPersonalTag->description);
+    }
+    
+    public function testSearchTags()
+    {
+        $filter = new Tinebase_Tags_Model_Filter(array(
+            'name' => 'tag::%'
+        ));
+        $paging = new Tinebase_Model_Pagination();
+        $tags = $this->_instance->searchTags($filter, $paging);
+        $this->_instance->getSearchTagsCount($filter);
+        
+    }
+    
+}
+
