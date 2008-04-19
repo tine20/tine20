538a1d548fb31acd77c4bafe44bb8c82b0e8fa6b
Cornelius Weiß <c.weiss@metaways.de>
2008-04-19 12:21:53 +0000

    make columform layout a column form panel to simplify handling
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2025 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/js/ux/form/ColumnFormPanel.js
@@ -0,0 +1,98 @@
+/*
+ * Tine 2.0
+ * 
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+ 
+Ext.namespace('Ext.ux', 'Ext.ux.form');
+
+/**
+ * @class Ext.ux.form.ColumnFormPanel
+ * Helper Class for creating form panels with a horizontal layout. This class could be directly
+ * created using the new keyword or by specifying the xtype: 'columnform'
+ * Example usage:</p>
+ * <pre><code>
+var p = new Ext.ux.form.ColumnFormPanel({
+    title: 'Horizontal Form Layout',
+    items: [
+        [
+            {
+                columnWidth: .6,
+                fieldLabel:'Company Name', 
+                name:'org_name'
+            },
+            {
+                columnWidth: .4,
+                fieldLabel:'Street', 
+                name:'adr_one_street'
+            }
+        ],
+        [
+            {
+                columnWidth: .7,
+                fieldLabel:'Region',
+                name:'adr_one_region'
+            },
+            {
+                columnWidth: .3,
+                fieldLabel:'Postal Code', 
+                name:'adr_one_postalcode'
+            }
+        ]
+    ]
+});
+</code></pre>
+ */
+Ext.ux.form.ColumnFormPanel = Ext.extend(Ext.Panel, {
+    /**
+     * @cfg
+     */
+    formDefaults: {
+        xtype:'icontextfield',
+        anchor: '100%',
+        labelSeparator: '',
+        columnWidth: .333
+    },
+    
+    layout: 'hfit',
+    labelAlign: 'top',
+    /**
+     * @private
+     */
+    initComponent: function() {
+        var items = [];
+            
+        // each item is an array with the config of one row
+        for (var i=0,j=this.items.length; i<j; i++) {
+            
+            var initialRowConfig = this.items[i];
+            var rowConfig = {
+                layout: 'column',
+                items: [],
+            };
+            // each row consits n column objects 
+            for (var n=0,m=initialRowConfig.length; n<m; n++) {
+                var column = initialRowConfig[n];
+                rowConfig.items.push({
+                    columnWidth: column.columnWidth ? column.columnWidth : this.formDefaults.columnWidth,
+                    layout: 'form',
+                    labelAlign: this.labelAlign,
+                    defaults: this.formDefaults,
+                    bodyStyle: 'padding-right: 5px;',
+                    items: column
+                });
+            }
+            items.push(rowConfig);
+        }
+        this.items = items;
+        
+        Ext.ux.form.ColumnFormPanel.superclass.initComponent.call(this);
+    }
+});
+
+Ext.reg('columnform', Ext.ux.form.ColumnFormPanel);
+
