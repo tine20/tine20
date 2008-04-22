078fde420030caa6adf97b149572dbc441935fd4
Matthias Greiling <m.greiling@metaways.de>
2008-04-22 11:11:24 +0000

    reviewed restructured setup
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2056 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/setup.php
+++ tine20/setup.php
@@ -44,137 +44,33 @@ try {
 /**
  * start setup
  */
-$setup = new Setup_Tables();
-$kindOfSetup = 'initalLoad';
-    
-if ( DO_TABLE_SETUP === TRUE ) {
-    $fileName = 'Tinebase/Setup/setup.xml';
-    if(file_exists($fileName)) {
-        echo "Processing tables definitions for <b>Tinebase</b>($fileName)<br>";
-        $kindOfSetup = $setup->parseFile($fileName);
-    }
-    
-    foreach ( new DirectoryIterator('./') as $item ) {
-    	if($item->isDir() && $item->getFileName() != 'Tinebase') {
-    		$fileName = $item->getFileName() . '/Setup/setup.xml';
-    		if(file_exists($fileName)) {
-    			echo "Processing tables definitions for <b>" . $item->getFileName() . "</b>($fileName)<br>";
-    			$setup->parseFile($fileName);
-    		}
-    	}
-    }
+$setup = new Setup_Controller('Tinebase/Setup/setup.xml', '/Setup/setup.xml');
+   
+/**
+ * build empty Database and fill with default values or update applications
+ */ 
+ if ( DO_TABLE_SETUP === TRUE ) {
+	$setup->run();
 }
-
 
 # either import data from eGroupWare 1.4 or tine 2.0 revision 949
 if ( IMPORT_EGW_14 === TRUE ) {
     $import = new Setup_Import_Egw14();
     $import->import();
-
     exit();
+
 } elseif ( IMPORT_TINE_REV_949 === TRUE ) {
     $import = new Setup_Import_TineRev949();
     $import->import();
-
     exit();
+
 }
 
-if ($kindOfSetup == 'initialLoad')
-{
-    echo "Creating initial user(tine20amdin) and groups...<br>";
-	# or initialize the database ourself
-	# add the admin group
-	$groupsBackend = Tinebase_Group_Factory::getBackend(Tinebase_Group_Factory::SQL);
-
-	$adminGroup = new Tinebase_Group_Model_Group(array(
-	    'name'          => 'Administrators',
-	    'description'   => 'Group of administrative accounts'
-	));
-	$adminGroup = $groupsBackend->addGroup($adminGroup);
-
-	# add the user group
-	$userGroup = new Tinebase_Group_Model_Group(array(
-	    'name'          => 'Users',
-	    'description'   => 'Group of user accounts'
-	));
-	$userGroup = $groupsBackend->addGroup($userGroup);
-
-	# add the admin account
-	$accountsBackend = Tinebase_Account_Factory::getBackend(Tinebase_Account_Factory::SQL);
-
-	$account = new Tinebase_Account_Model_FullAccount(array(
-	    'accountLoginName'      => 'tine20admin',
-	    'accountStatus'         => 'enabled',
-	    'accountPrimaryGroup'   => $userGroup->getId(),
-	    'accountLastName'       => 'Account',
-	    'accountDisplayName'    => 'Tine 2.0 Admin Account',
-	    'accountFirstName'      => 'Tine 2.0 Admin'
-	));
-
-	$accountsBackend->addAccount($account);
-
-	Zend_Registry::set('currentAccount', $account);
-
-	# set the password for the tine20admin account
-	Tinebase_Auth::getInstance()->setPassword('tine20admin', 'lars', 'lars');
-
-	# add the admin account to all groups
-	Tinebase_Group::getInstance()->addGroupMember($adminGroup, $account);
-	Tinebase_Group::getInstance()->addGroupMember($userGroup, $account);
-
-	# enable the applications for the user group
-	# give admin rights to the admin group for all applications
-	$applications = Tinebase_Application::getInstance()->getApplications();
-	foreach( $applications as $application) {
-	    
-	    //@todo    use 'right' field with const from Tinebase_Acl_Rights
-        if(strtolower($application->name) !== 'admin') {
-            // run right for user group
-            $right = new Tinebase_Acl_Model_Right(array(
-                'application_id'    => $application->getId(),
-                'account_id'        => $userGroup->getId(),
-                'account_type'      => 'group',
-                'right'             => Tinebase_Acl_Rights::RUN
-            ));
-            Tinebase_Acl_Rights::getInstance()->addRight($right);
-            
-            // run for admin group
-            $right->account_id = $adminGroup->getId();            
-            Tinebase_Acl_Rights::getInstance()->addRight($right);
-            
-            // admin for admin group
-            $right->right = Tinebase_Acl_Rights::ADMIN;            
-            Tinebase_Acl_Rights::getInstance()->addRight($right);
-            
-        } else {
-            // run right for admin group
-            $right = new Tinebase_Acl_Model_Right(array(
-                'application_id'    => $application->getId(),
-                'account_id'        => $adminGroup->getId(),
-                'account_type'      => 'group',
-                'right'             => Tinebase_Acl_Rights::RUN
-            ));
-            Tinebase_Acl_Rights::getInstance()->addRight($right);
-            
-            // admin for admin group
-            $right->right = Tinebase_Acl_Rights::ADMIN;     
-            Tinebase_Acl_Rights::getInstance()->addRight($right);            
-	    }
-	}
-
-	# give Users group read rights to the internal addressbook
-	# give Adminstrators group read/write rights to the internal addressbook
-	$internalAddressbook = Tinebase_Container::getInstance()->getContainerByName('Addressbook', 'Internal Contacts', Tinebase_Container::TYPE_INTERNAL);
-	Tinebase_Container::getInstance()->addGrants($internalAddressbook, 'group', $userGroup, array(
-	    Tinebase_Container::GRANT_READ
-	), TRUE);
-	Tinebase_Container::getInstance()->addGrants($internalAddressbook, 'group', $adminGroup, array(
-	    Tinebase_Container::GRANT_READ,
-	    Tinebase_Container::GRANT_ADD,
-	    Tinebase_Container::GRANT_EDIT,
-	    Tinebase_Container::GRANT_DELETE,
-	    Tinebase_Container::GRANT_ADMIN
-	), TRUE);
+/**
+ * initialise admin account if not given
+ */ 
+if ( DO_TABLE_SETUP === TRUE ) {
+	$setup->initialLoad();
 }
 
-echo "setup done!<br>";
+
