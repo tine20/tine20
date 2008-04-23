2a1feceac8027be086efb7c7f03d763c9f3bb8b3
Lars Kneschke <l.kneschke@metaways.de>
2008-04-23 05:18:29 +0000

    improved logic of setup.php
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2094 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Setup/Backend/Schema/Table.php
+++ tine20/Setup/Backend/Schema/Table.php
@@ -1,70 +1,127 @@
 <?php
 class Setup_Backend_Schema_Table
 {
+    /**
+     * the name of the table
+     *
+     * @var string
+     */
 	public $name;
+    
+    /**
+     * the table comment
+     *
+     * @var string
+     */
+    public $comment;
+	
+	/**
+	 * the table version
+	 *
+	 * @var int
+	 */
 	public $version;
-	public $declaration;
-//	public $engine;
+	
+	/**
+	 * the table definition
+	 *
+	 * @var array
+	 */
+	public $declaration = array();
 
 	public static function __set($prop, $value)
     {
-		if (isset(self::$prop))
-        {
+		if (isset(self::$prop)) {
 			self::$prop = $value;
-		}
-		else
-		{
+		} else {
 			throw new Exception('Undefined property [' . $prop . ']',1);
 		}
 	}
 
-
-
-
-
-	public function __construct($_tableInfo = array('TABLE_NAME'=> NULL, 'TABLE_COMMENT' => NULL ))
+	/**
+	 * the constructor
+	 *
+	 * @param string $_tableName
+	 * @param int $_tableVersion
+	 * @param string $_tableComment
+	 */
+	public function __construct($_tableName, $_tableVersion, $_tableComment = '')
 	{
-		$this->name = substr($_tableInfo['TABLE_NAME'], strlen( SQL_TABLE_PREFIX ));
-		//$this->engine = $_tableInfo['ENGINE'];
-
-		$version = explode(';', $_tableInfo['TABLE_COMMENT']);
-		$this->version = substr($version[0],9);
+		$this->name = $_tableName;
+		$this->version = $_tableVersion;
+		$this->comment = $_tableComment;
 	}
+    
+    /**
+     * the constructor
+     *
+     * @param unknown_type $prop
+     * @param unknown_type $value
+     */
+/*    public function __construct($_tableInfo = array('TABLE_NAME'=> NULL, 'TABLE_COMMENT' => NULL ))
+    {
+        $this->name = substr($_tableInfo['TABLE_NAME'], strlen( SQL_TABLE_PREFIX ));
 
-
-
-	public function setDeclarationField($_declaration)
+        $version = explode(';', $_tableInfo['TABLE_COMMENT']);
+        $this->version = substr($version[0],9);
+    } */
+	
+	/**
+	 * add one field to the table definition
+	 *
+	 * @param Setup_Backend_Schema_Field $_declaration
+	 */
+	public function addDeclarationField(Setup_Backend_Schema_Field $_declaration)
 	{
 		//var_dump(get_object_vars($_declaration));
-		$this->declaration['field'][] = new Setup_Backend_Mysql_SchemaField($_declaration);
+		$this->declaration['field'][] = $_declaration;
 	}
-
+    
+	public function setDeclarationField($_declaration)
+    {
+        //var_dump(get_object_vars($_declaration));
+        //$this->declaration['field'][] = new Setup_Backend_Schema_Field($_declaration);
+    }
+    
+    public function addDeclarationIndex(Setup_Backend_Mysql_SchemaIndex $_index)
+    {
+        $this->declaration['index'][] = new Setup_Backend_Mysql_SchemaIndex($_index);
+    }
+    
 	public function setDeclarationIndex($_index)
 	{
-	//	var_dump($_index);
+/*	//	var_dump($_index);
 	//	echo "<hr>";
 	//	foreach ($_index as $index) {
 			$this->declaration['index'][] = new Setup_Backend_Mysql_SchemaIndex($_index);
-	//	}
+	//	}*/
 	}
 
-	public function setIndex($_definition)
-	{
-//	var_dump($_definition);
+	/**
+	 * add one index to the table definition
+	 *
+	 * @param unknown_type $_definition
+	 */
+    public function addIndex($_definition)
+    {
+        foreach ($this->declaration['index'] as $index) {
+            if ($index->field['name'] == $_definition['COLUMN_NAME']) {
+                if ($_definition['CONSTRAINT_NAME'] == 'PRIMARY') {
+                    $index->setName($_definition['COLUMN_NAME']);
+                } else {
+                    $index->setName($_definition['CONSTRAINT_NAME']);
+                }
+            }
+        }
+    }
 	
-		foreach ($this->declaration['index'] as $index)
-		{
-			if ($index->field['name'] == $_definition['COLUMN_NAME'])
-			{
-				if ($_definition['CONSTRAINT_NAME'] == 'PRIMARY')
-				{
-				//var_dump($this->declaration['index']);
-				//echo "<hr>";
+    public function setIndex($_definition)
+	{
+		foreach ($this->declaration['index'] as $index) {
+			if ($index->field['name'] == $_definition['COLUMN_NAME']) {
+				if ($_definition['CONSTRAINT_NAME'] == 'PRIMARY') {
 					$index->setName($_definition['COLUMN_NAME']);
-				//	var_dump($this->declaration['index']);
-				}
-				else
-				{
+				} else {
 					$index->setName($_definition['CONSTRAINT_NAME']);
 				}
 			}
@@ -73,49 +130,30 @@ class Setup_Backend_Schema_Table
 
 	public function setForeign($_definition)
 	{
-		foreach ($this->declaration['index'] as $index)
-		{
-//			echo "<h1>"  . substr($_definition['CONSTRAINT_NAME'], strlen( SQL_TABLE_PREFIX )) . "/" .$index->field->name.  "</h1>";
-		
-//			if ($index->field->name == substr($_definition['CONSTRAINT_NAME'], strlen( SQL_TABLE_PREFIX )))
-			{
+		foreach ($this->declaration['index'] as $index) {
+            //echo "<h1>"  . substr($_definition['CONSTRAINT_NAME'], strlen( SQL_TABLE_PREFIX )) . "/" .$index->field->name.  "</h1>";
+            
+            //if ($index->field->name == substr($_definition['CONSTRAINT_NAME'], strlen( SQL_TABLE_PREFIX )))
+			//{
 				$index->setForeignKey($_definition);
-			}
+			//}
 		}
 	}
 	
 	public function fixFieldKey()
 	{
-	
-		foreach($this->declaration['index'] as $index)
-		{
-			foreach ($index->field as $fieldname)
-			{
-				foreach ($this->declaration['field'] as $field)
-				{
-				//var_dump($fieldname);
-				
-					if($fieldname == $field->name)
-					{
-						if ($index->primary == 'true')
-						{
+		foreach($this->declaration['index'] as $index) {
+			foreach ($index->field as $fieldname) {
+				foreach ($this->declaration['field'] as $field) {
+					if($fieldname == $field->name) {
+						if ($index->primary == 'true') {
 							$field->primary = 'true';
-						//	$field->unique = 'true';
-						}
-						if ($index->unique == 'true')
-						{
+						} elseif ($index->unique == 'true') {
 							$field->unique = 'true';
 						}
-						//if ($index->primary == 'true')
-						//{
-						//	$field->primary = 'true';
-						//}
 					}
 				}
 			}
 		}
 	}
-
-
-
 }
