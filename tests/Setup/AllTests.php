5aac079b6af78983ba717b4987512e055cc37c80
Matthias Greiling <m.greiling@metaways.de>
2008-04-24 18:52:53 +0000

    Added items remotely
    Y:\htdocs\tests\Setup
    
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2150 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/Setup/AllTests.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Setup
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id: AllTests.php 1536 2008-03-28 19:13:57Z lkneschke $
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+if (! defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Setup_AllTests::main');
+}
+
+class Setup_AllTests
+{
+    public static function main ()
+    {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+    
+    public static function suite ()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('Tine 2.0 Setup All Tests');
+        $suite->addTest(Setup_Backend_AllTests::suite());
+        //$suite->addTestSuite('Setup_ControllerTest');
+        return $suite;
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'Setup_AllTests::main') {
+    Setup_AllTests::main();
+}
---
+++ tests/Setup/Backend/MysqlTest.php
@@ -0,0 +1,307 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Setup
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id: SqlTest.php 1703 2008-04-03 18:16:32Z lkneschke $
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Setup_Backend_MysqlTest::main');
+}
+
+/**
+ * Test class for Tinebase_Account
+ */
+class Setup_Backend_MysqlTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var    Setup_Backend_Mysql
+     * @access protected
+     */
+    protected $object;
+
+    /**
+     * Runs the test methods of this class.
+     *
+     * @access public
+     * @static
+     */
+    public static function main()
+    {
+		$suite  = new PHPUnit_Framework_TestSuite('Tine 2.0 Setup Backend Mysql Tests');
+        PHPUnit_TextUI_TestRunner::run($suite);
+	}
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     *
+     * @access protected
+     */
+    protected function setUp()
+    {
+        $this->object = new Setup_Backend_Mysql;
+		
+		try {
+            $stmt = Zend_Registry::get('dbAdapter')->query('');
+        } catch (Zend_Db_Exception $e) {
+            var_dump($e);
+        }
+		
+		
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     *
+     * @access protected
+     */
+    protected function tearDown()
+    {
+    }
+
+    /**
+     * @todo Implement testCreateTable().
+     */
+    public function testCreateTable() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testApplicationExists().
+     */
+    public function testApplicationExists() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testTableExists().
+     */
+    public function testTableExists() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testTableVersionQuery().
+     */
+    public function testTableVersionQuery() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testApplicationVersionQuery().
+     */
+    public function testApplicationVersionQuery() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testAddTable().
+     */
+    public function testAddTable() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testExecInsertStatement().
+     */
+    public function testExecInsertStatement() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testDropTable().
+     */
+    public function testDropTable() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testRenameTable().
+     */
+    public function testRenameTable() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testAddCol().
+     */
+    public function testAddCol() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testAlterCol().
+     */
+    public function testAlterCol() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testDropCol().
+     */
+    public function testDropCol() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testAddForeignKey().
+     */
+    public function testAddForeignKey() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testDropForeignKey().
+     */
+    public function testDropForeignKey() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testDropPrimaryKey().
+     */
+    public function testDropPrimaryKey() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testAddPrimaryKey().
+     */
+    public function testAddPrimaryKey() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testAddIndex().
+     */
+    public function testAddIndex() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testDropIndex().
+     */
+    public function testDropIndex() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testExecQueryVoid().
+     */
+    public function testExecQueryVoid() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testExecQuery().
+     */
+    public function testExecQuery() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testGetMysqlDeclarations().
+     */
+    public function testGetMysqlDeclarations() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testGetMysqlIndexDeclarations().
+     */
+    public function testGetMysqlIndexDeclarations() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @todo Implement testGetMysqlForeignKeyDeclarations().
+     */
+    public function testGetMysqlForeignKeyDeclarations() {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+}		
+	
+
+if (PHPUnit_MAIN_METHOD == 'Setup_Backend_MysqlTest::main') {
+    Setup_Backend_MysqlTest::main();
+}
