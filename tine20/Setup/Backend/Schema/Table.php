82dd7406e70419bb75160bc54018f36703093126
Matthias Greiling <m.greiling@metaways.de>
2008-04-24 18:01:33 +0000

    I like developing on a more fault-tolerant platform ...
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2149 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Setup/Backend/Schema/Table.php
+++ tine20/Setup/Backend/Schema/Table.php
@@ -17,7 +17,7 @@ class Setup_Backend_Schema_Table
      *
      * @var string
      */
-	public $name;
+    public $name;
     
     /**
      * the table comment
@@ -25,176 +25,176 @@ class Setup_Backend_Schema_Table
      * @var string
      */
     public $comment;
-	
-	/**
-	 * the table version
-	 *
-	 * @var int
-	 */
-	public $version;
-	
-	
-	/**
-	 * the table engine (innodb)
-	 *
-	 * @var int
-	 */
-	public $engine = 'InnoDB';
-	
-	
-	/**
-	 * the table engine (innodb)
-	 *
-	 * @var int
-	 */
-	public $charset = 'UTF8';
-	
-		
-	
-	/**
-	 * the table columns
-	 *
-	 * @var array
-	 */
-	public $fields = array();
-	
-	/**
-	 * the table indices
-	 *
-	 * @var array
-	 */
-	public $indices = array();
-		
-	
-	/**
-	 * the constructor
-	 *
-	 * @param mixed $_tableDefinition
-	 */
-	public function __construct($_tableDefinition)
-	{
-		/*
-		 * while parsing an XML-document, tables are stored in a SimpleXMLElement structure
-		 */
-		if ($_tableDefinition instanceof SimpleXMLElement) {
-			$this->_setTableFromXml($_tableDefinition);
-		
-		/*
-		 * for easy cut&paste in update scripts table definitions can also stored as a string
-		 */		
-		} else if (isset($_tableDefinition['XmlString'])) {
-			$this->_setTableFromString($_tableDefinition);
-			
-		/*
-		 * while reading databases information schema, definitions are delivered as a stdClass object
-		 */	
-		} else if ($_tableDefinition instanceof stdClass) {
-			$this->_setTableFromObject($_tableDefinition);
-
-		} else {
-			throw new Exception('Can\'t create table');
-		}
-	
-		$this->_addIndexInformation();
-		
-	}
     
-	
-	/**
+    /**
+     * the table version
+     *
+     * @var int
+     */
+    public $version;
+    
+    
+    /**
+     * the table engine (innodb)
+     *
+     * @var int
+     */
+    public $engine = 'InnoDB';
+    
+    
+    /**
+     * the table engine (innodb)
+     *
+     * @var int
+     */
+    public $charset = 'UTF8';
+    
+        
+    
+    /**
+     * the table columns
+     *
+     * @var array
+     */
+    public $fields = array();
+    
+    /**
+     * the table indices
+     *
+     * @var array
+     */
+    public $indices = array();
+        
+    
+    /**
+     * the constructor
+     *
+     * @param mixed $_tableDefinition
+     */
+    public function __construct($_tableDefinition)
+    {
+        /*
+         * while parsing an XML-document, tables are stored in a SimpleXMLElement structure
+         */
+        if ($_tableDefinition instanceof SimpleXMLElement) {
+            $this->_setTableFromXml($_tableDefinition);
+        
+        /*
+         * for easy cut&paste in update scripts table definitions can also stored as a string
+         */        
+        } else if (isset($_tableDefinition['XmlString'])) {
+            $this->_setTableFromString($_tableDefinition);
+            
+        /*
+         * while reading databases information schema, definitions are delivered as a stdClass object
+         */    
+        } else if ($_tableDefinition instanceof stdClass) {
+            $this->_setTableFromObject($_tableDefinition);
+
+        } else {
+            throw new Exception('Can\'t create table');
+        }
+    
+        $this->_addIndexInformation();
+        
+    }
+    
+    
+    /**
      * set Table from simpleXMLObject
      *
      */
-	protected function _setTableFromXml(SimpleXMLElement $_tableDefinition)
-	{
-		$this->name = (string) $_tableDefinition->name;
-		$this->comment = (string) $_tableDefinition->comment;
-		$this->version = (string) $_tableDefinition->version;
-		//$this->charset = (string) $_tableDefinition->charset;
-		//$this->engine = (string) $_tableDefinition->engine;
-		
-		// do not worry about genus, in simpleXML, several field(s) are stored in an array named field  (pretty much the same on indices)
-		$this->_setFieldsFromXML($_tableDefinition->declaration->field);
-		$this->_setIndicesFromXML($_tableDefinition->declaration->index);
-	}
-	
-	/**
+    protected function _setTableFromXml(SimpleXMLElement $_tableDefinition)
+    {
+        $this->name = (string) $_tableDefinition->name;
+        $this->comment = (string) $_tableDefinition->comment;
+        $this->version = (string) $_tableDefinition->version;
+        //$this->charset = (string) $_tableDefinition->charset;
+        //$this->engine = (string) $_tableDefinition->engine;
+        
+        // do not worry about genus, in simpleXML, several field(s) are stored in an array named field  (pretty much the same on indices)
+        $this->_setFieldsFromXML($_tableDefinition->declaration->field);
+        $this->_setIndicesFromXML($_tableDefinition->declaration->index);
+    }
+    
+    /**
      * convert string if possible to simpleXMLElement
      *
-     */	
-	
-	protected function _setTableFromString(array $_tableDefinition)
-	{
-		try {
-			$xmlObject = new SimpleXMLElement($_tableDefinition['XmlString']);
-		} catch (Exception $e) {
-			echo $e->getMessage(); 
-		}
-		$this->_setTableFromXml($xmlObject);
-	}
-	
-	/**
+     */    
+    
+    protected function _setTableFromString(array $_tableDefinition)
+    {
+        try {
+            $xmlObject = new SimpleXMLElement($_tableDefinition['XmlString']);
+        } catch (Exception $e) {
+            echo $e->getMessage(); 
+        }
+        $this->_setTableFromXml($xmlObject);
+    }
+    
+    /**
      * set Table from database information schema
      *
      */
-	
-	protected function _setTableFromObject(stdClass $_tableDefinition)
-	{
-		//collect information von information schema - 
-		//	    $this->name = substr($_tableInfo['TABLE_NAME'], strlen( SQL_TABLE_PREFIX ));
+    
+    protected function _setTableFromObject(stdClass $_tableDefinition)
+    {
+        //collect information von information schema - 
+        //        $this->name = substr($_tableInfo['TABLE_NAME'], strlen( SQL_TABLE_PREFIX ));
 
-		//        $version = explode(';', $_tableInfo['TABLE_COMMENT']);
-		//        $this->version = substr($version[0],9);	
-	}
-	
-	
-	
-	
-	protected function _setFieldsFromXML(SimpleXMLElement $_fieldDefinitions)
+        //        $version = explode(';', $_tableInfo['TABLE_COMMENT']);
+        //        $this->version = substr($version[0],9);    
+    }
+    
+    
+    
+    
+    protected function _setFieldsFromXML(SimpleXMLElement $_fieldDefinitions)
     {
         foreach ($_fieldDefinitions as $fieldDefinition) {
-			$this->_addField(new Setup_Backend_Schema_Field($fieldDefinition, 'XML'));
-		}
-	}
+            $this->_addField(new Setup_Backend_Schema_Field($fieldDefinition, 'XML'));
+        }
+    }
 
     
-	protected function _setIndicesFromXML(SimpleXMLElement $_indicesDefinitions)
+    protected function _setIndicesFromXML(SimpleXMLElement $_indicesDefinitions)
     {
         foreach ($_indicesDefinitions as $indexDefinition) {
-			$this->_addIndex(new Setup_Backend_Schema_index($indexDefinition, 'XML'));
-		}
-	}
-    
-	
-	/**
-	 * add one field to the table definition
-	 *
-	 * @param Setup_Backend_Schema_Field $_declaration
-	 */
-	protected function _addField(Setup_Backend_Schema_Field $_field)
-	{
-		$this->fields[] = $_field;
-	}
+            $this->_addIndex(new Setup_Backend_Schema_Index($indexDefinition, 'XML'));
+        }
+    }
     
     
-	/**
-	 * add one index to the table definition
-	 *
-	 * @param unknown_type $_definition
-	 */
-	protected function _addIndex(Setup_Backend_Schema_Index $_index)
+    /**
+     * add one field to the table definition
+     *
+     * @param Setup_Backend_Schema_Field $_declaration
+     */
+    protected function _addField(Setup_Backend_Schema_Field $_field)
+    {
+        $this->fields[] = $_field;
+    }
+    
+    
+    /**
+     * add one index to the table definition
+     *
+     * @param unknown_type $_definition
+     */
+    protected function _addIndex(Setup_Backend_Schema_Index $_index)
     {
         $this->indices[] = $_index;
     }
-	
-	/**
-	 * add one index to the table definition
-	 *
-	 * @param unknown_type $_definition
-	 */
-	protected function _addIndexInformation()
+    
+    /**
+     * add one index to the table definition
+     *
+     * @param unknown_type $_definition
+     */
+    protected function _addIndexInformation()
     {
         foreach ($this->fields as $field) {
-			$field->fixFieldKey($this->indices);
-		}
-	}
+            $field->fixFieldKey($this->indices);
+        }
+    }
 }
