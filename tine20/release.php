f7b5bd1b542e13d5b59d744098e355e613acd69d
Cornelius Weiß <c.weiss@metaways.de>
2008-04-30 16:00:47 +0000

    beginning of a release script
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2245 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/release.php
@@ -0,0 +1,45 @@
+#! /Applications/xampp/xamppfiles/bin/php
+<?php
+
+if (isset($_SERVER['HTTP_HOST'])) {
+    die('not allowed!');
+}
+
+require_once 'Zend/Loader.php';
+Zend_Loader::registerAutoload();
+
+/**
+ * path to tine 2.0 checkout
+ */
+$tine20path = '/Applications/xampp/htdocs/tine20/';
+
+/**
+ * path to yui compressor
+ */
+$yuiCompressorPath = '/Users/nelius/htdocs/yuicompressor-2.3.4/build/yuicompressor-2.3.4.jar';
+
+
+
+$includeFiles = Tinebase_Http::getAllInclueFiles();
+
+$cssDebug = fopen($tine20path . 'Tinebase/css/tine-all-debug.css', 'w+');
+foreach ($includeFiles['css'] as $file) {
+    list($filename) = explode('?', $file);
+    if (file_exists("$tine20path/$filename")) {
+        fwrite($cssDebug, file_get_contents("$tine20path/$filename") . "\n");
+    }
+}
+fclose($cssDebug);
+system("java -jar $yuiCompressorPath -o $tine20path/Tinebase/css/tine-all.css $tine20path/Tinebase/css/tine-all-debug.css");
+
+$jsDebug = fopen($tine20path . '/Tinebase/js/tine-all-debug.js', 'w+');
+foreach ($includeFiles['js'] as $file) {
+    list($filename) = explode('?', $file);
+    if (file_exists("$tine20path/$filename")) {
+        fwrite($jsDebug, file_get_contents("$tine20path/$filename") . "\n");
+    }
+}
+fclose($jsDebug);
+system("java -jar $yuiCompressorPath -o $tine20path/Tinebase/js/tine-all.js $tine20path/Tinebase/js/tine-all-debug.js");
+
+?>
