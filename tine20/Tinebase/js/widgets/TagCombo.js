2cc52ef200f5c8ea2ad6ed05ef52e29c85a431af
Cornelius Weiß <c.weiss@metaways.de>
2008-05-02 09:04:16 +0000

    new tags filter widget
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2257 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/js/widgets/TagCombo.js
@@ -0,0 +1,50 @@
+/*
+ * Tine 2.0
+ * 
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+ 
+Ext.namespace('Tine.widgets', 'Tine.widgets.tags');
+
+Tine.widgets.tags.TagCombo = Ext.extend(Ext.ux.form.ClearableComboBox, {
+    /**
+     * @cfg {String} app Application which uses this panel
+     */
+    app: '',
+    /**
+     * @cfg {Bool} findGlobalTags true to find global tags during search (default: true)
+     */
+    findGlobalTags: true,
+    
+    emptyText: 'tag name',
+    typeAhead: true,
+    editable: false,
+    mode: 'remote',
+    triggerAction: 'all',
+    displayField:'name',
+    valueField:'id',
+    width: 100,
+    
+    /**
+     * @private
+     */
+    initComponent: function() {
+        this.store = new Ext.data.JsonStore({
+            id: 'id',
+            root: 'results',
+            totalProperty: 'totalCount',
+            fields: Tine.Tinebase.Model.Tag,
+            baseParams: {
+                method: 'Tinebase.getTags',
+                context: this.app,
+                owner: Tine.Tinebase.Registry.get('currentAccount').accountId,
+                findGlobalTags: this.findGlobalTags,
+            }
+        });
+        Tine.widgets.tags.TagCombo.superclass.initComponent.call(this);
+    }
+});
