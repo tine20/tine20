63573b11220dae61ed79db4aa5c28e42107262dd
Cornelius Weiß <c.weiss@metaways.de>
2008-05-05 07:26:19 +0000

    code formatting
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2285 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Addressbook/Backend/Factory.php
+++ tine20/Addressbook/Backend/Factory.php
@@ -6,18 +6,14 @@
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Lars Kneschke <l.kneschke@metaways.de>
  * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
- * @version     $Id$ *
+ * @version     $Id$
  */
-
 /**
  * backend factory class for the addressbook
  * 
- * an instance of the addressbook backendclass should be created using this class
- * 
+ * An instance of the addressbook backendclass should be created using this class
  * $contacts = Addressbook_Backend_Factory::factory(Addressbook_Backend::$type);
- * 
  * currently implemented backend classes: Addressbook_Backend_Factory::Sql
- * 
  * currently planned backend classed: Addressbook_Backend_Factory::Ldap
  * 
  * @package     Addressbook
@@ -29,35 +25,29 @@ class Addressbook_Backend_Factory
      *
      */
     const SQL = 'sql';
-    
     /**
      * constant for LDAP contacts backend class
      *
      */
     const LDAP = 'ldap';
-    
     /**
      * factory function to return a selected contacts backend class
      *
      * @param string $type
      * @return Addressbook_Backend_Interface
      */
-    static public function factory($type)
+    static public function factory ($type)
     {
-        switch($type) {
+        switch ($type) {
             case self::SQL:
                 $instance = Addressbook_Backend_Sql::getInstance();
                 break;
-                
             case self::LDAP:
                 $instance = Addressbook_Backend_Ldap::getInstance();
                 break;
-                
             default:
                 throw new Exception('unknown type');
         }
-
         return $instance;
     }
-    
 }    
