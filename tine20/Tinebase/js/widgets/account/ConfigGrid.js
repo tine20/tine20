4a1cb9c3bf5395e87acffafa19ae19ce850d0f30
Cornelius Weiß <c.weiss@metaways.de>
2008-05-06 13:11:56 +0000

    new general account config grid
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2305 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/js/widgets/account/ConfigGrid.js
@@ -0,0 +1,94 @@
+/*
+ * Tine 2.0
+ * 
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+ 
+Ext.namespace('Tine.widgets', 'Tine.widgets.account');
+Tine.widgets.account.ConfigGrid = Ext.extend(Ext.Panel, {
+    /**
+     * @cfg {Int} accountPickerWidth
+     */
+    accountPickerWidth: 200,
+    /**
+     * @cfg {Ext.data.JsonStore} configStore
+     */
+    configStore: null,
+    /**
+     * @cfg {String} property the account is stored in
+     */
+    accountProperty: 'accountId',
+    /**
+     * @cfg {Array} Array of column's config objects where the config options are in
+     */
+    configColumns: [],
+    
+    accountPicker: null,
+    configGridPanel: null,
+    
+    layout: 'column',
+    height: 200,
+    border: true,
+
+    /**
+     * @private
+     */
+    initComponent: function(){
+        
+        this.accountPicker = new Tine.widgets.account.PickerPanel(
+            this.accountPickerConfig
+        );
+        
+        var columnModel = new Ext.grid.ColumnModel([{
+                resizable: true, 
+                id: this.accountProperty, 
+                header: 'Name', 
+                dataIndex: this.accountProperty, 
+                renderer: Tine.Tinebase.Common.accountRenderer,
+                width: 70
+            }].concat(this.configColumns)
+        );
+        
+        this.configGridPanel = new Ext.grid.EditorGridPanel({
+            title: 'Permissions',
+            store: this.configStore,
+            cm: columnModel,
+            autoSizeColumns: false,
+            //selModel: rowSelectionModel,
+            enableColLock:false,
+            loadMask: true,
+            plugins: this.configColumns,
+            autoExpandColumn: this.accountProperty,
+            //bbar: permissionsBottomToolbar,
+            border: false
+        });
+        
+        this.items = this.getConfigGridLayout();
+        Tine.widgets.account.ConfigGrid.superclass.initComponent.call(this);
+        
+        this.on('afterlayout', function(){
+            var height = this.getEl().getSize().height;
+            this.items.each(function(item){
+                item.setHeight(height);
+            });
+        },this);
+    },
+    /**
+     * @private Layout
+     */
+    getConfigGridLayout: function() {
+        return [{
+            layout: 'hfit',
+            width: this.accountPickerWidth,
+            items: new Tine.widgets.account.PickerPanel({})
+        },{
+            layout: 'hfit',
+            columnWidth: 1,
+            items: this.configGridPanel
+        }];
+    }
+});
