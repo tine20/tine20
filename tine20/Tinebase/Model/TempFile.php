ee7e1e4797009889ae576c3d128b66648a5f5e79
Cornelius Weiß <c.weiss@metaways.de>
2008-05-09 14:14:08 +0000

    model of a tempFile
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2381 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/Model/TempFile.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Tinebase
+ * @subpackage  Record
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * class Tinebase_Model_TempFile
+ * 
+ * @package     Tinebase
+ * @subpackage  Record
+ */
+class Tinebase_Model_TempFile extends Tinebase_Record_Abstract 
+{
+
+    protected $_identifier = 'id';
+    
+    /**
+     * application the record belongs to
+     *
+     * @var string
+     */
+    protected $_application = 'Tinebase';
+    
+    protected $_validators = array(
+        'id'         => array('presence' => 'required', 'allowEmpty' => false, 'Alnum' ),
+        'session_id' => array('allowEmpty' => false, 'Alnum' ),
+        'time'       => array('allowEmpty' => false),
+        'path'       => array('allowEmpty' => false),
+        'name'       => array('allowEmpty' => false),
+        'type'       => array('allowEmpty' => false),
+        'error'      => array('allowEmpty' => false, 'Int'),
+        'size'       => array('allowEmpty' => false, 'Int')
+    );
+    
+    protected $_datetimeFields = array(
+        'time'
+    );
+} // end of Tinebase_Model_TempFile
