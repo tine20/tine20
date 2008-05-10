7c6ffda09a9228507c70192494890f97f328488a
Cornelius Weiß <c.weiss@metaways.de>
2008-05-10 09:15:26 +0000

    - wrap browse button to work in safari - generate img tag via XTemplate - respect witdh and height
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2399 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/ux/form/ImageField.js
+++ tine20/Tinebase/js/ux/form/ImageField.js
@@ -11,25 +11,42 @@
 Ext.namespace('Ext.ux.form');
 
 /**
- * a field which displayes a image of the given url and optionally supplies upload
- * button with the feature to display the newly uploaded image on the fly
+ * @class Ext.ux.form.ImageField
+ * 
+ * <p>A field which displayes a image of the given url and optionally supplies upload
+ * button with the feature to display the newly uploaded image on the fly</p>
+ * <p>Example usage:</p>
+ * <pre><code>
+ var formField = new Ext.ux.form.ImageField({
+     name: 'jpegimage',
+     width: 90,
+     height: 90
+ });
+ * </code></pre>
  */
 Ext.ux.form.ImageField = Ext.extend(Ext.form.Field, {
     defaultAutoCreate : {tag:'input', type:'hidden'},
     
     initComponent: function() {
         Ext.ux.form.ImageField.superclass.initComponent.call(this);
+        this.imageSrc = this.getValue();
     },
     onRender: function(ct, position) {
         Ext.ux.form.ImageField.superclass.onRender.call(this, ct, position);
-        var imgHtml = '<img src="' + this.getValue() + '" width="90px">';
-        this.imageCt = Ext.DomHelper.insertFirst(ct, imgHtml, true);
+        
+        // the container for the browe button
+        var buttonCt = Ext.DomHelper.insertFirst(ct, '<div>&nbsp;</div>', true);
+        buttonCt.setSize(90,100);
+        
+        //var imgHtml = '<img src="' + this.getValue() + '" width="90px">';
+        
+        this.imageCt = Ext.DomHelper.insertFirst(buttonCt, this.getImgTpl().apply(this), true);
 
         var bb = new Ext.ux.form.BrowseButton({
-            buttonCt: ct,
-            renderTo: this.imageCt,
+            buttonCt: buttonCt,
+            renderTo: buttonCt,
             scope: this,
-            handler: this.onFileSelect
+            handler: this.onFileSelect,
             //debug: true
         });
     },
@@ -46,10 +63,23 @@ Ext.ux.form.ImageField = Ext.extend(Ext.form.Field, {
         }).upload();
         uploader.on('uploadcomplete', function(uploader, record){
             var method = Ext.util.Format.htmlEncode('Tinebase.getTempFileThumbnail');
+            this.imageSrc = 'index.php?method=' + method + '&id=' + record.get('tempFile').id;
             var ct = this.imageCt.up('div');
-            var img = Ext.DomHelper.insertFirst(ct, '<img src="index.php?method=' + method + '&id=' + record.get('tempFile').id + '" width="90px">', true);
+            var img = Ext.DomHelper.insertFirst(ct, this.getImgTpl().apply(this), true);
             this.imageCt.remove();
             this.imageCt = img;
         }, this);
+    },
+    getImgTpl: function() {
+        if (!this.imgTpl) {
+            this.imgTpl = new Ext.XTemplate('<img ',
+                'src="{imageSrc}" ',
+                'width="{width}" ',
+                'height="{height}" ',
+                'style="border: 1px solid #B5B8C8;" ',
+                ' >'
+            ).compile();
+        }
+        return this.imgTpl;
     }
 });
