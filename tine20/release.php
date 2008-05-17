faa9adb2859e02ad8cb0d1b49f2ca6c66d025c81
Cornelius Weiß <c.weiss@metaways.de>
2008-05-17 19:46:40 +0000

    build lang releas files from *.po files
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2528 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/release.php
+++ tine20/release.php
@@ -42,4 +42,96 @@ foreach ($includeFiles['js'] as $file) {
 fclose($jsDebug);
 system("java -jar $yuiCompressorPath -o $tine20path/Tinebase/js/tine-all.js $tine20path/Tinebase/js/tine-all-debug.js");
 
+
+$translations = array();
+
+// collect translations
+$d = dir($tine20path);
+while (false !== ($appName = $d->read())) {
+    $appPath = "$tine20path/$appName";
+    if (is_dir($appPath) && $appName{0} != '.') {
+        $translationPath = "$appPath/translations";
+        if(is_dir($translationPath)) {
+            $files = scandir($translationPath);
+            foreach ($files as $file) {
+                $filePath = "$translationPath/$file";
+                if (is_file($filePath) && substr($file , -3) == '.po') {
+                    list($locale) = explode('.', $file);
+                    $poObject = po2jsObject($filePath);
+                    $translations[$locale][] = getJs($locale, $appName, $poObject);
+                }
+            }
+        }
+    }
+   
+}
+$d->close();
+
+// dump one langfile for each locale
+foreach ($translations as $locale => $domains) {
+    $js = '';
+    foreach ($domains as $domain) {
+        $js = $js . $domain;
+    }
+    file_put_contents("$tine20path/Tinebase/js/$locale-debug.js", $js);
+    system("java -jar $yuiCompressorPath -o $tine20path/Tinebase/js/$locale.js $tine20path/Tinebase/js/$locale-debug.js");
+}
+
+/**
+ * returns key of translations object in Locale.Gettext
+ *
+ * @param string $locale
+ * @param string $appName
+ * @return string
+ */
+function getJs($locale, $appName, $poObject)
+{
+    return "Locale.Gettext.prototype._msgs['./$locale/LC_MESSAGES/$appName'] = new Locale.Gettext.PO($poObject);";
+}
+
+/**
+ * convertes po file to js object
+ *
+ * @param string $filePath
+ * @return string
+ */
+function po2jsObject($filePath)
+{
+    $po = file_get_contents($filePath);
+    
+    global $first, $plural;
+    $first = true; 
+    $plural = false;
+    
+    $po = preg_replace('/\r?\n/', "\n", $po);
+    $po = preg_replace('/#.*\n/', '', $po);
+    $po = preg_replace('/"(\s+)"/', '', $po);
+    $po = preg_replace('/msgid "(.*?)"\nmsgid_plural "(.*?)"/', 'msgid "$1, $2"', $po);
+    $po = preg_replace_callback('/msg(\S+) /', create_function('$matches','
+        global $first, $plural;
+        switch ($matches[1]) {
+            case "id":
+                if ($first) {
+                    $first = false;
+                    return "";
+                }
+                if ($plural) {
+                    $plural = false;
+                    return "]\n, ";
+                }
+                return ", ";
+            case "str":
+                return ": ";
+            case "str[0]":
+                $plural = true;
+                return ": [\n  ";
+            default:
+                return " ,";
+        }
+    '), $po);
+    $po = "({\n" . (string)$po . ($plural ? "]\n})" : "\n})");
+    return $po;
+    //$js = "Locale.Gettext.prototype._msgs['./de/LC_MESSAGES/Addressbook'] = new Locale.Gettext.PO($po);";
+}
+
 ?>
