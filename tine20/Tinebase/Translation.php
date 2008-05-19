b2493f8bff1e0db07ab8d6cc72239f63e6eaeaa6
Cornelius Weiß <c.weiss@metaways.de>
2008-05-19 15:24:30 +0000

    add translations helper functions
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2537 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Translation.php
+++ tine20/Tinebase/Translation.php
@@ -18,7 +18,7 @@
  */
 class Tinebase_Translation
 {
-    static function getTranslation($_applicationName)
+    public static function getTranslation($_applicationName)
     {
         $path = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . ucfirst($_applicationName) . DIRECTORY_SEPARATOR . 'translations';
         
@@ -33,5 +33,100 @@ class Tinebase_Translation
         }
         
         return $translate;
+    }
+    
+    /**
+     * returns the available language java script from a given locale
+     * 
+     * @param  Zend_Locale|String $_locale
+     * $param  string             $_location required location on of {generic|tine|ext}
+     * @return string             filepath relative to tine installation
+     */
+    public static function getJsTranslationFile($_locale, $_location='tine')
+    {
+        switch ($_location) {
+        	case 'generic':
+        	   $dir = 'Tinebase/js/Locale/data/';
+        	   $prefix = 'generic-';
+        	   $suffix = '.js';
+        	   break;
+        	case 'tine':
+        	   $dir = 'Tinebase/js/';
+               $prefix = '';
+               $suffix = '.js';
+               break;
+        	case 'ext':
+        	    $dir = '../ExtJS/build/locale/';
+               $prefix = 'ext-lang-';
+               $suffix = '-min.js';
+               break;
+        	default:
+        		throw new Exception('no such location');
+        	   break;
+        }
+        
+        $locale = (string)$_locale;
+        $language = $_locale->getLanguage();
+                
+        if (! $_locale instanceof Zend_Locale) {
+            $_locale = new Zend_Locale($_locale);
+        }
+        
+        $file = $dir . $prefix . $locale . $suffix;
+        if (file_exists(dirname(__FILE__) . "/../$file")) {
+            return $file;
+        }
+        
+        $file = $dir . $prefix . $language . $suffix;
+        if (file_exists(dirname(__FILE__) . "/../$file")) {
+            return $file;
+        }
+        
+        // fallback
+        return $dir . $prefix . 'en' . $suffix;
+    }
+    
+    /**
+     * convertes po file to js object
+     *
+     * @param  string $filePath
+     * @return string
+     */
+    public static function po2jsObject($filePath)
+    {
+        $po = file_get_contents($filePath);
+        
+        global $first, $plural;
+        $first = true; 
+        $plural = false;
+        
+        $po = preg_replace('/\r?\n/', "\n", $po);
+        $po = preg_replace('/#.*\n/', '', $po);
+        $po = preg_replace('/"(\s+)"/', '', $po);
+        $po = preg_replace('/msgid "(.*?)"\nmsgid_plural "(.*?)"/', 'msgid "$1, $2"', $po);
+        $po = preg_replace_callback('/msg(\S+) /', create_function('$matches','
+            global $first, $plural;
+            switch ($matches[1]) {
+                case "id":
+                    if ($first) {
+                        $first = false;
+                        return "";
+                    }
+                    if ($plural) {
+                        $plural = false;
+                        return "]\n, ";
+                    }
+                    return ", ";
+                case "str":
+                    return ": ";
+                case "str[0]":
+                    $plural = true;
+                    return ": [\n  ";
+                default:
+                    return " ,";
+            }
+        '), $po);
+        $po = "({\n" . (string)$po . ($plural ? "]\n})" : "\n})");
+        return $po;
     }
 }
