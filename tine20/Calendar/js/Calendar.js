586714b7f2ad02b3711ba4b5e1df34bf035ac180
Cornelius Weiß <c.weiss@metaways.de>
2008-05-24 15:04:13 +0000

    toggle buttons are in api now. this solves the allways pressed buttons
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2615 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Calendar/js/Calendar.js
+++ tine20/Calendar/js/Calendar.js
@@ -543,48 +543,5 @@ Tine.Calendar.ToolBar = function() {
 	};
 }();
 
-/**
- * @class Ext.ux.ButtonLockedToggle & Ext.ux.tbButtonLockedToggle
- * @extends Ext.Button
- * The normal button, when enableToggle is used and toggleGroup is set correctly, still allows the user
- * to toggle off the toggled button by pressing on it. This class overrides the toggle-method so that
- * the toggled button is impossible to 'untoggle' other than programmatically or as a reaction
- * to any of the other buttons in the group getting toggled on.
- *
- * Toggle is by the way a very strange word when you repeat it enough.
- *
- * @author www.steinarmyhre.com
- * @constructor
- * Identical to Ext.Button and/or Ext.Toolbar.Button except that enableToggle is true by default.
- * @param (Object/Array) config A config object
- */
-Ext.ux.ButtonLockedToggle = Ext.extend(Ext.Button,{
-    enableToggle: true,
-
-    toggle: function(state){
-        if(state == undefined && this.pressed) return;
-        state = state === undefined ? !this.pressed : state;
-        if(state != this.pressed){
-            if(state){
-                this.el.addClass("x-btn-pressed");
-                this.pressed = true;
-                this.fireEvent("toggle", this, true);
-            }else{
-                this.el.removeClass("x-btn-pressed");
-                this.pressed = false;
-                this.fireEvent("toggle", this, false);
-            };
-            if(this.toggleHandler){
-                this.toggleHandler.call(this.scope || this, this, state);
-            };
-        };
-    }
-});
-
-Ext.ux.tbButtonLockedToggle = Ext.extend(Ext.Toolbar.Button, Ext.ux.ButtonLockedToggle);
-
-Ext.ComponentMgr.registerType('btnlockedtoggle', Ext.ux.ButtonLockedToggle);
-Ext.ComponentMgr.registerType('tbbtnlockedtoggle', Ext.ux.tbButtonLockedToggle);
-
 
 
