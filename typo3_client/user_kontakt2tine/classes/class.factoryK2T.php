07fc1f182dd66a25db8988e8d4357ef40c66b9e9
Matthias Greiling <m.greiling@metaways.de>
2008-05-26 08:05:55 +0000

    Added items remotely generate ext_tables xml definitions
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2617 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ typo3_client/user_kontakt2tine/classes/class.factoryK2T.php
@@ -0,0 +1,75 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+* @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * Plugin 'user_tine2typo' for the 'user_tine2typo' extension.
+ *
+ * @author  Matthias Greiling <typo3@metaways.de>
+ * @comment this plugin is designed for TINE20 http://www.tine20.org
+ * @version     $$
+ */
+ 
+class FactoryK2T
+{
+	public static function createDB( $type = 'PDO' )
+	{
+		include( PATH_site . 'typo3conf/localconf.php' );
+
+		if( !isset( $typo_db_adapter ) ) { $typo_db_adapter = 'mysql'; }
+
+		switch( $typo_db_adapter )
+		{
+			case 'PDO':
+				
+				try{
+					$conf = array(
+						'dsn' => 'mysql:host=' . $typo_db_host. ';dbname=' . $typo_db,
+						'user' => $typo_db_username,
+						'pass' => $typo_db_password,
+						'attr' => array(
+							PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+// 						PDO::ATTR_PERSISTENT => true,
+						),
+					);
+
+					$dbh = new PDO( $conf['dsn'], $conf['user'], $conf['pass'], $conf['attr'] );
+					$dbh->exec( "SET NAMES 'utf8'" );
+
+					return $dbh;
+				}
+				catch (Exception $e)
+				{
+				print_r($e->getMessage());
+				}
+				break;
+			case 'mysql':{
+			
+				
+			}	
+				
+			default:
+				
+				throw new Exception( 'Unknown database type: ' . $type );
+		}
+	}
+}
---
+++ typo3_client/user_kontakt2tine/classes/class.tx_DynamicFlexFormFieldsK2T.php
@@ -0,0 +1,217 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+* @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * Plugin 'user_tine2typo' for the 'user_tine2typo' extension.
+ *
+ * @author  Matthias Greiling <typo3@metaways.de>
+ * @comment this plugin is designed for TINE20 http://www.tine20.org
+ * @version     $$
+ */
+ 
+require_once( 'class.factoryK2T.php' );
+
+if (@is_dir(PATH_site.'typo3/sysext/cms/tslib/')) 
+{
+	define('PATH_tslib', PATH_site.'typo3/sysext/cms/tslib/');
+} 
+elseif (@is_dir(PATH_site.'tslib/')) 
+{
+	define('PATH_tslib', PATH_site.'tslib/');
+} else {
+	// define path to tslib/ here:
+	$configured_tslib_path = '';
+	// example:
+	// $configured_tslib_path = '/var/www/mysite/typo3/sysext/cms/tslib/';
+	define('PATH_tslib', $configured_tslib_path);
+}
+
+if (PATH_tslib=='') {
+	die('Cannot find tslib/. Please set path by defining $configured_tslib_path in '.basename(PATH_thisScript).'.');
+}
+require_once(PATH_tslib . 'class.tslib_pibase.php');
+
+
+class tx_DynamicFlexFormFieldsK2T extends tslib_pibase 
+{
+	var $UID;
+	var $flexform;
+	var $prefixId = 'tx_DynamicFlexFormFieldsK2T';		// Same as class name
+	var $scriptRelPath = 'pi1/class.tx_DynamicFlexFormFieldsK2T.php';	// Path to this script relative to the extension dir.
+	var $extKey = 'user_kontakt2tine';	// The extension key.
+   
+	/**
+     *Get UID of flexform saved data for prefilled form
+     */
+	public function __construct()
+	{
+		$preUID = str_replace('%5D%5B', '', explode('tt_content',$GLOBALS[_SERVER][QUERY_STRING]));
+		$preUID2 = explode(',', $preUID[1]);
+		$this->UID = trim(str_replace('][', '', $preUID2[0])) * 1 ;
+	}
+	
+	/**
+     *Get flexform saved data for prefilled form
+     *
+     * @param int $_uid
+     * @return array
+     */
+	public function getXML( $_uid )
+	{
+		$config = "-";
+		try
+		{
+			$dbh = Factory::createDB( 'PDO' );
+			
+			$sql = '
+				SELECT pi_flexform
+				FROM tt_content
+				WHERE uid > ' . $_uid ;
+			
+			$stmt = $dbh->prepare( $sql );
+			$stmt->execute();
+			$row = $stmt->fetch( PDO::FETCH_ASSOC ) ;
+			$config = $row['pi_flexform'];
+			
+			$dbh = null;
+		}
+		catch( PDOException $pe ) {
+			print_r( "PDO-Message" .  $pe->getMessage() . ' - ' . $pe->errorInfo );
+		}
+		catch( Exception $e ) {
+			// seems not to be able to PDO
+			include( PATH_site . 'typo3conf/localconf.php' );
+			
+			$mysql_conn = mysql_connect($typo_db_host, $typo_db_username, $typo_db_password);
+			@mysql_select_db($typo_db);
+			$sql = "SELECT `pi_flexform` FROM `tt_content` WHERE `uid` = '" . $_uid . "';";
+			$result = @mysql_query($sql) or die ($sql);
+			
+			$fetch = @mysql_fetch_array($result);
+			$config = $fetch['pi_flexform'];
+			
+			@mysql_close($mysql_conn);
+		}
+		
+		$xml = t3lib_div::xml2array($config);
+		return $xml;
+	}
+
+   
+	/**
+     *Get flexform saved data for prefilled form
+     *
+     * @param array (origin ext_tables)
+     * @return array
+     */
+	 function getContainers($config)
+	{
+	if (!empty($this->UID)) {
+		$this->flexform = $this->getXML($this->UID);
+	
+	
+		// zend auto loader will load any classes - can not use it in this enviroment
+		require_once( PATH_site . 'typo3conf/ext/user_kontakt2tine/pi1/TineClient/Connection.php');
+		require_once( PATH_site . 'typo3conf/ext/user_kontakt2tine/pi1/TineClient/Service/Abstract.php');
+		require_once( PATH_site . 'typo3conf/ext/user_kontakt2tine/pi1/Zend/Http/Client.php');
+		require_once( PATH_site . 'typo3conf/ext/user_kontakt2tine/pi1/Addressbook/Model/Contact.php');
+		require_once( PATH_site . 'typo3conf/ext/user_kontakt2tine/pi1/Addressbook/Service.php');
+		
+		try
+		{
+			// open connection
+			$client = new TineClient_Connection($this->pi_getFFvalue($this->flexform, 'tinehost'));
+			//$client->setDebugEnabled(true);
+			TineClient_Service_Abstract::setDefaultConnection($client);
+
+			// login to tine2.0
+			$client->login(
+						$this->pi_getFFvalue($this->flexform, 'tinehostlogin'), 
+						$this->pi_getFFvalue($this->flexform, 'tinehostpassword')
+						);
+		}
+		catch (Exception $e) 
+		{
+			$config['items'][] = array('+' . $this->UID . 'Can not log in - no access throught this data');
+			$config['items'][] = array($this->pi_getFFvalue($this->flexform, 'tinehostlogin') );
+			$config['items'][] = array($this->pi_getFFvalue($this->flexform, 'tinehostpassword'));
+			$config['items'][] = array($e->getMessage());
+			//exit;
+		}
+		try 
+		{
+			// get all contacts of the user
+			$addressbook = new Addressbook_Service();
+		}
+		catch (Exception $e) 
+		{
+		//var_dump($e);
+		}
+		
+		
+		try { 
+			$container = $client->getContainer();
+		
+		} catch(Exception $e) {
+		//	echo "can not get container" . $e;
+		//	var_dump($e);
+		}
+		
+
+		
+		try
+		{
+			$client->logout();
+		}
+			catch (Exception $e) 
+		{
+		//	var_dump($e);
+		}
+		
+		// bring it to typo3- interna formatting
+		if (is_array($container)) 
+		{
+			foreach ($container as $key => $val)
+			{
+				$config['items'][$val['id']] = array('[' . $val['id'] . ']' . utf8_decode($val['name']),  '[' . $val['id'] . ']' . utf8_decode($val['name']) );
+			}
+		}
+	}
+	
+	// unset first element 
+	unset($config['items'][0]);
+	//print_r($config['items');
+	// login succeded, but no data available'
+	if (empty($config['items']))
+	{
+		$config['items'][0] = array( 'login succeded, but no data available' , 'login succeded, but no data available');
+	}
+	if (empty($this->UID)){
+		$config['items'][0] = array( 'system unsufficent', 'system unsufficent');
+	}
+	return $config;
+	}
+	
+}
+
+?>
