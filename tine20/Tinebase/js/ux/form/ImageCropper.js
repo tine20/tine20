5dbc491f5e4a4a239b60697af5028d13a3b8879d
Cornelius Weiß <c.weiss@metaways.de>
2008-06-04 06:41:17 +0000

    yui basesd image croper
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2661 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/ux/form/ImageCropper.js
+++ tine20/Tinebase/js/ux/form/ImageCropper.js
@@ -25,21 +25,76 @@ Ext.ux.form.ImageCropper = function(config) {
     );
 };
 
-Ext.extend(Ext.ux.form.ImageCropper, Ext.Window, {
+Ext.extend(Ext.ux.form.ImageCropper, Ext.Component, {
+    /**
+     * @property {Object} natrual size of image
+     */
+    imageSize: false,
+    
     width: 320,
     height: 320,
-    title: 'Crop Image',
-    layout: 'fit',
+    
     initComponent: function() {
-        var dlg = new Tine.widgets.dialog.EditRecord({
-            handlerScope: this,
-            handlerCancle: this.close,
-            items: [{
-                xtype: 'panel',
-                html: '<img src="' + this.image + '" width="320" height="240">'
-            }]
-        });
-        this.items = dlg;
+        this.imageURL.width = 320;
+        this.imageURL.height = 240;
+
         Ext.ux.form.ImageCropper.superclass.initComponent.call(this);
+    },
+    onRender: function(ct, position) {
+        Ext.ux.form.ImageCropper.superclass.onRender.call(this, ct, position);
+        this.wrapEl = Ext.DomHelper.insertFirst(ct, {tag: 'div'}, true);
+        //this.maskEl = Ext.DomHelper.insertFirst(this.wrapEl, {tag: 'div', style: 'background-color: #000000'}, true);
+        
+        // bg image
+        this.bgImageEl = Ext.DomHelper.insertFirst(this.wrapEl, {tag: 'img', id: 'yui_img', src: this.imageURL}, true);
+        this.bgImageEl.setOpacity(0.5);
+        
+        // yui cropper is very fast. 
+        // cause of the window usage the yui crop mask does not work, so we use the mask obove
+        var Dom = YAHOO.util.Dom, 
+        Event = YAHOO.util.Event; 
+    
+        var crop = new YAHOO.widget.ImageCropper('yui_img');
+        
+        /*
+         // Ext only implementation is very slow!
+         // the bad news is that the resizeable dosn't fire events while resizeing 
+         // and that the dd onDrag looses scope
+         this.fgImageEl = Ext.DomHelper.insertFirst(this.wrapEl, {tag: 'div', style: {
+            width              : '100px',
+            height             : '100px',
+            position           : 'absolute',
+            top                : '30px',
+            left               : '30px',
+            'background-image' : 'url(' + this.imageURL + ')',
+            'background-repeat': 'no-repeat'
+        }}, true);
+        
+        this.resizeable = new Ext.Resizable(this.fgImageEl, {
+            wrap:true,
+            pinned:true,
+            handles: 's e se',
+            draggable:true,
+            dynamic:true,
+        });
+        
+        this.resizeable.dd.onDrag = function(e) {
+            var fgEl = Ext.get(e.getTarget());
+            var bgEl = fgEl.up('div').up('div').down('img');
+            var dx = bgEl.getX() - fgEl.getX();
+            var dy = bgEl.getY() - fgEl.getY();
+            fgEl.setStyle('background-position', dx + 'px ' + dy + 'px');
+        }
+        
+        // fix opacity, which might be broken by the inherited window properties
+        var rhs = this.resizeable.getEl().query('div.x-resizable-handle');
+        for (var i=0, j=rhs.length; i<j; i++) {
+            Ext.get(rhs[i]).setOpacity(1);
+        }
+        
+        var dx = bgEl.getX() - fgEl.getX();
+        var dy = bgEl.getY() - fgEl.getY();
+        fgEl.setStyle('background-position', dx + 'px ' + dy + 'px');
+        */
     }
 });
