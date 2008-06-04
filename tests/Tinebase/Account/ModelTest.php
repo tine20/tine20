9852b17971e30830cc4ae667f07bcffeaf3090f3
Philipp Schüle <p.schuele@metaways.de>
2008-06-04 13:44:47 +0000

    added new test class for account (user) models
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2675 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/Tinebase/Account/ModelTest.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Tinebase
+ * @subpackage  Account
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @version     $Id: SqlTest.php 1350 2008-03-25 21:09:20Z lkneschke $
+ * 
+ * @todo        write test class for each of the account models?
+ * @todo        implement other tests for the model functions
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Tinebase_Account_ModelTest::main');
+}
+
+/**
+ * Test class for Tinebase_Account_Model_*
+ */
+class Tinebase_Account_ModelTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var array test objects
+     */
+    protected $objects = array();
+
+    /**
+     * Runs the test methods of this class.
+     *
+     * @access public
+     * @static
+     */
+    public static function main()
+    {
+		$suite  = new PHPUnit_Framework_TestSuite('Tinebase_Account_ModelTest');
+        PHPUnit_TextUI_TestRunner::run($suite);
+	}
+
+    /**
+     * Sets up the fixture.
+     * This method is called before a test is executed.
+     *
+     * @access protected
+     */
+    protected function setUp()
+    {
+        $this->objects['account'] = Zend_Registry::get('currentAccount');
+    }
+
+    /**
+     * Tears down the fixture
+     * This method is called after a test is executed.
+     *
+     * @access protected
+     */
+    protected function tearDown()
+    {
+    }
+
+    /**
+     * try to get account applications 
+     *
+     */
+    public function testGetApplications()
+    {
+        $applications = $this->objects['account']->getApplications();
+        
+        $this->assertGreaterThan(0, count($applications));
+    }
+
+    /**
+     * try to get account applications 
+     *
+     */
+    public function testGetRights()
+    {
+        $rights = $this->objects['account']->getRights('Admin');
+
+        $this->assertGreaterThan(0, count($rights));
+    }
+    
+    /**
+     * try to check right
+     *
+     */
+    public function testHasRight()
+    {
+        $hasRight = $this->objects['account']->hasRight('Admin', Tinebase_Acl_Rights::ADMIN );
+
+        $this->assertTrue($hasRight);
+    }
+
+    /**
+     * try to get account group memberships 
+     *
+     */
+    public function testGetGroupMemberships()
+    {
+        $groups = $this->objects['account']->getGroupMemberships();
+
+        $this->assertGreaterThan(0, count($groups));
+    }
+}		
