0372780bbcd531fddf019228caaca045c8efea99
Philipp Schüle <p.schuele@metaways.de>
2008-06-16 13:40:58 +0000

    changed update_8 to create new relations tables
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2890 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Setup/Update/Release0.php
+++ tine20/Tinebase/Setup/Update/Release0.php
@@ -676,113 +676,151 @@ class Tinebase_Setup_Update_Release0 extends Setup_Update_Abstract
      */
     function update_8()
     {
-        $this->validateTableVersion('record_relations', '1');
+        //$this->validateTableVersion('record_relations', '1');
         $this->_backend->dropTable('record_relations');
         
         $tableDefinition = ('
-            <table>
-                <name>record_relations</name>
-                <version>2</version>
-                <declaration>
+        <table>
+            <name>relations</name>
+            <version>3</version>
+            <declaration>
+                <field>
+                    <name>id</name>
+                    <type>text</type>
+                    <length>40</length>
+                    <notnull>true</notnull>
+                </field>
+                <field>
+                    <name>own_model</name>
+                    <type>text</type>
+                    <length>40</length>
+                    <notnull>true</notnull>
+                </field>
+                <field>
+                    <name>own_backend</name>
+                    <type>text</type>
+                    <length>40</length>
+                    <notnull>true</notnull>
+                </field>
+                <field>
+                    <name>own_id</name>
+                    <type>text</type>
+                    <length>40</length>
+                    <notnull>true</notnull>
+                </field>
+                <field>
+                    <name>own_degree</name>
+                    <type>enum</type>
+                    <value>parent</value>
+                    <value>child</value>
+                    <value>sibling</value>
+                    <notnull>true</notnull>
+                </field>
+                <field>
+                    <name>related_model</name>
+                    <type>text</type>
+                    <length>40</length>
+                    <notnull>true</notnull>
+                </field>
+                <field>
+                    <name>related_backend</name>
+                    <type>text</type>
+                    <length>40</length>
+                    <notnull>true</notnull>
+                </field>
+                <field>
+                    <name>related_id</name>
+                    <type>text</type>
+                    <length>40</length>
+                    <notnull>true</notnull>
+                </field>
+                <field>
+                    <name>type</name>
+                    <type>text</type>
+                    <length>40</length>
+                    <notnull>true</notnull>
+                </field>
+                <field>
+                    <name>remark</name>
+                    <type>text</type>
+                    <length>256</length>
+                    <notnull>true</notnull>
+                </field>
+                <field>
+                    <name>created_by</name>
+                    <type>integer</type>
+                    <notnull>true</notnull>
+                </field>
+                <field>
+                    <name>creation_time</name>
+                    <type>datetime</type>
+                    <notnull>true</notnull>
+                </field> 
+                <field>
+                    <name>last_modified_by</name>
+                    <type>integer</type>
+                    <notnull>true</notnull>
+                </field>
+                <field>
+                    <name>last_modified_time</name>
+                    <type>datetime</type>
+                    <notnull>true</notnull>
+                </field>
+                <field>
+                    <name>is_deleted</name>
+                    <type>boolean</type>
+                    <notnull>true</notnull>
+                    <default>false</default>
+                </field>
+                <field>
+                    <name>deleted_by</name>
+                    <type>integer</type>
+                    <notnull>true</notnull>
+                </field>            
+                <field>
+                    <name>deleted_time</name>
+                    <type>datetime</type>
+                </field>
+                <index>
+                    <name>id-own_model-own_backend-own_id</name>
+                    <primary>true</primary>
                     <field>
                         <name>id</name>
-                        <type>text</type>
-                        <length>40</length>
-                        <notnull>true</notnull>
                     </field>
                     <field>
-                        <name>own_application</name>
-                        <type>text</type>
-                        <length>40</length>
-                        <notnull>true</notnull>
+                        <name>own_model</name>
+                    </field>
+                    <field>
+                        <name>own_backend</name>
                     </field>
                     <field>
                         <name>own_id</name>
-                        <type>text</type>
-                        <length>40</length>
-                        <notnull>true</notnull>
+                    </field>
+                </index>
+                <index>
+                    <name>unique-fields</name>
+                    <unique>true</unique>
+                    <field>
+                        <name>own_model</name>
                     </field>
                     <field>
-                        <name>related_application</name>
-                        <type>text</type>
-                        <length>40</length>
-                        <notnull>true</notnull>
+                        <name>own_backend</name>
+                    </field>
+                    <field>
+                        <name>own_id</name>
+                    </field>
+                    <field>
+                        <name>related_model</name>
+                    </field>
+                    <field>
+                        <name>related_backend</name>
                     </field>
                     <field>
                         <name>related_id</name>
-                        <type>text</type>
-                        <length>40</length>
-                        <notnull>true</notnull>
-                    </field> 
-                    <field>
-                        <name>related_role</name>
-                        <type>text</type>
-                        <length>64</length>
-                        <notnull>false</notnull>
-                    </field>                  
-                    <field>
-                        <name>created_by</name>
-                        <type>integer</type>
-                        <notnull>true</notnull>
                     </field>
-                    <field>
-                        <name>creation_time</name>
-                        <type>datetime</type>
-                        <notnull>true</notnull>
-                    </field> 
-                    <field>
-                        <name>last_modified_by</name>
-                        <type>integer</type>
-                        <notnull>true</notnull>
-                    </field>
-                    <field>
-                        <name>last_modified_time</name>
-                        <type>datetime</type>
-                        <notnull>true</notnull>
-                    </field>
-                    <field>
-                        <name>is_deleted</name>
-                        <type>boolean</type>
-                        <notnull>true</notnull>
-                        <default>false</default>
-                    </field>
-                    <field>
-                        <name>deleted_by</name>
-                        <type>integer</type>
-                        <notnull>true</notnull>
-                    </field>            
-                    <field>
-                        <name>deleted_time</name>
-                        <type>datetime</type>
-                    </field>
-                    <index>
-                        <name>id</name>
-                        <primary>true</primary>
-                        <field>
-                            <name>id</name>
-                        </field>
-                    </index>
-                    <index>
-                        <name>own_application_id-own_id-related-role_id</name>
-                        <unique>true</unique>
-                        <field>
-                            <name>own_application</name>
-                        </field>
-                        <field>
-                            <name>own_id</name>
-                        </field>
-                        <field>
-                            <name>related_role</name>
-                        </field>
-                        <field>
-                            <name>related_application</name>
-                        </field>
-                        <field>
-                            <name>related_id</name>
-                        </field>
-                    </index>
-                </declaration>
-            </table>
+                </index>
+            </declaration>
+        </table>
         ');
         
         $table = Setup_Backend_Schema_Table_Factory::factory('String', $tableDefinition); 
