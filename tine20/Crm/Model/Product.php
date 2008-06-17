4ae35d20cd6b1775e6a6e0a74fba9be76dbe375d
Philipp Schüle <p.schuele@metaways.de>
2008-06-17 21:37:34 +0000

    reset the right license (agpl)
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2937 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Crm/Model/Product.php
+++ tine20/Crm/Model/Product.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * class to hold option data
+ * class to hold product data
  * 
  * @package     Crm
- * @license     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Thomas Wadewitz <t.wadewitz@metaways.de>
  * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
