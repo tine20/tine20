d0aaab26addd682394c2e7e65416311bdc51574d
Philipp Schüle <p.schuele@metaways.de>
2008-06-17 16:25:54 +0000

    oops ... i meant the tasks_tag table ;)
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2921 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tasks/Backend/Sql.php
+++ tine20/Tasks/Backend/Sql.php
@@ -9,7 +9,7 @@
  * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
  *
- * @todo        remove tasks_tag table
+ * @todo        remove tasks_tag table 
  */
 
 /**
