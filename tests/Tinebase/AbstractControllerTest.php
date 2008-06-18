a0dd817f28ff7f220a19f08d31e2bf23001b95b6
Cornelius Weiß <c.weiss@metaways.de>
2008-06-18 08:51:43 +0000

    start of an abstract controller test
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2944 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/Tinebase/AbstractControllerTest.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Tinebase
+ * @subpackage  Record
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+
+
+/**
+ * Abstact tests for record controllers
+ */
+class Tinebase_AbstractControllerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * application name of the controller to test
+     *
+     * @var string
+     */
+    protected $_appName;
+    
+    /**
+     * Name of the model(s) this controller handels
+     *
+     * @var array <application>_Model_<modelName> => <modelName>
+     */
+    protected $_modelNames = array();
+    
+    /**
+     * Minimal data needed to gain a valid record of $this->_modelName
+     *
+     * @var array modelName => array data
+     */
+    protected $_miminamDatas = array();
+    
+    /**
+     * @var <Application>_Controller
+     */
+    protected $_controller;
+    
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     *
+     * @access protected
+     */
+    protected function setUp()
+    {
+        $this->_controller = Tinebase_Controller::getInstance()->getApplicationInstance($this->_appName);
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     *
+     * @access protected
+     */
+    protected function tearDown()
+    {
+        
+    }
+    
+    /**
+     * testGetInstance().
+     */
+    public function testGetInstance()
+    {
+        $controllerName = $this->_appName . '_Controller';
+        $this->assertTrue($this->_controller instanceof $controllerName);
+    }
+    
+    public function testCreateRecords()
+    {
+        foreach($this->_modelNames as $fullModelName => $modelName) {
+            $createFunctionName = 'Create' . $modelName;
+            $record = new $fullModelName($this->_miminamDatas[$modelName]);
+            
+            //$currentAccount = $this->_currentAccount = Zend_Registry::get('currentAccount');
+            //$personalContainer = $this->_controller->createPersonalFolder($currentAccount);
+            //$record->container_id = $personalContainer->getId();
+            
+            $persitentRecord = $this->_controller->$createFunctionName($record);
+        }
+    }
+    
+    public function testGetRecord()
+    {
+        
+    }
+    
+}
+
