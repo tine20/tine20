e8469764e0957adfb95df63240031e5d7c8cc2d4
Lars Kneschke <l.kneschke@metaways.de>
2008-06-18 10:27:15 +0000

    added new phone filter class
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2948 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Voipmanager/Model/PhoneFilter.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Voipmanager
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+
+/**
+ * Phone Filter Class
+ * @package Voipmanager
+ */
+class Voipmanager_Model_PhoneFilter extends Tinebase_Record_Abstract
+{
+	/**
+     * key in $_validators/$_properties array for the filed which 
+     * represents the identifier
+     * 
+     * @var string
+     */    
+    protected $_identifier = 'id';
+    
+    /**
+     * application the record belongs to
+     *
+     * @var string
+     */
+    protected $_application = 'Voipmanager';
+    
+    protected $_validators = array(
+        'id'                    => array('allowEmpty' => true,  'Int'   ),
+
+        'macaddress'            => array('allowEmpty' => true           ),
+        'ipaddress'             => array('allowEmpty' => true           ),
+        'description'           => array('allowEmpty' => true           ),
+        'query'                 => array('allowEmpty' => true           )
+        //'showClosed'          => array('allowEmpty' => true, 'InArray' => array(true,false)),
+    );
+}
