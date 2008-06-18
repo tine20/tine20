5e8067ca35c97923b07b4770e1899b17a528d46e
Cornelius Weiß <c.weiss@metaways.de>
2008-06-18 08:27:51 +0000

    update docu header
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2942 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tasks/Backend/Interface.php
+++ tine20/Tasks/Backend/Interface.php
@@ -4,7 +4,7 @@
  * 
  * @package     Tasks
  * @subpackage  Backend
- * @license     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Cornelius Weiss <c.weiss@metaways.de>
  * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
--- tine20/Tasks/Controller.php
+++ tine20/Tasks/Controller.php
@@ -3,7 +3,7 @@
  * Tine 2.0
  * 
  * @package     Tasks
- * @license     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Cornelius Weiss <c.weiss@metaways.de>
  * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
