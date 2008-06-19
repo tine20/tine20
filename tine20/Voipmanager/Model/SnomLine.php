b99d1f8bd1f2c6cdc781c025e3c7a03e03db8a0e
Lars Kneschke <l.kneschke@metaways.de>
2008-06-19 19:59:14 +0000

    new model for SnomLine and SnomLineFilter
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@2987 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Voipmanager/Model/SnomLine.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * class to hold snom phone line data
+ * 
+ * @package     Voipmanager Management
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+
+/**
+ * class to hold snom phone line data
+ * 
+ * @package     Voipmanager Management
+ */
+class Voipmanager_Model_SnomLine extends Tinebase_Record_Abstract
+{
+    /**
+     * key in $_validators/$_properties array for the filed which 
+     * represents the identifier
+     * 
+     * @var string
+     */    
+    protected $_identifier = 'id';
+    
+    /**
+     * application the record belongs to
+     *
+     * @var string
+     */
+    protected $_application = 'Voipmanager';
+    
+    /**
+     * list of zend inputfilter
+     * 
+     * this filter get used when validating user generated content with Zend_Input_Filter
+     *
+     * @var array
+     */
+    protected $_filters = array(
+        '*'                     => 'StringTrim'
+    );
+    
+    /**
+     * list of zend validator
+     * 
+     * this validators get used when validating user generated content with Zend_Input_Filter
+     *
+     * @var array
+     */
+    protected $_validators = array(
+        'id' 			    => array(Zend_Filter_Input::ALLOW_EMPTY => true, Zend_Filter_Input::DEFAULT_VALUE => NULL),
+        'snomphone_id'      => array(Zend_Filter_Input::ALLOW_EMPTY => false, 'presence' => 'required'),
+        'asteriskline_id'   => array(Zend_Filter_Input::ALLOW_EMPTY => false, 'presence' => 'required'),
+        'linenumber'        => array(Zend_Filter_Input::ALLOW_EMPTY => false, 'presence' => 'required'),
+        'lineactive'        => array(Zend_Filter_Input::ALLOW_EMPTY => false, 'presence' => 'required'),
+        'idletext'          => array(Zend_Filter_Input::ALLOW_EMPTY => true)
+    );
+    
+    /**
+     * converts a int, string or Voipmanager_Model_SnomLine to an phone id
+     *
+     * @param int|string|Voipmanager_Model_SnomLine $_snomLineId the snomline id to convert
+     * @return int
+     */
+    static public function convertSnomLineIdToInt($_snomLineId)
+    {
+        if ($_snomLineId instanceof Voipmanager_Model_SnomLine) {
+            if (empty($_snomLineId->id)) {
+                throw new Exception('no line id set');
+            }
+            $id = (string) $_snomLineId->id;
+        } else {
+            $id = (string) $_snomLineId;
+        }
+        
+        if (empty($id)) {
+            throw new Exception('lineid id can not be \'\'');
+        }
+        
+        return $id;
+    }
+
+}
---
+++ tine20/Voipmanager/Model/SnomLineFilter.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Voipmanager
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+
+/**
+ * Phone Filter Class
+ * @package Voipmanager
+ */
+class Voipmanager_Model_SnomLineFilter extends Tinebase_Record_Abstract
+{
+	/**
+     * key in $_validators/$_properties array for the filed which 
+     * represents the identifier
+     * 
+     * @var string
+     */    
+    protected $_identifier = 'id';
+    
+    /**
+     * application the record belongs to
+     *
+     * @var string
+     */
+    protected $_application = 'Voipmanager';
+    
+    protected $_validators = array(
+        'id'                    => array('allowEmpty' => true,  'Int'   ),
+
+        'snomphone_id'          => array('allowEmpty' => true           )
+    );
+}
