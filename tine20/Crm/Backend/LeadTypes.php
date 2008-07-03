aac3928660cbe14b5761dde928e991ba0b64d324
Sebastian Lenk <sebastian-lenk>
2008-07-03 09:30:29 +0000

    - saveLeadTypes() removed, because it's no longer in use.
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@3141 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Crm/Backend/LeadTypes.php
+++ tine20/Crm/Backend/LeadTypes.php
@@ -19,18 +19,6 @@
 class Crm_Backend_LeadTypes extends Tinebase_Abstract_SqlTableBackend
 {
     /**
-    * Instance of Crm_Backend_Types
-    *
-    * @var Crm_Backend_Types
-    */
-    protected $_table;
-    
-   /**
-     * @var Zend_Db_Adapter_Abstract
-     */
-    protected $_db;
-
-    /**
      * the constructor
      */
     public function __construct ()
@@ -39,38 +27,5 @@ class Crm_Backend_LeadTypes extends Tinebase_Abstract_SqlTableBackend
         $this->_modelName = 'Crm_Model_Leadtype';
     	$this->_db = Zend_Registry::get('dbAdapter');
         $this->_table = new Tinebase_Db_Table(array('name' => $this->_tableName));
-    }
-    
-    /**
-    * add or updates an option
-    *
-    * @param Crm_Leadtype $_optionData the optiondata
-    * @return unknown
-    */
-    public function saveLeadtypes(Tinebase_Record_Recordset $_optionData)
-    {
-
-        $_daten = $_optionData->toArray();
-    
-
-        $db = Zend_Registry::get('dbAdapter');
-  
-        $db->beginTransaction();
-        
-        try {
-            $db->delete(SQL_TABLE_PREFIX . 'metacrm_leadtype');
-
-            foreach($_daten as $_data) {
-                $db->insert(SQL_TABLE_PREFIX . 'metacrm_leadtype', $_data);                
-            }
-
-            $db->commit();
-
-        } catch (Exception $e) {
-            $db->rollBack();
-            error_log($e->getMessage());
-        }
-
-        return $_optionData;
     }
 }
