fb8d86f9fb6f1d4f5a3f1b859c9424f0db2e0552
Thomas Wadewitz <t.wadewitz@metaways.de>
2008-07-03 13:43:57 +0000

    - reorganisation of Ext.ux.Form.Checkbox
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@3151 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/ux/form/ImgCheckbox.js
+++ tine20/Tinebase/js/ux/form/ImgCheckbox.js
@@ -12,26 +12,24 @@
 
 Ext.ns('Ext.ux.form');
 Ext.ux.form.ImgCheckbox = Ext.extend(Ext.form.Checkbox, {
-     offCls:'imgcheckbox-off'
-    ,onCls:'imgcheckbox-on'
-    ,submitOffValue:'0'
-    ,submitOnValue:'1'
-    ,chk: '0'
+    offCls:'imgcheckbox-off',
+    onCls:'imgcheckbox-on',
+    submitOffValue:'0',
+    submitOnValue:'1',
+    chk: '0',
     
-    ,onRender:function(ct) {
-        // call parent
+    onRender:function(ct) {
         Ext.ux.form.ImgCheckbox.superclass.onRender.apply(this, arguments);
 
-        // save tabIndex remove & re-create this.el
+        // saving the tab-index, remove and recreate this.elment
         var tabIndex = this.el.dom.tabIndex;
         var id = this.el.dom.id;
         this.el.remove();
         this.el = ct.createChild({tag:'input', type:'hidden', name:this.name, id:id});
 
-        // update value of hidden field
+        // hidden field value update
         this.updateHidden();
 
-        // adjust wrap class and create link with bg image to click on
         this.wrap.replaceClass('x-form-check-wrap', 'imgcheckbox-wrap');
         this.cbEl = this.wrap.createChild({
 								tag:'a',
@@ -39,26 +37,26 @@ Ext.ux.form.ImgCheckbox = Ext.extend(Ext.form.Checkbox, {
 								cls: ((this.chk == '1')? this.onCls : this.offCls)                                
 								});
 
-        // reposition boxLabel if any
+        // repositioning the boxLabel
         var boxLabel = this.wrap.down('label');
         if(boxLabel) {
             this.wrap.appendChild(boxLabel);
         }
 
-        // support tooltip
+        // supporting of tooltip
         if(this.tooltip) {
             this.cbEl.set({qtip:this.tooltip});
         }
 
-        // install event handlers
+        // installation of event handlers
         this.wrap.on({click:{scope:this, fn:this.onClick, delegate:'a'}});
         this.wrap.on({keyup:{scope:this, fn:this.onClick, delegate:'a'}});
 
-        // restore tabIndex
+        // restoring the tab index
         this.cbEl.dom.tabIndex = tabIndex;
-    } // eo function onRender
+    },
 
-    ,onClick:function(e) {
+    onClick:function(e) {
         if(this.disabled || this.readOnly) {
             return;
         }
@@ -70,56 +68,56 @@ Ext.ux.form.ImgCheckbox = Ext.extend(Ext.form.Checkbox, {
 				case true:  this.setValue('0'); break;                
 				}
         }
-    } // eo function onClick
+    },
 
-    ,onDisable:function() {
+    onDisable:function() {
         this.cbEl.addClass(this.disabledClass);
         this.el.dom.disabled = '1';
-    } // eo function onDisable
+    },
 
-    ,onEnable:function() {
+    onEnable:function() {
         this.cbEl.removeClass(this.disabledClass);
         this.el.dom.disabled = '0';
-    } // eo function onEnable
+    },
 
-    ,setValue:function(val) {
+    setValue:function(val) {
 
-			if('string' == typeof val) { //Match to the submit value
-            this.chk = val === this.submitOnValue;
-			} else { //Match to a boolean
-            this.chk = !(!val);
-         }
+			if('string' == typeof val) 
+            {
+                this.chk = val === this.submitOnValue;
+			} else 
+            {
+                this.chk = !(!val);
+            }
 
-			//Redraw the checkbox
 			this.redraw();
-			
-			//Notify that the state was changed
+
         this.fireEvent('check', this, this.chk);
 
-    } // eo function setValue
+    },
 	 
-	 ,redraw: function() {
+	redraw: function() {
 
         if(this.rendered && this.cbEl) {
             this.updateHidden();
             this.cbEl.removeClass([this.offCls, this.onCls]);
             this.cbEl.addClass(
-										 ((this.chk == '1')? this.onCls : this.offCls)
-									);
+    			 ((this.chk == '1')? this.onCls : this.offCls)
+			);
         }
-	 }
+	 },
 
-    ,updateHidden:function() {
+    updateHidden:function() {
         this.el.dom.value = (
-							  ((this.chk =='1')? this.submitOnValue : this.submitOffValue)
-                              );
-    } // eo function updateHidden
+	  	  ((this.chk =='1')? this.submitOnValue : this.submitOffValue)
+        );
+    },
 
-    ,getValue:function() {
+    getValue:function() {
         return this.chk;
-    } // eo function getValue
+    }
 
-}); // eo extend
+}); 
 
-// register xtype
+// xtype registration
 Ext.reg('imgcheckbox', Ext.ux.form.ImgCheckbox);
