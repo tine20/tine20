351e6c9307ae6555e21013566fcbb8b7cdbccbea
Thomas Wadewitz <t.wadewitz@metaways.de>
2008-07-04 15:07:34 +0000

    new Form Element "LockCombo" (still needs some love)
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@3169 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/js/ux/LockCombo.js
@@ -0,0 +1,143 @@
+/*
+ * Tine 2.0
+ * 
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Thomas Wadewitz <t.wadewitz@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id: LockCombo.js 2256 2008-07-04 14:51:39Z twadewitz $
+ *
+ */
+ 
+Ext.namespace('Ext.ux');
+
+/**
+ * Generic widget for a twin trigger combo field
+ */
+Ext.ux.LockCombo = Ext.extend(Ext.form.ComboBox, {
+    /**
+     * @cfg {String} paramName
+     */
+    paramName : 'query',
+    /**
+     * @cfg {Bool} selectOnFocus
+     */
+    selectOnFocus : true,
+    /**
+     * @cfg {String} emptyText
+     */
+    emptyText: 'enter searchfilter',
+    
+	hiddenFieldId: '',
+	
+	hiddenFieldData: '',
+	
+    validationEvent:false,
+    validateOnBlur:false,
+    trigger1Class:'x-form-trigger',
+    trigger2ClassLocked:'x-form-locked-trigger',
+	trigger2ClassUnlocked:'x-form-unlocked-trigger',
+    hideTrigger1:false,
+    width:180,
+    hasSearch : false,
+    /**
+     * @private
+     */
+    initComponent : function(){
+        Ext.ux.LockCombo.superclass.initComponent.call(this);
+
+        this.triggerConfig = {
+            tag:'span', cls:'x-form-twin-triggers', cn:[
+            {tag: "img", id:'trigger1', src: Ext.BLANK_IMAGE_URL, cls: "x-form-trigger " + this.trigger1Class},
+            {tag: "img", id:'trigger2', src: Ext.BLANK_IMAGE_URL, cls: "x-form-trigger " }
+        ]};  
+    },
+
+    getTrigger : function(index){
+        return this.triggers[index];
+    },
+
+    initTrigger : function(){
+
+        var ts = this.trigger.select('.x-form-trigger', true);	
+        this.wrap.setStyle('overflow', 'hidden');
+        var triggerField = this;
+        ts.each(function(t, all, index){
+            t.hide = function(){
+                var w = triggerField.wrap.getWidth();
+                this.dom.style.display = 'none';
+                triggerField.el.setWidth(w-triggerField.trigger.getWidth());
+            };
+            t.show = function(){
+                var w = triggerField.wrap.getWidth();
+                this.dom.style.display = '';
+                triggerField.el.setWidth(w-triggerField.trigger.getWidth());
+            };
+            var triggerIndex = 'Trigger'+(index+1);
+
+            if(this['hide'+triggerIndex]){
+                t.dom.style.display = 'none';
+            }
+            t.on("click", this['on'+triggerIndex+'Click'], this, {preventDefault:true});
+	
+			if(t.id == 'trigger2') {
+				if(this.hiddenFieldData == '0') {
+		            t.addClass(this.trigger2ClassLocked);		
+				}
+				if(this.hiddenFieldData == '1') {
+		            t.addClass(this.trigger2ClassUnlocked);							
+				}				
+			}
+	
+            t.addClassOnOver('x-form-trigger-over');
+            t.addClassOnClick('x-form-trigger-click');
+        }, this);
+        this.triggers = ts.elements;  
+    },
+	
+
+	onTrigger1Click: function(){
+        if(this.disabled){
+            return;
+        }
+        if(this.isExpanded()){
+            this.collapse();
+            this.el.focus();
+        }else {
+            this.onFocus({});
+            if(this.triggerAction == 'all') {
+                this.doQuery(this.allQuery, true);
+            } else {
+                this.doQuery(this.getRawValue());
+            }
+            this.el.focus();
+        }
+    },
+	
+    onTrigger2Click : function(){
+		var _currentValue = Ext.getCmp(this.hiddenFieldId).getValue();
+        var ts = this.trigger.select('.x-form-trigger', true);	
+
+
+
+		if (_currentValue == '0') {
+			
+			Ext.getCmp(this.hiddenFieldId).setValue('1');
+			ts.each(function(t, all, index){
+				if (t.id == 'trigger2') {
+					t.replaceClass("x-form-trigger " + this.trigger2ClassLocked, "x-form-trigger " + this.trigger2ClassUnlocked);
+				}
+			});
+		}
+		else {
+			Ext.getCmp(this.hiddenFieldId).setValue('0');
+			ts.each(function(t, all, index){
+				if (t.id == 'trigger2') {			
+					t.replaceClass("x-form-trigger " + this.trigger2ClassUnlocked, "x-form-trigger " + this.trigger2ClassLocked);
+				}
+			});
+		}
+
+		console.log(Ext.getCmp(this.hiddenFieldId).getValue());
+    }	
+});
+Ext.reg('lockCombo', Ext.ux.LockCombo);
