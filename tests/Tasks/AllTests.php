d3e85596f6623283102b2edccb7735c73f053668
Philipp Schüle <p.schuele@metaways.de>
2008-07-17 14:36:39 +0000

    added @todos
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@3389 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/Tasks/AllTests.php
+++ tests/Tasks/AllTests.php
@@ -7,6 +7,8 @@
  * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Cornelius Weiss <c.weiss@metaways.de>
  * @version     $Id$
+ * 
+ * @todo        add json + controller tests
  */
 
 /**
