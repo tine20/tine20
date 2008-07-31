6eb9e65604c1571a46c12f5a94dbabdf353b863e
Lars Kneschke <l.kneschke@metaways.de>
2008-07-31 06:06:44 +0000

    send the http_client values only once
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@3589 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- snom/settings.php
+++ snom/settings.php
@@ -47,10 +47,9 @@ header('Content-Type: text/xml');
 $xmlBackend = new Voipmanager_Backend_Snom_Xml();        
 echo $xmlBackend->getConfig($phone);
 
-/*
 $phone->http_client_info_sent = true;
 $phoneBackend->update($phone);
-*/
+
 
 function updateStatus($_phone, $_userAgent)
 {
