5a3c5e4ce4377f64e42d9b49d18eb60264db1819
Cornelius Weiß <c.weiss@metaways.de>
2008-08-15 10:12:26 +0000

    remote adb tests
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@3906 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/php_client/Addressbook/ServiceTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     tests
+ * @subpackage  php_client
+ * @license     yet unknown
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+/*
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Tinebase_UserTest::main');
+}
+*/
+
+class Tinebase_Addressbook_ServiceTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Tinebase_Connection
+     */
+    protected $_connection = NULL;
+    
+    /**
+     * @var Addressbook_Service
+     */
+    protected $_service = NULL;
+    
+    /**
+     * Runs the test methods of this class.
+     *
+     * @access public
+     * @static
+     */
+    public static function main()
+    {
+        $suite  = new PHPUnit_Framework_TestSuite('Tinebase_Addressbook_ServiceTest');
+        PHPUnit_TextUI_TestRunner::run($suite);
+    }
+    
+    public function setup()
+    {
+        $this->_connection = Tinebase_Connection::getInstance();
+        $this->_service = new Addressbook_Service($this->_connection);
+    }
+
+    /**
+     * tests remote adding of a contact
+     *
+     */
+    public function testAddContact()
+    {
+        
+    }
+    
+    /**
+     * test remote retrivial of a contact
+     *
+     */
+    public function testGetContact()
+    {
+        
+    }
+}
