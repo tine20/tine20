200a14cbd3be683973e69aff9a5afc47159d28b8
Cornelius Weiß <c.weiss@metaways.de>
2008-08-15 18:47:07 +0000

    update demo action
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@3928 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- php_client/backend.php
+++ php_client/backend.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * this is just the first draft
+ * this is just an usage example for the Tine 2.0 PHP Client
  *
  * @package     Remote API
- * @license     yet unknown
+ * @license     New BSD License
  * @author      Lars Kneschke <l.kneschke@metaways.de>
  * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
@@ -12,38 +12,41 @@
 var_dump($_POST);
 
 require_once 'Zend/Loader.php';
-
 Zend_Loader::registerAutoload();
 
-$client = new Tinebase_Connection($_POST['url']);
-
-Tinebase_Service_Abstract::setDefaultConnection($client);
-
-if($_POST['debug'] == 'yes') {
-    $client->setDebugEnabled(true);
-}
-
-
 echo "Try to login...<br>";
+// feteching the connection
+$connection = Tinebase_Connection::getInstance($_POST['url'], $_POST['username'], $_POST['password']);
+// setting default connection
+Tinebase_Service_Abstract::setDefaultConnection($connection);
 
-$client->login($_POST['username'], $_POST['password']);
-
+// getting the Tinebase service
+$TinbaseService = new Tinebase_Service($connection);
+if($_POST['debug'] == 'yes') {
+    $connection->setDebugEnabled(true);
+    $TinbaseService->debugEnabled = true;
+}
+// login to remote tine installation
+$TinbaseService->login();
 
 echo "<hr>Try to add contact...<br>";
 
+// NOTE: all data are expected to be UTF-8 encoded!
 $contactData = $_POST['contact'];
-$contactData['owner'] = 5;
 
-$contact = new Addressbook_Model_Contact($contactData);
-
+// getting Addressbook service
 $addressbook = new Addressbook_Service();
 
+// creating a _local_ model of the contact data
+// NOTE: with this we do client side data validation
+$contact = new Addressbook_Model_Contact($contactData);
+
+// call remote addContact
 $updatedContact = $addressbook->addContact($contact);
 
 var_dump($updatedContact->toArray());
 
 echo "<hr>Try to logout...<br>";
-
-$client->logout();
+$TinbaseService->logout();
 
 echo '<a href="index.html">Back to contact form</a>';
