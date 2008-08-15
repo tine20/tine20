cec09d1e39442f5f3cebd6c4fea932c9ee12bc92
Cornelius Weiß <c.weiss@metaways.de>
2008-08-15 12:56:50 +0000

    test infrastructure
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@3914 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/php_client/AllTests.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     tests
+ * @subpackage  php_client
+ * @license     yet unknown
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'AllTests::main');
+}
+
+/**
+ * Test helper
+ */
+require_once 'TestHelper.php';
+
+class AllTests
+{
+    public static function main()
+    {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('Tine 2.0 PHP Client');
+        $suite->addTestSuite('Tinebase_LoginTest');
+        $suite->addTestSuite('Addressbook_ServiceTest');
+        $suite->addTestSuite('Tinebase_LogoutTest');
+        //$suite->addTest(Tinebase_LoginTest::suite());
+        //$suite->addTest(Addressbook_ServiceTest::suite());
+        return $suite;
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'AllTests::main') {
+    AllTests::main();
+}
