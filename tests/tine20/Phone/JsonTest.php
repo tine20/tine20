1a8ae7621cacd18fb54b4b31b7810850b3be2ea7
Philipp Schüle <p.schuele@metaways.de>
2008-09-04 14:16:25 +0000

    implemented getUserPhones test
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@4211 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/tine20/Phone/JsonTest.php
+++ tests/tine20/Phone/JsonTest.php
@@ -48,6 +48,55 @@ class Phone_JsonTest extends PHPUnit_Framework_TestCase
      */
     protected function setUp()
     {
+        $this->_backend = new Voipmanager_Backend_Snom_Phone();
+        
+        $this->_objects['location'] = new Voipmanager_Model_SnomLocation(array(
+            'id' => 20001,
+            'name' => 'phpunit test location'
+        ));
+
+        $this->_objects['software'] = new Voipmanager_Model_SnomSoftware(array(
+            'id' => 20003
+        ));       
+        
+        $this->_objects['template'] = new Voipmanager_Model_SnomTemplate(array(
+            'id' => 20002,
+            'name' => 'phpunit test location',
+            'model' => 'snom320',
+            'software_id' => $this->_objects['software']->getId()
+        ));
+        
+        $this->_objects['phone'] = new Voipmanager_Model_SnomPhone(array(
+            'id' => 1001,
+            'macaddress' => "1234567890cd",
+            'location_id' => $this->_objects['location']->getId(),
+            'template_id' => $this->_objects['template']->getId(),
+            'current_model' => 'snom320',
+            'redirect_event' => 'none'
+        ));
+        
+        $this->_objects['phoneOwner'] = array(
+            'account_id' => Zend_Registry::get('currentAccount')->getId(),
+            'account_type' => 'user'
+        );
+        
+        $rights = new Tinebase_Record_RecordSet('Voipmanager_Model_SnomPhoneRight', array(
+            $this->_objects['phoneOwner']
+        )); 
+        
+        $this->_objects['phone']->rights = $rights;
+        
+        // create phone, location, template, rights
+        $phoneBackend                = new Voipmanager_Backend_Snom_Phone();
+        $snomLocationBackend         = new Voipmanager_Backend_Snom_Location();
+        $snomTemplateBackend         = new Voipmanager_Backend_Snom_Template();     
+        $snomSoftwareBackend         = new Voipmanager_Backend_Snom_Software(); 
+        
+        $snomSoftwareBackend->create($this->_objects['software']);
+        $snomLocationBackend->create($this->_objects['location']);
+        $snomTemplateBackend->create($this->_objects['template']);
+        $phoneBackend->create($this->_objects['phone']);
+        $phoneBackend->setPhoneRights($this->_objects['phone']);
     }
 
     /**
@@ -58,16 +107,30 @@ class Phone_JsonTest extends PHPUnit_Framework_TestCase
      */
     protected function tearDown()
     {	
+        // remove phone, location, template
+        $phoneBackend                = new Voipmanager_Backend_Snom_Phone();
+        $snomLocationBackend         = new Voipmanager_Backend_Snom_Location();
+        $snomTemplateBackend         = new Voipmanager_Backend_Snom_Template();     
+        $snomSoftwareBackend         = new Voipmanager_Backend_Snom_Software(); 
+        
+        $phoneBackend->delete($this->_objects['phone']->getId());
+        $snomLocationBackend->delete($this->_objects['location']->getId());
+        $snomTemplateBackend->delete($this->_objects['template']->getId());
+        $snomSoftwareBackend->delete($this->_objects['software']->getId());
     }
     
     /**
      * try to get user phones
      *
-     * @todo implement
      */
     public function testGetUserPhones()
-    {
+    {        
+        // get phone json
         $json = new Phone_Json();
-        
+        $phones = $json->getUserPhones(Zend_Registry::get('currentAccount')->getId());
+
+        //print_r($phones);
+        $this->assertEquals(1, count($phones['results']), 'only 1 phone expected');        
+        $this->assertEquals($this->_objects['phone']->macaddress, $phones['results'][0]['macaddress'], 'got wrong phone');
     }    
 }		
