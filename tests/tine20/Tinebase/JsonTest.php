96bcf710771299e1f974469003a06446595103ac
Philipp Schüle <p.schuele@metaways.de>
2008-09-15 10:22:27 +0000

    lowered country count requirement
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@4361 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/tine20/Tinebase/JsonTest.php
+++ tests/tine20/Tinebase/JsonTest.php
@@ -104,7 +104,7 @@ class Tinebase_JsonTest extends PHPUnit_Framework_TestCase
     public function testGetCountryList()
     {
         $list = $this->_instance->getCountryList();
-        $this->assertTrue(count($list['results']) >= 263);
+        $this->assertTrue(count($list['results']) >= 247);
     }
     
     /**
