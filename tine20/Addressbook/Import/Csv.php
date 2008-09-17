bdb0d92cd0a4a71a1a0588feb3678af6bb19b6ff
Philipp Schüle <p.schuele@metaways.de>
2008-09-17 13:15:53 +0000

    implemented read function
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@4432 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Addressbook/Import/Csv.php
+++ tine20/Addressbook/Import/Csv.php
@@ -12,6 +12,8 @@
 
 /**
  * csv import class for the addressbook
+ * 
+ * for the use of the mapping parameter => see tests/tine20/Addressbook/Import/CsvTest 
  *
  * @package     Addressbook
  * @subpackage  Import
@@ -22,6 +24,13 @@ class Addressbook_Import_Csv implements Addressbook_Import_Interface
      * @var Zend_Db_Adapter_Abstract
      */
     protected $_db;
+    
+    /**
+     * delimiter for csv file
+     *
+     * @var string
+     */
+    protected $_delimiter = ';';
     
     /**
      * the constructor
@@ -66,22 +75,74 @@ class Addressbook_Import_Csv implements Addressbook_Import_Interface
      * read data from import file
      *
      * @param string $_filename filename to import
-     * @param array $_mapping mapping csv columns to record fields
+     * @param array $_mapping mapping record fields to csv columns (destination => source) 
      * @return Tinebase_Record_RecordSet of Addressbook_Model_Contact
      */
     public function read($_filename, $_mapping)
     {
+        // read file
+        if (!file_exists($_filename)) {
+            throw new Exception("File $_filename not found.");
+        }
+        $fileArray = file($_filename);
         
+        // get headline
+        $headline = trim(array_shift($fileArray));
+        
+        $result = new Tinebase_Record_RecordSet('Addressbook_Model_Contact');
+        foreach($fileArray as $line) {
+            $record = $this->_parseLine(trim($line), $headline, $_mapping);
+            if (!empty($record)) {
+                try {
+                    $result->addRecord(new Addressbook_Model_Contact($record));
+                } catch (Exception $e) {
+                    //-- don't add incorrect record (name missing for example)
+                }
+            }
+        }
+        
+        return $result;
     }
     
     /**
      * import the data
      *
      * @param Tinebase_Record_RecordSet of Addressbook_Model_Contact
+     * @todo implement
      */
     public function import(Tinebase_Record_RecordSet $_records)
     {
         
     }
     
+    /**
+     * parse a csv line
+     *
+     * @param string $_line
+     * @param string $_headline
+     * @param array $_line
+     * @return array with values
+     */
+    protected function _parseLine($_line, $_headline, $_mapping)
+    {
+        $headline = array_flip(explode($this->_delimiter, $_headline));
+        $values = explode($this->_delimiter, $_line);
+        
+        //print_r($headline);
+        //print_r($values);
+        
+        $result = array();
+        foreach($_mapping as $destination => $source) {
+            if (is_array($source)) {
+                //-- add
+            } elseif (is_string($source) && !empty($source)) {
+                if (isset($values[$headline[$source]])) {
+                    //echo $destination .'-'.$headline[$source]. '-'.$values[$headline[$source]].' ';
+                    $result[$destination] = $values[$headline[$source]];
+                }
+            }
+        }
+        //print_r($result);
+        return $result;
+    }
 }
