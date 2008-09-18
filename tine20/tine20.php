140fbf0e44e3fb78872d24154db0df6f91d9f8dd
Philipp Schüle <p.schuele@metaways.de>
2008-09-18 14:02:51 +0000

    removed obsolete code
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@4463 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/tine20.php
+++ tine20/tine20.php
@@ -28,11 +28,6 @@ $tine20path = dirname(__FILE__);
 try {
     $opts = new Zend_Console_Getopt(
     array(
-    /*
-        'verbose|v'             => 'Output messages',
-        'format|f'              => 'File format [csv]',
-        'dry|d'                 => "Dry run - don't import data",
-    */
         'help|h'                => 'Display this help Message',
         'verbose|v'             => 'Output messages',
         'dry|d'                 => "Dry run - don't change anything",    
@@ -44,13 +39,11 @@ try {
     ));
     $opts->parse();
 } catch (Zend_Console_Getopt_Exception $e) {
-   //echo $e->__toString();
    echo $e->getUsageMessage();
    exit;
 }
 
 if (count($opts->toArray()) === 0 || $opts->h || empty($opts->method) || empty($opts->username) || empty($opts->password)) {
-    //print_r($opts->toArray());
     echo $opts->getUsageMessage();
     exit;
 }
@@ -58,76 +51,4 @@ if (count($opts->toArray()) === 0 || $opts->h || empty($opts->method) || empty($
 $tineBase = Tinebase_Controller::getInstance();
 $tineBase->handleCli($opts);
 
-/*
-if (count($opts->toArray()) === 0 || $opts->h || empty($opts->filename)) {
-    echo $opts->getUsageMessage();
-    exit;
-}
-
-if ($opts->v) {
-    echo "filename to import: " . $opts->filename ."\n";
-}
-
-// Set up basic tine 2.0 environment
-$_SERVER['DOCUMENT_ROOT'] = dirname(__FILE__);
-
-$tinebaseController = Tinebase_Controller::getInstance();
-$tinebaseController->initFramework();
-
-// set default locale
-$tinebaseController->setupUserLocale('de');
-
-if (!$tinebaseController->login('tine20admin', 'lars', '127.0.0.1')){
-    throw new Exception("Couldn't login, user session required for import! \n");
-}
-
-// get csv importer
-$importer = Addressbook_Import_Factory::factory('Csv');
-
-// set mapping
-$mapping = array(
-    'adr_one_locality'      => 'Ort',
-    'adr_one_postalcode'    => 'Plz',
-    'adr_one_street'        => 'Straße',
-    'org_name'              => 'Name1',
-    'org_unit'              => 'Name2',
-    'note'                  => array(
-        'Mitarbeiter'       => 'inLab Spezi',
-        'Anzahl Mitarbeiter' => 'ANZMitarbeiter',
-        'Bemerkung'         => 'Bemerkung',
-    ),
-    'tel_work'              => 'TelefonZentrale',
-    'tel_cell'              => 'TelefonDurchwahl',
-    'n_family'              => 'Nachname',
-    'n_given'               => 'Vorname',
-    'n_prefix'              => array('Anrede', 'Titel'),
-);
-
-// read file
-if ($opts->v) {
-    echo "reading file ...";
-}
-$records = $importer->read($opts->filename, $mapping);
-if ($opts->v) {
-    echo "done.\n";
-}
-
-// import (check if dry run)
-if (!$opts->d) {
-    if ($opts->v) {
-        echo "importing ". count($records) ." records ...";
-    }
-    $importedRecords = $importer->import($records);
-    if ($opts->v) {
-        echo "done.\n";
-    }
-    if ($opts->v) {
-        foreach ($importedRecords as $contact) {
-            echo "Imported contact: " . $contact->n_full;
-        }   
-    }
-} else {
-    print_r($records->toArray());
-}
-*/
 ?>
