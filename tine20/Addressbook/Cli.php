4ef55360992a6a14ed4f295d274dce8a665e29df
Philipp Schüle <p.schuele@metaways.de>
2008-09-18 14:07:14 +0000

    implemented import function
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@4467 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Addressbook/Cli.php
+++ tine20/Addressbook/Cli.php
@@ -31,9 +31,9 @@ class Addressbook_Cli
         'import' => array(
             'description'   => 'Import new contacts into the addressbook.',
             'params'        => array(
-                '--filename <string>'   => 'Filename of import file',
-                '--format <string>'     => 'Supported fromats (only csv at the moment',
-                '--config <string>'     => 'Mapping config file (use mapping.inc.php as default)',
+                'filenames'   => 'Filename(s) of import file(s) [required]',
+                //'format'     => 'Import file format (default: csv) [optional]',
+                //'config'     => 'Mapping config file (default: mapping.inc.php) [optional]',
             )
         )
     );
@@ -58,10 +58,70 @@ class Addressbook_Cli
      *
      * @param Zend_Console_Getopt $_opts
      * 
-     * @todo implement
+     * @todo get mapping from config file
      */
     public function import($_opts)
     {
-        echo "not implemented yet.\n";
+        $args = $_opts->getRemainingArgs();
+            
+        // get csv importer
+        $importer = Addressbook_Import_Factory::factory('Csv');
+        
+        // set mapping
+        $mapping = array(
+            'adr_one_locality'      => 'Ort',
+            'adr_one_postalcode'    => 'Plz',
+            'adr_one_street'        => 'Straße',
+            'org_name'              => 'Name1',
+            'org_unit'              => 'Name2',
+            'note'                  => array(
+                'Mitarbeiter'       => 'inLab Spezi',
+                'Anzahl Mitarbeiter' => 'ANZMitarbeiter',
+                'Bemerkung'         => 'Bemerkung',
+            ),
+            'tel_work'              => 'TelefonZentrale',
+            'tel_cell'              => 'TelefonDurchwahl',
+            'n_family'              => 'Nachname',
+            'n_given'               => 'Vorname',
+            'n_prefix'              => array('Anrede', 'Titel'),
+        );
+        
+        foreach ($args as $filename) {
+            // read file
+            if ($_opts->v) {
+                echo "reading file $filename ...";
+            }
+            try {
+                $records = $importer->read($filename, $mapping);
+                if ($_opts->v) {
+                    echo "done.\n";
+                }
+            } catch (Exception $e) {
+                if ($_opts->v) {
+                    echo "failed (". $e->getMessage() . ").\n";
+                } else {
+                    echo $e->getMessage() . "\n";
+                }
+                return FALSE;
+            }
+            
+            // import (check if dry run)
+            if (!$_opts->d) {
+                if ($_opts->v) {
+                    echo "importing ". count($records) ." records...";
+                }
+                $importedRecords = $importer->import($records);
+                if ($_opts->v) {
+                    echo "done.\n";
+                }
+                if ($_opts->v) {
+                    foreach ($importedRecords as $contact) {
+                        echo "Imported contact: " . $contact->n_full;
+                    }   
+                }
+            } else {
+                print_r($records->toArray());
+            }        
+        }
     }
 }
