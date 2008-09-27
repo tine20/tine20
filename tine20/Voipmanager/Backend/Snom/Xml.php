b15f50bab915cc2f99af46f0b1bf3f3488c04cfa
Lars Kneschke <l.kneschke@metaways.de>
2008-09-27 09:05:53 +0000

    made mehtod the first parameter of get request
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@4641 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Voipmanager/Backend/Snom/Xml.php
+++ tine20/Voipmanager/Backend/Snom/Xml.php
@@ -118,23 +118,23 @@ class Voipmanager_Backend_Snom_Xml
         $child = $xmlPhoneSettings->addChild('user_dp_str1');
         $child->addAttribute('perm', 'RW');
         // add directory button
-        $child = $xmlPhoneSettings->addChild('dkey_directory', 'url ' . $baseURL . '?mac=$mac&amp;method=Phone.directory');
+        $child = $xmlPhoneSettings->addChild('dkey_directory', 'url ' . $baseURL . '?method=Phone.directory&amp;mac=$mac');
         $child->addAttribute('perm', 'RO');
         // add redirect on/off action url
-        $child = $xmlPhoneSettings->addChild('action_redirection_on_url', $baseURL . '?mac=$mac&amp;method=Voipmanager.redirect&amp;event=$redirect_event&amp;number=$redirect_number&amp;time=$redirect_time');
+        $child = $xmlPhoneSettings->addChild('action_redirection_on_url', $baseURL . '?method=Voipmanager.redirect&amp;mac=$mac&amp;event=$redirect_event&amp;number=$redirect_number&amp;time=$redirect_time');
         $child->addAttribute('perm', 'RO');
-        $child = $xmlPhoneSettings->addChild('action_redirection_off_url', $baseURL . '?mac=$mac&amp;method=Voipmanager.redirect&amp;event=$redirect_event&amp;number=$redirect_number&amp;time=$redirect_time');
+        $child = $xmlPhoneSettings->addChild('action_redirection_off_url', $baseURL . '?method=Voipmanager.redirect&amp;mac=$mac&amp;event=$redirect_event&amp;number=$redirect_number&amp;time=$redirect_time');
         $child->addAttribute('perm', 'RO');
         // callhistory logging
-        $child = $xmlPhoneSettings->addChild('action_incoming_url', $baseURL . '?mac=$mac&amp;method=Phone.callHistory&amp;event=incoming&amp;callId=$call-id&amp;local=$local&amp;remote=$remote');
+        $child = $xmlPhoneSettings->addChild('action_incoming_url', $baseURL . '?method=Phone.callHistory&amp;mac=$mac&amp;event=incoming&amp;callId=$call-id&amp;local=$local&amp;remote=$remote');
         $child->addAttribute('perm', 'RO');
-        $child = $xmlPhoneSettings->addChild('action_outgoing_url', $baseURL . '?mac=$mac&amp;method=Phone.callHistory&amp;event=outgoing&amp;callId=$call-id&amp;local=$local&amp;remote=$remote');
+        $child = $xmlPhoneSettings->addChild('action_outgoing_url', $baseURL . '?method=Phone.callHistory&amp;mac=$mac&amp;event=outgoing&amp;callId=$call-id&amp;local=$local&amp;remote=$remote');
         $child->addAttribute('perm', 'RO');
-        $child = $xmlPhoneSettings->addChild('action_connected_url', $baseURL . '?mac=$mac&amp;method=Phone.callHistory&amp;event=connected&amp;callId=$call-id&amp;local=$local&amp;remote=$remote');
+        $child = $xmlPhoneSettings->addChild('action_connected_url', $baseURL . '?method=Phone.callHistory&amp;mac=$mac&amp;event=connected&amp;callId=$call-id&amp;local=$local&amp;remote=$remote');
         $child->addAttribute('perm', 'RO');
-        $child = $xmlPhoneSettings->addChild('action_disconnected_url', $baseURL . '?mac=$mac&amp;method=Phone.callHistory&amp;event=disconnected&amp;callId=$call-id&amp;local=$local&amp;remote=$remote');
+        $child = $xmlPhoneSettings->addChild('action_disconnected_url', $baseURL . '?method=Phone.callHistory&amp;mac=$mac&amp;event=disconnected&amp;callId=$call-id&amp;local=$local&amp;remote=$remote');
         $child->addAttribute('perm', 'RO');
-        $child = $xmlPhoneSettings->addChild('action_missed_url', $baseURL . '?mac=$mac&amp;method=Phone.callHistory&amp;event=missed&amp;callId=$call-id&amp;local=$local&amp;remote=$remote');
+        $child = $xmlPhoneSettings->addChild('action_missed_url', $baseURL . '?method=Phone.callHistory&amp;mac=$mac&amp;event=missed&amp;callId=$call-id&amp;local=$local&amp;remote=$remote');
         $child->addAttribute('perm', 'RO');
         // disable redundant keys
         $child = $xmlPhoneSettings->addChild('redundant_fkeys', 'off');
