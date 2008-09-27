31197b388be8139b3c09c94c8273531b72877823
Lars Kneschke <l.kneschke@metaways.de>
2008-09-27 21:16:44 +0000

    new controller test for voipmanager
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@4661 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/tine20/Voipmanager/ControllerTest.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Voipmanager
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Voipmanager_ControllerTest::main');
+}
+
+/**
+ * Test class for Tinebase_Group
+ */
+class Voipmanager_ControllerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Fixtures
+     * 
+     * @var array test objects
+     */
+    protected $_objects = array();
+
+    /**
+     * Backend
+     *
+     * @var Voipmanager_Controller
+     */
+    protected $_backend;
+    
+    /**
+     * Runs the test methods of this class.
+     *
+     * @access public
+     * @static
+     */
+    public static function main()
+    {
+		$suite  = new PHPUnit_Framework_TestSuite('Tine 2.0 Voipmanager Controller Tests');
+        PHPUnit_TextUI_TestRunner::run($suite);
+	}
+
+    /**
+     * Sets up the fixture.
+     * This method is called before a test is executed.
+     *
+     * @access protected
+     */
+    protected function setUp()
+    {
+        $this->_backend = Voipmanager_Controller::getInstance();    
+
+        #$this->_objects['call'] = new Phone_Model_Call(array(
+        #    'id'                    => 'phpunitcallid',
+        #    'line_id'               => 'phpunitlineid',
+        #    'phone_id'              => 'phpunitphoneid',
+        #    'direction'             => Phone_Model_Call::TYPE_INCOMING,
+        #    'source'                => '26',
+        #    'destination'           => '0406437435',    
+        #));
+    }
+
+    /**
+     * Tears down the fixture
+     * This method is called after a test is executed.
+     *
+     * @access protected
+     */
+    protected function tearDown()
+    {	
+    }
+
+    /**
+     * test getDBInstance
+     * Zend_Db_Adapter_Abstract
+     */
+    public function testGetDBInstance()
+    {
+        $db = $this->_backend->getDBInstance();
+        
+        $this->assertType('Zend_Db_Adapter_Abstract', $db);
+    }
+}		
