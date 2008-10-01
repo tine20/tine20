7366d08b2e5bd8972039437ba74e523d3b8e6a74
Cornelius Weiß <c.weiss@metaways.de>
2008-10-01 11:42:46 +0000

    fix timezone handing in datepicker
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@4761 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/ExtFixes.js
+++ tine20/Tinebase/js/ExtFixes.js
@@ -9,6 +9,47 @@
  */
 
 /**
+ * fix timezone handling for date picker
+ * 
+ * The getValue function always returns 00:00:00 as time. So if a form got filled
+ * with a date like 2008-10-01T21:00:00 the form returns 2008-10-01T00:00:00 although 
+ * the user did not change the fieled.
+ * 
+ * In a multi timezone context this is fatal! When a user in a certain timezone set
+ * a date (just a date and no time information), this means in his timezone the 
+ * time range from 2008-10-01T00:00:00 to 2008-10-01T23:59:59. 
+ * _BUT_ for an other user sitting in a different timezone it means e.g. the 
+ * time range from 2008-10-01T02:00:00 to 2008-10-02T21:59:59.
+ * 
+ * So on the one hand we need to make shure, that the date picker only returns 
+ * changed datetime information when the user did a change. 
+ * 
+ * @todo On the other hand we
+ * need adjust the day +/- one day according to the timeshift. 
+ */
+/**
+ * @private
+ */
+ Ext.form.DateField.prototype.setValue = function(date){
+    // preserv original datetime information
+    this.fullDateTime = date;
+    Ext.form.DateField.superclass.setValue.call(this, this.formatDate(this.parseDate(date)));
+};
+/**
+ * @private
+ */
+Ext.form.DateField.prototype.getValue = function(){
+    //var value = this.parseDate(Ext.form.DateField.superclass.getValue.call(this));
+    
+    // return the value that was set (has time information when unchanged in client) 
+    // and not just the date part!
+    value =  this.fullDateTime;
+    return value || "";
+};
+
+
+
+/**
  * utility class used by Button
  * 
  * Fix: http://yui-ext.com/forum/showthread.php?p=142049
