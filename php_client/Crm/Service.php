63589abba0e3bee3b754a96ea5ebece90f768fd7
Cornelius Weiß <c.weiss@metaways.de>
2008-10-07 19:01:55 +0000

    methods for simple remote creation/retirival/deletion of a lead
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@4880 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- php_client/Crm/Service.php
+++ php_client/Crm/Service.php
@@ -45,4 +45,53 @@ class Crm_Service extends Tinebase_Service_Abstract
         $lead = new Crm_Model_Lead($responseData);
         return $lead;
     }
+    
+    /**
+     * retreaves a remote lead identified by its id
+     *
+     * @param  int $_leadId
+     * @return Crm_Model_Lead
+     */
+    public function getLead($_leadId)
+    {
+        $response = $this->_connection->request(array(
+            'method'    => 'Crm.getLead',
+            'leadId' => $_leadId
+        ));
+
+        if(!$response->isSuccessful()) {
+            throw new Exception('getting lead failed');
+        }
+                
+        $responseData = Zend_Json::decode($response->getBody());
+        if($this->debugEnabled === true) {
+            var_dump($responseData);
+        }
+        
+        $lead = new Crm_Model_Lead($responseData);
+        return $lead;
+    }
+    
+    /**
+     * deletes a remote lead identified by its id
+     *
+     * @param  int $_id
+     * @return void
+     */
+    public function deleteLead($_id)
+    {
+        $response = $this->_connection->request(array(
+            'method'   => 'Crm.deleteLeads',
+            '_leadIds'  => Zend_Json::encode(array($_id))
+        ));
+
+        if(!$response->isSuccessful()) {
+            throw new Exception('deleting lead failed');
+        }
+                
+        $responseData = Zend_Json::decode($response->getBody());
+        if($this->debugEnabled === true) {
+            var_dump($responseData);
+        }
+    }
 }
