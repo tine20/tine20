a2e93bea1fa96b71b0d9124df90ad8900a3a8925
Philipp Schüle <p.schuele@metaways.de>
2008-10-28 12:36:29 +0000

    added top level admin exception class :)
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5140 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Admin/Exception.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Admin
+ * @subpackage  Exception
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @version     $Id$
+ *
+ * @todo        add methods/attributes? application name for example?
+ */
+
+/**
+ * Admin exception
+ * 
+ * @package     Admin
+ * @subpackage  Exception
+ */
+class Admin_Exception extends Exception
+{
+}
