6764b7a8f270140d14b4a7faa4d226ab09da8a59
Philipp Schüle <p.schuele@metaways.de>
2008-11-05 09:42:21 +0000

    added frontend classes for erp application
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5239 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Erp/Frontend/Http.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Erp
+ * @subpackage  Frontend
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ */
+
+/**
+ * This class handles all Http requests for the Erp application
+ *
+ * @package     Erp
+ * @subpackage  Frontend
+ */
+class Erp_Frontend_Http extends Tinebase_Application_Frontend_Http_Abstract
+{
+    protected $_appname = 'Erp';
+}
