671ed00aaf213d651881a78080c9f90c7c1a9f30
Florian Blasel <f.blasel@metaways.de>
2008-11-11 13:36:42 +0000

    magic_quotes_gpc Hack (disabled)
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5374 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/index.php
+++ tine20/index.php
@@ -6,9 +6,42 @@
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Lars Kneschke <l.kneschke@metaways.de>
  * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
- * @version     $Id$
+ * @version     $Id: index.php 5122 2008-10-27 11:53:24Z p.schuele@metaways.de 
  * 
  */
+
+
+/**
+ * magic_quotes_gpc Hack!!!
+ * @autor Florian Blasel
+ * 
+ * If you are on a shared host you may not able to change the php setting for magic_quotes_gpc
+ * this hack will solve this BUT this takes performance (speed)!
+ */
+/*
+if (ini_get('magic_quotes_gpc')) {
+    exit('magic quotes');
+
+    function __magic_quotes_gpc(  $requests ) {
+        $a = array();
+        foreach(  $requests AS $k=>&$v ) {
+            if (  is_array(  $v ) ) {
+                $requests[stripslashes($k)] = __magic_quotes_gpc(  $v );
+            } else {
+        $requests[stripslashes($k)] = stripslashes(  $v );
+            }
+        }
+        return $requests;
+    } 
+  // Change the incomming data if needed
+    $_GET = __magic_quotes_gpc( $_GET );
+  $_POST = __magic_quotes_gpc( $_POST );
+  $_COOKIE = __magic_quotes_gpc( $_COOKIE );
+    $_ENV = __magic_quotes_gpc(  $_ENV );
+  $_REQUEST = __magic_quotes_gpc( $_REQUEST );
+} // end magic_quotes_gpc Hack
+ // end magic_quotes_gpc Hack
+*/
 
 $time_start = microtime(true);
 
