9f6ef2d1f96138894c1fad04f355d68f43dfd33b
Cornelius Weiß <c.weiss@metaways.de>
2008-11-18 16:28:11 +0000

    remove comma
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5522 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/widgets/AccountpickerPanel.js
+++ tine20/Tinebase/js/widgets/AccountpickerPanel.js
@@ -154,7 +154,7 @@ Tine.widgets.AccountpickerDialog = Ext.extend(Ext.Component, {
             plain: true,
             bodyStyle: 'padding:5px;',
             buttonAlign: 'right',
-			buttons: [cancle_button, ok_button],
+			buttons: [cancle_button, ok_button]
         });
 		
 		this.accountPicker = new Tine.widgets.account.PickerPanel({
