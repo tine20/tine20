5c59a724d5829bc241a95075b244304649242f55
Philipp Schüle <p.schuele@metaways.de>
2008-11-20 17:03:13 +0000

    extend new abstract filter record
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5548 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Addressbook/Model/ContactFilter.php
+++ tine20/Addressbook/Model/ContactFilter.php
@@ -8,233 +8,57 @@
  * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
  *
- * @todo        generalise that?
  */
 
 /**
  * Addressbook Filter Class
  * @package Addressbook
  */
-class Addressbook_Model_ContactFilter extends Tinebase_Record_Abstract
+class Addressbook_Model_ContactFilter extends Tinebase_Record_AbstractFilter
 {
-    
-    
-    /**
-     * key in $_validators/$_properties array for the filed which 
-     * represents the identifier
-     * 
-     * @var string
-     */    
-    protected $_identifier = 'id';
-    
     /**
      * application the record belongs to
      *
      * @var string
      */
     protected $_application = 'Addressbook';
-    
-    protected $_validators = array(
-        'id'                   => array('allowEmpty' => true,  'Int'   ),
-        
-        'containerType'        => array('allowEmpty' => true           ),
-        'owner'                => array('allowEmpty' => true           ),
-        'container'            => array('allowEmpty' => true           ),
-
-        'query'                => array('allowEmpty' => true           ),
-        'n_given'              => array('allowEmpty' => true           ),
-        'n_family'             => array('allowEmpty' => true           ),
-        'org_name'             => array('allowEmpty' => true           ),
-        'title'                => array('allowEmpty' => true           ),
-        'adr_one_street'       => array('allowEmpty' => true           ),
-        'adr_one_postalcode'   => array('allowEmpty' => true           ),
-        'adr_one_locality'     => array('allowEmpty' => true           ),
-        'adr_two_street'       => array('allowEmpty' => true           ),
-        'adr_two_postalcode'   => array('allowEmpty' => true           ),
-        'adr_two_locality'     => array('allowEmpty' => true           ),
-        'role'                 => array('allowEmpty' => true           ),
-        'tag'                  => array('allowEmpty' => true           ),
-        
-    );
-    
-    /**
-     * @var array hold selected operators
-     */
-    protected $_operators = array();
-    
-    /**
-     * @var array holds additional options
-     */
-    protected $_options = array();
-    
-    /**
-     * @var array maps abstract operators to sql operators
-     */
-    protected $_opSqlMap = array(
-        'contains' => 'LIKE',
-        'equals'   => 'LIKE',
-        'greater'  => '>',
-        'less'     => '<',
-        'not'      => 'NOT LIKE'
-    );
-    
-    /**
-     * 
-     */
-    //public function __construct($_data = NULL, $_bypassFilters = false, $_convertDates = true)
-    //{
-        
-    //}
-    
-    /**
-     * sets the record related properties from user generated input.
-     * 
-     * Input-filtering and validation by Zend_Filter_Input can enabled and disabled
-     *
-     * @param array $_data the new data to set
-     * @throws Tinebase_Exception_Record_Validation when content contains invalid or missing data
-     */
-    public function setFromArray(array $_data)
-    {
-        //Zend_Registry::get('logger')->debug(__METHOD__ . '::' . __LINE__ . " setting filters from array with data: " .
-        //    print_r($_data, true));
-        
-        $data = array();
-        foreach ($_data as $filter) {
-            $field = (isset($filter['field']) && isset($filter['value'])) ? $filter['field'] : '';
-            if (array_key_exists($field, $this->_validators)) {
-                $data[$field] = $filter['value'];
-                $this->_operators[$field] = $filter['operator'];
-                $this->_options[$field] = array_diff_key($filter, array(
-                    'field'    => NULL, 
-                    'operator' => NULL,
-                    'value'    => NULL
-                ));
-            }
-        }
-        
-        if ($this->bypassFilters !== true) {
-            // $this->validateOperators();
-            // $this->validateOptions();
-        }
-        parent::setFromArray($data);
-    }
-    
-    /**
-     * appends current filters to a given select object
-     * 
-     * @param  Zend_Db_Select
-     * @return void
-     */
-    public function appendFilterSql($_select)
-    {
-        $db = Zend_Registry::get('dbAdapter');
-        
-        foreach ($this->_properties as $field => $value)
-        {
-            //Zend_Registry::get('logger')->debug(__METHOD__ . '::' . __LINE__ . " append sql for filter '$field' width value '$value'");
-            $value = str_replace(array('*', '_'), array('%', '\_'), $value);
-            
-            switch ($field) {
-                case 'containerType':
-                case 'container':
-                case 'owner':
-                    // skip container here handling for the moment
-                    break;
-                case 'query':
-                    $queries = explode(' ', $value);
-                    foreach ($queries as $query) {
-                        $_select->where($db->quoteInto('(' . 
-                            $db->quoteIdentifier('n_family') . ' LIKE ? OR ' . 
-                            $db->quoteIdentifier('n_given') . ' LIKE ? OR ' . 
-                            $db->quoteIdentifier('org_name')  . 'LIKE ? OR ' . 
-                            $db->quoteIdentifier('email') . 'LIKE ? OR ' . 
-                            $db->quoteIdentifier('adr_one_locality') . ' LIKE ?)', '%' . trim($query) . '%'));
-                    
-                    }
-                    break;
-                case 'tag':
-                    if (strlen($value) == 40) {
-                        Tinebase_Tags::appendSqlFilter($_select, $value);
-                    }
-                    break;
-                default:
-                    $op = $this->_operators[$field];
-                    $value = $op == 'contains' ? '%' . trim($value) . '%' : trim($value);
-                    $where = array(
-                        $db->quoteIdentifier($field),
-                        $this->_opSqlMap[$op],
-                        $db->quote($value)
-                    );
-                    $_select->where(implode(' ', $where));
-                    break;
-            }
-        }
-    }
 
     /**
-     * gets record related properties
+     * the constructor
+     * it is needed because we have more validation fields in Tasks
      * 
-     * @param string name of property
-     * @return mixed value of property
+     * @param mixed $_data
+     * @param bool $bypassFilters sets {@see this->bypassFilters}
+     * @param bool $convertDates sets {@see $this->convertDates}
      */
-    public function __get($_name)
+    public function __construct($_data = NULL, $_bypassFilters = false, $_convertDates = true)
     {
-        switch ($_name) {
-            case 'container':
-                $this->_resolveContainer();
-                break;
-            default:
-        }
-        return parent::__get($_name);
+        // add more filters
+        $this->_validators = array_merge($this->_validators, array(
+            'n_given'              => array('allowEmpty' => true           ),
+            'n_family'             => array('allowEmpty' => true           ),
+            'org_name'             => array('allowEmpty' => true           ),
+            'title'                => array('allowEmpty' => true           ),
+            'adr_one_street'       => array('allowEmpty' => true           ),
+            'adr_one_postalcode'   => array('allowEmpty' => true           ),
+            'adr_one_locality'     => array('allowEmpty' => true           ),
+            'adr_two_street'       => array('allowEmpty' => true           ),
+            'adr_two_postalcode'   => array('allowEmpty' => true           ),
+            'adr_two_locality'     => array('allowEmpty' => true           ),
+            'role'                 => array('allowEmpty' => true           ),
+            'tag'                  => array('allowEmpty' => true           ),
+        ));
+        
+        // define query fields
+        $this->_queryFields = array(
+            'n_family',
+            'n_given',
+            'org_name',
+            'email',
+            'adr_one_locality',
+        );
+        
+        parent::__construct($_data, $_bypassFilters, $_convertDates);
     }
-    
-    /**
-     * Resolves containers from selected nodes
-     * 
-     * @throws Addressbook_Exception_UnexpectedValue
-     * @return void
-     */
-    protected function _resolveContainer()
-    {
-        if (isset($this->_properties['container']) && is_array($this->_properties['container'])) {
-            return;
-        }
-        if (!$this->containerType) {
-            throw new Addressbook_Exception_UnexpectedValue('You need to set a containerType.');
-        }
-        if ($this->containerType == 'Personal' && !$this->owner) {
-            throw new Addressbook_Exception_UnexpectedValue('You need to set an owner when containerType is "Personal".');
-        }
-        
-        $cc = Tinebase_Container::getInstance();
-        switch($this->containerType) {
-            case 'all':
-                $containers = $cc->getContainerByACL(Zend_Registry::get('currentAccount'), $this->_application, Tinebase_Model_Container::GRANT_READ);
-                break;
-            case 'personal':
-                $containers = Zend_Registry::get('currentAccount')->getPersonalContainer($this->_application, $this->owner, Tinebase_Model_Container::GRANT_READ);
-                break;
-            case 'shared':
-                $containers = Zend_Registry::get('currentAccount')->getSharedContainer($this->_application, Tinebase_Model_Container::GRANT_READ);
-                break;
-            case 'otherUsers':
-                $containers = Zend_Registry::get('currentAccount')->getOtherUsersContainer($this->_application, Tinebase_Model_Container::GRANT_READ);
-                break;
-            case 'internal':
-                $containers = array(Tinebase_Container::getInstance()->getInternalContainer(Zend_Registry::get('currentAccount'), 'Addressbook'));
-                break;    
-            case 'singleContainer':
-                $this->_properties['container'] = array($this->_properties['container']);
-                return;
-            default:
-                throw new Addressbook_Exception_UnexpectedValue('ContainerType not supported.');
-        }
-        $container = array();
-        foreach ($containers as $singleContainer) {
-            $container[] = $singleContainer->getId();
-        }
-        
-        $this->_properties['container'] = $container;
-    }    
+
 }
