c4bd4c6faf1bf75fdad6731f5dbdb40bee6fe144
Cornelius Weiß <c.weiss@metaways.de>
2008-11-21 17:25:20 +0000

    first simple test for adb ldap backend
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5579 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/tine20/Addressbook/Backend/LdapTest.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Addressbook
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Addressbook_Backend_LdapTest::main');
+}
+
+/**
+ * Test class for Tinebase_User
+ */
+class Addressbook_Backend_LdapTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var array test objects
+     */
+    protected $objects = array();
+
+    /**
+     * Backend
+     *
+     * @var Addressbook_Backend_Ldap
+     */
+    protected $_backend;
+    
+    /**
+     * Runs the test methods of this class.
+     *
+     * @access public
+     * @static
+     */
+    public static function main()
+    {
+        $suite  = new PHPUnit_Framework_TestSuite('Tine 2.0 Addressbook LDAP Backend Tests');
+        PHPUnit_TextUI_TestRunner::run($suite);
+    }
+
+    /**
+     * Sets up the fixture.
+     * This method is called before a test is executed.
+     *
+     * @access protected
+     */
+    protected function setUp()
+    {
+        $config = Tinebase_Core::getConfig()->get('accounts')->get('ldap')->toArray();
+        $this->_backend = new Addressbook_Backend_Ldap($config);
+    }
+
+    /**
+     * Tears down the fixture
+     * This method is called after a test is executed.
+     *
+     * @access protected
+     */
+    protected function tearDown()
+    {
+    
+    }
+    
+    public function testGetAll()
+    {
+        $this->_backend->getAll();
+    }
+    
+}       
+    
+
+if (PHPUnit_MAIN_METHOD == 'Addressbook_Backend_LdapTest::main') {
+    Addressbook_Backend_LdapTest::main();
+}
