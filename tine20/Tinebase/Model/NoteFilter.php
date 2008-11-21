19a4a188bc8afcea583ac2908a20cfe9cbed1e51
Philipp Schüle <p.schuele@metaways.de>
2008-11-21 12:01:36 +0000

    extend abstract filter record in note filter model
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5553 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Model/NoteFilter.php
+++ tine20/Tinebase/Model/NoteFilter.php
@@ -16,91 +16,43 @@
  * @package     Tinebase
  * @subpackage  Notes 
  */
-class Tinebase_Model_NoteFilter extends Tinebase_Record_Abstract
-{
-    
-    
-    /**
-     * key in $_validators/$_properties array for the filed which 
-     * represents the identifier
-     * 
-     * @var string
-     */    
-    protected $_identifier = 'id';
-    
+class Tinebase_Model_NoteFilter extends Tinebase_Record_AbstractFilter
+{    
     /**
      * application the record belongs to
      *
      * @var string
      */
     protected $_application = 'Tinebase';
-    
-    protected $_validators = array(
-        'id'                     => array('allowEmpty' => true,  'Int'   ),
-        
-        'note_type_id'           => array('allowEmpty' => true),
-    
-        'query'                  => array('allowEmpty' => true),
-            
-        'record_id'              => array('allowEmpty' => true),
-        'record_model'           => array('allowEmpty' => true),
-        'record_backend'         => array('allowEmpty' => true),
-    
-        'created_by'             => array('allowEmpty' => true),
-        'creation_time'          => array('allowEmpty' => true),
-    );
-    
+
     /**
-     * @var array hold selected operators
-     */
-    protected $_operators = array();
-    
-    /**
-     * @var array holds additional options
-     */
-    protected $_options = array();
-    
-    /**
-     * @var array maps abstract operators to sql operators
-     */
-    protected $_opSqlMap = array(
-        'contains' => 'LIKE',
-        'equals'   => 'LIKE',
-        'greater'  => '>',
-        'less'     => '<',
-        'not'      => 'NOT LIKE'
-    );
-        
-    /**
-     * sets the record related properties from user generated input.
+     * the constructor
+     * it is needed because we have more validation fields in Tasks
      * 
-     * Input-filtering and validation by Zend_Filter_Input can enabled and disabled
-     *
-     * @param array $_data the new data to set
-     * @throws Tinebase_Exception_Record_Validation when content contains invalid or missing data
+     * @param mixed $_data
+     * @param bool $bypassFilters sets {@see this->bypassFilters}
+     * @param bool $convertDates sets {@see $this->convertDates}
      */
-    public function setFromArray(array $_data)
+    public function __construct($_data = NULL, $_bypassFilters = false, $_convertDates = true)
     {
-        $data = array();
-        foreach ($_data as $filter) {
-            $field = (isset($filter['field']) && isset($filter['value'])) ? $filter['field'] : '';
-            if (array_key_exists($field, $this->_validators)) {
-                $data[$field] = $filter['value'];
-                $this->_operators[$field] = $filter['operator'];
-                $this->_options[$field] = array_diff_key($filter, array(
-                    'field'    => NULL, 
-                    'operator' => NULL,
-                    'value'    => NULL
-                ));
-            }
-        }
+        $this->_validators = array_merge($this->_validators, array(
+            'creation_time'          => array('allowEmpty' => true),
+        // not used yet
+            'record_id'              => array('allowEmpty' => true),
+            'record_model'           => array('allowEmpty' => true),
+            'record_backend'         => array('allowEmpty' => true),        
+            'note_type_id'           => array('allowEmpty' => true),
+        // 'special' defines a filter rule that doesn't fit into the normal operator/opSqlMap model 
+            'created_by'             => array('allowEmpty' => true, 'special' => TRUE),
+        ));
         
-        if ($this->bypassFilters !== true) {
-            // $this->validateOperators();
-            // $this->validateOptions();
-        }
-        parent::setFromArray($data);
-    }
+        // define query fields
+        $this->_queryFields = array(
+            'note',
+        );
+        
+        parent::__construct($_data, $_bypassFilters, $_convertDates);
+    }    
     
     /**
      * appends current filters to a given select object
@@ -112,34 +64,6 @@ class Tinebase_Model_NoteFilter extends Tinebase_Record_Abstract
      */
     public function appendFilterSql($_select)
     {
-        $db = Zend_Registry::get('dbAdapter');
-        
-        //Zend_Registry::get('logger')->debug(__METHOD__ . '::' . __LINE__ . ' ' . print_r($this->_properties, true));
-        
-        foreach ($this->_properties as $field => $value)
-        {
-            //Zend_Registry::get('logger')->debug(__METHOD__ . '::' . __LINE__ . ' ' . $field . ' - ' . $value);
-            
-            $value = str_replace(array('*', '_'), array('%', '\_'), $value);
-            $op = $this->_operators[$field];
-            
-            switch ($field) {
-                case 'created_by':
-                    //--
-                    break;
-                case 'query':
-                    $_select->where($db->quoteInto('(' . $db->quoteIdentifier('note') . ' LIKE ?)', '%' . trim($value) . '%'));
-                    break;
-                default:
-                    $value = $op == 'contains' ? '%' . trim($value) . '%' : trim($value);
-                    $where = array(
-                        $db->quoteIdentifier($field),
-                        $this->_opSqlMap[$op],
-                        $db->quote($value)
-                    );
-                    $_select->where(implode(' ', $where));
-                    break;
-            }
-        }
+        parent::appendFilterSql($_select);
     }
 }
