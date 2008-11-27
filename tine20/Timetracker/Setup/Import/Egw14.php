3063a83967f2eded0349c375b6ee5f6fadf89492
Philipp Schüle <p.schuele@metaways.de>
2008-11-27 15:39:36 +0000

    almost done with project migration script
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5653 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Timetracker/Setup/Import/Egw14.php
+++ tine20/Timetracker/Setup/Import/Egw14.php
@@ -9,11 +9,13 @@
  * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$ 
  *
- * @todo        finish import script
+ * @todo        get timesheet custom fields
+ * @todo        create additional timeaccounts for special (egw-)timesheet categories
  * @todo        parse description fields (striptags?)
- * @todo        import timesheets
- * @todo        import subprojects
- * @todo        add relations (between contract and timeaccount)
+ * @todo        import all relevant record fields
+ * @todo        add db transaction?
+ * @todo        remove limit() in sql stmts
+ * @todo        finish import script
  */
 
 /**
@@ -37,6 +39,13 @@ class Timetracker_Setup_Import_Egw14
     protected $_oldTablePrefix = "egw_";
                         
     /**
+     * counter for the imported objects
+     *
+     * @var array
+     */
+    protected $_counters = array();
+    
+    /**
      * the constructor 
      *
      * @param   string $_importAccountName [OPTIONAL]
@@ -50,6 +59,12 @@ class Timetracker_Setup_Import_Egw14
         Tinebase_Core::set(Tinebase_Core::USER, $account);
         
         $this->_db = Tinebase_Core::getDb();
+        
+        $this->_counters = array (
+            'contracts' => 0,
+            'timeaccounts' => 0,
+            'timesheets' => 0,
+        );
     }
     
     /**
@@ -59,8 +74,16 @@ class Timetracker_Setup_Import_Egw14
     public function import()
     {
         echo "Importing projects data from egroupware ...\n";
-        $this->importProjects();        
+        
+        //-- get+save timesheet custom fields
+        
+        $this->importProjects();
+                
         echo "done with import.\n";
+        
+        foreach ($this->_counters as $key => $value) {
+            echo "Imported $value $key \n";
+        }
     }
     
     /**
@@ -94,7 +117,8 @@ class Timetracker_Setup_Import_Egw14
             )
             ->where('links.link_id2 IS NULL')
             //->limit(10)
-            //-- order by number?
+            ->limit(5)
+            //-- order by project number?
             ;
             
         //echo $select->__toString();
@@ -116,36 +140,51 @@ class Timetracker_Setup_Import_Egw14
      */
     protected function _importProject($_data, $_parentId = 0)
     {
-        // check for subprojects
+        // get subprojects
         $select = $this->_db->select()
-            ->from($this->_oldTablePrefix . 'links')
+            ->from(array('projects' => $this->_oldTablePrefix . 'pm_projects'))
+            ->joinLeft(
+                array('links'  => $this->_oldTablePrefix . 'links'), 
+                "(link_app2='projectmanager' AND link_app1='projectmanager' AND link_id2=projects.pm_id)", 
+                array()
+            )
             ->where($this->_db->quoteInto('link_id1 = ?', $_data['pm_id']))
-            ->where("link_app1 = 'projectmanager'")
-            ->where("link_app2 = 'projectmanager'");
+            ->limit(5)
+        ;
+
         $stmt = $this->_db->query($select);
         $queryResult = $stmt->fetchAll();
-        
+
         if (count($queryResult) > 0) {
-            //-- import subprojects
-            /*
+            // import subprojects
             foreach ($queryResult as $row) {
-                $this->_importProject($row);
+                $this->_importProject($row, $_data['pm_id']);
             }
-            */
+        
         } else {
             // no subprojects
+            if ($_parentId == 0) {
+                echo "Importing mainproject: " . $_data['pm_title'] . ' ' .$_data['pm_id'] . "\n";
+            } else {
+                echo "- Importing subproject: " . $_data['pm_title'] . ' ' .$_data['pm_id'] . "\n";
+            }
             
-            echo "Importing project without subprojects: " . $_data['pm_title'] . ' ' .$_data['pm_id'] . "\n";
-            
-            // create timeaccount
-            $timeaccount = $this->_createTimeaccount($_data);
-
             // create contract
             $contract = $this->_createContract($_data);        
             
-            //-- link contract to timeaccount
-            
-            //-- add timesheets
+            // create timeaccount
+            // @todo put timeaccounts for this project in an array
+            $timeaccount = $this->_createTimeaccount($_data, $contract);
+
+            // get timesheets
+            $timesheets = $this->_getTimesheetsForProject($_data['pm_id']);
+             
+            // add timesheets
+            foreach ($timesheets as &$timesheet) {
+                // @todo scan timesheets and add additional timeaccounts for special categories
+                
+                $timesheet = $this->_createTimesheet($timesheet, $timeaccount->getId());
+            }
         }        
     }
     
@@ -165,6 +204,8 @@ class Timetracker_Setup_Import_Egw14
         //-- add modlog info?
         ), TRUE);
         
+        $this->_counters['contracts']++;
+        
         return Erp_Controller_Contract::getInstance()->create($contract);
     }
 
@@ -172,11 +213,12 @@ class Timetracker_Setup_Import_Egw14
      * create tine timeaccount
      *
      * @param array $_data with egw project data
-     * @return Timesheet_Model_Timeaccount
+     * @param Erp_Model_Contract $_contract
+     * @return Tinebase_Record_RecordSet of Timetracker_Model_Timeaccount
      * 
      * @todo    add more fields
      */
-    protected function _createTimeaccount($_data)
+    protected function _createTimeaccount($_data, $_contract)
     {
         $timeaccount = new Timetracker_Model_Timeaccount(array(
             'title'                 => $_data['pm_title'],
@@ -192,6 +234,101 @@ class Timetracker_Setup_Import_Egw14
         */        
         ), TRUE);
         
-        return Timetracker_Controller_Timeaccount::getInstance()->create($timeaccount);
+        // link contract to timeaccount
+        $timeaccount->relations = array(array(
+            'own_model'              => 'Timetracker_Model_Timeaccount',
+            'own_backend'            => Timetracker_Backend_Timeaccount::TYPE,
+            'own_degree'             => Tinebase_Model_Relation::DEGREE_SIBLING,
+            'type'                   => Timetracker_Model_Timeaccount::RELATION_TYPE_CONTRACT,
+            'related_id'             => $_contract->getId(),   
+            'related_model'          => 'Erp_Model_Contract',
+            'related_backend'        => Erp_Backend_Contract::TYPE     
+        ));
+           
+        $timeaccount = Timetracker_Controller_Timeaccount::getInstance()->create($timeaccount);
+        
+        $this->_counters['timeaccounts']++;
+        
+        return $timeaccount;
     }    
+
+    /**
+     * create tine timesheet
+     *
+     * @param Timetracker_Model_Timesheet $_record
+     * @param string $_timeaccountId
+     * @return Timetracker_Model_Timesheet 
+     * 
+     */
+    protected function _createTimesheet($_record, $_timeaccountId)
+    {
+        $_record->timeaccount_id = $_timeaccountId;
+        
+        // check if user is available
+        try {
+            Tinebase_User::getInstance()->getUserById($_record->account_id);
+        } catch (Tinebase_Exception_NotFound $enf) {
+            //echo "  Couldn't find user with id " . $_record->account_id . "\n";
+            return NULL;
+        }
+        
+        if ($_record->isValid()) {
+            Timetracker_Controller_Timesheet::getInstance()->create($_record);
+        } else {
+            //echo "  Timesheet invalid: ". print_r($_record->getValidationErrors(), true) . "\n";
+            return NULL;
+        }
+
+        $this->_counters['timesheets']++;
+    }
+    
+    /**
+     * get timesheets for project
+     *
+     * @param integer $_projectId
+     * @return Tinebase_Record_RecordSet of Timetracker_Model_Timesheet records
+     * 
+     * @todo add correct account id again
+     */
+    protected function _getTimesheetsForProject($_projectId)
+    {
+        $select = $this->_db->select()
+            ->from(array('timesheets' => $this->_oldTablePrefix . 'timesheet'))
+            ->joinLeft(
+                array('links'  => $this->_oldTablePrefix . 'links'), 
+                "(link_app1='timesheet' AND link_app2='projectmanager' AND link_id2=timesheets.ts_id)", 
+                array()
+            )
+            ->where($this->_db->quoteInto('links.link_id2 = ?', $_projectId))
+            //->limit(10)
+            //-- order by?
+            ;
+            
+        $stmt = $this->_db->query($select);
+        $queryResult = $stmt->fetchAll();
+        
+        echo "  Timesheets to import for project id $_projectId: " . count($queryResult) . "\n";
+        
+        $timesheets = new Tinebase_Record_RecordSet('Timetracker_Model_Timesheet');
+        foreach ($queryResult as $row) {
+            
+            //-- get custom fields
+            
+            // create timesheet record
+            $record = new Timetracker_Model_Timesheet(array(
+                //'account_id'            => $row['ts_owner'],
+                'account_id'            => Tinebase_Core::getUser()->getId(),
+                'start'                 => $row['ts_start'],
+                'duration'              => $row['ts_duration'],
+                'description'           => $row['ts_description'],
+                'is_cleared'            => 1,
+                //'timeaccount_id'        => ,
+                //'is_billable'           => ,
+            ), TRUE);
+            
+            $timesheets->addRecord($record);
+        }        
+        
+        return $timesheets;
+    }
 }
