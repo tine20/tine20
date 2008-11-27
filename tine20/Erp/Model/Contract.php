a6487e34bdcc27c5e5ecd90c02a56cc5a25d1754
Philipp Schüle <p.schuele@metaways.de>
2008-11-27 15:38:42 +0000

    updated @todo
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5651 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Erp/Model/Contract.php
+++ tine20/Erp/Model/Contract.php
@@ -8,7 +8,7 @@
  * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
  * 
- * @todo        add contract status table or as enum?
+ * @todo        add contract status table
  */
 
 /**
