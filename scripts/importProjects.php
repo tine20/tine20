652c421b1513f503aa1fa7c4843bc82cc2897920
Philipp Schüle <p.schuele@metaways.de>
2008-11-28 17:38:56 +0000

    added projects import script
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5672 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ scripts/importProjects.php
@@ -0,0 +1,29 @@
+#!/usr/bin/env php
+<?php
+/**
+ * import projects
+ * - copy this to your tine20 root dir
+ *
+ * @package     HelperScripts
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id:  $
+ *
+ * usage: ./importProjects.php 
+ */
+
+if (php_sapi_name() != 'cli') {
+    die('not allowed!');
+}
+
+set_include_path(dirname(__FILE__) .'/Zend' . PATH_SEPARATOR . get_include_path());
+
+require_once 'Zend/Loader.php';
+
+Zend_Loader::registerAutoload();
+
+$importer = new Timetracker_Setup_Import_Egw14();
+$importer->import();
+
+?>
