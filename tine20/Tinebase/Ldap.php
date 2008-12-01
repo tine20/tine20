0ee2826fcd1672ea34c0cd7e22fd9784338fafcb
Cornelius Weiß <c.weiss@metaways.de>
2008-12-01 20:02:15 +0000

    add handling for binary data in ldap backend
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5698 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Ldap.php
+++ tine20/Tinebase/Ldap.php
@@ -65,7 +65,8 @@ class Tinebase_Ldap extends Zend_Ldap
      *
      * @param   string $_dn base dn (where to start searching)
      * @param   string $_filter search filter
-     * @param   array $_attributes which fields to return
+     * @param   array  $_attributes which fields to return
+     * @param   string $_order sort result by given attreibute ASC
      * @return  array
      * @throws  Exception with ldap error
      */
@@ -156,6 +157,39 @@ class Tinebase_Ldap extends Zend_Ldap
         ldap_free_result($searchResult);
         
         return $entries[0];
+    }
+    
+    /**
+     * read binary attribute from one entry from the ldap directory
+     *
+     * @param string $_dn the dn to read
+     * @param string $_filter search filter
+     * @param array $_attribute which field to return
+     * @return blob binary data of given field
+     * @throws  Exception with ldap error
+     */
+    public function fetchBinaryAttribute($_dn, $_filter, $_attribute)
+    {
+        if(!is_resource($this->_resource)) {
+            throw new Exception('Not connected to ldap server.');
+        }
+        
+        $searchResult = @ldap_search($this->_resource, $_dn, $_filter, $_attributes, $this->_attrsOnly, $this->_sizeLimit, $this->_timeLimit);  
+        
+        if($searchResult === FALSE) {
+            throw new Exception(ldap_error($this->_resource));
+        }
+        
+        $searchCount = ldap_count_entries($this->_resource, $searchResult);
+        if($searchCount === 0) {
+            throw new Exception('Nothing found for filter: ' . $_filter);
+        } elseif ($searchCount > 1) {
+            throw new Exception('More than one entry found for filter: ' . $_filter);
+        }
+        
+        $entry = ldap_first_entry($this->_resource, $searchResult);
+        
+        return ldap_get_values_len($this->_resource, $entry, $attribute);
     }
     
     /**
