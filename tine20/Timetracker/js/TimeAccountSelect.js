17446f381f4674d4f21f7a1811c69502d951280a
Cornelius Weiß <c.weiss@metaways.de>
2008-12-02 13:56:11 +0000

    trigger build
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5706 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Timetracker/js/TimeAccountSelect.js
@@ -0,0 +1,12 @@
+/**
+ * Tine 2.0
+ * 
+ * @package     Timetracker
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+ 
+Ext.ns('Tine.Timetracker');
