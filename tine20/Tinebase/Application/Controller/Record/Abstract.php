732d01e909891238839f6e01bf3f532d38b88be4
Philipp Schüle <p.schuele@metaways.de>
2008-12-09 14:54:01 +0000

    added some debug output in case of an exception
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5793 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Application/Controller/Record/Abstract.php
+++ tine20/Tinebase/Application/Controller/Record/Abstract.php
@@ -249,6 +249,8 @@ abstract class Tinebase_Application_Controller_Record_Abstract extends Tinebase_
             
         } catch (Exception $e) {
             Tinebase_TransactionManager::getInstance()->rollBack();
+            Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' ' . $e->getMessage());
+            Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' ' . $e->getTraceAsString());
             throw $e;
         }
         
