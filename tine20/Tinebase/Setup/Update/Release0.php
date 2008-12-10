9ca5c54c9fb4034bc8483210a54e23964d953d66
Philipp Schüle <p.schuele@metaways.de>
2008-12-10 12:47:03 +0000

    changed foreign key name
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5817 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Setup/Update/Release0.php
+++ tine20/Tinebase/Setup/Update/Release0.php
@@ -1399,7 +1399,7 @@ class Tinebase_Setup_Update_Release0 extends Setup_Update_Abstract
         // tables with application_id as foreign key
         $appIdTables = array(
             'application_tables' => 'application_tables::application_id--applications::id',
-            'container' => 'container_application_id',
+            'container' => 'application_id',
             'role_rights' => 'role_rights::application_id--applications::id',
             'config' => 'config::application_id--applications::id',
             'config_user' => 'config_user::application_id--applications::id',
