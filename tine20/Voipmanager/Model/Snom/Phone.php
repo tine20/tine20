fb5fc43fa7d15a76c4e98fa433e2cc8023bd59bd
Bernd Müller <b.mueller@metaways.de>
2008-12-11 13:11:38 +0000

    todo added
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5869 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Voipmanager/Model/Snom/Phone.php
+++ tine20/Voipmanager/Model/Snom/Phone.php
@@ -8,6 +8,7 @@
  * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
  *
+ * @todo        Check location_id and template_id in javascript edit dialogue, no empty strings allowed
  */
 
 /**
