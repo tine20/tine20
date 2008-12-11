5c18147342f5e5ed37f84c9e019bcb5ebadd065c
Bernd Müller <b.mueller@metaways.de>
2008-12-11 13:12:26 +0000

    Constructor added
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5872 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Voipmanager/Model/Snom/Setting.php
+++ tine20/Voipmanager/Model/Snom/Setting.php
@@ -6,7 +6,7 @@
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Thomas Wadewitz <t.wadewitz@metaways.de>
  * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
- * @version     $Id:  $
+ * @version     $Id$
  *
  */
 
@@ -80,6 +80,33 @@ class Voipmanager_Model_Snom_Setting extends Tinebase_Record_Abstract
         'show_call_status_writable' => array(Zend_Filter_Input::ALLOW_EMPTY => true, Zend_Filter_Input::DEFAULT_VALUE => NULL)
     );
 
+    /**
+     * overwrite constructor to add more filters
+     *
+     * @param mixed $_data
+     * @param bool $_bypassFilters
+     * @param mixed $_convertDates
+     * @return void
+     */
+    public function __construct($_data = NULL, $_bypassFilters = false, $_convertDates = true)
+    {
+        // set default value if field is empty
+        $this->_filters['web_language'] = new Zend_Filter_Empty('English');
+        $this->_filters['language'] = new Zend_Filter_Empty('English');
+        $this->_filters['display_method'] = new Zend_Filter_Empty('full_contact');
+        $this->_filters['mwi_notification'] = new Zend_Filter_Empty('silent');
+        $this->_filters['mwi_dialtone'] = new Zend_Filter_Empty('normal');
+        $this->_filters['headset_device'] = new Zend_Filter_Empty('none');
+        $this->_filters['message_led_other'] = new Zend_Filter_Empty(0);
+        $this->_filters['global_missed_counter'] = new Zend_Filter_Empty(0);
+        $this->_filters['scroll_outgoing'] = new Zend_Filter_Empty(0);
+        $this->_filters['show_local_line'] = new Zend_Filter_Empty(0);
+        $this->_filters['show_call_status'] = new Zend_Filter_Empty(0);
+        $this->_filters['call_waiting'] = new Zend_Filter_Empty('on');
+        
+        parent::__construct($_data, $_bypassFilters, $_convertDates);
+    }
+    
     /**
      * converts a int, string or Voipmanager_Model_Setting to an setting id
      *
