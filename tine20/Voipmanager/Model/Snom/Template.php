9d243723504b82e33e1edf23820ee2489ed12e65
Bernd Müller <b.mueller@metaways.de>
2008-12-11 13:11:51 +0000

    todo added
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5870 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Voipmanager/Model/Snom/Template.php
+++ tine20/Voipmanager/Model/Snom/Template.php
@@ -6,8 +6,9 @@
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Thomas Wadewitz <t.wadewitz@metaways.de>
  * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
- * @version     $Id:  $
+ * @version     $Id$
  *
+ * @todo        Check setting_id and software_id in javascript edit dialogue, no empty strings allowed
  */
 
 /**
