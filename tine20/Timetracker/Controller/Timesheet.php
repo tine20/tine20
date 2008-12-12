4a24aff61e32258ede67bba0294981550af32839
Philipp Schüle <p.schuele@metaways.de>
2008-12-12 23:02:24 +0000

    added grants to CRUD functions
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5935 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Timetracker/Controller/Timesheet.php
+++ tine20/Timetracker/Controller/Timesheet.php
@@ -127,6 +127,21 @@ class Timetracker_Controller_Timesheet extends Tinebase_Application_Controller_R
         return parent::searchCount($_filter);
     }
 
+    /**
+     * get by id
+     *
+     * @param string $_id
+     * @param int $_containerId
+     * @return Tinebase_Record_Interface
+     */
+    public function get($_id, $_containerId = NULL)
+    {
+        $result = parent::get($_id, $_containerId);
+
+        $this->_checkGrant($result, 'get', TRUE, 'No permission to read this Timesheet.');
+        
+        return $result;
+    }
 
     /**
      * add one record
@@ -137,11 +152,80 @@ class Timetracker_Controller_Timesheet extends Tinebase_Application_Controller_R
      */
     public function create(Tinebase_Record_Interface $_record)
     {        
-        if (!Timetracker_Model_TimeaccountGrants::hasGrant($_record->timeaccount_id, Timetracker_Model_TimeaccountGrants::BOOK_OWN)
-            || !Timetracker_Model_TimeaccountGrants::hasGrant($_record->timeaccount_id, Timetracker_Model_TimeaccountGrants::BOOK_ALL)) {
-            throw new Tinebase_Exception_AccessDenied('No permission to create this Timesheet');
-        }
+        $this->_checkGrant($_record, 'create', TRUE, 'No permission to create this Timesheet.');
         
         return parent::create($_record);
+    }
+
+    /**
+     * update one record
+     *
+     * @param   Tinebase_Record_Interface $_record
+     * @return  Tinebase_Record_Interface
+     */
+    public function update(Tinebase_Record_Interface $_record)
+    {
+        $this->_checkGrant($_record, 'update', TRUE, 'No permission to update this Timesheet.');
+        
+        return parent::update($_record);
+    }
+    
+    /**
+     * Deletes a set of records.
+     * 
+     * If one of the records could not be deleted, no record is deleted
+     * 
+     * @param   array array of record identifiers
+     * @deprecated ?
+     */
+    /*
+    public function delete($_ids)
+    {
+        return parent::delete($_ids);
+    }
+    */
+    
+    /****************************** protected functions ************************/
+    
+    /**
+     * check grant for action
+     *
+     * @param Timetracker_Model_Timeaccount $_record
+     * @param string $_action
+     * @param boolean $_throw
+     * @param string $_errorMessage
+     * @return boolean
+     * @throws Tinebase_Exception_AccessDenied
+     */
+    protected function _checkGrant($_record, $_action, $_throw = FALSE, $_errorMessage = 'No Permission.')
+    {
+        switch ($_action) {
+            case 'create':
+            case 'update':
+            case 'delete':
+                $hasGrant = (   
+                    (Timetracker_Model_TimeaccountGrants::hasGrant($_record->timeaccount_id, Timetracker_Model_TimeaccountGrants::BOOK_OWN)
+                        && $_record->account_id == $this->_currentAccount->getId())
+                    || Timetracker_Model_TimeaccountGrants::hasGrant($_record->timeaccount_id, Timetracker_Model_TimeaccountGrants::BOOK_ALL) 
+                        
+                );
+                break;
+            case 'get':
+                $hasGrant = (   
+                    (Timetracker_Model_TimeaccountGrants::hasGrant($_record->timeaccount_id, Timetracker_Model_TimeaccountGrants::BOOK_OWN)
+                        && $_record->account_id == $this->_currentAccount->getId())
+                    || Timetracker_Model_TimeaccountGrants::hasGrant($_record->timeaccount_id, Timetracker_Model_TimeaccountGrants::VIEW_ALL)
+                    || Timetracker_Model_TimeaccountGrants::hasGrant($_record->timeaccount_id, Timetracker_Model_TimeaccountGrants::BOOK_ALL) 
+                );
+                break;
+            default:
+                $hasGrant = FALSE;
+        }
+        
+        if ($_throw && !$hasGrant) {
+            throw new Tinebase_Exception_AccessDenied($_errorMessage);
+        }
+        
+        return $hasGrant;
     }
 }
