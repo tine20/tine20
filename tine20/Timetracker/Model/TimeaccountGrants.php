c50fd865ecff90b0a5df49db80a8d6f8cde90f18
Cornelius Weiß <c.weiss@metaways.de>
2008-12-15 09:19:40 +0000

    add function to resolve multiple TA grants
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@5945 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Timetracker/Model/TimeaccountGrants.php
+++ tine20/Timetracker/Model/TimeaccountGrants.php
@@ -142,6 +142,23 @@ class Timetracker_Model_TimeaccountGrants extends Tinebase_Record_Abstract
     }
     
     /**
+     * get grants assigned to multiple records
+     *
+     * @param   Tinebase_Record_RecordSet $_records records to get the grants for
+     * @param   int|Tinebase_Model_User $_accountId the account to get the grants for
+     * @throws  Tinebase_Exception_NotFound
+     */
+    public static function getGrantsOfRecords(Tinebase_Record_RecordSet $_records, $_accountId, $_timeaccountProperty = 'timeaccount_id')
+    {
+        $timeaccounts = new Tinebase_Record_RecordSet('Timetracker_Model_Timeaccount', $_records->$_timeaccountProperty);
+        Tinebase_Container::getInstance()->getGrantsOfRecords($timeaccounts, $_accountId);
+        
+        foreach ($timeaccounts as $timeaccount) {
+            $timeaccount->account_grants = $timeaccount->container_id['account_grants'];
+        }
+    }
+    
+    /**
      * get timeaccounts by grant
      *
      * @param integer $_grant
