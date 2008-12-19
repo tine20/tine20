63725c0abf8eec891b350d5cceb0b7b35f9bef51
Cornelius Weiß <c.weiss@metaways.de>
2008-12-19 13:36:40 +0000

    add date and account filter
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@6072 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Timetracker/js/TimesheetGridPanel.js
+++ tine20/Timetracker/js/TimesheetGridPanel.js
@@ -49,8 +49,11 @@ Tine.Timetracker.TimesheetGridPanel = Ext.extend(Tine.Tinebase.widgets.app.GridP
     initFilterToolbar: function() {
         this.filterToolbar = new Tine.widgets.grid.FilterToolbar({
             filterModels: [
-                {label: this.app.i18n._('Timesheet'),    field: 'query',    operators: ['contains']}
-                //{label: this.app.i18n._('Summary'), field: 'summary' }
+                {label: this.app.i18n._('Timesheet'),    field: 'query',    operators: ['contains']},
+                //{label: this.app.i18n._('Timeaccount'),    field: 'equals',    operators: ['contains']},
+                {label: this.app.i18n._('Account'),      field: 'account_id', valueType: 'user'},
+                {label: this.app.i18n._('Date'),         field: 'start_date', valueType: 'date'},
+                {label: this.app.i18n._('Description'),  field: 'description' }
              ],
              defaultFilter: 'query',
              filters: []
