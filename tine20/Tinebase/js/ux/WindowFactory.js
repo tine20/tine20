83f1a40b44e2161c57582d7fbf15cac57827c67b
Cornelius Weiß <c.weiss@metaways.de>
2009-01-14 10:02:00 +0000

    workaround IE cross window problem
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@6212 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/ux/WindowFactory.js
+++ tine20/Tinebase/js/ux/WindowFactory.js
@@ -96,15 +96,45 @@ Ext.ux.WindowFactory.prototype = {
         if (config.contentPanelConstructor) {
             config.contentPanelConstructorConfig = config.contentPanelConstructorConfig || {};
             
+            /*
+             * IE fix for listeners
+             * 
+             * In IE we have two problems when dealing with listeners accros windows
+             * 1. listeners (functions) are defined in the parent window. a typeof (some function from parent) returns object in IE
+             *    the Ext.Observable code can't deal with this
+             * 2. listeners get executed by fn.apply(scope, arguments). For some reason in IE this dosn't work with functions defined
+             *    in an other window.
+             *    
+             * To work araoud this, we create new fresh listeners in the new window and proxy the event calls
+             */
+            var ls = config.contentPanelConstructorConfig.listeners;
+            if (ls /* && Ext.isIE */) {
+                var lsProxy = {};
+                for (var p in ls) {
+                    if (ls.hasOwnProperty(p)) {
+                        // NOTE apply dosn't work here for some strange reason, so we hope that there are not more than 5 params
+                        if (ls[p].fn) {
+                            lsProxy[p] = function() {ls[p].fn.call(ls[p].scope, arguments[0], arguments[1], arguments[2], arguments[3], arguments[4]);}
+                        } else {
+                            lsProxy[p] = function() {ls[p].call(ls.scope, arguments[0], arguments[1], arguments[2], arguments[3], arguments[4]);}
+                        }
+                    }
+                }
+                config.contentPanelConstructorConfig.listeners = lsProxy;
+            }
+            
+            // place a referece to current window class in the itemConsturctor.
+            // this may be overwritten depending on concrete window implementation
+            config.contentPanelConstructorConfig.window = config;
+            
+            // find the constructor in this context
             var parts = config.contentPanelConstructor.split('.');
             var ref = window;
             for (var i=0; i<parts.length; i++) {
                 ref = ref[parts[i]];
             }
             
-            // place a referece to current window class in the itemConsturctor.
-            // this may be overwritten depending on concrete window implementation
-            config.contentPanelConstructorConfig.window = config;
+            // finally construct the content panel
             var items = new ref(config.contentPanelConstructorConfig);
         } else {
             items = config.items ? config.items : {};
