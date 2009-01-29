d6337b4a72d506305769db55c9562cd285036945
Cornelius Weiß <c.weiss@metaways.de>
2009-01-29 21:12:39 +0000

    export button must use _all_ filterplugins
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@6468 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/widgets/grid/ExportButton.js
+++ tine20/Tinebase/js/widgets/grid/ExportButton.js
@@ -37,29 +37,28 @@ Ext.extend(Tine.widgets.grid.ExportButton, Ext.Button, {
      */
     exportFunction: null,
     /**
-     * @cfg {Object} the filter toolbar with filter settings
+     * @cfg {Tine.Tinebase.widgets.grid.FilterSelectionModel} sm
      */
-    filterToolbar: null,
+    sm: null,
     /**
-     * @cfg {String} application tree id
+     * @cfg {Tine.Tinebase.widgets.app.GridPanel} gridPanel
+     * use this alternativly to sm
      */
-    appTreeId: null,
+    gridPanel: null,
     
     /**
      * do export
      */
-    doExport: function() {    	
-    	var filterSettings = this.filterToolbar.getValue();
+    doExport: function() {
+        // get selection model
+        if (!this.sm) {
+            this.sm = this.gridPanel.grid.getSelectionModel();
+        }
         
-        // add container to filter
-    	if (this.appTreeId) {
-            var nodeAttributes = Ext.getCmp(this.appTreeId).getSelectionModel().getSelectedNode().attributes || {};
-            filterSettings.push(
-                {field: 'containerType', operator: 'equals', value: nodeAttributes.containerType ? nodeAttributes.containerType : 'all' },
-                {field: 'container',     operator: 'equals', value: nodeAttributes.container ? nodeAttributes.container.id : null       },
-                {field: 'owner',         operator: 'equals', value: nodeAttributes.owner ? nodeAttributes.owner.accountId : null        }
-            );
-    	}
+        // temporary select all
+        this.sm.selectAll();
+        
+    	var filterSettings = this.sm.getSelectionFilter();
 
         //console.log(filterSettings);
     	//Tine.Tinebase.common.openWindow('exportWindow', 'index.php?method=' + 
