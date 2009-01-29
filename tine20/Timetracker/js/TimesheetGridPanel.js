c263323eaaf16436a696e7a48a76247801ce84a9
Cornelius Weiß <c.weiss@metaways.de>
2009-01-29 10:35:40 +0000

    add filters for TA title/number
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@6447 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Timetracker/js/TimesheetGridPanel.js
+++ tine20/Timetracker/js/TimesheetGridPanel.js
@@ -47,6 +47,8 @@ Tine.Timetracker.TimesheetGridPanel = Ext.extend(Tine.Tinebase.widgets.app.GridP
             filterModels: [
                 //{label: this.app.i18n._('Timesheet'),    field: 'query',    operators: ['contains']}, // query only searches description
                 new Tine.Timetracker.TimeAccountGridFilter(),
+                {label: this.app.i18n._('Time Account') + ' - ' + this.app.i18n._('Number'), field: 'timeaccount_number'},
+                {label: this.app.i18n._('Time Account') + ' - ' + this.app.i18n._('Title'),   field: 'timeaccount_title'},
                 {label: this.app.i18n._('Account'),      field: 'account_id', valueType: 'user'},
                 {label: this.app.i18n._('Date'),         field: 'start_date', valueType: 'date'},
                 {label: this.app.i18n._('Description'),  field: 'description' },
