6a5b36e0eb0c86665e24f03df5dd9a8385fbaadf
Philipp Schüle <p.schuele@metaways.de>
2009-02-05 11:55:43 +0000

    removed obsolete code
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@6587 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tasks/Model/TaskFilter.php
+++ tine20/Tasks/Model/TaskFilter.php
@@ -17,13 +17,6 @@
 class Tasks_Model_TaskFilter extends Tinebase_Model_Filter_FilterGroup
 {
     /**
-     * @var string class name of this filter group
-     *      this is needed to overcome the static late binding
-     *      limitation in php < 5.3
-     */
-    protected $_className = 'Tasks_Model_TaskFilter';
-    
-    /**
      * @var string application of this filter group
      */
     protected $_applicationName = 'Tasks';
