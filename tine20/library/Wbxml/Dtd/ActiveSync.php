6563ef253cb3af0e4f51a0feea8b095fd0be9e0d
Lars Kneschke <l.kneschke@metaways.de>
2009-02-17 19:07:11 +0000

    phpdoc updates
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@6813 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/library/Wbxml/Dtd/ActiveSync.php
+++ tine20/library/Wbxml/Dtd/ActiveSync.php
@@ -19,40 +19,60 @@
 
 class Wbxml_Dtd_ActiveSync
 {
-    const CODEPAGE_AIRSYNC=0;
-    const CODEPAGE_CONTACTS=1;
-    const CODEPAGE_EMAIL=2;
-    const CODEPAGE_AIRNOTIFY=3;
-    const CODEPAGE_CALENDAR=4;
-    const CODEPAGE_MOVE=5;
-    const CODEPAGE_ITEMESTIMATE=6;
-    const CODEPAGE_FOLDERHIERARCHY=7;
-    const CODEPAGE_MEETINGRESPONSE=8;
-    const CODEPAGE_TASKS=9;
-    const CODEPAGE_RESOLVERECIPIENTS=10;
-    const CODEPAGE_VALIDATECERT=11;
-    const CODEPAGE_CONTACTS2=12;
-    const CODEPAGE_PING=13;
-    const CODEPAGE_PROVISION=14;
-    const CODEPAGE_SEARCH=15;
-    const CODEPAGE_GAL=16;
-    const CODEPAGE_AIRSYNCBASE=17;
-    const CODEPAGE_SETTINGS=18;
-    const CODEPAGE_DOCUMENTLIBRARY=19;
-    const CODEPAGE_ITEMOPERATIONS=20;
+    const CODEPAGE_AIRSYNC          = 0;
+    const CODEPAGE_CONTACTS         = 1;
+    const CODEPAGE_EMAIL            = 2;
+    const CODEPAGE_AIRNOTIFY        = 3;
+    const CODEPAGE_CALENDAR         = 4;
+    const CODEPAGE_MOVE             = 5;
+    const CODEPAGE_ITEMESTIMATE     = 6;
+    const CODEPAGE_FOLDERHIERARCHY  = 7;
+    const CODEPAGE_MEETINGRESPONSE  = 8;
+    const CODEPAGE_TASKS            = 9;
+    const CODEPAGE_RESOLVERECIPIENTS = 10;
+    const CODEPAGE_VALIDATECERT     = 11;
+    const CODEPAGE_CONTACTS2        = 12;
+    const CODEPAGE_PING             = 13;
+    const CODEPAGE_PROVISION        = 14;
+    const CODEPAGE_SEARCH           = 15;
+    const CODEPAGE_GAL              = 16;
+    const CODEPAGE_AIRSYNCBASE      = 17;
+    const CODEPAGE_SETTINGS         = 18;
+    const CODEPAGE_DOCUMENTLIBRARY  = 19;
+    const CODEPAGE_ITEMOPERATIONS   = 20;
     
+    /**
+     * variable to hold currently active codepage
+     *
+     * @var Wbxml_Dtd_ActiveSync_Abstract
+     */
     protected $_currentCodePage;
     
+    /**
+     * the constructor
+     *
+     */
     public function __construct()
     {
         $this->_currentCodePage = new Wbxml_Dtd_ActiveSync_CodePage0();
     }
     
+    /**
+     * returns reference to current codepage
+     *
+     * @return Wbxml_Dtd_ActiveSync_Abstract
+     */
     public function getCurrentCodePage() 
     {
         return $this->_currentCodePage;
     }
     
+    /**
+     * switch to another codepage
+     *
+     * @param integer $_codePageId id of the codepage
+     * @return Wbxml_Dtd_ActiveSync_Abstract
+     */
     public function switchCodePage($_codePageId)
     {
         $className = 'Wbxml_Dtd_ActiveSync_CodePage' . $_codePageId;
