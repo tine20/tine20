1330f611a908c7588a48e56d9ef77825f5d735ba
Lars Kneschke <l.kneschke@metaways.de>
2009-02-17 16:54:15 +0000

    first public release of wbxml encoder and decoder
    Currently only ActiveSync works as expected. Support for SyncML will get added later.
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@6800 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/library/Wbxml/Dtd/ActiveSync.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Wbxml
+ * @subpackage  ActiveSync
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2008-2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id:Factory.php 4968 2008-10-17 09:09:33Z l.kneschke@metaways.de $
+ */
+
+/**
+ * class documentation
+ *
+ * @package     Wbxml
+ * @subpackage  ActiveSync
+ */
+
+class Wbxml_Dtd_ActiveSync
+{
+    const CODEPAGE_AIRSYNC=0;
+    const CODEPAGE_CONTACTS=1;
+    const CODEPAGE_EMAIL=2;
+    const CODEPAGE_AIRNOTIFY=3;
+    const CODEPAGE_CALENDAR=4;
+    const CODEPAGE_MOVE=5;
+    const CODEPAGE_ITEMESTIMATE=6;
+    const CODEPAGE_FOLDERHIERARCHY=7;
+    const CODEPAGE_MEETINGRESPONSE=8;
+    const CODEPAGE_TASKS=9;
+    const CODEPAGE_RESOLVERECIPIENTS=10;
+    const CODEPAGE_VALIDATECERT=11;
+    const CODEPAGE_CONTACTS2=12;
+    const CODEPAGE_PING=13;
+    const CODEPAGE_PROVISION=14;
+    const CODEPAGE_SEARCH=15;
+    const CODEPAGE_GAL=16;
+    const CODEPAGE_AIRSYNCBASE=17;
+    const CODEPAGE_SETTINGS=18;
+    const CODEPAGE_DOCUMENTLIBRARY=19;
+    const CODEPAGE_ITEMOPERATIONS=20;
+    
+    protected $_currentCodePage;
+    
+    public function __construct()
+    {
+        $this->_currentCodePage = new Wbxml_Dtd_ActiveSync_CodePage0();
+    }
+    
+    public function getCurrentCodePage() 
+    {
+        return $this->_currentCodePage;
+    }
+    
+    public function switchCodePage($_codePageId)
+    {
+        $className = 'Wbxml_Dtd_ActiveSync_CodePage' . $_codePageId;
+        
+        $this->_currentCodePage = new $className();
+        
+        return $this->_currentCodePage;
+    }
+    
+    /**
+     * get initial dom document
+     *
+     * @return DOMDocument
+     */
+    public function getDomDocument($_nameSpace, $_tag)
+    {
+        $dtd = DOMImplementation::createDocumentType('AirSync', "-//AIRSYNC//DTD AirSync//EN", "http://www.microsoft.com/");
+        $dom = DOMImplementation::createDocument($_nameSpace, $_tag, $dtd);
+        $dom->encoding = 'utf-8';
+        
+        return $dom;
+    }
+}    
---
+++ tine20/library/Wbxml/Dtd/Exception/CodePageNotFound.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Wbxml
+ * @subpackage  Wbxml
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2008-2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id:CodePageNotFound.php 4968 2008-10-17 09:09:33Z l.kneschke@metaways.de $
+ */
+
+/**
+ * class documentation
+ *
+ * @package     Wbxml
+ * @subpackage  Wbxml
+ */
+ 
+ class Wbxml_Dtd_Exception_CodePageNotFound extends Exception 
+ {
+ }
