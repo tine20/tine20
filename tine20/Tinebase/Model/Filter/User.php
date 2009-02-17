1cc413e3ab2989580da9c0d84e2f4f3a1e9abc9d
Cornelius Weiß <c.weiss@metaways.de>
2009-02-17 13:59:22 +0000

    new user filter
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@6783 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/Model/Filter/User.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Tinebase
+ * @subpackage  Filter
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Tinebase_Model_Filter_User
+ * 
+ * @package     Tinebase
+ * @subpackage  Filter
+ * 
+ * filters for user id
+ * 
+ * adds a inGroup operator
+ */
+class Tinebase_Model_Filter_User extends Tinebase_Model_Filter_Int
+{
+    protected $_userOperator = NULL;
+    protected $_userValue = NULL;
+    
+    /**
+     * sets operator
+     *
+     * @param string $_operator
+     */
+    public function setOperator($_operator)
+    {
+        if ($_operator == 'inGroup') {
+            $this->_userOperator = $_operator;
+            $_operator = 'in';
+        }
+        
+        parent::setOperator($_operator);
+    }
+    
+    /**
+     * sets value
+     *
+     * @param mixed $_value
+     */
+    public function setValue($_value)
+    {
+        if ($this->_userOperator && $this->_userOperator == 'inGroup' && $this->_userValue) {
+            $this->_userValue = $_value;
+            $_value = Tinebase_Group::getInstance()->getGroupMembers($this->_userValue);
+        }
+        
+        parent::setValue($_value);
+    }
+    
+    /**
+     * returns array with the filter settings of this filter
+     *
+     * @param  bool $_valueToJson resolve value for json api?
+     * @return array
+     */
+    public function toArray($_valueToJson = false)
+    {
+        $result = parent::toArray($_valueToJson);
+        
+        if ($this->_userOperator && $this->_userOperator == 'inGroup') {
+            $result['operator'] = $this->_userOperator;
+            $result['value']    = $this->_userValue;
+        }
+        
+        if ($_valueToJson == true ) {
+            if ($this->_userOperator && $this->_userOperator == 'inGroup' && $this->_userValue) {
+                $result['value'] = Tinebase_Group::getInstance()->getGroupById($this->_userValue)->toArray();
+            } else {
+                switch ($this->_operator) {
+                    case 'equals':
+                        $result['value'] = Tinebase_User::getInstance()->getUserById($this->_value)->toArray();
+                        break;
+                    case 'in':
+                        $result['value'] = array();
+                        foreach ($this->_value as $userId) {
+                            $result['value'][] = Tinebase_User::getInstance()->getUserById($userId)->toArray();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        return $result;
+    }
+}
