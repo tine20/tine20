cb9aa4675d8937b51924f4be0ec6b01ae164da3d
Cornelius Weiß <c.weiss@metaways.de>
2009-02-18 14:06:26 +0000

    adopt desc. flter
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@6825 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Timetracker/js/TimesheetGridPanel.js
+++ tine20/Timetracker/js/TimesheetGridPanel.js
@@ -184,7 +184,7 @@ Tine.Timetracker.TimesheetGridPanel = Ext.extend(Tine.Tinebase.widgets.app.GridP
                 new Tine.Timetracker.TimeAccountStatusGridFilter(),
                 {label: this.app.i18n._('Account'),      field: 'account_id', valueType: 'user'},
                 {label: this.app.i18n._('Date'),         field: 'start_date', valueType: 'date', pastOnly: true},
-                {label: this.app.i18n._('Description'),  field: 'description' },
+                {label: this.app.i18n._('Description'),  field: 'ts.description', defaultOperator: 'contains'},
                 {label: this.app.i18n._('Billable'),     field: 'ts.is_billable', valueType: 'bool', defaultValue: true },
                 {label: this.app.i18n._('Cleared'),      field: 'is_cleared',  valueType: 'bool', defaultValue: false },
                 new Tine.widgets.tags.TagFilter({app: this.app})
