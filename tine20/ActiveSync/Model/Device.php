41688d920b782933b2270aeaeda8445b7a7ac819
Philipp Schüle <p.schuele@metaways.de>
2009-02-25 12:35:19 +0000

    changed path to license text
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@6991 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/ActiveSync/Model/Device.php
+++ tine20/ActiveSync/Model/Device.php
@@ -3,7 +3,7 @@
  * Tine 2.0
  * 
  * @package     ActiveSync
- * @license     http://www.tine20.org/licenses/agpl-nonus.html AGPL Version 1 (Non-US)
+ * @license     http://www.tine20.org/licenses/agpl-nonus.txt AGPL Version 1 (Non-US)
  *              NOTE: According to sec. 8 of the AFFERO GENERAL PUBLIC LICENSE (AGPL), 
  *              Version 1, the distribution of the Tine 2.0 ActiveSync module in or to the 
  *              United States of America is excluded from the scope of this license.
