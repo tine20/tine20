cfac9925efccd5a06e829c178680cc052061af91
Cornelius Weiß <c.weiss@metaways.de>
2009-02-26 12:36:46 +0000

    get app grid working
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7020 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Setup/js/ApplicationGridPanel.js
+++ tine20/Setup/js/ApplicationGridPanel.js
@@ -12,5 +12,52 @@
  Ext.ns('Tine', 'Tine.Setup');
 
 Tine.Setup.GridPanel = Ext.extend(Tine.Tinebase.widgets.app.GridPanel, {
-
+    recordClass: Tine.Setup.Model.Application,
+    recordProxy: Tine.Setup.ApplicationBackend,
+    
+    evalGrants: false,
+    
+    gridConfig: {
+        loadMask: true,
+        autoExpandColumn: 'description'
+    },
+    
+    initComponent: function() {
+                
+        this.gridConfig.columns = this.getColumns();
+        //this.actionToolbarItems = this.getToolbarItems();
+        //this.initDetailsPanel();
+        
+        Tine.Setup.GridPanel.superclass.initComponent.call(this);
+    },
+    
+    getColumns: function() {
+        return  [
+            {id: 'name',            width: 400, sortable: true, dataIndex: 'name',            header: this.app.i18n._("Name")}, 
+            {id: 'status',          width: 70,  sortable: true, dataIndex: 'status',          header: this.app.i18n._("Enabled"),       renderer: this.enabledRenderer}, 
+            {id: 'order',           width: 50,  sortable: true, dataIndex: 'order',           header: this.app.i18n._("Order")},
+            {id: 'current_version', width: 70,  sortable: true, dataIndex: 'current_version', header: this.app.i18n._("Current ersion")},
+            {id: 'version',         width: 70,  sortable: true, dataIndex: 'version',         header: this.app.i18n._("Version")},
+            {id: 'upgrade_status',  width: 70,  sortable: true, dataIndex: 'version',         header: this.app.i18n._("Status"),        renderer: this.upgradeStatusRenderer.createDelegate(this)}
+        ];
+    },
+    
+    initActions: function() {
+        this.actionToolbar = new Ext.Toolbar({});
+        this.contextMenu = new Ext.menu.Menu({});
+    },
+    
+    enabledRenderer: function(value) {
+        return Tine.Tinebase.common.booleanRenderer(value == 'enabled');
+    },
+    
+    upgradeStatusRenderer: function() {
+        var app = arguments[2];
+        
+        if (app.get('current_version') == app.get('version')) {
+            return this.app.i18n._('up to date');
+        } else {
+            return this.app.i18n._('please update');
+        }
+    }
 });
--- tine20/Setup/js/Setup.js
+++ tine20/Setup/js/Setup.js
@@ -18,3 +18,36 @@ Tine.Setup.TreePanel = Ext.extend(Ext.Panel, {
     border: false,
     html: ''
 });
+
+Ext.ns('Tine', 'Tine.Setup', 'Tine.Setup.Model');
+
+Tine.Setup.Model.ApplicationArray = Tine.Tinebase.Model.genericFields.concat([
+    { name: 'id'              },
+    { name: 'name'            },
+    { name: 'status'          },
+    { name: 'order'           },
+    { name: 'version'         },
+    { name: 'current_version' }
+]);
+
+/**
+ * Task record definition
+ */
+Tine.Setup.Model.Application = Tine.Tinebase.Record.create(Tine.Setup.Model.ApplicationArray, {
+    appName: 'Setup',
+    modelName: 'Application',
+    idProperty: 'name',
+    titleProperty: 'name',
+    // ngettext('Application', 'Applications', n); gettext('Application');
+    recordName: 'Application',
+    recordsName: 'Applications'
+});
+
+/**
+ * default tasks backend
+ */
+Tine.Setup.ApplicationBackend = new Tine.Tinebase.widgets.app.JsonBackend({
+    appName: 'Setup',
+    modelName: 'Application',
+    recordClass: Tine.Setup.Model.Application
+});
