03b81a7e400f886edd08ad15ea6f52d1b4f6e4e7
Philipp Schüle <p.schuele@metaways.de>
2009-03-06 11:27:29 +0000

    added group filter (not used at the moment)
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7202 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/Model/GroupFilter.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Tinebase
+ * @subpackage  Group
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ * 
+ * @todo        this isn't used yet because the group backends don't have search / filter functionality
+ */
+
+/**
+ *  Groups filter class
+ * 
+ * @package     Tinebase
+ * @subpackage  Groups 
+ */
+class Tinebase_Model_GroupFilter extends Tinebase_Model_Filter_FilterGroup
+{    
+    /**
+     * @var string application of this filter group
+     */
+    protected $_applicationName = 'Tinebase';
+    
+    /**
+     * @var array filter model fieldName => definition
+     */
+    protected $_filterModel = array(
+        'name'           => array('filter' => 'Tinebase_Model_Filter_Text'),
+        'query'          => array('filter' => 'Tinebase_Model_Filter_Query', 'options' => array('fields' => array('name', 'description'))),
+    );
+}
