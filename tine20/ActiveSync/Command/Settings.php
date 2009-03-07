7aa50fe96e13df8b0ccf3e0a29189010b7d3601c
Lars Kneschke <l.kneschke@metaways.de>
2009-03-07 08:27:12 +0000

    stub class for handling settings
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7226 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/ActiveSync/Command/Settings.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     ActiveSync
+ * @subpackage  ActiveSync
+ * @license     http://www.tine20.org/licenses/agpl-nonus.txt AGPL Version 1 (Non-US)
+ *              NOTE: According to sec. 8 of the AFFERO GENERAL PUBLIC LICENSE (AGPL), 
+ *              Version 1, the distribution of the Tine 2.0 ActiveSync module in or to the 
+ *              United States of America is excluded from the scope of this license.
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * class documentation
+ *
+ * @package     ActiveSync
+ * @subpackage  ActiveSync
+ */
+ 
+class ActiveSync_Command_Settings extends ActiveSync_Command_Wbxml 
+{
+    const STATUS_NO_CHANGES_FOUND = 1;
+    
+    /**
+     * Enter description here...
+     *
+     * @var ActiveSync_Backend_StandAlone_Abstract
+     */
+    protected $_dataBackend;
+
+    protected $_defaultNameSpace = 'uri:Settings';
+    protected $_documentElement = 'Settings';
+    
+    
+    /**
+     * process the XML file and add, change, delete or fetches data 
+     *
+     * @todo can we get rid of LIBXML_NOWARNING
+     * @todo we need to stored the initial data for folders and lifetime as the phone is sending them only when they change
+     * @return resource
+     */
+    public function handle()
+    {
+        $controller = ActiveSync_Controller::getInstance();
+        
+        if($this->_inputDom instanceof DOMDocument) {
+        }        
+    }    
+    
+    /**
+     * this function generates the response for the client
+     */
+    public function getResponse()
+    {
+        parent::getResponse();
+    }
+}
