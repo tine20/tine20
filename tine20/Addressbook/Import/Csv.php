4711c1b011beed2f6df1f1a640d3a874c50bce66
Philipp Schüle <p.schuele@metaways.de>
2009-03-09 15:16:49 +0000

    added option 'container_id' to import into a container
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7253 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Addressbook/Import/Csv.php
+++ tine20/Addressbook/Import/Csv.php
@@ -8,7 +8,6 @@
  * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
  * 
- * @todo        remove obsolete code
  *
 
 /**
@@ -19,36 +18,21 @@
  * @package     Addressbook
  * @subpackage  Import
  * 
- * a sample mapping:
- * --
- * array(
-    'mapping' => array(
-        'adr_one_locality'      => 'Ort',
-        'adr_one_postalcode'    => 'Plz',
-        'adr_one_street'        => 'Straße',
-        'org_name'              => 'Name1',
-        'org_unit'              => 'Name2',
-        'note'                  => array(
-            'Mitarbeiter'           => 'inLab Spezi',
-            'Anzahl Mitarbeiter'    => 'ANZMitarbeiter',
-            'Bemerkung'             => 'Bemerkung',
-        ),
-        'tel_work'              => 'TelefonZentrale',
-        'tel_cell'              => 'TelefonDurchwahl',
-        'n_family'              => 'Nachname',
-        'n_given'               => 'Vorname',
-        'n_prefix'              => array('Anrede', 'Titel'),
-        'container_id'                 => array(
-            'inLab Spezi'           => array(
-            'Name 1'                 => 92,
-            'Name 2'                 => 66,
-            'Name 3'                 => 88
-            ),
-        ),
-    ),
-    //'containerId' => 2,
- *
  */
 class Addressbook_Import_Csv extends Tinebase_Import_Csv_Abstract
 {
+    /**
+     * add some more values (container id)
+     *
+     * @return array
+     */
+    protected function _addData()
+    {
+        $result = array();
+        if (isset($this->_options['container_id'])) {
+            $result['container_id'] = $this->_options['container_id'];
+        }
+
+        return $result;
+    }
 }
