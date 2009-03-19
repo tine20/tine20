e9fb9445e771f452760425a306303d547e21146a
Cornelius Weiß <c.weiss@metaways.de>
2009-03-19 08:15:18 +0000

    implement workstation grid
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7380 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Admin/js/SambaMachineGrid.js
+++ tine20/Admin/js/SambaMachineGrid.js
@@ -10,6 +10,89 @@
  *
  */
 
-Ext.namespace('Tine.Admin.SambaMachine');
+Ext.namespace('Tine.Admin.sambaMachine');
 
+/**
+ * Samba machine 'mainScreen'
+ */
+Tine.Admin.sambaMachine.show = function() {
+    var app = Tine.Tinebase.appMgr.get('Admin');
+    if (! Tine.Admin.sambaMachine.gridPanel) {
+        Tine.Admin.sambaMachine.gridPanel = new Tine.Admin.SambaMachineGridPanel({
+            app: app
+        });
+    }
+    
+    Tine.Tinebase.MainScreen.setActiveContentPanel(Tine.Admin.sambaMachine.gridPanel, true);
+    Tine.Tinebase.MainScreen.setActiveToolbar(Tine.Admin.sambaMachine.gridPanel.actionToolbar, true);
+    Tine.Admin.sambaMachine.gridPanel.store.load();
+};
 
+/**
+ * SambaMachine grid panel
+ */
+Tine.Admin.SambaMachineGridPanel = Ext.extend(Tine.Tinebase.widgets.app.GridPanel, {
+    // model generics
+    recordClass: Tine.Admin.Model.SambaMachine,
+    recordProxy: Tine.Admin.sambaMachineBackend,
+    defaultSortInfo: {field: 'accountLoginName', direction: 'ASC'},
+    evalGrants: false,
+    gridConfig: {
+        loadMask: true,
+        autoExpandColumn: 'accountDisplayName'
+    },
+    
+    initComponent: function() {
+        this.gridConfig.columns = this.getColumns();
+        this.initFilterToolbar();
+        
+        this.plugins = this.plugins || [];
+        this.plugins.push(this.filterToolbar);
+        
+        Tine.Admin.SambaMachineGridPanel.superclass.initComponent.call(this);
+        
+        //this.action_addInNewWindow.setDisabled(! Tine.Tinebase.common.hasRight('manage', 'Admin', 'sambaMachines'));
+        //this.action_editInNewWindow.requiredGrant = 'editGrant';
+        
+    },
+    
+    /**
+     * initialises filter toolbar
+     */
+    initFilterToolbar: function() {
+        this.filterToolbar = new Tine.widgets.grid.FilterToolbar({
+            filterModels: [
+                {label: this.app.i18n._('Computer Name'),    field: 'query',       operators: ['contains']}
+                //{label: this.app.i18n._('Description'),    field: 'description', operators: ['contains']},
+             ],
+             defaultFilter: 'query',
+             filters: []
+        });
+    },    
+    
+    /**
+     * returns cm
+     * @private
+     */
+    getColumns: function(){
+        return [{
+            id: 'accountId',
+            header: this.app.i18n._("ID"),
+            width: 100,
+            sortable: true,
+            dataIndex: 'accountId'
+        },{
+            id: 'accountLoginName',
+            header: this.app.i18n._("Name"),
+            width: 350,
+            sortable: true,
+            dataIndex: 'accountLoginName'
+        },{
+            id: 'accountDisplayName',
+            header: this.app.i18n._("Description"),
+            width: 350,
+            sortable: true,
+            dataIndex: 'accountDisplayName'
+        }];
+    }
+});
--- tine20/Admin/js/SambaMachineModel.js
+++ tine20/Admin/js/SambaMachineModel.js
@@ -2,7 +2,7 @@
  * Tine 2.0
  * 
  * @package     Admin
- * @subpackage  SambaMachine
+ * @subpackage  sambaMachine
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Cornelius Weiss <c.weiss@metaways.de>
  * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
@@ -10,6 +10,41 @@
  *
  */
 
-Ext.namespace('Tine.Admin.SambaMachine');
+Ext.namespace('Tine.Admin.Model');
 
+Tine.Admin.Model.sambaMachineArray = [
+    { name: 'accountId'            },
+    { name: 'accountLoginName'     },
+    { name: 'accountLastName'      },
+    { name: 'accountFullName'      },
+    { name: 'accountDisplayName'   },
+    { name: 'accountPrimaryGroup'  },
+    { name: 'accountHomeDirectory' },
+    { name: 'accountLoginShell'    },
+    { name: 'sid'                  },
+    { name: 'primaryGroupSID'      },
+    { name: 'acctFlags'            },
+    //{ name: 'logonTime',     type: 'date', dateFormat: Date.patterns.ISO8601Long },
+    //{ name: 'logoffTime',    type: 'date', dateFormat: Date.patterns.ISO8601Long },
+    //{ name: 'kickoffTime',   type: 'date', dateFormat: Date.patterns.ISO8601Long },
+    //{ name: 'pwdLastSet',    type: 'date', dateFormat: Date.patterns.ISO8601Long },
+    //{ name: 'pwdCanChange',  type: 'date', dateFormat: Date.patterns.ISO8601Long },
+    { name: 'pwdMustChange', type: 'date', dateFormat: Date.patterns.ISO8601Long }
+];
 
+Tine.Admin.Model.SambaMachine = Tine.Tinebase.Record.create(Tine.Admin.Model.sambaMachineArray, {
+    appName: 'Admin',
+    modelName: 'SambaMachine',
+    idProperty: 'accountId',
+    titleProperty: 'accountDisplayName',
+    // ngettext('Computer', 'Computers', n);
+    recordName: 'Computer',
+    recordsName: 'Computers'
+});
+
+Tine.Admin.sambaMachineBackend = new Tine.Tinebase.widgets.app.JsonBackend({
+    appName: 'Admin',
+    modelName: 'SambaMachine',
+    recordClass: Tine.Admin.Model.SambaMachine,
+    idProperty: 'accountId'
+});
