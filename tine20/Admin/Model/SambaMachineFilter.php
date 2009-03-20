ef4130863d91e2504efa513c1d73874c9e0d1353
Cornelius Weiß <c.weiss@metaways.de>
2009-03-20 12:33:18 +0000

    forgot to commit class
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7393 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Admin/Model/SambaMachineFilter.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Admin
+ * @subpackage  Samba
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Admin Samba Machine Filter
+ *
+ * @package     Admin
+ * @subpackage  Samba
+ */
+class Admin_Model_SambaMachineFilter extends Tinebase_Model_Filter_FilterGroup
+{
+    /**
+     * application the record belongs to
+     *
+     * @var string
+     */
+    protected $_applicationName = 'Admin';
+    
+    protected $_filterModel = array(
+        'query'                => array(
+            'filter' => 'Tinebase_Model_Filter_Query', 
+            'options' => array(
+                'fields' => array('accountLoginName')
+            )
+        ),
+    );
+}
