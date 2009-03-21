4f96823398ad7521430d33e672e568384b3c9c2d
Lars Kneschke <l.kneschke@metaways.de>
2009-03-21 21:45:06 +0000

    store device information to database now
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7399 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/ActiveSync/Command/Settings.php
+++ tine20/ActiveSync/Command/Settings.php
@@ -22,7 +22,15 @@
  
 class ActiveSync_Command_Settings extends ActiveSync_Command_Wbxml 
 {
-    const STATUS_NO_CHANGES_FOUND = 1;
+    const STATUS_SUCCESS                = 1;
+    const STATUS_PROTOCOL_ERROR         = 2;
+    const STATUS_ACCESS_DENIED          = 3;
+    const STATUS_SERVICE_UNAVAILABLE    = 4;
+    const STATUS_INVALID_ARGUMENTS      = 5;
+    const STATUS_CONFLICTING_ARGUMENTS  = 6;
+    
+    const STATUS_DEVICEPASSWORD_TO_LONG = 5;
+    const STATUS_DEVICEPASSWORD_PASSWORD_RECOVERY_DISABLED = 7;
     
     /**
      * Enter description here...
@@ -33,6 +41,9 @@ class ActiveSync_Command_Settings extends ActiveSync_Command_Wbxml
 
     protected $_defaultNameSpace = 'uri:Settings';
     protected $_documentElement  = 'Settings';
+    
+    protected $_deviceInformationSet     = false;
+    protected $_userInformationRequested = false;
     
     
     /**
@@ -46,8 +57,25 @@ class ActiveSync_Command_Settings extends ActiveSync_Command_Wbxml
     {
         $controller = ActiveSync_Controller::getInstance();
         
-        if($this->_inputDom instanceof DOMDocument) {
-        }        
+        $xml = simplexml_import_dom($this->_inputDom);
+        
+        if(isset($xml->DeviceInformation->Set)) {
+            $this->_deviceInformationSet = true;
+            
+            $this->_device->model           = (string)$xml->DeviceInformation->Set->Model;
+            $this->_device->imei            = (string)$xml->DeviceInformation->Set->IMEI;
+            $this->_device->friendlyname    = (string)$xml->DeviceInformation->Set->FriendlyName;
+            $this->_device->os              = (string)$xml->DeviceInformation->Set->OS;
+            $this->_device->oslanguage      = (string)$xml->DeviceInformation->Set->OSLanguage;
+            $this->_device->phonenumber     = (string)$xml->DeviceInformation->Set->PhoneNumber;
+            
+            $this->_device = $controller->updateDevice($this->_device);
+        }
+        
+        if(isset($xml->UserInformation->Get)) {
+            $this->_userInformationRequested = true;
+        }
+        
     }    
     
     /**
@@ -55,6 +83,28 @@ class ActiveSync_Command_Settings extends ActiveSync_Command_Wbxml
      */
     public function getResponse()
     {
+        $settings = $this->_outputDom->documentElement;
+        
+        $settings->appendChild($this->_outputDom->createElementNS('uri:Settings', 'Status', self::STATUS_SUCCESS));
+        
+        if($this->_deviceInformationSet === true) {
+            $deviceInformation = $settings->appendChild($this->_outputDom->createElementNS('uri:Settings', 'DeviceInformation'));
+            $set = $deviceInformation->appendChild($this->_outputDom->createElementNS('uri:Settings', 'Set'));
+            $set->appendChild($this->_outputDom->createElementNS('uri:Settings', 'Status', self::STATUS_SUCCESS));
+        }
+        
+        if($this->_userInformationRequested === true) {
+            $smtpAddresses = array();
+            
+            $userInformation = $settings->appendChild($this->_outputDom->createElementNS('uri:Settings', 'UserInformation'));
+            $userInformation->appendChild($this->_outputDom->createElementNS('uri:Settings', 'Status', self::STATUS_SUCCESS));
+            $get = $userInformation->appendChild($this->_outputDom->createElementNS('uri:Settings', 'Get'));
+            $emailAddresses = $get->appendChild($this->_outputDom->createElementNS('uri:Settings', 'EmailAddresses'));
+            foreach($smtpAddresses as $smtpAddress) {
+                $emailAddresses->appendChild($this->_outputDom->createElementNS('uri:Settings', 'SMTPAddress', $smtpAddress));
+            }
+        }
+        
         parent::getResponse();
     }
 }
