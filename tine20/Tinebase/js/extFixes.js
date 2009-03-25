902740c9e5f663cbcde53a92c593f75464b0d17c
Cornelius Weiß <c.weiss@metaways.de>
2009-03-25 10:14:43 +0000

    fix datepicker width in ie8
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7435 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/extFixes.js
+++ tine20/Tinebase/js/extFixes.js
@@ -1,13 +1,24 @@
 /**
- * fix twin trigger field layout in IE8
+ * IE8 fixes
  */
-Ext.form.TwinTriggerField.prototype.onRender = function(ct, position) {
-    Ext.form.TwinTriggerField.superclass.onRender.call(this, ct, position);
-    if (Ext.isIE8) {
+if (Ext.isIE8) {
+    /**
+     * fix twin trigger field layout
+     */
+    Ext.form.TwinTriggerField.prototype.onRender = function(ct, position) {
+        Ext.form.TwinTriggerField.superclass.onRender.call(this, ct, position);
         this.el.setTop.defer(10, this.el, ['1px']);
     }
+    
+    /**
+     * fix datepicker width
+     */
+    Ext.DatePicker.prototype.onRender = Ext.DatePicker.prototype.onRender.createSequence(function(ct, position) {
+        var wrap = ct.up('div[class="x-layer x-menu x-menu-plain x-date-menu"]');
+        var orgWidth = this.el.getWidth();
+        wrap.setWidth.defer(10, wrap, [orgWidth]);
+    });
 }
-
 
 /**
  * fix timezone handling for date picker
