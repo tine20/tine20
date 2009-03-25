3ba585784490b8dab25d53b71a09390501e5e60c
Cornelius Weiß <c.weiss@metaways.de>
2009-03-25 18:21:40 +0000

    EventFilter
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7446 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Calendar/Model/EventFilter.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Calendar
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+
+class Calendar_Model_EventFilter extends Tinebase_Model_Filter_FilterGroup 
+{
+    /**
+     * @var string application of this filter group
+     */
+    protected $_applicationName = 'Calendar';
+    
+    /**
+     * @var string class name of this filter group
+     *      this is needed to overcome the static late binding
+     *      limitation in php < 5.3
+     */
+    protected $_className = 'Calendar_Model_EventFilter';
+    
+    /**
+     * @var array filter model fieldName => definition
+     */
+    protected $_filterModel = array(
+        'id'             => array('filter' => 'Tinebase_Model_Filter_Id'),
+        'container_id'   => array('filter' => 'Tinebase_Model_Filter_Container', 'options' => array('applicationName' => 'Calendar')),
+        'query'          => array('filter' => 'Tinebase_Model_Filter_Query', 'options' => array('fields' => array('summary', 'description'))),
+        'dtstart'        => array('filter' => 'Tinebase_Model_Filter_DateTime'),
+        'dtend'          => array('filter' => 'Tinebase_Model_Filter_DateTime'),
+        //'class_id'       => array('filter' => 'Tinebase_Model_Filter_Text'),
+        //'status'         => array('filter' => 'Tinebase_Model_Filter_Text'),
+        'tag'            => array('filter' => 'Tinebase_Model_Filter_Tag'),
+    );
+}
