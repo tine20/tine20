bfee23cca37a73afe71ae411c8d12954e36ad45b
Cornelius Weiß <c.weiss@metaways.de>
2009-03-30 22:24:29 +0000

    deactivate direct / recurBase fn's
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7491 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Calendar/Backend/Sql.php
+++ tine20/Calendar/Backend/Sql.php
@@ -67,12 +67,13 @@ class Calendar_Backend_Sql extends Tinebase_Application_Backend_Sql_Abstract
      * @param Tinebase_Model_Pagination $_pagination
      * @param boolean $_onlyIds
      * @return Tinebase_Record_RecordSet|array
-     */
+     *
     public function searchDirectEvents(Tinebase_Model_Filter_FilterGroup $_filter = NULL, Tinebase_Model_Pagination $_pagination = NULL, $_onlyIds = FALSE)
     {
         Calendar_Model_PeriodFilter::setType(Calendar_Model_PeriodFilter::TYPE_DIRECT);
         return parent::search($_filter, $_pagination, $_onlyIds);
     }
+    */
     
     /**
      * Search for base events of recuring events matching given filter
@@ -84,12 +85,13 @@ class Calendar_Backend_Sql extends Tinebase_Application_Backend_Sql_Abstract
      * @param Tinebase_Model_Pagination $_pagination
      * @param boolean $_onlyIds
      * @return Tinebase_Record_RecordSet|array
-     */
+     *
     public function searchRecurBaseEvents(Tinebase_Model_Filter_FilterGroup $_filter = NULL, Tinebase_Model_Pagination $_pagination = NULL, $_onlyIds = FALSE)
     {
         Calendar_Model_PeriodFilter::setType(Calendar_Model_PeriodFilter::TYPE_RECURBASE);
         return parent::search($_filter, $_pagination, $_onlyIds);
     }
+    */
     
     /**
      * sets rrule until field in event model
