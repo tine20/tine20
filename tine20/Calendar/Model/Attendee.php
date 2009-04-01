1b0dbc45ff26ab2a130c25a35454c74845fc1c9e
Cornelius Weiß <c.weiss@metaways.de>
2009-04-01 08:03:54 +0000

    model of attendee
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7527 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Calendar/Model/Attendee.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Sql Calendar 
+ * 
+ * @package     Calendar
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ */
+
+/**
+ * Model of an attendee
+ *
+ * @package Calendar
+ */
+class Calendar_Model_Attendee extends Tinebase_Record_Abstract
+{
+    /**
+     * supported user types
+     */
+    const USERTYPE_USER        = 'user';
+    const USERTYPE_GROUP       = 'group';
+    const USERTYPE_GROUPMEMBER = 'groupmember';
+    const USERTYPE_RESOURCE    = 'resource';
+    
+    /**
+     * supported roles
+     */
+    const ROLE_REQUIRED        = 'REQ';
+    const ROLE_OPTIONAL        = 'OPT';
+    
+    /**
+     * supported status
+     */
+    const STATUS_NEEDSACTION   = 'NEEDS-ACTION';
+    const STATUS_ACCEPTED      = 'ACCEPTED';
+    const STATUS_DECLINED      = 'DECLINED';
+    const STATUS_TENTATIVE     = 'TENTATIVE';
+    
+    /**
+     * key in $_validators/$_properties array for the filed which 
+     * represents the identifier
+     * 
+     * @var string
+     */
+    protected $_identifier = 'id';
+    
+    /**
+     * application the record belongs to
+     *
+     * @var string
+     */
+    protected $_application = 'Calendar';
+    
+    /**
+     * validators
+     *
+     * @var array
+     */
+    protected $_validators = array(
+        // tine record fields
+        'id'                   => array('allowEmpty' => true,  'Alnum'),
+        /*
+        'created_by'           => array('allowEmpty' => true,  'Int'  ),
+        'creation_time'        => array('allowEmpty' => true          ),
+        'last_modified_by'     => array('allowEmpty' => true          ),
+        'last_modified_time'   => array('allowEmpty' => true          ),
+        'is_deleted'           => array('allowEmpty' => true          ),
+        'deleted_time'         => array('allowEmpty' => true          ),
+        'deleted_by'           => array('allowEmpty' => true          ),
+        'seq'                  => array('allowEmpty' => true,  'Int'  ),
+        */
+        
+        'cal_event_id'         => array('allowEmpty' => false, 'Alnum'),
+        'user_id'              => array('allowEmpty' => false, 'Int'  ),
+        'user_type'            => array('allowEmpty' => true,  'InArray' => array(self::USERTYPE_USER, self::USERTYPE_GROUP, self::USERTYPE_GROUPMEMBER, self::USERTYPE_RESOURCE)),
+        'role'                 => array('allowEmpty' => true,  'InArray' => array(self::ROLE_OPTIONAL, self::ROLE_REQUIRED)),
+        'status'               => array('allowEmpty' => true,  'InArray' => array(self::STATUS_NEEDSACTION, self::STATUS_TENTATIVE, self::STATUS_ACCEPTED, self::STATUS_DECLINED)),
+        'quantity'             => array('allowEmpty' => true, 'Int'   ),
+        'status_authkey'       => array('allowEmpty' => true, 'Alnum' ),
+    );
+    
+    /**
+     * datetime fields
+     *
+     * @var array
+     */
+    protected $_datetimeFields = array();
+    
+}
