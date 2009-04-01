1873d24baed536515ddccfa44bb584153218dcf2
Cornelius Weiß <c.weiss@metaways.de>
2009-04-01 08:50:04 +0000

    adopt json api to nonexistant account return
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7528 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Admin/Frontend/Json.php
+++ tine20/Admin/Frontend/Json.php
@@ -104,12 +104,8 @@ class Admin_Frontend_Json extends Tinebase_Application_Frontend_Json_Abstract
         }
         
         foreach ($result['results'] as $key => $value) {
-            try {
+            if (! empty($value['account_id'])) {
                 $result['results'][$key]['accountObject'] = Admin_Controller_User::getInstance()->get($value['account_id'])->toArray();
-            } catch (Tinebase_Exception_NotFound $e) {
-                // account not found
-                // do nothing so far
-                Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' account ' . $value['account_id'] .' not found');
             }
         }
         
