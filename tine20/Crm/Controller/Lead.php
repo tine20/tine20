9acc1fe0b06bc6fecc0115270bb9d215dbb2a44c
Philipp Schüle <p.schuele@metaways.de>
2009-04-14 12:14:10 +0000

    typo
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7696 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Crm/Controller/Lead.php
+++ tine20/Crm/Controller/Lead.php
@@ -255,7 +255,7 @@ class Crm_Controller_Lead extends Tinebase_Controller_Record_Abstract
             $pdfGenerator->generateLeadPdf($_lead);
             $pdfOutput = $pdfGenerator->render();
         } catch ( Zend_Pdf_Exception $e ) {
-            Tinebacse_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' error creating pdf: ' . $e->__toString());
+            Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' error creating pdf: ' . $e->__toString());
             $pdfOutput = NULL;
         }
                 
