4dc9eb69ed80ddb6093213fcbae77f2180ef67e9
Lars Kneschke <l.kneschke@metaways.de>
2009-04-16 11:13:37 +0000

    new models for folderstate and folderstatefilter
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7731 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/ActiveSync/Model/FolderState.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     ActiveSync
+ * @license     http://www.tine20.org/licenses/agpl-nonus.txt AGPL Version 1 (Non-US)
+ *              NOTE: According to sec. 8 of the AFFERO GENERAL PUBLIC LICENSE (AGPL), 
+ *              Version 1, the distribution of the Tine 2.0 ActiveSync module in or to the 
+ *              United States of America is excluded from the scope of this license.
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2008-2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ * 
+ */
+
+/**
+ * class to hold FolderState data
+ * 
+ * @package     ActiveSync
+ */
+class ActiveSync_Model_FolderState extends Tinebase_Record_Abstract
+{  
+    /**
+     * key in $_validators/$_properties array for the filed which 
+     * represents the identifier
+     * 
+     * @var string
+     */    
+    protected $_identifier = 'id';    
+    
+    /**
+     * application the record belongs to
+     *
+     * @var string
+     */
+    protected $_application = 'ActiveSync';
+
+    /**
+     * list of zend validator
+     * 
+     * this validators get used when validating user generated content with Zend_Input_Filter
+     *
+     * @var array
+     */
+    protected $_validators = array(
+        'id'                => array(Zend_Filter_Input::ALLOW_EMPTY => true, Zend_Filter_Input::DEFAULT_VALUE => NULL),
+        'device_id'         => array(Zend_Filter_Input::ALLOW_EMPTY => false, 'presence'=>'required'),
+        'class'             => array(Zend_Filter_Input::ALLOW_EMPTY => false, 'presence'=>'required'),
+        'folderid'          => array(Zend_Filter_Input::ALLOW_EMPTY => false, 'presence'=>'required'),
+        'creation_time'     => array(Zend_Filter_Input::ALLOW_EMPTY => false, 'presence'=>'required'),
+    );
+    
+    /**
+     * name of fields containing datetime or an array of datetime information
+     *
+     * @var array list of datetime fields
+     */    
+    protected $_datetimeFields = array(
+        'creation_time'
+    );    
+    
+}
---
+++ tine20/ActiveSync/Model/FolderStateFilter.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     ActiveSync
+ * @license     http://www.tine20.org/licenses/agpl-nonus.txt AGPL Version 1 (Non-US)
+ *              NOTE: According to sec. 8 of the AFFERO GENERAL PUBLIC LICENSE (AGPL), 
+ *              Version 1, the distribution of the Tine 2.0 ActiveSync module in or to the 
+ *              United States of America is excluded from the scope of this license.
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+
+/**
+ * folder state filter class
+ * @package     ActiveSync
+ */
+class ActiveSync_Model_FolderStateFilter extends Tinebase_Model_Filter_FilterGroup
+{    
+    /**
+     * @var string class name of this filter group
+     *      this is needed to overcome the static late binding
+     *      limitation in php < 5.3
+     */
+    protected $_className = 'ActiveSync_Model_FolderStateFilter';
+    
+    /**
+     * application the record belongs to
+     *
+     * @var string
+     */
+    protected $_application = 'ActiveSync';
+
+    /**
+     * @var array filter model fieldName => definition
+     */
+    protected $_filterModel = array(
+        'id'                   => array('filter' => 'Tinebase_Model_Filter_Id'),
+        'device_id'            => array('filter' => 'Tinebase_Model_Filter_Id'),
+        'class'                => array('filter' => 'Tinebase_Model_Filter_Text'),
+        'folderid'             => array('filter' => 'Tinebase_Model_Filter_Id')
+    );    
+}
