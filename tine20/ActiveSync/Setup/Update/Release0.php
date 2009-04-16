94c4252997056541192d0581f4a188ddff98f7e5
Lars Kneschke <l.kneschke@metaways.de>
2009-04-16 11:11:40 +0000

    add table to keep track of folders sent to client
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7729 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/ActiveSync/Setup/Update/Release0.php
+++ tine20/ActiveSync/Setup/Update/Release0.php
@@ -186,4 +186,88 @@ class ActiveSync_Setup_Update_Release0 extends Setup_Update_Abstract
         
         $this->setApplicationVersion('ActiveSync', '0.4');
     }
+    
+    /**
+     * add table for folders sent to client(foldersync command)
+     * 
+     */    
+    public function update_4()
+    {
+        $tableDefinition = ('
+            <table>
+                <name>acsync_folder</name>
+                <engine>InnoDB</engine>
+                <charset>utf8</charset>
+                <version>1</version>
+                <declaration>
+                    <field>
+                        <name>id</name>
+                        <type>text</type>
+                        <length>40</length>
+                        <notnull>true</notnull>
+                    </field>
+                    <field>
+                        <name>device_id</name>
+                        <type>text</type>
+                        <length>64</length>
+                        <notnull>false</notnull>
+                    </field>
+                    <field>
+                        <name>class</name>
+                        <type>text</type>
+                        <length>64</length>
+                        <notnull>false</notnull>
+                    </field>
+                    <field>
+                        <name>folderid</name>
+                        <type>text</type>
+                        <length>254</length>
+                        <notnull>false</notnull>
+                    </field>
+                    <field>
+                        <name>creation_time</name>
+                        <type>datetime</type>
+                    </field> 
+                    <index>
+                        <name>id</name>
+                        <primary>true</primary>
+                        <field>
+                            <name>id</name>
+                        </field>
+                    </index>
+                    <index>
+                        <name>device_id--class--folderid</name>
+                        <unique>true</unique>
+                        <field>
+                            <name>device_id</name>
+                        </field>
+                        <field>
+                            <name>class</name>
+                        </field>
+                        <field>
+                            <name>folderid</name>
+                        </field>
+                    </index>
+                    <index>
+                        <name>acsync_folder::device_id--acsync_device::id</name>
+                        <field>
+                            <name>device_id</name>
+                        </field>
+                        <foreign>true</foreign>
+                        <reference>
+                            <table>acsync_device</table>
+                            <field>id</field>
+                        </reference>
+                        <ondelete>cascade</ondelete>
+                        <onupdate>cascade</onupdate>
+                    </index>   
+                </declaration>
+            </table>
+        ');
+    
+        $table = Setup_Backend_Schema_Table_Factory::factory('Xml', $tableDefinition); 
+        $this->_backend->createTable($table);
+                
+        $this->setApplicationVersion('ActiveSync', '0.5');
+    }    
 }
