ab024f2e7147a519383e249c78c3072f73e582b1
Philipp Schüle <p.schuele@metaways.de>
2009-04-17 11:09:09 +0000

    overwrite session save path to make cli work even if user has no rights for the 'normal' session save path
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7762 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Server/Cli.php
+++ tine20/Tinebase/Server/Cli.php
@@ -5,10 +5,11 @@
  * @package     Tinebase
  * @subpackage  Server
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
- * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2007-2009 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Philipp Schuele <p.schuele@metaways.de>
  * @version     $Id$
  * 
+ * @todo remove cli session/cache path (add http://aidanlister.com/2004/04/recursively-deleting-a-folder-in-php/ to helpers?)
  */
 
 /**
@@ -19,6 +20,49 @@
  */
 class Tinebase_Server_Cli extends Tinebase_Server_Abstract
 {
+    /**
+     * cli session path
+     *
+     */
+    const CLI_SESSION_PATH = '/tmp/tine20_cli_session';
+    
+    /**
+     * init tine framework
+     *
+     */
+    protected function _initFramework()
+    {
+        $this->_setupCliConfig();
+        Tinebase_Core::setupServerTimezone();
+        Tinebase_Core::setupLogger();
+        Tinebase_Core::setupSession();
+        Tinebase_Core::set('locale', new Zend_Locale('en_US'));
+        Tinebase_Core::set('userTimeZone', 'UTC');
+        Tinebase_Core::setupMailer();
+        Tinebase_Core::setupDatabaseConnection();
+        Tinebase_Core::setupCache();
+        Tinebase_Core::setupUserTimezone();
+        Tinebase_Core::setupUserLocale();
+    }
+    
+    /**
+     * initializes the config
+     * - overwrite session_save_path
+     *
+     */
+    public function _setupCliConfig()
+    {
+        $configData = include('config.inc.php');
+        if($configData === false) {
+            die ('central configuration file config.inc.php not found in includepath: ' . get_include_path());
+        }
+        $configData['session.save_path'] = self::CLI_SESSION_PATH;
+        
+        $config = new Zend_Config($configData);
+        
+        Tinebase_Core::set(Tinebase_Core::CONFIG, $config);  
+    }
+    
     /**
      * handler for command line scripts
      * 
@@ -34,6 +78,10 @@ class Tinebase_Server_Cli extends Tinebase_Server_Abstract
 
         $tinebaseServer = new Tinebase_Frontend_Cli();
         $tinebaseServer->authenticate($opts->username, $opts->password);
-        return $tinebaseServer->handle($opts);        
+        $result = $tinebaseServer->handle($opts);
+
+        //@todo remove cli session path
+        
+        return $result;
     }
 }
