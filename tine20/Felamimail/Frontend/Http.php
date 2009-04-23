45be957bbaa6ad62f893ca9278df8569c34ae9ed
Philipp Schüle <p.schuele@metaways.de>
2009-04-23 12:37:58 +0000

    include needed js files
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7840 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/Frontend/Http.php
+++ tine20/Felamimail/Frontend/Http.php
@@ -7,11 +7,32 @@
  * @package     Felamimail
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Lars Kneschke <l.kneschke@metaways.de>
- * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2007-2009 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
  *
  */
 class Felamimail_Frontend_Http extends Tinebase_Frontend_Http_Abstract
 {
+    /**
+     * app name
+     *
+     * @var string
+     */
     protected $_applicationName = 'Felamimail';
+    
+    /**
+     * Returns all JS files which must be included for this app
+     *
+     * @return array Array of filenames
+     */
+    public function getJsFilesToInclude()
+    {
+        return array(
+            'Felamimail/js/Models.js',
+            'Felamimail/js/Felamimail.js',
+            'Felamimail/js/MainScreen.js',
+            'Felamimail/js/FelamimailGridPanel.js',
+            'Felamimail/js/FelamimailEditDialog.js',
+        );
+    }
 }
