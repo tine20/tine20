6c9066938aa723aa3013ceb1b4f416b4942872ce
Philipp Schüle <p.schuele@metaways.de>
2009-04-29 15:36:34 +0000

    made cache updating work for new messages
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@7893 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/Controller/Cache.php
+++ tine20/Felamimail/Controller/Cache.php
@@ -86,66 +86,77 @@ class Felamimail_Controller_Cache extends Felamimail_Controller_Abstract
      *
      * @param string $_folderId
      * 
-     * @todo    add mechanism to update cache without the need to clear it first?
-     *          -> only fetch messages with uid > nextuid?
-     * @todo    parse received to get Zend_Date
+     * @todo    check if mails have been deleted (compare counts)
+     * @todo    parse received data to get Zend_Date
      * @todo    get message size
      * @todo    make encoding changeable via prefs or backend settings
-     * @todo    save nextuid/validity in folder
      */
     public function update($_folderId)
     {
         $folder = $this->_folderBackend->get($_folderId);
-        
-        //if ($this->_cacheController->updateRequired($folder)) {
-            
-        //}
-        
-        // clear cache
-        $this->clear($_folderId);
-        
-        // get all (?) message headers from folder and save them in cache db
+
         $backend = $this->_getBackend($folder->backend_id);
-        $backend->selectFolder($folder->globalname);
-        $uids = $backend->getUid(1, $backend->countMessages());
-        $messages = $backend->getSummary($uids);
+        $backendFolderValues = $backend->selectFolder($folder->globalname);
         
-        foreach ($messages as $uid => $message) {
-            $cachedMessage = new Felamimail_Model_Message(array(
-                'messageuid'    => $uid,
-                'subject'       => $message->subject,
-                'from'          => @iconv('ISO_8859-1', $this->_encoding, $message->from),
-                'to'            => $message->to,
-                'sent'          => new Zend_Date($message->date),
-                'folder_id'     => $_folderId,
-                'timestamp'     => Zend_Date::now()
-                //'received'      => $message->received,
-                //'size'          => $message->size
-            ));
+        //print_r($backendFolderValues);
+        
+        // check uidnext
+        if ($folder->uidnext < $backendFolderValues['uidnext']) {
+            // get missing mails
+            if (empty($folder->uidnext)) {
+                $uids = $backend->getUid(1, $backend->countMessages());
+                $messages = $backend->getSummary($uids);
+                $folder->uidvalidity = $backendFolderValues['uidvalidity'];
+            } else {
+                // only get messages with $backendFolderValues['uidnext'] > uid > $folder->uidnext
+                $messages = $backend->getSummary($folder->uidnext, $backendFolderValues['uidnext']);
+            }
+
+            Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ . 
+                ' Trying to add ' . count($messages) . ' new messages to cache. Old uidnext: ' . $folder->uidnext .
+                ' New uidnext: ' . $backendFolderValues['uidnext']
+            );
             
-            $this->_messageCacheBackend->create($cachedMessage);
+            // get all (?) message headers from folder and save them in cache db
+            foreach ($messages as $uid => $message) {
+                
+                try {
+                    $cachedMessage = new Felamimail_Model_Message(array(
+                        'messageuid'    => $uid,
+                        'subject'       => @iconv('ISO_8859-1', $this->_encoding, $message->subject),
+                        'from'          => @iconv('ISO_8859-1', $this->_encoding, $message->from),
+                        'to'            => @iconv('ISO_8859-1', $this->_encoding, $message->to),
+                        'sent'          => new Zend_Date($message->date),
+                        'folder_id'     => $_folderId,
+                        'timestamp'     => Zend_Date::now()
+                        //'received'      => $message->received,
+                        //'size'          => $message->size
+                    ));
+                    
+                    $this->_messageCacheBackend->create($cachedMessage);
+                } catch (Zend_Mail_Exception $zme) {
+                    Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ . 
+                        ' Could not parse message ' . $uid . ' in folder ' . $folder->globalname .
+                        '. Error: ' . $zme->getMessage()
+                    );
+                }
+            }
+        } else {
+            Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ . ' No need to get new messages, cache is up to date.');
         }
         
-        //-- save nextuid/validity in folder 
-    }
-    
-    /**
-     * check if cache update/rebuild is required for this folder
-     *
-     * @param string $_folderId
-     * @return boolean
-     * 
-     * @todo    add nextuid/validity check
-     */
-    public function updateRequired($_folderId)
-    {
-        // try to get folder from db
-        $folder = $this->_folderBackend->get($_folderId);
+        if ($folder->uidvalidity != $backendFolderValues['uidvalidity']) {
+            throw new Tinebase_Exception_UnexpectedValue(
+                'Got non matching uidvalidity value: ' . $backendFolderValues['uidvalidity'] .'. Expected: ' . $folder->uidvalidity);
+        }
         
-        //-- check backend and folder nextuid/validity flags
+        // save nextuid/validity in folder
+        if ($folder->uidnext != $backendFolderValues['uidnext']) {
+            $folder->uidnext = $backendFolderValues['uidnext'];
+            $this->_folderBackend->update($folder);
+        }
         
-        $result = TRUE;
-        return $result;
+        //-- check if mails have been deleted (compare counts)
     }
     
     /**
@@ -156,5 +167,11 @@ class Felamimail_Controller_Cache extends Felamimail_Controller_Abstract
     public function clear($_folderId)
     {
         $this->_messageCacheBackend->deleteByFolderId($_folderId);
+        
+        // set folder uidnext + uidvalidity to 0
+        $folder = $this->_folderBackend->get($_folderId);
+        $folder->uidnext = 0;
+        $folder->uidvalidity = 0;
+        $this->_folderBackend->update($folder);
     }
 }
