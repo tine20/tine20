d585faa9c4f52361add92c7cc1f6b2b75573097c
Cornelius Weiß <c.weiss@metaways.de>
2009-05-25 15:23:57 +0000

    start with week view
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@8204 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Calendar/js/MainScreenCenterPanel.js
+++ tine20/Calendar/js/MainScreenCenterPanel.js
@@ -13,7 +13,7 @@ Tine.Calendar.MainScreenCenterPanel = Ext.extend(Ext.Panel, {
     /**
      * @cfg {String} activeView
      */
-    activeView: 'month',
+    activeView: 'week',
     
     startDate: new Date().clearTime(),
     
