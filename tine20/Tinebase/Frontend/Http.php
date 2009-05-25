37d1380a4e7e6a1506edb24e858ee2dd89c324fb
Cornelius Weiß <c.weiss@metaways.de>
2009-05-25 15:20:11 +0000

    include mem backend
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@8203 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Frontend/Http.php
+++ tine20/Tinebase/Frontend/Http.php
@@ -136,6 +136,7 @@ class Tinebase_Frontend_Http extends Tinebase_Frontend_Http_Abstract
             'Tinebase/js/data/sync/Ping.js',
             'Tinebase/js/data/Record.js',
             'Tinebase/js/data/AbstractBackend.js',
+            'Tinebase/js/data/MemoryBackend.js',
             'Tinebase/js/ExceptionDialog.js',
             'Tinebase/js/Container.js',
             'Tinebase/js/Models.js',
