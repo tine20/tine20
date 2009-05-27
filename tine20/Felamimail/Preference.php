72e83828b9df144d876d09558e18cb21c1815a07
Philipp Schüle <p.schuele@metaways.de>
2009-05-27 11:56:26 +0000

    first steps for felamimail preferences
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@8250 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Felamimail/Preference.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Felamimail
+ * @subpackage  Backend
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id:Preference.php 7161 2009-03-04 14:27:07Z p.schuele@metaways.de $
+ * 
+ * @todo        add default account settings
+ * @todo        add preference 'user email account'
+ */
+
+
+/**
+ * backend for Felamimail preferences
+ *
+ * @package     Felamimail
+ * @subpackage  Backend
+ */
+class Felamimail_Preference extends Tinebase_Preference_Abstract
+{
+    /**************************** application preferences/settings *****************/
+    
+    /**
+     * timezone pref const
+     *
+     */
+    const USERACCOUNT = 'userEmailAccount';
+
+    /**
+     * application
+     *
+     * @var string
+     */
+    protected $_application = 'Felamimail';    
+        
+    /**************************** public functions *********************************/
+    
+    /**
+     * get all possible application prefs
+     *
+     * @return  array   all application prefs
+     */
+    public function getAllApplicationPreferences()
+    {
+        $allPrefs = array(
+            self::USERACCOUNT
+        );
+            
+        return $allPrefs;
+    }
+    
+    /**
+     * get translated right descriptions
+     * 
+     * @return  array with translated descriptions for this applications preferences
+     */
+    public function getTranslatedPreferences()
+    {
+        $translate = Tinebase_Translation::getTranslation($this->_application);
+
+        $prefDescriptions = array(
+            self::USERACCOUNT  => array(
+                'label'         => $translate->_('User Email Account'),
+                'description'   => $translate->_('Use user credentials for IMAP email account.'),
+            ),
+        );
+        
+        return $prefDescriptions;
+    }
+}
