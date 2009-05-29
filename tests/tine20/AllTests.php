3ab6d01614d15bb08830a5695da598710bffac19
Philipp Schüle <p.schuele@metaways.de>
2009-05-29 14:29:31 +0000

    only run felamimail tests if default account from config is activated
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@8294 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/tine20/AllTests.php
+++ tests/tine20/AllTests.php
@@ -42,7 +42,7 @@ class AllTests
         $suite->addTest(Calendar_AllTests::suite());
         
         // only call Felamimail tests if imap is configured in config.inc.php
-        if (Tinebase_Core::getConfig()->imap) {
+        if (Tinebase_Core::getConfig()->imap && Tinebase_Core::getConfig()->imap->useAsDefault) {
             $suite->addTest(Felamimail_AllTests::suite());
         }
         
