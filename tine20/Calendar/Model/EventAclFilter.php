fb80a970ff4a7f7d8919c7d0c64d12ff6cfabc8c
Cornelius Weiß <c.weiss@metaways.de>
2009-06-11 14:34:18 +0000

    find a new pattern for multiple container filters
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@8517 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Calendar/Model/EventAclFilter.php
+++ tine20/Calendar/Model/EventAclFilter.php
@@ -7,54 +7,96 @@
  * @author      Cornelius Weiss <c.weiss@metaways.de>
  * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
- *
  */
 
 /**
  * Calendar Acl Filter
  * 
- * Manages implicit grants of participants and organizers
+ * Manages calnedar grant for search actions
+ * 
+ * 
+ * Assuring event grants is a two stage process for search operations.
+ *  1. limiting the query (mixture of grants and filter)
+ *  2. transform event set (all events user has only free/busy grant for need to be cleaned)
+ * 
+ * NOTE: The effective grants of the events get dynamically appended to the
+ *       event rows in SQL (at the moment by the sql backend class.)
+ *       As such there is no need to compute the effective grants in stage 2
+ * 
+ * NOTE: If the required grant is other than GRANT_READ, we skip all free/busy 
+ *       grants. With this, also stage 2 is obsolete!
  * 
  * @package Calendar
  */
-class Calendar_Model_EventAclFilter extends Tinebase_Model_Filter_Container
+class Calendar_Model_EventAclFilter extends Tinebase_Model_Filter_FilterGroup implements Tinebase_Model_Filter_AclFilter
 {
     /**
-     * appeds sql to given select statement
+     * @var string application of this filter group
+     */
+    protected $_applicationName = 'Calendar';
+    
+    /**
+     * @var array filter model fieldName => definition
+     */
+    protected $_filterModel = array(
+        'container_id'   => array('filter' => 'Tinebase_Model_Container', 'options' => array('applicationName' => 'Calendar')),
+    );
+    
+    /**
+     * @var array one of theese grants must be met
+     */
+    protected $_requiredGrants = array(
+        Tinebase_Model_Container::GRANT_READ
+    );
+    
+    /**
+     * is acl filter resolved?
      *
+     * @var boolean
+     */
+    protected $_isResolved = FALSE;
+    
+    /**
+     * sets the grants this filter needs to assure
+     *
+     * @param array $_grants
+     */
+    public function setRequiredGrants(array $_grants)
+    {
+        $this->_requiredGrants = $_grants;
+        $this->_isResolved = FALSE;
+    }
+    
+    /**
+     * appends custom filters to a given select object
+     * 
      * @param  Zend_Db_Select                    $_select
      * @param  Tinebase_Backend_Sql_Abstract     $_backend
-     * @throws Tinebase_Exception_NotFound
+     * @return void
      */
     public function appendFilterSql($_select, $_backend)
     {
         
-//        parent::appendFilterSql($_select, $_backend);
-//        
-//        $db = $_backend->getAdapter();
-//        $currentUserId = Tinebase_Core::getUser()->getId();
-//        
-//        // organizer gets all grants implicitly 
-//        $_select->orWhere($db->quoteIdentifier('organizer') . ' = ?', $currentUserId, Zend_Db::INT_TYPE);
-//        
-//        
-//        if (! in_array(Tinebase_Model_Container::GRANT_EDIT, $this->_requiredGrants)) {
-//            
-//            // participants get read grant implicitly
-//            $_select->joinLeft(
-//                /* table  */ array('attendee' => $_backend->getTablePrefix() . 'cal_attendee'), 
-//                /* on     */ $db->quoteIdentifier('attendee.cal_event_id') . ' = ' . $db->quoteIdentifier($_backend->getTableName() . '.id') . ' AND (' . 
-//                                '( ' . $db->quoteInto($db->quoteIdentifier('attendee.user_type') . ' = ? ',  Calendar_Model_Attender::USERTYPE_USER) . ' AND ' . $db->quoteInto($db->quoteIdentifier('attendee.user_id') . ' = ? ',  $currentUserId, Zend_Db::INT_TYPE) . ' ) OR ' .
-//                                '( ' . $db->quoteInto($db->quoteIdentifier('attendee.user_type') . ' = ? ',  Calendar_Model_Attender::USERTYPE_GROUP) . ' AND ' . $db->quoteInto($db->quoteIdentifier('attendee.user_id') . ' IN (?) ',  Tinebase_Group::getInstance()->getGroupMemberships($currentUserId), Zend_Db::INT_TYPE) . ' )' .
-//                             ')',
-//                /* select */ array());
-//    
-//            $_select->orWhere($db->quoteIdentifier('attendee.user_id') . ' IS NOT NULL');
-//            
-//            // free/busy get limited read grand implicitly
-//            // @todo implement some free/busy acl logic
-//            //$_select->orWhere('1=1');
-//        }
-
+        //Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' ' . $_select->__toString());
+    }
+    
+    /**
+     * returns array with the filter settings of this filter group 
+     *
+     * @param  bool $_valueToJson resolve value for json api?
+     * @return array
+     */
+    public function toArray($_valueToJson = false)
+    {
+        $result = parent::toArray($_valueToJson);
+        
+        foreach ($result as &$filterData) {
+            if ($filterData['field'] == 'id' && $_valueToJson == true && ! empty($filterData['value'])) {
+                //Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' value:' . print_r($filterData['value'], true));
+                $filterData['value'] = Timetracker_Controller_Timeaccount::getInstance()->get($filterData['value'])->toArray();
+            }
+        }
+        
+        return $result;
     }
 }
