d27dbea5ab0f711a46bd04f15fda41b5938f51bc
Cornelius Weiß <c.weiss@metaways.de>
2009-06-11 18:00:03 +0000

    typo
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@8527 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Model/Filter/Container.php
+++ tine20/Tinebase/Model/Filter/Container.php
@@ -110,7 +110,7 @@ class Tinebase_Model_Filter_Container extends Tinebase_Model_Filter_Abstract imp
     }
     
     /**
-     * appeds sql to given select statement
+     * appends sql to given select statement
      *
      * @param  Zend_Db_Select                    $_select
      * @param  Tinebase_Backend_Sql_Abstract     $_backend
