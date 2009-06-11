c5152096ec4ef00fb34954295f2d15180456ea7e
Philipp Schüle <p.schuele@metaways.de>
2009-06-11 14:22:57 +0000

    forgot to remove return statement ...
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@8511 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/Frontend/Json.php
+++ tine20/Felamimail/Frontend/Json.php
@@ -163,8 +163,6 @@ class Felamimail_Frontend_Json extends Tinebase_Frontend_Json_Abstract
     {
         $result = $this->_search($filter, $paging, Felamimail_Controller_Message::getInstance(), 'Felamimail_Model_MessageFilter');
         
-        return $result;
-        
         // no paging -> don't do initial cache import
         if (empty($paging) || $result['totalcount'] == 0) {
             return $result;
