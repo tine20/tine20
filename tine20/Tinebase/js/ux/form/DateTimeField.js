fbdb0c6085c5fbc0082f261854c0aa9f219d0fbf
Cornelius Weiß <c.weiss@metaways.de>
2009-06-15 13:54:08 +0000

    lightweight datetimefield form element
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@8642 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/ux/form/DateTimeField.js
+++ tine20/Tinebase/js/ux/form/DateTimeField.js
@@ -14,10 +14,58 @@ Ext.namespace('Ext.ux', 'Ext.ux.form');
  * A combination of datefield and timefield
  */
 Ext.ux.form.DateTimeField = Ext.extend(Ext.form.DateField, {
+    /*
+    initComponent: function() {
+        
+        Ext.ux.form.DateTimeField.superclass.initComponent.call(this);
+        
+    },
+    */
+    getValue: function() {
+        
+        var date = Ext.ux.form.DateTimeField.superclass.getValue.apply(this, arguments);
+        var time = this.timeField.getValue();
+        
+        var timeParts = time.split(':');
+        
+        if (Ext.isDate(date) && timeParts.length >= 2) {
+            date.clearTime();
+            date.setHours(timeParts[0]);
+            date.setMinutes(timeParts[1]);
+        }
+        return date;
+    },
+    
     onRender: function(ct, position) {
         Ext.ux.form.DateTimeField.superclass.onRender.call(this, ct, position);
         
-        //console.log(ct);
+        this.timeFieldEl = Ext.DomHelper.insertAfter(this.wrap.last(), {dom: 'div', style: {'position': 'absolute', 'top': '0px'}}, true);
+        this.timeField = new Ext.form.TimeField({
+            renderTo: this.timeFieldEl,
+            readOnly: this.readOnly,
+            hideTrigger: this.readOnly
+        });
+    },
+    
+    onResize : function(w, h) {
+        Ext.ux.form.DateTimeField.superclass.onResize.apply(this, arguments);
+        
+        var wrapWidth = this.wrap.getWidth();
+        var totalFieldWidth = wrapWidth - 2*this.trigger.getWidth() - 10;
+        
+        var dateFieldWidth = totalFieldWidth * 0.6;
+        var timeFieldWidth = totalFieldWidth * 0.4;
+        
+        this.el.setWidth(dateFieldWidth);
+        this.timeField.getEl().setWidth(timeFieldWidth);
+        
+        this.timeFieldEl.setLeft(dateFieldWidth + this.trigger.getWidth() + 10);
+        this.timeField.wrap.setWidth(timeFieldWidth + this.trigger.getWidth());
+    },
+    
+    setValue: function(value) {
+        Ext.ux.form.DateTimeField.superclass.setValue.apply(this, arguments);
+        this.timeField.setValue(value);
     }
 });
 Ext.reg('datetimefield', Ext.ux.form.DateTimeField);
