93b839e40d74422057b25cae6f90cc16f9b89753
Cornelius Weiß <c.weiss@metaways.de>
2009-06-16 12:47:17 +0000

    test basic create/load cycle
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@8686 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/tine20/Calendar/JsonTests.php
+++ tests/tine20/Calendar/JsonTests.php
@@ -23,8 +23,51 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
  * 
  * @package     Calendar
  */
-class Calendar_JsonTests extends PHPUnit_Framework_TestCase
+class Calendar_JsonTests extends Calendar_TestCase
 {
+    /**
+     * Calendar Json Object
+     *
+     * @var Calendar_Frontend_Json
+     */
+    protected $_uit = null;
+    
+    public function setUp()
+    {
+        parent::setUp();
+        $this->_uit = new Calendar_Frontend_Json();
+    }
+    
+    public function testCreateEvent()
+    {
+        $eventData = $this->_getEvent()->toArray();
+        
+        $tag = Tinebase_Tags::getInstance()->createTag(new Tinebase_Model_Tag(array(
+            'name' => 'phpunit-' . substr(Tinebase_Record_Abstract::generateUID(), 0, 10),
+            'type' => Tinebase_Model_Tag::TYPE_PERSONAL
+        )));
+        $eventData['tags'] = array($tag->toArray());
+        
+        $note = new Tinebase_Model_Note(array(
+            'note'         => 'very important note!',
+            'note_type_id' => Tinebase_Notes::getInstance()->getNoteTypes()->getFirstRecord()->getId(),
+        ));
+        $eventData['notes'] = array($note->toArray());
+        
+        $persistentEventData = $this->_uit->saveEvent(Zend_Json::encode($eventData));
+        $loadedEventData = $this->_uit->getEvent($persistentEventData['id']);
+        
+        $this->assertEquals($eventData['summary'], $loadedEventData['summary'], 'failed to create/load event');
+        
+        // container, assert attendee, tags, relations
+        $this->assertTrue(is_array($loadedEventData['container_id']), 'failed to "resolve" container');
+        $this->assertTrue(is_array($loadedEventData['container_id']['account_grants']), 'failed to "resolve" container account_grants');
+        $this->assertEquals(2, count($loadedEventData['attendee']), 'faild to append attendee');
+        $this->assertEquals(1, count($loadedEventData['tags']), 'faild to append tag');
+        $this->assertEquals(1, count($loadedEventData['notes']), 'faild to create note');
+        
+    }
+    
     public function testUpdateRecurExceptionsFromSeriesOverDstMove()
     {
         /*
