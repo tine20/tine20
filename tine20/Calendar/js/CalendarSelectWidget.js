d6528977e8e3ef9290383996e9f600f4a615bd8c
Cornelius Weiß <c.weiss@metaways.de>
2009-06-19 21:33:31 +0000

    simplify calselect widget
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@8820 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Calendar/js/CalendarSelectWidget.js
+++ tine20/Calendar/js/CalendarSelectWidget.js
@@ -18,6 +18,10 @@ Tine.Calendar.CalendarSelectWidget = function(EventEditDialog) {
     this.app = Tine.Tinebase.appMgr.get('Calendar');
     this.recordClass = Tine.Calendar.Model.Event;
     
+    this.calMapStore = new Ext.data.SimpleStore({
+        fields: this.calMapRecord
+    });
+    
     this.initTpl();
     
     this.fakeCombo = new Ext.form.ComboBox({
@@ -28,9 +32,10 @@ Tine.Calendar.CalendarSelectWidget = function(EventEditDialog) {
         value         : null,
         forceSelection: true,
         width         : 450,
-        store         : EventEditDialog.attendeeStore,
+        store         : this.calMapStore,
         //itemSelector  : 'div.list-item',
-        tpl           : this.attendeeListTpl
+        tpl           : this.attendeeListTpl,
+        onSelect      : this.onCalMapSelect.createDelegate(this)
     });
     
     this.calCombo = new Tine.widgets.container.selectionComboBox({
@@ -50,7 +55,11 @@ Tine.Calendar.CalendarSelectWidget = function(EventEditDialog) {
     
 };
 
-Ext.extend(Tine.Calendar.CalendarSelectWidget, Ext.form.ComboBox, {
+Ext.extend(Tine.Calendar.CalendarSelectWidget, Ext.util.Observable, {
+    
+    calMapRecord: Ext.data.Record.create([
+        {name: 'calendar'}, {name: 'user'}, {name: 'calendarName'}, {name: 'userName'}, {name: 'isOriginal'}
+    ]),
     
     /**
      * @property {Tine.Calendar.EventEditDialog}
@@ -61,38 +70,88 @@ Ext.extend(Tine.Calendar.CalendarSelectWidget, Ext.form.ComboBox, {
      */
     calCombo: null,
     
-    initTpl: function() {
-        //this.attendeeListTpl = '<tpl for="."><div class="x-combo-list-item">Hier{' + 'type' + '}</div></tpl>';
-        var getContainerName = this.EventEditDialog.attendeeGridPanel.renderAttenderDispContainer.createDelegate(this);
-        var getAccountName = this.EventEditDialog.attendeeGridPanel.renderAttenderName.createDelegate(this);
+    getPhysContainer: function() {
+        return this.record.get('container_id')
+    },
+    
+    /**
+     *  builds attendee -> calendar map
+     */
+    buildAttendeeCalMapStore: function() {
+        this.calMapStore.removeAll();
+        var physCal = this.record.get('container_id');
         
+        this.EventEditDialog.attendeeStore.each(function(attender){
+            var calendar = attender.get('displaycontainer_id');
+            var user = attender.get('user_id');
+            var calendarName = this.EventEditDialog.attendeeGridPanel.renderAttenderDispContainer(calendar, {});
+            var userName = this.EventEditDialog.attendeeGridPanel.renderAttenderName(user, {});
+            
+            // check if attender is a valid user
+            if (userName && attender.get('user_type') == 'user') {
+                
+                // check if container is resoved
+                if (calendar && calendar.name) {
+                    // check that calendar is not the physCal
+                    if (! (physCal && physCal.name) || physCal.id != calendar.id) {
+                        this.calMapStore.add([new this.calMapRecord({
+                            calendar: calendar, 
+                            user: user,
+                            calendarName: calendarName,
+                            userName: String.format('for {0}', userName),
+                            isOriginal: false
+                        })]);
+                    }
+                }
+            }
+        }, this);
+        
+        // finally add physCal if acceptable
+        if (physCal && physCal.name) {
+            this.calMapStore.add([new this.calMapRecord({
+                calendar: physCal, 
+                user: 'Originator',
+                calendarName: physCal.name,
+                userName: this.app.i18n._('Originally'),
+                isOriginal: true
+            })]);
+        }
+    },
+    
+    initTpl: function() {
         this.attendeeListTpl = new Ext.XTemplate(
             '<tpl for=".">' +
-                '<div class="'+'x-combo-list'+'-item">' +
-                        '<div style="width: 290px; float: left;">{[this.getContainerName(values.displaycontainer_id, {})]}</div><div>{[this.getAccountName(values.user_id, {})]}</div>' +
-                '</div>' +
-            '</tpl>', {
-                /**
-                 * encode
-                 */
-                encode: function(value) {
-                    return value ? Ext.util.Format.htmlEncode(value) : '';
-                },
-                getContainerName: getContainerName,
-                getAccountName: getAccountName
-            }
+                    '<div class="cal-calselectwidget-fakelist-item x-combo-list-item">' +
+                            '<div class="cal-calselectwidget-fakelist-calendar">{calendarName}</div>' +
+                            '<div class="cal-calselectwidget-fakelist-account">{userName}</div>' +
+                    '</div>' +
+            '</tpl>'
         );
+    },
+    
+    onCalMapSelect: function(record, index) {
+        this.calCombo.setValue(record.get('calendar'));
+        this.calCombo.setTrigger2Text(String.format(record.get('userName')));
+        
+        this.fakeCombo.collapse();
+        this.fakeCombo.fireEvent('select', this.fakeCombo, record, index);
     },
     
     onRecordLoad: function(record) {
         this.record = record;
-        this.calCombo.setValue(record.get('container_id'));
-        this.calCombo.setTrigger2Text('fuer Cornelius Weiss');
+        this.buildAttendeeCalMapStore();
+        
+        var physCal = record.get('container_id');
+        if (physCal.name) {
+            this.calCombo.setValue(record.get('container_id'));
+            this.calCombo.setTrigger2Text('Originally');
+        }
         
     },
     
     onRecordUpdate: function(record) {
-        console.log('onRecordUpdate');
+        // nothing do do here!
+        //console.log('todo: onRecordUpdate');
     },
     
     render: function(el) {
