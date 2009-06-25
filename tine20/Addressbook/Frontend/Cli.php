110a310ac3adc198c882c6b38080c66196f312e0
Philipp Schüle <p.schuele@metaways.de>
2009-06-25 08:11:00 +0000

    added adb sampledata cli func
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@8899 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Addressbook/Frontend/Cli.php
+++ tine20/Addressbook/Frontend/Cli.php
@@ -81,5 +81,54 @@ class Addressbook_Frontend_Cli extends Tinebase_Frontend_Cli_Abstract
         $csvExporter = new Addressbook_Export_Csv();
         
         $csvExporter->exportContacts($filter, TRUE);        
-    }    
+    }
+    
+    public function sampledata($_opts/*$_file*/)
+    {
+        echo 'importing data ...';
+        include '/var/www/tine20/Addressbook/sampledata.php';
+        $controller = Addressbook_Controller_Contact::getInstance();
+        $contact = array();
+        
+        for ($i=0; $i<10; $i++) {
+            
+            // create a company
+            $contact['org_name'] = $sampledata['companyNames'][array_rand($sampledata['companyNames'])] . ' ' .
+                                   $sampledata['companyDesc'][array_rand($sampledata['companyDesc'])] . ' ' .
+                                   $sampledata['companyFrom'][array_rand($sampledata['companyFrom'])];
+                                   
+            $randCompNumber = array_rand($sampledata['companyPlz']);
+            $contact['adr_one_street']     = $sampledata['companyStreet'][array_rand($sampledata['companyStreet'])];
+            $contact['adr_one_postalcode'] = $sampledata['companyPlz'][$randCompNumber];
+            $contact['adr_one_locality']   = $sampledata['companyOrt'][$randCompNumber];
+            
+            for ($j=0; $j<10; $j++) {
+                // create person
+                $contact['tel_work']           = $sampledata['companyDialcode'][$randCompNumber] . rand(2456, 871234);
+                $contact['tel_fax']            = $contact['tel_work'] . '9';
+                $contact['tel_cell']           = $sampledata['mobileDialcode'][array_rand($sampledata['mobileDialcode'])] . rand(245634, 87123224);
+                $contact['role']               = $sampledata['position'][array_rand($sampledata['position'])];
+                
+                $randNameNumber = array_rand($sampledata['personFirstName']);
+                $contact['n_given']            = $sampledata['personFirstName'][$randNameNumber];
+                // todo: generate salutation even maile / odd femail
+                $contact['n_family']            = $sampledata['personLastName'][array_rand($sampledata['personLastName'])];
+                
+                $randPersNumber = array_rand($sampledata['personPlz']);
+                $contact['adr_two_street']     = $sampledata['personStreet'][array_rand($sampledata['personStreet'])];
+                $contact['adr_two_postalcode'] = $sampledata['personPlz'][$randPersNumber];
+                $contact['adr_two_locality']   = $sampledata['personOrt'][$randPersNumber];
+                
+                $contact['tel_home']           = $sampledata['personDialcode'][$randPersNumber] . rand(2456, 871234);
+                $contact['tel_cell_private']   = $sampledata['mobileDialcode'][array_rand($sampledata['mobileDialcode'])] . rand(245634, 87123224);
+                
+                $contact['container_id'] = 133;
+                $contactObj = new Addressbook_Model_Contact($contact, true);
+                
+                print_r($contactObj->toArray());
+                
+                $controller->create($contactObj);
+            }
+        }
+    }
 }
