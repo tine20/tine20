a9b3434ee9a0ce83151f17451899bc9f44140851
Philipp Schüle <p.schuele@metaways.de>
2009-06-30 10:52:24 +0000

    improved email replacement pattern
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@8964 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/Controller/Message.php
+++ tine20/Felamimail/Controller/Message.php
@@ -870,7 +870,7 @@ class Felamimail_Controller_Message extends Tinebase_Controller_Record_Abstract
         $result = preg_replace($mailtoPattern, "\\1", $_content);
         
         //$emailPattern = '/(?<!mailto:)([a-z0-9_\+-\.]+@[a-z0-9-\.]+\.[a-z]{2,4})/i';
-        $emailPattern = '/([a-z0-9_\+-\.]+@[a-z0-9-\.]+\.[a-z]{2,4})/i';
+        $emailPattern = '/([a-z0-9_\+-\.]+@[a-z0-9-\.]+\.[a-z]{2,4})^\&/i';
         $result = preg_replace($emailPattern, "<a href=\"#\" id=\"123:\\1\" class=\"tinebase-email-link\">\\1</a>", $result);
         
         return $result;
