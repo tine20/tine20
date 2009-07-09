337470df4c5a5afacb53f29b654afa55ab4dd49d
Jonas Fischer <j.fischer@metaways.de>
2009-07-09 14:33:30 +0000

    removed log warning message and commented code fragments
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@9294 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/User/Sql.php
+++ tine20/Tinebase/User/Sql.php
@@ -117,10 +117,7 @@ class Tinebase_User_Sql extends Tinebase_User_Abstract
         // throw exception if data is empty (if the row is no array, the setFromArray function throws a fatal error 
         // because of the wrong type that is not catched by the block below)
         if ( $row === false ) {
-            Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ . ' FIXME!!!! Why was the exception commented out?????');
             throw new Tinebase_Exception_NotFound("User $_loginName not found.");
-             //$account = $this->getNonExistentUser($_accountClass);
-             
         } else {
             try {
                 $account = new $_accountClass();
@@ -162,10 +159,7 @@ class Tinebase_User_Sql extends Tinebase_User_Abstract
 
         $row = $stmt->fetch(Zend_Db::FETCH_ASSOC);
         if ($row === false) {
-            Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ . ' FIXME!!!! Why was the exception commented out?????');
-            throw new Tinebase_Exception_NotFound('User with id ' . $accountId . ' not found.');
-            //$account = $this->getNonExistentUser($_accountClass, $accountId);
-            
+            throw new Tinebase_Exception_NotFound('User with id ' . $accountId . ' not found.');           
         } else {
             try {
                 $account = new $_accountClass();
