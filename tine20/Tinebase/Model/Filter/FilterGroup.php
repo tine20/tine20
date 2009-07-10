d213fea9c56c2844f307684e00fd8fb5b00a5193
Lars Kneschke <l.kneschke@metaways.de>
2009-07-10 15:06:43 +0000

    added missing brace
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@9345 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Model/Filter/FilterGroup.php
+++ tine20/Tinebase/Model/Filter/FilterGroup.php
@@ -48,7 +48,7 @@
  *     array('condition' => 'OR', 'filters' => array(
  *         'field' => 'created_by',  'operator' => 'equals', 'value' => 2,
  *         'field' => 'modified_by', 'operator' => 'equals', 'value' => 2
- *     ),
+ *     )),
  *     array('field' => 'container_id', 'operator' => 'in', 'value' => array(2,4,6,7)
  *     array('field' => 'foreign_id',  'operator' => 'AND', value => array(
  *         array('field' => 'foreignfieldname',  'operator' => 'contains', 'value' => 'test'),
