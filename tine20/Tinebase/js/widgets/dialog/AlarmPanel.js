49f7bb5fd589ec4ee8eab3fad5682d0bcc97bdb8
Philipp Schüle <p.schuele@metaways.de>
2009-07-20 13:08:21 +0000

    added '0 minutes before' as option to alarms panel
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@9562 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/widgets/dialog/AlarmPanel.js
+++ tine20/Tinebase/js/widgets/dialog/AlarmPanel.js
@@ -53,12 +53,13 @@ Tine.widgets.dialog.AlarmPanel = Ext.extend(Ext.Panel, {
             forceSelection: true,
             value: 'none',
             store: [
-                ['none', _('None')],
-                ['15',  _('15 minutes before')],
-                ['30',  _('30 minutes before')],
-                ['60',  _('1 hour before')],
-                ['120',  _('2 hours before')],
-                ['1440',  _('1 day before')]
+                ['none',    _('None')],
+                ['0',       _('0 minutes before')],
+                ['15',      _('15 minutes before')],
+                ['30',      _('30 minutes before')],
+                ['60',      _('1 hour before')],
+                ['120',     _('2 hours before')],
+                ['1440',    _('1 day before')]
             ]
         });
         
