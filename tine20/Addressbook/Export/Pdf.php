6167e32a4394c836467649c68f95ec7ef98bba83
Philipp Schüle <p.schuele@metaways.de>
2009-07-22 12:27:45 +0000

    added more debug logging
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@9631 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Addressbook/Export/Pdf.php
+++ tine20/Addressbook/Export/Pdf.php
@@ -134,9 +134,9 @@ class Addressbook_Export_Pdf extends Tinebase_Export_Pdf
             $contactPhoto = Zend_Pdf_Image::imageWithPath($tmpPath);
             unlink($tmpPath);
         } catch (Exception $e) {
-            //Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' ' . $e->__toString());
-            //$contactPhoto = Zend_Pdf_Image::imageWithPath(dirname(dirname(__FILE__)).'/images/empty_photo.jpg');
-            Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' ' . $e->getMessage());
+            // gif images are not supported yet by zf (or some other error)
+            Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' ' . $e->__toString());
+            Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ . ' ' . $e->getMessage());
             $contactPhoto = NULL;
         }
         
