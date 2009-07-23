a623f331da882bf3d3fb026501094c038290dd62
Philipp Schüle <p.schuele@metaways.de>
2009-07-23 09:59:28 +0000

    we don't need the default prefs here any longer
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@9651 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Setup/Update/Release0.php
+++ tine20/Tinebase/Setup/Update/Release0.php
@@ -2083,26 +2083,6 @@ class Tinebase_Setup_Update_Release0 extends Setup_Update_Abstract
         ');
         $this->_backend->addForeignKey('preferences', $declaration);
         
-        // add default settings for timezone and locale
-        $timezonePref = new Tinebase_Model_Preference(array(
-            'application_id'    => Tinebase_Application::getInstance()->getApplicationByName('Tinebase')->getId(),
-            'name'              => Tinebase_Preference::TIMEZONE,
-            'value'             => 'Europe/Berlin',
-            'account_id'        => 0,
-            'account_type'      => Tinebase_Acl_Rights::ACCOUNT_TYPE_ANYONE,
-            'type'              => Tinebase_Model_Preference::TYPE_DEFAULT
-        ));
-        Tinebase_Core::getPreference()->create($timezonePref);
-        $localePref = new Tinebase_Model_Preference(array(
-            'application_id'    => Tinebase_Application::getInstance()->getApplicationByName('Tinebase')->getId(),
-            'name'              => Tinebase_Preference::LOCALE,
-            'value'             => 'auto',
-            'account_id'        => 0,
-            'account_type'      => Tinebase_Acl_Rights::ACCOUNT_TYPE_ANYONE,
-            'type'              => Tinebase_Model_Preference::TYPE_DEFAULT
-        ));
-        Tinebase_Core::getPreference()->create($localePref);
-        
         // delete old user config settings
         $oldSettings = array('Locale', 'Timezone');
         foreach ($oldSettings as $oldSetting) {
