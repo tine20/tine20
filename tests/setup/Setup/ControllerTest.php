dd18a74e4924c08e22f8b82136c08df9c8522ab0
Jonas Fischer <j.fischer@metaways.de>
2009-07-24 07:22:01 +0000

    added tests for Setup_Controller
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@9678 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/setup/Setup/ControllerTest.php
+++ tests/setup/Setup/ControllerTest.php
@@ -24,12 +24,12 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
 /**
  * Test class for Tinebase_Group
  */
-class Setup_JsonTest extends PHPUnit_Framework_TestCase
+class Setup_ControllerTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @var Setup_Frontend_Json
      */
-    protected $_json = array();
+    protected $_uit = null;
     
     /**
      * Runs the test methods of this class.
@@ -39,7 +39,7 @@ class Setup_JsonTest extends PHPUnit_Framework_TestCase
      */
     public static function main()
     {
-		$suite  = new PHPUnit_Framework_TestSuite('Tine 2.0 Setup Json Tests');
+		$suite  = new PHPUnit_Framework_TestSuite('Tine 2.0 Setup Controller Tests');
         PHPUnit_TextUI_TestRunner::run($suite);
 	}
 
@@ -51,7 +51,7 @@ class Setup_JsonTest extends PHPUnit_Framework_TestCase
      */
     protected function setUp()
     {
-        $this->_json = new Setup_Frontend_Json();
+        $this->_uit = Setup_Controller::getInstance();
     }
 
     /**
@@ -71,13 +71,13 @@ class Setup_JsonTest extends PHPUnit_Framework_TestCase
     public function testUninstallApplications()
     {
     	try {
-    		$result = $this->_json->uninstallApplications(Zend_Json::encode(array('ActiveSync')));
+    		$result = $this->_uit->uninstallApplications(array('ActiveSync'));
     	} catch (Tinebase_Exception_NotFound $e) {
-    		$this->_json->installApplications(Zend_Json::encode(array('ActiveSync')));
-    		$result = $this->_json->uninstallApplications(Zend_Json::encode(array('ActiveSync')));
+    		$this->_uit->installApplications(array('ActiveSync'));
+    		$result = $this->_uit->uninstallApplications(array('ActiveSync'));
     	}
     	        
-        $apps = $this->_json->searchApplications();
+        $apps = $this->_uit->searchApplications();
         
         // get active sync
         foreach ($apps['results'] as $app) {
@@ -91,7 +91,7 @@ class Setup_JsonTest extends PHPUnit_Framework_TestCase
         $this->assertTrue(isset($activeSyncApp));
         $this->assertEquals('uninstalled', $activeSyncApp['install_status']);
 
-	    $this->_json->installApplications(Zend_Json::encode(array('ActiveSync'))); //cleanup
+	    $this->_uit->installApplications(array('ActiveSync')); //cleanup
     }
     
     /**
@@ -101,7 +101,7 @@ class Setup_JsonTest extends PHPUnit_Framework_TestCase
     public function testUninstallTinebaseShouldThrowDependencyException()
     {
         $this->setExpectedException('Setup_Exception_Dependency');
-    	$result = $this->_json->uninstallApplications(Zend_Json::encode(array('Tinebase')));
+    	$result = $this->_uit->uninstallApplications(array('Tinebase'));
     }
     
     /**
@@ -110,7 +110,7 @@ class Setup_JsonTest extends PHPUnit_Framework_TestCase
      */
     public function testSearchApplications()
     {
-        $apps = $this->_json->searchApplications();
+        $apps = $this->_uit->searchApplications();
         
         $this->assertGreaterThan(0, $apps['totalcount']);
         
@@ -135,14 +135,14 @@ class Setup_JsonTest extends PHPUnit_Framework_TestCase
     public function testInstallApplications()
     {
         try {
-            $result = $this->_json->installApplications(Zend_Json::encode(array('ActiveSync')));
+            $result = $this->_uit->installApplications(array('ActiveSync'));
         } catch (Exception $e) {
-            $this->_json->uninstallApplications(Zend_Json::encode(array('ActiveSync')));
-            $result = $this->_json->installApplications(Zend_Json::encode(array('ActiveSync')));
+            $this->_uit->uninstallApplications(array('ActiveSync'));
+            $result = $this->_uit->installApplications(array('ActiveSync'));
         }
         
 
-        $apps = $this->_json->searchApplications();
+        $apps = $this->_uit->searchApplications();
         
         // get active sync
         foreach ($apps['results'] as $app) {
@@ -166,8 +166,10 @@ class Setup_JsonTest extends PHPUnit_Framework_TestCase
      */
     public function testUpdateApplications()
     {
-        $result = $this->_json->updateApplications(Zend_Json::encode(array('ActiveSync')));
-        $this->assertTrue($result['success']);
+        $applications = new Tinebase_Record_RecordSet('Tinebase_Model_Application');
+        $applications->addRecord(Tinebase_Application::getInstance()->getApplicationByName('ActiveSync'));
+        $result = $this->_uit->updateApplications($applications);
+        $this->assertTrue(is_array($result)); //Setup_Controller::updateApplications just returns an array of messages and throws exceptions on failure
     }
 
     /**
@@ -176,83 +178,16 @@ class Setup_JsonTest extends PHPUnit_Framework_TestCase
      */
     public function testEnvCheck()
     {
-        $result = $this->_json->envCheck();
+        $result = $this->_uit->checkRequirements();
         
         $this->assertTrue(isset($result['success']));
         $this->assertGreaterThan(16, count($result['results']));
     }
-
-    public function testCheckCOnfig()
-    {
-    	$result = $this->_json->checkConfig();
-    	$this->assertTrue(is_array($result));
-    	$this->assertTrue($result['configExists']);
-    	$this->assertTrue(isset($result['configWritable']));
-    }
     
     public function testLoginWithWrongUsernameAndPassword()
     {
-    	$result = $this->_json->login('unknown_user_xxyz', 'wrong_password');
-    	$this->assertTrue(is_array($result));
-        $this->assertFalse($result['success']);
-        $this->assertTrue(isset($result['errorMessage']));
-        
-//        $config = Tinebase_Core::getConfig();
-//        $result = $this->_json->login($config->setupuser->username, $config->setupuser->password);
+    	$result = $this->_uit->login('unknown_user_xxyz', 'wrong_password');
+        $this->assertFalse($result);
+    }
 
-    }
-    
-    /**
-     * test load config
-     *
-     */
-    public function testLoadConfig()
-    {
-        $result = $this->_json->loadConfig();
-        
-        $this->assertTrue(is_array($result));
-        $this->assertTrue(isset($result['database']));
-        $this->assertGreaterThan(1, count($result));
-    }
-    
-    public function testGetRegistryData()
-    {
-    	$result = $this->_json->getRegistryData();
-    	
-    	$this->assertTrue(is_array($result));
-    	$this->assertTrue(isset($result['configExists']));
-        $this->assertTrue(isset($result['configWritable']));
-        $this->assertTrue(isset($result['checkDB']));
-        $this->assertTrue(isset($result['setupChecks']));
-    }
-    
-//    public function testGetAllRegistryData()
-//    {
-//    	ob_start();
-//    	$this->_json->getAllRegistryData();
-//    	$result = ob_get_contents();
-//    	ob_end_clean();
-//    	//var_dump(Zend_Json::decode($result));
-//    }
-
-    /**
-     * test load config
-     *
-     */
-    public function testSaveConfig()
-    {
-        $configData = $this->_json->loadConfig();
-        
-        // add something to config
-        $configData['test'] = 'value';
-        $this->_json->saveConfig(Zend_Json::encode($configData));
-
-        // load
-        $result = $this->_json->loadConfig();
-        
-        // check
-        $this->assertTrue(isset($result['test']));
-        $this->assertEquals('value', $result['test']);
-        $this->assertEquals($configData['database'], $result['database']);
-    }
 }
