03c3302582bd37c77cd002884a63d56362c97775
Jonas Fischer <j.fischer@metaways.de>
2009-08-03 11:16:57 +0000

    deleted test config action from setup because it tested the "old" data stored in config.inc.php and not the just enterd data (that probably should be stored in config.inc.php)
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@9724 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Setup/js/ConfigManagerGridPanel.js
+++ tine20/Setup/js/ConfigManagerGridPanel.js
@@ -73,29 +73,6 @@ Tine.Setup.ConfigManagerGridPanel = Ext.extend(Ext.FormPanel, {
     },
     
     /**
-     * check config from server and update setup registry
-     */
-    onCheckConfig: function() {
-        this.loadMask.show();
-            Ext.Ajax.request({
-                scope: this,
-                params: {
-                    method: 'Setup.checkConfig'
-                },
-                success: function(response) {
-                    var regData = Ext.util.JSON.decode(response.responseText);
-                    // replace some registry data
-                    for (key in regData) {
-                        if (key != 'status') {
-                            Tine.Setup.registry.replace(key, regData[key]);
-                        }
-                    }
-                    this.loadMask.hide();
-                }
-            });
-    },
-    
-    /**
      * @private
      */
     initComponent: function() {
@@ -293,14 +270,7 @@ Tine.Setup.ConfigManagerGridPanel = Ext.extend(Ext.FormPanel, {
     /**
      * @private
      */
-    initActions: function() {
-        this.action_reCheck = new Ext.Action({
-            text: this.app.i18n._('Check config'),
-            handler: this.onCheckConfig,
-            iconCls: 'x-tbar-loading',
-            scope: this
-        });
-        
+    initActions: function() {       
         this.action_saveConfig = new Ext.Action({
             text: this.app.i18n._('Save config'),
             iconCls: 'setup_action_save_config',
@@ -317,7 +287,6 @@ Tine.Setup.ConfigManagerGridPanel = Ext.extend(Ext.FormPanel, {
         
         this.actionToolbar = new Ext.Toolbar({
             items: [
-                this.action_reCheck,
                 this.action_saveConfig,
                 this.action_downloadConfig
             ]
