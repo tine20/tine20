43d03a9e3bdc2f45676f2abff3cb68ea6251208e
Jonas Fischer <j.fischer@metaways.de>
2009-08-06 14:20:27 +0000

    added BaseTest class that extends PHPUnit_Framework_TestCase and that proved a getProxy method to test protected methods
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@9782 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/setup/BaseTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Tests
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Jonas Fischer <j.fischer@metaways.de>
+ * @version     $Id: $
+ */
+
+
+class BaseTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Create proxy to the given {@param $className} that allows to test protected class methods
+     * 
+     * @param String $_className
+     * @param Array | optional $_params
+     * 
+     * @return object [instance of the proxy class] 
+     */
+    public function getProxy($_className, array $_params = null)
+    {
+        $proxyClassName = "{$_className}Proxy";
+     
+        if (!class_exists($proxyClassName, false)) {
+     
+            $proxyClass = "
+                class $proxyClassName extends $_className
+                {
+                    public function __call(\$function, \$args)
+                    {
+                        \$function = str_replace('proxy_', '_', \$function);
+                        return call_user_func_array(array(\$this, \$function), \$args);
+                    }
+                }
+            ";
+            eval($proxyClass);
+        }
+     
+        return new $proxyClassName($_params);
+    }
+}
