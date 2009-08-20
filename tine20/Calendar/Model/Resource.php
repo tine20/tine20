99c2e2c2b0de7c45adf77f753d8d28d5280c0a83
Cornelius Weiß <c.weiss@metaways.de>
2009-08-20 09:04:58 +0000

    Incomplete - # 1534: add resources http://www.tine20.org/bugtracker/view.php?id=1534
    model of a resource
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@10069 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Calendar/Model/Resource.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Calendar
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+
+/**
+ * Model of a resource
+ * 
+ * @package Calendar
+ */
+class Calendar_Model_Resource extends Tinebase_Record_Abstract
+{
+
+    /**
+     * key in $_validators/$_properties array for the filed which 
+     * represents the identifier
+     * 
+     * @var string
+     */
+    protected $_identifier = 'id';
+    
+    /**
+     * application the record belongs to
+     *
+     * @var string
+     */
+    protected $_application = 'Calendar';
+    
+    /**
+     * validators
+     *
+     * @var array
+     */
+    protected $_validators = array(
+        // tine record fields
+        'id'                   => array('allowEmpty' => true,  'Alnum'),
+        'container_id'         => array('allowEmpty' => true,  'Int'  ),
+        'created_by'           => array('allowEmpty' => true,         ),
+        'creation_time'        => array('allowEmpty' => true          ),
+        'last_modified_by'     => array('allowEmpty' => true          ),
+        'last_modified_time'   => array('allowEmpty' => true          ),
+        'is_deleted'           => array('allowEmpty' => true          ),
+        'deleted_time'         => array('allowEmpty' => true          ),
+        'deleted_by'           => array('allowEmpty' => true          ),
+        'seq'                  => array('allowEmpty' => true,  'Int'  ),
+        // resource specific fields
+        'name'                 => array('allowEmpty' => true          ),
+        'email'                => array('allowEmpty' => true          ),
+        'is_location'          => array('allowEmpty' => true          ),
+    );
+    
+    /**
+     * datetime fields
+     *
+     * @var array
+     */
+    protected $_datetimeFields = array(
+        'creation_time', 
+        'last_modified_time', 
+        'deleted_time', 
+    );
+}
