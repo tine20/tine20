b1441f48641d7c677b69775bdd8dc4737baf7db9
Jonas Fischer <j.fischer@metaways.de>
2009-08-21 14:16:14 +0000

    deleted unused and buggy method Tinebase_Auth_Ldap::_authenticate()
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@10131 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Auth/Ldap.php
+++ tine20/Tinebase/Auth/Ldap.php
@@ -96,60 +96,6 @@ class Tinebase_Auth_Ldap extends Zend_Auth_Adapter_Ldap implements Tinebase_Auth
         return $result;
     }
     
-    /**
-     * Performs an authentication attempt
-     *
-     * @throws Zend_Auth_Adapter_Exception If authentication cannot be performed
-     * @return Zend_Auth_Result
-     */
-    public function _authenticate()
-    {
-        if (empty($this->_identity)) {
-            throw new Zend_Auth_Adapter_Exception('identity can not be empty');
-        } 
-        if (empty($this->_credential)) {
-            throw new Zend_Auth_Adapter_Exception('credential can not be empty');
-        }        
-        
-        Tinebase_Core::getLogger()->debug(__CLASS__ . '::' . __FUNCTION__ . '('. __LINE__ . ') trying to authenticate '. $this->_identity . ' against ' . $this->_host);
-        
-        $ldapServer = new Tinebase_Ldap_LdapServer($this->_host);
-        
-        try {
-            $ldapServer->bind($this->_adminDN, $this->_adminPassword);
-        } catch (Exception $e) {
-            throw new Zend_Auth_Adapter_Exception('could not bind to ldap server: ' . $this->_adminDN . ' as ' . $this->_adminPassword);
-        }
-        
-        $account = $ldapServer->fetchAll($this->_searchDN, 'uid=' . $this->_identity, $this->accountAttributes);
-        
-        $result = NULL;
-        
-        if(count($account) < 1) {
-            $result = Zend_Auth_Result::FAILURE_IDENTITY_NOT_FOUND;
-            Tinebase_Core::getLogger()->debug(__CLASS__ . '::' . __FUNCTION__ . '('. __LINE__ . ') account ' . $this->_identity . ' not found');
-        } elseif(count($account) > 1) {
-            $result = Zend_Auth_Result::FAILURE_IDENTITY_AMBIGUOUS;
-            Tinebase_Core::getLogger()->debug(__CLASS__ . '::' . __FUNCTION__ . '('. __LINE__ . ') multiple accounts for ' . $this->_identity . ' found');
-        } else {
-            if(!$ldapServer->bind($account[0]['dn'], $this->_credential)) {
-                $result = Zend_Auth_Result::FAILURE_CREDENTIAL_INVALID;
-                Tinebase_Core::getLogger()->debug(__CLASS__ . '::' . __FUNCTION__ . '('. __LINE__ . ') invalid password for ' . $account[0]['dn']);
-            }
-        }
-        
-        $ldapServer->disconnect();
-        
-        if($result !== NULL) {
-            return $this->_getAuthResult($result);
-        }
-
-        $this->_resultRow = $account[0];
-    
-        Tinebase_Core::getLogger()->debug(__CLASS__ . '::' . __FUNCTION__ . '('. __LINE__ . ') authentication of '. $this->_identity . ' succeeded');
-        return $this->_getAuthResult(Zend_Auth_Result::SUCCESS);
-    }
-    
     protected function _getAuthResult($_code)
     {
         switch($_code) {
