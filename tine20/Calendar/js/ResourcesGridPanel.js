34d23f24478fcda3bfd3bbe32d3c24b623df658d
Cornelius Weiß <c.weiss@metaways.de>
2009-08-22 07:34:34 +0000

    basic resources grid panel
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@10144 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Calendar/js/ResourcesGridPanel.js
@@ -0,0 +1,58 @@
+/*
+ * Tine 2.0
+ * 
+ * @package     Calendar
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+ 
+Ext.ns('Tine.Calendar');
+
+/**
+ * @namespace Tine.Calendar
+ * @class     Tine.Calendar.ResourcesGridPanel
+ * @extends   Tine.Tinebase.widgets.app.GridPanel
+ * Resources Grid Panel <br>
+ * 
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+Tine.Calendar.ResourcesGridPanel = Ext.extend(Tine.Tinebase.widgets.app.GridPanel, {
+    // model generics
+    recordClass: Tine.Calendar.Model.Resource,
+    
+    // grid specific
+    defaultSortInfo: {field: 'name', dir: 'ASC'},
+    
+    // not yet
+    evalGrants: false,
+    
+    initComponent: function() {
+        this.app = Tine.Tinebase.appMgr.get('Calendar');
+        this.gridConfig = {
+        };
+        
+        this.gridConfig.columns = [{
+            id: 'name',
+            header: this.app.i18n._("Name"),
+            width: 400,
+            sortable: true,
+            dataIndex: 'name'
+        }, {
+            id: 'email',
+            header: this.app.i18n._("Email"),
+            width: 400,
+            sortable: true,
+            dataIndex: 'email'
+        }, new Ext.ux.grid.CheckColumn({
+            header: _('Location'),
+            dataIndex: 'is_location',
+            width: 55
+        })];
+        
+        this.supr().initComponent.call(this);
+    }
+});
