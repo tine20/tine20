017d1704c66823a4d05e15c518d1659fd548ec0d
Cornelius Weiß <c.weiss@metaways.de>
2009-08-26 08:27:42 +0000

    update docu
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@10194 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Calendar/Model/Attender.php
+++ tine20/Calendar/Model/Attender.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * Sql Calendar 
- * 
  * @package     Calendar
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Cornelius Weiss <c.weiss@metaways.de>
