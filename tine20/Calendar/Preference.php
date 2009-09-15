2c8f53601088313ba8724966d77445e24e39d82c
Cornelius Weiß <c.weiss@metaways.de>
2009-09-15 09:04:09 +0000

    don't autosave pref
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@10434 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Calendar/Preference.php
+++ tine20/Calendar/Preference.php
@@ -98,9 +98,6 @@ class Calendar_Preference extends Tinebase_Preference_Abstract
                 $calendars          = Tinebase_Container::getInstance()->getPersonalContainer($accountId, 'Calendar', $accountId, 0, true);
                 $preference->value  = $calendars->getFirstRecord()->getId();
                 
-                // make sure we never come back here again ;-)
-                $this->setValueForUser(self::DEFAULTCALENDAR, $preference->value, $accountId, TRUE);
-                
                 break;
             default:
                 throw new Tinebase_Exception_NotFound('Default preference with name ' . $_preferenceName . ' not found.');
