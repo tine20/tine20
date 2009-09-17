13f6a1ea8ec9eee051068445d2301080d11a104b
Cornelius Weiß <c.weiss@metaways.de>
2009-09-17 17:45:49 +0000

    Incomplete - # 1626: import calendars/events from egw 1.4 http://www.tine20.org/bugtracker/view.php?id=1626
    cli frontend for imports
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@10477 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Calendar/Frontend/Cli.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Tine 2.0
+ * @package     Calendar
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ */
+
+/**
+ * Cli frontend for Calendar
+ *
+ * This class handles cli requests for the Calendar
+ *
+ * @package     Calendar
+ */
+class Calendar_Frontend_Cli extends Tinebase_Frontend_Cli_Abstract
+{
+    /**
+     * the internal name of the application
+     * 
+     * @var string
+     */
+    protected $_applicationName = 'Calendar';
+    
+    /**
+     * help array with function names and param descriptions
+     * 
+     * @return void
+     */
+    protected $_help = array(
+        'importegw14' => array(
+            'description'   => 'imports calendars/events from egw 1.4',
+            'params'        => array(
+                'host'     => 'dbhost',
+                'username' => 'username',
+                'password' => 'password',
+                'dbname'   => 'dbname'
+            )
+        ),
+    );
+    
+    public function importegw14($_opts) {
+        //$args = $_opts->getRemainingArgs();
+        list($host, $username, $password, $dbname, $charset) = $_opts->getRemainingArgs();
+        
+        $egwDb = Zend_Db::factory('PDO_MYSQL', array(
+            'host'     => $host,
+            'username' => $username,
+            'password' => $password,
+            'dbname'   => $dbname
+        ));
+        $egwDb->query("SET NAMES $charset");
+        
+        $config = new Zend_Config(array(
+            /**
+             * convert egw owner grants to tine container 
+             * grants for newly created calendars
+             */ 
+            'setPersonalCalendarGrants'   => TRUE,
+            /**
+             * forece converting grants regardless if calendars are new or not
+             */ 
+            'forcePersonalCalendarGrants' => FALSE,
+        ));
+        $importer = new Calendar_Setup_Import_Egw14($egwDb, $config);
+    }
+}
