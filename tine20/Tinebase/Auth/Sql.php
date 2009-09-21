b603b2e93ca57e971d845235f7bc573efb0b3aeb
Philipp Schüle <p.schuele@metaways.de>
2009-09-21 11:25:49 +0000

    added METHOD and LINE to debug messages
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@10491 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Auth/Sql.php
+++ tine20/Tinebase/Auth/Sql.php
@@ -34,7 +34,7 @@ class Tinebase_Auth_Sql extends Zend_Auth_Adapter_DbTable implements Tinebase_Au
             // username and password are correct, let's do some additional tests
             
             if($this->_resultRow['status'] != 'enabled') {
-                Tinebase_Core::getLogger()->debug('account: '. $this->_identity . ' is disabled');
+                Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' Account: '. $this->_identity . ' is disabled');
                 // account is disabled
                 $authResult['code'] = Zend_Auth_Result::FAILURE_UNCATEGORIZED;
                 $authResult['messages'][] = 'Account disabled.';
@@ -44,15 +44,15 @@ class Tinebase_Auth_Sql extends Zend_Auth_Adapter_DbTable implements Tinebase_Au
             //if(($this->_resultRow['expires_at'] !== NULL) && $this->_resultRow['expires_at'] < Zend_Date::now()->getTimestamp()) {
             if(($this->_resultRow['expires_at'] !== NULL) && Zend_Date::now()->isLater($this->_resultRow['expires_at'])) {
                 // account is expired
-                Tinebase_Core::getLogger()->debug('account: '. $this->_identity . ' is expired');
+                Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' Account: '. $this->_identity . ' is expired');
                 $authResult['code'] = Zend_Auth_Result::FAILURE_UNCATEGORIZED;
                 $authResult['messages'][] = 'Account expired.';
                 return new Zend_Auth_Result($authResult['code'], $result->getIdentity(), $authResult['messages']);
             }
             
-            Tinebase_Core::getLogger()->debug('authentication of '. $this->_identity . ' succeeded');
+            Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' Authentication of '. $this->_identity . ' succeeded');
         } else {
-            Tinebase_Core::getLogger()->debug('authentication of '. $this->_identity . ' failed');
+            Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' Authentication of '. $this->_identity . ' failed');
         }
         
         return $result;
