a3e289a77bb22c623f2c52288d5e9403e862b612
Philipp Schüle <p.schuele@metaways.de>
2009-09-23 15:22:31 +0000

    use column model defaults
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@10531 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Addressbook/js/ContactGrid.js
+++ tine20/Addressbook/js/ContactGrid.js
@@ -47,6 +47,7 @@ Tine.Addressbook.ContactGridPanel = Ext.extend(Tine.Tinebase.widgets.app.GridPan
     gridConfig: {
         loadMask: true,
         autoExpandColumn: 'n_fileas'
+        
     },
     
     /**
@@ -64,7 +65,7 @@ Tine.Addressbook.ContactGridPanel = Ext.extend(Tine.Tinebase.widgets.app.GridPan
             //this.actions_composeEmail
         ];
         
-        this.gridConfig.columns = this.getColumns();
+        this.gridConfig.cm = this.getColumnModel();
         this.filterToolbar = this.getFilterToolbar();
         this.detailsPanel = this.getDetailsPanel();
         
@@ -105,49 +106,57 @@ Tine.Addressbook.ContactGridPanel = Ext.extend(Tine.Tinebase.widgets.app.GridPan
     },    
     
     /**
-     * returns columns
+     * returns column model
+     * 
+     * @return Ext.grid.ColumnModel
      * @private
      */
-    getColumns: function(){
-        return [
-            { resizable: true, sortable: true, id: 'tid', header: this.app.i18n._('Type'), dataIndex: 'tid', width: 30, renderer: this.contactTidRenderer.createDelegate(this) },
-            { resizable: true, sortable: true, id: 'tags', header: this.app.i18n._('Tags'), dataIndex: 'tags', width: 60, renderer: Tine.Tinebase.common.tagsRenderer },
-            { resizable: true, sortable: true, id: 'n_family', header: this.app.i18n._('Last Name'), dataIndex: 'n_family', hidden: true },
-            { resizable: true, sortable: true, id: 'n_given', header: this.app.i18n._('First Name'), dataIndex: 'n_given', width: 80, hidden: true },
-            { resizable: true, sortable: true, id: 'n_fn', header: this.app.i18n._('Full Name'), dataIndex: 'n_fn', hidden: true },
-            { resizable: true, sortable: true, id: 'n_fileas', header: this.app.i18n._('Display Name'), dataIndex: 'n_fileas'},
-            { resizable: true, sortable: true, id: 'org_name', header: this.app.i18n._('Company'), dataIndex: 'org_name', width: 200 },
-            { resizable: true, sortable: true, id: 'org_unit', header: this.app.i18n._('Unit'), dataIndex: 'org_unit' , hidden: true },
-            { resizable: true, sortable: true, id: 'title', header: this.app.i18n._('Job Title'), dataIndex: 'title', hidden: true },
-            { resizable: true, sortable: true, id: 'role', header: this.app.i18n._('Job Role'), dataIndex: 'role', hidden: true },
-            { resizable: true, sortable: true, id: 'room', header: this.app.i18n._('Room'), dataIndex: 'room', hidden: true },
-            { resizable: true, sortable: true, id: 'adr_one_street', header: this.app.i18n._('Street'), dataIndex: 'adr_one_street', hidden: true },
-            { resizable: true, sortable: true, id: 'adr_one_locality', header: this.app.i18n._('City'), dataIndex: 'adr_one_locality', width: 150, hidden: false },
-            { resizable: true, sortable: true, id: 'adr_one_region', header: this.app.i18n._('Region'), dataIndex: 'adr_one_region', hidden: true },
-            { resizable: true, sortable: true, id: 'adr_one_postalcode', header: this.app.i18n._('Postalcode'), dataIndex: 'adr_one_postalcode', hidden: true },
-            { resizable: true, sortable: true, id: 'adr_one_countryname', header: this.app.i18n._('Country'), dataIndex: 'adr_one_countryname', hidden: true },
-            { resizable: true, sortable: true, id: 'adr_two_street', header: this.app.i18n._('Street (private)'), dataIndex: 'adr_two_street', hidden: true },
-            { resizable: true, sortable: true, id: 'adr_two_locality', header: this.app.i18n._('City (private)'), dataIndex: 'adr_two_locality', hidden: true },
-            { resizable: true, sortable: true, id: 'adr_two_region', header: this.app.i18n._('Region (private)'), dataIndex: 'adr_two_region', hidden: true },
-            { resizable: true, sortable: true, id: 'adr_two_postalcode', header: this.app.i18n._('Postalcode (private)'), dataIndex: 'adr_two_postalcode', hidden: true },
-            { resizable: true, sortable: true, id: 'adr_two_countryname', header: this.app.i18n._('Country (private)'), dataIndex: 'adr_two_countryname', hidden: true },
-            { resizable: true, sortable: true, id: 'email', header: this.app.i18n._('Email'), dataIndex: 'email', width: 150},
-            { resizable: true, sortable: true, id: 'tel_work', header: this.app.i18n._('Phone'), dataIndex: 'tel_work', hidden: false },
-            { resizable: true, sortable: true, id: 'tel_cell', header: this.app.i18n._('Mobile'), dataIndex: 'tel_cell', hidden: false },
-            { resizable: true, sortable: true, id: 'tel_fax', header: this.app.i18n._('Fax'), dataIndex: 'tel_fax', hidden: true },
-            { resizable: true, sortable: true, id: 'tel_car', header: this.app.i18n._('Car phone'), dataIndex: 'tel_car', hidden: true },
-            { resizable: true, sortable: true, id: 'tel_pager', header: this.app.i18n._('Pager'), dataIndex: 'tel_pager', hidden: true },
-            { resizable: true, sortable: true, id: 'tel_home', header: this.app.i18n._('Phone (private)'), dataIndex: 'tel_home', hidden: true },
-            { resizable: true, sortable: true, id: 'tel_fax_home', header: this.app.i18n._('Fax (private)'), dataIndex: 'tel_fax_home', hidden: true },
-            { resizable: true, sortable: true, id: 'tel_cell_private', header: this.app.i18n._('Mobile (private)'), dataIndex: 'tel_cell_private', hidden: true },
-            { resizable: true, sortable: true, id: 'email_home', header: this.app.i18n._('Email (private)'), dataIndex: 'email_home', hidden: true },
-            { resizable: true, sortable: true, id: 'url', header: this.app.i18n._('Web'), dataIndex: 'url', hidden: true },
-            { resizable: true, sortable: true, id: 'url_home', header: this.app.i18n._('URL (private)'), dataIndex: 'url_home', hidden: true },
-            { resizable: true, sortable: true, id: 'note', header: this.app.i18n._('Note'), dataIndex: 'note', hidden: true },
-            { resizable: true, sortable: true, id: 'tz', header: this.app.i18n._('Timezone'), dataIndex: 'tz', hidden: true },
-            { resizable: true, sortable: true, id: 'geo', header: this.app.i18n._('Geo'), dataIndex: 'geo', hidden: true },
-            { resizable: true, sortable: true, id: 'bday', header: this.app.i18n._('Birthday'), dataIndex: 'bday', hidden: true }
-        ];
+    getColumnModel: function(){
+        return new Ext.grid.ColumnModel({ 
+            defaults: {
+                sortable: true,
+                hidden: true,
+                resizable: true
+            },
+            columns: [
+            { id: 'tid', header: this.app.i18n._('Type'), dataIndex: 'tid', width: 30, renderer: this.contactTidRenderer.createDelegate(this), hidden: false },
+            { id: 'tags', header: this.app.i18n._('Tags'), dataIndex: 'tags', width: 60, renderer: Tine.Tinebase.common.tagsRenderer, sortable: false },
+            { id: 'n_family', header: this.app.i18n._('Last Name'), dataIndex: 'n_family' },
+            { id: 'n_given', header: this.app.i18n._('First Name'), dataIndex: 'n_given', width: 80 },
+            { id: 'n_fn', header: this.app.i18n._('Full Name'), dataIndex: 'n_fn' },
+            { id: 'n_fileas', header: this.app.i18n._('Display Name'), dataIndex: 'n_fileas', hidden: false},
+            { id: 'org_name', header: this.app.i18n._('Company'), dataIndex: 'org_name', width: 200, hidden: false },
+            { id: 'org_unit', header: this.app.i18n._('Unit'), dataIndex: 'org_unit'  },
+            { id: 'title', header: this.app.i18n._('Job Title'), dataIndex: 'title' },
+            { id: 'role', header: this.app.i18n._('Job Role'), dataIndex: 'role' },
+            { id: 'room', header: this.app.i18n._('Room'), dataIndex: 'room' },
+            { id: 'adr_one_street', header: this.app.i18n._('Street'), dataIndex: 'adr_one_street' },
+            { id: 'adr_one_locality', header: this.app.i18n._('City'), dataIndex: 'adr_one_locality', width: 150, hidden: false },
+            { id: 'adr_one_region', header: this.app.i18n._('Region'), dataIndex: 'adr_one_region' },
+            { id: 'adr_one_postalcode', header: this.app.i18n._('Postalcode'), dataIndex: 'adr_one_postalcode' },
+            { id: 'adr_one_countryname', header: this.app.i18n._('Country'), dataIndex: 'adr_one_countryname' },
+            { id: 'adr_two_street', header: this.app.i18n._('Street (private)'), dataIndex: 'adr_two_street' },
+            { id: 'adr_two_locality', header: this.app.i18n._('City (private)'), dataIndex: 'adr_two_locality' },
+            { id: 'adr_two_region', header: this.app.i18n._('Region (private)'), dataIndex: 'adr_two_region' },
+            { id: 'adr_two_postalcode', header: this.app.i18n._('Postalcode (private)'), dataIndex: 'adr_two_postalcode' },
+            { id: 'adr_two_countryname', header: this.app.i18n._('Country (private)'), dataIndex: 'adr_two_countryname' },
+            { id: 'email', header: this.app.i18n._('Email'), dataIndex: 'email', width: 150, hidden: false },
+            { id: 'tel_work', header: this.app.i18n._('Phone'), dataIndex: 'tel_work', hidden: false },
+            { id: 'tel_cell', header: this.app.i18n._('Mobile'), dataIndex: 'tel_cell', hidden: false },
+            { id: 'tel_fax', header: this.app.i18n._('Fax'), dataIndex: 'tel_fax' },
+            { id: 'tel_car', header: this.app.i18n._('Car phone'), dataIndex: 'tel_car' },
+            { id: 'tel_pager', header: this.app.i18n._('Pager'), dataIndex: 'tel_pager' },
+            { id: 'tel_home', header: this.app.i18n._('Phone (private)'), dataIndex: 'tel_home' },
+            { id: 'tel_fax_home', header: this.app.i18n._('Fax (private)'), dataIndex: 'tel_fax_home' },
+            { id: 'tel_cell_private', header: this.app.i18n._('Mobile (private)'), dataIndex: 'tel_cell_private' },
+            { id: 'email_home', header: this.app.i18n._('Email (private)'), dataIndex: 'email_home' },
+            { id: 'url', header: this.app.i18n._('Web'), dataIndex: 'url' },
+            { id: 'url_home', header: this.app.i18n._('URL (private)'), dataIndex: 'url_home' },
+            { id: 'note', header: this.app.i18n._('Note'), dataIndex: 'note' },
+            { id: 'tz', header: this.app.i18n._('Timezone'), dataIndex: 'tz' },
+            { id: 'geo', header: this.app.i18n._('Geo'), dataIndex: 'geo' },
+            { id: 'bday', header: this.app.i18n._('Birthday'), dataIndex: 'bday' }
+        ]});
     },
     
     /**
