777755455888c9643214cb225b39012915490a12
Philipp Schüle <p.schuele@metaways.de>
2009-09-25 11:12:54 +0000

    ASSIGNED - # 900: show tags in grid panels http://www.tine20.org/bugtracker/view.php?id=900
    - we can't use the parent function here because of the different container handling for timesheets
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@10570 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Timetracker/Frontend/Json.php
+++ tine20/Timetracker/Frontend/Json.php
@@ -132,7 +132,12 @@ class Timetracker_Frontend_Json extends Tinebase_Frontend_Json_Abstract
                 break;
         }
         
-        $result = parent::_multipleRecordsToJson($_records, $_filter);
+        Tinebase_Tags::getInstance()->getMultipleTagsOfRecords($_records);
+        
+        $_records->setTimezone(Tinebase_Core::get('userTimeZone'));
+        $_records->convertDates = true;
+        
+        $result = $_records->toArray();
         
         return $result;
     }
