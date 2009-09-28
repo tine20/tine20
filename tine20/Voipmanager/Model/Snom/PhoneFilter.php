f21e1d221443756af38f810c336ac18637ee6e02
Lars Kneschke <l.kneschke@metaways.de>
2009-09-28 06:52:06 +0000

    no separate database needed anymore
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@10609 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Voipmanager/Model/Snom/PhoneFilter.php
+++ tine20/Voipmanager/Model/Snom/PhoneFilter.php
@@ -71,6 +71,7 @@ class Voipmanager_Model_Snom_PhoneFilter extends Tinebase_Model_Filter_FilterGro
     {
         $accountIdFilter = $this->_findFilter('account_id');
         if($accountIdFilter !== NULL) {
+            $db = Tinebase_Core::getDb();
             $backend = new Voipmanager_Backend_Snom_Phone();
             $_validPhoneIds = $backend->getValidPhoneIds($accountIdFilter->getValue());   
             if(empty($_validPhoneIds)) {
