3fd68876e4bf4f67078c59f19c31f5030a658aa2
Philipp Schüle <p.schuele@metaways.de>
2009-09-29 15:05:44 +0000

    added example record grid panel to extdoc
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@10648 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/ExampleApplication/js/ExampleRecordGridPanel.js
+++ tine20/ExampleApplication/js/ExampleRecordGridPanel.js
@@ -1,10 +1,10 @@
-/**
+/*
  * Tine 2.0
  * 
  * @package     ExampleApplication
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Philipp Schuele <p.schuele@metaways.de>
- * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2007-2009 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id:ExampleRecordGridPanel.js 7170 2009-03-05 10:58:55Z p.schuele@metaways.de $
  *
  */
@@ -13,24 +13,56 @@ Ext.namespace('Tine.ExampleApplication');
 
 /**
  * ExampleRecord grid panel
+ * 
+ * @namespace   Tine.ExampleApplication
+ * @class       Tine.ExampleApplication.ExampleRecordGridPanel
+ * @extends     Tine.Tinebase.widgets.app.GridPanel
+ * 
+ * <p>ExampleRecord Grid Panel</p>
+ * <p><pre>
+ * </pre></p>
+ * 
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id:ExampleRecordGridPanel.js 7170 2009-03-05 10:58:55Z p.schuele@metaways.de $
+ * 
+ * @param       {Object} config
+ * @constructor
+ * Create a new Tine.ExampleApplication.ExampleRecordGridPanel
  */
 Tine.ExampleApplication.ExampleRecordGridPanel = Ext.extend(Tine.Tinebase.widgets.app.GridPanel, {
-    // model generics
+    /**
+     * record class
+     * @cfg {Tine.ExampleApplication.Model.ExampleRecord} recordClass
+     */
     recordClass: Tine.ExampleApplication.Model.ExampleRecord,
+    
+    /**
+     * eval grants
+     * @cfg {Boolean} evalGrants
+     */
     evalGrants: false,
     
-    // grid specific
+    /**
+     * grid specific
+     * @private
+     */
     defaultSortInfo: {field: 'creation_time', direction: 'DESC'},
     gridConfig: {
         loadMask: true,
         autoExpandColumn: 'title'
     },
      
+    /**
+     * inits this cmp
+     * @private
+     */
     initComponent: function() {
         this.recordProxy = Tine.ExampleApplication.recordBackend;
         
         //this.actionToolbarItems = this.getToolbarItems();
-        this.gridConfig.columns = this.getColumns();
+        this.gridConfig.cm = this.getColumnModel();
         //this.initFilterToolbar();
         
         this.plugins = this.plugins || [];
@@ -45,6 +77,7 @@ Tine.ExampleApplication.ExampleRecordGridPanel = Ext.extend(Tine.Tinebase.widget
     
     /**
      * initialises filter toolbar
+     *  @private
      */
     initFilterToolbar: function() {
         this.filterToolbar = new Tine.widgets.grid.FilterToolbar({
@@ -66,37 +99,45 @@ Tine.ExampleApplication.ExampleRecordGridPanel = Ext.extend(Tine.Tinebase.widget
     
     /**
      * returns cm
+     * 
+     * @return Ext.grid.ColumnModel
      * @private
      * 
-     * @todo    add more columns
+     * TODO    add more columns
      */
-    getColumns: function(){
-        return [/*{
-            id: 'number',
-            header: this.app.i18n._("Number"),
-            width: 100,
-            sortable: true,
-            dataIndex: 'number'
-        },{
-            id: 'title',
-            header: this.app.i18n._("Title"),
-            width: 350,
-            sortable: true,
-            dataIndex: 'title'
-        },{
-            id: 'status',
-            header: this.app.i18n._("Status"),
-            width: 150,
-            sortable: true,
-            dataIndex: 'status',
-            renderer: this.statusRenderer.createDelegate(this)
-        },{
-            id: 'budget',
-            header: this.app.i18n._("Budget"),
-            width: 100,
-            sortable: true,
-            dataIndex: 'budget'
-        }*/];
+    getColumnModel: function(){
+        return new Ext.grid.ColumnModel({ 
+            defaults: {
+                sortable: true,
+                resizable: true
+            },
+            columns: [/*{
+                id: 'number',
+                header: this.app.i18n._("Number"),
+                width: 100,
+                sortable: true,
+                dataIndex: 'number'
+            },{
+                id: 'title',
+                header: this.app.i18n._("Title"),
+                width: 350,
+                sortable: true,
+                dataIndex: 'title'
+            },{
+                id: 'status',
+                header: this.app.i18n._("Status"),
+                width: 150,
+                sortable: true,
+                dataIndex: 'status',
+                renderer: this.statusRenderer.createDelegate(this)
+            },{
+                id: 'budget',
+                header: this.app.i18n._("Budget"),
+                width: 100,
+                sortable: true,
+                dataIndex: 'budget'
+            }*/]
+        });
     },
     
     /**
@@ -110,6 +151,7 @@ Tine.ExampleApplication.ExampleRecordGridPanel = Ext.extend(Tine.Tinebase.widget
     
     /**
      * return additional tb items
+     * @private
      */
     getToolbarItems: function(){
     	/*
