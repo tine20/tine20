9b6f912b26a9fdcf0c4b93b60fcf2e1e9887b4d8
Lars Kneschke <l.kneschke@metaways.de>
2009-09-29 05:48:15 +0000

    updated classes available
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@10634 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Voipmanager/Server/Asterisk.php
+++ tine20/Voipmanager/Server/Asterisk.php
@@ -27,18 +27,37 @@ class Voipmanager_Server_Asterisk extends Tinebase_Server_Abstract
     public function handle()
     {        
         $this->_initFramework();
-        Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ .' is Asterisk curl request. method: ' . (isset($_REQUEST['method']) ? $_REQUEST['method'] : 'EMPTY'));
+        #Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ .' is Asterisk curl request: ' . print_r($_REQUEST, true));
         
         if(Tinebase_Controller::getInstance()->login($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'], $_SERVER['REMOTE_ADDR']) === true) {
             $server = new Tinebase_Http_Server();
-            $server->setClass('Voipmanager_Frontend_Asterisk_SipPeers', 'Voipmanager_SipPeers');
+            $server->setClass('Voipmanager_Frontend_Asterisk_SipPeers',    'Voipmanager_SipPeers');
+            $server->setClass('Voipmanager_Frontend_Asterisk_SipRegs',     'Voipmanager_SipRegs');
+            $server->setClass('Voipmanager_Frontend_Asterisk_CallForward', 'Voipmanager_CallForward');
             
-            // set method to a usefull value
-            list($class) = explode('.', $_REQUEST['method']);
-            $method = ucfirst(substr($_REQUEST['action'],1));
-            $_REQUEST['method'] = $class . '.handle' . $method;
+            list($class, $method) = explode('.', $_REQUEST['method']);
             
+            // ugly hack to parse requests from res_config_curl
+            if($method == 'handleResConfig') {
+                // set method to a usefull value
+                $pos = strpos($_REQUEST['action'], '?');
+                if($pos !== false) {
+                    $action = substr($_REQUEST['action'], 0, $pos);
+                    list($key, $value) = explode('=', substr($_REQUEST['action'], $pos+1));
+                    $_REQUEST[$key] = $value;
+                } else {
+                    $action = $_REQUEST['action'];
+                }
+                #Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ .' action: ' . $action);
+                
+                $method = ucfirst(substr($action, 1));
+                $_REQUEST['method'] = $class . '.handle' . $method;
+            }
+
+            #Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ .' action: ' . print_r($_REQUEST, true));
             $server->handle($_REQUEST);
+
+            Tinebase_Controller::getInstance()->logout($_SERVER['REMOTE_ADDR']);
         } else {
             Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ .' auth failed ');
         }
