046b07d2011975ea55e6debb76f30df1bfcdd590
Lars Kneschke <l.kneschke@metaways.de>
2009-10-02 15:53:14 +0000

    add redirect model
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@10700 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Voipmanager/Model/Asterisk/Redirect.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Voipmanager
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+
+/**
+ * class to hold asterisk redirect data
+ * 
+ * @package     Voipmanager 
+ */
+class Voipmanager_Model_Asterisk_Redirect extends Tinebase_Record_Abstract
+{
+    /**
+     * key in $_validators/$_properties array for the filed which 
+     * represents the identifier
+     * 
+     * @var string
+     */    
+    protected $_identifier = 'id';
+    
+    /**
+     * application the record belongs to
+     *
+     * @var string
+     */
+    protected $_application = 'Voipmanager';
+    
+    /**
+     * list of zend inputfilter
+     * 
+     * this filter get used when validating user generated content with Zend_Input_Filter
+     *
+     * @var array
+     */
+    protected $_filters = array(
+        '*'                     => 'StringTrim'
+    );
+    
+    /**
+     * list of zend validator
+     * 
+     * this validators get used when validating user generated content with Zend_Input_Filter
+     *
+     * @var array
+     */
+    protected $_validators = array(
+        'id'            => array(Zend_Filter_Input::ALLOW_EMPTY => true, Zend_Filter_Input::DEFAULT_VALUE => NULL),
+        'sippeer_id'    => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'cfi_mode'      => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'cfi_number'    => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'cfb_mode'      => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'cfb_number'    => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'cfd_mode'      => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'cfd_number'    => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+        'cfd_time'      => array(Zend_Filter_Input::ALLOW_EMPTY => true)
+    );
+
+    /**
+     * converts a string or Voipmanager_Model_Asterisk_Redirect to an redirect id
+     *
+     * @param int|string|Voipmanager_Model_Asterisk_Redirect $_id the redirect id to convert
+     * @return int
+     * @throws  Voipmanager_Exception_InvalidArgument
+     */
+    static public function convertObjectToId($_id)
+    {
+        if ($_id instanceof Voipmanager_Model_Asterisk_Redirect) {
+            if (empty($_id->id)) {
+                throw new Voipmanager_Exception_InvalidArgument('no redirect id set');
+            }
+            $id = (string) $_id->id;
+        } else {
+            $id = (string) $_id;
+        }
+                
+        return $id;
+    }
+
+}
