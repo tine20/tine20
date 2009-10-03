32c896f55c78ea045706c328bd38134b1ab2f968
Lars Kneschke <l.kneschke@metaways.de>
2009-10-03 08:18:48 +0000

    redirect filter class
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@10701 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Voipmanager/Model/Asterisk/RedirectFilter.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Voipmanager
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+
+/**
+ * Asterisk Redirect Filter Class
+ * @package Voipmanager
+ */
+class Voipmanager_Model_Asterisk_RedirectFilter extends Tinebase_Model_Filter_FilterGroup
+{
+    /**
+     * @var string application of this filter group
+     */
+    protected $_applicationName = 'Voipmanager';
+    
+    /**
+     * @var string name of model this filter group is designed for
+     */
+    protected $_modelName = 'Voipmanager_Model_Asterisk_Redirect';
+    
+    /**
+     * @var array filter model fieldName => definition
+     */
+    protected $_filterModel = array(
+        'sippeer_id'   => array('filter' => 'Tinebase_Model_Filter_Text')
+    );
+}
