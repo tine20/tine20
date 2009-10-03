93e2babd19a40247444db44ea1bdcc211c3b0098
Lars Kneschke <l.kneschke@metaways.de>
2009-10-03 17:33:12 +0000

    publish voicemail configuration
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@10714 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Voipmanager/Controller/Asterisk/Voicemail.php
+++ tine20/Voipmanager/Controller/Asterisk/Voicemail.php
@@ -58,4 +58,111 @@ class Voipmanager_Controller_Asterisk_Voicemail extends Voipmanager_Controller_A
         
         return self::$_instance;
     }
+    
+    /**
+     * (non-PHPdoc)
+     * @see Tinebase/Controller/Record/Tinebase_Controller_Record_Abstract#create($_record)
+     */
+    public function create(Tinebase_Record_Interface $_record)
+    {
+        $result =  parent::create($_record);
+        
+        if(isset(Tinebase_Core::getConfig()->asterisk)) {
+            $this->publishConfiguration();
+        }
+        
+        return $result;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Tinebase/Controller/Record/Tinebase_Controller_Record_Abstract#delete($_ids)
+     */
+    public function delete($_ids)
+    {
+        $result = parent::delete($_ids);
+        
+        if(isset(Tinebase_Core::getConfig()->asterisk)) {
+            $this->publishConfiguration();
+        }
+        
+        return $result;
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see Tinebase/Controller/Record/Tinebase_Controller_Record_Abstract#update($_record)
+     */
+    public function update(Tinebase_Record_Interface $_record)
+    {
+        $result =  parent::update($_record);
+        
+        Zend_Registry::get('logger')->debug(__METHOD__ . '::' . __LINE__ . ' update voicemail configuration');
+        
+        if(isset(Tinebase_Core::getConfig()->asterisk)) {
+            $this->publishConfiguration();
+        }
+        
+        return $result;
+    }
+    
+    /**
+     * create voicemail.conf and upload to asterisk server
+     * 
+     * @return void
+     */
+    public static function publishConfiguration()
+    {   
+        Zend_Registry::get('logger')->debug(__METHOD__ . '::' . __LINE__ . ' publish voicemail configuration');
+        
+        if(isset(Tinebase_Core::getConfig()->asterisk)) {
+            $asteriskConfig = Tinebase_Core::getConfig()->asterisk;
+            
+            $url        = $asteriskConfig->managerbaseurl;
+            $username   = $asteriskConfig->managerusername;
+            $password   = $asteriskConfig->managerpassword;
+        } else {
+            throw new Voipmanager_Exception_NotFound('can\'t publish configuration. No settings found for asterisk backend in config file!');
+        }
+
+        $filter = new Voipmanager_Model_Asterisk_ContextFilter();
+        $contexts = Voipmanager_Controller_Asterisk_Context::getInstance()->search($filter);
+        
+        $fp = fopen("php://temp", 'r+');
+        
+        foreach($contexts as $context) {
+            $filter = new Voipmanager_Model_Asterisk_VoicemailFilter(array(
+                array(
+                    'field'     => 'context_id',
+                    'operator'  => 'equals',
+                    'value'     => $context->getId()
+                )
+            ));
+            $voicemails = Voipmanager_Controller_Asterisk_Voicemail::getInstance()->search($filter);
+
+            if(count($voicemails) == 0) {
+                continue;
+            }
+
+            fputs($fp, "[" . $context->name . "]\n");
+            
+            foreach($voicemails as $voicemail) {
+                fputs($fp, sprintf("%s = %s,%s,%s\n",
+                    $voicemail->mailbox,
+                    $voicemail->password,
+                    $voicemail->fullname,
+                    $voicemail->email
+                ));
+            }
+            fputs($fp, "\n");
+        }
+        
+        rewind($fp);
+        
+        $ajam = new Ajam_Connection($url);
+        $ajam->login($username, $password);
+        $ajam->upload($url . '/tine20config', 'voicemail.conf', stream_get_contents($fp));
+        $ajam->command('voicemail reload');
+        $ajam->logout();
+    }
 }
