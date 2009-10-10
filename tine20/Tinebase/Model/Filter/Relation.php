2022813e055d3f739ac171e2443c8146278a2c53
Cornelius Weiß <c.weiss@metaways.de>
2009-10-10 12:04:31 +0000

    generic relation filter
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@10875 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/Model/Filter/Relation.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Tinebase
+ * @subpackage  Filter
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Tinebase_Model_Filter_Relation
+ * 
+ * @package     Tinebase
+ * @subpackage  Filter
+ * 
+ * filters own ids match result of related filter
+ * 
+ * @example
+        'contact'        => array('filter' => 'Tinebase_Model_Filter_Relation', 'options' => array(
+            'related_model'     => 'Addressbook_Model_Contact',
+            'related_filter'    => 'Addressbook_Model_ContactFilter'
+        )     
+ */
+class Tinebase_Model_Filter_Relation extends Tinebase_Model_Filter_Abstract
+{
+    /**
+     * @var array list of allowed operators
+     */
+    protected $_operators = array(
+        0 => 'AND',
+        1 => 'OR',
+    );
+    
+    /**
+     * appends sql to given select statement
+     *
+     * @param Zend_Db_Select                $_select
+     * @param Tinebase_Backend_Sql_Abstract $_backend
+     */
+    public function appendFilterSql($_select, $_backend)
+    {
+        $relatedFilterConstructor = $this->_options['related_filter'];
+        $relatedFilter = new $relatedFilterConstructor(array(
+            $this->_value,
+        ));
+        
+        $relatedIds = Addressbook_Controller_Contact::getInstance()->search($relatedFilter, NULL, FALSE, TRUE);
+        
+        $relationFilter = new Tinebase_Model_RelationFilter(array(
+            array('field' => 'own_model',     'operator' => 'equals', 'value' => $this->_options['related_model']),
+            array('field' => 'related_model', 'operator' => 'equals', 'value' => $this->_options['related_filter']),
+            array('field' => 'related_id',    'operator' => 'in'    , 'value' => $relatedIds)
+        ));
+        $ownIds = Tinebase_Relations::getInstance()->search($relationFilter, NULL)->own_id;
+        
+        $idField = array_key_exists('idProperty', $this->_options) ? $this->_options['idProperty'] : 'id';
+        
+        $db = $_backend->getAdapter();
+        $qField = $db->quoteIdentifier($_backend->getTableName() . '.' . $idField);
+        
+        $_select->where($db->quoteInto("$qField IN (?)", empty($ownIds) ? ' ' : $ownIds));
+     }
+     
+}
