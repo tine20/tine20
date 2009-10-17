5eb03359e0a32df3e9c60470c4b01f5f2c955671
Lars Kneschke <l.kneschke@metaways.de>
2009-10-17 06:59:56 +0000

    view for user infopage
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11019 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/views/userInfoPage.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * user info page
+ * 
+ * @package     Tinebase
+ * @subpackage  Views
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    
+        <link rel="icon" href="../../images/favicon.ico" type="image/x-icon" />
+    
+        <link rel="openid2.provider" href="<?php echo $this->escape($this->openIdUrl) ?>"/>
+        <link rel="openid.server"    href="<?php echo $this->escape($this->openIdUrl) ?>"/>        
+    </head>
+    <body>
+        This is the info page for user <?php echo $this->escape($this->username) ?>.
+    </body>
+</html>
