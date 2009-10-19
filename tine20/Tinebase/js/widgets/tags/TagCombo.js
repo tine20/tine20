3739a1c9c99eb28e42b754236667f9f212902b6a
Philipp Schüle <p.schuele@metaways.de>
2009-10-19 15:39:50 +0000

    RESOLVED - # 1772: make tag filter search a livesearch combobox http://www.tine20.org/bugtracker/view.php?id=1772
    - added onBeforeQuery function to set a 'name' filter and set minChars to 3
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11032 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/widgets/tags/TagCombo.js
+++ tine20/Tinebase/js/widgets/tags/TagCombo.js
@@ -3,9 +3,10 @@
  * 
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Cornelius Weiss <c.weiss@metaways.de>
- * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2007-2009 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
  *
+ * TODO         use new filter syntax in onBeforeQuery when TagFilter is refactored and extends Tinebase_Model_Filter_FilterGroup 
  */
  
 Ext.namespace('Tine.widgets', 'Tine.widgets.tags');
@@ -30,6 +31,7 @@ Tine.widgets.tags.TagCombo = Ext.extend(Ext.ux.form.ClearableComboBox, {
     displayField:'name',
     valueField:'id',
     width: 100,
+    minChars: 3,
     
     /**
      * @private
@@ -44,9 +46,6 @@ Tine.widgets.tags.TagCombo = Ext.extend(Ext.ux.form.ClearableComboBox, {
             fields: Tine.Tinebase.Model.Tag,
             baseParams: {
                 method: 'Tinebase.searchTags',
-                filter: Ext.util.JSON.encode({
-                    application: this.app ? this.app.appName : ''
-                }),
                 paging : Ext.util.JSON.encode({})
             }
         });
@@ -58,7 +57,24 @@ Tine.widgets.tags.TagCombo = Ext.extend(Ext.ux.form.ClearableComboBox, {
                 this.fireEvent('change', this, v, this.startValue);
             }
         }, this);
+        
+        this.on('beforequery', this.onBeforeQuery, this);
     },
+    
+    /**
+     * use beforequery to set query filter
+     * 
+     * @param {Event} qevent
+     */
+    onBeforeQuery: function(qevent){
+        var filter = {
+            name: (qevent.query && qevent.query != '') ? '%' + qevent.query + '%' : '',
+            application: this.app ? this.app.appName : ''
+        };
+        
+        this.store.baseParams.filter = Ext.util.JSON.encode(filter);
+    },
+
     
     setValue: function(value) {
         if(typeof value === 'object' && Object.prototype.toString.call(value) === '[object Object]') {
