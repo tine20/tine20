9308c7b62140704e4e3d3f160501f54f2b251983
Philipp Schüle <p.schuele@metaways.de>
2009-10-21 14:25:15 +0000

    use correct package name
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11059 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/widgets/form/ConfigPanel.js
+++ tine20/Tinebase/js/widgets/form/ConfigPanel.js
@@ -1,7 +1,7 @@
 /*
  * Tine 2.0
  * 
- * @package     Setup
+ * @package     form
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Philipp Schuele <p.schuele@metaways.de>
  * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
