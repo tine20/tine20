d3cc8981ea0e7dc031150e4a3bfd70bfda26ad8b
Lars Kneschke <l.kneschke@metaways.de>
2009-10-21 18:59:44 +0000

    OpenID login and trust layouts
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11077 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/views/openidLogin.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * OpenId login screen
+ * 
+ * @package     Tinebase
+ * @subpackage  Views
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    
+        <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+        <link rel="stylesheet" type="text/css" href="styles/tine20.css" />
+    </head>
+    <body>
+        <form method="post">
+            <fieldset>
+                <legend>OpenID Login</legend>
+                <table border=0>
+                    <tr>
+                        <td>OpenID:</td>
+                        <td>
+                            <?php echo $this->escape($this->openIdIdentity);?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Loginname:</td>
+                        <td>
+                            <input type="text"
+                                   name="openid_identifier"
+                                   size="50"
+                                   value="<?php echo $this->escape($this->loginName);?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Password:</td>
+                        <td>
+                            <input type="password" name="openid_password" value="">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>
+                            <input type="hidden" name="openid_action" value="login">
+                            <input type="submit" value="Login">
+                        </td>
+                    </tr>
+                </table>
+            </fieldset>
+        </form>
+    </body>
+</html>
---
+++ tine20/Tinebase/views/openidTrust.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * OpenId trust screen
+ * 
+ * @package     Tinebase
+ * @subpackage  Views
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    
+        <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+        <link rel="stylesheet" type="text/css" href="styles/tine20.css" />
+    </head>
+    <body>
+        <p>A site identifying as 
+        <a href="<?php echo $this->escape($this->openIdConsumer);?>">
+            <?php echo $this->escape($this->openIdConsumer);?>
+        </a> has asked us for confirmation that
+        <a href="<?php echo $this->escape($this->openIdIdentity);?>">
+            <?php echo $this->escape($this->openIdIdentity);?>
+        </a> is your identity URL.
+        </p>
+        <form method="post">
+            <input type="checkbox" name="forever">
+            <label for="forever">für immer</label><br>
+            <input type="hidden" name="openid_action" value="trust">
+            <input type="submit" name="allow" value="Allow">
+            <input type="submit" name="deny" value="Deny">
+        </form>    
+    </body>
+</html>
