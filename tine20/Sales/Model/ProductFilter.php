d725d9ff9e36ec5bce852f74038a37ac1bb34688
Philipp Schüle <p.schuele@metaways.de>
2009-10-22 13:09:05 +0000

    ASSIGNED - # 1690: move products from crm to sales http://www.tine20.org/bugtracker/view.php?id=1690
    - added id filter (needed for multi-attaching tags in grid)
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11102 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Sales/Model/ProductFilter.php
+++ tine20/Sales/Model/ProductFilter.php
@@ -37,11 +37,12 @@ class Sales_Model_ProductFilter extends Tinebase_Model_Filter_FilterGroup
      * @var array filter model fieldName => definition
      */
     protected $_filterModel = array(
-        'query'                => array(
+        'id'                    => array('filter' => 'Tinebase_Model_Filter_Id'),
+        'query'                 => array(
             'filter' => 'Tinebase_Model_Filter_Query', 
             'options' => array('fields' => array('description', 'name'))
         ),
-        'description'          => array('filter' => 'Tinebase_Model_Filter_Text'),
-        'name'                 => array('filter' => 'Tinebase_Model_Filter_Text'),
+        'description'           => array('filter' => 'Tinebase_Model_Filter_Text'),
+        'name'                  => array('filter' => 'Tinebase_Model_Filter_Text'),
     );
 }
