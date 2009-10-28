a57dd0028119142f314002810e4cb3a80d79e80e
Lars Kneschke <l.kneschke@metaways.de>
2009-10-28 11:05:58 +0000

    add lon and lat columns
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11170 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Addressbook/Setup/Update/Release2.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Addressbook
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL3
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ */
+
+class Addressbook_Setup_Update_Release2 extends Setup_Update_Abstract
+{
+    /**
+     * change all fields which store account ids from integer to string
+     * 
+     */
+    public function update_0()
+    {
+        $declaration = new Setup_Backend_Schema_Field_Xml('
+            <field>
+                <name>lon</name>
+                <type>float</type>
+            </field>');
+        $this->_backend->addCol('addressbook', $declaration);
+
+        $declaration = new Setup_Backend_Schema_Field_Xml('
+            <field>
+                <name>lat</name>
+                <type>float</type>
+            </field>');
+        $this->_backend->addCol('addressbook', $declaration);
+        
+        $this->setApplicationVersion('Addressbook', '2.1');
+    }    
+}
