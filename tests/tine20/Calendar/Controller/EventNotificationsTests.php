dcc21c7041a2971e6ea27c6e52ec4de470206361
Cornelius Weiß <c.weiss@metaways.de>
2009-11-02 13:33:01 +0000

    test notifications
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11230 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/tine20/Calendar/Controller/EventNotificationsTests.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Calendar
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Calendar_Controller_EventNotificationsTests::main');
+}
+
+/**
+ * Test class for Calendar_Controller_EventNotifications
+ * 
+ * @package     Calendar
+ */
+class Calendar_Controller_EventNotificationsTests extends Calendar_TestCase
+{
+    /**
+     * @var Calendar_Controller_Event controller unter test
+     */
+    protected $_eventController;
+    
+    /**
+     * @var Calendar_Controller_EventNotifications controller unter test
+     */
+    protected $_notificationController;
+    
+    /**
+     * @var Tinebase_Model_Container
+     */
+    protected $_testCalendar;
+    
+    public function setUp()
+    {
+        parent::setUp();
+        $this->_eventController = Calendar_Controller_Event::getInstance();
+        $this->_notificationController = Calendar_Controller_EventNotifications::getInstance();
+
+    }
+    
+    public function testInvitation()
+    {
+        $event = $this->_getEvent();
+        $event->attendee = $this->_getAttendee();
+        
+        $persitentEvent = $this->_eventController->create($event);
+        $persitentEvent = $this->_eventController->delete($persitentEvent);
+        
+        
+        // assert 'you have been invited'
+        // assert 'event has been canceled'
+    }
+    
+    public function testUpdateEmpty()
+    {
+        $event = $this->_getEvent();
+        $event->attendee = $this->_getAttendee();
+        $persitentEvent = $this->_eventController->create($event);
+        
+        // no updates
+        $updatedEvent = $this->_eventController->update($persitentEvent);
+        
+        // assert 'invited'
+        // assert no update etc.
+    }
+    
+    public function testUpdateReschedule()
+    {
+        $event = $this->_getEvent();
+        $event->attendee = $this->_getAttendee();
+        $persitentEvent = $this->_eventController->create($event);
+        
+        $persitentEvent->summary = 'reschedule notification has precedence over normal update';
+        $persitentEvent->dtstart->addHour(1);
+        $persitentEvent->dtend->addHour(1);
+        $updatedEvent = $this->_eventController->update($persitentEvent);
+        
+        // assert reschedule
+    }
+    
+    public function testUpdateDetails()
+    {
+        $event = $this->_getEvent();
+        $event->attendee = $this->_getAttendee();
+        $persitentEvent = $this->_eventController->create($event);
+        
+        $persitentEvent->summary = 'detail update notification has precedence over attendee update';
+        $persitentEvent->url = 'http://somedetail.com';
+        $persitentEvent->attendee[1]->status = Calendar_Model_Attender::STATUS_ACCEPTED;
+        
+        $updatedEvent = $this->_eventController->update($persitentEvent);
+        
+        // assert update
+        // assert update
+    }
+        
+    public function testUpdateAttendeeStatus()
+    {
+        $event = $this->_getEvent();
+        $event->attendee = $this->_getAttendee();
+        $persitentEvent = $this->_eventController->create($event);
+        
+        $persitentEvent->attendee[1]->status = Calendar_Model_Attender::STATUS_ACCEPTED;
+        
+        $updatedEvent = $this->_eventController->update($persitentEvent);
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'Calendar_Controller_EventNotificationsTests::main') {
+    Calendar_Controller_EventNotificationsTests::main();
+}
