e1fc65a22473939111323848d859ee85c63fcb2d
Cornelius Weiß <c.weiss@metaways.de>
2009-11-03 18:33:24 +0000

    role out mail subject tests - create level testcases
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11282 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/tine20/Calendar/Controller/EventNotificationsTests.php
+++ tests/tine20/Calendar/Controller/EventNotificationsTests.php
@@ -52,77 +52,176 @@ class Calendar_Controller_EventNotificationsTests extends Calendar_TestCase
         $this->_notificationController = Calendar_Controller_EventNotifications::getInstance();
         
         $this->_mailer = Tinebase_Smtp::getDefaultTransport();
+        
+        $this->_setupPreferences();
 
     }
     
     public function testInvitation()
     {
         $event = $this->_getEvent();
-        $event->attendee = $this->_getAttendee();
+        $event->attendee = $this->_getPersonaAttendee('jsmith, pwulf, sclever, jmcblack, rwright');
         
         $this->_mailer->flush();
         $persitentEvent = $this->_eventController->create($event);
-        $subject = array_value(0, $this->_mailer->getMessages())->getSubject();
-        $this->assertTrue((bool) strpos($subject, 'invit'), 'Mail subject should contain "invit" but '. $subject . ' is given');
+        //$this->_assertMail('jsmith', NULL);
+        $this->_assertMail('pwulf, sclever, jmcblack, rwright', 'invit');
         
         $this->_mailer->flush();
         $persitentEvent = $this->_eventController->delete($persitentEvent);
-        $subject = array_value(0, $this->_mailer->getMessages())->getSubject();
-        $this->assertTrue((bool) strpos($subject, 'cancel'), 'Mail subject should contain "cancel" but '. $subject . ' is given');
+        //$this->_assertMail('jsmith', NULL);
+        $this->_assertMail('pwulf, sclever, jmcblack, rwright', 'cancel');
     }
     
     public function testUpdateEmpty()
     {
         $event = $this->_getEvent();
-        $event->attendee = $this->_getAttendee();
+        $event->attendee = $this->_getPersonaAttendee('jsmith, pwulf, sclever, jmcblack, rwright');
         $persitentEvent = $this->_eventController->create($event);
         
         // no updates
+        $this->_mailer->flush();
         $updatedEvent = $this->_eventController->update($persitentEvent);
-        
-        // assert 'invited'
-        // assert no update etc.
+        $this->_assertMail('jsmith, pwulf, sclever, jmcblack, rwright', NULL);
     }
     
     public function testUpdateReschedule()
     {
         $event = $this->_getEvent();
-        $event->attendee = $this->_getAttendee();
+        $event->attendee = $this->_getPersonaAttendee('jsmith, pwulf, sclever, jmcblack, rwright');
         $persitentEvent = $this->_eventController->create($event);
         
         $persitentEvent->summary = 'reschedule notification has precedence over normal update';
         $persitentEvent->dtstart->addHour(1);
         $persitentEvent->dtend->addHour(1);
-        $updatedEvent = $this->_eventController->update($persitentEvent);
         
-        // assert reschedule
+        $this->_mailer->flush();
+        $updatedEvent = $this->_eventController->update($persitentEvent);
+        //$this->_assertMail('jsmith, pwulf', NULL);
+        $this->_assertMail('sclever, jmcblack, rwright', 'reschedul');
     }
     
     public function testUpdateDetails()
     {
         $event = $this->_getEvent();
-        $event->attendee = $this->_getAttendee();
+        $event->attendee = $this->_getPersonaAttendee('jsmith, pwulf, sclever, jmcblack, rwright');
         $persitentEvent = $this->_eventController->create($event);
         
         $persitentEvent->summary = 'detail update notification has precedence over attendee update';
         $persitentEvent->url = 'http://somedetail.com';
         $persitentEvent->attendee[1]->status = Calendar_Model_Attender::STATUS_ACCEPTED;
         
+        $this->_mailer->flush();
         $updatedEvent = $this->_eventController->update($persitentEvent);
-        
-        // assert update
-        // assert update
+        //$this->_assertMail('jsmith, pwulf, sclever', NULL);
+        $this->_assertMail('jmcblack, rwright', 'update');
     }
         
     public function testUpdateAttendeeStatus()
     {
         $event = $this->_getEvent();
-        $event->attendee = $this->_getAttendee();
+        $event->attendee = $this->_getPersonaAttendee('jsmith, pwulf, sclever, jmcblack, rwright');
         $persitentEvent = $this->_eventController->create($event);
         
         $persitentEvent->attendee[1]->status = Calendar_Model_Attender::STATUS_DECLINED;
         
+        $this->_mailer->flush();
         $updatedEvent = $this->_eventController->update($persitentEvent);
+        //$this->_assertMail('jsmith, pwulf, sclever, jmcblack', NULL);
+        $this->_assertMail('rwright', 'decline');
+    }
+    
+    /**
+     * checks if mail for persona got send
+     * 
+     * @param string $_personas
+     * @param string $_assertString
+     * @return void
+     */
+    protected function _assertMail($_personas, $_assertString = NULL)
+    {
+        foreach (explode(',', $_personas) as $personaName) {
+            $mailsForPersona = array();
+            $personaEmail = $this->_personas[trim($personaName)]->accountEmailAddress;
+            
+            foreach($this->_mailer->getMessages() as $message) {
+                if (array_value(0, $message->getRecipients()) == $personaEmail) {
+                    array_push($mailsForPersona, $message);
+                }
+            }
+            
+            $this->assertTrue(count($mailsForPersona) < 2, 'maximum one notification per action should be send!');
+            
+            if (! $_assertString) {
+                $this->assertEquals(0, count($mailsForPersona), 'No mail should be send for '. $personaName);
+            } else {
+                $subject = $mailsForPersona[0]->getSubject();
+                $this->assertTrue((bool) strpos($subject, $_assertString), 'Mail subject for ' . $personaName . ' should contain "' . $_assertString . '" but '. $subject . ' is given');
+            }
+        }
+    }
+    
+    protected function _getPersonaAttendee($_personas)
+    {
+        $attendee = new Tinebase_Record_RecordSet('Calendar_Model_Attender');
+        foreach (explode(',', $_personas) as $personaName) {
+            $attendee->addRecord(new Calendar_Model_Attender(array(
+                'user_id'        => $this->_personasContacts[trim($personaName)]->getId(),
+                'user_type'      => Calendar_Model_Attender::USERTYPE_USER,
+                'role'           => Calendar_Model_Attender::ROLE_REQUIRED,
+                'status_authkey' => Tinebase_Record_Abstract::generateUID(),
+            )));
+        }
+        
+        return $attendee;
+    }
+    
+    /**
+     * setup preferences for personas
+     * 
+     * jsmith   -> no updates
+     * pwulf    -> on invitaion/cancelation
+     * sclever  -> on reschedules
+     * jmblack  -> on updates except answers
+     * rwright  -> even on ansers
+     * 
+     * @return void
+     */
+    protected function _setupPreferences()
+    {
+        // set notification levels
+        $calPreferences = Tinebase_Core::getPreference('Calendar');
+        $calPreferences->setValueForUser(
+            Calendar_Preference::NOTIFICATION_LEVEL, 
+            Calendar_Controller_EventNotifications::NOTIFICATION_LEVEL_NONE,
+            $this->_personas['jsmith']->getId(), TRUE
+        );
+        $calPreferences->setValueForUser(
+            Calendar_Preference::NOTIFICATION_LEVEL, 
+            Calendar_Controller_EventNotifications::NOTIFICATION_LEVEL_INVITE_CANCLE,
+            $this->_personas['pwulf']->getId(), TRUE
+        );
+        $calPreferences->setValueForUser(
+            Calendar_Preference::NOTIFICATION_LEVEL, 
+            Calendar_Controller_EventNotifications::NOTIFICATION_LEVEL_EVENT_RESCHEDULE,
+            $this->_personas['sclever']->getId(), TRUE
+        );
+        $calPreferences->setValueForUser(
+            Calendar_Preference::NOTIFICATION_LEVEL, 
+            Calendar_Controller_EventNotifications::NOTIFICATION_LEVEL_EVENT_UPDATE,
+            $this->_personas['jmcblack']->getId(), TRUE
+        );
+        $calPreferences->setValueForUser(
+            Calendar_Preference::NOTIFICATION_LEVEL, 
+            Calendar_Controller_EventNotifications::NOTIFICATION_LEVEL_ATTENDEE_STATUS_UPDATE,
+            $this->_personas['rwright']->getId(), TRUE
+        );
+        
+        // set all languages to en
+        $preferences = Tinebase_Core::getPreference('Tinebase');
+        foreach ($this->_personas as $name => $account) {
+            $preferences->setValueForUser(Tinebase_Preference::LOCALE, 'en', $account->getId(), TRUE);
+        }
     }
 }
 
