3ff24e1dd2de96695d1b0af0c2b77a232b72adc5
Philipp Schüle <p.schuele@metaways.de>
2009-11-04 16:31:16 +0000

    fixed formatting
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11309 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Model/Filter/Query.php
+++ tine20/Tinebase/Model/Filter/Query.php
@@ -54,7 +54,7 @@ class Tinebase_Model_Filter_Query extends Tinebase_Model_Filter_Abstract
      */
     public function appendFilterSql($_select, $_backend)
     {
-         if ( empty($this->_value)) {
+         if (empty($this->_value)) {
              $_select->where('1=1/* empty query */');
              return;
          }
@@ -76,5 +76,4 @@ class Tinebase_Model_Filter_Query extends Tinebase_Model_Filter_Abstract
              }
          }
      }
-     
 }
