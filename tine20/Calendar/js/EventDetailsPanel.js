2582b41d45da3e5a289a646d1fc15fced8ce7b54
Cornelius Weiß <c.weiss@metaways.de>
2009-11-09 14:40:38 +0000

    ASSIGNED - # 1722: Refactor Preview Panel http://www.tine20.org/bugtracker/view.php?id=1722
    show title & container in extra area
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11362 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Calendar/js/EventDetailsPanel.js
+++ tine20/Calendar/js/EventDetailsPanel.js
@@ -30,6 +30,13 @@ Tine.Calendar.EventDetailsPanel = Ext.extend(Tine.Tinebase.widgets.grid.DetailsP
         return a.join("\n");
     },
     
+    containerRenderer: function(container) {
+        return this.containerTpl.apply({
+            color: Tine.Calendar.colorMgr.getColor(this.record).color,
+            name: Ext.util.Format.htmlEncode(container && container.name ? container.name : '')
+        });
+    },
+    
     datetimeRenderer: function(dt) {
         return String.format(this.app.i18n._("{0} {1} o'clock"), Tine.Tinebase.common.dateRenderer(dt), dt.format('H:i'));
     },
@@ -43,6 +50,14 @@ Tine.Calendar.EventDetailsPanel = Ext.extend(Tine.Tinebase.widgets.grid.DetailsP
             this.eventDetailsPanel
         ];
         
+        this.containerTpl = new Ext.XTemplate(
+            '<div class="x-tree-node-el x-tree-node-leaf x-unselectable file">',
+                '<img class="x-tree-node-icon" unselectable="on" src="', Ext.BLANK_IMAGE_URL, '">',
+                '<span style="color: {color};">&nbsp;&#9673;&nbsp</span>',
+                '<span>{name}</span>',
+            '</div>'
+        ).compile();
+        
         this.supr().initComponent.call(this);
     },
     
@@ -52,63 +67,89 @@ Tine.Calendar.EventDetailsPanel = Ext.extend(Tine.Tinebase.widgets.grid.DetailsP
             layout: 'fit',
             border: false,
             items: [{
-                layout: 'hbox',
+                layout: 'vbox',
                 border: false,
                 layoutConfig: {
-                    padding:'5',
                     align:'stretch'
                 },
-                defaults:{margins:'0 5 0 0'},
                 items: [{
-                    flex: 2,
-                    layout: 'ux.display',
-                    labelWidth: 60,
-                    layoutConfig: {
-                        background: 'solid'
-                    },
-                    items: [{
-                        xtype: 'ux.displayfield',
-                        name: 'summary',
-                        fieldLabel: this.app.i18n._('Summary')
-                    }, {
-                        xtype: 'ux.displayfield',
-                        name: 'location',
-                        fieldLabel: this.app.i18n._('Location')
-                    }, {
-                        xtype: 'ux.displayfield',
-                        name: 'dtstart',
-                        fieldLabel: this.app.i18n._('Start Time'),
-                        renderer: this.datetimeRenderer.createDelegate(this)
-                    }, {
-                        xtype: 'ux.displayfield',
-                        name: 'dtend',
-                        fieldLabel: this.app.i18n._('End Time'),
-                        renderer: this.datetimeRenderer.createDelegate(this)
-                    }]
-                }, {
-                    flex: 2,
-                    layout: 'ux.display',
-                    labelAlign: 'top',
-                    autoScroll: true,
-                    layoutConfig: {
-                        background: 'solid'
-                    },
-                    items: [{
-                        xtype: 'ux.displayfield',
-                        name: 'attendee',
-                        nl2br: true,
-                        fieldLabel: this.app.i18n._('Attendee'),
-                        renderer: this.attendeeRenderer
-                    }]
-                }, {
-                    flex: 3,
-                    layout: 'fit',
-                    
+                    layout: 'hbox',
+                    flex: 0,
+                    height: 16,
                     border: false,
+                    style: 'padding-left: 5px; padding-right: 5px',
+                    layoutConfig: {
+                        align:'stretch'
+                    },
                     items: [{
-                        cls: 'x-ux-display-background-border',
-                        xtype: 'ux.displaytextarea',
-                        name: 'description'
+                        flex: 1,
+                        xtype: 'ux.displayfield',
+                        name: 'summary'
+                        //fieldLabel: this.app.i18n._('Summary')
+                    }, {
+                        flex: 1,
+                        xtype: 'ux.displayfield',
+                        style: 'text-align: right;',
+                        name: 'container_id',
+                        htmlEncode: false,
+                        renderer: this.containerRenderer.createDelegate(this)
+                    }]
+                }, {
+                    layout: 'hbox',
+                    flex: 1,
+                    border: false,
+                    layoutConfig: {
+                        padding:'5',
+                        align:'stretch'
+                    },
+                    defaults:{margins:'0 5 0 0'},
+                    items: [{
+                        flex: 2,
+                        layout: 'ux.display',
+                        labelWidth: 60,
+                        layoutConfig: {
+                            background: 'solid'
+                        },
+                        items: [{
+                            xtype: 'ux.displayfield',
+                            name: 'dtstart',
+                            fieldLabel: this.app.i18n._('Start Time'),
+                            renderer: this.datetimeRenderer.createDelegate(this)
+                        }, {
+                            xtype: 'ux.displayfield',
+                            name: 'dtend',
+                            fieldLabel: this.app.i18n._('End Time'),
+                            renderer: this.datetimeRenderer.createDelegate(this)
+                        }, {
+                            xtype: 'ux.displayfield',
+                            name: 'location',
+                            fieldLabel: this.app.i18n._('Location')
+                        }]
+                    }, {
+                        flex: 2,
+                        layout: 'ux.display',
+                        labelAlign: 'top',
+                        autoScroll: true,
+                        layoutConfig: {
+                            background: 'solid'
+                        },
+                        items: [{
+                            xtype: 'ux.displayfield',
+                            name: 'attendee',
+                            nl2br: true,
+                            fieldLabel: this.app.i18n._('Attendee'),
+                            renderer: this.attendeeRenderer
+                        }]
+                    }, {
+                        flex: 3,
+                        layout: 'fit',
+                        
+                        border: false,
+                        items: [{
+                            cls: 'x-ux-display-background-border',
+                            xtype: 'ux.displaytextarea',
+                            name: 'description'
+                        }]
                     }]
                 }]
             }]
