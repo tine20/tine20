77b635ff39420767757311aee94fc071373ab15d
Philipp Schüle <p.schuele@metaways.de>
2009-11-12 15:14:23 +0000

    ASSIGNED - # 1872: add xls export support for reports http://www.tine20.org/bugtracker/view.php?id=1872
    - made basic xls export work
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11426 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Crm/Export/Xls.php
+++ tine20/Crm/Export/Xls.php
@@ -9,7 +9,8 @@
  * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id: Ods.php 10912 2009-10-12 14:40:25Z p.schuele@metaways.de $
  * 
- * @todo        make it work
+ * @todo        add relations, notes/history, formulas (?)
+ * @todo        use template
  */
 
 /**
@@ -28,9 +29,67 @@ class Crm_Export_Xls extends Tinebase_Export_Xls
     /**
      * export records to Xls file
      *
-     * @param Tinebase_Model_Filter_FilterGroup $_filter
+     * @param Crm_Model_LeadFilter $_filter
      * @return void
      */
-    public function generate(Tinebase_Model_Filter_FilterGroup $_filter) {
+    public function generate(Crm_Model_LeadFilter $_filter)
+    {
+        $this->_generate($_filter, Crm_Controller_Lead::getInstance());
+    }
+    
+    /**
+     * get default export config
+     * 
+     * @return array
+     */
+    protected function _getDefaultConfig()
+    {
+        return array(
+            'fields' => array(
+                'lead_name' => array(
+                    'header'    => $this->_translate->_('Lead Name'),
+                    'type'      => 'string', 
+                    'width'     => '5cm',
+                ),
+                'description' => array(
+                    'header'    => $this->_translate->_('Description'),
+                    'type'      => 'string', 
+                    'width'     => '10cm'
+                ),
+                'turnover' => array(
+                    'header'    => $this->_translate->_('Turnover'),
+                    'type'      => 'string', 
+                    'width'     => '2cm'
+                ),
+                'probability' => array(
+                    'header'    => $this->_translate->_('Probability'),
+                    'type'      => 'string', 
+                    'width'     => '2cm'
+                ),
+                'start' => array(
+                    'header'    => $this->_translate->_('Date Start'),
+                    'type'      => 'datetime', 
+                    'width'     => '2,5cm'
+                ),
+                'end' => array(
+                    'header'    => $this->_translate->_('Date End'),
+                    'type'      => 'datetime', 
+                    'width'     => '2,5cm'
+                ),
+                'end_scheduled' => array(
+                    'header'    => $this->_translate->_('Date End Scheduled'),
+                    'type'      => 'datetime', 
+                    'width'     => '2,5cm'
+                ),
+                /*
+                'created_by' => array(
+                    'header'    => $this->_translate->_('Created By'),
+                    'type'      => 'created_by', 
+                    'field'     => 'accountDisplayName', 
+                    'width'     => '4cm'
+                ),
+                */
+            )
+        );
     }
 }
--- tine20/Tinebase/Export/Xls.php
+++ tine20/Tinebase/Export/Xls.php
@@ -9,7 +9,7 @@
  * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id: Ods.php 10912 2009-10-12 14:40:25Z p.schuele@metaways.de $
  * 
- * @todo        make it work
+ * @todo        set metadata
  * @todo        allow templates
  */
 
@@ -31,12 +31,42 @@ class Tinebase_Export_Xls extends PHPExcel
     protected $_applicationName = 'Tinebase';
     
     /**
+     * export config
+     * 
+     * @var array
+     */
+    protected $_config = array();
+    
+    /**
+     * translation object
+     *
+     * @var Zend_Translate
+     */
+    protected $_translate;
+
+    /**
+     * current row number
+     * 
+     * @var integer
+     */
+    protected $_currentRowIndex = 0;
+    
+    /**
      * the constructor
      *
      */
     public function __construct()
     {
         parent::__construct();
+        
+        $this->_translate = Tinebase_Translation::getTranslation($this->_applicationName);
+        $this->_config = Tinebase_Config::getInstance()->getConfigAsArray(
+            Tinebase_Model_Config::XLSEXPORTCONFIG, 
+            $this->_applicationName, 
+            $this->_getDefaultConfig()
+        );
+        
+        $this->_currentRowIndex = 1;
     }
     
     /**
@@ -45,7 +75,72 @@ class Tinebase_Export_Xls extends PHPExcel
      * @param Tinebase_Model_Filter_FilterGroup $_filter
      * @return void
      */
-    public function generate(Tinebase_Model_Filter_FilterGroup $_filter)
+    protected function _generate(Tinebase_Model_Filter_FilterGroup $_filter, Tinebase_Controller_SearchInterface $_controller)
     {
+        $this->_addHead();
+        
+        $records = $_controller->search($_filter);
+        $this->_addBody($records);
+    }
+    
+    /**
+     * add xls head (headline, column styles)
+     */
+    protected function _addHead()
+    {
+        $columnId = 0;
+        foreach($this->_config['fields'] as $field) {
+            $this->getActiveSheet()->setCellValueByColumnAndRow($columnId++, $this->_currentRowIndex, $field['header']);
+        }
+        
+        $this->_currentRowIndex++;
+    }
+    
+    /**
+     * add body rows
+     *
+     * @param OpenDocument_SpreadSheet_Table $table
+     * @param Tinebase_Record_RecordSet $records
+     * @param array
+     * 
+     * @todo add more different data types
+     * @todo add different styles?
+     */
+    protected function _addBody($_records)
+    {
+        $locale = Tinebase_Core::get(Tinebase_Core::LOCALE);
+        
+        // add record rows
+        $i = 0;
+        foreach ($_records as $record) {
+            
+            //Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' ' . print_r($record->toArray(), true));
+            
+            $columnId = 0;
+            foreach ($this->_config['fields'] as $key => $params) {
+                switch($params['type']) {
+                    case 'datetime':
+                        $value = ($record->$key) ? $record->$key->toString(Zend_Locale_Format::getDateFormat($locale), $locale) : '';
+                        break;
+                    default:
+                        $value = $record->$key;
+                }
+                
+                $this->getActiveSheet()->setCellValueByColumnAndRow($columnId++, $this->_currentRowIndex, $value);
+            }
+            
+            $i++;
+            $this->_currentRowIndex++;
+        }
+    }
+    
+    /**
+     * get default export config
+     * 
+     * @return array
+     */
+    protected function _getDefaultConfig()
+    {
+        return array();
     }
 }
