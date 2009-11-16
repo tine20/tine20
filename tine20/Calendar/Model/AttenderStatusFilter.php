c393aff366bbbca1f55df3cb4ead4c6712623f37
Cornelius Weiß <c.weiss@metaways.de>
2009-11-16 15:00:44 +0000

    Incomplete - # 1548: don't show declined events per default http://www.tine20.org/bugtracker/view.php?id=1548
    new attendee status filter
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11490 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Calendar/Model/AttenderStatusFilter.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Calendar
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+
+/**
+ * filters for events whose attendee have a certain status
+ * 
+ * 
+ * @package     Calendar
+ */
+class Calendar_Model_AttenderStatusFilter extends Tinebase_Model_Filter_Text 
+{
+    /**
+     * @var array list of allowed operators
+     */
+    protected $_operators = array(
+        0 => 'equals',
+        1 => 'not',
+    );
+    
+    /**
+     * returns quoted column name for sql backend
+     *
+     * @param  Tinebase_Backend_Sql_Interface $_backend
+     * @return string
+     * 
+     * @todo to be removed once we split filter model / backend
+     */
+    protected function _getQuotedFieldName($_backend) {
+        return $_backend->getAdapter()->quoteIdentifier(
+            'attendee.status'
+        );
+    }
+}
