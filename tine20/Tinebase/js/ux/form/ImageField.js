380a55d87ed5d5625620fa29b39b0eeead2b81dc
Cornelius Weiß <c.weiss@metaways.de>
2009-11-23 08:10:08 +0000

    use predefined loadMask to make safari happy
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11654 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/ux/form/ImageField.js
+++ tine20/Tinebase/js/ux/form/ImageField.js
@@ -62,6 +62,8 @@ Ext.ux.form.ImageField = Ext.extend(Ext.form.Field, {
         this.buttonCt.setSize(this.width, this.height);
         this.buttonCt.on('contextmenu', this.onContextMenu, this);
         
+        this.loadMask = new Ext.LoadMask(this.buttonCt, {msg: _('Loading'), msgCls: 'x-mask-loading'});
+        
         // the click to edit text container
         var clickToEditText = _('click to edit');
         this.textCt = Ext.DomHelper.insertFirst(this.buttonCt, '<div class="x-ux-from-imagefield-text">' + clickToEditText + '</div>', true);
@@ -83,16 +85,6 @@ Ext.ux.form.ImageField = Ext.extend(Ext.form.Field, {
             buttonCt: this.buttonCt,
             renderTo: this.buttonCt
         });
-        
-        /*
-        this.bb = new Ext.ux.form.BrowseButton({
-            //debug: true,
-            buttonCt: this.buttonCt,
-            renderTo: this.buttonCt,
-            scope: this,
-            handler: this.onFileSelect
-        });
-        */
     },
     getValue: function() {
         var value = Ext.ux.form.ImageField.superclass.getValue.call(this);
@@ -123,7 +115,6 @@ Ext.ux.form.ImageField = Ext.extend(Ext.form.Field, {
             return;
         }
         uploader.on('uploadcomplete', function(uploader, record){
-            //var method = Ext.util.Format.htmlEncode('');
             this.imageSrc = new Ext.ux.util.ImageURL({
                 id: record.get('tempFile').id,
                 width: this.width,
@@ -136,7 +127,7 @@ Ext.ux.form.ImageField = Ext.extend(Ext.form.Field, {
         }, this);
         uploader.on('uploadfailure', this.onUploadFail, this);
         
-        this.buttonCt.mask(_('Loading'), 'x-mask-loading');
+        this.loadMask.show();
         uploader.upload();
         
         if (this.ctxMenu) {
@@ -165,21 +156,6 @@ Ext.ux.form.ImageField = Ext.extend(Ext.form.Field, {
             scope: this,
             plugins: [new Ext.ux.file.BrowsePlugin({})]
         });
-        /*
-        upload.on('render', function(){
-            var ct = upload.getEl();
-            var bb = new Ext.ux.form.BrowseButton({
-                buttonCt: ct,
-                renderTo: ct,
-                //debug: true,
-                scope: this,
-                handler: function(bb) {
-                    this.ctxMenu.hide();
-                    this.onFileSelect(bb);
-                }
-            });
-        }, this);
-        */
         
         this.ctxMenu = new Ext.menu.Menu({
             items: [
@@ -251,11 +227,11 @@ Ext.ux.form.ImageField = Ext.extend(Ext.form.Field, {
                 this.imageCt = img;
                 this.textCt.setVisible(this.imageSrc == this.defaultImage);
                 this.imageCt.setOpacity(this.imageSrc == this.defaultImage ? 0.2 : 1);
-                this.buttonCt.unmask();
+                this.loadMask.hide();
             }, this);
             img.on('error', function() {
                 Ext.MessageBox.alert(_('Image Failed'), _('Could not load image. Please notify your Administrator')).setIcon(Ext.MessageBox.ERROR);
-                this.buttonCt.unmask();
+                this.loadMask.hide();
             }, this);
         }
     }
