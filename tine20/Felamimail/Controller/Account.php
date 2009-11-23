c8b16f3bd548edcf06447f12b02303be8e7b648f
Philipp Schüle <p.schuele@metaways.de>
2009-11-23 15:07:45 +0000

    catch Felamimail_Exception_InvalidCredentials exceptions
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11664 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/Controller/Account.php
+++ tine20/Felamimail/Controller/Account.php
@@ -411,6 +411,12 @@ class Felamimail_Controller_Account extends Tinebase_Controller_Record_Abstract
                     . '(' . $zmpe->getMessage() . ')'
                 );
                 return $_account;
+            } catch (Felamimail_Exception_InvalidCredentials $zmpe) {
+                Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ 
+                    . ' Wrong user credentials ... '
+                    . '(' . $zmpe->getMessage() . ')'
+                );
+                return $_account;
             }
         }
         
