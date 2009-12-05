5cad1a3298c697dc3b57656d726ffe71993a7436
Lars Kneschke <l.kneschke@metaways.de>
2009-12-05 21:04:40 +0000

    widget to display maps from openstreetmap
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11842 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/js/widgets/MapPanel.js
@@ -0,0 +1,39 @@
+/*
+ * Tine 2.0
+ * 
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+ 
+Ext.namespace('Tine.widgets');
+
+/**
+ * Widget to display maps
+ */
+Tine.widgets.MapPanel = Ext.extend(GeoExt.MapPanel, {
+	zoom: 4,
+	map: new OpenLayers.Map(),
+	layers: [new OpenLayers.Layer.OSM()],
+	
+    /**
+     * @private
+     */
+    initComponent: function() {
+        //this.store = this.getCountryStore();
+		if(this.center instanceof Array) {
+	        this.center = new OpenLayers.LonLat(this.center[0], this.center[1]).transform(new OpenLayers.Projection("EPSG:4326"), new OpenLayers.Projection("EPSG:900913"));
+	    }
+
+        Tine.widgets.MapPanel.superclass.initComponent.call(this);   
+    },
+    
+    setCenter: function(lon, lat) {
+    	this.center = new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection("EPSG:4326"), this.map.getProjectionObject());
+    	this.map.setCenter(this.center);
+    }
+});
+
+Ext.reg('widget-mappanel', Tine.widgets.MapPanel);
