bad6bbc94d5af559ae96790c8ed641d40879bdce
Cornelius Weiß <c.weiss@metaways.de>
2009-12-10 20:36:04 +0000

    add a marker to the map
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11957 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/widgets/MapPanel.js
+++ tine20/Tinebase/js/widgets/MapPanel.js
@@ -23,7 +23,9 @@ Tine.widgets.MapPanel = Ext.extend(GeoExt.MapPanel, {
      */
     initComponent: function() {
         this.map =  new OpenLayers.Map();
-        this.layers = [new OpenLayers.Layer.OSM()];
+        this.layers = [
+            new OpenLayers.Layer.OSM()
+        ];
         
 		if(this.center instanceof Array) {
 	        this.center = new OpenLayers.LonLat(this.center[0], this.center[1]).transform(new OpenLayers.Projection("EPSG:4326"), new OpenLayers.Projection("EPSG:900913"));
@@ -35,6 +37,15 @@ Tine.widgets.MapPanel = Ext.extend(GeoExt.MapPanel, {
     setCenter: function(lon, lat) {
     	this.center = new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection("EPSG:4326"), this.map.getProjectionObject());
     	this.map.setCenter(this.center);
+        
+        // add a marker
+        var size = new OpenLayers.Size(32,32);
+        var offset = new OpenLayers.Pixel(0, -size.h);
+        var icon = new OpenLayers.Icon('images/oxygen/32x32/actions/flag-red.png', size, offset);
+        
+        var markers = new OpenLayers.Layer.Markers( "Markers" );
+        markers.addMarker(new OpenLayers.Marker(this.center, icon));
+        this.map.addLayer(markers);
     }
 });
 
