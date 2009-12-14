4724cbe2a301123d9206a809f016bb3b22ff8995
Philipp Schüle <p.schuele@metaways.de>
2009-12-14 14:33:56 +0000

    removed unused function
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@11986 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Setup/Backend/Schema/Table/Xml.php
+++ tine20/Setup/Backend/Schema/Table/Xml.php
@@ -32,29 +32,4 @@ class Setup_Backend_Schema_Table_Xml extends Setup_Backend_Schema_Table_Abstract
             }
         }
     }    
-    
-    public function setIndices($_declaration)
-    {
-        foreach ($_declaration as $key => $val) {
-            if ($key != 'field' && $key != 'reference') {
-                $this->$key = (string) $val;
-                
-            // field definition is stored in SimpleXMLElement in quite different ways, depending on quantity
-            } else if ($key == 'field') {
-                if ($val instanceof SimpleXMLElement) {
-                    $this->field[] = (string) $val->name;
-                } else {
-                    $this->field = (string) $val;
-                }
-            
-            // reduce complexity of storage of foreign keys 
-            } else if ($key == 'reference') {
-                $this->referenceTable = $val->table;
-                $this->referenceField = $val->field;
-                $this->referenceOnUpdate = $val->onupdate;
-                $this->referenceOnDelete= $val->ondelete;
-                $this->field = $this->field[0];
-            }
-        }
-    }
 }
