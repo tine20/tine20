ee391f88a7eb1995c02d88172299ac7c757ff6eb
Philipp Schüle <p.schuele@metaways.de>
2009-12-16 12:10:16 +0000

    set precedence to bulk to avoid triggering autoresponders
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@12019 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Notification/Backend/Smtp.php
+++ tine20/Tinebase/Notification/Backend/Smtp.php
@@ -68,7 +68,11 @@ class Tinebase_Notification_Backend_Smtp implements Tinebase_Notification_Interf
             $mail->setBodyHtml($_messageHtml);
         }
         
+        // set mail generator
         $mail->addHeader('X-MailGenerator', 'Tine 2.0');
+        
+        // don't reply to this mail, dear autoresponder ... :)
+        $mail->addHeader('Precedence', 'bulk');
 
         if (empty($this->_fromAddress)) {
             Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ . ' No notification service address set. Could not send notification.');
