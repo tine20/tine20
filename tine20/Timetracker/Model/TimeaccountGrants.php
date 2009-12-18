72a88c00733572a021f91fdb195743fd21796a06
Philipp Schüle <p.schuele@metaways.de>
2009-12-18 11:31:13 +0000

    we need to convert the cache id because of possible unallowed chars in user ids
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@12057 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Timetracker/Model/TimeaccountGrants.php
+++ tine20/Timetracker/Model/TimeaccountGrants.php
@@ -207,7 +207,7 @@ class Timetracker_Model_TimeaccountGrants extends Tinebase_Record_Abstract
     public static function getGrantsOfAccount($_accountId, $_timeaccountId, $_ignoreAcl = FALSE)
     {
         $cache = Tinebase_Core::get(Tinebase_Core::CACHE);
-        $cacheId = 'getGrantsOfAccount' . Tinebase_Model_User::convertUserIdToInt($_accountId) . (($_timeaccountId instanceof Timetracker_Model_Timeaccount) ? $_timeaccountId->getId() : $_timeaccountId) . $_ignoreAcl;
+        $cacheId = convertCacheId('getGrantsOfAccount' . Tinebase_Model_User::convertUserIdToInt($_accountId) . (($_timeaccountId instanceof Timetracker_Model_Timeaccount) ? $_timeaccountId->getId() : $_timeaccountId) . $_ignoreAcl);
         $result = $cache->load($cacheId);
         
         if (!$result) {
@@ -245,7 +245,7 @@ class Timetracker_Model_TimeaccountGrants extends Tinebase_Record_Abstract
         //Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' get grant: ' . print_r($_grant, true));
         
         $cache = Tinebase_Core::get(Tinebase_Core::CACHE);
-        $cacheId = 'getTimeaccountsByAcl' . Tinebase_Core::getUser()->getId() . $_grant . $_onlyIds;
+        $cacheId = convertCacheId('getTimeaccountsByAcl' . Tinebase_Core::getUser()->getId() . $_grant . $_onlyIds);
         $result = $cache->load($cacheId);
         
         if (!$result) {
@@ -297,7 +297,7 @@ class Timetracker_Model_TimeaccountGrants extends Tinebase_Record_Abstract
         }
         
         $cache = Tinebase_Core::get(Tinebase_Core::CACHE);
-        $cacheId = 'getTimeaccountGrants' . Tinebase_Core::getUser()->getId() . $_timeaccount->getId() . $_ignoreACL;
+        $cacheId = convertCacheId('getTimeaccountGrants' . Tinebase_Core::getUser()->getId() . $_timeaccount->getId() . $_ignoreACL);
         $result = $cache->load($cacheId);
         
         if (!$result) {
