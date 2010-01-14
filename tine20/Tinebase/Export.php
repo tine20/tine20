015bb32823fe4ba151cf406da2239ab0d1bd5e04
Philipp Schüle <p.schuele@metaways.de>
2010-01-14 12:59:56 +0000

    ASSIGNED - # 2084: move (ods) export configs to import_export definitions http://www.tine20.org/bugtracker/view.php?id=2084
    - added export factory
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@12254 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/Export.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Tinebase
+ * @subpackage  Export
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Export Factory Class
+ *
+ * @package     Tinebase
+ * @subpackage  Export
+ */
+class Tinebase_Export
+{
+    /**
+     * get export object for given filter and format
+     * 
+     * @param Tinebase_Model_Filter_FilterGroup $_filter
+     * @param string $_format
+     * @param Tinebase_Controller_Record_Interface $_controller (optional)
+     * @param array $_additionalOptions (optional)
+     * @return Tinebase_Export_Abstract
+     * @throws Tinebase_Exception_NotFound
+     */
+    public static function factory($_filter, $_format, $_controller = NULL, $_additionalOptions = array()) 
+    {
+        $appName = $_filter->getApplicationName();
+        $model = $_filter->getModelName();
+        
+        $exportClass = $appName . '_Export_' . ucfirst(strtolower($_format));
+        if (! @class_exists($exportClass)) {
+            
+            // check for model specific export class
+            list($a, $b, $modelPart) = explode('_', $model);
+            $exportClass = $exportClass . '_' . $modelPart;
+            
+            if (! class_exists($exportClass)) {
+                throw new Tinebase_Exception_NotFound('No ' . $_format . ' export class found for ' . $appName . ' / ' . $model);
+            }
+        }
+        $result = new $exportClass($_filter, $_controller, $_additionalOptions);
+        
+        return $result;
+    }
+}
