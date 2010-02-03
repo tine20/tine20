20ec7d9e8c778a636f3aa726bfd9d5e34499c71a
Philipp Schüle <p.schuele@metaways.de>
2010-02-03 12:58:39 +0000

    check for existance
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@12627 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Crm/Export/Helper.php
+++ tine20/Crm/Export/Helper.php
@@ -42,12 +42,22 @@ class Crm_Export_Helper
             case 'source':
                 $settings = Crm_Controller::getInstance()->getSettings();
                 $source = $settings->getOptionById($_record->leadsource_id, 'leadsources');
-                $value = $source['leadsource'];
+                if (isset($source['leadsource'])) {
+                    $value = $source['leadsource'];
+                } else {
+                    Tinebase_Core::getLogger()->notice(__METHOD__ . '::' . __LINE__ . ' Leadsource id not found:' . $_record->leadsource_id); 
+                    $value = '';
+                }
                 break;
             case 'type':
                 $settings = Crm_Controller::getInstance()->getSettings();
                 $type = $settings->getOptionById($_record->leadtype_id, 'leadtypes');
-                $value = $type['leadtype'];                
+                if (isset($type['leadtype'])) {
+                    $value = $type['leadtype'];
+                } else {
+                    Tinebase_Core::getLogger()->notice(__METHOD__ . '::' . __LINE__ . ' Leadtype id not found:' . $_record->leadtype_id); 
+                    $value = '';
+                }
                 break;
             default:
                 $value = '';
