f8bb7492cb4717bb2e8cd166e38f9eb5c9c742a8
Cornelius Weiß <c.weiss@metaways.de>
2010-02-04 11:44:54 +0000

    Incomplete - # 1936: Add private-option to any appointment http://www.tine20.org/bugtracker/view.php?id=1936
    rework class handling
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@12647 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Calendar/Setup/Update/Release3.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Calendar
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL3
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+
+class Calendar_Setup_Update_Release3 extends Setup_Update_Abstract
+{
+    /**
+     * migragte from class_id (not used) to class
+     */
+    public function update_0()
+    {
+        $this->_backend->dropCol('cal_events', 'class_id');
+        
+        $declaration = new Setup_Backend_Schema_Field_Xml('
+            <field>
+                <name>class</name>
+                <type>text</type>
+                <length>40</length>
+                <default>PUBLIC</default>
+                <notnull>true</notnull>
+            </field>');
+        $this->_backend->addCol('cal_events', $declaration, 11);
+        
+        $this->setTableVersion('cal_events', 3);
+        $this->setApplicationVersion('Calendar', '3.1');
+    }
+}
