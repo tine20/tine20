756c91e6a464be29c78a37be714dc153b7acb1eb
Cornelius Weiß <c.weiss@metaways.de>
2010-02-05 11:20:12 +0000

    Incomplete - # 1936: Add private-option to any appointment http://www.tine20.org/bugtracker/view.php?id=1936
    add private/export and sync grants to backend
    have translated descriptions
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@12682 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Model/Grants.php
+++ tine20/Tinebase/Model/Grants.php
@@ -1,46 +1,62 @@
 <?php
 /**
- * class to handle grants
+ * grants model of a container
  * 
  * @package     Tinebase
  * @subpackage  Record
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @copyright   Copyright (c) 2007-2010 Metaways Infosystems GmbH (http://www.metaways.de)
- * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
  * @version     $Id$
  */
 
 /**
- * defines the datatype for one application
+ * grants model of a container
  * 
  * @package     Tinebase
  * @subpackage  Record
- *  */
+ *  
+ */
 class Tinebase_Model_Grants extends Tinebase_Record_Abstract
 {
     
     /**
-     * read grant to all records of a container / a single record
+     * grant to read all records of a container / a single record
      */
     const GRANT_READ     = 'readGrant';
     
     /**
-     * add grant to a container
+     * grant to add a record to a container
      */
     const GRANT_ADD      = 'addGrant';
     
     /**
-     * edit grant to all records of a container / a single record
+     * grant to edit all records of a container / a single record
      */
     const GRANT_EDIT     = 'editGrant';
     
     /**
-     * delete grant to all records of a container / a single record
+     * grant to delete  all records of a container / a single record
      */
     const GRANT_DELETE   = 'deleteGrant';
     
     /**
-     * admin grant to a container
+     * grant to _access_ records marked as private (GRANT_X = GRANT_X * GRANT_PRIVATE)
+     */
+    const GRANT_PRIVATE = 'privateGrant';
+    
+    /**
+     * grant to export all records of a container / a single record
+     */
+    const GRANT_EXPORT = 'exportGrant';
+    
+    /**
+     * grant to sync all records of a container / a single record
+     */
+    const GRANT_SYNC = 'syncGrant';
+    
+    /**
+     * grant to administrate a container
      */
     const GRANT_ADMIN    = 'adminGrant';
     
@@ -58,26 +74,6 @@ class Tinebase_Model_Grants extends Tinebase_Record_Abstract
      * @var string
      */
     protected $_application = 'Tinebase';
-    
-    /**
-     * list of zend inputfilter
-     * 
-     * this filter get used when validating user generated content with Zend_Filter_Input
-     *
-     * @var array
-     */
-    protected $_filters = array(
-        //'*'      => 'StringTrim'
-    );
-
-    /**
-     * list of zend validator
-     * 
-     * this validators get used when validating user generated content with Zend_Filter_Input
-     *
-     * @var array
-     */
-    protected $_validators = array();
 
     public function __construct($_data = NULL, $_bypassFilters = FALSE, $_convertDates = NULL)
     {
@@ -102,7 +98,7 @@ class Tinebase_Model_Grants extends Tinebase_Record_Abstract
     /**
      * get all possible grants
      *
-     * @return  array   all application prefs
+     * @return  array   all container grants
      */
     public function getAllGrants()
     {
@@ -111,9 +107,60 @@ class Tinebase_Model_Grants extends Tinebase_Record_Abstract
             self::GRANT_ADD,
             self::GRANT_EDIT,
             self::GRANT_DELETE,
+            self::GRANT_PRIVATE,
+            self::GRANT_EXPORT,
+            self::GRANT_SYNC,
             self::GRANT_ADMIN,
         );
     
         return $allGrants;
     }
+    
+    /**
+     * get translated grants
+     * 
+     * @return  array with translated descriptions for the containers grants
+     */
+    public function getTranslatedGrants()
+    {
+        $translate = Tinebase_Translation::getTranslation($this->_application);
+
+        $descriptions = array(
+            self::GRANT_READ  => array(
+                'label'         => $translate->_('Read'),
+                'description'   => $translate->_('The grant to read records of this container'),
+            ),
+            self::GRANT_ADD  => array(
+                'label'         => $translate->_('Add'),
+                'description'   => $translate->_('The grant to add records to this container'),
+            ),
+            self::GRANT_EDIT => array(
+                'label'         => $translate->_('Edit'),
+                'description'   => $translate->_('The grant to edit records in this container'),
+            ),
+            self::GRANT_DELETE => array(
+                'label'         => $translate->_('Delete'),
+                'description'   => $translate->_('The grant to delete records in this container'),
+            ),
+            self::GRANT_PRIVATE => array(
+                'label'         => $translate->_('Private'),
+                'description'   => $translate->_('The grant to access records marked as private in this container'),
+            ),
+            self::GRANT_EXPORT => array(
+                'label'         => $translate->_('Export'),
+                'description'   => $translate->_('The grant to export records from this container'),
+            ),
+            self::GRANT_SYNC => array(
+                'label'         => $translate->_('Sync'),
+                'description'   => $translate->_('The grant to synchronise records with this container'),
+            ),
+            self::GRANT_ADMIN => array(
+                'label'         => $translate->_('Admin'),
+                'description'   => $translate->_('The grant to administrate this container'),
+            ),
+        );
+        
+        return $descriptions;
+    }
+    
 }
