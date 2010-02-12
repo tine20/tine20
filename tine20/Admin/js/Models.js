5d9ee04e59b9450c59c7c71269afe06f71a534e0
Philipp Schüle <p.schuele@metaways.de>
2010-02-12 16:12:37 +0000

    moved user models to Models.js
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@12801 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Admin/js/Models.js
+++ tine20/Admin/js/Models.js
@@ -4,7 +4,7 @@
  * @package     Admin
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Philipp Schuele <p.schuele@metaways.de>
- * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2009-2010 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id: Models.js 10409 2009-09-11 12:23:23Z p.schuele@metaways.de $
  *
  */
@@ -26,3 +26,91 @@ Tine.Admin.Model.TagRight = Ext.data.Record.create([
     {name: 'view_right', type: 'boolean'},
     {name: 'use_right',  type: 'boolean'}
 ]);
+
+/**
+ * Model of an account
+ */
+Tine.Admin.Model.UserArray = [
+    { name: 'accountId' },
+    { name: 'accountFirstName' },
+    { name: 'accountLastName' },
+    { name: 'accountLoginName' },
+    { name: 'accountPassword' },
+    { name: 'accountDisplayName' },
+    { name: 'accountFullName' },
+    { name: 'accountStatus' },
+    { name: 'accountPrimaryGroup' },
+    { name: 'accountExpires', type: 'date', dateFormat: Date.patterns.ISO8601Long },
+    { name: 'accountLastLogin', type: 'date', dateFormat: Date.patterns.ISO8601Long },
+    { name: 'accountLastPasswordChange', type: 'date', dateFormat: Date.patterns.ISO8601Long },
+    { name: 'accountLastLoginfrom' },
+    { name: 'accountEmailAddress' },
+    { name: 'accountHomeDirectory' },
+    { name: 'accountLoginShell' },
+    { name: 'openid'},
+    { name: 'visibility'},
+    { name: 'sambaSAM' },
+    { name: 'emailUser' }
+];
+
+Tine.Admin.Model.User = Tine.Tinebase.data.Record.create(Tine.Admin.Model.UserArray, {
+    appName: 'Admin',
+    modelName: 'User',
+    idProperty: 'accountId',
+    titleProperty: 'accountDisplayName',
+    // ngettext('User', 'Users', n);
+    recordName: 'User',
+    recordsName: 'Users'
+});
+
+Tine.Admin.Model.SAMUserArray = [
+    { name: 'sid'              },
+    { name: 'primaryGroupSID'  },
+    { name: 'acctFlags'        },
+    { name: 'homeDrive'        },
+    { name: 'homePath'         },
+    { name: 'profilePath'      },
+    { name: 'logonScript'      },
+    { name: 'logonTime',     type: 'date', dateFormat: Date.patterns.ISO8601Long },
+    { name: 'logoffTime',    type: 'date', dateFormat: Date.patterns.ISO8601Long },
+    { name: 'kickoffTime',   type: 'date', dateFormat: Date.patterns.ISO8601Long },
+    { name: 'pwdLastSet',    type: 'date', dateFormat: Date.patterns.ISO8601Long },
+    { name: 'pwdCanChange',  type: 'date', dateFormat: Date.patterns.ISO8601Long },
+    { name: 'pwdMustChange', type: 'date', dateFormat: Date.patterns.ISO8601Long }
+];
+
+Tine.Admin.Model.SAMUser = Tine.Tinebase.data.Record.create(Tine.Admin.Model.SAMUserArray, {
+    appName: 'Admin',
+    modelName: 'SAMUser',
+    idProperty: 'sid',
+    titleProperty: null,
+    // ngettext('Samba User', 'Samba Users', n);
+    recordName: 'Samba User',
+    recordsName: 'Samba Users'
+});
+
+Tine.Admin.Model.EmailUserArray = [
+    { name: 'emailUID' },
+    { name: 'emailGID' },
+    { name: 'emailMailQuota' },
+    { name: 'emailMailSize' },
+    { name: 'emailSieveQuota' },
+    { name: 'emailSieveSize' },
+    { name: 'emailLastLogin', type: 'date', dateFormat: Date.patterns.ISO8601Long },
+    { name: 'emailUserId' },
+    { name: 'emailAliases' },
+    { name: 'emailForwards' },
+    { name: 'emailForwardOnly' },
+    { name: 'emailAddress' },
+    { name: 'emailUsername' }
+];
+
+Tine.Admin.Model.EmailUser = Tine.Tinebase.data.Record.create(Tine.Admin.Model.EmailUserArray, {
+    appName: 'Admin',
+    modelName: 'EmailUser',
+    idProperty: 'sid',
+    titleProperty: null,
+    // ngettext('Email User', 'Email Users', n);
+    recordName: 'Email User',
+    recordsName: 'Email Users'
+});
--- tine20/Admin/js/Users.js
+++ tine20/Admin/js/Users.js
@@ -5,7 +5,7 @@
  * @subpackage  User
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Cornelius Weiss <c.weiss@metaways.de>
- * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2007-2010 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
  *
  * TODO         add all actions to toolbar (some are only in ctx menu at the moment) 
@@ -454,98 +454,6 @@ Tine.Admin.Users.Main = function() {
     };
     
 }();
-
-/************** models *****************/
-
-Ext.ns('Tine.Admin.Model');
-
-/**
- * Model of an account
- */
-Tine.Admin.Model.UserArray = [
-    { name: 'accountId' },
-    { name: 'accountFirstName' },
-    { name: 'accountLastName' },
-    { name: 'accountLoginName' },
-    { name: 'accountPassword' },
-    { name: 'accountDisplayName' },
-    { name: 'accountFullName' },
-    { name: 'accountStatus' },
-    { name: 'accountPrimaryGroup' },
-    { name: 'accountExpires', type: 'date', dateFormat: Date.patterns.ISO8601Long },
-    { name: 'accountLastLogin', type: 'date', dateFormat: Date.patterns.ISO8601Long },
-    { name: 'accountLastPasswordChange', type: 'date', dateFormat: Date.patterns.ISO8601Long },
-    { name: 'accountLastLoginfrom' },
-    { name: 'accountEmailAddress' },
-    { name: 'accountHomeDirectory' },
-    { name: 'accountLoginShell' },
-    { name: 'openid'},
-    { name: 'visibility'},
-    { name: 'sambaSAM' },
-    { name: 'emailUser' }
-];
-
-Tine.Admin.Model.User = Tine.Tinebase.data.Record.create(Tine.Admin.Model.UserArray, {
-    appName: 'Admin',
-    modelName: 'User',
-    idProperty: 'accountId',
-    titleProperty: 'accountDisplayName',
-    // ngettext('User', 'Users', n);
-    recordName: 'User',
-    recordsName: 'Users'
-});
-
-Tine.Admin.Model.SAMUserArray = [
-    { name: 'sid'              },
-    { name: 'primaryGroupSID'  },
-    { name: 'acctFlags'        },
-    { name: 'homeDrive'        },
-    { name: 'homePath'         },
-    { name: 'profilePath'      },
-    { name: 'logonScript'      },
-    { name: 'logonTime',     type: 'date', dateFormat: Date.patterns.ISO8601Long },
-    { name: 'logoffTime',    type: 'date', dateFormat: Date.patterns.ISO8601Long },
-    { name: 'kickoffTime',   type: 'date', dateFormat: Date.patterns.ISO8601Long },
-    { name: 'pwdLastSet',    type: 'date', dateFormat: Date.patterns.ISO8601Long },
-    { name: 'pwdCanChange',  type: 'date', dateFormat: Date.patterns.ISO8601Long },
-    { name: 'pwdMustChange', type: 'date', dateFormat: Date.patterns.ISO8601Long }
-];
-
-Tine.Admin.Model.SAMUser = Tine.Tinebase.data.Record.create(Tine.Admin.Model.SAMUserArray, {
-    appName: 'Admin',
-    modelName: 'SAMUser',
-    idProperty: 'sid',
-    titleProperty: null,
-    // ngettext('Samba User', 'Samba Users', n);
-    recordName: 'Samba User',
-    recordsName: 'Samba Users'
-});
-
-Tine.Admin.Model.EmailUserArray = [
-    { name: 'emailUID' },
-    { name: 'emailGID' },
-    { name: 'emailMailQuota' },
-    { name: 'emailMailSize' },
-    { name: 'emailSieveQuota' },
-    { name: 'emailSieveSize' },
-    { name: 'emailLastLogin', type: 'date', dateFormat: Date.patterns.ISO8601Long },
-    { name: 'emailUserId' },
-    { name: 'emailAliases' },
-    { name: 'emailForwards' },
-    { name: 'emailForwardOnly' },
-    { name: 'emailAddress' },
-    { name: 'emailUsername' }
-];
-
-Tine.Admin.Model.EmailUser = Tine.Tinebase.data.Record.create(Tine.Admin.Model.EmailUserArray, {
-    appName: 'Admin',
-    modelName: 'EmailUser',
-    idProperty: 'sid',
-    titleProperty: null,
-    // ngettext('Email User', 'Email Users', n);
-    recordName: 'Email User',
-    recordsName: 'Email Users'
-});
 
 /************** backends *****************/
 
