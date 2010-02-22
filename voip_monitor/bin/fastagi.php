caf1fe0dfb15af896d289ece627cd09ca989bfd0
Lars Kneschke <l.kneschke@metaways.de>
2010-02-22 07:49:24 +0000

    renamed to fastagi.php
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@12890 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ voip_monitor/bin/fastagi.php
@@ -0,0 +1,21 @@
+#!/usr/bin/env php
+<?php
+/**
+ * FastAGI Deamon
+ * 
+ * @package     VoipMonitor
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ */
+
+set_include_path(dirname(__FILE__) . PATH_SEPARATOR . dirname(__FILE__) . '/library' . PATH_SEPARATOR . get_include_path());
+
+require_once 'Zend/Loader/Autoloader.php';
+$autoloader = Zend_Loader_Autoloader::getInstance();
+$autoloader->setFallbackAutoloader(true);
+
+$fastAgi = new FastAGI();
+
+$fastAgi->run();
