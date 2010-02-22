19933e04e89fc0e6029ce7d72a6814a0e74b4701
Philipp Schüle <p.schuele@metaways.de>
2010-02-22 16:32:42 +0000

    RESOLVED - # 2320: add call forwarding options to sip peer edit dialog http://www.tine20.org/bugtracker/view.php?id=2320 - finished line grid panel with call forward form
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@12906 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Voipmanager/js/Snom/LineGridPanel.js
+++ tine20/Voipmanager/js/Snom/LineGridPanel.js
@@ -22,7 +22,6 @@ Ext.namespace('Tine.Voipmanager');
  * <p>Line Picker GridPanel</p>
  * <p><pre>
  * TODO         check max number of lines ?
- * TODO         remove lineactive stuff / checkcolumn ?
  * </pre></p>
  * 
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
@@ -43,30 +42,73 @@ Tine.Voipmanager.LineGridPanel = Ext.extend(Tine.widgets.grid.PickerGridPanel, {
     
     /**
      * 
+     * @type Tine.Voipmanager.CallForwardPanel
+     */
+    cfPanel: null,
+    
+    /**
+     * Tine record
+     * @type 
+     */
+    activeRecord: null,
+    
+    /**
+     * 
      * @cfg
      */
-    clicksToEdit: 1,
+    clicksToEdit: 2,
     autoExpandColumn: 'name',
+    // needed to select the first row after render!
+    deferRowRender: false,
     
     /**
      * @private
      */
     initComponent: function() {
-        /*
-        this.checkColumn = new Ext.ux.grid.CheckColumn({
-           header: this.app.i18n._('Line active'),
-           dataIndex: 'lineactive',
-           width: 50
-        });
-        */
         
         this.recordClass = Tine.Voipmanager.Model.SnomLine;
         this.searchRecordClass = Tine.Voipmanager.Model.AsteriskSipPeer;
-        //this.configColumns = this.checkColumn;
+
+        this.cfPanel.on('change', this.onFieldChange, this);
+        this.cfPanel.setDisabled(true);
         
         Tine.Voipmanager.LineGridPanel.superclass.initComponent.call(this);
+        
+        this.on('afterrender', this.onAfterRender, this);
     },
 
+    /**
+     * select first row after render
+     */
+    onAfterRender: function() {
+        if (this.store.getCount() > 0) {
+            this.getSelectionModel().selectFirstRow();
+        }
+    },
+    
+    /**
+     * on call forward form field change: update store
+     */
+    onFieldChange: function() {
+        this.cfPanel.getForm().updateRecord(this.activeRecord);
+        this.getView().refresh();
+    },
+    
+    /**
+     * Return CSS class to apply to rows depending upon record state
+     * 
+     * @param {} record
+     * @param {Integer} index
+     * @return {String}
+     */
+    getViewRowClass: function(record, index) {
+        var result = '';
+        if (record.dirty) {
+            result = 'voipmanager-row-changed';
+        }
+        return result;
+    },
+    
     /**
      * init actions and toolbars
      */
@@ -93,8 +135,6 @@ Tine.Voipmanager.LineGridPanel = Ext.extend(Tine.widgets.grid.PickerGridPanel, {
     
     /**
      * @param {Record} recordToAdd
-     * 
-     * TODO make reset work correctly -> show emptyText again
      */
     onAddRecordFromCombo: function(recordToAdd) {
         
@@ -104,17 +144,62 @@ Tine.Voipmanager.LineGridPanel = Ext.extend(Tine.widgets.grid.PickerGridPanel, {
             lineactive: 1
         };
         var record = new this.newRecordClass(recordData);
-        
-        this.recordStore.add([record]);
-        
-        // TODO check if already in
-        /*
-        if (! this.recordStore.getById(record.id)) {
+        var fields = ['cfi_mode','cfi_number','cfb_mode','cfb_number','cfd_mode','cfd_number','cfd_time' ];
+        for (var i=0; i < fields.length; i++) {
+            record.data[fields[i]] = recordToAdd.data[fields[i]];
         }
-        */
+
+        // check if already in
+        var found = false;
+        this.recordStore.each(function (line) {
+            if (line.data.asteriskline_id.id == recordToAdd.data.id) {
+                found = true;
+            }
+        }, this);
+        if (! found) {
+            // if not found -> add
+            this.recordStore.add([record]);
+        }
+        
         this.collapse();
         this.clearValue();
         this.reset();
+    },
+    
+    /**
+     * init selection model
+     */
+    initGrid: function() {
+        Tine.Voipmanager.LineGridPanel.superclass.initGrid.call(this);
+        
+        this.selModel.on('selectionchange', this.onSelectionChange, this);
+
+        // init view
+        this.view =  new Ext.grid.GridView({
+            getRowClass: this.getViewRowClass,
+            autoFill: true,
+            forceFit:true
+        });
+    },
+    
+    /**
+     * on selection change handler
+     * @param {} sm
+     */
+    onSelectionChange: function(sm) {
+        var rowCount = sm.getCount();
+        if (rowCount == 1) {
+            var selectedRows = sm.getSelections();
+            this.activeRecord = selectedRows[0];
+            this.cfPanel.setDisabled(false);
+            this.cfPanel.getForm().loadRecord(this.activeRecord);
+            this.cfPanel.onRecordLoad(this.activeRecord);
+        } else {
+            this.cfPanel.getForm().reset();
+            this.cfPanel.setDisabled(true);
+        }
+
+        this.actionRemove.setDisabled(rowCount == 0);
     },
     
     /**
@@ -122,21 +207,6 @@ Tine.Voipmanager.LineGridPanel = Ext.extend(Tine.widgets.grid.PickerGridPanel, {
      * @private
      */
     getColumnModel: function() {
-        var genericComboConfig = {
-            typeAhead: true,
-            triggerAction: 'all',
-            lazyRender:true,
-            triggerAction: 'all',
-            allowBlank: false,
-            editable: false,
-            blurOnSelect: true,
-            store: [
-                ['off', 'off'],
-                ['number', 'number'],
-                ['voicemail', 'voicemail']
-            ]
-        };
-
         return new Ext.grid.ColumnModel({
             defaults: {
                 sortable: true
@@ -144,35 +214,11 @@ Tine.Voipmanager.LineGridPanel = Ext.extend(Tine.widgets.grid.PickerGridPanel, {
             columns:  [
                 {id: 'linenumber',  header: '', dataIndex: 'linenumber', width: 20},
                 {id: 'name', header: this.app.i18n._('Line'), dataIndex: 'asteriskline_id', width: 120, renderer: this.nameRenderer},
-                {id: 'idletext', header: this.app.i18n._('Idle Text'), dataIndex: 'idletext', width: 80, editor: new Ext.form.TextField({
+                {id: 'idletext', header: this.app.i18n._('Idle Text'), dataIndex: 'idletext', width: 100, editor: new Ext.form.TextField({
                     allowBlank: false,
                     allowNegative: false,
                     maxLength: 60
-                })},
-                {id: 'cfi_mode',    header: this.app.i18n._('Forward'), dataIndex: 'cfi_mode', width: 70, editor: new Ext.form.ComboBox(genericComboConfig)},
-                {id: 'cfi_number',    header: this.app.i18n._('Forward #'), dataIndex: 'cfi_number', width: 90, editor: new Ext.form.TextField({
-                    allowBlank: true,
-                    allowNegative: false,
-                    maxLength: 60
-                })},
-                {id: 'cfb_mode',    header: this.app.i18n._('Forward Busy'), dataIndex: 'cfb_mode', width: 70, editor: new Ext.form.ComboBox(genericComboConfig)},
-                {id: 'cfb_number',    header: this.app.i18n._('Busy #'), dataIndex: 'cfb_number', width: 90, editor: new Ext.form.TextField({
-                    allowBlank: true,
-                    allowNegative: false,
-                    maxLength: 60
-                })},
-                {id: 'cfd_mode',    header: this.app.i18n._('Forward No Answer'), dataIndex: 'cfd_mode', width: 70, editor: new Ext.form.ComboBox(genericComboConfig)},
-                {id: 'cfd_number',    header: this.app.i18n._('No Answer #'), dataIndex: 'cfd_number', width: 90, editor: new Ext.form.TextField({
-                    allowBlank: true,
-                    allowNegative: false,
-                    maxLength: 60
-                })},
-                {id: 'cfd_time',    header: this.app.i18n._('No Answer Time'), dataIndex: 'cfd_time', width: 80, editor: new Ext.form.TextField({
-                    allowBlank: true,
-                    allowNegative: false,
-                    maxLength: 60
                 })}
-                //this.checkColumn
             ]
         });
     },
