b4965ffb80921140f29613e4c06b0ff3b1c77b45
Philipp Schüle <p.schuele@metaways.de>
2010-02-26 09:05:14 +0000

    removed obsolete fn
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@13009 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Phone/Model/MyPhone.php
+++ tine20/Phone/Model/MyPhone.php
@@ -6,7 +6,7 @@
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Thomas Wadewitz <t.wadewitz@metaways.de>
  * @copyright   Copyright (c) 2007-2010 Metaways Infosystems GmbH (http://www.metaways.de)
- * @version     $Id: MyPhone.php 10601 2009-09-27 13:09:59Z l.kneschke@metaways.de $
+ * @version     $Id$
  *
  */
 
@@ -56,30 +56,4 @@ class Phone_Model_MyPhone extends Tinebase_Record_Abstract
         'lines'                 => array(Zend_Filter_Input::ALLOW_EMPTY => true),
         'description'           => array(Zend_Filter_Input::ALLOW_EMPTY => true),
     );
-    
-    /**
-     * converts a int, string or Phone_Model_MyPhone to an phone id
-     *
-     * @param int|string|Phone_Model_MyPhone $_phoneId the phone id to convert
-     * @return int
-     * @throws  Phone_Exception_InvalidArgument
-     */
-    static public function convertMyPhoneIdToInt($_phoneId)
-    {
-        if ($_phoneId instanceof Phone_Model_MyPhone) {
-            if (empty($_phoneId->id)) {
-                throw new Phone_Exception_InvalidArgument('no phone id set');
-            }
-            $id = (string) $_phoneId->id;
-        } else {
-            $id = (string) $_phoneId;
-        }
-        
-        if ($id == '') {
-            throw new Phone_Exception_InvalidArgument('phone id can not be 0');
-        }
-
-        return $id;
-    }
-
 }
