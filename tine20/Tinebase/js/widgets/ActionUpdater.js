bc1fac7bf6f3291a2a321c6ab59a8ed159e01477
Cornelius Weiß <c.weiss@metaways.de>
2010-03-01 13:35:32 +0000

    deal with evalGrants prop
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@13049 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/widgets/ActionUpdater.js
+++ tine20/Tinebase/js/widgets/ActionUpdater.js
@@ -1,14 +1,10 @@
 /*
  * Tine 2.0
  * 
- * @package     Tinebase
- * @subpackage  widgets
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Cornelius Weiss <c.weiss@metaways.de>
- * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2007-2010 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
- *
- * @todo        refactor this (requiredGrant is required even if skipGrants == true) 
  */
  
  Ext.namespace('Tine', 'Tine.widgets');
