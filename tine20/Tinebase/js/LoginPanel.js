77e415f78b5d26b1fad9d6303c67070c646d055b
Cornelius Weiß <c.weiss@metaways.de>
2010-03-01 09:18:06 +0000

    ASSIGNED - # 1308: main app layout http://www.tine20.org/bugtracker/view.php?id=1308
    docu updates
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@13043 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/LoginPanel.js
+++ tine20/Tinebase/js/LoginPanel.js
@@ -3,18 +3,17 @@
  * 
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Cornelius Weiss <c.weiss@metaways.de>
- * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2009-2010 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
  */
- 
-Ext.namespace('Tine.Tinebase');
+Ext.ns('Tine.Tinebase');
 
 /**
- * @class Tine.Tinebase.LoginPanel
- * @namespace Tine.Tinebase
- * @extends Ext.Panel
- * @author Cornelius Weiss <c.weiss@metaways.de>
- * @version $Id$
+ * @class       Tine.Tinebase.LoginPanel
+ * @namespace   Tine.Tinebase
+ * @extends     Ext.Panel
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
  */
 Tine.Tinebase.LoginPanel = Ext.extend(Ext.Panel, {
     
