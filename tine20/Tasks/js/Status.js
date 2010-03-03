4bbb4b884bdda323b54e5ec9b84e4a42a54a7e78
Cornelius Weiß <c.weiss@metaways.de>
2010-03-03 14:58:44 +0000

    - new status filterModel - use layerCombo
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@13106 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tasks/js/Status.js
+++ tine20/Tasks/js/Status.js
@@ -1,15 +1,110 @@
-/**
+/*
  * Tine 2.0
  * 
- * @package     Tasks
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Cornelius Weiss <c.weiss@metaways.de>
- * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2007-2010 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
- *
  */
-Ext.namespace('Tine.Tasks', 'Tine.Tasks.status');
+Ext.ns('Tine.Tasks.status');
 
+/**
+ * @namespace   Tine.Tasks
+ * @class       Tine.Tasks.status.StatusFilter
+ * @extends     Tine.widgets.grid.FilterModel
+ * 
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+Tine.Tasks.status.StatusFilter = Ext.extend(Tine.widgets.grid.FilterModel, {
+    /**
+     * @property Tine.Tinebase.Application app
+     */
+    app: null,
+    
+    field: 'status_id',
+    defaultOperator: 'in',
+    
+    /**
+     * @private
+     */
+    initComponent: function() {
+        Tine.widgets.tags.TagFilter.superclass.initComponent.call(this);
+        
+        this.app = Tine.Tinebase.appMgr.get('Tasks');
+        
+        this.operators = ['in'];
+        this.label = _('Status');
+    },
+    
+    /**
+     * value renderer
+     * 
+     * @param {Ext.data.Record} filter line
+     * @param {Ext.Element} element to render to 
+     */
+    valueRenderer: function(filter, el) {
+        var value = new Tine.Tasks.status.StatusFilterValueField({
+            app: this.app,
+            filter: filter,
+            width: 200,
+            id: 'tw-ftb-frow-valuefield-' + filter.id,
+            value: filter.data.value ? filter.data.value : this.defaultValue,
+            renderTo: el
+        });
+        value.on('specialkey', function(field, e){
+             if(e.getKey() == e.ENTER){
+                 this.onFiltertrigger();
+             }
+        }, this);
+        //value.on('select', this.onFiltertrigger, this);
+        
+        return value;
+    }
+});
+
+Tine.widgets.grid.FilterToolbar.FILTERS['tasks.status'] = Tine.Tasks.status.StatusFilter;
+
+/**
+ * @namespace   Tine.Tasks
+ * @class       Tine.Tasks.status.StatusFilterValueField
+ * @extends     Ext.ux.form.LayerCombo
+ * 
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+Tine.Tasks.status.StatusFilterValueField = Ext.extend(Ext.ux.form.LayerCombo, {
+    hideButtons: false,
+    formConfig: {
+        labelAlign: 'left',
+        labelWidth: 30
+    },
+    
+    
+    getItems: function() {
+        var items = [];
+        
+        Tine.Tasks.status.getStore().each(function(status) {
+            items.push({
+                xtype: 'checkbox',
+                boxLabel: status.get('status_name'),
+                icon: status.get('status_icon'),
+                name: status.get('id')
+            });
+        }, this);
+        
+        return items;
+    }
+});
+
+/**
+ * @namespace   Tine.Tasks
+ * @class       Tine.Tasks.status.ComboBox
+ * @extends     Ext.form.ComboBox
+ * 
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
 Tine.Tasks.status.ComboBox = Ext.extend(Ext.form.ComboBox, {
 	/**
      * @cfg {bool} autoExpand Autoexpand comboBox on focus.
