2866d4746399c34e9191c018aa85c6c27fd4d317
Lars Kneschke <l.kneschke@metaways.de>
2010-03-06 18:08:20 +0000

    class to store Sieve rule actiong and to generate Sieve code for action
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@13204 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Felamimail/Sieve/Rule/Action.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Felamimail
+ * @subpackage  Sieve
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ * 
+ */
+
+/**
+ * class to store Sieve rule actiong and to generate Sieve code for action
+ * 
+ * @package     Felamimail
+ * @subpackage  Sieve
+ */
+class Felamimail_Sieve_Rule_Action
+{
+    const DISCARD   = 'discard';
+    const FILEINTO  = 'fileinto';
+    const KEEP      = 'keep';
+    const REJECT    = 'reject';
+    const REDIRECT  = 'redirect';
+    
+    /**
+     * type of action
+     * 
+     * @var string
+     */
+    protected $_type;
+    
+    /**
+     * argument for action
+     * 
+     * @var string
+     */
+    protected $_argument;
+    
+    /**
+     * set type of action
+     * 
+     * @param   string  $type   type of action
+     * @return  Felamimail_Sieve_Rule_Action
+     */
+    public function setType($type)
+    {
+        if(!defined('self::' . strtoupper($type))) {
+            throw new InvalidArgumentException('invalid type: ' . $type);
+        }
+        $this->_type = $type;
+        
+        return $this;
+    }
+    
+    /**
+     * set argument for action
+     * 
+     * @param   string  $argument   argument
+     * @return  Felamimail_Sieve_Rule_Action
+     */
+    public function setArgument($argument)
+    {
+        $this->_argument = $argument;
+        
+        return $this;
+    }
+    
+    /**
+     * return the Sieve code for this action
+     * 
+     * @return string
+     */
+    public function __toString() 
+    {
+        switch($this->_type) {
+            case self::DISCARD:
+                return "    $this->_type;";
+                break;
+                
+            default:
+                $argument = $this->_quoteString($this->_argument);
+                return "    $this->_type $argument;";
+                break;
+        }
+    }
+    
+    /**
+     * quote string for usage in Sieve script 
+     * 
+     * @param   string  $string     the srting to quote
+     */
+    protected function _quoteString($string)
+    {
+        if(is_array($string)) {
+            $string = array_map(array($this, '_quoteString'), $string);
+            return '[' . implode(',', $string) . ']'; 
+        } else {
+            return '"' . str_replace('"', '\""', $string) . '"';
+        }
+    }
+}
