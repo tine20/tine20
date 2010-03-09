13fe366adfba8e2c25d72130f39e03a425f47e9f
Cornelius Weiß <c.weiss@metaways.de>
2010-03-09 11:49:33 +0000

    static helper fns
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@13262 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/Container.js
+++ tine20/Tinebase/js/Container.js
@@ -31,3 +31,40 @@ Tine.Tinebase.container = {
      */
     TYPE_SHARED: 'shared'
 };
+
+/**
+ * gets translated container name by path
+ * 
+ * @static
+ * @param {String} path
+ * @param {String} containerName
+ * @param {String} containersName
+ * @return {String}
+ */
+Tine.Tinebase.container.path2name = function(path, containerName, containersName) {
+    switch (path) {
+        case '/':           return String.format(_('All {0}'), containersName);
+        case '/shared':     return String.format(_('Shared {0}'), containersName);
+        case '/personal':   return String.format(_('Other Users {0}'), containersName);
+        case '/internal':   return String.format(_('Internal {0}'), containersName);
+    }
+    
+    if (path === '/personal/' + Tine.Tinebase.registry.get('currentAccount').accountId) {
+        return String.format(_('My {0}'), containersName);
+    }
+    
+    return path;
+};
+
+/**
+ * gets container type by path
+ * 
+ * @static
+ * @param {String} path
+ * @return {String}
+ */
+Tine.Tinebase.container.path2type = function(path) {
+    var pathParts = Ext.isArray(path) ? path : path.split('/');
+    
+    return pathParts[1] === Tine.Tinebase.container.TYPE_PERSONAL && pathParts.length === 2 ? 'otherUsers' : pathParts[1]; 
+};
