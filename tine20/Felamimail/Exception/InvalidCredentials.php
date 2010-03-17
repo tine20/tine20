57e7ace5f1d8397248c36b90ef9d3dc24b64cd15
Philipp Schüle <p.schuele@metaways.de>
2010-03-17 17:01:04 +0000

    added svn keyword Id
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@13465 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/Exception/InvalidCredentials.php
+++ tine20/Felamimail/Exception/InvalidCredentials.php
@@ -7,7 +7,7 @@
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Philipp Schuele <p.schuele@metaways.de>
- * @version     $Id: Exception.php 7990 2009-05-08 17:25:05Z p.schuele@metaways.de $
+ * @version     $Id$
  *
  */
 
