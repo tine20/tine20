dc9ef66922424f58035ebebdb7c72aace4c5b4a4
Philipp Schüle <p.schuele@metaways.de>
2010-03-17 17:01:27 +0000

    - added service unavailable exception
    ASSIGNED - # 2374: message caching [client] 
    http://www.tine20.org/bugtracker/view.php?id=2374
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@13466 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Felamimail/Exception/ServiceUnavailable.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Felamimail
+ * @subpackage  Exception
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @version     $Id$
+ *
+ */
+
+/**
+ * ServiceUnavailable Exception
+ * 
+ * @package     Felamimail
+ * @subpackage  Exception
+ */
+class Felamimail_Exception_ServiceUnavailable extends Felamimail_Exception
+{
+    /**
+     * construct
+     * 
+     * @param string $_message
+     * @param integer $_code
+     * @return void
+     */
+    public function __construct($_message = 'IMAP service unavailable or some unrecognized server error.', $_code = 03) {
+        parent::__construct($_message, $_code);
+    }
+}
