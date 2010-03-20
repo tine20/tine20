65b0e5da045e1486b4c25a50d15277f75663779c
Cornelius Weiß <c.weiss@metaways.de>
2010-03-20 19:46:37 +0000

    move filter serialisatio here add static filter getter
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@13527 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/PersistentFilter.php
+++ tine20/Tinebase/PersistentFilter.php
@@ -42,4 +42,58 @@ class Tinebase_PersistentFilter extends Tinebase_Backend_Sql_Abstract
      * @var boolean
      */
     protected $_modlogActive = TRUE;
+    
+    /**
+     * returns persistent filter identified by id
+     * 
+     * @param  string $_id
+     * @return Tinebase_Model_Filter_FilterGroup
+     */
+    public static function getFilterById($_id)
+    {
+        $obj = new Tinebase_PersistentFilter();
+        $persistentFilter = $obj->get($_id);
+        
+        return $persistentFilter->filters;
+    }
+    
+    /**
+     * converts record into raw data for adapter
+     *
+     * @param  Tinebase_Record_Abstract $_record
+     * @return array
+     */
+    protected function _recordToRawData($_record)
+    {
+        $rawData = $_record->toArray();
+        $rawData['filters'] = Zend_Json::encode($rawData['filters']);
+        
+        return $rawData;
+    }
+    
+    /**
+     * converts raw data from adapter into a single record
+     *
+     * @param  array $_data
+     * @return Tinebase_Record_Abstract
+     */
+    protected function _rawDataToRecord(array $_rawData)
+    {
+        $_rawData['filters'] = Zend_Json::decode($_rawData['filters']);
+        return new $this->_modelName($_rawData, true);
+    }
+    
+    /**
+     * converts raw data from adapter into a set of records
+     *
+     * @param  array $_rawDatas of arrays
+     * @return Tinebase_Record_RecordSet
+     */
+    protected function _rawDataToRecordSet(array $_rawDatas)
+    {
+        foreach($_rawDatas as $idx => $rawData) {
+            $_rawDatas[$idx]['filters'] = Zend_Json::decode($rawData['filters']);
+        }
+        return new Tinebase_Record_RecordSet($this->_modelName, $_rawDatas, true);
+    }
 }
