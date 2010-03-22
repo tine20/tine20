c4f1af0d1792bf549a4983c1f4080a728a6ffd63
Lars Kneschke <l.kneschke@metaways.de>
2010-03-22 12:17:55 +0000

    PHPDOC updates
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@13595 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/ActiveSync/Model/Device.php
+++ tine20/ActiveSync/Model/Device.php
@@ -19,9 +19,9 @@
  * @package     ActiveSync
  * @property    string  $id the id
  * @property    string  calendarfilter_id   the calendar filter id
- * @property    string  contactfilter_id    the contact filter id
+ * @property    string  contactsfilter_id   the contacts filter id
  * @property    string  emailfilter_id      the email filter id
- * @property    string  taskfilter_id       the tasks filter id
+ * @property    string  tasksfilter_id      the tasks filter id
  */
 class ActiveSync_Model_Device extends Tinebase_Record_Abstract
 {  
