4c1f6941cc9a1a319ae735f3ae303d5ce4471f95
Cornelius Weiß <c.weiss@metaways.de>
2010-03-22 09:36:47 +0000

    content filter handling
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@13565 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/ActiveSync/Frontend/Json.php
+++ tine20/ActiveSync/Frontend/Json.php
@@ -26,4 +26,38 @@ class ActiveSync_Frontend_Json extends Tinebase_Frontend_Json_Abstract
 {
     protected $_applicationName = 'ActiveSync';
     
+    /**
+     * 
+     * @param $deviceId
+     * @param $contentType one of {Calendar, Contacts, Email, Tasks}
+     * @param $filterId
+     * 
+     * @return device data
+     */
+    public function setDeviceContentFilter($deviceId, $contentType, $filterId)
+    {
+        
+    }
+    
+    /**
+     * Returns registry data of the application.
+     *
+     * Each application has its own registry to supply static data to the client.
+     * Registry data is queried only once per session from the client.
+     *
+     * This registry must not be used for rights or ACL purposes. Use the generic
+     * rights and ACL mechanisms instead!
+     */
+    public function getRegistryData()
+    {
+        
+        $deviceBackend = new ActiveSync_Backend_Device();
+        $userDevices = $deviceBackend->search(new ActiveSync_Model_DeviceFilter(array(
+            array('field' => 'owner_id', 'operator' => 'equals', 'value' => Tinebase_Core::getUser()->getId())
+        )));
+        
+        return array(
+            'userDevices' => $userDevices->toArray()
+        );
+    }
 }
