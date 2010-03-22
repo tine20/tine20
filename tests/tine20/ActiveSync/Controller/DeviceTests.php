2c6740eced6cf2b679df1233551e5480daabc9f3
Lars Kneschke <l.kneschke@metaways.de>
2010-03-22 19:54:59 +0000

    test setting content filter
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@13631 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/tine20/ActiveSync/Controller/DeviceTests.php
+++ tests/tine20/ActiveSync/Controller/DeviceTests.php
@@ -54,7 +54,25 @@ class ActiveSync_Controller_DeviceTests extends PHPUnit_Framework_TestCase
         
         $testDevice = ActiveSync_Backend_DeviceTests::getTestDevice();
         
-        $this->_device = $deviceBackend->create($testDevice);
+        $this->objects['device'] = $deviceBackend->create($testDevice);
+        
+        
+        $filterBackend = new Tinebase_PersistentFilter();
+        
+        $filter = new Tinebase_Model_PersistentFilter(array(
+            'application_id'    => Tinebase_Application::getInstance()->getApplicationByName('Addressbook')->getId(),
+            'account_id'        => Tinebase_Core::getUser()->getId(),
+            'model'             => 'Addressbook_Model_ContactFilter',
+            'filters'           => array(array(
+                'field' => 'query', 
+                'operator' => 'contains', 
+                'value' => 'blabla'
+            )),
+            'name'              => 'Sync Test',
+            'description'       => 'Created by unit test'
+        ));
+        
+        $this->objects['filter'] = $filterBackend->create($filter);
     }
     
     /**
@@ -65,7 +83,10 @@ class ActiveSync_Controller_DeviceTests extends PHPUnit_Framework_TestCase
      */
     protected function tearDown()
     {
-        ActiveSync_Controller_Device::getInstance()->delete($this->_device);
+        ActiveSync_Controller_Device::getInstance()->delete($this->objects['device']);
+        
+        $filterBackend = new Tinebase_PersistentFilter();
+        $filterBackend->delete($this->objects['filter']->getId());
     }
     
     
@@ -74,22 +95,25 @@ class ActiveSync_Controller_DeviceTests extends PHPUnit_Framework_TestCase
      */
     public function testGetDevice()
     {
-        $device = ActiveSync_Controller_Device::getInstance()->get($this->_device->getId());
+        $device = ActiveSync_Controller_Device::getInstance()->get($this->objects['device']->getId());
         
-        $this->assertEquals($device->id, $this->_device->id);
+        $this->assertEquals($device->id, $this->objects['device']->id);
     }
     
+    /**
+     * test setting content filter
+     */
     public function testSetDeviceContentFilter()
-    {
-        #$filter = new Tinebase_Model_PersistentFilter(array(
-        #    array(
-        #        'field' => 'query', 
-        #        'operator' => 'contains', 
-        #        'value' => 'blabla'
-        #    ),
-        #));
-        #//$filder = Tinebase_PersistentFilter::getFilterById($_filterId);
-        #ActiveSync_Controller_Device::getInstance()->setDeviceContentFilter($this->_device->getId(), ActiveSync_Controller::CLASS_CONTACTS, $filterId);
+    {        
+        #$filter = Tinebase_PersistentFilter::getFilterById($_filterId);
+        ActiveSync_Controller_Device::getInstance()->setDeviceContentFilter(
+            $this->objects['device']->getId(), 
+            ActiveSync_Controller::CLASS_CONTACTS, 
+            $this->objects['filter']->getId());
+            
+        $device = ActiveSync_Controller_Device::getInstance()->get($this->objects['device']->getId());
+        
+        $this->assertEquals($device->contactsfilter_id, $this->objects['filter']->getId());
     }
 }
     
