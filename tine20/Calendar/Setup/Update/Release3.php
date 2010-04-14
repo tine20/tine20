aea390b461e5f62de2cf4d20bc93fb95dcae2bf5
Cornelius Weiß <c.weiss@metaways.de>
2010-04-14 16:44:55 +0000

    updatescript for freebusy pref->grant
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@13845 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Calendar/Setup/Update/Release3.php
+++ tine20/Calendar/Setup/Update/Release3.php
@@ -40,4 +40,52 @@ class Calendar_Setup_Update_Release3 extends Setup_Update_Abstract
         $this->setTableVersion('cal_events', 3);
         $this->setApplicationVersion('Calendar', '3.1');
     }
+    
+    /**
+     * freebusy right/pref -> freebusy grant
+     */
+    public function update_1()
+    {
+        /**
+         * old const from pref class
+         * give all useraccounts grants to view free/busy of the account this preference is yes
+         */
+        $FREEBUSY = 'freeBusy';
+        
+        try {
+            // get all users with freebusy pref
+            $freebusyUserIds = Tinebase_Core::getPreference('Calendar')->getUsersWithPref($FREEBUSY, 1);
+            
+            // get all affected calendars
+            $containerIds = array();
+            foreach ($freebusyUserIds as $userId) {
+                $containerIds = array_merge($containerIds, Tinebase_Container::getInstance()->getPersonalContainer($userId, 'Calendar', $userId, NULL, TRUE)->getId());
+            }
+            
+            // grant freebusy to anyone for this calendars
+            foreach($containerIds as $containerId) {
+                $containerGrants = Tinebase_Container::getInstance()->getGrantsOfContainer($containerId, TRUE);
+                $anyoneGrant = $containerGrants->filter('account_type', Tinebase_Acl_Rights::ACCOUNT_TYPE_ANYONE)->getFirstRecord();
+                if (! $anyoneGrant) {
+                    $anyoneGrant = new Tinebase_Model_Grants(array(
+                        'account_id'   => 0,
+                        'account_type' => Tinebase_Acl_Rights::ACCOUNT_TYPE_ANYONE,
+                    ));
+                    $containerGrants->addRecord($anyoneGrant);
+                }
+                
+                $anyoneGrant->{Tinebase_Model_Grants::GRANT_FREEBUSY} = TRUE;
+                
+                Tinebase_Container::getInstance()->setGrants($containerId, $containerGrants, TRUE, TRUE);
+            }
+            
+            // drop freeBusy prefs from pref table
+            $this->_db->delete(SQL_TABLE_PREFIX . 'preferences', "name LIKE 'freeBusy'");
+        
+        } catch (Tinebase_Exception_NotFound $nfe) {
+            // pref was not found in system -> no user ever set freebusy -> nothing to do
+        }
+        
+        $this->setApplicationVersion('Calendar', '3.2');
+    }
 }
