f86ff6487e038d26f86a209160233b629c352433
Gökmen Ciyiltepe <g.ciyiltepe@metaways.de>
2010-04-21 12:20:49 +0000

    user filter for logging
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@13940 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Log/Filter/User.php
+++ tine20/Tinebase/Log/Filter/User.php
@@ -6,10 +6,9 @@
  * @subpackage  Log
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
- * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @author      Gökmen Ciyiltepe <g.ciyiltepe@metaways.de>
  * @version     $Id$
  * 
- * @todo        make this configurable
  */
 
 /**
@@ -21,6 +20,28 @@
 class Tinebase_Log_Filter_User implements Zend_Log_Filter_Interface
 {
     /**
+     * @var string
+     */
+    protected $_name;
+    
+    /**
+     * Filter out any log messages not matching $name.
+     *
+     * @param  string  $name     Username to log message
+     * @throws Zend_Log_Exception
+     */
+    public function __construct($name)
+    {
+        if (! is_string($name)) {
+            require_once 'Zend/Log/Exception.php';
+            throw new Zend_Log_Exception('Name must be a string');
+        }
+
+        $this->_name = $name;
+    }
+    
+    
+    /**
      * Returns TRUE to accept the message, FALSE to block it.
      *
      * @param  array    $event    event data
@@ -28,11 +49,8 @@ class Tinebase_Log_Filter_User implements Zend_Log_Filter_Interface
      */
     public function accept($event)
     {
-         $logUsers = array(
-            // @todo add users here
-         );
-    
-         $username = Tinebase_Core::getUser()->accountLoginName;
-         return in_array($username, $logUsers);    
-    }  
+        $username = Tinebase_Core::getUser()->accountLoginName;
+        return strtolower($this->_name) == strtolower($username) ? true : false;    
+    }
 }
+#EOF
