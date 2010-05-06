37ed328c091c53f63ad1e3f4dcf77aad7a96ea09
Cornelius Weiß <c.weiss@metaways.de>
2010-05-06 18:43:58 +0000

    update docu
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@14225 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/Models.js
+++ tine20/Tinebase/js/Models.js
@@ -3,12 +3,10 @@
  * 
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Cornelius Weiss <c.weiss@metaways.de>
- * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2007-2010 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
- *
  */
- 
-Ext.namespace('Tine', 'Tine.Tinebase', 'Tine.Tinebase.Model');
+Ext.ns('Tine.Tinebase.Model');
 
 /**
  * @type {Array}
