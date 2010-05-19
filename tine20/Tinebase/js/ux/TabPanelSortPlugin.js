8b56a9d45af6a958dc27827662cad35d64813ad6
Frank Wiechmann <f.wiechmann@metaways.de>
2010-05-19 08:00:47 +0000

    ASSIGNED - # 2504: Possibility to rearrange application tabs http://www.tine20.org/bugtracker/view.php?id=2504
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@14407 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/js/ux/TabPanelSortPlugin.js
@@ -0,0 +1,26 @@
+/*
+ * Tine 2.0
+ * 
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Frank Wiechmann <f.wiechmann@metaways.de>
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+
+
+Ext.ns('Ext.ux');
+
+/**
+ * @class SortPlugin
+ * @param {Object} config Configurations options
+ */
+Ext.ux.TabPanelSortPlugin = function(config) {
+    Ext.apply(this, config);
+};
+
+Ext.ux.TabPanelSortPlugin.prototype = {
+	
+	
+	
+};
