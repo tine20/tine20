49846064bdf7f056d4e98db1960d66d8da6c7cdc
Lars Kneschke <l.kneschke@metaways.de>
2010-05-21 19:56:55 +0000

    interface for syncable account backend classes
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@14463 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/User/Interface/SyncAble.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Tinebase
+ * @subpackage  User
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * syncable user backend interface
+ *
+ * @package     Tinebase
+ * @subpackage  User
+ */
+interface Tinebase_User_Interface_SyncAble
+{
+}
