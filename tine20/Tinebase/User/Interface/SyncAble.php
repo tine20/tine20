a465d80855a8928a6f1f46313962a81e113b4a5e
Lars Kneschke <l.kneschke@metaways.de>
2010-05-21 21:06:33 +0000

    some additional functions for the interface
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@14470 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/User/Interface/SyncAble.php
+++ tine20/Tinebase/User/Interface/SyncAble.php
@@ -18,4 +18,28 @@
  */
 interface Tinebase_User_Interface_SyncAble
 {
+    /**
+     * get user by login name
+     *
+     * @param   string  $_property
+     * @param   string  $_accountId
+     * @return Tinebase_Model_User the user object
+     */
+    public function getSyncAbleUserByProperty($_property, $_accountId, $_accountClass = 'Tinebase_Model_User');
+
+    /**
+     * updates an existing user in sql backend only
+     *
+     * @param  Tinebase_Model_FullUser  $_account
+     * @return Tinebase_Model_FullUser
+     */
+    public function updateLocalUser(Tinebase_Model_FullUser $_account);
+    
+    /**
+     * adds a new user to local sql backend only
+     *
+     * @param  Tinebase_Model_FullUser  $_account
+     * @return Tinebase_Model_FullUser
+     */
+    public function addLocalUser(Tinebase_Model_FullUser $_account);
 }
