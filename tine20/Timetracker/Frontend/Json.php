fd018902715909235ae38f4719eef44aff28cbb7
Gökmen Ciyiltepe <g.ciyiltepe@metaways.de>
2010-06-02 09:04:19 +0000

    client handle exceptions
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@14759 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Timetracker/Frontend/Json.php
+++ tine20/Timetracker/Frontend/Json.php
@@ -297,15 +297,6 @@ class Timetracker_Frontend_Json extends Tinebase_Frontend_Json_Abstract
      */
     public function saveTimeaccount($recordData)
     {
-        $title = trim($recordData['title']);
-        if (empty($title)) {
-            $result = array(
-                'errors'            => 'invalid title',
-                'errorMessage'      => 'title is required.',
-                'status'            => 'failure'
-            );
-            return $result;
-        }
         return $this->_save($recordData, $this->_timeaccountController, 'Timeaccount');        
     }
     
