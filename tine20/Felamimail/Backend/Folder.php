7c5fa63ecce03ef65e719b7ffe42a2bb872cce96
Lars Kneschke <l.kneschke@metaways.de>
2010-06-15 13:48:54 +0000

    new functions to resolve counters on the fly
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@14948 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/Backend/Folder.php
+++ tine20/Felamimail/Backend/Folder.php
@@ -57,4 +57,79 @@ class Felamimail_Backend_Folder extends Tinebase_Backend_Sql_Abstract
         
         return $result;
     }
+    
+    /**
+     * get folder counter like total, unseen and recent count
+     *  
+     * @param  string  $_folderId  the folderid
+     * @return array
+     */
+    protected function _getFolderCounter($_folderId)
+    {
+        // fetch total count
+        $cols = array('cache_totalcount' => new Zend_Db_Expr('COUNT(*)'));
+        $select = $this->_db->select()
+            ->from(array('felamimail_cache_message' => $this->_tablePrefix . 'felamimail_cache_message'), $cols)
+            ->where($this->_db->quoteIdentifier('felamimail_cache_message.folder_id') . ' = ?', $_folderId);
+        
+        $stmt = $this->_db->query($select);
+        $totalCount = $stmt->fetchColumn(0);
+        $stmt->closeCursor();
+        
+        // get unseen count
+        
+        // get recent count
+        
+        return array(
+            'cache_totalcount' => $totalCount
+        );
+    }
+    
+    /**
+     * converts raw data from adapter into a single record
+     *
+     * @param  array $_rawData
+     * @return Tinebase_Record_Abstract
+     */
+    protected function _rawDataToRecord(array $_rawData)
+    {
+        $folderCounter = $this->_getFolderCounter($_rawData['id']);
+
+        $rawData = array_merge($_rawData, $folderCounter);
+        
+        return parent::_rawDataToRecord($rawData);
+    }
+    
+    /**
+     * converts raw data from adapter into a set of records
+     *
+     * @param  array $_rawDatas of arrays
+     * @return Tinebase_Record_RecordSet
+     */
+    protected function _rawDataToRecordSet(array $_rawDatas)
+    {
+        foreach($_rawDatas as &$rawData) {
+            $folderCounter = $this->_getFolderCounter($rawData['id']);
+            $rawData = array_merge($rawData, $folderCounter);
+        }
+        return parent::_rawDataToRecordSet($_rawDatas);
+    }
+    
+    /**
+     * converts record into raw data for adapter
+     *
+     * @param  Tinebase_Record_Abstract $_record
+     * @return array
+     */
+    protected function _recordToRawData($_record)
+    {
+        $result = parent::_recordToRawData($_record);
+
+        // some columns got joined from another table and can't be written
+        unset($result['cache_totalcount']);
+        unset($result['cache_recentcount']);
+        unset($result['cache_unreadcount']);
+        
+        return $result;
+    }
 }
