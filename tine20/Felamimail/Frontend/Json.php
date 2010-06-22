5d70a044dd7cfad12bf7febc16d444ce94c75759
Lars Kneschke <l.kneschke@metaways.de>
2010-06-22 21:51:02 +0000

    use new function signature
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@15051 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/Frontend/Json.php
+++ tine20/Felamimail/Frontend/Json.php
@@ -184,7 +184,7 @@ class Felamimail_Frontend_Json extends Tinebase_Frontend_Json_Abstract
     public function getMessage($id)
     {
         $controller = Felamimail_Controller_Message::getInstance();
-        $message = $controller->getCompleteMessage($id, TRUE, TRUE);
+        $message = $controller->getCompleteMessage($id, true);
         
         return $this->_recordToJson($message);
     }
