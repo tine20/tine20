1033bf49441230e5293ad0d8e4439a60aaba1723
Philipp Schüle <p.schuele@metaways.de>
2010-06-30 11:58:15 +0000

    fixed Id keyword
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@15212 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Timetracker/Controller/Timesheet.php
+++ tine20/Timetracker/Controller/Timesheet.php
@@ -6,8 +6,8 @@
  * @subpackage  Controller
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Philipp Schuele <p.schuele@metaways.de>
- * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
- * @version     $Id:Timesheet.php 5576 2008-11-21 17:04:48Z p.schuele@metaways.de $
+ * @copyright   Copyright (c) 2007-2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
  */
 
 /**
