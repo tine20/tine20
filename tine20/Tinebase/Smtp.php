635e891bb5dfd5b96fb65d114f4d2c97893b84d4
Lars Kneschke <l.kneschke@metaways.de>
2010-07-04 11:48:46 +0000

    setup >default< transport only
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@15354 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Smtp.php
+++ tine20/Tinebase/Smtp.php
@@ -60,7 +60,7 @@ class Tinebase_Smtp
                 unset($config['ssl']);
             }
             
-            Tinebase_Core::getLogger()->info(__METHOD__ . '::' . __LINE__ . ' Setting SMTP transport. Hostname: ' . $config['hostname']);
+            Tinebase_Core::getLogger()->info(__METHOD__ . '::' . __LINE__ . ' Setting default SMTP transport. Hostname: ' . $config['hostname']);
             //if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . print_r($config, TRUE));
             
             self::setDefaultTransport(new Zend_Mail_Transport_Smtp($config['hostname'], $config));
