076621f62e19e24dcece3f429146161465dce1cb
Lars Kneschke <l.kneschke@metaways.de>
2010-07-06 12:53:42 +0000

    added todo
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@15373 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/Exception/IMAPMessageNotFound.php
+++ tine20/Felamimail/Exception/IMAPMessageNotFound.php
@@ -25,6 +25,7 @@ class Felamimail_Exception_IMAPMessageNotFound extends Felamimail_Exception_IMAP
      * @param string $_message
      * @param integer $_code
      * @return void
+     * @todo for Cornelius: switch back to error code 914
      */
     #public function __construct($_message = 'Message not found on IMAP server.', $_code = 914) {
     public function __construct($_message = 'Message not found on IMAP server.', $_code = 404) {
