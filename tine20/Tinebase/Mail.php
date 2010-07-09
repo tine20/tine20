5439448e5add57b1fb67e619a7599a5b1b6e6fd7
Lars Kneschke <l.kneschke@metaways.de>
2010-07-09 10:04:41 +0000

    added function to create Tinebase_Mail from Zend_Mail_Message
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@15409 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Mail.php
+++ tine20/Tinebase/Mail.php
@@ -25,6 +25,148 @@ class Tinebase_Mail extends Zend_Mail
     protected $_sender = null;
     
     /**
+     * create Tinebase_Mail from Zend_Mail_Message
+     * 
+     * @param  Zend_Mail_Message  $_zmm
+     * @return Tinebase_Mail
+     */
+    public static function createFromZMM(Zend_Mail_Message $_zmm)
+    {
+        $contentStream = fopen("php://temp", 'r+');
+        fputs($contentStream, $_zmm->getContent());
+        rewind($contentStream);
+        
+        $mp = new Zend_Mime_Part($contentStream);
+        
+        if ($_zmm->headerExists('content-transfer-encoding')) {
+            $mp->encoding = $_zmm->getHeader('content-transfer-encoding');
+            
+            // create decoded stream
+            $mp = new Zend_Mime_Part($mp->getDecodedStream());
+            $mp->encoding = $_zmm->getHeader('content-transfer-encoding');
+        }
+        
+        if ($_zmm->headerExists('content-type')) {
+            $contentTypeHeader = Zend_Mime_Decode::splitHeaderField($_zmm->getHeader('content-type'));
+            
+            $mp->type = $contentTypeHeader[0];
+            
+            if (isset($contentTypeHeader['boundary'])) {
+                $mp->boundary = $contentTypeHeader['boundary'];
+            }
+            
+            if (isset($contentTypeHeader['charset'])) {
+                $mp->charset = $contentTypeHeader['charset'];
+            }
+        } else {
+            $mp->type = Zend_Mime::TYPE_TEXT;
+        }        
+        
+        $result = new Tinebase_Mail('utf-8');
+        
+        $result->setBodyText($mp);
+        
+        foreach ($_zmm->getHeaders() as $header => $values) {
+            foreach ((array)$values as $value) {
+                switch ($header) {
+                    case 'content-transfer-encoding':
+                        // do nothing
+                        
+                        break;
+                        
+                    case 'bcc':
+                        $addresses = Felamimail_Message::parseAdresslist($value);
+                        foreach ($addresses as $address) {
+                            $result->addBcc($address['address'], $address['name']);
+                        }
+                        
+                        break;
+                        
+                    case 'cc':
+                        $addresses = Felamimail_Message::parseAdresslist($value);
+                        foreach ($addresses as $address) {
+                            $result->addCc($address['address'], $address['name']);
+                        }
+                                                
+                        break;
+                        
+                    case 'date':
+                        $result->setDate($value);
+                        
+                        break;
+                        
+                    case 'from':
+                        $addresses = Felamimail_Message::parseAdresslist($value);
+                        foreach ($addresses as $address) {
+                            $result->setFrom($address['address'], $address['name']);
+                        }
+                        
+                        break;
+                        
+                    case 'message-id':
+                        $result->setMessageId($value);
+                        
+                        break;
+                        
+                    case 'return-path':
+                        $result->setReturnPath($value);
+                        
+                        break;
+                        
+                    case 'subject':
+                        $result->setSubject($value);
+                        
+                        break;
+                        
+                    case 'to':
+                        $addresses = Felamimail_Message::parseAdresslist($value);
+                        foreach ($addresses as $address) {
+                            $result->addTo($address['address'], $address['name']);
+                        }
+                        
+                        break;
+                        
+                    default:
+                        $result->addHeader($header, $value);
+                        
+                        break;
+                }
+            }
+        }
+        
+        return $result;
+    }
+    
+    /**
+     * Sets the text body for the message.
+     *
+     * @param  string|Zend_Mime_Part $txt
+     * @param  string $charset
+     * @param  string $encoding
+     * @return Zend_Mail Provides fluent interface
+    */
+    public function setBodyText($txt, $charset = null, $encoding = Zend_Mime::ENCODING_QUOTEDPRINTABLE)
+    {
+        if ($txt instanceof Zend_Mime_Part) {
+            $mp = $txt;
+        } else {
+            if ($charset === null) {
+                $charset = $this->_charset;
+            }
+    
+            $mp = new Zend_Mime_Part($txt);
+            $mp->encoding = $encoding;
+            $mp->type = Zend_Mime::TYPE_TEXT;
+            $mp->disposition = Zend_Mime::DISPOSITION_INLINE;
+            $mp->charset = $charset;
+        }
+        
+        $this->_bodyText = $mp;
+
+        return $this;
+    }
+    
+    /**
      * Sets Sender-header and sender of the message
      *
      * @param  string    $email
