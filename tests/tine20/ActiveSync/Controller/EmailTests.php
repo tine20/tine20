5c77253d387af81e3d20f5aee78b6b16895da4e2
Lars Kneschke <l.kneschke@metaways.de>
2010-07-27 05:51:47 +0000

    stub for ActiveSync_Controller_Email tests
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@15548 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/tine20/ActiveSync/Controller/EmailTests.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     ActiveSync
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'ActiveSync_Controller_EmailTests::main');
+}
+
+/**
+ * Test class for ActiveSync_Controller_Email
+ * 
+ * @package     Calendar
+ */
+class ActiveSync_Controller_EmailTests extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var array test objects
+     */
+    protected $objects = array();
+    
+    protected $_exampleXMLNotExisting = '<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/">
+<Sync xmlns="uri:AirSync" xmlns:Contacts="uri:Contacts"><Collections><Collection><Class>Contacts</Class><SyncKey>1</SyncKey><CollectionId>addressbook-root</CollectionId><DeletesAsMoves/><GetChanges/><WindowSize>50</WindowSize><Options><FilterType>0</FilterType><Truncation>2</Truncation><Conflict>0</Conflict></Options><Commands><Add><ClientId>1</ClientId><ApplicationData><Contacts:FileAs>ads2f, asdfadsf</Contacts:FileAs><Contacts:FirstName>asdf </Contacts:FirstName><Contacts:LastName>asdfasdfaasd </Contacts:LastName><Contacts:MobilePhoneNumber>+4312341234124</Contacts:MobilePhoneNumber><Contacts:Body>&#13;
+</Contacts:Body></ApplicationData></Add></Commands></Collection></Collections></Sync>';
+    
+    protected $_exampleXMLExisting = '<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/">
+<Sync xmlns="uri:AirSync" xmlns:Contacts="uri:Contacts"><Collections><Collection><Class>Contacts</Class><SyncKey>1</SyncKey><CollectionId>addressbook-root</CollectionId><DeletesAsMoves/><GetChanges/><WindowSize>50</WindowSize><Options><FilterType>0</FilterType><Truncation>2</Truncation><Conflict>0</Conflict></Options><Commands><Add><ClientId>1</ClientId><ApplicationData><Contacts:FileAs>Kneschke, Lars</Contacts:FileAs><Contacts:FirstName>Lars</Contacts:FirstName><Contacts:LastName>Kneschke</Contacts:LastName></ApplicationData></Add></Commands></Collection></Collections></Sync>';
+    
+    /**
+     * Runs the test methods of this class.
+     *
+     * @access public
+     * @static
+     */
+    public static function main()
+    {
+        $suite  = new PHPUnit_Framework_TestSuite('Tine 2.0 ActiveSync Controller Email Tests');
+        PHPUnit_TextUI_TestRunner::run($suite);
+    }
+    
+    
+    protected function setUp()
+    {   	
+        ########### define test devices
+        $palm = ActiveSync_Backend_DeviceTests::getTestDevice();
+        $palm->devicetype = 'palm';
+        $palm->acsversion = '12.0';
+        $this->objects['devicePalm']   = ActiveSync_Controller_Device::getInstance()->create($palm);
+        
+        $iphone = ActiveSync_Backend_DeviceTests::getTestDevice();
+        $iphone->devicetype = 'iphone';
+        $iphone->acsversion = '2.5';
+        $this->objects['deviceIPhone'] = ActiveSync_Controller_Device::getInstance()->create($iphone);
+    }
+
+    /**
+     * Tears down the fixture
+     * This method is called after a test is executed.
+     *
+     * @access protected
+     */
+    protected function tearDown()
+    {
+        ActiveSync_Controller_Device::getInstance()->delete($this->objects['devicePalm']);
+        ActiveSync_Controller_Device::getInstance()->delete($this->objects['deviceIPhone']);
+    }
+    
+    /**
+     * validate getFolders for all devices except IPhone
+     */
+    public function testAppendFileReference()
+    {
+    	$controller = new ActiveSync_Controller_Email($this->objects['devicePalm'], new Zend_Date(null, null, 'de_DE'));
+    	
+        #$this->assertArrayHasKey("addressbook-root", $folders, "key addressbook-root not found");
+    }
+    
+}
+    
+if (PHPUnit_MAIN_METHOD == 'ActiveSync_Controller_EmailTests::main') {
+    ActiveSync_Controller_EmailTests::main();
+}
