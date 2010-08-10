f6cdfbaafec226aca9f34730002682a7046f0389
Philipp Schüle <p.schuele@metaways.de>
2010-08-10 13:13:01 +0000

    added header
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@15780 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Log/Formatter/Session.php
+++ tine20/Tinebase/Log/Formatter/Session.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Tinebase
+ * @subpackage  Log
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ * 
+ */
 
 /**
  * prefixes log statements with session info
