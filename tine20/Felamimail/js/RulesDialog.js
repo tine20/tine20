d9f01a52c7a3583089c2212729f2f23702dbc0ab
Philipp Schüle <p.schuele@metaways.de>
2010-08-11 16:30:07 +0000

    now extends Tine.widgets.dialog.EditDialog again
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@15819 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/js/RulesDialog.js
+++ tine20/Felamimail/js/RulesDialog.js
@@ -14,12 +14,12 @@ Ext.namespace('Tine.Felamimail');
 /**
  * @namespace   Tine.Felamimail
  * @class       Tine.Felamimail.RulesDialog
- * @extends     Ext.TabPanel
+ * @extends     Tine.widgets.dialog.EditDialog
  * 
  * <p>Sieve Filter Dialog</p>
  * <p>This dialog is editing sieve filters (rules).</p>
  * <p>
- * TODO         add ok/cancel buttons
+ * TODO         implement onApplyChanges
  * </p>
  * 
  * @author      Philipp Schuele <p.schuele@metaways.de>
@@ -31,28 +31,89 @@ Ext.namespace('Tine.Felamimail');
  * @constructor
  * Create a new RulesDialog
  */
-Tine.Felamimail.RulesDialog = Ext.extend(Ext.TabPanel, {
+Tine.Felamimail.RulesDialog = Ext.extend(Tine.widgets.dialog.EditDialog, {
 
     /**
      * @cfg {Tine.Felamimail.Model.Account}
      */
     account: null,
-    
-    activeTab: 0,
 
     /**
      * @private
      */
-    initComponent: function() {
+    windowNamePrefix: 'VacationEditWindow_',
+    appName: 'Felamimail',
+    //recordClass: Tine.Felamimail.Model.Rules,
+    loadRecord: false,
+    mode: 'local',
+    tbarItems: [],
+    evalGrants: false,
+    
+    /**
+     * overwrite update toolbars function (we don't have record grants yet)
+     * 
+     * @private
+     */
+    updateToolbars: function() {
+
+    },
+    
+    /**
+     * init record to edit
+     * -> we don't have a real record here
+     */
+    initRecord: function() {
+        this.onRecordLoad();
+    },
+    
+    /**
+     * executed after record got updated from proxy
+     * -> we don't have a real record here
+     * 
+     * @private
+     */
+    onRecordLoad: function() {
+        // interrupt process flow till dialog is rendered
+        if (! this.rendered) {
+            this.onRecordLoad.defer(250, this);
+            return;
+        }
         
-        this.app = Tine.Tinebase.appMgr.get('Felamimail');
+        var title = String.format(this.app.i18n._('Sieve Filter Rules for {0}'), this.account.get('name'));
+        this.window.setTitle(title);
         
-        this.items = [new Tine.Felamimail.RulesGridPanel({
-            title: String.format(this.app.i18n._('Sieve Filter Rules for {0}'), this.account.get('name')),
+        this.loadMask.hide();
+    },
+        
+    /**
+     * returns dialog
+     * 
+     * NOTE: when this method gets called, all initalisation is done.
+     * 
+     * @return {Object}
+     * @private
+     * 
+     */
+    getFormItems: function() {
+        this.rulesGrid = new Tine.Felamimail.RulesGridPanel({
             account: this.account
-        })];
+        }); 
         
-        Tine.Felamimail.RulesDialog.superclass.initComponent.call(this);
+        return [this.rulesGrid];
+    },
+    
+    /**
+     * generic apply changes handler
+     * 
+     * TODO get all rules from grid and send them to Felamimail.saveRules
+     */
+    onApplyChanges: function(button, event, closeWindow) {
+        Tine.log.info('not yet implemented');
+        
+        if (closeWindow) {
+            this.purgeListeners();
+            this.window.close();
+        }
     }
 });
 
@@ -64,8 +125,8 @@ Tine.Felamimail.RulesDialog = Ext.extend(Ext.TabPanel, {
  */
 Tine.Felamimail.RulesDialog.openWindow = function (config) {
     var window = Tine.WindowFactory.getWindow({
-        width: 640,
-        height: 480,
+        width: 800,
+        height: 400,
         name: Tine.Felamimail.RulesDialog.prototype.windowNamePrefix + Ext.id(),
         contentPanelConstructor: 'Tine.Felamimail.RulesDialog',
         contentPanelConstructorConfig: config
