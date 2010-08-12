024e235f3cbaf47d1cbb5f2d3a18b09f0e7e9285
Lars Kneschke <l.kneschke@metaways.de>
2010-08-12 20:00:50 +0000

    moved getNamespace function to Zend class
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@15838 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/Protocol/Imap.php
+++ tine20/Felamimail/Protocol/Imap.php
@@ -198,44 +198,6 @@ class Felamimail_Protocol_Imap extends Zend_Mail_Protocol_Imap
     }
 
     /**
-     * get server namespace
-     * 
-     * @return bool|array false if error, array with returned namespace information
-     */
-    public function getNamespace()
-    {
-        $this->sendRequest('NAMESPACE', array(), $tag);
-
-        $result = array();
-        while (!$this->readLine($tokens, $tag)) {
-            //if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' ' . print_r($tokens, TRUE));
-            
-            $nsNames = array('personal', 'other', 'shared');
-            $index = 0;
-            
-            foreach ($tokens as $token) {
-                if (is_array($token)) {
-                    $result[$nsNames[$index]] = array(
-                        'name' => preg_replace('/"/', '', $token[0][0]), 
-                        'delimiter' => preg_replace('/"/', '', $token[0][1]),
-                    );
-                } else if ($token == 'NIL') {
-                    $result[$nsNames[$index]] = array('name' => 'NIL');
-                } else {
-                    continue;
-                }
-                $index++;
-            }
-        }
-
-        if ($tokens[0] != 'OK') {
-            return false;
-        }
-        
-        return $result;
-    }
-    
-    /**
      * get status of a folder (unseen, recent, ...)
      * 
      * @param  string $box which folder to change to or examine
