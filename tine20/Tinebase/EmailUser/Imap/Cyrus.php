d8f15b7eece7ec6d9c001cd33d14d33746194dc9
Lars Kneschke <l.kneschke@metaways.de>
2010-08-12 19:54:46 +0000

    first draft of class to manage imap accounts on cyrus imap servers
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@15834 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/EmailUser/Imap/Cyrus.php
+++ tine20/Tinebase/EmailUser/Imap/Cyrus.php
@@ -8,8 +8,6 @@
  * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Philipp Schuele <p.schuele@metaways.de>
  * @version     $Id$
- * 
- * @todo        implement
  */
 
 /**
@@ -23,14 +21,47 @@
 class Tinebase_EmailUser_Imap_Cyrus extends Tinebase_EmailUser_Abstract
 {
     /**
+     * 
+     * @var Zend_Mail_Protocol_Imap
+     */
+    protected $_imap;
+    
+    /**
+     * the constructor
+     *
+     */
+    public function __construct()
+    {
+        $this->_config = Tinebase_Config::getInstance()->getConfigAsArray(Tinebase_Model_Config::IMAP);
+        
+        #$this->_config = array_merge($imapConfig['cyrus'], $this->_config);
+        #$this->_config['domain'] = (isset($imapConfig['domain'])) ? $imapConfig['domain'] : '';
+        
+        Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ . '  imap config: ' . print_r($this->_config, true));
+    }
+    
+    /**
      * get email user by id
      *
-     * @param   int         $_userId
+     * @todo    retrieve quota from imap server
+     * 
+     * @param   string  $_userId
      * @return  Tinebase_Model_EmailUser user
      */
     public function getUserById($_userId)
     {
-        throw new Tinebase_Exception('not implemented yet');
+        $user = ($_userId instanceof Tinebase_Model_FullUser) ? $_userId : Tinebase_User::getInstance()->getFullUserById($_userId);
+        
+        #$imap = $this->_getImapConnection();
+        #$imap->getQuota();
+        
+        $data = array(
+            'emailUserId'   => $user->accountLoginName,
+            'emailMailQuota' => 0,
+            'emailMailSize'  => 0
+        );
+        
+        return new Tinebase_Model_EmailUser($data, true);
     }
 
     /**
@@ -43,7 +74,7 @@ class Tinebase_EmailUser_Imap_Cyrus extends Tinebase_EmailUser_Abstract
      */
     public function addUser($_user, Tinebase_Model_EmailUser $_emailUser)
     {
-        throw new Tinebase_Exception('not implemented yet');
+        return $this->updateUser($_user, $_emailUser);
     }
     
     /**
@@ -55,7 +86,24 @@ class Tinebase_EmailUser_Imap_Cyrus extends Tinebase_EmailUser_Abstract
      */
     public function updateUser($_user, Tinebase_Model_EmailUser $_emailUser)
     {
-        throw new Tinebase_Exception('not implemented yet');
+        $imap = $this->_getImapConnection();
+        
+        $mailboxString = $this->_getUserMailbox($_user->accountLoginName);
+        
+        $mailbox = $imap->listMailbox('', $mailboxString);
+        
+        Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ . '  imap config: ' . print_r($mailbox, true));
+        
+        if (!array_key_exists($mailboxString, $mailbox)) {
+            Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ . ' must create mailbox ');
+            if ($imap->create($mailboxString) == true) {
+                if ($imap->setACL($mailboxString, $_user->accountLoginName, 'lrswipcda') !== true) {
+                    // failed to set acl
+                }
+            }
+        }
+        
+        return $this->getUserById($_user);
     }
 
     /**
@@ -65,7 +113,21 @@ class Tinebase_EmailUser_Imap_Cyrus extends Tinebase_EmailUser_Abstract
      */
     public function deleteUser($_userId)
     {
-        throw new Tinebase_Exception('not implemented yet');
+        $imap = $this->_getImapConnection();
+        
+        $mailboxString = $this->_getUserMailbox($_user->accountLoginName);
+        
+        $mailbox = $imap->listMailbox('', $mailboxString);
+        
+        Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ . '  imap config: ' . print_r($mailbox, true));
+        
+        // does mailbox exist at all?
+        if (array_key_exists($mailboxString, $mailbox)) {
+            Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ . ' must delete mailbox ');
+            if ($imap->setACL($mailboxString, $this->_config['cyrus']['admin'], 'lrswipcda') === true) {
+                $imap->delete($mailboxString);
+            }
+        }
     }
     
     /**
@@ -77,6 +139,42 @@ class Tinebase_EmailUser_Imap_Cyrus extends Tinebase_EmailUser_Abstract
      */
     public function setPassword($_userId, $_password)
     {
-        throw new Tinebase_Exception('not implemented yet');
+        // nothing to be done for cyrus imap server
+    }
+    
+    /**
+     * 
+     * @return Zend_Mail_Protocol_Imap
+     */
+    protected function _getImapConnection()
+    {
+        if (! $this->_imap instanceof Zend_Mail_Protocol_Imap) {
+            $this->_imap = new Zend_Mail_Protocol_Imap($this->_config['host'], $this->_config['port']);
+            $this->_imap->login($this->_config['cyrus']['admin'], $this->_config['cyrus']['password']);
+        }
+
+        return $this->_imap;
+    }
+
+    /**
+     * get mailbox string for users aka user.loginname
+     * 
+     * @param  string  $_username  the imap account name
+     * @throws Tinebase_Exception_InvalidArgument
+     * @return string
+     */
+    protected function _getUserMailbox($_username)
+    {
+        $imap = $this->_getImapConnection();
+        
+        $namespaces = $imap->getNamespace();
+        
+        if (!isset($namespaces['other'])) {
+            throw new Tinebase_Exception_InvalidArgument('other namespace not found');
+        }
+        
+        $mailboxString = $namespaces['other']['name'] . $_username;
+        
+        return $mailboxString;
     }
 }
