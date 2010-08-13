b2ec7a80ac27ee22a869d1c74a306472677d834b
Lars Kneschke <l.kneschke@metaways.de>
2010-08-13 05:18:43 +0000

    resolve school and course name
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@15851 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Courses/Controller/Course.php
+++ tine20/Courses/Controller/Course.php
@@ -99,35 +99,38 @@ class Courses_Controller_Course extends Tinebase_Controller_Record_Abstract
      */
     public function create(Tinebase_Record_Interface $_record)
     {
-        $record = parent::create($_record);
+        $course = parent::create($_record);
         
         // add teacher account
         $i18n = Tinebase_Translation::getTranslation('Courses');
-        $loginname = $record->name . '-' . $i18n->_('teacher');
+        
+        $courseName = strtolower($course->name);
+        $loginName  = strtolower($course->name . '-' . $i18n->_('teacher'));
+        $schoolName = strtolower(Tinebase_Department::getInstance()->get($course->type)->name); 
         
         $account = new Tinebase_Model_FullUser(array(
-            'accountLoginName'      => $loginname,
+            'accountLoginName'      => $loginName,
             'accountLoginShell'     => '/bin/false',
             'accountStatus'         => 'enabled',
-            'accountPrimaryGroup'   => $record->group_id,
+            'accountPrimaryGroup'   => $course->group_id,
             'accountLastName'       => $i18n->_('Teacher'),
-            'accountDisplayName'    => $record->name . ' ' .  $i18n->_('Teacher Account'),
-            'accountFirstName'      => $record->name,
+            'accountDisplayName'    => $course->name . ' ' .  $i18n->_('Teacher Account'),
+            'accountFirstName'      => $course->name,
             'accountExpires'        => NULL,
-            'accountEmailAddress'   => (isset($this->_config->domain)&& !empty($this->_config->domain)) ? $loginname . '@' . $this->_config->domain : '',
-            'accountHomeDirectory'  => '/storage/klassen/bs/gs91c/' . $loginname
+            'accountEmailAddress'   => (isset($this->_config->domain)&& !empty($this->_config->domain)) ? $loginName . '@' . $this->_config->domain : '',
+            'accountHomeDirectory'  => '/storage/klassen/' . $schoolName . '/'. $courseName . '/' . $loginName
         ));
         
         $samUser = new Tinebase_Model_SAMUser(array(
-            'homePath' => '\\\\iserver2\\' . $account->accountLoginName,
-            'homeDrive' => 'U:',
-            'logonScript' => strtolower($record->name) . '.bat',
-            'profilePath' => '\\\\iserver2\\profiles\\bs\\gs91c\\' . $loginname
+            'homePath'    => '\\\\iserver2\\' . $loginName,
+            'homeDrive'   => 'U:',
+            'logonScript' => $courseName . '-lehrer.bat',
+            'profilePath' => '\\\\iserver2\\profiles\\' . $schoolName . '\\' . $courseName . '\\' . $loginName
         ));
         
         $account->sambaSAM = $samUser;
         
-        #$event = new Courses_Event_BeforeAddTeacher($account, $record);
+        #$event = new Courses_Event_BeforeAddTeacher($account, $course);
         #Tinebase_Event::fireEvent($event);
         
         $password = $this->_config->get('teacher_password', $account->accountLoginName);
@@ -143,7 +146,7 @@ class Courses_Controller_Course extends Tinebase_Controller_Record_Abstract
             Admin_Controller_Group::getInstance()->addGroupMember($this->_config->students_group, $account->getId());
         }
         
-        return $record;
+        return $course;
     }
     
     /**
