999d3c8f5bebfa558f85651e9e7d54f8d5979a75
Lars Kneschke <l.kneschke@metaways.de>
2010-08-16 01:20:57 +0000

    set the correct parameters
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@15881 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Admin/Controller/User.php
+++ tine20/Admin/Controller/User.php
@@ -192,7 +192,7 @@ class Admin_Controller_User extends Tinebase_Controller_Abstract
             throw new Admin_Exception("Passwords don't match.");
         }
         
-        $this->_userBackend->setPassword($_account, $_password, $_mustChange);
+        $this->_userBackend->setPassword($_account, $_password, true, $_mustChange);
         
         Tinebase_Core::getLogger()->info(
             __METHOD__ . '::' . __LINE__ . 
