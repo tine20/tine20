d1eb34a1810282fd0a40f2fe07a6850d23fbf058
Philipp Schüle <p.schuele@metaways.de>
2010-08-17 09:12:03 +0000

    disabled user reg tests
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@15886 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/tine20/Tinebase/User/AllTests.php
+++ tests/tine20/Tinebase/User/AllTests.php
@@ -26,7 +26,8 @@ class Tinebase_User_AllTests
     {
         $suite = new PHPUnit_Framework_TestSuite('Tine 2.0 Tinebase All User Tests');
         $suite->addTestSuite('Tinebase_User_SqlTest');
-        $suite->addTestSuite('Tinebase_User_RegistrationTest');
+        // disabled user registration tests -> this is not used atm and not functional
+        //$suite->addTestSuite('Tinebase_User_RegistrationTest');
         $suite->addTestSuite('Tinebase_User_ModelTest');
         $suite->addTestSuite('Tinebase_User_AbstractTest');
         
