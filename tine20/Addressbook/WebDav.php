d0f7fe96a5b7cc22b8c8d502e4b6cca4153abbef
Lars Kneschke <l.kneschke@metaways.de>
2010-08-18 13:58:31 +0000

    class to handle webdav requests for addressbook
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@15929 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Addressbook/WebDav.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Addressbook
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ * 
+ */
+
+/**
+ * class to handle webdav requests for addressbook
+ * 
+ * @package     Addressbook
+ */
+class Addressbook_WebDav extends Tinebase_WebDav_Application_Abstract
+{
+    protected $_modelName = 'Contact';
+}
