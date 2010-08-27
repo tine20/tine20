70f8d4dc6ae8cb64f152dd12f48e78e58e8b5ca2
Cornelius Weiß <c.weiss@metaways.de>
2010-08-27 14:00:20 +0000

    add color validator
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@16055 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Model/Container.php
+++ tine20/Tinebase/Model/Container.php
@@ -81,6 +81,7 @@ class Tinebase_Model_Container extends Tinebase_Record_Abstract
         'name'              => array('presence' => 'required'),
         'type'              => array('InArray' => array(self::TYPE_INTERNAL, self::TYPE_PERSONAL, self::TYPE_SHARED)),
         'backend'           => array('presence' => 'required'),
+        'color'             => array('allowEmpty' => true, array('regex', '/^#[0-9a-fA-F]{6}$/')),
         'application_id'    => array('Alnum', 'presence' => 'required'),
         'account_grants'    => array('allowEmpty' => true), // non persistent
         'path'              => array('allowEmpty' => true), // non persistent
