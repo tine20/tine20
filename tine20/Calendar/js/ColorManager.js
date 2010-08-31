ee39465e6b44e658d7356be1e6ed596e6306028b
Cornelius Weiß <c.weiss@metaways.de>
2010-08-31 08:14:41 +0000

    - update color definitions - no more state saving
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@16080 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Calendar/js/ColorManager.js
+++ tine20/Calendar/js/ColorManager.js
@@ -86,7 +86,7 @@ Ext.extend(Tine.Calendar.ColorManager, Ext.util.Observable, {
      * @cfg {Boolean} stateful
      * Is this component statefull?
      */
-    stateful: true,
+    stateful: false,
     
     /**
      * current color map 
@@ -110,7 +110,7 @@ Ext.extend(Tine.Calendar.ColorManager, Ext.util.Observable, {
      * @type Object 
      * @property gray
      */
-    gray: {color: '#808080', light: '#EDEDED', text: '#808080', lightText: '#FFFFFF'},
+    gray: {color: '#808080', light: '#EDEDED', text: '#FFFFFF', lightText: '#FFFFFF'},
     
     /**
      * color palette from Ext.ColorPalette
@@ -126,48 +126,49 @@ Ext.extend(Tine.Calendar.ColorManager, Ext.util.Observable, {
      * @type Array 
      * @property colorSchemata
      */
-    colorSchemata : [
-        /*"000000" :*/ {},
-        /*"993300" :*/ {},
-        /*"333300" :*/ {}, 
-        /*"003300" :*/ {},
-        /*"003366" :*/ {},
-        /*"000080" :*/ {},
-        /*"333399" :*/ {},
-        /*"333333" :*/ {},
-        /*"800000" :*/ {},
-        /*"FF6600" :*/ {color: '#FF7200', light: '#FFB87F', text: '#FFFFFF', lightText: '#FFFFFF'}, // orange
-        /*"808000" :*/ {},
-        /*"008000" :*/ {},
-        /*"008080" :*/ {},
-        /*"0000FF" :*/ {},
-        /*"666699" :*/ {},
-        /*"808080" :*/ {},
-        /*"FF0000" :*/ {color: '#FD0000', light: '#FE7F7F', text: '#FFFFFF', lightText: '#FFFFFF'}, // red
-        /*"FF9900" :*/ {},
-        /*"99CC00" :*/ {},
-        /*"339966" :*/ {},
-        /*"33CCCC" :*/ {},
-        /*"3366FF" :*/ {color: '#0050D9', light: '#7FA7EC', text: '#FFFFFF', lightText: '#FFFFFF'}, // blue
-        /*"800080" :*/ {},
-        /*"969696" :*/ {},
-        /*"FF00FF" :*/ {color: '#C302B1', light: '#E080D7', text: '#FFFFFF', lightText: '#FFFFFF'}, // purple
-        /*"FFCC00" :*/ {},
-        /*"FFFF00" :*/ {},
-        /*"00FF00" :*/ {color: '#00A700', light: '#7FD27F', text: '#FFFFFF', lightText: '#FFFFFF'}, // green
-        /*"00FFFF" :*/ {},
-        /*"00CCFF" :*/ {},
-        /*"993366" :*/ {color: '#5123A5', light: '#A790D1', text: '#FFFFFF', lightText: '#FFFFFF'}, // violet
-        /*"C0C0C0" :*/ {},
-        /*"FF99CC" :*/ {},
-        /*"FFCC99" :*/ {},
-        /*"FFFF99" :*/ {},
-        /*"CCFFCC" :*/ {},
-        /*"CCFFFF" :*/ {},
-        /*"99CCFF" :*/ {},
-        /*"CC99FF" :*/ {},
-        /*"FFFFFF" :*/ {}
-    ],
+    colorSchemata : {
+        "000000" : {color: '#000000', light: '#8F8F8F', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "993300" : {color: '#993300', light: '#CEA590', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "333300" : {color: '#333300', light: '#A6A691', text: '#FFFFFF', lightText: '#FFFFFF'}, 
+        "003300" : {color: '#003300', light: '#8FA48F', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "003366" : {color: '#003366', light: '#90A5B9', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "000080" : {color: '#000080', light: '#9090C4', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "333399" : {color: '#333399', light: '#A5A5CE', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "333333" : {color: '#333333', light: '#A6A6A6', text: '#FFFFFF', lightText: '#FFFFFF'},
+        
+        "800000" : {color: '#800000', light: '#C79393', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "FF6600" : {color: '#FF6600', light: '#F8BB92', text: '#FFFFFF', lightText: '#FFFFFF'}, // orange
+        "808000" : {color: '#808000', light: '#C6C692', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "008000" : {color: '#008000', light: '#92C692', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "008080" : {color: '#008080', light: '#91C5C5', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "0000FF" : {color: '#0000FF', light: '#9292F8', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "666699" : {color: '#666699', light: '#BBBBD0', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "808080" : {color: '#808080', light: '#C6C6C6', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "FF0000" : {color: '#FF0000', light: '#F89292', text: '#FFFFFF', lightText: '#FFFFFF'}, // red
+        "FF9900" : {color: '#FF9900', light: '#F8D092', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "99CC00" : {color: '#99CC00', light: '#D0E492', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "339966" : {color: '#339966', light: '#A7D0BB', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "33CCCC" : {color: '#33CCCC', light: '#A8E5E5', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "3366FF" : {color: '#3366FF', light: '#A7BBF8', text: '#FFFFFF', lightText: '#FFFFFF'}, // blue
+        "800080" : {color: '#800080', light: '#C692C6', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "969696" : {color: '#969696', light: '#CECECE', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "FF00FF" : {color: '#FF00FF', light: '#F690F6', text: '#FFFFFF', lightText: '#FFFFFF'}, // purple
+        "FFCC00" : {color: '#FFCC00', light: '#F7E391', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "FFFF00" : {color: '#FFFF00', light: '#F7F791', text: '#000000', lightText: '#000000'},
+        "00FF00" : {color: '#00FF00', light: '#93F993', text: '#000000', lightText: '#000000'}, // green
+        "00FFFF" : {color: '#00FFFF', light: '#93F9F9', text: '#000000', lightText: '#000000'},
+        "00CCFF" : {color: '#00CCFF', light: '#93E5F9', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "993366" : {color: '#993366', light: '#D1A8BC', text: '#FFFFFF', lightText: '#FFFFFF'}, // violet
+        "C0C0C0" : {color: '#C0C0C0', light: '#DFDFDF', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "FF99CC" : {color: '#FF99CC', light: '#F8F0E4', text: '#FFFFFF', lightText: '#FFFFFF'},
+        "FFCC99" : {color: '#FFCC99', light: '#F8E4D0', text: '#000000', lightText: '#000000'},
+        "FFFF99" : {color: '#FFFF99', light: '#F9F9D1', text: '#000000', lightText: '#000000'},
+        "CCFFCC" : {color: '#CCFFCC', light: '#E5F9E5', text: '#000000', lightText: '#000000'},
+        "CCFFFF" : {color: '#CCFFFF', light: '#E5F9F9', text: '#000000', lightText: '#000000'},
+        "99CCFF" : {color: '#99CCFF', light: '#D0E4F8', text: '#000000', lightText: '#000000'},
+        "CC99FF" : {color: '#CC99FF', light: '#E5D1F9', text: '#000000', lightText: '#000000'},
+        "FFFFFF" : {color: '#DFDFDF', light: '#F8F8F8', text: '#000000', lightText: '#000000'}
+    },
     
     /**
      * hack for container only support
@@ -188,70 +189,75 @@ Ext.extend(Tine.Calendar.ColorManager, Ext.util.Observable, {
             }
         }
         
-        var container_id = container.id ? container.id : container;
-        return container ? this.getColorSchema(container_id) : this.gray;
-    },
-    
-    /**
-     * gets the next free color set
-     * 
-     * @param {String} item e.g. a calendar id
-     * @return {Object} colorset
-     */
-    getColorSchema: function(item) {
-        if (this.colorMap[item]) {
-            return this.colorSchemata[this.colorMap[item]];
+        if (! container.color) {
+            return this.gray;
         }
         
-        // find a 'free' schema
-        for (var i=1,cpi; i<=this.colorPalette.length; i++) {
-            // color palette index
-            cpi = (i+this.colorSchemataPointer) % this.colorPalette.length;
-            if (this.colorSchemata[cpi].color && !this.inUse(this.colorPalette[cpi])) {
-                this.colorSchemataPointer = cpi;
-                this.colorMap[item] = this.colorSchemataPointer;
-                this.saveState();
-                //console.log('assigned color ' + this.colorMap[item] + ' to item ' + item);
-                
-                return this.colorSchemata[this.colorSchemataPointer];
-            }
-        }
-
-        // no more free colors ;-(
-        this.colorSchemataPointer++;
-        this.colorMap[item] = this.colorSchemataPointer;
-        return this.colorSchemata[this.colorSchemataPointer];
-    },
-    
-    /**
-     * checkes if given color is already in use
-     * 
-     * @param {String} color
-     * @return {Boolean}
-     */
-    inUse: function(color) {
-        for (var item in this.colorMap) {
-            if (this.colorMap.hasOwnProperty(item) && this.colorMap[item] == color) {
-                //console.log(color + ' is already used');
-                return true;
-            }
-        }
-        //console.log(color + 'is not in use yet');
-        return false;
-    },
-    
-    /* state handling */
-    initState:       Ext.Component.prototype.initState,
-    getStateId:      Ext.Component.prototype.getStateId,
-    //initStateEvents: Ext.Component.prototype.initState,
-    applyState:      Ext.Component.prototype.applyState,
-    saveState:       Ext.Component.prototype.saveState,
-    getState:        function() {
-        return {
-            colorMap            : this.colorMap,
-            colorSchemataPointer: this.colorSchemataPointer
-        };
+        return this.colorSchemata[container.color.replace('#', '')];
+        //var container_id = container.id ? container.id : container;
+        //return container ? this.getColorSchema(container_id) : this.gray;
     }
+    
+//    /**
+//     * gets the next free color set
+//     * 
+//     * @param {String} item e.g. a calendar id
+//     * @return {Object} colorset
+//     */
+//    getColorSchema: function(item) {
+//        if (this.colorMap[item]) {
+//            return this.colorSchemata[this.colorMap[item]];
+//        }
+//        
+//        // find a 'free' schema
+//        for (var i=1,cpi; i<=this.colorPalette.length; i++) {
+//            // color palette index
+//            cpi = (i+this.colorSchemataPointer) % this.colorPalette.length;
+//            if (this.colorSchemata[cpi].color && !this.inUse(this.colorPalette[cpi])) {
+//                this.colorSchemataPointer = cpi;
+//                this.colorMap[item] = this.colorSchemataPointer;
+//                this.saveState();
+//                //console.log('assigned color ' + this.colorMap[item] + ' to item ' + item);
+//                
+//                return this.colorSchemata[this.colorSchemataPointer];
+//            }
+//        }
+//
+//        // no more free colors ;-(
+//        this.colorSchemataPointer++;
+//        this.colorMap[item] = this.colorSchemataPointer;
+//        return this.colorSchemata[this.colorSchemataPointer];
+//    },
+//    
+//    /**
+//     * checkes if given color is already in use
+//     * 
+//     * @param {String} color
+//     * @return {Boolean}
+//     */
+//    inUse: function(color) {
+//        for (var item in this.colorMap) {
+//            if (this.colorMap.hasOwnProperty(item) && this.colorMap[item] == color) {
+//                //console.log(color + ' is already used');
+//                return true;
+//            }
+//        }
+//        //console.log(color + 'is not in use yet');
+//        return false;
+//    },
+//    
+//    /* state handling */
+//    initState:       Ext.Component.prototype.initState,
+//    getStateId:      Ext.Component.prototype.getStateId,
+//    //initStateEvents: Ext.Component.prototype.initState,
+//    applyState:      Ext.Component.prototype.applyState,
+//    saveState:       Ext.Component.prototype.saveState,
+//    getState:        function() {
+//        return {
+//            colorMap            : this.colorMap,
+//            colorSchemataPointer: this.colorSchemataPointer
+//        };
+//    }
     
     
     
