509898b45cf99335631dc947511bd127167dde96
Philipp Schüle <p.schuele@metaways.de>
2010-09-07 11:38:12 +0000

    added account model test class with smtp config test
    0002994: felamimail smtp auth doesnt append domain name
    http://www.tine20.org/bugtracker/view.php?id=2994
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@16171 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/tine20/Felamimail/AllTests.php
+++ tests/tine20/Felamimail/AllTests.php
@@ -47,6 +47,7 @@ class Felamimail_AllTests
             $suite->addTestSuite('Felamimail_Controller_MessageTest');
             $suite->addTestSuite('Felamimail_Controller_AccountTest');
             $suite->addTestSuite('Felamimail_Model_MessageTest');
+            $suite->addTestSuite('Felamimail_Model_AccountTest');
         }
         $suite->addTestSuite('Felamimail_Sieve_ScriptTest');
         
---
+++ tests/tine20/Felamimail/Model/AccountTest.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Felamimail
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @version     $Id$
+ * 
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Felamimail_Model_AccountTest::main');
+}
+
+/**
+ * Test class for Felamimail_Model_AccountTest
+ */
+class Felamimail_Model_AccountTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Runs the test methods of this class.
+     *
+     * @access public
+     * @static
+     */
+    public static function main()
+    {
+		$suite  = new PHPUnit_Framework_TestSuite('Tine 2.0 Felamimail Account Model Tests');
+        PHPUnit_TextUI_TestRunner::run($suite);
+	}
+
+    /**
+     * Sets up the fixture.
+     * This method is called before a test is executed.
+     *
+     * @access protected
+     */
+    protected function setUp()
+    {
+    }
+
+    /**
+     * Tears down the fixture
+     * This method is called after a test is executed.
+     *
+     * @access protected
+     */
+    protected function tearDown()
+    {
+    }
+
+    /********************************* test funcs *************************************/
+    
+    /**
+     * test get smtp config
+     */
+    public function testGetSmtpConfig()
+    {
+        $smtpConfig = Tinebase_Config::getInstance()->getConfigAsArray(Tinebase_Model_Config::SMTP);
+        
+        $account = new Felamimail_Model_Account(array(
+            'type'      => Felamimail_Model_Account::TYPE_SYSTEM,
+        ));
+        $accountSmtpConfig = $account->getSmtpConfig();
+        
+        if (array_key_exists('primarydomain', $smtpConfig)) {
+            $this->assertContains($smtpConfig['primarydomain'], $accountSmtpConfig['username']);
+        }
+        
+        $this->assertEquals('mail.metaways.net', $accountSmtpConfig['hostname']);
+    }
+}
