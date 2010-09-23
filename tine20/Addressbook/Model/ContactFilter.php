862698fc82df1342208c8b968973a2d3f6aa423f
Cornelius Weiß <c.weiss@metaways.de>
2010-09-23 14:43:43 +0000

    add org_unit to filters
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@16378 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Addressbook/Model/ContactFilter.php
+++ tine20/Addressbook/Model/ContactFilter.php
@@ -41,7 +41,7 @@ class Addressbook_Model_ContactFilter extends Tinebase_Model_Filter_FilterGroup
         'id'                   => array('filter' => 'Tinebase_Model_Filter_Id'),
         'query'                => array(
             'filter' => 'Tinebase_Model_Filter_Query', 
-            'options' => array('fields' => array('n_family', 'n_given', 'org_name', 'email', 'adr_one_locality'))
+            'options' => array('fields' => array('n_family', 'n_given', 'org_name', 'org_unit', 'email', 'adr_one_locality'))
         ),
         'telephone'            => array(
             'filter' => 'Tinebase_Model_Filter_Query', 
@@ -70,6 +70,7 @@ class Addressbook_Model_ContactFilter extends Tinebase_Model_Filter_FilterGroup
         'n_family'             => array('filter' => 'Tinebase_Model_Filter_Text'),
         'n_fileas'             => array('filter' => 'Tinebase_Model_Filter_Text'),
         'org_name'             => array('filter' => 'Tinebase_Model_Filter_Text'),
+        'org_unit'             => array('filter' => 'Tinebase_Model_Filter_Text'),
         'title'                => array('filter' => 'Tinebase_Model_Filter_Text'),
         'adr_one_street'       => array('filter' => 'Tinebase_Model_Filter_Text'),
         'adr_one_postalcode'   => array('filter' => 'Tinebase_Model_Filter_Text'),
