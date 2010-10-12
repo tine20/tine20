7d335dee5c04779380158a8268e6251dc12d1c08
Philipp Schüle <p.schuele@metaways.de>
2010-10-12 09:35:01 +0000

    lowered loglevel
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@16586 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Export/Abstract.php
+++ tine20/Tinebase/Export/Abstract.php
@@ -297,7 +297,7 @@ abstract class Tinebase_Export_Abstract
                     Tinebase_Core::getLogger()->info(__METHOD__ . '::' . __LINE__ . ' Using definition from file ' . $definition->filename);
                     $definition->plugin_options = $fileDefinition->plugin_options;
                 } catch (Tinebase_Exception_NotFound $tenf) {
-                    Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ . ' ' . $tenf->getMessage());
+                    Tinebase_Core::getLogger()->notice(__METHOD__ . '::' . __LINE__ . ' ' . $tenf->getMessage());
                 }
             }
         }
