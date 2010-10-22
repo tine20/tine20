462e0b8c54daf0d8819d6b1648054ef51da8cf6e
Philipp Schüle <p.schuele@metaways.de>
2010-10-22 17:00:34 +0000

    - add additional record if contact has multiple email addresses - add name (n_fileas) to value of combobox 0001748: compose email only suggests company email address http://www.tine20.org/bugtracker/view.php?id=1748 0003212: add recipient name again http://www.tine20.org/bugtracker/view.php?id=3212
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@16776 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/js/ContactSearchCombo.js
+++ tine20/Felamimail/js/ContactSearchCombo.js
@@ -23,7 +23,7 @@ Ext.namespace('Tine.Felamimail');
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Philipp Schuele <p.schuele@metaways.de>
  * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
- * @version     $Id:MessageEditDialog.js 7170 2009-03-05 10:58:55Z p.schuele@metaways.de $
+ * @version     $Id$
  * 
  * @param       {Object} config
  * @constructor
@@ -47,20 +47,6 @@ Tine.Felamimail.ContactSearchCombo = Ext.extend(Tine.Addressbook.SearchCombo, {
             '<tpl for="."><div class="search-item">',
                 '{[this.encode(values.n_fileas)]}',
                 ' (<b>{[this.encode(values.email, values.email_home)]}</b>)',
-                /*
-                '<table cellspacing="0" cellpadding="2" border="0" style="font-size: 11px;" width="100%">',
-                    '<tr>',
-                        //'<td width="50%"><b>{[this.encode(values.n_fileas)]}</b></td>',
-                        //'<td width="50%"><b>{[this.encode(values.email)]}</b></td>',
-                        '<td width="40%"><b>{[this.encode(values.n_fileas)]}</b><br/>{[this.encode(values.org_name)]}</td>',
-                        '<td width="40%">{[this.encode(values.email)]}<br/>',
-                            '{[this.encode(values.email_home)]}</td>',
-                        '<td width="20%">',
-                            '<img width="45px" height="39px" src="{jpegphoto}" />',
-                        '</td>',
-                    '</tr>',
-                '</table>',
-                */
             '</div></tpl>',
             {
                 encode: function(email, email_home) {
@@ -76,6 +62,8 @@ Tine.Felamimail.ContactSearchCombo = Ext.extend(Tine.Addressbook.SearchCombo, {
         );
         
         Tine.Felamimail.ContactSearchCombo.superclass.initComponent.call(this);
+        
+        this.store.on('load', this.onStoreLoad, this);
     },
     
     /**
@@ -84,18 +72,43 @@ Tine.Felamimail.ContactSearchCombo = Ext.extend(Tine.Addressbook.SearchCombo, {
      * 
      * @param {} record
      * @private
-     * 
-     * TODO add name
-     * TODO make it possible to choose between office/home email addresses
      */
     onSelect: function(record) {
+        var value = record.get('n_fileas') + ' <';
         if (record.get('email') != '') {
-            this.setValue(record.get('email'));
+            value += record.get('email');
         } else {
-            this.setValue(record.get('email_home'));
-        } 
+            value += record.get('email_home');
+        }
+        value += '>';
+        this.setValue(value);
+        
         this.collapse();
         this.fireEvent('blur', this);
-    }    
+    },
+    
+    /**
+     * on load handler of combo store
+     * -> add additional record if contact has multiple email addresses
+     * 
+     * @param {} store
+     * @param {} records
+     * @param {} options
+     */
+    onStoreLoad: function(store, records, options) {
+        var index = 0,
+            newRecord,
+            recordData;
+            
+        Ext.each(records, function(record) {
+            if (record.get('email') && record.get('email_home')) {
+                index++;
+                recordData = Ext.copyTo({}, record.data, ['email_home', 'n_fileas']);
+                newRecord = Tine.Addressbook.contactBackend.recordReader({responseText: Ext.util.JSON.encode(recordData)});
+                store.insert(index, [newRecord]);
+            }
+            index++;
+        });
+    }
 });
 Ext.reg('felamimailcontactcombo', Tine.Felamimail.ContactSearchCombo);
