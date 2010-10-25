1d214daf563298e4f9a901b43d99478032f503b9
Philipp Schüle <p.schuele@metaways.de>
2010-10-25 14:39:28 +0000

    added getRawMessage()
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@16792 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/Transport.php
+++ tine20/Felamimail/Transport.php
@@ -5,7 +5,7 @@
  * @package     Felamimail
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Philipp Schuele <p.schuele@metaways.de>
- * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2009-2010 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
  * 
  */
@@ -40,8 +40,6 @@ class Felamimail_Transport extends Zend_Mail_Transport_Smtp
      * get mail headers as string
      *
      * @return string
-     * 
-     * @todo add creation of headers from Zend_Mail?
      */
     public function getHeaders()
     {
@@ -51,5 +49,63 @@ class Felamimail_Transport extends Zend_Mail_Transport_Smtp
         }
         
         return $this->header;
+    }
+    
+    /**
+     * get raw message as string
+     * 
+     * @param Zend_Mail $mail
+     * @return string
+     */
+    public function getRawMessage(Zend_Mail $mail = NULL)
+    {
+        if ($mail !== NULL) {
+            // this part is from Zend_Mail_Transport_Abstract::send()
+            $this->_isMultipart = false;
+            $this->_mail        = $mail;
+            $this->_parts       = $mail->getParts();
+            $mime               = $mail->getMime();
+    
+            // Build body content
+            $this->_buildBody();
+    
+            // Determine number of parts and boundary
+            $count    = count($this->_parts);
+            $boundary = null;
+            if ($count < 1) {
+                /**
+                 * @see Zend_Mail_Transport_Exception
+                 */
+                require_once 'Zend/Mail/Transport/Exception.php';
+                throw new Zend_Mail_Transport_Exception('Mail is empty');
+            }
+    
+            if ($count > 1) {
+                // Multipart message; create new MIME object and boundary
+                $mime     = new Zend_Mime($this->_mail->getMimeBoundary());
+                $boundary = $mime->boundary();
+            } elseif ($this->_isMultipart) {
+                // multipart/alternative -- grab boundary
+                $boundary = $this->_parts[0]->boundary;
+            }
+    
+            // Determine recipients, and prepare headers
+            $this->recipients = implode(',', $mail->getRecipients());
+            $this->_prepareHeaders($this->_getHeaders($boundary));
+    
+            // Create message body
+            // This is done so that the same Zend_Mail object can be used in
+            // multiple transports
+            $message = new Zend_Mime_Message();
+            $message->setParts($this->_parts);
+            $message->setMime($mime);
+            $this->body = $message->generateMessage($this->EOL);
+        }
+        
+        $mailAsString = $this->getHeaders() . $this->EOL. $this->getBody();
+        // convert \n to \r\n
+        $mailAsString = preg_replace("/(?<!\\r)\\n(?!\\r)/", "\r\n", $mailAsString);
+        
+        return $mailAsString;
     }
 }
