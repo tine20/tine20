89669b61884069ae23fba4ecad1810e8948d6fd9
Cornelius Weiß <c.weiss@metaways.de>
2010-10-28 09:30:11 +0000

    new itemRegistry ux for dynamic forms
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@16834 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Frontend/Http.php
+++ tine20/Tinebase/Frontend/Http.php
@@ -259,6 +259,7 @@ class Tinebase_Frontend_Http extends Tinebase_Frontend_Http_Abstract
             'Tinebase/js/ux/layout/Display.js',
             'Tinebase/js/ux/MessageBox.js',
             'Tinebase/js/ux/TabPanelSortPlugin.js',
+            'Tinebase/js/ux/ItemRegistry.js',
         // Tinebase
             // NOTE: All the data stuff is going to and extra worker build
             //'Tinebase/js/data/sync/Ping.js',
