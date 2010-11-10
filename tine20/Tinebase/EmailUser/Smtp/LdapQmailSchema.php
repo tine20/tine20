d1c96186b3c2e3c50ce061d03678985bce75b701
Lars Kneschke <l.kneschke@metaways.de>
2010-11-10 14:13:06 +0000

    Refactor Tine 2.0 Emailhandling
    http://www.tine20.org/bugtracker/view.php?id=3302
    
    rewrote ldap qmail class to work as plugin
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17014 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/EmailUser/Smtp/LdapQmailSchema.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Tinebase
+ * @subpackage  EmailUser
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @version     $Id$
+ * 
+ * @todo        add domain check for aliases
+ */
+
+/**
+ * plugin to handle smtp settings for qmail ldap schema
+ * 
+ * @package    Tinebase
+ * @subpackage EmailUser
+ */
+class Tinebase_EmailUser_Smtp_LdapQmailSchema extends Tinebase_EmailUser_Smtp_LdapDbmailSchema
+{
+    /**
+     * objectclasses required for users
+     *
+     * @var array
+     */
+    protected $_requiredObjectClass = array(
+        'qmailUser',
+    );
+}  
