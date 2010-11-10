23b01bf2e2955c1f4ec14ce91e1453b41cd99e2d
Lars Kneschke <l.kneschke@metaways.de>
2010-11-10 14:08:28 +0000

    Refactor Tine 2.0 Emailhandling
    http://www.tine20.org/bugtracker/view.php?id=3302
    
    rewrote ldap dmail class to work as plugin
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17008 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/EmailUser/Imap/LdapDbmailSchema.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Tinebase
+ * @subpackage  EmailUser
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @version     $Id: Ldap.php 10296 2009-09-02 14:12:35Z p.schuele@metaways.de $
+ */
+
+/**
+ * plugin to handle imap settings for dbmail ldap schema
+ * 
+ * @package    Tinebase
+ * @subpackage EmailUser
+ */
+class Tinebase_EmailUser_Imap_LdapDbmailSchema extends Tinebase_EmailUser_Ldap
+{
+    /**
+     * dbmail config
+     * 
+     * @var array 
+     */
+    protected $_config = array(
+        'prefix'            => 'dbmail_',
+        'userTable'         => 'users',
+        'encryptionType'    => 'md5',
+        'mailboxTable'      => 'mailboxes',
+        'emailGID'			=> null
+    );
+    
+    /**
+     * user properties mapping 
+     * -> we need to use lowercase for ldap fields because ldap_fetch returns lowercase keys
+     *
+     * @var array
+     */
+    protected $_propertyMapping = array(
+        'emailUID'          => 'dbmailuid', 
+        'emailGID'          => 'dbmailgid', 
+        'emailMailQuota'    => 'mailquota'
+    );
+    
+    /**
+     * objectclasses required for users
+     *
+     * @var array
+     */
+    protected $_requiredObjectClass = array(
+        'dbmailUser'
+    );    
+    
+    protected $_backendType = Tinebase_Model_Config::IMAP;
+    
+    /**
+     * the constructor
+     */
+    public function __construct(array $_options = array())
+    {
+        parent::__construct($_options);
+        
+        $this->_config['emailGID'] = sprintf("%u", crc32(Tinebase_Application::getInstance()->getApplicationByName('Tinebase')->getId()));
+    }
+    
+    /**
+     * returns array of ldap data
+     *
+     * @param  Tinebase_Model_FullUser  $_user
+     * @param  array                    $_ldapData
+     * @param  array                    $_ldapEntry
+     * @return array
+     */
+    protected function _user2Ldap(Tinebase_Model_FullUser $_user, array &$_ldapData, array &$_ldapEntry)
+    {
+        if (empty($_user->accountEmailAddress)) {
+            foreach ($this->_propertyMapping as $ldapKeyName) {
+                $_ldapData[$ldapKeyName] = array();
+            }
+            $_ldapData['accountStatus'] = array();
+            $_ldapData['mailHost']      = array();
+            
+            $_ldapData['objectclass'] = array_unique(array_diff($_ldapEntry['objectclass'], $this->_requiredObjectClass));
+            
+        } else {
+            parent::_user2Ldap($_user, $_ldapData, $_ldapEntry);
+        }
+        
+        #if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . '  $ldapData: ' . print_r($_ldapData, true));
+    }
+}
