0da91a55442209c13fab36b52181e757db97167d
Lars Kneschke <l.kneschke@metaways.de>
2010-11-10 13:42:47 +0000

    Refactor Tine 2.0 Emailhandling
    http://www.tine20.org/bugtracker/view.php?id=3302
    
    new user interface
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17000 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/User/Interface.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Tinebase
+ * @subpackage  User
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL3
+ * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id: Abstract.php 16650 2010-10-16 20:35:53Z l.kneschke@metaways.de $
+ * 
+ * @deprecated  user backends should be refactored
+ * @todo        add searchCount function
+ */
+
+/**
+ * abstract class for all user backends
+ *
+ * @package     Tinebase
+ * @subpackage  User
+ */
+ 
+interface Tinebase_User_Interface
+{
+    /**
+     * get list of users
+     *
+     * @param string $_filter
+     * @param string $_sort
+     * @param string $_dir
+     * @param int $_start
+     * @param int $_limit
+     * @param string $_accountClass the type of subclass for the Tinebase_Record_RecordSet to return
+     * @return Tinebase_Record_RecordSet with record class Tinebase_Model_User
+     */
+    public function getUsers($_filter = NULL, $_sort = NULL, $_dir = 'ASC', $_start = NULL, $_limit = NULL, $_accountClass = 'Tinebase_Model_User');
+    
+    /**
+     * get user by property
+     *
+     * @param   string  $_property
+     * @param   string  $_accountId
+     * @param   string  $_accountClass  type of model to return
+     * @return  Tinebase_Model_User user
+     */
+    public function getUserByProperty($_property, $_accountId, $_accountClass = 'Tinebase_Model_User');
+    
+    /**
+     * register plugins
+     * 
+     * @param Tinebase_User_Plugin_Interface $_plugin
+     */
+    public function registerPlugin(Tinebase_User_Plugin_Interface $_plugin);
+}
