cf922cd19a20869ecf1ef36f7bd7e2d7d81b5661
Philipp Schüle <p.schuele@metaways.de>
2010-11-12 22:11:02 +0000

    - removed some columns (hidden) - set class for contacts without email address - first steps of adding context menu actions 0003208: integrate addressbook grid/filter in email compose dialog http://www.tine20.org/bugtracker/view.php?id=3208
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17079 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/js/ContactGrid.js
+++ tine20/Felamimail/js/ContactGrid.js
@@ -20,6 +20,9 @@ Ext.ns('Tine.Felamimail');
  * 
  * <p>Contact Grid Panel</p>
  * 
+ * TODO         make ctx menu actions work
+ * TODO         add recipient on doubleclick
+ * 
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Philipp Schuele <p.schuele@metaways.de>
  * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
@@ -55,6 +58,86 @@ Tine.Felamimail.ContactGridPanel = Ext.extend(Tine.Addressbook.ContactGridPanel,
         });
         
         Tine.Felamimail.ContactGridPanel.superclass.initComponent.call(this);
+    },
+    
+    /**
+     * returns array with columns
+     * 
+     * @return {Array}
+     */
+    getColumns: function() {
+        var columns = Tine.Felamimail.ContactGridPanel.superclass.getColumns.call(this);
+        
+        // hide all columns except name/company/email/email_home (?)
+        Ext.each(columns, function(column) {
+            if (['n_fileas', 'org_name', 'email'].indexOf(column.dataIndex) === -1) {
+                column.hidden = true;
+            }
+        });
+        
+        return columns;
+    },
+    
+    /**
+     * Return CSS class to apply to rows depending upon email set or not
+     * 
+     * @param {Tine.Addressbook.Model.Contact} record
+     * @param {Integer} index
+     * @return {String}
+     */
+    getViewRowClass: function(record, index) {
+        var className = '';
+        
+        if ((! record.get('email') || record.get('email') == '') && (! record.get('email_home') || record.get('email_home') == '')) {
+            className = 'felamimail-no-email';
+        }
+        
+        return className;
+    },
+    
+    /**
+     * @private
+     */
+    initActions: function() {
+        this.actions_addAsTo = new Ext.Action({
+            requiredGrant: 'readGrant',
+            //actionUpdater: this.updatePhoneActions,
+            text: this.app.i18n._('Add as "To"'),
+            disabled: true,
+            iconCls: 'action_add',
+            handler: this.onAdd,
+            scope: this
+        });
+        
+//        //register actions in updater
+//        this.actionUpdater.addActions([
+//            this.actions_exportContact,
+//            this.actions_callContact,
+//            this.actions_composeEmail,
+//            this.actions_import
+//        ]);
+//        
+//        Tine.Addressbook.ContactGridPanel.superclass.initActions.call(this);
+    },
+    
+    onAdd: function() {
+        console.log();
+    },
+
+    /**
+     * returns rows context menu
+     * 
+     * @return {Ext.menu.Menu}
+     */
+    getContextMenu: function() {
+        if (! this.contextMenu) {
+            var items = [
+                // TODO add more actions
+                this.actions_addAsTo
+            ];
+            this.contextMenu = new Ext.menu.Menu({items: items});
+        }
+        return this.contextMenu;
     }
 });
 
