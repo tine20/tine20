40fab8d62ead6437f19f4f89e7ce74abff19e7f3
Philipp Schüle <p.schuele@metaways.de>
2010-11-12 22:07:51 +0000

    - pass subject to recipient picker window 0003208: integrate addressbook grid/filter in email compose dialog http://www.tine20.org/bugtracker/view.php?id=3208
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17076 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/js/MessageEditDialog.js
+++ tine20/Felamimail/js/MessageEditDialog.js
@@ -500,7 +500,9 @@ Ext.namespace('Tine.Felamimail');
      */
     onSearchContacts: function() {
         Tine.Felamimail.RecipientPickerDialog.openWindow({
-            record: new this.recordClass({}, Ext.id()),
+            record: new this.recordClass({
+                subject: this.record.get('subject')
+            }, Ext.id()),
             listeners: {
                 scope: this,
                 'update': function(record) {
