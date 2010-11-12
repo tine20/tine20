ae2177ad7914c4a9d58f62d8b50db7fa986d5101
Cornelius Weiß <c.weiss@metaways.de>
2010-11-12 10:34:24 +0000

    ASSIGNED - # 3318: use correct default grants for new calendars! http://www.tine20.org/bugtracker/view.php?id=3318
    -> new event
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17062 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/Event/Container/BeforeCreate.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Tinebase
+ * @subpackage  Event
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * event fired before a new container is created
+ *
+ * @package     Tinebase
+ * @subpackage  Container
+ */
+class Tinebase_Event_Container_BeforeCreate extends Tinebase_Event_Abstract
+{
+    /**
+     * @var String
+     */
+    public $accountId;
+    
+    /**
+     * @var Tinebase_Model_Container
+     */
+    public $container;
+    
+    /**
+     * @var Tinebase_Model_Grants
+     */
+    public $grants;
+}
