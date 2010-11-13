c0e0c7430de8e669c79265a9798bc4056228eaec
Cornelius Weiß <c.weiss@metaways.de>
2010-11-13 08:37:26 +0000

    new generic date Exception
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17085 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/Exception/Date.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Tinebase
+ * @subpackage  Exception
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ *
+ */
+
+/**
+ * generic date exception
+ * 
+ * @package     Tinebase
+ * @subpackage  Exception
+ */
+class Tinebase_Exception_Date extends Tinebase_Exception
+{
+}
