4a0947b664ad2fe5c0533be87919ec0486cb5fc8
Philipp Schüle <p.schuele@metaways.de>
2010-11-17 11:45:45 +0000

    implements Setup_Server_Interface, too
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17132 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Setup/Server/Json.php
+++ tine20/Setup/Server/Json.php
@@ -16,7 +16,7 @@
  * @package     Setup
  * @subpackage  Server
  */
-class Setup_Server_Json extends Tinebase_Server_Json
+class Setup_Server_Json extends Tinebase_Server_Json implements Setup_Server_Interface
 {
     /**
      * handler for JSON api requests
