77f7d39a6f664ab153e2395c9fed08eedc27da27
Cornelius Weiß <c.weiss@metaways.de>
2010-11-19 13:31:11 +0000

    ASSIGNED - # 2308: add printing for calendar http://www.tine20.org/bugtracker/view.php?id=2308
    -- kill syntax error
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17203 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Calendar/js/Printer/MonthView.js
+++ tine20/Calendar/js/Printer/MonthView.js
@@ -1,3 +1,3 @@
 Tine.Calendar.Printer.MonthViewRenderer = Ext.extend(Tine.Calendar.Printer.BaseRenderer, {
-    return view.numOfDays === 1 ? days[0] : String.format('<table>{0}</table>', this.generateCalRows(days, view.numOfDays < 9 ? 2 : 7));
+    
 });
