e66f808a32ed08565f45cefaba680495bf6a0a02
Philipp Schüle <p.schuele@metaways.de>
2010-11-23 15:29:38 +0000

    - added change credentials event 0002944: Change email account password on user password change http://www.tine20.org/bugtracker/view.php?id=2944
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17257 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/Event/User/ChangeCredentialCache.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Tinebase
+ * @subpackage  Event
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * event class for change credential cache
+ *
+ * @package     Tinebase
+ * @subpackage  Event
+ */
+class Tinebase_Event_User_ChangeCredentialCache extends Tinebase_Event_Abstract
+{
+    /**
+     * the old credential cache
+     *
+     * @var Tinebase_Model_CredentialCache
+     */
+    public $oldCredentialCache;
+    
+    /**
+     * the constructor
+     * 
+     * @param Tinebase_Model_CredentialCache $_credentialCache
+     */
+    public function __construct(Tinebase_Model_CredentialCache $_oldCredentialCache = NULL)
+    {
+        if ($_oldCredentialCache !== NULL) {
+            $this->oldCredentialCache = $_oldCredentialCache;
+        }
+    }
+}
