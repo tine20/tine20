a2b500e1b9e165fa456aae1ce583fae26771ba00
Cornelius Weiß <c.weiss@metaways.de>
2010-11-23 14:01:55 +0000

    ASSIGNED - # 2116: Display official holidays ("Feiertage") in calendar http://www.tine20.org/bugtracker/view.php?id=2116
    ical import tests
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17255 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/tine20/Calendar/Import/ICalTests.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Felamimail
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ * 
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Calendar_ICalTests::main');
+}
+
+/**
+ * Test class for Calendar_ICalTests
+ */
+class Calendar_ICalTests extends Calendar_TestCase
+{
+    public function testImportSimpleFromString()
+    {
+        $importer = new Calendar_Import_Ical(array(
+            'importContainerId' => $this->_testCalendar->getId(),
+        ));
+
+        $icalData = file_get_contents(dirname(__FILE__) . '/files/simple.ics');
+        $importer->importData($icalData);
+        
+        $events = Calendar_Controller_Event::getInstance()->search(new Calendar_Model_EventFilter(array(
+            array('field' => 'container_id', 'operator' => 'equals', 'value' => $this->_testCalendar->getId())
+        )), NULL);
+        
+        $this->assertEquals(6, $events->count(), 'events was not imported');
+        
+        $startbucks = $events->find('uid', '3632597');
+        $this->assertType('Calendar_Model_Event', $startbucks);
+        $this->assertEquals('2008-11-05 15:00:00', $startbucks->dtstart->format(Tinebase_Record_Abstract::ISO8601LONG));
+    }
+    
+    public function testImportSimpleFromFile()
+    {
+        $importer = new Calendar_Import_Ical(array(
+            'importContainerId' => $this->_testCalendar->getId(),
+        ));
+        
+        $importer->importFile(dirname(__FILE__) . '/files/simple.ics');
+        $events = Calendar_Controller_Event::getInstance()->search(new Calendar_Model_EventFilter(array(
+            array('field' => 'container_id', 'operator' => 'equals', 'value' => $this->_testCalendar->getId())
+        )), NULL);
+        
+        $this->assertEquals(6, $events->count(), 'events where not imported');
+        
+        $startbucks = $events->find('uid', '3632597');
+        $this->assertType('Calendar_Model_Event', $startbucks);
+        $this->assertEquals('2008-11-05 15:00:00', $startbucks->dtstart->format(Tinebase_Record_Abstract::ISO8601LONG));
+    }
+    
+    public function testImportRecur()
+    {
+        $importer = new Calendar_Import_Ical(array(
+            'importContainerId' => $this->_testCalendar->getId(),
+        ));
+        
+        $importer->importFile(dirname(__FILE__) . '/files/XMAS_DE.ics');
+        
+        $events = Calendar_Controller_Event::getInstance()->search(new Calendar_Model_EventFilter(array(
+            array('field' => 'container_id', 'operator' => 'equals', 'value' => $this->_testCalendar->getId()),
+            array('field' => 'period', 'operator' => 'within', 'value' => array(
+                'from'  => '2015-12-23 22:00:00',
+                'until' => '2015-12-26 22:00:00'
+            )),
+        )), NULL);
+        
+        $this->assertEquals(3, $events->count(), 'events where not imported');
+    }
+}
