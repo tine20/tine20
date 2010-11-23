c9143fe5d5a0d57f1e01d80f1d8773bc6d4d84bf
Lars Kneschke <l.kneschke@metaways.de>
2010-11-23 18:53:23 +0000

    moved debug statements to Tinebase_Auth which makes Tinebase_Auth_Ldap::authenticate useless
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17277 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Auth/Ldap.php
+++ tine20/Tinebase/Auth/Ldap.php
@@ -52,28 +52,6 @@ class Tinebase_Auth_Ldap extends Zend_Auth_Adapter_Ldap implements Tinebase_Auth
     }
     
     /**
-     * authenticate() - defined by Zend_Auth_Adapter_Interface.
-     *
-     * @throws Zend_Auth_Adapter_Exception if answering the authentication query is impossible
-     * @return Zend_Auth_Result
-     */
-    public function authenticate()
-    {
-        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' Trying to authenticate '. $this->getUsername());
-        
-        $result = parent::authenticate();
-        
-        if($result->isValid()) {
-            // username and password are correct, let's do some additional tests            
-            Tinebase_Core::getLogger()->info(__METHOD__ . '::' . __LINE__ . ' Authentication of '. $this->getUsername() . ' succeeded');
-        } else {
-            Tinebase_Core::getLogger()->info(__METHOD__ . '::' . __LINE__ . ' Authentication of '. $this->getUsername() . ' failed');
-        }
-        
-        return $result;
-    }
-    
-    /**
      * set loginname
      *
      * @param string $_identity
