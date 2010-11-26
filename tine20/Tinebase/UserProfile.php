c02b3fa6638d21a2ba360f96f575f86258639361
Lars Kneschke <l.kneschke@metaways.de>
2010-11-26 15:43:18 +0000

    PHPDOC updates
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17383 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/UserProfile.php
+++ tine20/Tinebase/UserProfile.php
@@ -6,9 +6,14 @@
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Cornelius Weiss <c.weiss@metaways.de>
  * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
- * @version     $Id $
+ * @version     $Id$
  */
 
+/**
+ * userprofile class
+ * 
+ * @package     ActiveSync
+ */
 class Tinebase_UserProfile
 {
     /**
