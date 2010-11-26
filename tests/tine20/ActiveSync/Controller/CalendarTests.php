587a8e921f8b0e2b962dac6677296903c9b28bcb
Lars Kneschke <l.kneschke@metaways.de>
2010-11-26 08:42:00 +0000

    added test to test converting an xml event to Calendar_Model_Event
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17358 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/tine20/ActiveSync/Controller/CalendarTests.php
+++ tests/tine20/ActiveSync/Controller/CalendarTests.php
@@ -37,6 +37,13 @@ class ActiveSync_Controller_CalendarTests extends PHPUnit_Framework_TestCase
     protected $objects = array();
     
     /**
+     * test domdocument with an calendar event
+     * 
+     * @var DOMDocument
+     */
+    protected $testDOM;
+    
+    /**
      * Runs the test methods of this class.
      *
      * @access public
@@ -53,6 +60,14 @@ class ActiveSync_Controller_CalendarTests extends PHPUnit_Framework_TestCase
     {   	
     	$appName = 'Calendar';
     	
+    	############# TEST DOMDOCUMENT #############
+    	$this->testDOM = new DOMDocument();
+        $this->testDOM->formatOutput = false;
+        $this->testDOM->encoding     = 'utf-8';
+        $this->testDOM->loadXML('<!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/"><Sync xmlns="uri:AirSync" xmlns:Calendar="uri:Calendar"><Collections><Collection><Class>Calendar</Class><SyncKey>9</SyncKey><CollectionId>41</CollectionId><DeletesAsMoves/><GetChanges/><WindowSize>50</WindowSize><Options><FilterType>5</FilterType></Options><Commands><Change><ServerId>6de7cb687964dc6eea109cd81750177979362217</ServerId><ApplicationData><Calendar:Timezone>xP///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAoAAAAFAAMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMAAAAFAAIAAAAAAAAAxP///w==</Calendar:Timezone><Calendar:AllDayEvent>0</Calendar:AllDayEvent><Calendar:BusyStatus>2</Calendar:BusyStatus><Calendar:DtStamp>20101125T150537Z</Calendar:DtStamp><Calendar:EndTime>20101123T160000Z</Calendar:EndTime><Calendar:Sensitivity>0</Calendar:Sensitivity><Calendar:Subject>Repeat</Calendar:Subject><Calendar:StartTime>20101123T130000Z</Calendar:StartTime><Calendar:UID>6de7cb687964dc6eea109cd81750177979362217</Calendar:UID><Calendar:MeetingStatus>1</Calendar:MeetingStatus><Calendar:Attendees><Calendar:Attendee><Calendar:Name>Lars Kneschke</Calendar:Name><Calendar:Email>lars@kneschke.de</Calendar:Email></Calendar:Attendee></Calendar:Attendees><Calendar:Recurrence><Calendar:Type>0</Calendar:Type><Calendar:Interval>1</Calendar:Interval><Calendar:Until>20101128T225959Z</Calendar:Until></Calendar:Recurrence><Calendar:Exceptions><Calendar:Exception><Calendar:Deleted>0</Calendar:Deleted><Calendar:ExceptionStartTime>20101125T130000Z</Calendar:ExceptionStartTime><Calendar:StartTime>20101125T140000Z</Calendar:StartTime><Calendar:EndTime>20101125T170000Z</Calendar:EndTime><Calendar:Subject>Repeat mal anders</Calendar:Subject><Calendar:BusyStatus>2</Calendar:BusyStatus><Calendar:AllDayEvent>0</Calendar:AllDayEvent></Calendar:Exception><Calendar:Exception><Calendar:Deleted>1</Calendar:Deleted><Calendar:ExceptionStartTime>20101124T130000Z</Calendar:ExceptionStartTime></Calendar:Exception></Calendar:Exceptions></ApplicationData></Change></Commands></Collection></Collections></Sync>');
+        //echo $this->testDOM->saveXML();
+    	
+        
     	############# TEST USER ##########
     	$user = new Tinebase_Model_FullUser(array(
             'accountId'             => 10,
@@ -167,11 +182,8 @@ class ActiveSync_Controller_CalendarTests extends PHPUnit_Framework_TestCase
         $exceptionInstance2 = Calendar_Controller_Event::getInstance()->createRecurException($exceptionInstance2);
         
         // reread event from database again
-        #$eventDaily = Calendar_Controller_Event::getInstance()->get($eventDaily);        
+        $eventDaily = Calendar_Controller_Event::getInstance()->get($eventDaily);        
         #var_dump($eventDaily->toArray());
-        
-        $exceptions = Calendar_Controller_Event::getInstance()->getRecurExceptions($eventDaily, TRUE);
-        #var_dump($exceptions->toArray());
         
         $this->objects['eventDaily'] = $eventDaily;
         
@@ -318,9 +330,6 @@ class ActiveSync_Controller_CalendarTests extends PHPUnit_Framework_TestCase
      */
     public function testAppendXmlDailyEvent()
     {
-        #$eventDaily = Calendar_Controller_Event::getInstance()->get($this->objects['eventDaily']->getId());
-        #var_dump($eventDaily->toArray());
-        
         $imp                   = new DOMImplementation();
         
         $dtd                   = $imp->createDocumentType('AirSync', "-//AIRSYNC//DTD AirSync//EN", "http://www.microsoft.com/");
@@ -347,6 +356,25 @@ class ActiveSync_Controller_CalendarTests extends PHPUnit_Framework_TestCase
         $untilTime = Calendar_Model_Rrule::getRruleFromString($this->objects['eventDaily']->rrule)->until->format("Ymd\THis") . 'Z';
         $this->assertEquals($untilTime, @$testDom->getElementsByTagNameNS('uri:Calendar', 'Until')->item(0)->nodeValue, $testDom->saveXML());
         
+    }
+    
+    /**
+     * test xml generation for IPhone
+     * 
+     * birthday must have 12 hours added
+     */
+    public function testConvertToTine20Model()
+    {
+        $xml = simplexml_import_dom($this->testDOM);
+        
+        $controller = new ActiveSync_Controller_Calendar($this->objects['deviceIPhone'], new Tinebase_DateTime());     
+        
+        $event = $controller->toTineModel($xml->Collections->Collection->Commands->Change[0]->ApplicationData);
+        
+        //var_dump($event->toArray());
+        
+        $this->assertEquals('Repeat',        $event->summary);
+        $this->assertEquals('Europe/Berlin', $event->originator_tz);
     }
     
     /**
