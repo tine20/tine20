69fb0f5093fda3ec486a84bdfa12253223bcfab9
Lars Kneschke <l.kneschke@metaways.de>
2010-11-29 16:57:30 +0000

    abstract class for tasks tests
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17427 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/tine20/Tasks/TestCase.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Tasks
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2010-2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+/**
+ * abstract class for tasks tests
+ * 
+ * @package     Tasks
+ */
+abstract class Tasks_TestCase extends PHPUnit_Framework_TestCase
+{
+    /**
+     * 
+     * @return Tasks_Model_Task
+     */
+    public static function getTestRecord()
+    {
+        $task = new Tasks_Model_Task(array(
+            // tine record fields
+            'container_id'         => NULL,
+            'created_by'           => 6,
+            'creation_time'        => Tinebase_DateTime::now(),
+            'is_deleted'           => 0,
+            'deleted_time'         => NULL,
+            'deleted_by'           => NULL,
+            // task only fields
+            'percent'              => 70,
+            'completed'            => NULL,
+            'due'                  => Tinebase_DateTime::now()->addMonth(1),
+            // ical common fields
+            //'class_id'             => 2,
+            'description'          => str_pad('', 1000, '.'),
+            'geo'                  => 0.2345,
+            'location'             => 'here and there',
+            'organizer'            => 4,
+            'priority'             => 2,
+            //'status_id'            => 2,
+            'summary'              => 'our first test task',
+            'url'                  => 'http://www.testtask.com',
+        ),true, false);
+        
+        $task->convertDates = true;
+        
+        return $task;
+    }
+}
