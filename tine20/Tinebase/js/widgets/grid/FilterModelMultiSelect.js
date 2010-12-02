78239a102c998c8fd7270527588cf42fff192690
Philipp Schüle <p.schuele@metaways.de>
2010-12-02 15:50:39 +0000

    added picker grid to layer combo
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17559 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/widgets/grid/FilterModelMultiSelect.js
+++ tine20/Tinebase/js/widgets/grid/FilterModelMultiSelect.js
@@ -59,10 +59,36 @@ Tine.widgets.grid.FilterModelMultiSelect = Ext.extend(Tine.widgets.grid.FilterMo
     valueStore: null,
     
     /**
+     * @cfg recordClass
+     * @type Tine.Tinebase.data.Record
+     */
+    recordClass: null,
+    
+    /**
+     * xtype for the value select field
+     * 
+     * @cfg valueXtype
+     * @type String
+     */
+    valueXtype: 'checkbox',
+    
+    /**
+     * @cfg labelField
+     * @type String
+     */
+    labelField: 'name',
+    
+    /**
+     * @cfg layerHeight
+     * @type Integer
+     */
+    layerHeight: null,
+    
+    /**
      * @private
      */
     initComponent: function() {
-        this.operators = ['in', 'notin'];
+        this.operators = this.operators || ['in', 'notin'];
         
         Tine.widgets.grid.FilterModelMultiSelect.superclass.initComponent.call(this);
     },
@@ -81,7 +107,11 @@ Tine.widgets.grid.FilterModelMultiSelect = Ext.extend(Tine.widgets.grid.FilterMo
             id: 'tw-ftb-frow-valuefield-' + filter.id,
             value: filter.data.value ? filter.data.value : this.defaultValue,
             renderTo: el,
-            valueStore: this.valueStore 
+            valueStore: this.valueStore,
+            xtype: this.valueXtype,
+            recordClass: this.recordClass,
+            labelField: this.labelField,
+            layerHeight: this.layerHeight
         });
         value.on('specialkey', function(field, e){
              if(e.getKey() == e.ENTER){
@@ -103,8 +133,6 @@ Tine.widgets.grid.FilterToolbar.FILTERS['tinebase.multiselect'] = Tine.widgets.g
  * 
  * @author      Philipp Schuele <p.schuele@metaways.de>
  * @version     $Id: LeadStateFilterModel.js 14369 2010-05-14 13:53:58Z c.weiss@metaways.de $
- * 
- * TODO         allow grid panel as items (make this configurable)
  */
 Tine.widgets.grid.FilterModelMultiSelectValueField = Ext.extend(Ext.ux.form.LayerCombo, {
     hideButtons: false,
@@ -114,6 +142,22 @@ Tine.widgets.grid.FilterModelMultiSelectValueField = Ext.extend(Ext.ux.form.Laye
     },
     labelField: 'name',
     xtype: 'checkbox',
+    recordClass: null,
+    valueStore: null,
+    
+    /**
+     * init
+     */
+    initComponent: function() {
+        this.on('beforecollapse', this.onBeforeCollapse, this);
+        if (! this.store) {
+            this.store = new Ext.data.SimpleStore({
+                fields: this.recordClass
+            });
+        }
+        
+        Tine.widgets.grid.FilterModelMultiSelectValueField.superclass.initComponent.call(this);
+    },
     
     /**
      * get form values
@@ -121,16 +165,22 @@ Tine.widgets.grid.FilterModelMultiSelectValueField = Ext.extend(Ext.ux.form.Laye
      * @return {Array}
      */
     getFormValue: function() {
-        var ids = [];
+        var values = [];
 
-        var formValues = this.getInnerForm().getForm().getValues();
-        for (var id in formValues) {
-            if (formValues[id] === 'on' && this.valueStore.getById(id)) {
-                ids.push(id);
+        if (this.xtype == 'checkbox') {
+            var formValues = this.getInnerForm().getForm().getValues();
+            for (var id in formValues) {
+                if (formValues[id] === 'on' && this.valueStore.getById(id)) {
+                    values.push(id);
+                }
             }
+        } else {
+            this.store.each(function(record) {
+                values.push(record.data);
+            }, this);            
         }
         
-        return ids;
+        return values;
     },
     
     /**
@@ -140,18 +190,91 @@ Tine.widgets.grid.FilterModelMultiSelectValueField = Ext.extend(Ext.ux.form.Laye
      */
     getItems: function() {
         var items = [];
-        
-        this.valueStore.each(function(record) {
-            items.push({
-                xtype: this.xtype,
-                boxLabel: record.get(this.labelField),
-                name: record.get('id')
-                //icon: record.get('status_icon'),
+
+        if (this.xtype == 'wdgt.pickergrid') {
+//            var selectionCombo = this.containerSelectionCombo = new Tine.widgets.container.selectionComboBox({
+//                allowNodeSelect: true,
+//                allowBlank: true,
+//                blurOnSelect: true,
+//                recordClass: this.recordClass,
+//                appName: this.recordClass.getMeta('appName'),
+//                containerName: this.containerName,
+//                containersName: this.containersName,
+//                listeners: {
+//                    scope: this, 
+//                    select: this.onContainerSelect
+//                }
+//            });
+                    
+            this.pickerGridPanel = new Tine.widgets.grid.PickerGridPanel({
+                height: this.layerHeight || 'auto',
+                recordClass: this.recordClass,
+                store: this.store,
+                autoExpandColumn: this.labelField,
+                getColumnModel: this.getColumnModel.createDelegate(this),
+                initActionsAndToolbars: function() {
+                    Tine.widgets.grid.PickerGridPanel.prototype.initActionsAndToolbars.call(this);
+                    
+//                    this.tbar = new Ext.Toolbar({
+//                        layout: 'fit',
+//                        items: [
+//                            this.selectionCombo
+//                        ]
+//                    });
+                }
             });
-        }, this);
+            
+            items.push(this.pickerGridPanel);
+            
+        } else if (this.xtype == 'checkbox') {
+            this.valueStore.each(function(record) {
+                items.push({
+                    xtype: this.xtype,
+                    boxLabel: record.get(this.labelField),
+                    name: record.get('id')
+                    //icon: record.get('status_icon'),
+                });
+            }, this);
+        }
         
         return items;
     },
+    
+    /**
+     * @return Ext.grid.ColumnModel
+     */
+    getColumnModel: function() {
+        return new Ext.grid.ColumnModel({
+            defaults: {
+                sortable: false
+            },
+            columns:  [
+                {id: this.labelField, header: String.format(_('Selected  {0}'), this.recordClass.getMeta('recordsName')), dataIndex: this.labelField}
+            ]
+        });
+    },
+    
+    /**
+     * record select
+     * 
+     * @param {String} field
+     * @param {Object} recordData
+     */
+    onRecordSelect: function(field, recordData) {
+        var existingRecord = this.store.getById(recordData.id);
+        if (! existingRecord) {
+            var data = (recordData.data) ? recordData.data : recordData;
+            this.store.add(new Tine.Tinebase.Model.Container(data));
+            
+        } else {
+            var idx = this.store.indexOf(existingRecord);
+            var row = this.pickerGridPanel.getView().getRow(idx);
+            Ext.fly(row).highlight();
+        }
+        
+        this.containerSelectionCombo.clearValue();
+    },
+    
     
     /**
      * @param {String} value
@@ -163,17 +286,31 @@ Tine.widgets.grid.FilterModelMultiSelectValueField = Ext.extend(Ext.ux.form.Laye
         var recordText = [];
         this.currentValue = [];
         
-        this.valueStore.each(function(record) {
-            var id = record.get('id');
-            var name = record.get(this.labelField);
-            Ext.each(value, function(valueId) {
-                // NOTE: no type match id's might be int or string and should match anyway!
-                if (valueId == id) {
-                    recordText.push(name);
-                    this.currentValue.push(id);
-                }
+        if (this.xtype == 'checkbox') {
+            this.valueStore.each(function(record) {
+                var id = record.get('id');
+                var name = record.get(this.labelField);
+                Ext.each(value, function(valueId) {
+                    // NOTE: no type match id's might be int or string and should match anyway!
+                    if (valueId == id) {
+                        recordText.push(name);
+                        this.currentValue.push(id);
+                    }
+                }, this);
             }, this);
-        }, this);
+        } else {
+            this.store.removeAll();
+            var record;
+            console.log(value);
+            for (var i=0; i < value.length; i++) {
+                record = this.valueStore.getById(value[i]);
+                if (record) {
+                    this.currentValue.push(record.id);
+                    this.store.add(record);
+                    recordText.push(record.get(this.labelField));
+                }
+            }
+        }
         
         this.setRawValue(recordText.join(', '));
         
@@ -181,13 +318,22 @@ Tine.widgets.grid.FilterModelMultiSelectValueField = Ext.extend(Ext.ux.form.Laye
     },
     
     /**
-     * sets values to innerForm
+     * cancel collapse if ctx menu or record selection is shown
      * 
-     * TODO do we need this?
+     * @return Boolean
+     */
+    onBeforeCollapse: function() {
+        if (this.pickerGridPanel) {
+            var contextMenuVisible = this.pickerGridPanel.contextMenu && ! this.pickerGridPanel.contextMenu.hidden;
+            return ! (contextMenuVisible);
+        } else {
+            return true;
+        }
+    },
+    
+    /**
+     * sets values to innerForm
      */
     setFormValue: function(value) {
-        this.getInnerForm().getForm().items.each(function(item) {
-            item.setValue(value.indexOf(item.name) >= 0 ? 'on' : 'off');
-        }, this);
     }
 });
