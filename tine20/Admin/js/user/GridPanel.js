8ba0bdf29c3043d8e14a01fafbda66bb9bd123c4
Cornelius Weiß <c.weiss@metaways.de>
2010-12-03 16:09:30 +0000

    stay within the grid / selection
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17605 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Admin/js/user/GridPanel.js
+++ tine20/Admin/js/user/GridPanel.js
@@ -191,7 +191,7 @@ Tine.Admin.user.GridPanel = Ext.extend(Tine.widgets.grid.GridPanel, {
                 if(_success === true) {
                     var result = Ext.util.JSON.decode(_response.responseText);
                     if(result.success === true) {
-                        this.grid.getStore().reload();
+                        this.loadData(true, true, true);
                     }
                 }
             }
