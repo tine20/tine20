c64ea315bcf496bba1f2cd9c98f7f6ae9267da31
Philipp Schüle <p.schuele@metaways.de>
2010-12-03 12:38:17 +0000

    - fmail specific stuff has been moved here from generic layer combo - use config object for the layer combo cfg/overwritten functions 0003376: use favorites concept in felamimail http://www.tine20.org/bugtracker/view.php?id=3376
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17585 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/js/FolderFilterModel.js
+++ tine20/Felamimail/js/FolderFilterModel.js
@@ -21,25 +21,39 @@ Tine.Felamimail.FolderFilterModel = Ext.extend(Tine.widgets.grid.FilterModelMult
 
     /**
      * @cfg 
+     * TODO make notin work
      */
-    operators: ['in'],
+    operators: ['in'/*, 'notin' */],
     field: 'folder_id',
-    valueXtype: 'wdgt.pickergrid',
-    labelField: 'globalname',
-    layerHeight: 200,
     
     /**
      * @private
      */
     initComponent: function() {
         this.label = this.app.i18n._('Folder');
-        this.valueStore = this.app.getFolderStore();
-        this.recordClass = Tine.Felamimail.Model.Folder;
-        this.selectionCombo = new Tine.Felamimail.FolderSelectTriggerField({
-            emptyValue: this.app.i18n._('Select Folder'),
-            name: 'folder',
-            allAccounts: true
-        });
+        
+        this.multiselectFieldConfig = {
+            xtype: 'wdgt.pickergrid',
+            labelField: 'globalname',
+            layerHeight: 200,
+            selectionWidget: new Tine.Felamimail.FolderSelectTriggerField({
+                emptyValue: this.app.i18n._('Select Folder'),
+                name: 'folder',
+                allAccounts: true
+            }),
+            recordClass: Tine.Felamimail.Model.Folder,
+            valueStore: this.app.getFolderStore(),
+            
+            /**
+             * functions
+             */
+            initSelectionWidget: function() {
+                this.selectionWidget.onSelectFolder = this.addRecord.createDelegate(this);
+            },
+            isSelectionVisible: function() {
+                return this.selectionWidget.selectPanel && ! this.selectionWidget.selectPanel.isDestroyed        
+            }
+        };
 
         Tine.Felamimail.FolderFilterModel.superclass.initComponent.call(this);
     }
