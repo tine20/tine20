97fbfc215a2d2238bc248c3bf1f999f6e687cebf
Lars Kneschke <l.kneschke@metaways.de>
2010-12-04 23:15:42 +0000

    added dovecot tests
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17633 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/tine20/Tinebase/User/EmailUser/Imap/DovecotTest.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Tinebase
+ * @subpackage  User
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Tinebase_User_EmailUser_Imap_DovecotTest::main');
+}
+
+/**
+ * Test class for Tinebase_DovecotTest
+ */
+class Tinebase_User_EmailUser_Imap_DovecotTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * email user backend
+     *
+     * @var Tinebase_EmailUser_Abstract
+     */
+    protected $_backend = NULL;
+        
+    /**
+     * @var array test objects
+     */
+    protected $_objects = array();
+    
+    protected $_config;
+
+    /**
+     * Runs the test methods of this class.
+     *
+     * @access public
+     * @static
+     */
+    public static function main()
+    {
+        $suite  = new PHPUnit_Framework_TestSuite('Tinebase_User_EmailUser_Imap_DovecotTest');
+        PHPUnit_TextUI_TestRunner::run($suite);
+    }
+
+    /**
+     * Sets up the fixture.
+     * This method is called before a test is executed.
+     *
+     * @access protected
+     */
+    protected function setUp()
+    {
+        $this->_config = Tinebase_Config::getInstance()->getConfigAsArray(Tinebase_Model_Config::IMAP);
+        if (!isset($this->_config['backend']) || !(ucfirst($this->_config['backend']) == Tinebase_EmailUser::DOVECOT_IMAP) || $this->_config['active'] != true) {
+            $this->markTestSkipped('Dovecot MySQL backend not configured or not enabled');
+        }
+        
+        $this->_backend = Tinebase_EmailUser::getInstance(Tinebase_Model_Config::IMAP);
+        
+        $personas = Zend_Registry::get('personas');
+        $this->_objects['user'] = clone $personas['jsmith'];
+
+        $this->_objects['addedUsers'] = array();
+    }
+
+    /**
+     * Tears down the fixture
+     * This method is called after a test is executed.
+     *
+     * @access protected
+     */
+    protected function tearDown()
+    {
+        // delete email account
+        foreach ($this->_objects['addedUsers'] as $user) {
+            $this->_backend->inspectDeleteUser($user->getId());
+        }
+    }
+    
+    /**
+     * try to add an email account
+     */
+    public function testAddEmailAccount()
+    {
+        $emailUser = clone $this->_objects['user'];
+        $emailUser->imapUser = new Tinebase_Model_EmailUser(array(
+            'emailPassword' => Tinebase_Record_Abstract::generateUID(),
+            'emailUID'      => '1000',
+            'emailGID'      => '1000'
+        ));
+        
+        $this->_backend->inspectAddUser($this->_objects['user'], $emailUser);
+        $this->_objects['addedUsers']['emailUser'] = $this->_objects['user'];
+        
+        //var_dump($this->_objects['user']->imapUser->toArray());
+        
+        $this->assertEquals(array(
+            'emailUserId'     => $this->_objects['user']->getId(),
+            'emailUsername'   => $this->_objects['user']->imapUser->emailUsername,
+            'emailUID'        => !empty($this->_config['dovecot']['uid']) ? $this->_config['dovecot']['uid'] : '1000',
+            'emailGID'        => !empty($this->_config['dovecot']['gid']) ? $this->_config['dovecot']['gid'] : '1000',
+            'emailLastLogin'  => null,
+            'emailMailQuota'  => 500,
+            'emailMailSize'   => 0,
+            'emailSieveQuota' => 0,
+            'emailSieveSize'  => 0
+        ), $this->_objects['user']->imapUser->toArray());
+        
+        return $this->_objects['user'];
+    }
+    
+    /**
+     * try to update an email account
+     */
+    public function testUpdateAccount()
+    {
+        // add smtp user
+        $user = $this->testAddEmailAccount();
+        
+        // update user
+        $user->imapUser->emailMailQuota = 600;
+        
+        $this->_backend->inspectUpdateUser($this->_objects['user'], $user);
+        
+        //print_r($user->toArray());
+        
+        $this->assertEquals(array(
+            'emailUserId'      => $this->_objects['user']->getId(),
+            'emailUsername'    => $this->_objects['user']->imapUser->emailUsername,
+            'emailUID'         => !empty($this->_config['dovecot']['uid']) ? $this->_config['dovecot']['uid'] : '1000',
+            'emailGID'         => !empty($this->_config['dovecot']['gid']) ? $this->_config['dovecot']['gid'] : '1000',
+            'emailLastLogin'   => null,
+            'emailMailQuota'   => 600,
+            'emailMailSize'    => 0,
+            'emailSieveQuota'  => 0,
+            'emailSieveSize'   => 0,
+            'emailAliases'     => null,
+            'emailForwards'    => null,
+            'emailForwardOnly' => null,
+            'emailAddress'     => null
+        ), $this->_objects['user']->imapUser->toArray());
+    }
+    
+    /**
+     * try to update an email account
+     */
+    public function testSetPassword()
+    {
+        // add smtp user
+        $user = $this->testAddEmailAccount();
+        
+        $this->_backend->inspectSetPassword($this->_objects['user']->getId(), Tinebase_Record_Abstract::generateUID());
+        
+        //$this->assertEquals(md5('password'), $updatedUser->emailPassword);
+    }
+}	
