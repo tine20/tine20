cc51a6c286aa0271b0d5745f86876399478efa59
Lars Kneschke <l.kneschke@metaways.de>
2010-12-04 22:14:04 +0000

    restructured tests
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17632 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/tine20/Tinebase/User/AllTests.php
+++ tests/tine20/Tinebase/User/AllTests.php
@@ -16,12 +16,14 @@ require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'TestHe
 if (! defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'Tinebase_User_AllTests::main');
 }
+
 class Tinebase_User_AllTests
 {
     public static function main ()
     {
         PHPUnit_TextUI_TestRunner::run(self::suite());
     }
+    
     public static function suite ()
     {
         $suite = new PHPUnit_Framework_TestSuite('Tine 2.0 Tinebase All User Tests');
@@ -33,22 +35,11 @@ class Tinebase_User_AllTests
         $suite->addTestSuite('Tinebase_User_ModelTest');
         $suite->addTestSuite('Tinebase_User_AbstractTest');
         
-#        $imapConfig = Tinebase_Config::getInstance()->getConfigAsArray(Tinebase_Model_Config::IMAP);
-#        if (isset($imapConfig['backend'])) {
-#            switch (ucfirst($imapConfig['backend'])) {
-#                case Tinebase_EmailUser::DBMAIL:
-#                   $suite->addTestSuite('Tinebase_User_EmailUser_DbmailTest');
-#                    break;
-#                case Tinebase_EmailUser::LDAP_IMAP:
-#                   $suite->addTestSuite('Tinebase_User_EmailUser_LdapImapTest');
-#                   break;
-#           }
-#       }
-
-        $suite->addTestSuite('Tinebase_User_EmailUser_PostfixTest');
+        $suite->addTestSuite('Tinebase_User_EmailUser_AllTests');
         return $suite;
     }
 }
+
 if (PHPUnit_MAIN_METHOD == 'Tinebase_User_AllTests::main') {
     Tinebase_User_AllTests::main();
 }
---
+++ tests/tine20/Tinebase/User/EmailUser/AllTests.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Tinebase
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2010-2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id: AllTests.php 17625 2010-12-04 18:26:39Z l.kneschke@metaways.de $
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+if (! defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Tinebase_User_EmailUser_AllTests::main');
+}
+
+class Tinebase_User_EmailUser_AllTests
+{
+    public static function main ()
+    {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+    
+    public static function suite ()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('Tine 2.0 Tinebase All EmailUser Tests');
+#        $imapConfig = Tinebase_Config::getInstance()->getConfigAsArray(Tinebase_Model_Config::IMAP);
+#        if (isset($imapConfig['backend'])) {
+#            switch (ucfirst($imapConfig['backend'])) {
+#                case Tinebase_EmailUser::DBMAIL:
+#                   $suite->addTestSuite('Tinebase_User_EmailUser_DbmailTest');
+#                    break;
+#                case Tinebase_EmailUser::LDAP_IMAP:
+#                   $suite->addTestSuite('Tinebase_User_EmailUser_LdapImapTest');
+#                   break;
+#           }
+#       }
+
+        $suite->addTestSuite('Tinebase_User_EmailUser_Smtp_PostfixTest');
+        return $suite;
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'Tinebase_User_EmailUser_AllTests::main') {
+    Tinebase_User_AllTests::main();
+}
