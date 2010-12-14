176a34e07c49410a86d001892817888382853a09
Philipp Schüle <p.schuele@metaways.de>
2010-12-14 15:47:06 +0000

    added Id keyword
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17851 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Setup/Initialize.php
+++ tine20/Tinebase/Setup/Initialize.php
@@ -5,8 +5,8 @@
  * @package     Tinebase
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Jonas Fischer <j.fischer@metaways.de>
- * @copyright   Copyright (c) 2008-2009 Metaways Infosystems GmbH (http://www.metaways.de)
- * @version     $Id: TineInitial.php 9535 2009-07-20 10:30:05Z p.schuele@metaways.de $
+ * @copyright   Copyright (c) 2008-2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
  *
  */
 
