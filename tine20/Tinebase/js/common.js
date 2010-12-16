77e722e70fc7f293ea55cf5539782fc42c8d4258
Karlo Vezijak <airmike23@gmail.com>
2010-12-16 16:39:19 +0000

    - for datetime, date and time renderers ISO string can be sent - jsLinted
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17901 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/common.js
+++ tine20/Tinebase/js/common.js
@@ -8,7 +8,9 @@
  * @copyright   Copyright (c) 2007-2010 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id: Common.js 4995 2008-10-20 10:20:01Z c.weiss@metaways.de $
  */
- 
+
+/*global Ext, Tine, Locale*/
+
 Ext.ns('Tine', 'Tine.Tinebase');
 
 /**
@@ -18,24 +20,26 @@ Tine.Tinebase.common = {
     
     /**
      * Open browsers native popup
-     * @param {string} _windowName
-     * @param {string} _url
-     * @param {int} _width
-     * @param {int} _height
+     * 
+     * @param {string} 	windowName
+     * @param {string} 	url
+     * @param {int} 	width
+     * @param {int} 	height
      */
-    openWindow: function(_windowName, _url, _width, _height){
+    openWindow: function (windowName, url, width, height) {
         // M$ IE has its internal location bar in the viewport
-        if(Ext.isIE) {
-            _height = _height + 20;
+        if (Ext.isIE) {
+            height = height + 20;
         }
         
         // chrome counts window decoration and location bar to window height
         if (Ext.isChrome) {
-            _height += 40;
+            height += 40;
         }
         
-        var windowName = Ext.isString(_windowName) ? _windowName.replace(/[^a-zA-Z0-9_]/g, '') : _windowName;
-        var w,h,x,y,leftPos,topPos,popup;
+        windowName = Ext.isString(windowName) ? windowName.replace(/[^a-zA-Z0-9_]/g, '') : windowName;
+        
+        var	w, h, x, y, leftPos, topPos, popup;
 
         if (document.all) {
             w = document.body.clientWidth;
@@ -50,28 +54,29 @@ Tine.Tinebase.common = {
                 y = window.screenY;
             }
         }
-        leftPos = ((w - _width) / 2) + y;
-        topPos = ((h - _height) / 2) + x;
+        leftPos = ((w - width) / 2) + y;
+        topPos = ((h - height) / 2) + x;
         
-        popup = window.open(_url, windowName, 'width=' + _width + ',height=' + _height + ',top=' + topPos + ',left=' + leftPos +
+        popup = window.open(url, windowName, 'width=' + width + ',height=' + height + ',top=' + topPos + ',left=' + leftPos +
         ',directories=no,toolbar=no,location=no,menubar=no,scrollbars=no,status=no,resizable=yes,dependent=no');
         
         return popup;
     },
     
-    showDebugConsole: function() {
+    showDebugConsole: function () {
         if (! Ext.debug) {
-            var head = document.getElementsByTagName("head")[0];
-            var scriptTag = document.createElement("script");
+            var head = document.getElementsByTagName("head")[0],
+            	scriptTag = document.createElement("script");
+            
             scriptTag.setAttribute("src", 'library/ExtJS/src/debug.js');
             scriptTag.setAttribute("type", "text/javascript");
             head.appendChild(scriptTag);
             
             var scriptEl = Ext.get(scriptTag);
-            scriptEl.on('load', function() {
+            scriptEl.on('load', function () {
                 Ext.log('debug console initialised');
             });
-            scriptEl.on('fail', function() {
+            scriptEl.on('fail', function () {
                 Ext.msg.alert('could not activate debug console');
             });
         } else {
@@ -82,12 +87,14 @@ Tine.Tinebase.common = {
     /**
      * Returns localised date and time string
      * 
-     * @param {mixed} date
+     * @param {mixed} $_iso8601
      * @see Ext.util.Format.date
-     * @return {string} localised date and time
+     * @return {String} localised date and time
      */
-    dateTimeRenderer: function($_iso8601){
-        return Ext.util.Format.date($_iso8601, Locale.getTranslationData('Date', 'medium') + ' ' + Locale.getTranslationData('Time', 'medium'));
+    dateTimeRenderer: function ($_iso8601) {
+    	var dateObj = $_iso8601 instanceof Date ? $_iso8601 : Date.parseDate($_iso8601, Date.patterns.ISO8601Long);
+    	
+        return Ext.util.Format.date(dateObj, Locale.getTranslationData('Date', 'medium') + ' ' + Locale.getTranslationData('Time', 'medium'));
     },
 
     /**
@@ -95,10 +102,12 @@ Tine.Tinebase.common = {
      * 
      * @param {mixed} date
      * @see Ext.util.Format.date
-     * @return {string} localised date
+     * @return {String} localised date
      */
-    dateRenderer: function(date){
-        return Ext.util.Format.date(date, Locale.getTranslationData('Date', 'medium'));
+    dateRenderer: function (date) {
+    	var dateObj = date instanceof Date ? date : Date.parseDate(date, Date.patterns.ISO8601Long);
+    	
+        return Ext.util.Format.date(dateObj, Locale.getTranslationData('Date', 'medium'));
     },
     
     /**
@@ -106,10 +115,12 @@ Tine.Tinebase.common = {
      * 
      * @param {mixed} date
      * @see Ext.util.Format.date
-     * @return {string} localised time
+     * @return {String} localised time
      */
-    timeRenderer: function(date){
-        return Ext.util.Format.date(date, Locale.getTranslationData('Time', 'medium'));
+    timeRenderer: function (date) {
+    	var dateObj = date instanceof Date ? date : Date.parseDate(date, Date.patterns.ISO8601Long);
+    	
+        return Ext.util.Format.date(dateObj, Locale.getTranslationData('Time', 'medium'));
     },
     
     /**
@@ -120,40 +131,41 @@ Tine.Tinebase.common = {
      * 
      * TODO add style for tag divs
      */
-    tagsRenderer: function(tags) {
+    tagsRenderer: function (tags) {
         var result = '';
         if (tags) {
-            for (var i=0; i < tags.length; i++) {
+            for (var i = 0; i < tags.length; i += 1) {
                 var qtipText = tags[i].name;
                 if (tags[i].description) {
                     qtipText += ' | ' + tags[i].description;
                 }
-                result += '<div ext:qtip="' + qtipText + '" class="tb-grid-tags" style="background-color:' 
-                    + tags[i].color 
-                    + ';">&#160;</div>';
+                result += '<div ext:qtip="' + qtipText + '" class="tb-grid-tags" style="background-color:' + tags[i].color + ';">&#160;</div>';
             }
         }
+        
         return result;
     },
     
     /**
      * Returns prettyfied minutes
+     * 
      * @param  {Number} minutes
      * @param  {String} format -> {0} will be replaced by Hours, {1} with minutes
      * @return {String}
      */
-    minutesRenderer: function(minutes, format){
-        var s;
-        var i = minutes%60;
-        var H = Math.floor(minutes/60);//%(24);
-        //var d = Math.floor(minutes/(60*24));
+    minutesRenderer: function (minutes, format) {
+        var s,
+        	i = minutes % 60,
+        	H = Math.floor(minutes / 60), // % (24);
+        	Hs;
+        //var d = Math.floor(minutes / (60 * 24));
         
         if (! format) {
-            var s = String.format(Tine.Tinebase.translation.ngettext('{0} minute', '{0} minutes', i), i);
-            var Hs = String.format(Tine.Tinebase.translation.ngettext('{0} hour', '{0} hours', H), H);
+            s = String.format(Tine.Tinebase.translation.ngettext('{0} minute', '{0} minutes', i), i);
+            Hs = String.format(Tine.Tinebase.translation.ngettext('{0} hour', '{0} hours', H), H);
             //var ds = String.format(Tine.Tinebase.translation.ngettext('{0} workday', '{0} workdays', d), d);
             
-            if (i == 0) {
+            if (i === 0) {
             	s = Hs;
             } else {
                 s = H ? Hs + ', ' + s : s;
@@ -168,14 +180,15 @@ Tine.Tinebase.common = {
 
     /**
      * Returns prettyfied seconds
+     * 
      * @param  {Number} seconds
      * @return {String}
      */
-    secondsRenderer: function(seconds){
+    secondsRenderer: function (seconds) {
         
-        var s = seconds%60;
-        var m = Math.floor(seconds/60);
-        var result = '';
+        var s = seconds % 60,
+        	m = Math.floor(seconds / 60),
+        	result = '';
         
         var secondResult = String.format(Tine.Tinebase.translation.ngettext('{0} second', '{0} seconds', s), s);
         
@@ -199,40 +212,47 @@ Tine.Tinebase.common = {
      * @param {object} account object 
      * @return {string} formated user display name
      */
-    usernameRenderer: function(accountObject){
+    usernameRenderer: function (accountObject) {
         var result = (accountObject) ? accountObject.accountDisplayName : '';
+        
         return Ext.util.Format.htmlEncode(result);
     },
     
     /**
      * Returns a username or groupname with according icon in front
      */
-    accountRenderer: function(_accountObject, _metadata, _record, _rowIndex, _colIndex, _store) {
-        if (! _accountObject) return '';
+    accountRenderer: function (accountObject, metadata, record, rowIndex, colIndex, store) {
+        if (! accountObject) {
+        	return '';
+        }
+        
         var type, iconCls, displayName;
         
-        if(_accountObject.accountDisplayName){
+        if (accountObject.accountDisplayName) {
             type = 'user';
-            displayName = _accountObject.accountDisplayName;
-        } else if (_accountObject.name){
+            displayName = accountObject.accountDisplayName;
+        } else if (accountObject.name) {
             type = 'group';
-            displayName = _accountObject.name;
-        } else if (_record.data.name) {
-            type = _record.data.type;
-            displayName = _record.data.name;
-        } else if (_record.data.account_name) {
-            type = _record.data.account_type;
-            displayName = _record.data.account_name;
+            displayName = accountObject.name;
+        } else if (record.data.name) {
+            type = record.data.type;
+            displayName = record.data.name;
+        } else if (record.data.account_name) {
+            type = record.data.account_type;
+            displayName = record.data.account_name;
         }
-        iconCls = type == 'user' ? 'renderer renderer_accountUserIcon' : 'renderer renderer_accountGroupIcon';
+        
+        iconCls = type === 'user' ? 'renderer renderer_accountUserIcon' : 'renderer renderer_accountGroupIcon';
+        
         return '<div class="' + iconCls  + '">&#160;</div>' + Ext.util.Format.htmlEncode(displayName); 
     },
     
     /**
      * Returns account type icon
      */
-    accountTypeRenderer: function(type) {
-        var iconCls = (type) == 'user' ? 'renderer_accountUserIcon' : 'renderer_accountGroupIcon';
+    accountTypeRenderer: function (type) {
+        var iconCls = (type) === 'user' ? 'renderer_accountUserIcon' : 'renderer_accountGroupIcon';
+        
         return '<div style="background-position: 0px" class="' + iconCls  + '">&#160;</div>'; 
     },
     
@@ -242,8 +262,8 @@ Tine.Tinebase.common = {
      * @param {string} value
      * @return {string}
      */
-    booleanRenderer: function(value) {
-        var translationString = String.format("{0}",(value==1) ? Locale.getTranslationData('Question', 'yes') : Locale.getTranslationData('Question', 'no'));
+    booleanRenderer: function (value) {
+        var translationString = String.format("{0}",(value == 1) ? Locale.getTranslationData('Question', 'yes') : Locale.getTranslationData('Question', 'no'));
         
         return translationString.substr(0, translationString.indexOf(':'));
     },
@@ -260,51 +280,51 @@ Tine.Tinebase.common = {
      * @param {String} name of the customfield
      * @return {String}
      */
-    customfieldRenderer: function(customfields, metadata, record, rowIndex, colIndex, store, name) {
+    customfieldRenderer: function (customfields, metadata, record, rowIndex, colIndex, store, name) {
         return Ext.util.Format.htmlEncode(customfields[name]); 
     },
     
     /**
      * check if user has right to view/manage this application/resource
      * 
-     * @param   string      right (view, admin, manage)
-     * @param   string      application
-     * @param   string      resource (for example roles, accounts, ...)
-     * @returns boolean 
+     * @param   {String}      right (view, admin, manage)
+     * @param   {String}      application
+     * @param   {String}      resource (for example roles, accounts, ...)
+     * @returns {Boolean} 
      */
-    hasRight: function(_right, _application, _resource) {
+    hasRight: function (right, application, resource) {
         var userRights = [];
         
-        if (!(Tine && Tine[_application] && Tine[_application].registry && Tine[_application].registry.get('rights'))) {
+        if (! (Tine && Tine[application] && Tine[application].registry && Tine[application].registry.get('rights'))) {
             if (! Tine.Tinebase.appMgr) {
                 console.error('Tine.Tinebase.appMgr not yet available');
-            } else if (Tine.Tinebase.appMgr.get(_application)) {
-                console.error('Tine.' + _application + '.rights is not available, initialisation Error!');
+            } else if (Tine.Tinebase.appMgr.get(application)) {
+                console.error('Tine.' + application + '.rights is not available, initialisation Error!');
             }
             return false;
         }
-        userRights = Tine[_application].registry.get('rights');
+        userRights = Tine[application].registry.get('rights');
         
         //console.log(userRights);
         var result = false;
         
-        for (var i=0; i < userRights.length; i++) {
-            if (userRights[i] == 'admin') {
+        for (var i = 0; i < userRights.length; i += 1) {
+            if (userRights[i] === 'admin') {
                 result = true;
                 break;
             }
             
-            if (_right == 'view' && (userRights[i] == 'view_' + _resource || userRights[i] == 'manage_' + _resource) ) {
+            if (right === 'view' && (userRights[i] === 'view_' + resource || userRights[i] === 'manage_' + resource)) {
                 result = true;
                 break;
             }
             
-            if (_right == 'manage' && userRights[i] == 'manage_' + _resource) {
+            if (right === 'manage' && userRights[i] === 'manage_' + resource) {
                 result = true;
                 break;
             }
             
-            if (_right == userRights[i]) {
+            if (right === userRights[i]) {
                 result = true;
                 break;
             }
@@ -319,13 +339,13 @@ Tine.Tinebase.common = {
      * @param {Integer} max
      * @return {Integer}
      */
-    getRandomNumber: function(min, max) {
+    getRandomNumber: function (min, max) {
         if (min > max) {
             return -1;
         }
-        if (min == max) {
+        if (min === max) {
             return min;
         }
-        return min + parseInt(Math.random() * (max-min+1));
+        return min + parseInt(Math.random() * (max - min + 1), 10);
     }
 };
