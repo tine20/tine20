7a3c85850de9d0e4bc86544005736ce764bcc656
Philipp Schüle <p.schuele@metaways.de>
2010-12-17 13:05:27 +0000

    - added admin app controller and config model 0003192: add default addressbook for user/groups to config + registry data http://www.tine20.org/bugtracker/view.php?id=3192
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17920 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Admin/Controller.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * MAIN controller for addressbook, does event and container handling
+ *
+ * @package     Admin
+ * @subpackage  Controller
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ * 
+ */
+
+/**
+ * main controller for Admin
+ *
+ * @package     Admin
+ * @subpackage  Controller
+ */
+class Admin_Controller extends Tinebase_Controller_Abstract
+{
+    /**
+     * holds the instance of the singleton
+     *
+     * @var Admin_Controller
+     */
+    private static $_instance = NULL;
+
+    /**
+     * constructor (get current user)
+     */
+    private function __construct() {
+        $this->_currentAccount = Tinebase_Core::getUser();
+        $this->_applicationName = 'Admin';
+        $this->_defaultsSettings = array(
+            Admin_Model_Config::DEFAULTINTERNALADDRESSBOOK  => NULL,
+        );
+    }
+    
+    /**
+     * don't clone. Use the singleton.
+     *
+     */
+    private function __clone() 
+    {        
+    }
+    
+    /**
+     * the singleton pattern
+     *
+     * @return Admin_Controller
+     */
+    public static function getInstance() 
+    {
+        if (self::$_instance === NULL) {
+            self::$_instance = new Admin_Controller;
+        }
+        
+        return self::$_instance;
+    }
+}
---
+++ tine20/Admin/Model/Config.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Admin
+ * @subpackage  Record
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @version     $Id$
+ * 
+ */
+
+/**
+ * class Admin_Model_Config
+ * 
+ * @package     Admin
+ * @subpackage  Record
+ */
+class Admin_Model_Config extends Tinebase_Record_Abstract 
+{   
+    /**
+     * default internal addressbook for new users/groups
+     * 
+     * @var string
+     */
+    const DEFAULTINTERNALADDRESSBOOK = 'defaultInternalAddressbook';
+        
+    /**
+     * identifier
+     * 
+     * @var string
+     */ 
+    protected $_identifier = 'id';
+    
+    /**
+     * application the record belongs to
+     *
+     * @var string
+     */
+    protected $_application = 'Admin';
+    
+    /**
+     * record validators
+     *
+     * @var array
+     */
+    protected $_validators = array(
+        'id'                => array('allowEmpty' => true ),
+        'defaults'          => array('allowEmpty' => true ),
+    );
+}
