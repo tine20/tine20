65977e0587f9c342cfb6dd19c895b40802372304
Lars Kneschke <l.kneschke@metaways.de>
2010-12-18 05:46:21 +0000

    Test class for Tinebase_FileSystem
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17949 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/tine20/Tinebase/FileSystemTest.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Tinebase
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2010-2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Tinebase_FileSystemTest::main');
+}
+
+/**
+ * Test class for Tinebase_FileSystem
+ */
+class Tinebase_FileSystemTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var array test objects
+     */
+    protected $objects = array();
+
+    /**
+     * @var Filemanager_Controller_Filesystem
+     */
+    protected $_controller;
+    /**
+     * Backend
+     *
+     * @var Filemanager_Backend_Node
+     */
+    protected $_backend;
+    
+    /**
+     * Runs the test methods of this class.
+     *
+     * @access public
+     * @static
+     */
+    public static function main()
+    {
+		$suite  = new PHPUnit_Framework_TestSuite('Tine 2.0 filesystem tests');
+        PHPUnit_TextUI_TestRunner::run($suite);
+	}
+
+    /**
+     * Sets up the fixture.
+     * This method is called before a test is executed.
+     *
+     * @access protected
+     */
+    protected function setUp()
+    {
+        $this->_controller = new Tinebase_FileSystem();
+        $this->_basePath   = '/' . Tinebase_Application::getInstance()->getApplicationByName('Tinebase')->getId() . '/' . Tinebase_Model_Container::TYPE_SHARED;
+        
+        $this->objects['directories'] = array();
+        
+        $this->_controller->initializeApplication(Tinebase_Application::getInstance()->getApplicationByName('Tinebase'));
+    }
+
+    /**
+     * Tears down the fixture
+     * This method is called after a test is executed.
+     *
+     * @access protected
+     */
+    protected function tearDown()
+    {
+        foreach ($this->objects['directories'] as $directory) {
+            $this->_controller->rmDir($directory, true);
+        } 
+    }
+    
+    public function testMkdir()
+    {
+        $testPath = $this->_basePath . '/PHPUNIT';
+        $this->_controller->mkDir($testPath);
+        $this->objects['directories'][] = $testPath;
+        
+        $this->assertTrue($this->_controller->fileExists($testPath));
+    }
+    
+    public function testScandir()
+    {
+        $this->testMkdir();
+        
+        $children = $this->_controller->scanDir($this->_basePath);
+        
+        $this->assertTrue(in_array('PHPUNIT', $children));
+    }
+    
+    public function testIsDir()
+    {
+        $this->testMkdir();
+        
+        $result = $this->_controller->isDir($this->_basePath . '/PHPUNIT');
+        
+        $this->assertTrue($result);
+    }
+    
+    public function testCreateFile()
+    {
+        $this->testMkdir();
+        
+        $handle = $this->_controller->fopen($this->_basePath . '/PHPUNIT/phpunit.txt', 'x');
+        
+        $this->assertType('resource', $handle, 'opening file failed');
+        
+        $written = fwrite($handle, 'phpunit');
+        
+        $this->assertEquals(7, $written);
+        
+        $this->_controller->fclose($handle);
+        
+        $children = $this->_controller->scanDir($this->_basePath . '/PHPUNIT');
+        
+        $this->assertTrue(in_array('phpunit.txt', $children));
+    }
+    
+    public function testOpenFile()
+    {
+        $this->testCreateFile();
+        
+        $handle = $this->_controller->fopen($this->_basePath . '/PHPUNIT/phpunit.txt', 'r');
+        
+        $this->assertEquals('phpunit', stream_get_contents($handle), 'file content mismatch');
+        
+        $this->_controller->fclose($handle);
+    }
+    
+    public function testDeleteFile()
+    {
+        $this->testCreateFile();
+        
+        $this->_controller->unlink($this->_basePath . '/PHPUNIT/phpunit.txt');
+        
+        $children = $this->_controller->scanDir($this->_basePath . '/PHPUNIT');
+        
+        $this->assertTrue(!in_array('phpunit.txt', $children));
+    }
+    
+    public function testGetFileSize()
+    {
+        $this->testCreateFile();
+        
+        $filesize = $this->_controller->filesize($this->_basePath . '/PHPUNIT/phpunit.txt');
+        
+        $this->assertEquals(7, $filesize);
+    }
+    
+    public function testGetContentType()
+    {
+        $this->testCreateFile();
+        
+        $contentType = $this->_controller->getContentType($this->_basePath . '/PHPUNIT/phpunit.txt');
+        
+        $this->assertEquals('text/plain', $contentType);
+    }
+    
+    public function testGetMTime()
+    {
+        $now = Tinebase_DateTime::now()->getTimestamp();
+        
+        $this->testCreateFile();
+        
+        $timestamp = $this->_controller->getMTime($this->_basePath . '/PHPUNIT/phpunit.txt');
+        
+        $this->assertGreaterThanOrEqual(sprintf('%u', $now), sprintf('%u', $timestamp));
+    }
+    
+    /**
+     * @return Filemanager_Model_Directory
+     */
+    public static function getTestRecord()
+    {
+        $object  = new Tinebase_Model_Tree_Node(array(
+            'name'     => 'PHPUnit test node',
+        ), true); 
+        
+        return $object;
+    }
+}		
+	
+
+if (PHPUnit_MAIN_METHOD == 'Tinebase_FileSystemTest::main') {
+    Tinebase_FileSystemTest::main();
+}
