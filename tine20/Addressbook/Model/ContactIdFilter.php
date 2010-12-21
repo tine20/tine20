4e7a24767ceb7584820d6be8faa6bdb5a1b7de13
Cornelius Weiß <c.weiss@metaways.de>
2010-12-21 21:25:17 +0000

    contact id filter backend
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@17996 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Addressbook/Model/ContactIdFilter.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Tinebase
+ * @subpackage  Filter
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Tinebase_Model_Filter_Id
+ * 
+ * @package     Addressbook
+ * 
+ * filters one or more ids
+ */
+class Addressbook_Model_ContactIdFilter extends Tinebase_Model_Filter_Id
+{
+    /**
+     * appends sql to given select statement
+     *
+     * @param  Zend_Db_Select                    $_select
+     * @param  Tinebase_Backend_Sql_Abstract     $_backend
+     */
+    public function appendFilterSql($_select, $_backend)
+    {
+        if (is_array($this->_value)) {
+            foreach($this->_value as $key => $value) {
+                if ($value === Addressbook_Model_Contact::CURRENTCONTACT) {
+                    $this->_value[$key] = Tinebase_Core::getUser()->contact_id;
+                }
+            }
+        } else if ($this->_value === Addressbook_Model_Contact::CURRENTCONTACT) {
+            $this->_value = Tinebase_Core::getUser()->contact_id;
+        }
+        
+        parent::appendFilterSql($_select, $_backend);
+    }
+    
+    /**
+     * returns array with the filter settings of this filter
+     *
+     * @param  bool $_valueToJson resolve value for json api?
+     * @return array
+     */
+    public function toArray($_valueToJson = false)
+    {
+        $result = parent::toArray($_valueToJson);
+        
+        if ($_valueToJson) {
+            if (is_array($result['value'])) {
+                foreach ($result['value'] as $key => $value) {
+                    if ($value === Addressbook_Model_Contact::CURRENTCONTACT) {
+                        $value = Tinebase_Core::getUser()->contact_id;
+                    }
+                    $result['value'][$key] = Addressbook_Controller_Contact::getInstance()->get($value)->toArray();
+                }
+            } else {
+                if ($result['value'] === Addressbook_Model_Contact::CURRENTCONTACT) {
+                    $result['value'] = Tinebase_Core::getUser()->contact_id;
+                }
+                $result['value'] = Addressbook_Controller_Contact::getInstance()->get($result['value'])->toArray();
+            }
+        }
+        
+        return $result;
+    }
+}
