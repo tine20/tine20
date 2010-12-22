6c678a5785cd98044cc8f17d8c82c3c04ed63fba
Lars Kneschke <l.kneschke@metaways.de>
2010-12-22 23:05:24 +0000

    added Filemanager controller
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18044 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Filemanager/Controller.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * MAIN controller for filemanager, does event and container handling
+ *
+ * @package     Filemanager
+ * @subpackage  Controller
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2010-2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ * 
+ */
+
+/**
+ * main controller for Filemanager
+ *
+ * @package     Filemanager
+ * @subpackage  Controller
+ */
+class Filemanager_Controller extends Tinebase_Controller_Abstract implements Tinebase_Event_Interface, Tinebase_Container_Interface
+{
+    /**
+     * holds the instance of the singleton
+     *
+     * @var Filemamager_Controller
+     */
+    private static $_instance = NULL;
+
+    /**
+     * constructor (get current user)
+     */
+    private function __construct() {
+        $this->_currentAccount = Tinebase_Core::getUser();
+    }
+    
+    /**
+     * don't clone. Use the singleton.
+     *
+     */
+    private function __clone() 
+    {        
+    }
+    
+    /**
+     * the singleton pattern
+     *
+     * @return Addressbook_Controller
+     */
+    public static function getInstance() 
+    {
+        if (self::$_instance === NULL) {
+            self::$_instance = new Filemanager_Controller;
+        }
+        
+        return self::$_instance;
+    }
+
+    /**
+     * event handler function
+     * 
+     * all events get routed through this function
+     *
+     * @param Tinebase_Event_Abstract $_eventObject the eventObject
+     * 
+     * @todo    write test
+     */
+    public function handleEvents(Tinebase_Event_Abstract $_eventObject)
+    {
+        if (Tinebase_Core::isLogLevel(Zend_Log::TRACE)) Tinebase_Core::getLogger()->trace(__METHOD__ . ' (' . __LINE__ . ') handle event of type ' . get_class($_eventObject));
+        
+        switch(get_class($_eventObject)) {
+            case 'Admin_Event_AddAccount':
+                $this->createPersonalFolder($_eventObject->account);
+                break;
+            case 'Admin_Event_DeleteAccount':
+                $this->deletePersonalFolder($_eventObject->account);
+                break;
+        }
+    }
+        
+    /**
+     * creates the initial folder for new accounts
+     *
+     * @param mixed[int|Tinebase_Model_User] $_account   the accountd object
+     * @return Tinebase_Record_RecordSet of subtype Tinebase_Model_Container
+     */
+    public function createPersonalFolder($_account)
+    {
+        $translation = Tinebase_Translation::getTranslation('Filemanager');
+        
+        $userId  = $_account instanceof Tinebase_Model_User ? $_account->getId() : $_account;
+        $user    = Tinebase_User::getInstance()->getUserById($userId);
+        $newContainer = new Tinebase_Model_Container(array(
+            'name'              => sprintf($translation->_("%s's personal files"), $user->accountFullName),
+            'type'              => Tinebase_Model_Container::TYPE_PERSONAL,
+            'backend'           => 'Sql',
+            'application_id'    => Tinebase_Application::getInstance()->getApplicationByName('Filemanager')->getId() 
+        ));
+        
+        $personalContainer = Tinebase_Container::getInstance()->addContainer($newContainer, NULL, FALSE, $userId);
+        
+        mkdir('tine20:///' . Tinebase_Application::getInstance()->getApplicationByName('Filemanager')->getId() . '/folders/personal/' . $user->getId() . '/' . $personalContainer->getId(), 0777, true);
+        
+        $container = new Tinebase_Record_RecordSet('Tinebase_Model_Container', array($personalContainer));
+        
+        return $container;
+    }
+    
+    /**
+     * delete all personal user folders and the contacts associated with these folders
+     *
+     * @param Tinebase_Model_User $_account the accountd object
+     * @todo implement and write test
+     */
+    public function deletePersonalFolder($_account)
+    {
+    }
+}
