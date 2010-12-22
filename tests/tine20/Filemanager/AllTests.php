b979cef076ae35f4add80d9cf990ddc98bea4ecd
Lars Kneschke <l.kneschke@metaways.de>
2010-12-22 07:19:20 +0000

    Filemanager webdav tree tests
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18003 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tests/tine20/Filemanager/AllTests.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Filemanager
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2010-2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+if (! defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Filemanager_AllTests::main');
+}
+
+class Filemanager_AllTests
+{
+    public static function main ()
+    {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+    
+    public static function suite ()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('Tine 2.0 Filemanager All Tests');
+        $suite->addTestSuite('Filemanager_Frontend_TreeTest');
+        
+        return $suite;
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'Filemanager_AllTests::main') {
+    Filemanager_AllTests::main();
+}
