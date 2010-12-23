86cb2076d6e9b1df0e90597e132867c6cae52965
Philipp Schüle <p.schuele@metaways.de>
2010-12-23 16:22:28 +0000

    - added timesheet default favorites 0003540: Default favorites for Timetracker http://www.tine20.org/bugtracker/view.php?id=3540
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18087 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Timetracker/Setup/Initialize.php
+++ tine20/Timetracker/Setup/Initialize.php
@@ -2,19 +2,102 @@
 /**
  * Tine 2.0
  * 
- * @package     TimeTracker
+ * @package     Timetracker
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Jonas Fischer <j.fischer@metaways.de>
- * @copyright   Copyright (c) 2008-2009 Metaways Infosystems GmbH (http://www.metaways.de)
- * @version     $Id: TineInitial.php 9535 2009-07-20 10:30:05Z p.schuele@metaways.de $
+ * @copyright   Copyright (c) 2008-2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
  *
  */
 
 /**
- * class for TimeTracker initialization
+ * class for Timetracker initialization
  * 
  * @package     Setup
  */
-class TimeTracker_Setup_Initialize extends Setup_Initialize
+class Timetracker_Setup_Initialize extends Setup_Initialize
 {
+    /**
+     * init favorites
+     */
+    protected function _initializeFavorites()
+    {
+        $pfe = new Tinebase_PersistentFilter_Backend_Sql();
+        
+        $commonValues = array(
+            'account_id'        => NULL,
+            'application_id'    => Tinebase_Application::getInstance()->getApplicationByName('Timetracker')->getId(),
+            'model'             => 'Timetracker_Model_TimesheetFilter',
+        );
+        
+        $pfe->create(new Tinebase_Model_PersistentFilter(array_merge($commonValues, array(
+            'name'              => "Timesheets today", // _("Timesheets today")
+            'description'       => "Timesheets today",
+            'filters'           => array(array(
+                'field'     => 'account_id',
+                'operator'  => 'equals',
+                'value'     => Tinebase_Model_User::CURRENTACCOUNT,
+            ), array(
+                'field'     => 'start_date',
+                'operator'  => 'within',
+                'value'     => 'dayThis',
+            )),
+        ))));
+
+        $pfe->create(new Tinebase_Model_PersistentFilter(array_merge($commonValues, array(
+            'name'              => "Timesheets this week", // _("Timesheets this week")
+            'description'       => "Timesheets this week",
+            'filters'           => array(array(
+                'field'     => 'account_id',
+                'operator'  => 'equals',
+                'value'     => Tinebase_Model_User::CURRENTACCOUNT,
+            ), array(
+                'field'     => 'start_date',
+                'operator'  => 'within',
+                'value'     => 'weekThis',
+            )),
+        ))));
+        
+        $pfe->create(new Tinebase_Model_PersistentFilter(array_merge($commonValues, array(
+            'name'              => "Timesheets last week", // _("Timesheets last week")
+            'description'       => "Timesheets last week",
+            'filters'           => array(array(
+                'field'     => 'account_id',
+                'operator'  => 'equals',
+                'value'     => Tinebase_Model_User::CURRENTACCOUNT,
+            ), array(
+                'field'     => 'start_date',
+                'operator'  => 'within',
+                'value'     => 'weekLast',
+            )),
+        ))));
+
+        $pfe->create(new Tinebase_Model_PersistentFilter(array_merge($commonValues, array(
+            'name'              => "Timesheets this month", // _("Timesheets this month")
+            'description'       => "Timesheets this month",
+            'filters'           => array(array(
+                'field'     => 'account_id',
+                'operator'  => 'equals',
+                'value'     => Tinebase_Model_User::CURRENTACCOUNT,
+            ), array(
+                'field'     => 'start_date',
+                'operator'  => 'within',
+                'value'     => 'monthThis',
+            )),
+        ))));
+
+        $pfe->create(new Tinebase_Model_PersistentFilter(array_merge($commonValues, array(
+            'name'              => "Timesheets last month", // _("Timesheets last month")
+            'description'       => "Timesheets last month",
+            'filters'           => array(array(
+                'field'     => 'account_id',
+                'operator'  => 'equals',
+                'value'     => Tinebase_Model_User::CURRENTACCOUNT,
+            ), array(
+                'field'     => 'start_date',
+                'operator'  => 'within',
+                'value'     => 'monthLast',
+            )),
+        ))));
+    }
 }
--- tine20/Timetracker/Setup/Update/Release3.php
+++ tine20/Timetracker/Setup/Update/Release3.php
@@ -5,9 +5,9 @@
  * @package     Timetracker
  * @subpackage  Setup
  * @license     http://www.gnu.org/licenses/agpl.html AGPL3
- * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
- * @author      Philipp Schuele <p.schuele@metaways.de>
- * @version     $Id: Release0.php 9867 2009-08-11 13:37:53Z p.schuele@metaways.de $
+ * @copyright   Copyright (c) 2009-2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schüle <p.schuele@metaways.de>
+ * @version     $Id$
  */
 
 class Timetracker_Setup_Update_Release3 extends Setup_Update_Abstract
@@ -46,5 +46,93 @@ class Timetracker_Setup_Update_Release3 extends Setup_Update_Abstract
         Setup_Controller::getInstance()->createImportExportDefinitions(Tinebase_Application::getInstance()->getApplicationByName('Timetracker'));
         
         $this->setApplicationVersion('Timetracker', '3.2');
+    }
+    
+    /**
+     * added more default favorites
+     * 
+     * @return void
+     */
+    public function update_2()
+    {
+        $pfe = new Tinebase_PersistentFilter_Backend_Sql();
+        
+        $commonValues = array(
+            'account_id'        => NULL,
+            'application_id'    => Tinebase_Application::getInstance()->getApplicationByName('Timetracker')->getId(),
+            'model'             => 'Timetracker_Model_TimesheetFilter',
+        );
+        
+        $pfe->create(new Tinebase_Model_PersistentFilter(array_merge($commonValues, array(
+            'name'              => "Timesheets today",
+            'description'       => "Timesheets today",
+            'filters'           => array(array(
+                'field'     => 'account_id',
+                'operator'  => 'equals',
+                'value'     => Tinebase_Model_User::CURRENTACCOUNT,
+            ), array(
+                'field'     => 'start_date',
+                'operator'  => 'within',
+                'value'     => 'dayThis',
+            )),
+        ))));
+
+        $pfe->create(new Tinebase_Model_PersistentFilter(array_merge($commonValues, array(
+            'name'              => "Timesheets this week",
+            'description'       => "Timesheets this week",
+            'filters'           => array(array(
+                'field'     => 'account_id',
+                'operator'  => 'equals',
+                'value'     => Tinebase_Model_User::CURRENTACCOUNT,
+            ), array(
+                'field'     => 'start_date',
+                'operator'  => 'within',
+                'value'     => 'weekThis',
+            )),
+        ))));
+        
+        $pfe->create(new Tinebase_Model_PersistentFilter(array_merge($commonValues, array(
+            'name'              => "Timesheets last week",
+            'description'       => "Timesheets last week",
+            'filters'           => array(array(
+                'field'     => 'account_id',
+                'operator'  => 'equals',
+                'value'     => Tinebase_Model_User::CURRENTACCOUNT,
+            ), array(
+                'field'     => 'start_date',
+                'operator'  => 'within',
+                'value'     => 'weekLast',
+            )),
+        ))));
+
+        $pfe->create(new Tinebase_Model_PersistentFilter(array_merge($commonValues, array(
+            'name'              => "Timesheets this month",
+            'description'       => "Timesheets this month",
+            'filters'           => array(array(
+                'field'     => 'account_id',
+                'operator'  => 'equals',
+                'value'     => Tinebase_Model_User::CURRENTACCOUNT,
+            ), array(
+                'field'     => 'start_date',
+                'operator'  => 'within',
+                'value'     => 'monthThis',
+            )),
+        ))));
+
+        $pfe->create(new Tinebase_Model_PersistentFilter(array_merge($commonValues, array(
+            'name'              => "Timesheets last month",
+            'description'       => "Timesheets last month",
+            'filters'           => array(array(
+                'field'     => 'account_id',
+                'operator'  => 'equals',
+                'value'     => Tinebase_Model_User::CURRENTACCOUNT,
+            ), array(
+                'field'     => 'start_date',
+                'operator'  => 'within',
+                'value'     => 'monthLast',
+            )),
+        ))));
+                
+        $this->setApplicationVersion('Timetracker', '3.3');
     }
 }
