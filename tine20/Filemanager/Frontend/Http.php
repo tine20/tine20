95eb4ba5ae2669cea5a9638bbc2973d81d91ad25
Philipp Schüle <p.schuele@metaways.de>
2010-12-23 10:24:41 +0000

    this is needed for js client
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18068 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Filemanager/Frontend/Http.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Filemanager Http frontend
+ *
+ * This class handles all Http requests for the Filemanager application
+ *
+ * @package     Filemanager
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schüle <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ *
+ */
+class Filemanager_Frontend_Http extends Tinebase_Frontend_Http_Abstract
+{
+    /**
+     * app name
+     *
+     * @var string
+     */
+    protected $_applicationName = 'Filemanager';
+    
+    /**
+     * Returns all JS files which must be included for this app
+     *
+     * @return array Array of filenames
+     */
+    public function getJsFilesToInclude()
+    {
+        return array(
+            'Filemanager/js/Application.js',
+        );
+    }
+}
