612ddc8eda1cc61d8db31ab19c1b623f1bcc6577
Philipp Schüle <p.schuele@metaways.de>
2010-12-29 12:09:23 +0000

    - moved initialization code to test server - added setTestUserEmail()
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18175 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/tine20/TestHelper.php
+++ tests/tine20/TestHelper.php
@@ -5,7 +5,7 @@
  * @package     tests
  * @subpackage  test root
  * @license     http://www.gnu.org/licenses/agpl.html AGPL3
- * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2008-2010 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Matthias Greiling <m.greiling@metaways.de>
  * @version     $Id$
  */
@@ -49,92 +49,13 @@ require_once 'Zend/Loader/Autoloader.php';
 $autoloader = Zend_Loader_Autoloader::getInstance();
 $autoloader->setFallbackAutoloader(true);
 
-
-// get config
-$configData = @include('phpunitconfig.inc.php');
-if($configData === false) {
-    $configData = include('config.inc.php');
-}
-if($configData === false) {
-    die ('central configuration file config.inc.php not found in includepath: ' . get_include_path());
-}
-$config = new Zend_Config($configData);
-
-Zend_Registry::set('testConfig', $config);
-
-$_SERVER['DOCUMENT_ROOT'] = $config->docroot;
-
-// set default test mailer
-Tinebase_Smtp::setDefaultTransport(new Zend_Mail_Transport_Array());
-
-// set max execution time
-Tinebase_Core::setExecutionLifeTime(1200);
-
-// finally init base framework
+// init base framework
 TestServer::getInstance()->initFramework();
 
-Zend_Registry::set('locale', new Zend_Locale($config->locale));
-
 $tinebaseController = Tinebase_Controller::getInstance();
+$config = TestServer::getInstance()->getConfig();
 if (!$tinebaseController->login($config->username, $config->password, $config->ip, 'TineUnittest')){
     throw new Exception("Couldn't login, user session required for tests! \n");
 }
 
-initTestUsers();
-
-function initTestUsers() {
-    $personas = array(
-        'jsmith'   => 'John Smith',
-        'sclever'  => 'Susan Clever',
-        'pwulf'    => 'Paul Wulf',
-        'jmcblack' => 'James McBlack',
-        'rwright'  => 'Roberta Wright',
-    );
-    
-    foreach ($personas as $login => $fullName) {
-        try {
-            $user = Tinebase_User::getInstance()->getFullUserByLoginName($login);
-        } catch (Tinebase_Exception_NotFound $e) {
-            list($given, $last) = explode(' ', $fullName);
-            
-            $group   = Tinebase_Group::getInstance()->getGroupByName('Users');
-            $groupId = $group->getId();
-            
-            $user = new Tinebase_Model_FullUser(array(
-                'accountLoginName'      => $login,
-                'accountPrimaryGroup'   => $groupId,
-                'accountDisplayName'    => $fullName,
-                'accountLastName'       => $last,
-                'accountFirstName'      => $given,
-                'accountFullName'       => $fullName,
-                //'accountEmailAddress'   => $login . '@tine-publications.co.uk',
-                'accountEmailAddress'   => $login . '@tine20.org'
-            ));
-            
-            if (Tinebase_Application::getInstance()->isInstalled('Addressbook') === true) {
-                $internalAddressbook = Tinebase_Container::getInstance()->getContainerByName('Addressbook', 'Internal Contacts', Tinebase_Model_Container::TYPE_SHARED);
-                
-                $user->container_id = $internalAddressbook->getId();
-                
-                $contact = Admin_Controller_User::getInstance()->createOrUpdateContact($user);
-                
-                $user->contact_id = $contact->getId();
-            }
-            
-            $user = Tinebase_User::getInstance()->addUser($user);
-            
-            Tinebase_Group::getInstance()->addGroupMember($groupId, $user);
-            
-            if (Tinebase_Application::getInstance()->isInstalled('Addressbook') === true) {
-                $listBackend = new Addressbook_Backend_List();
-                
-                $listBackend->addListMember($group->list_id, $user->contact_id);
-            }
-            
-            // give additional testusers the same password as the primary test account
-            Tinebase_User::getInstance()->setPassword($user, Zend_Registry::get('testConfig')->password);
-        }
-        $personas[$login] = $user;
-    }
-    Zend_Registry::set('personas', $personas);
-}
+TestServer::getInstance()->setTestUserEmail();
--- tests/tine20/TestServer.php
+++ tests/tine20/TestServer.php
@@ -5,13 +5,11 @@
  * @package     tests
  * @subpackage  test root
  * @license     http://www.gnu.org/licenses/agpl.html AGPL3
- * @copyright   Copyright (c) 2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2008-2010 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Cornelius Weiss <c.weiss@metaways.de>
  * @version     $Id$
  * 
- * @todo        extend Tinebase_Server_Abstract to init framework
  */
-
      
 /**
  * helper class
@@ -54,18 +52,42 @@ class TestServer
      */
     public function initFramework()
     {
+        // get config
+        $configData = @include('phpunitconfig.inc.php');
+        if ($configData === false) {
+            $configData = include('config.inc.php');
+        }
+        if ($configData === false) {
+            die ('central configuration file config.inc.php not found in includepath: ' . get_include_path());
+        }
+        $config = new Zend_Config($configData);
+        
+        Zend_Registry::set('testConfig', $config);
+        
+        $_SERVER['DOCUMENT_ROOT'] = $config->docroot;
+        
         Tinebase_Core::initFramework();
         
+        // set default test mailer
+        Tinebase_Smtp::setDefaultTransport(new Zend_Mail_Transport_Array());
+        
+        // set max execution time
+        Tinebase_Core::setExecutionLifeTime(1200);
+
         // set default internal encoding
-        //ini_set('iconv.internal_encoding', 'utf-8');
         iconv_set_encoding("internal_encoding", "UTF-8");
+        
+        Zend_Registry::set('locale', new Zend_Locale($config->locale));
+        
+        $this->_initTestUsers();
     }
     
     /**
      * inits (adds) some test users
      *
      */
-    protected function _initTestUsers() {
+    protected function _initTestUsers()
+    {
         $personas = array(
             'jsmith'   => 'John Smith',
             'sclever'  => 'Susan Clever',
@@ -120,5 +142,28 @@ class TestServer
             $personas[$login] = $user;
         }
         Zend_Registry::set('personas', $personas);
+    }
+    
+    /**
+     * set test user email address if in config
+     */
+    public function setTestUserEmail()
+    {
+        if (Zend_Registry::get('testConfig')->email) {
+            // set email of test user contact
+            $testUserContact = Addressbook_Controller_Contact::getInstance()->getContactByUserId(Tinebase_Core::getUser()->getId());
+            $testUserContact->email = Zend_Registry::get('testConfig')->email;
+            Addressbook_Controller_Contact::getInstance()->update($testUserContact);
+        }
+    }
+    
+    /**
+     * get test config
+     * 
+     * @return Zend_Config
+     */
+    public function getConfig()
+    {
+        return Zend_Registry::get('testConfig');
     }
 }
