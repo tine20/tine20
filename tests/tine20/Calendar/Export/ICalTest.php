377e3d2bc86e6fbb6483d9a36c2de32cec35a437
Cornelius Weiß <c.weiss@metaways.de>
2011-01-03 10:48:23 +0000

    improve ical tests
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18248 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/tine20/Calendar/Export/ICalTest.php
+++ tests/tine20/Calendar/Export/ICalTest.php
@@ -20,9 +20,9 @@ require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'TestHe
  */
 class Calendar_Export_ICalTest extends PHPUnit_Framework_TestCase //extends Calendar_TestCase
 {
-    public function testExport()
+    public function setUp()
     {
-        $event = new Calendar_Model_Event(array(
+        $this->_testEvent = new Calendar_Model_Event(array(
             'dtstart'       => '2010-12-30 12:00:00',
             'dtend'         => '2010-12-30 13:00:00',
             'originator_tz' => 'Europe/Berlin',
@@ -36,12 +36,16 @@ class Calendar_Export_ICalTest extends PHPUnit_Framework_TestCase //extends Cale
             'priority'      => 1,
             'rrule'         => 'FREQ=DAILY;INTERVAL=1;UNTIL=2015-12-30 13:00:00'
         ));
-        
-        $ics = Calendar_Export_Ical::eventToIcal($event);
+    }
+    
+    public function testExport()
+    {
+        $exporter = new Calendar_Export_Ical();
+        $ics = $exporter->eventToIcal($this->_testEvent);
 //        echo $ics;
 
         // assert basics
-        $this->assertEquals(1, preg_match("/SUMMARY:{$event->summary}\r\n/", $ics), 'SUMMARY not correct');
+        $this->assertEquals(1, preg_match("/SUMMARY:{$this->_testEvent->summary}\r\n/", $ics), 'SUMMARY not correct');
         
         // assert dtstart/dtend tz
         $this->assertEquals(1, preg_match("/DTSTART;TZID=Europe\/Berlin:20101230T130000\r\n/", $ics), 'DTSTART not correct');
@@ -54,5 +58,57 @@ class Calendar_Export_ICalTest extends PHPUnit_Framework_TestCase //extends Cale
         // assert rrule
         $this->assertEquals(1, preg_match("/RRULE:FREQ=DAILY;INTERVAL=1;UNTIL=20151230T130000Z\r\n/", $ics), 'RRULE broken');
     }
+    
+    public function testExportRecurId()
+    {
+        $exceptions = new Tinebase_Record_RecordSet('Calendar_Model_Event');
+        $nextOccurance = Calendar_Model_Rrule::computeNextOccurrence($this->_testEvent, $exceptions, $this->_testEvent->dtstart);
+        
+        $exporter = new Calendar_Export_Ical();
+        $ics = $exporter->eventToIcal($nextOccurance);
+//        echo $ics;
 
+        // assert recurid
+        $this->assertEquals(1, preg_match("/RECURRENCE-ID;TZID=Europe\/Berlin:20101231T130000\r\n/", $ics), 'RECURRENCE-ID broken');
+    }
+    
+    public function testExportExdate()
+    {
+        $exceptions = new Tinebase_Record_RecordSet('Calendar_Model_Event');
+        $from = clone $this->_testEvent->dtstart;
+        $until = clone $this->_testEvent->dtend;
+        $until->addDay(2);
+        
+        $recurSet = Calendar_Model_Rrule::computeRecuranceSet($this->_testEvent, $exceptions, $from, $until);
+        $this->_testEvent->exdate = $recurSet->dtstart;
+
+        $exporter = new Calendar_Export_Ical();
+        $ics = $exporter->eventToIcal($this->_testEvent);
+//        echo $ics;
+
+        // assert exdate
+        $this->assertEquals(1, preg_match("/EXDATE;TZID=Europe\/Berlin:20101231T130000,20110101T130000\r\n/", $ics), 'RECURRENCE-ID broken');
+    }
+    
+    public function testExportRecurSet()
+    {
+        $exceptions = new Tinebase_Record_RecordSet('Calendar_Model_Event');
+        $from = clone $this->_testEvent->dtstart;
+        $until = clone $this->_testEvent->dtend;
+        $until->addDay(2);
+        
+        $recurSet = Calendar_Model_Rrule::computeRecuranceSet($this->_testEvent, $exceptions, $from, $until);
+        $this->_testEvent->exdate = array($recurSet->dtstart[0]);
+        
+        $eventSet = new Tinebase_Record_RecordSet('Calendar_Model_Event', array(
+            $this->_testEvent,
+            $recurSet[1]
+        ));
+
+        $exporter = new Calendar_Export_Ical();
+        $ics = $exporter->eventToIcal($eventSet);
+//        echo $ics;
+
+        $this->assertEquals(2, preg_match_all('/BEGIN:VEVENT\r\n/', $ics, $matches), 'There should be exactly 2 VEVENT compontents');
+    }
 }
