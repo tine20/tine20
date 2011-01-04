cbd0d0ea47c24c713662784fffb3dca058a30d62
Lars Kneschke <l.kneschke@metaways.de>
2011-01-04 15:44:21 +0000

    ASSIGNED - # 3518: Im Kalender eingetragene Termine werden nicht angepasst wenn sich eine eingeladene Gruppe nachträglich ändert http://www.tine20.org/bugtracker/view.php?id=3518
    new event for setting group memberships
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18293 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/Group/Event/SetGroupMemberships.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Tinebase
+ * @subpackage  Group
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2011-2011 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * event for setting group memberships of an user
+ *
+ * @package     Tinebase
+ * @subpackage  Group
+ */
+class Tinebase_Group_Event_SetGroupMemberships extends Tinebase_Event_Abstract
+{
+    /**
+     * the group id
+     *
+     * @var string
+     */
+    public $groupId;
+
+    /**
+     * the user id
+     *
+     * @var string
+     */
+    public $userId;
+}
