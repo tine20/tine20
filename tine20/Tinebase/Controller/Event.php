744341ba28c2a91a6d1540d4e5319cae7bff7dd1
Lars Kneschke <l.kneschke@metaways.de>
2011-01-04 14:06:18 +0000

    ASSIGNED - # 3638: Improve event handling http://www.tine20.org/bugtracker/view.php?id=3638
    controller abstract for applications with event handling
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18277 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/Controller/Event.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Tinebase
+ * @subpackage  Controller
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ * 
+ */
+
+/**
+ * controller abstract for applications with event handling
+ *
+ * @package     Tinebase
+ * @subpackage  Controller
+ */
+abstract class Tinebase_Controller_Event extends Tinebase_Controller_Abstract implements Tinebase_Event_Interface
+{
+    /**
+     * disable events on demand
+     * 
+     * @var mixed   false => no events filtered, true => all events filtered, array => disable only specific events
+     */
+    protected $_disabledEvents = false;
+    
+    /**
+     * (non-PHPdoc)
+     * @see Tinebase_Event_Interface::handleEvent()
+     */
+    public function handleEvent(Tinebase_Event_Abstract $_eventObject)
+    {
+        if ($this->_disabledEvents === true) {
+            // nothing todo
+            if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . 
+                ' events are disabled. do nothing'
+            );
+            return;
+        }
+        
+        $this->_handleEvent($_eventObject);
+    }
+    
+    /**
+     * implement logic for each controller in this function
+
+     * @param Tinebase_Event_Abstract $_eventObject
+     */
+    protected function _handleEvent(Tinebase_Event_Abstract $_eventObject)
+    {
+        // do nothing
+        //if (Tinebase_Core::isLogLevel(Zend_Log::TRACE)) Tinebase_Core::getLogger()->trace(__METHOD__ . ' (' . __LINE__ . ') handle event of type ' . get_class($_eventObject));
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see Tinebase_Event_Interface::suspendEvents()
+     */
+    public function suspendEvents()
+    {
+        $this->_disabledEvents = true;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Tinebase_Event_Interface::resumeEvents()
+     */
+    public function resumeEvents()
+    {
+        $this->_disabledEvents = false;
+    }
+}
