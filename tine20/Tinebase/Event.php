922f33cf92159e4f7055a1f5e70b32da870ef8ab
Lars Kneschke <l.kneschke@metaways.de>
2011-01-04 13:15:23 +0000

    ASSIGNED - # 3638: Improve event handling http://www.tine20.org/bugtracker/view.php?id=3638
    prepared logic to detect duplicate events
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18273 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Event.php
+++ tine20/Tinebase/Event.php
@@ -19,12 +19,26 @@
 class Tinebase_Event
 {
     /**
-     * calls the handleEvents function in the controller of all enabled applications 
+     * keeps a list of currently processed events
+     * 
+     * @var array
+     */
+    static public $events = array();
+    
+    /**
+     * calls the handleEvent function in the controller of all enabled applications 
      *
-     * @param Tinebase_Event_Object $_eventObject the event object
+     * @param  Tinebase_Event_Object  $_eventObject  the event object
      */
     static public function fireEvent(Tinebase_Event_Abstract $_eventObject)
     {
+        self::$events[get_class($_eventObject)][$_eventObject->getId()] = $_eventObject;
+        
+        if (self::isDuplicateEvent($_eventObject)) {
+            // do nothing
+            return;
+        }
+        
         foreach(Tinebase_Application::getInstance()->getApplicationsByState(Tinebase_Application::ENABLED) as $application) {
             try {
                 $controller = Tinebase_Core::getApplicationInstance($application);
@@ -35,7 +49,7 @@ class Tinebase_Event
             if($controller instanceof Tinebase_Event_Interface) {
                 if (Tinebase_Core::isLogLevel(Zend_Log::TRACE)) Tinebase_Core::getLogger()->trace(__METHOD__ . ' (' . __LINE__ . ') calling eventhandler of ' . (string) $application);
                 try {
-                    $controller->handleEvents($_eventObject);
+                    $controller->handleEvent($_eventObject);
                 } catch (Exception $e) {
                     if (Tinebase_Core::isLogLevel(Zend_Log::NOTICE)) Tinebase_Core::getLogger()->notice(__METHOD__ . ' (' . __LINE__ . ') ' 
                         . (string) $application . ' threw an exception: '
@@ -50,13 +64,27 @@ class Tinebase_Event
         try { 
             if (@class_exists('CustomEventHooks')) {
                 $methods = get_class_methods('CustomEventHooks');
-                if (in_array('handleEvents', (array)$methods)) {
+                if (in_array('handleEvent', (array)$methods)) {
                     Tinebase_Core::getLogger()->info(__METHOD__ . ' (' . __LINE__ . ') ' . ' about to process user defined event hook for '. get_class($_eventObject));
-                    CustomEventHooks::handleEvents($_eventObject);
+                    CustomEventHooks::handleEvent($_eventObject);
                 }
             }
         } catch (Exception $e) {
             Tinebase_Core::getLogger()->info(__METHOD__ . ' (' . __LINE__ . ') ' . ' failed to process user defined event hook with message: ' . $e);
         }
+        
+        unset(self::$events[get_class($_eventObject)][$_eventObject->getId()]);
+    }
+    
+    /**
+     * checks if an event is duplicate
+     * 
+     * @todo   implement logic
+     * @param  Tinebase_Event_Abstract  $_eventObject  the event object
+     * @return boolean
+     */
+    static public function isDuplicateEvent(Tinebase_Event_Abstract $_eventObject)
+    {
+        return false;
     }
 }
