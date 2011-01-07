c1dffa4615d87402bc8310871e796cf19e89f2c7
Philipp Schüle <p.schuele@metaways.de>
2011-01-07 13:07:31 +0000

    create default favorites
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18414 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Calendar/Setup/Initialize.php
+++ tine20/Calendar/Setup/Initialize.php
@@ -5,7 +5,7 @@
  * @package     Calendar
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Jonas Fischer <j.fischer@metaways.de>
- * @copyright   Copyright (c) 2008-2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2008-2011 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
  *
  */
@@ -24,12 +24,15 @@ class Calendar_Setup_Initialize extends Setup_Initialize
     {
         $pfe = new Tinebase_PersistentFilter_Backend_Sql();
         
-        $myEventsPFilter = $pfe->create(new Tinebase_Model_PersistentFilter(array(
-            'name'              => Calendar_Preference::DEFAULTPERSISTENTFILTER_NAME,
-            'description'       => "All events I attend", // _("All events I attend")
+        $commonValues = array(
             'account_id'        => NULL,
             'application_id'    => Tinebase_Application::getInstance()->getApplicationByName('Calendar')->getId(),
             'model'             => 'Calendar_Model_EventFilter',
+        );
+        
+        $myEventsPFilter = $pfe->create(new Tinebase_Model_PersistentFilter(array_merge($commonValues, array(
+            'name'              => Calendar_Preference::DEFAULTPERSISTENTFILTER_NAME,
+            'description'       => "All events I attend", // _("All events I attend")
             'filters'           => array(
                 array('field' => 'attender'    , 'operator' => 'equals', 'value' => array(
                     'user_type' => Calendar_Model_Attender::USERTYPE_USER,
@@ -39,6 +42,44 @@ class Calendar_Setup_Initialize extends Setup_Initialize
                     'DECLINED'
                 ))
             )
-        )));
+        ))));
+        
+        $pfe->create(new Tinebase_Model_PersistentFilter(array_merge($commonValues, array(
+            'name'              => "Awaiting response", //_("Awaiting response")
+            'description'       => "Events I have not yet responded to", // _("Events I have not yet responded to")
+            'filters'           => array(
+                array('field' => 'attender'    , 'operator' => 'equals', 'value' => array(
+                    'user_type' => Calendar_Model_Attender::USERTYPE_USER,
+                    'user_id'   => Addressbook_Model_Contact::CURRENTCONTACT,
+                )),
+                array('field' => 'attender_status'    , 'operator' => 'in', 'value' => array(
+                    Calendar_Model_Attender::STATUS_NEEDSACTION,
+                    Calendar_Model_Attender::STATUS_TENTATIVE,
+                    
+                )),
+            )
+        ))));
+        
+        $pfe->create(new Tinebase_Model_PersistentFilter(array_merge($commonValues, array(
+            'name'              => "Declined events", //_("Declined events")
+            'description'       => "Events I have declined", // _("Events I have declined")
+            'filters'           => array(
+                array('field' => 'attender'    , 'operator' => 'equals', 'value' => array(
+                    'user_type' => Calendar_Model_Attender::USERTYPE_USER,
+                    'user_id'   => Addressbook_Model_Contact::CURRENTCONTACT,
+                )),
+                array('field' => 'attender_status'    , 'operator' => 'in', 'value' => array(
+                    Calendar_Model_Attender::STATUS_DECLINED,
+                )),
+            )
+        ))));
+        
+        $pfe->create(new Tinebase_Model_PersistentFilter(array_merge($commonValues, array(
+            'name'              => "I'm organizer", //_("I'm organizer")
+            'description'       => "Events I'm the organizer of", // _("Events I'm the organizer of")
+            'filters'           => array(
+                array('field' => 'organizer', 'operator' => 'equals', 'value' => Addressbook_Model_Contact::CURRENTCONTACT)
+            )
+        ))));        
     }
 }
