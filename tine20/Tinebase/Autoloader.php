2afbb2a90da0cf9e8a954a390f20fb2dbd52df2e
Lars Kneschke <l.kneschke@metaways.de>
2011-01-07 18:28:05 +0000

    final implementation of Tine 2.0 autoloader
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18449 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Autoloader.php
+++ tine20/Tinebase/Autoloader.php
@@ -16,17 +16,34 @@
  */
 class Tinebase_Autoloader implements Zend_Loader_Autoloader_Interface
 {
+    /**
+     * (non-PHPdoc)
+     * @see Zend_Loader_Autoloader_Interface::autoload()
+     */
     public function autoload($class)
     {
-        if (($pos = strpos($class, '_')) !== false) {
-            $topLevelDirectory = substr($class, 0, $pos);
-        } else {
-            $topLevelDirectory = $class;
+        if (class_exists($class, false) || interface_exists($class, false)) {
+            return;
         }
         
-        Zend_Loader::loadClass($class, dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'library' . DIRECTORY_SEPARATOR . $topLevelDirectory);
+        $topLevelDirectory = (($pos = strpos($class, '_')) !== false) ? substr($class, 0, $pos) : $class;
+        
+        $file = $topLevelDirectory . DIRECTORY_SEPARATOR . str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
+        
+        $fullPath = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'library' . DIRECTORY_SEPARATOR . $file;
+        
+        include_once $fullPath;
+        
+        if (!class_exists($class, false) && !interface_exists($class, false)) {
+            throw new Zend_Exception("File \"$fullPath\" does not exist or class \"$class\" was not found in the file");
+        }
     }
     
+    /**
+     * initialize Tine 2.0 autoloader for different prefixes
+     * 
+     * @param Zend_Loader_Autoloader $_autoloader
+     */
     public static function initialize(Zend_Loader_Autoloader $_autoloader)
     {
         $_autoloader->unshiftAutoloader(new self(), array('Rediska', 'HTMLPurifier'));
