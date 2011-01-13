43f242fe95275cad6b6a9ef58707b9c44c9fe24a
Philipp Schüle <p.schuele@metaways.de>
2011-01-13 10:19:02 +0000

    throw specialized exception if put script fails
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18574 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Felamimail/Exception/SievePutScriptFail.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Felamimail
+ * @subpackage  Exception
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2011 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schüle <p.schuele@metaways.de>
+ * @version     $Id$
+ *
+ */
+
+/**
+ * Put Sieve Script Failed Exception
+ * 
+ * @package     Felamimail
+ * @subpackage  Exception
+ */
+class Felamimail_Exception_SievePutScriptFail extends Felamimail_Exception
+{
+    /**
+     * construct
+     * 
+     * @param string $_message
+     * @param integer $_code
+     * @return void
+     */
+    public function __construct($_message = 'Could not save script on Sieve server.', $_code = 931)
+    {
+        parent::__construct($_message, $_code);
+    }
+}
