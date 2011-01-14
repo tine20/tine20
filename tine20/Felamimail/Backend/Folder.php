4c7f64fb6d6bc7c364dde46b9946850aeb05f794
Philipp Schüle <p.schuele@metaways.de>
2011-01-14 16:12:50 +0000

    - catch exception and write trace and error to debug log 0003766: lock wait timeout when updating folder counts http://www.tine20.org/bugtracker/view.php?id=3766
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18617 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/Backend/Folder.php
+++ tine20/Felamimail/Backend/Folder.php
@@ -161,8 +161,9 @@ class Felamimail_Backend_Folder extends Tinebase_Backend_Sql_Abstract
         try {
             $this->_db->update($this->_tablePrefix . $this->_tableName, $data, $where);
         } catch (Zend_Db_Statement_Exception $zdse) {
-            if (Tinebase_Core::isLogLevel(Zend_Log::NOTICE)) Tinebase_Core::getLogger()->notice(__METHOD__ . '::' . __LINE__ . ' data: ' . print_r($data, TRUE) . ' where: ' . print_r($where, TRUE));
-            throw $zdse;
+            if (Tinebase_Core::isLogLevel(Zend_Log::WARN)) Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ . ' Could not update folder counts: ' . $zdse->getMessage());
+            if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' ' . $zdse->getTraceAsString());
+            if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' data: ' . print_r($data, TRUE) . ' where: ' . print_r($where, TRUE));
         }
     }
 }
