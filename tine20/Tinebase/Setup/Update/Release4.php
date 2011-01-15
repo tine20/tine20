756a73cdbe2fe7bc4a11bc82b1b65962503d19f8
Lars Kneschke <l.kneschke@metaways.de>
2011-01-15 07:40:26 +0000

    added missing index
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18634 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Tinebase/Setup/Update/Release4.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Tinebase
+ * @subpackage  Setup
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL3
+ * @copyright   Copyright (c) 2009-2011 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schüle <p.schuele@metaways.de>
+ * @version     $Id$
+ */
+
+/**
+ * Tinebase updates for version 4.x
+ *
+ * @package     Tinebase
+ * @subpackage  Setup
+ */
+class Tinebase_Setup_Update_Release4 extends Setup_Update_Abstract
+{    
+    /**
+     * update to 4.1
+     * - add value_search option field to customfield_config
+     */
+    public function update_0()
+    {
+        $declaration = new Setup_Backend_Schema_Index_Xml('
+            <index>
+                <name>contact_id</name>
+                <field>
+                    <name>contact_id</name>
+                </field>
+            </index>
+        ');
+        $this->_backend->addIndex('accounts', $declaration);
+        $this->setTableVersion('accounts', '7');
+        
+        $this->setApplicationVersion('Tinebase', '4.1');
+    }    
+}
