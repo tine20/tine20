9d58cd6480aac410726a454bcaecf8bdb77ee901
Philipp Schüle <p.schuele@metaways.de>
2011-01-17 12:06:12 +0000

    updated header
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18658 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Setup/js/ConfigManagerPanel.js
+++ tine20/Setup/js/ConfigManagerPanel.js
@@ -4,7 +4,7 @@
  * @package     Setup
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Cornelius Weiss <c.weiss@metaways.de>
- * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2009-2011 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
  *
  */
@@ -26,7 +26,6 @@ Ext.ns('Tine', 'Tine.Setup');
  * 
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Cornelius Weiss <c.weiss@metaways.de>
- * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
  * 
  * @param       {Object} config
