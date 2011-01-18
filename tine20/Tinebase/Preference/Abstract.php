1713ef83469d87b7639d39e4a85a37ec90a1a166
Cornelius Weiß <c.weiss@metaways.de>
2011-01-18 14:11:49 +0000

    RESOLVED #0003748: don't show '_default_' for personal only preferences
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18708 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Preference/Abstract.php
+++ tine20/Tinebase/Preference/Abstract.php
@@ -453,27 +453,25 @@ abstract class Tinebase_Preference_Abstract extends Tinebase_Backend_Sql_Abstrac
              $options = $this->_convertXmlOptionsToArray($_preference->options);
         }
         
-        if (! $_preference->personal_only) {
-            $translate = Tinebase_Translation::getTranslation($this->_application);
-            
-            // get default pref and add value to string
-            $default = $this->_getDefaultPreference($_preference->name); 
-            $defaultLabel = $translate->_('default');
-            // check if value is in options and use that label
-            $valueLabel = $default->value;
-            foreach ($options as $option) {
-                if ($default->value == $option[0]) {
-                    $valueLabel = $option[1];
-                    break;
-                }
+        // get default pref
+        $default = $this->_getDefaultPreference($_preference->name); 
+        
+        // check if value is in options and use that label
+        $valueLabel = $default->value;
+        foreach ($options as $option) {
+            if ($default->value == $option[0]) {
+                $valueLabel = $option[1];
+                break;
             }
-            // add default setting to the top of options
-            $defaultLabel .= ' (' . $valueLabel . ')';
-            array_unshift($options, array(
-                Tinebase_Model_Preference::DEFAULT_VALUE,
-                $defaultLabel,
-            ));
         }
+        // add default setting to the top of options
+        $defaultLabel = Tinebase_Translation::getTranslation('Tinebase')->_('default') . 
+                        ' (' . $valueLabel . ')';
+        
+        array_unshift($options, array(
+            Tinebase_Model_Preference::DEFAULT_VALUE,
+            $defaultLabel,
+        ));
         
         $_preference->options = $options;
     }
