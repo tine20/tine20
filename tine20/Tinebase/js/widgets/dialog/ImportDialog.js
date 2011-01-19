0048d97db35568d9ec881ba717a8565d995885ab
Philipp Schüle <p.schuele@metaways.de>
2011-01-19 10:20:10 +0000

    oops, forgot to remove the log
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/branches/2011-01@18741 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/widgets/dialog/ImportDialog.js
+++ tine20/Tinebase/js/widgets/dialog/ImportDialog.js
@@ -140,7 +140,6 @@ Tine.widgets.dialog.ImportDialog = Ext.extend(Tine.widgets.dialog.EditDialog, {
             
             if (this.sendRequest) {
                 this.loadMask.show();
-                console.log(this.record.get('files'));
                 
                 var params = {
                     method: this.appName + '.import' + this.record.get('model').getMeta('recordsName'),
