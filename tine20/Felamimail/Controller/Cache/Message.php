8a5c650040451deab97d06f2e6394ff03c602dc4
Philipp Schüle <p.schuele@metaways.de>
2011-01-27 10:30:25 +0000

    no need to return the folder because it gets updated anyway
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18880 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Felamimail/Controller/Cache/Message.php
+++ tine20/Felamimail/Controller/Cache/Message.php
@@ -546,7 +546,7 @@ class Felamimail_Controller_Cache_Message extends Felamimail_Controller_Message
                     
                     $messages = $_imap->getSummary($messageSequenceStart, $messageSequenceEnd, false);
 
-                    $_folder = $this->_addMessagesToCacheAndIncreaseCounters($messages, $_folder);
+                    $this->_addMessagesToCacheAndIncreaseCounters($messages, $_folder);
                     
                     $messageSequenceStart = $messageSequenceEnd + 1;
                     
@@ -594,7 +594,7 @@ class Felamimail_Controller_Cache_Message extends Felamimail_Controller_Message
         
         Tinebase_TransactionManager::getInstance()->commitTransaction($transactionId);
         
-        return Felamimail_Controller_Folder::getInstance()->updateFolderCounter($_folder, array(
+        Felamimail_Controller_Folder::getInstance()->updateFolderCounter($_folder, array(
             'cache_totalcount'  => "+$incrementMessagesCounter",
             'cache_unreadcount' => "+$incrementUnreadCounter",
         ));
