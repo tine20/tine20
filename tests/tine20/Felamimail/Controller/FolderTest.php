7245e54e123731efca1f598f7790f0db96d1b044
Philipp Schüle <p.schuele@metaways.de>
2011-01-27 10:32:43 +0000

    added testUpdateFolderCounter
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18883 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tests/tine20/Felamimail/Controller/FolderTest.php
+++ tests/tine20/Felamimail/Controller/FolderTest.php
@@ -4,8 +4,8 @@
  * 
  * @package     Felamimail
  * @license     http://www.gnu.org/licenses/agpl.html
- * @copyright   Copyright (c) 2009-2010 Metaways Infosystems GmbH (http://www.metaways.de)
- * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2009-2011 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schüle <p.schuele@metaways.de>
  * @version     $Id$
  */
 
@@ -13,10 +13,6 @@
  * Test helper
  */
 require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
-
-if (!defined('PHPUnit_MAIN_METHOD')) {
-    define('PHPUnit_MAIN_METHOD', 'Felamimail_Controller_FolderTest::main');
-}
 
 /**
  * Test class for Felamimail_Controller_Folder
@@ -89,16 +85,11 @@ class Felamimail_Controller_FolderTest extends PHPUnit_Framework_TestCase
 
     /**
      * get folders from the server
-     *
      */
     public function testGetFolders()
     {
-        $result = $this->_controller->search($this->_getFolderFilter(''));
+        $inboxFolder = $this->_getInbox();
         
-        $this->assertGreaterThan(0, count($result));
-        
-        // get inbox folder and do more checks
-        $inboxFolder = $result->filter('localname', 'INBOX')->getFirstRecord();
         $this->assertFalse($inboxFolder === NULL, 'inbox not found');
         $this->assertTrue(($inboxFolder->is_selectable == 1), 'should be selectable');
         $this->assertTrue(($inboxFolder->has_children == 0), 'has children');
@@ -111,8 +102,22 @@ class Felamimail_Controller_FolderTest extends PHPUnit_Framework_TestCase
     }
     
     /**
+     * returns inbox
+     * 
+     * @return Felamimail_Model_Folder
+     */
+    protected function _getInbox()
+    {
+        $result = $this->_controller->search($this->_getFolderFilter(''));
+        $this->assertGreaterThan(0, count($result));
+        
+        // get inbox folder and do more checks
+        $inboxFolder = $result->filter('localname', 'INBOX')->getFirstRecord();
+        return $inboxFolder;
+    }
+    
+    /**
      * create a mail folder on the server
-     *
      */
     public function testCreateFolder()
     {
@@ -140,7 +145,6 @@ class Felamimail_Controller_FolderTest extends PHPUnit_Framework_TestCase
 
     /**
      * rename mail folder on the server
-     *
      */
     public function testRenameFolder()
     {
@@ -162,7 +166,6 @@ class Felamimail_Controller_FolderTest extends PHPUnit_Framework_TestCase
     
     /**
      * rename mail folder on the server
-     *
      */
     public function testRenameFolderWithSubfolder()
     {
@@ -188,7 +191,6 @@ class Felamimail_Controller_FolderTest extends PHPUnit_Framework_TestCase
 
     /**
      * rename mail folder on the server and create a subfolder afterwards
-     *
      */
     public function testRenameFolderAndCreateSubfolder()
     {
@@ -202,6 +204,69 @@ class Felamimail_Controller_FolderTest extends PHPUnit_Framework_TestCase
         $subfolder = $this->_controller->create($this->_account->getId(), 'testsub', 'INBOX' . $this->_account->delimiter . 'test_renamed');
         
         $this->assertEquals('INBOX' . $this->_account->delimiter . 'test_renamed' . $this->_account->delimiter . 'testsub', $subfolder->globalname);
+    }
+    
+    /**
+     * test update folder counter
+     */
+    public function testUpdateFolderCounter()
+    {
+        $inbox = $this->_getInbox();
+
+        $this->_folderCountsTestHelper($inbox, array(
+            'cache_totalcount'  => 0,
+            'cache_recentcount' => 0,
+            'cache_unreadcount' => 0
+        ), array(
+            'cache_totalcount'  => 0,
+            'cache_unreadcount' => 0
+        ));
+        
+        $this->_folderCountsTestHelper($inbox, array(
+            'cache_totalcount'  => "+200",
+            'cache_unreadcount' => "+25",
+        ), array(
+            'cache_totalcount'  => 200,
+            'cache_unreadcount' => 25
+        ));
+        
+        $this->_folderCountsTestHelper($inbox, array(
+            'cache_totalcount'  => "-1",
+            'cache_unreadcount' => "22",
+        ), array(
+            'cache_totalcount'  => 199,
+            'cache_unreadcount' => 22
+        ));
+        
+        $this->_folderCountsTestHelper($inbox, array(
+            'cache_totalcount'  => "-100",
+            'cache_unreadcount' => "-30",
+        ), array(
+            'cache_totalcount'  => 99,
+            'cache_unreadcount' => 0
+        ));
+        
+        // reset
+        $updatedCounters = Felamimail_Controller_Cache_Folder::getInstance()->getCacheFolderCounter($inbox);
+        $this->assertEquals(0, $updatedCounters['cache_totalcount'], 'cache_totalcount does not match.');
+        $this->assertEquals(0, $updatedCounters['cache_unreadcount'], 'cache_unreadcount does not match.');
+    }
+    
+    /**
+     * folder counts test helper
+     * 
+     * @param Felamimail_Model_Folder $_folder
+     * @param array $_newCounters
+     * @param array $_expectedValues
+     */
+    protected function _folderCountsTestHelper($_folder, $_newCounters, $_expectedValues)
+    {
+        $updatedFolder = $this->_controller->updateFolderCounter($_folder, $_newCounters);
+        foreach ($_expectedValues as $key => $value) {
+            $this->assertEquals($value, $updatedFolder->{$key}, $key . ' does not match.');
+        }
+        $folderInDb = $this->_controller->get($_folder->getId());
+        $this->assertTrue($updatedFolder->toArray() == $folderInDb->toArray(), 'folder values do not match');
     }
     
     /**
