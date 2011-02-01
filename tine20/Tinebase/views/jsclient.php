bc8463d38d5e8e8767633d2632c162bc0d109f43
Lars Kneschke <l.kneschke@metaways.de>
2011-02-01 11:07:36 +0000

    add title to <a> tag
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@18968 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/views/jsclient.php
+++ tine20/Tinebase/views/jsclient.php
@@ -47,6 +47,6 @@
 </head>
 <body>
     Loading Tine 2.0 ...
-    <noscript><p>You need to enable javascript to use <a href="http://www.tine20.org">Tine 2.0</a></p></noscript>
+    <noscript><p>You need to enable javascript to use <a href="http://www.tine20.org/" title="online open source groupware and crm">Tine 2.0</a></p></noscript>
 </body>
 </html>
