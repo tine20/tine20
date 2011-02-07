13f783b4c79a32d3c7e67073c6eb08379098d106
Philipp Schüle <p.schuele@metaways.de>
2011-02-07 15:29:40 +0000

    added filemanager CLI
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/branches/2011-01@19089 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Filemanager/Frontend/Cli.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Tine 2.0
+ * @package     Filemanager
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schüle <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2011 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id: Cli.php 17308 2010-11-24 10:48:03Z p.schuele@metaways.de $
+ */
+
+/**
+ * Cli frontend for Filemanager
+ *
+ * This class handles cli requests for the Filemanager
+ *
+ * @package     Filemanager
+ */
+class Filemanager_Frontend_Cli extends Tinebase_Frontend_Cli_Abstract
+{
+    /**
+     * the internal name of the application
+     * 
+     * @var string
+     */
+    protected $_applicationName = 'Filemanager';
+}
