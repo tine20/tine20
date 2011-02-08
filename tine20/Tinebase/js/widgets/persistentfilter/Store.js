508d2142a65bf5b68184a5f83548ef08a72ef8ec
Philipp Schüle <p.schuele@metaways.de>
2011-02-08 10:46:47 +0000

    - fetch persistent filter from mainwindow if we are in popup 0003914: new / updated favorites are not reflected in recipient grid http://forge.tine20.org/mantisbt/view.php?id=3914
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@19104 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/widgets/persistentfilter/Store.js
+++ tine20/Tinebase/js/widgets/persistentfilter/Store.js
@@ -3,7 +3,7 @@
  * 
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Cornelius Weiss <c.weiss@metaways.de>
- * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2010-2011 Metaways Infosystems GmbH (http://www.metaways.de)
  * @version     $Id$
  */
 Ext.ns('Tine.widgets.persistentfilter.store');
@@ -38,18 +38,25 @@ Tine.widgets.persistentfilter.store.PersistentFilterStore = Ext.extend(Ext.data.
  */
 Tine.widgets.persistentfilter.store.getPersistentFilterStore = function() {
     if (! Tine.widgets.persistentfilter.store.persistentFilterStore) {
-        // create store
-        var s = Tine.widgets.persistentfilter.store.persistentFilterStore = new Tine.widgets.persistentfilter.store.PersistentFilterStore({
-            fields: Tine.widgets.persistentfilter.model.PersistentFilter.getFieldDefinitions(),
-            sortInfo: {field: 'name', direction: 'ASC'}
-        });
         
-        // populate store
-        var persistentFiltersData = Tine.Tinebase.registry.get("persistentFilters").results;
-        Ext.each(persistentFiltersData, function(data) {
-            var r = new Tine.widgets.persistentfilter.model.PersistentFilter(data);
-            s.addSorted(r);
-        }, this);
+        if (window.isMainWindow) {
+            // create store
+            var s = Tine.widgets.persistentfilter.store.persistentFilterStore = new Tine.widgets.persistentfilter.store.PersistentFilterStore({
+                fields: Tine.widgets.persistentfilter.model.PersistentFilter.getFieldDefinitions(),
+                sortInfo: {field: 'name', direction: 'ASC'}
+            });
+            
+            // populate store
+            var persistentFiltersData = Tine.Tinebase.registry.get("persistentFilters").results;
+            Ext.each(persistentFiltersData, function(data) {
+                var r = new Tine.widgets.persistentfilter.model.PersistentFilter(data);
+                s.addSorted(r);
+            }, this);
+        } else {
+            // TODO test this in IE!
+            var mainWindow = Ext.ux.PopupWindowMgr.getMainWindow();
+            Tine.widgets.persistentfilter.store.persistentFilterStore = mainWindow.Tine.widgets.persistentfilter.store.getPersistentFilterStore();
+        }
     }
     
     return Tine.widgets.persistentfilter.store.persistentFilterStore;
