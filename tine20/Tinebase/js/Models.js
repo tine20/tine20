7bf82e291f75bb53389acaa0a7e117e33d2b4ebd
Philipp Schüle <p.schuele@metaways.de>
2011-02-09 16:23:50 +0000

    - do not use record property 0003796: Changed items in the grid should be greyed-out if they do not fit the current filter/favorite after editing http://forge.tine20.org/mantisbt/view.php?id=3796
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/branches/2011-01@19157 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/js/Models.js
+++ tine20/Tinebase/js/Models.js
@@ -20,8 +20,7 @@ Tine.Tinebase.Model.genericFields = [
     { name: 'last_modified_by'},
     { name: 'is_deleted',         type: 'boolean'              },
     { name: 'deleted_time',       type: 'date', dateFormat: Date.patterns.ISO8601Long},
-    { name: 'deleted_by'},
-    { name: 'not_in_filter',      type: 'bool', defaultValue: false } // client only
+    { name: 'deleted_by'}
 ];
     
 /**
