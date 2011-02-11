6bfd24113aaf60a97fa133198501427bfcd35752
Lars Kneschke <l.kneschke@metaways.de>
2011-02-11 20:45:13 +0000

    add defer attribute to javascript script tag, to improve download performance
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/branches/2011-01@19215 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/views/jsclient.php
+++ tine20/Tinebase/views/jsclient.php
@@ -35,8 +35,8 @@
         }
      ?>
     
-    <script type="text/javascript" src="library/ExtJS/adapter/ext/ext-base.js"></script>
-    <script type="text/javascript" src="library/ExtJS/ext-all<?php echo TINE20_BUILDTYPE != 'RELEASE' ? '-debug' : '' ?>.js"></script>
+    <script defer type="text/javascript" src="library/ExtJS/adapter/ext/ext-base.js"></script>
+    <script defer type="text/javascript" src="library/ExtJS/ext-all<?php echo TINE20_BUILDTYPE != 'RELEASE' ? '-debug' : '' ?>.js"></script>
     
     <!--  not yet
         <link rel="stylesheet" type="text/css"  media="print" href="Tinebase/css/print.css" />
