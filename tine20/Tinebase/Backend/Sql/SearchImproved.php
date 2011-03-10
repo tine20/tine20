f4103890d19f1a2ab5218cb5b439b1a62d143486
Philipp Schüle <p.schuele@metaways.de>
2011-03-10 19:34:38 +0000

    use sort fn from pagination model
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@19326 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/Backend/Sql/SearchImproved.php
+++ tine20/Tinebase/Backend/Sql/SearchImproved.php
@@ -154,11 +154,7 @@ class Tinebase_Backend_Sql_SearchImproved extends Tinebase_Backend_Sql_Abstract
             // (2) get other columns and do joins
             $select = $this->_getSelect($_cols);
             $this->_addWhereIdIn($select, $ids);
-            if ($_pagination !== null && !empty($_pagination->sort)) {
-                foreach((array)$_pagination->sort as $sort) {
-                    $select->order($sort . (!empty($_pagination->dir) ? ' ' . $_pagination->dir : null));
-                }
-            }
+            $_pagination->appendSort($select);
             
             $rows = $this->_fetch($select, self::FETCH_ALL);
             
