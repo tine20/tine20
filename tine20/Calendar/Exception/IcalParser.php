909a2e9b7d89bb6c55ba88be6ef0cdec45f9a5f2
Cornelius Weiß <c.weiss@metaways.de>
2011-03-15 09:39:47 +0000

    RESOLVED #0004006: cli kalender import schlägt fehl
    - add parser exception
    
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/branches/2011-01@19374 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

---
+++ tine20/Calendar/Exception/IcalParser.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @package     Calendar
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2011 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @version     $Id$
+ */
+
+/**
+ * ical parser error
+ *
+ * @package Calendar
+ */
+class Calendar_Exception_IcalParser extends Exception
+{
+    /**
+     * @var Exception
+     */
+	protected $_parseError = NULL;
+	
+	/**
+     * construct
+     * 
+     * @param string $_message
+     * @param integer $_code
+     * @return void
+     */
+    public function __construct($_message = 'can not parse ical file => syntax errors ?', $_code = 970) {
+        parent::__construct($_message, $_code);
+    }
+    
+    /**
+     * set parse error
+     * 
+     * @param Exception $_e
+     */
+	public function setParseError(Exception $_e)
+	{
+	   $this->_parseError = $_e;
+	}
+	
+	/**
+	 * get parse error
+	 * 
+	 * @return Exception
+	 */
+	public function getFreeBusyInfo()
+	{
+	    return $this->_parseError;
+	}
+
+}
