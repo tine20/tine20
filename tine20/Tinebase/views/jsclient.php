3df100c8905eb6a89f648d59e4ac8cb6ef41ae58
Cornelius Weiß <c.weiss@metaways.de>
2011-03-21 09:42:38 +0000

    RESOLVED #0004110: Support IE 9
    git-svn-id: file:///www/metaways/svn.tine20.org/repo/trunk@19452 8d11cdf3-9d2f-0410-bbb5-e098f187f06b

--- tine20/Tinebase/views/jsclient.php
+++ tine20/Tinebase/views/jsclient.php
@@ -15,6 +15,7 @@
 <head>
     <title>Tine 2.0</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=8; IE=7" >
 
     <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
     <link rel="chrome-application-definition" href="chrome_web_app.json">
@@ -36,7 +37,7 @@
         }
      ?>
     
-    <script type="text/javascript" src="library/ExtJS/adapter/ext/ext-base.js"></script>
+    <script type="text/javascript" src="library/ExtJS/adapter/ext/ext-base<?php echo TINE20_BUILDTYPE != 'RELEASE' ? '-debug' : '' ?>.js"></script>
     <script type="text/javascript" src="library/ExtJS/ext-all<?php echo TINE20_BUILDTYPE != 'RELEASE' ? '-debug' : '' ?>.js"></script>
     
     <!--  not yet
