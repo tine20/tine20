ccf1923acf9566116289f60976130f58e788d4f1
Philipp Schüle <p.schuele@metaways.de>
2011-03-31 23:53:43 +0200

    allow leftOperand option that is not quoted

--- tine20/Tinebase/Model/Filter/Bool.php
+++ tine20/Tinebase/Model/Filter/Bool.php
@@ -5,9 +5,8 @@
  * @package     Tinebase
  * @subpackage  Filter
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
- * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2007-2011 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Cornelius Weiss <c.weiss@metaways.de>
- * @version     $Id$
  */
 
 /**
@@ -58,6 +57,8 @@ class Tinebase_Model_Filter_Bool extends Tinebase_Model_Filter_Abstract
                      $_select->orwhere($quotedField . $action['sqlop'], $value);
                  }
              }
+         } else if (! empty($this->_options['leftOperand'])) {
+             $_select->where($this->_options['leftOperand'] . $action['sqlop'], $value);
          } else {  
              $_select->where($this->_getQuotedFieldName($_backend) . $action['sqlop'], $value);
          }
