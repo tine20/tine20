3fdfb165244cbecfbf0a31d013ba13e6de084fb9
Philipp Schüle <p.schuele@metaways.de>
2011-04-05 12:45:34 +0200

    added new message controller stubs

---
+++ tine20/Felamimail/Controller/Message/Send.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Felamimail
+ * @subpackage  Controller
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schüle <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2011 Metaways Infosystems GmbH (http://www.metaways.de)
+ */
+
+/**
+ * message flags controller for Felamimail
+ *
+ * @package     Felamimail
+ * @subpackage  Controller
+ */
+class Felamimail_Controller_Message_Send extends Felamimail_Controller_Message
+{
+    /**
+     * holds the instance of the singleton
+     *
+     * @var Felamimail_Controller_Message_Send
+     */
+    private static $_instance = NULL;
+    
+    /**
+     * the constructor
+     *
+     * don't use the constructor. use the singleton
+     */
+    private function __construct() 
+    {
+        $this->_backend = new Felamimail_Backend_Cache_Sql_Message();
+        $this->_currentAccount = Tinebase_Core::getUser();
+    }
+    
+    /**
+     * don't clone. Use the singleton.
+     *
+     */
+    private function __clone() 
+    {        
+    }
+    
+    /**
+     * the singleton pattern
+     *
+     * @return Felamimail_Controller_Message_Send
+     */
+    public static function getInstance() 
+    {
+        if (self::$_instance === NULL) {            
+            self::$_instance = new Felamimail_Controller_Message_Send();
+        }
+        
+        return self::$_instance;
+    }
+}
