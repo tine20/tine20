d0fbae60518e75775bf86aa5c560eae0888d87b6
Philipp Schüle <p.schuele@metaways.de>
2011-04-14 14:49:18 +0200

    added stub of container controller in admin (#4212)

---
+++ tine20/Admin/Controller/Container.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Admin
+ * @subpackage  Controller
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schüle <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2011 Metaways Infosystems GmbH (http://www.metaways.de)
+ */
+
+/**
+ * Container Controller for Admin application
+ *
+ * @package     Admin
+ * @subpackage  Controller
+ */
+class Admin_Controller_Container extends Tinebase_Controller_Record_Abstract
+{
+    /**
+     * the constructor
+     *
+     * don't use the constructor. use the singleton 
+     */
+    private function __construct() 
+    {
+        $this->_currentAccount = Tinebase_Core::getUser();        
+        $this->_applicationName = 'Admin';
+		$this->_modelName = 'Tinebase_Model_Container';
+        
+        $this->_backend = new Tinebase_Backend_Sql(array(
+            'tableName' => 'container',
+            'modelName' => $this->_modelName,
+        ));
+    }
+
+    /**
+     * don't clone. Use the singleton.
+     *
+     */
+    private function __clone() 
+    {        
+    }
+
+    /**
+     * holds the instance of the singleton
+     *
+     * @var Admin_Controller_Container
+     */
+    private static $_instance = NULL;
+
+    /**
+     * the singleton pattern
+     *
+     * @return Admin_Controller_Container
+     */
+    public static function getInstance() 
+    {
+        if (self::$_instance === NULL) {
+            self::$_instance = new Admin_Controller_Container;
+        }
+        
+        return self::$_instance;
+    }
+}
