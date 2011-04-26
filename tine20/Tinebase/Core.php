3fc52ab35baae5b4a82545549cf328e8c29a19ab
Philipp Schüle <p.schuele@metaways.de>
2011-04-26 12:40:43 +0200

    disabled cache garbage collection (fixes #4246)

--- tine20/Tinebase/Core.php
+++ tine20/Tinebase/Core.php
@@ -467,6 +467,8 @@ class Tinebase_Core
      * setup the cache and add it to zend registry
      *
      * @param bool $_enabled disabled cache regardless what's configured in config.inc.php
+     * 
+     * @todo use the same config keys as Zend_Cache (backend + frontend) to simplify this
      */
     public static function setupCache($_enabled = true)
     {
@@ -478,7 +480,8 @@ class Tinebase_Core
                 'cache_id_prefix' => SQL_TABLE_PREFIX . 'CACHE_',
                 'lifetime' => ($config->caching->lifetime) ? $config->caching->lifetime : 7200,
                 'automatic_serialization' => true, // turn that off for more speed
-                'caching' => true
+                'caching'                   => true,
+                'automatic_cleaning_factor' => 0, // no garbage collection as this is done by a scheduler task
             );
 
             $backendType = ($config->caching->backend) ? ucfirst($config->caching->backend) : 'File';
