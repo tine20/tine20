92fcc0a7a7562c750bc0852a2e5fd958990564f2
Philipp Schüle <p.schuele@metaways.de>
2011-04-29 16:43:41 +0200

    only allow 'contains' operator for query filter (fixes #4256)

--- tine20/Addressbook/js/Model.js
+++ tine20/Addressbook/js/Model.js
@@ -141,7 +141,7 @@ Tine.Addressbook.Model.Contact.getFilterModel = function() {
         {label: app.i18n._('Job Title'),                                                field: 'title'},
         {label: app.i18n._('Job Role'),                                                 field: 'role'},
         {label: app.i18n._('Note'),                                                     field: 'note'},
-        {label: app.i18n._('E-Mail'),                                                   field: 'email_query'},
+        {label: app.i18n._('E-Mail'),                                                   field: 'email_query',        operators: ['contains']},
         {filtertype: 'tinebase.tag', app: app},
         //{label: app.i18n._('Birthday'),    field: 'bday', valueType: 'date'},
         {label: app.i18n._('Street') + ' (' + app.i18n._('Company Address') + ')',      field: 'adr_one_street',     defaultOperator: 'equals'},
