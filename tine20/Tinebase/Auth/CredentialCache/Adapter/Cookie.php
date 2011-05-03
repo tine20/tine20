a702407a33f2a9ab00819ff35a1e856832b62a56
Philipp Schüle <p.schuele@metaways.de>
2011-05-03 13:08:25 +0200

    added logging + id/key getters (#2546)

--- tine20/Tinebase/Auth/CredentialCache/Adapter/Cookie.php
+++ tine20/Tinebase/Auth/CredentialCache/Adapter/Cookie.php
@@ -10,7 +10,7 @@
  */
 
 /**
- * credential cache adapter interface
+ * credential cache adapter (cookie)
  *  
  * @package     Tinebase
  * @subpackage  Auth
@@ -33,6 +33,7 @@ class Tinebase_Auth_CredentialCache_Adapter_Cookie implements Tinebase_Auth_Cred
     {
         $cacheId = $_cache->getCacheId();
         setcookie(self::COOKIE_KEY, base64_encode(Zend_Json::encode($cacheId)));
+        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' Set credential cache cookie.');
     }
     
     /**
@@ -47,6 +48,8 @@ class Tinebase_Auth_CredentialCache_Adapter_Cookie implements Tinebase_Auth_Cred
             $cacheId = Zend_Json::decode(base64_decode($_COOKIE[self::COOKIE_KEY]));
             if (is_array($cacheId)) {
                 $result = new Tinebase_Model_CredentialCache($cacheId);
+            } else {
+                Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ . ' Something went wrong with the CredentialCache / could not get CC from cookie.');                
             }
         }
         
@@ -59,5 +62,26 @@ class Tinebase_Auth_CredentialCache_Adapter_Cookie implements Tinebase_Auth_Cred
     public function resetCache()
     {
         setcookie(self::COOKIE_KEY, '', time() - 3600);
+        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' Reset credential cache cookie.');
+    }
+    
+    /**
+     * getDefaultKey() - get default cache key
+     * 
+     * @return string
+     */
+    public function getDefaultKey()
+    {
+        return Tinebase_Record_Abstract::generateUID();
+    }
+
+    /**
+     * getDefaultId() - get default cache id
+     * 
+     * @return string
+     */
+    public function getDefaultId()
+    {
+        return Tinebase_Record_Abstract::generateUID();
     }
 }
