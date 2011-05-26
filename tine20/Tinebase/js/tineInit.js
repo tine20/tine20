75be0b9cb62803668740003856ba562658b8d280
Philipp Schüle <p.schuele@metaways.de>
2011-05-26 22:43:58 +0200

    removed BACKSPACE handling for the moment
    it prevented BACKSPACE in input fields, too :(

--- tine20/Tinebase/js/tineInit.js
+++ tine20/Tinebase/js/tineInit.js
@@ -99,9 +99,10 @@ Tine.Tinebase.tineInit = {
             if (e.ctrlKey && e.getKey() === e.A) {
                 // disable the native 'select all'
                 e.preventDefault();
-            } else if (e.getKey() === e.BACKSPACE) {
-                // disable the native 'history back'
-                e.preventDefault();
+                // TODO only disable BACKSPACE if no input field is focussed / no other components handle this event
+//            } else if (e.getKey() === e.BACKSPACE) {
+//                // disable the native 'history back'
+//                e.preventDefault();
             } else if (!window.isMainWindow && e.ctrlKey && e.getKey() === e.T) {
                 // disable the native 'new tab' if in popup window
                 e.preventDefault();
