7044417141d12b5cbf401dfad06da7adc1d27c81
Philipp Schüle <p.schuele@metaways.de>
2011-06-03 12:20:17 +0200

    get drafts & templates folder names from account

--- tine20/Felamimail/js/TreeLoader.js
+++ tine20/Felamimail/js/TreeLoader.js
@@ -90,25 +90,25 @@ Tine.Felamimail.TreeLoader = Ext.extend(Tine.widgets.tree.Loader, {
         
         // show standard folders icons 
         if (account) {
-            if (account.get('trash_folder') == attr.globalname) {
+            if (account.get('trash_folder') === attr.globalname) {
                 if (attr.cache_totalcount > 0) {
                     attr.cls = 'felamimail-node-trash-full';
                 } else {
                     attr.cls = 'felamimail-node-trash';
                 }
             }
-            if (account.get('sent_folder') == attr.globalname) {
+            if (account.get('sent_folder') === attr.globalname) {
                 attr.cls = 'felamimail-node-sent';
+            }
+            if (account.get('drafts_folder') === attr.globalname) {
+                attr.cls = 'felamimail-node-drafts';
+            }
+            if (account.get('templates_folder') === attr.globalname) {
+                attr.cls = 'felamimail-node-templates';
             }
         }
         if ('INBOX' == attr.globalname) {
             attr.cls = 'felamimail-node-inbox';
-        }
-        if ('Drafts' == attr.globalname) {
-            attr.cls = 'felamimail-node-drafts';
-        }
-        if ('Templates' == attr.globalname) {
-            attr.cls = 'felamimail-node-templates';
         }
         if ('Junk' == attr.globalname) {
             attr.cls = 'felamimail-node-junk';
