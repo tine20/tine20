73749a0b164bf6bad7acc94633b91b8d5c35d94f
Lars Kneschke <l.kneschke@metaways.de>
2011-06-06 11:22:36 +0200

    test commit

--- tine20/tine20.php
+++ tine20/tine20.php
@@ -7,6 +7,7 @@
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Philipp Schuele <p.schuele@metaways.de>
  * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * 
  */
 
 if (php_sapi_name() != 'cli') {
