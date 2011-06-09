5656e9e56b830268b2c110318a61a24f0b46ee27
mjatho <mjatho@lapmjatho.hh.metaways.de>
2011-06-09 15:24:23 +0200

    filemanager
    Change-Id: I24db37fbe7767e5845a69eee02420cd7dbd6550d

---
+++ tine20/Filemanager/js/Filemanager.js
@@ -0,0 +1,62 @@
+/*
+ * Tine 2.0
+ * 
+ * @package     Tinebase
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schüle <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ */
+ 
+Ext.ns('Tine.Filemanager');
+
+/**
+ * @namespace   Tine.Filemanager
+ * @class       Tine.Filemanager.Application
+ * @extends     Tine.Tinebase.Application
+ * 
+ * @author      Philipp Schüle <p.schuele@metaways.de>
+ */
+Tine.Filemanager.Application = Ext.extend(Tine.Tinebase.Application, {
+    
+    hasMainScreen: true,
+    
+    /**
+     * Get translated application title of this application /test
+     * 
+     * @return {String}
+     */
+    getTitle: function() {
+        return this.i18n.gettext('Filemanager');
+    }
+});
+
+/**
+ * @namespace   Tine.Filemanager
+ * @class       Tine.Filemanager.MainScreen
+ * @extends     Tine.widgets.MainScreen
+ * 
+ * @author      Martin Jatho <m.jatho@metaways.de>
+ */
+Tine.Filemanager.MainScreen = Ext.extend(Tine.widgets.MainScreen, {
+    
+    activeContentType: 'File'
+});
+
+    
+/**
+ * @namespace   Tine.Filemanager
+ * @class       Tine.Filemanager.TreePanel
+ * @extends     Tine.widgets.container.TreePanel
+ * 
+ * @author      Martin Jatho <m.jatho@metaways.de>
+ */
+Tine.Filemanager.TreePanel = Ext.extend(Tine.widgets.container.TreePanel, {
+    id: 'Filemanager_Tree',
+    filterMode: 'filterToolbar',
+    recordClass: Tine.Filemanager.Model.File,
+    plugins: [{
+        ptype: 'ux.browseplugin',
+        multiple: true,
+        handler: function() {alert("tree drop");}
+    }]
+});
