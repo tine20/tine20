e58af3d491b8aa6f1936803aaef873bdc9a92e46
Philipp Schüle <p.schuele@metaways.de>
2011-06-09 14:58:59 +0200

    added node controller stub (#4514)

---
+++ tine20/Filemanager/Controller/Node.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Filemanager
+ * @subpackage  Controller
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schüle <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2011 Metaways Infosystems GmbH (http://www.metaways.de)
+ */
+
+/**
+ * Node controller for Filemanager
+ *
+ * @package     Filemanager
+ * @subpackage  Controller
+ */
+class Filemanager_Controller_Node extends Tinebase_Controller_Abstract
+{
+    /**
+     * application name (is needed in checkRight())
+     *
+     * @var string
+     */
+    protected $_applicationName = 'Filemanager';
+    
+    /**
+     * Filesystem backend
+     *
+     * @var Tinebase_FileSystem
+     */
+    protected $_backend = NULL;
+    
+    /**
+     * holds the instance of the singleton
+     *
+     * @var Filemanager_Controller_Node
+     */
+    private static $_instance = NULL;
+    
+    /**
+     * the constructor
+     *
+     * don't use the constructor. use the singleton
+     */
+    private function __construct() {
+        $this->_currentAccount = Tinebase_Core::getUser();
+        $this->_backend = Tinebase_FileSystem::getInstance();
+    }
+    
+    /**
+     * don't clone. Use the singleton.
+     *
+     */
+    private function __clone() 
+    {        
+    }
+    
+    /**
+     * the singleton pattern
+     *
+     * @return Filemanager_Controller_Node
+     */
+    public static function getInstance() 
+    {
+        if (self::$_instance === NULL) {
+            self::$_instance = new Filemanager_Controller_Node();
+        }
+        
+        return self::$_instance;
+    }
+}
