4d6af2a8a3a1c3f240133610288f7102f8957d8a
Philipp Schüle <p.schuele@metaways.de>
2011-06-09 17:23:36 +0200

    added test stub for Filemanager_Frontend_Json (#4512)

---
+++ tests/tine20/Filemanager/Frontend/JsonTest.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ * 
+ * @package     Filemanager
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2011 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schüle <p.schuele@metaways.de>
+ */
+
+/**
+ * Test helper
+ */
+require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+/**
+ * Test class for Filemanager_Frontend_Json
+ * 
+ * @package     Filemanager
+ */
+class Filemanager_Frontend_JsonTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var array test objects
+     */
+    protected $_objects = array();
+    
+    /**
+     * Tree
+     *
+     * @var Filemanager_Frontend_Json
+     */
+    protected $_json;
+    
+    /**
+     * Runs the test methods of this class.
+     *
+     * @access public
+     * @static
+     */
+    public static function main()
+    {
+		$suite  = new PHPUnit_Framework_TestSuite('Tine 2.0 webdav tree tests');
+        PHPUnit_TextUI_TestRunner::run($suite);
+	}
+
+    /**
+     * Sets up the fixture.
+     * This method is called before a test is executed.
+     *
+     * @access protected
+     */
+    protected function setUp()
+    {
+    }
+
+    /**
+     * Tears down the fixture
+     * This method is called after a test is executed.
+     *
+     * @access protected
+     */
+    protected function tearDown()
+    {
+    }
+}		
