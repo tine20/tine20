44728804a1fa1b516b22cca4bf7691de8cb6836f
Philipp Schüle <p.schuele@metaways.de>
2011-06-29 20:03:50 +0200

    added filter acl check and some helper functions (#4528)

--- tine20/Filemanager/Controller/Node.php
+++ tine20/Filemanager/Controller/Node.php
@@ -80,14 +80,157 @@ class Filemanager_Controller_Node extends Tinebase_Controller_Abstract implement
      * @param bool $_onlyIds
      * @param string|optional $_action
      * @return Tinebase_Record_RecordSet of Tinebase_Model_Tree_Node
-     * 
-     * @todo make sure a parent_id filter is set
      */
     public function search(Tinebase_Model_Filter_FilterGroup $_filter = NULL, Tinebase_Record_Interface $_pagination = NULL, $_getRelations = FALSE, $_onlyIds = FALSE, $_action = 'get')
     {
+        $this->_checkFilterACL($_filter, 'get');
+        
         $result = $this->_backend->searchNodes($_filter, $_pagination);
         return $result;
     }
+    
+    /**
+     * checks filter acl and adds base path
+     * 
+     * @param Tinebase_Model_Filter_FilterGroup $_filter
+     * @param string $_action get|update
+     * @throws Tinebase_Exception_AccessDenied
+     */
+    protected function _checkFilterACL(Tinebase_Model_Filter_FilterGroup $_filter, $_action = 'get')
+    {
+        if ($_filter === NULL) {
+            $_filter = new Tinebase_Model_Tree_NodeFilter();
+        }
+        
+        $pathFilters = $_filter->getFilter('path', TRUE);
+        
+        if (! $pathFilters) {
+            // force a standard path filter as ACL filter
+            $pathFilters = array($_filter->createFilter('path', 'equals', '/personal/' . $this->_currentAccount->accountLoginName));
+            $_filter->addFilter($pathFilters[0]);
+        }
+        
+        // add base path and check grants
+        foreach ($pathFilters as $key => $pathFilter) {
+            $hasGrant = $this->_checkACLContainer($pathFilter->getValue());
+            if (! $hasGrant) {
+                unset($pathFilters[$key]);
+            }
+        }
+
+        if (empty($pathFilters)) {
+            throw new Tinebase_Exception_AccessDenied('Access denied.');
+        }
+    }
+    
+    /**
+     * add base path
+     * 
+     * @param Tinebase_Model_Tree_NodePathFilter $_pathFilter
+     * @return string
+     * 
+     * @todo use this
+     */
+    protected function _addBasePath($_path)
+    {
+        $application = Tinebase_Application::getInstance()->getApplicationByName($this->_applicationName);
+        $basePath = 'tine20:///' . $application->getId() . '/folders';
+        
+        return $basePath . $_path;
+    }
+    
+    /**
+     * check if user has the permissions for the container
+     * 
+     * @param string $_path
+     * @param string $_action get|update|...
+     * @return boolean
+     */
+    protected function _checkACLContainer($_path, $_action = 'get')
+    {
+        $container = $this->getContainer($_path);
+        
+        if (! $container) {
+            return FALSE;
+        }
+        
+        if (Tinebase_Container::getInstance()->hasGrant($this->_currentAccount, $container, Tinebase_Model_Grants::GRANT_ADMIN)) {
+            return TRUE;
+        }
+        
+        switch ($_action) {
+            case 'get':
+                $requiredGrant = Tinebase_Model_Grants::GRANT_READ;
+                break;
+            case 'update':
+                $requiredGrant = Tinebase_Model_Grants::GRANT_EDIT;
+                break;
+            default:
+                throw new Tinebase_Exception_UnexpectedValue('Unknown action: ' . $_action);
+        }
+        
+        return Tinebase_Container::getInstance()->hasGrant($this->_currentAccount, $container, $requiredGrant);
+    }
+    
+    /**
+     * get container from path
+     * 
+     * path can be: 
+     *   /shared/containername(/*)
+     *   /personal/username/containername(/*)
+     * 
+     * @param string $_path
+     * @return Tinebase_Model_Container
+     * 
+     * @todo replace getContainerByName!
+     */
+    public function getContainer($_path)
+    {
+        // split path into parts
+        $pathParts = explode('/', trim($_path, '/'), 4);
+        
+        Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' PATH PARTS: ' . print_r($pathParts, true));
+        
+        $container = NULL;
+        
+        if (!empty($pathParts[0])) {
+            $containerType          = strtolower($pathParts[0]);
+            
+            switch($containerType) {
+                case Tinebase_Model_Container::TYPE_SHARED:
+                    if (!empty($pathParts[1])) {
+                        // @todo replace getContainerByName!
+                        $container = Tinebase_Container::getInstance()->getContainerByName($this->_applicationName, $pathParts[2], $containerType);
+                    }
+                    
+                    break;
+                    
+                case Tinebase_Model_Container::TYPE_PERSONAL:
+                    if (!empty($pathParts[1])) {
+                        if ($pathParts[1] != Tinebase_Core::getUser()->accountLoginName) {
+                            throw new Tinebase_Exception_NotFound('Invalid user name: ' . $pathParts[1] . '.');
+                        }
+                        
+                        if (!empty($pathParts[2])) {
+                            // explode again
+                            $subPathParts = explode('/', $pathParts[2], 2);
+                            // @todo replace getContainerByName!
+                            $container = Tinebase_Container::getInstance()->getContainerByName($this->_applicationName, $subPathParts[0], $containerType);
+                        }
+                    } else {
+                        throw new Tinebase_Exception_NotFound('User name missing.');
+                    }
+                    break;
+                    
+                default:
+                    throw new Tinebase_Exception_NotFound('Invalid path');
+                    break;
+            }
+        }
+        
+        return $container;
+    }
+    
 
     /**
      * Gets total count of search with $_filter
