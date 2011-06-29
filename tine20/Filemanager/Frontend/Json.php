5de291ee623c9a30ed8f0c894555366f5f47ded6
Philipp Schüle <p.schuele@metaways.de>
2011-06-29 18:44:36 +0200

    removed special handling for container filter (#4528)

--- tine20/Filemanager/Frontend/Json.php
+++ tine20/Filemanager/Frontend/Json.php
@@ -38,42 +38,7 @@ class Filemanager_Frontend_Json extends Tinebase_Frontend_Json_Abstract
      */
     public function searchNodes($filter, $paging)
     {
-        $nodeFilter = $this->_convertContainerToParentIdFilter($filter);
         $result = $this->_search($nodeFilter, $paging, Filemanager_Controller_Node::getInstance(), 'Tinebase_Model_Tree_NodeFilter', FALSE, self::TOTALCOUNT_COUNTRESULT);
-        
-        return $result;
-    }
-    
-    /**
-     * convert generic container filter (with path) to node filter (with parent id)
-     * 
-     * @param array $_filter
-     * @return array
-     */
-    protected function _convertContainerToParentIdFilter($_filter)
-    {
-        $basePath = Tinebase_FileSystem::getInstance()->getApplicationBasePath(Tinebase_Application::getInstance()->getApplicationByName('Filemanager'));
-        
-        $result = array();
-        foreach ($_filter as $filterData) {
-            if ($filterData['field'] === 'container_id') {
-                $parentIdFilter = array('field' => 'parent_id', 'operator' => 'in');
-                $filterData['field'] = 'parent_id';
-                $parentIds = array();
-                foreach ((array) $filterData['value'] as $value) {
-                    if (array_key_exists('path', $value)) {
-                        $path = $basePath . $value['path'];
-                        $node = Tinebase_FileSystem::getInstance()->stat($path);
-                        $parentIds[] = $node->getId();
-                    }
-                }
-                $parentIdFilter['value'] = $parentIds;
-                $result[] = $parentIdFilter;
-                
-            } else {
-                $result[] = $filterData;
-            }
-        }
         
         return $result;
     }
