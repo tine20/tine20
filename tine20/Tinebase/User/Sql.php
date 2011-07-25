24cd72f321bf7acd29697a8c97f12e1a1c74c910
Lars Kneschke <l.kneschke@metaways.de>
2011-07-25 09:12:46 +0200

    convert IF THEN (MySQL specific) to CASE WHEN

--- tine20/Tinebase/User/Sql.php
+++ tine20/Tinebase/User/Sql.php
@@ -250,14 +250,14 @@ class Tinebase_User_Sql extends Tinebase_User_Abstract
     protected function _getUserSelectObject()
     {
         /*
-         * IF (`status` = 'enabled', (CASE WHEN NOW() > `expires_at` THEN 'expired' 
+         * CASE WHEN `status` = 'enabled' THEN (CASE WHEN NOW() > `expires_at` THEN 'expired' 
          * WHEN (`login_failures` > 5 AND `last_login_failure_at` + INTERVAL 15 MINUTE > NOW()) 
-         * THEN 'blocked' ELSE 'enabled' END), 'disabled')
+         * THEN 'blocked' ELSE 'enabled' END) ELSE 'disabled' END
          */
-        $statusSQL = 'IF (' . $this->_db->quoteIdentifier($this->rowNameMapping['accountStatus']) . ' = ' . $this->_db->quote('enabled') . ', (CASE WHEN NOW() > ' . $this->_db->quoteIdentifier($this->rowNameMapping['accountExpires']) . ' THEN ' . $this->_db->quote('expired') . 
+        $statusSQL = 'CASE WHEN ' . $this->_db->quoteIdentifier($this->rowNameMapping['accountStatus']) . ' = ' . $this->_db->quote('enabled') . ' THEN (CASE WHEN NOW() > ' . $this->_db->quoteIdentifier($this->rowNameMapping['accountExpires']) . ' THEN ' . $this->_db->quote('expired') . 
             ' WHEN (' . $this->_db->quoteIdentifier($this->rowNameMapping['loginFailures']) . " > {$this->_maxLoginFailures} AND " . 
                 $this->_db->quoteIdentifier($this->rowNameMapping['lastLoginFailure']) . " + INTERVAL '{$this->_blockTime}' MINUTE > NOW()) THEN 'blocked'" . 
-            ' ELSE ' . $this->_db->quote('enabled') . ' END), ' . $this->_db->quote('disabled') . ')';
+            ' ELSE ' . $this->_db->quote('enabled') . ' END) ELSE ' . $this->_db->quote('disabled') . ' END';
         
         $select = $this->_db->select()
             ->from(SQL_TABLE_PREFIX . 'accounts', 
