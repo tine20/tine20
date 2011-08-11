f99e473d005b10b1d77c8833a95b8ade4686dc15
Philipp Schüle <p.schuele@metaways.de>
2011-08-11 17:18:44 +0200

    removed obsolete code

--- tine20/Felamimail/Model/Account.php
+++ tine20/Felamimail/Model/Account.php
@@ -244,21 +244,6 @@ class Felamimail_Model_Account extends Tinebase_Record_Abstract
             $result['ssl'] = strtoupper($this->ssl);
         }
         
-        // overwrite settings with config.inc.php values if set
-        if (Tinebase_Core::getConfig()->imap) {
-            $imapConfig = Tinebase_Core::getConfig()->imap->toArray();
-            $imapConfigOverwriteFields = array('host', 'port', 'secure_connection');
-            foreach ($imapConfigOverwriteFields as $field) {
-                if (array_key_exists($field, $imapConfig)) {
-                    if ($field == 'secure_connection' && in_array($imapConfig[$field], array('ssl', 'tls'))) {
-                        $result['ssl'] = strtoupper($imapConfig[$field]);
-                    } else {
-                        $result[$field] = $imapConfig[$field];
-                    }
-                }
-            }
-        }
-        
         //if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' ' . print_r($result, true));
         
         return $result;
