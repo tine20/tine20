371e2937e28529b7a712670846de48ee6f8ba514
Philipp Schüle <p.schuele@metaways.de>
2011-09-05 12:41:14 +0200

    added project status key field (init + config) (#4704)

--- tine20/Projects/Setup/Initialize.php
+++ tine20/Projects/Setup/Initialize.php
@@ -16,4 +16,30 @@
  */
 class Projects_Setup_Initialize extends Setup_Initialize
 {
+    /**
+     * init key fields
+     */
+    protected function _initializeKeyFields()
+    {
+        // create status config
+        $cb = new Tinebase_Backend_Sql(array(
+            'modelName' => 'Tinebase_Model_Config', 
+            'tableName' => 'config',
+        ));
+        
+        $projectsStatusConfig = array(
+            'name'    => Projects_Config::PROJECT_STATUS,
+            'records' => array(
+                array('id' => 'COMPLETED',    'value' => 'Completed',   'is_open' => 0, 'icon' => 'images/oxygen/16x16/actions/ok.png',                   'system' => true), //_('Completed')
+                array('id' => 'CANCELLED',    'value' => 'Cancelled',   'is_open' => 0, 'icon' => 'images/oxygen/16x16/actions/dialog-cancel.png',        'system' => true), //_('Cancelled')
+                array('id' => 'IN-PROCESS',   'value' => 'In process',  'is_open' => 1, 'icon' => 'images/oxygen/16x16/actions/view-refresh.png',         'system' => true), //_('In process')
+            ),
+        );
+        
+        $cb->create(new Tinebase_Model_Config(array(
+            'application_id'    => Tinebase_Application::getInstance()->getApplicationByName('Projects')->getId(),
+            'name'              => Projects_Config::PROJECT_STATUS,
+            'value'             => json_encode($projectsStatusConfig),
+        )));
+    }
 }
