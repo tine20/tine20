1d0dd6426fbf8493257b05f748a9d77e2f8723bc
Philipp Schüle <p.schuele@metaways.de>
2011-09-06 12:50:57 +0200

    added column model, renderer and record add listener (#4714)

--- tine20/Tinebase/js/widgets/grid/LinkGridPanel.js
+++ tine20/Tinebase/js/widgets/grid/LinkGridPanel.js
@@ -4,8 +4,6 @@
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Philipp Schüle <p.schuele@metaways.de>
  * @copyright   Copyright (c) 2011 Metaways Infosystems GmbH (http://www.metaways.de)
- * 
- * TODO         add relations stuff
  */
 Ext.ns('Tine.widgets.grid');
 
@@ -28,11 +26,64 @@ Ext.ns('Tine.widgets.grid');
  * Create a new Tine.widgets.grid.LinkGridPanel
  */
 Tine.widgets.grid.LinkGridPanel = Ext.extend(Tine.widgets.grid.PickerGridPanel, {
+    
+    record: null,
+    recordClass: Tine.Tinebase.Model.Relation,
+    
+    // TODO allow to configure allowed types
+    
     /**
      * @private
      */
     initComponent: function() {
+        this.searchRecordClass = this.searchRecordClass || this.recordClass;
+        
         Tine.widgets.grid.LinkGridPanel.superclass.initComponent.call(this);
+    },
+    
+    /**
+     * @return Ext.grid.ColumnModel
+     * @private
+     */
+    getColumnModel: function () {
+        return new Ext.grid.ColumnModel({
+            defaults: {
+                sortable: true
+            },
+            columns:  [
+                {id: 'name', header: _('Name'), dataIndex: 'related_record', renderer: this.relatedRecordRender, scope: this}
+                // TODO add type chooser combo
+            ].concat(this.configColumns)
+        });
+    },
+    
+    /**
+     * related record renderer
+     * 
+     * @param {Record} value
+     * @return {String}
+     */
+    relatedRecordRender: function(value) {
+        return Ext.util.Format.htmlEncode(value.get(this.searchRecordClass.getMeta('titleProperty')));
+    },
+    
+    /**
+     * @param {Record} recordToAdd
+     */
+    onAddRecordFromCombo: function(recordToAdd) {
+        // TODO add more relation fields
+        var record = new Tine.Tinebase.Model.Relation({
+            related_record: new this.newRecordClass(recordToAdd.data, recordToAdd.id),
+            related_id: recordToAdd.id,
+            own_id: (this.record) ? this.record.id : null
+        });
+        // check if already in
+        if (this.recordStore.findExact('related_id', recordToAdd.id) === -1) {
+            this.recordStore.add([record]);
+        }
+        this.collapse();
+        this.clearValue();
+        this.reset();
     }
 });
 
