9c5fd3bbfdd648a8256467d8b0dad0317b5b45a9
Philipp Schüle <p.schuele@metaways.de>
2011-09-06 16:06:48 +0200

    allow relation types in keyfields (#4714)

--- tine20/Tinebase/js/widgets/grid/LinkGridPanel.js
+++ tine20/Tinebase/js/widgets/grid/LinkGridPanel.js
@@ -43,6 +43,12 @@ Tine.widgets.grid.LinkGridPanel = Ext.extend(Tine.widgets.grid.PickerGridPanel, 
     record: null,
     
     /**
+     * relation types can be stored in keyfield
+     * @type 
+     */
+    relationTypesKeyfieldName: null,
+    
+    /**
      * relation types for combobox in editor grid
      * @type Array
      */
@@ -64,6 +70,29 @@ Tine.widgets.grid.LinkGridPanel = Ext.extend(Tine.widgets.grid.PickerGridPanel, 
      * @private
      */
     getColumnModel: function () {
+        
+        this.typeEditor = (this.relationTypesKeyfieldName) 
+            ? new Tine.Tinebase.widgets.keyfield.ComboBox({
+                app: this.app.appName,
+                keyFieldName: this.relationTypesKeyfieldName
+            }) 
+            : (this.relationTypes) 
+                ? new Ext.form.ComboBox({
+                    displayField: 'label',
+                    valueField: 'relation_type',
+                    mode: 'local',
+                    triggerAction: 'all',
+                    lazyInit: false,
+                    autoExpand: true,
+                    blurOnSelect: true,
+                    listClass: 'x-combo-list-small',
+                    store: new Ext.data.SimpleStore({
+                        fields: ['label', 'relation_type'],
+                        data: this.relationTypes
+                    })
+                }) 
+                : null;
+        
         return new Ext.grid.ColumnModel({
             defaults: {
                 sortable: true
@@ -75,22 +104,11 @@ Tine.widgets.grid.LinkGridPanel = Ext.extend(Tine.widgets.grid.PickerGridPanel, 
                     dataIndex: 'type', 
                     width: 100, 
                     sortable: true,
-                    renderer: this.relationTypeRenderer,
+                    renderer: (this.relationTypesKeyfieldName) 
+                        ? Tine.Tinebase.widgets.keyfield.Renderer.get(this.app.appName, this.relationTypesKeyfieldName) 
+                        : this.relationTypeRenderer,
                     scope: this,
-                    editor: new Ext.form.ComboBox({
-                        displayField: 'label',
-                        valueField: 'relation_type',
-                        mode: 'local',
-                        triggerAction: 'all',
-                        lazyInit: false,
-                        autoExpand: true,
-                        blurOnSelect: true,
-                        listClass: 'x-combo-list-small',
-                        store: new Ext.data.SimpleStore({
-                            fields: ['label', 'relation_type'],
-                            data: this.relationTypes
-                        })
-                    })
+                    editor: this.typeEditor
                 }
             ].concat(this.configColumns)
         });
@@ -164,15 +182,22 @@ Tine.widgets.grid.LinkGridPanel = Ext.extend(Tine.widgets.grid.PickerGridPanel, 
 
         Tine.log.debug('Loading relations into store...');
         if (record.get('relations') && record.get('relations').length > 0) {
+            var relationRecords = [];
             Ext.each(record.get('relations'), function(relation) {
                 relation.related_record = new this.searchRecordClass(
                     relation.related_record, 
                     relation.related_id
                 );
-                var relationRecord = new Tine.Tinebase.Model.Relation(relation, relation.id);
-                this.store.add([relationRecord]);
+                relationRecords.push(new Tine.Tinebase.Model.Relation(relation, relation.id));
+                
             }, this);
+            this.store.add(relationRecords);
         }
+        
+        // activate highlighting after initial load
+        (function() {
+            this.highlightRowAfterAdd = true;
+        }).defer(400, this);
         
         // TODO perhaps we should filter all that do not match the model
     },
