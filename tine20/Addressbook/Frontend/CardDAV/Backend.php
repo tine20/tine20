3fa66606ef9b08bd823f0b4430d22df61c9a4683
Lars Kneschke <l.kneschke@metaways.de>
2011-09-16 10:19:40 +0200

    bitbucket: this is the first implementation of the carddav backend will be deleted later

---
+++ tine20/Addressbook/Frontend/CardDAV/Backend.php
@@ -0,0 +1,410 @@
+<?php
+/**
+ * Tine 2.0
+ * @package     Calendar
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ */
+
+/**
+ * @package     Calendar
+ */
+class Addressbook_Frontend_CardDAV_Backend extends Sabre_CardDAV_Backend_Abstract
+{
+    /**
+     * Returns the list of addressbooks for a specific user.
+     *
+     * Every addressbook should have the following properties:
+     *   id - an arbitrary unique id
+     *   uri - the 'basename' part of the url
+     *   principaluri - Same as the passed parameter
+     *
+     * Any additional clark-notation property may be passed besides this. Some 
+     * common ones are :
+     *   {DAV:}displayname
+     *   {urn:ietf:params:xml:ns:carddav}addressbook-description
+     *   {http://calendarserver.org/ns/}getctag
+     * 
+     * @param string $principalUri 
+     * @return array 
+     */
+    function getAddressBooksForUser($principalUri)
+    {
+        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' $principalUri: ' . $principalUri);
+        
+        $principalParts = explode('/', $principalUri);
+        if (count($principalParts) == 2) {
+            $owner = Tinebase_User::getInstance()->getUserByLoginName($principalParts[1]);
+            $containers = Tinebase_Container::getInstance()->getPersonalContainer(Tinebase_Core::getUser(), 'Addressbook', $owner, Tinebase_Model_Grants::GRANT_READ);
+        } else {
+            throw new Sabre_DAV_Exception_PreconditionFailed('unsupported pricipalUri');
+        }
+        //if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' $conatiners: ' . print_r($containers->toArray(), true));
+        $addressbooks = array();
+        
+        foreach($containers as $container) {
+            $containerId = $container->getId();
+            
+            $addressbooks[] = array(
+                'id'                => $container->getId(),
+                'uri'               => $container->name,
+                'principaluri'      => $principalUri,
+                '{DAV:}displayname' => $container->name,
+                '{' . Sabre_CardDAV_Plugin::NS_CARDDAV . '}addressbook-description' => $container->name,
+                '{http://calendarserver.org/ns/}getctag' => time()
+            );
+        }
+        
+        return $addressbooks;
+    }
+
+    /**
+     * Creates a new address book 
+     *
+     * @param string $principalUri 
+     * @param string $url Just the 'basename' of the url. 
+     * @param array $properties 
+     * @return void
+     */
+    public function createAddressBook($principalUri, $url, array $properties)
+    {
+        throw new Sabre_DAV_Exception_MethodNotAllowed('createAddressBook');
+    }
+
+    /**
+     * Updates an addressbook's properties
+     *
+     * See Sabre_DAV_IProperties for a description of the mutations array, as 
+     * well as the return value. 
+     *
+     * @param mixed $addressBookId
+     * @param array $mutations
+     * @see Sabre_DAV_IProperties::updateProperties
+     * @return bool|array
+     */
+    public function updateAddressBook($addressBookId, array $mutations)
+    {
+        throw new Sabre_DAV_Exception_MethodNotAllowed('updateAddressBook');
+    }
+
+    /**
+     * Deletes an entire addressbook and all its contents
+     *
+     * @param int $addressBookId 
+     * @return void
+     */
+    public function deleteAddressBook($addressBookId) 
+    {
+        throw new Sabre_DAV_Exception_MethodNotAllowed('deleteAddressBook');
+    }
+
+    /**
+    * Returns all cards for a specific addressbook id.
+    *
+    * @param mixed $addressbookId
+    * @return array
+    */
+    public function getCards($addressbookId) 
+    {
+        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' adressbook ID: ' . $addressbookId);
+        
+        $cards = array();
+        
+        $filter = new Addressbook_Model_ContactFilter(array(
+            array(
+                'field'     => 'container_id',
+                'operator'  => 'equals',
+                'value'     => $addressbookId
+            )
+        
+        ));
+        
+        $contacts = Addressbook_Controller_Contact::getInstance()->search($filter);
+        
+        //if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' contacts ' . print_r($contacts->toArray(), true));
+        
+        foreach ($contacts as $contact) {
+            $cards[] = array(
+                        'id' => $contact->getId(),
+                        'carddata' => $this->_convertToVobject($contact)->serialize(),
+                        'uri' => $contact->getId(),
+                        'lastmodified' => $contact->last_modified_time instanceof Tinebase_DateTime ? $contact->last_modified_time->toString() : $contact->creation_time->toString() 
+            );
+        }
+        
+        return $cards;
+    }
+    
+    /**
+     * convert Addressbook_Model_Contact to Sabre_VObject_Component
+     * 
+     * @param Addressbook_Model_Contact $_contact
+     * @return Sabre_VObject_Component
+     */
+    protected function _convertToVobject(Addressbook_Model_Contact $_contact)
+    {
+        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' contact ' . print_r($_contact->toArray(), true));
+        
+        $card = new Sabre_VObject_Component('VCARD');
+        
+        // required vcard fields
+        $card->add(new Sabre_VObject_Property('VERSION', '3.0'));
+        $card->add(new Sabre_VObject_Property('FN', $_contact->n_fileas));
+        $card->add(new Sabre_VObject_Property('N', $_contact->n_family . ';' . $_contact->n_given));
+        
+        $card->add(new Sabre_VObject_Property('PRODID', '-//tine20.org//Tine 2.0//EN'));
+        $card->add(new Sabre_VObject_Property('UID', $_contact->getId()));
+
+        $card->add(new Sabre_VObject_Property('ORG', Sabre_VObject_Property::concatCompoundValues(array($_contact->org_name, $_contact->org_unit))));
+        $card->add(new Sabre_VObject_Property('TITLE', $_contact->title));
+        
+        $card->add(new Sabre_VObject_Property('TEL;TYPE=work', $_contact->tel_work));
+        $card->add(new Sabre_VObject_Property('TEL;TYPE=cell', $_contact->tel_cell));
+        $card->add(new Sabre_VObject_Property('TEL;TYPE=fax',  $_contact->tel_fax));
+        $card->add(new Sabre_VObject_Property('TEL;TYPE=home', $_contact->tel_home));
+        
+        $card->add(new Sabre_VObject_Property('ADR;TYPE=work', Sabre_VObject_Property::concatCompoundValues(array(null, $_contact->adr_one_street2, $_contact->adr_one_street, $_contact->adr_one_locality, $_contact->adr_one_region, $_contact->adr_one_postalcode, $_contact->adr_one_countryname))));
+        $card->add(new Sabre_VObject_Property('ADR;TYPE=home', Sabre_VObject_Property::concatCompoundValues(array(null, $_contact->adr_two_street2, $_contact->adr_two_street, $_contact->adr_two_locality, $_contact->adr_two_region, $_contact->adr_two_postalcode, $_contact->adr_two_countryname))));
+        
+        $card->add(new Sabre_VObject_Property('EMAIL;TYPE=work', $_contact->email));
+        $card->add(new Sabre_VObject_Property('EMAIL;TYPE=home', $_contact->email_home));
+        
+        $card->add(new Sabre_VObject_Property('URL;TYPE=work', $_contact->url));
+        $card->add(new Sabre_VObject_Property('URL;TYPE=home', $_contact->url_home));
+        
+        $card->add(new Sabre_VObject_Property('NOTE', $_contact->note));
+        
+        if(! empty($_contact->jpegphoto)) {
+            try {
+                $image = Tinebase_Controller::getInstance()->getImage('Addressbook', $_contact->getId());
+                $jpegData = $image->getBlob('image/jpeg');
+                $card->add(new Sabre_VObject_Property('PHOTO;ENCODING=b;TYPE=JPEG', base64_encode($jpegData)));
+            } catch (Exception $e) {
+                Tinebase_Core::getLogger()->info(__METHOD__ . '::' . __LINE__ . " Image for contact {$_contact->getId()} not found or invalid");
+            }
+        
+        
+        }        
+        if(isset($_contact->tags) && count($_contact->tags) > 0) {
+            $card->add(new Sabre_VObject_Property('CATEGORIES', Sabre_VObject_Property::concatCompoundValues((array) $_contact->tags->name, ',')));
+        }
+        
+        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' card ' . $card->serialize());
+        
+        return $card;
+    }
+    
+
+    /**
+     * convert Sabre_VObject_Element to Addressbook_Model_Contact
+     */
+    protected function _convertToAddressbookModelContact(Sabre_VObject_Component $_vcard, Addressbook_Model_Contact $_contact = null)
+    {
+        #if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' cardData ' . print_r($_vcard, true));
+        
+        if ($_contact instanceof Addressbook_Model_Contact) {
+            $contact = $_contact;
+        } else {
+            $contact = new Addressbook_Model_Contact(null, false);
+        }
+        
+        $data = array();
+        
+        foreach($_vcard->children() as $property) {
+            
+            switch($property->name) {
+                case 'VERSION':
+                case 'FN':
+                case 'UID':
+                case 'PRODID':
+                    // do nothing
+                    break;
+                
+                case 'ADR':
+                    $components = Sabre_VObject_Property::splitCompoundValues($property->value);
+                    
+                    if (isset($property['TYPE']) && $property['TYPE'] == 'home') {
+                        // home address
+                        $data['adr_two_street2']     = $components[1];
+                        $data['adr_two_street']      = $components[2];
+                        $data['adr_two_locality']    = $components[3];
+                        $data['adr_two_region']      = $components[4];
+                        $data['adr_two_postalcode']  = $components[5];
+                        $data['adr_two_countryname'] = $components[6];
+                    } else {
+                        // work address
+                        $data['adr_one_street2']     = $components[1];
+                        $data['adr_one_street']      = $components[2];
+                        $data['adr_one_locality']    = $components[3];
+                        $data['adr_one_region']      = $components[4];
+                        $data['adr_one_postalcode']  = $components[5];
+                        $data['adr_one_countryname'] = $components[6];
+                    }
+                    break;
+                    
+                case 'CATEGORIES':
+                    $tags = Sabre_VObject_Property::splitCompoundValues($property->value, ',');
+                    
+                    if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' cardData ' . print_r($tags, true));
+                    break;
+                    
+                case 'EMAIL':
+                    switch ($property['TYPE']) {
+                        case 'home':
+                            $data['email_home'] = $property->value;
+                            break;
+                        case 'work':
+                        default:
+                            $data['email'] = $property->value;
+                            break;
+                    }
+                    break;
+                    
+                case 'N':
+                    $components = Sabre_VObject_Property::splitCompoundValues($property->value);
+                    
+                    $data['n_family'] = $components[0];
+                    $data['n_given']  = $components[1];
+                    $data['n_middle'] = isset($components[2]) ? $components[2] : null;
+                    $data['n_prefix'] = isset($components[3]) ? $components[3] : null;
+                    $data['n_suffix'] = isset($components[4]) ? $components[4] : null;
+                    break;
+                    
+                case 'NOTE':
+                    $data['note'] = $property->value;
+                    break;
+                    
+                case 'ORG':
+                    $components = Sabre_VObject_Property::splitCompoundValues($property->value);
+                    
+                    $data['org_name'] = $components[0];
+                    $data['org_unit'] = isset($components[1]) ? $components[1] : null;
+                    break;
+                
+                case 'PHOTO':
+                    $data['jpegphoto'] = base64_decode($property->value);
+                    break;
+                    
+                case 'TEL':
+                    switch ($property['TYPE']) {
+                        case 'cell':
+                            $data['tel_cell'] = $property->value;
+                            break;
+                        case 'fax':
+                            $data['tel_fax'] = $property->value;
+                            break;
+                        case 'home':
+                            $data['tel_home'] = $property->value;
+                            break;
+                        case 'work':
+                            $data['tel_work'] = $property->value;
+                            break;
+                    }
+                    break;
+                    
+                case 'URL':
+                    switch ($property['TYPE']) {
+                        case 'home':
+                            $data['url_home'] = $property->value;
+                            break;
+                        case 'work':
+                        default:
+                            $data['url'] = $property->value;
+                            break;
+                    }
+                    break;
+                    
+                case 'TITLE':
+                    $data['title'] = $property->value;
+                    break;
+                    
+                default:
+                    if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' cardData ' . $property->name);
+                    break;
+            }
+        }
+        
+        #if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' data ' . print_r($data, true));
+        
+        $contact->setFromArray($data);
+        
+        #if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' data ' . print_r($contact->toArray(), true));
+        
+        return $contact;
+    }
+    
+    /**
+     * Returns a specfic card
+     * 
+     * @param mixed $addressBookId 
+     * @param string $cardUri 
+     * @return array 
+     */
+    public function getCard($addressBookId, $cardUri) 
+    {
+        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' cardUri ' . $cardUri);
+        
+        $contact = Addressbook_Controller_Contact::getInstance()->get($cardUri);
+        
+        return array(
+            'id' => $contact->getId(),
+            'carddata' => $this->_convertToVobject($contact)->serialize(),
+            'uri' => $contact->getId(),
+            'lastmodified' => $contact->last_modified_time instanceof Tinebase_DateTime ? $contact->last_modified_time->toString() : $contact->creation_time->toString()
+        );
+    }
+    
+    /**
+     * Creates a new card
+     *
+     * @param mixed $addressBookId
+     * @param string $cardUri
+     * @param string $cardData
+     * @return bool
+     */
+    public function createCard($addressBookId, $cardUri, $cardData) 
+    {
+        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' cardUri ' . $cardUri);
+        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' cardData ' . $cardData);
+        
+        return true;
+    }
+    
+    /**
+     * Updates a card
+     * 
+     * @param mixed $addressBookId 
+     * @param string $cardUri 
+     * @param string $cardData 
+     * @return bool 
+     */
+    public function updateCard($addressBookId, $cardUri, $cardData) 
+    {
+        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' cardUri ' . $cardUri);
+        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' cardData ' . $cardData);
+        
+        
+        $vcard = Sabre_VObject_Reader::read($cardData);
+        
+        $contact = Addressbook_Controller_Contact::getInstance()->get($cardUri);
+        
+        $contact = $this->_convertToAddressbookModelContact($vcard, $contact);
+        
+        $contact = Addressbook_Controller_Contact::getInstance()->update($contact);
+        
+        return true;
+    }
+    
+    /**
+     * Deletes a card
+     * 
+     * @param mixed $addressBookId 
+     * @param string $cardUri 
+     * @return bool 
+     */
+    public function deleteCard($addressBookId, $cardUri) 
+    {
+        return false;
+    }
+}
