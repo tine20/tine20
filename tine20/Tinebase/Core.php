ddc19c8a0919616507bee14d2a988b7e0ac4abdf
Lars Kneschke <l.kneschke@metaways.de>
2011-09-21 09:33:52 +0200

    updated RewriteRules

--- tine20/Tinebase/Core.php
+++ tine20/Tinebase/Core.php
@@ -188,14 +188,10 @@ class Tinebase_Core
             $server = new Tinebase_Server_WebDav();
 
 
-            /**************************** CALDAV API **********************************
-             * RewriteRule ^/caldav /index.php [E=CALDAV:true,E=REDIRECT_CALDAV:true,E=REMOTE_USER:%{HTTP:Authorization},L]
-             */
-        } elseif(isset($_SERVER['REDIRECT_CALDAV']) && $_SERVER['REDIRECT_CALDAV'] == 'true') {
-            $server = new Tinebase_Server_CalDav();
-
-            /**************************** CALDAV API **********************************
-             * RewriteRule ^/carddav /index.php [E=CARDDAV:true,E=REDIRECT_CARDDAV:true,E=REMOTE_USER:%{HTTP:Authorization},L]
+            /**************************** CardDAV + CalDAV API **********************************
+             * RewriteRule ^/addressbooks /index.php [E=CARDDAV:true,E=REDIRECT_CARDDAV:true,E=REMOTE_USER:%{HTTP:Authorization},L]
+             * RewriteRule ^/calendars    /index.php [E=CARDDAV:true,E=REDIRECT_CARDDAV:true,E=REMOTE_USER:%{HTTP:Authorization},L]
+             * RewriteRule ^/principals   /index.php [E=CARDDAV:true,E=REDIRECT_CARDDAV:true,E=REMOTE_USER:%{HTTP:Authorization},L]
              */
         } elseif(isset($_SERVER['REDIRECT_CARDDAV']) && $_SERVER['REDIRECT_CARDDAV'] == 'true') {
             $server = new Tinebase_Server_CardDav();
