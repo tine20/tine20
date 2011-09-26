a976e92672648805f86bd2c28b7b752251600c1e
Lars Kneschke <l.kneschke@metaways.de>
2011-09-26 18:03:27 +0200

    use new vcalendar converter class

--- tine20/Calendar/Frontend/WebDAV/Event.php
+++ tine20/Calendar/Frontend/WebDAV/Event.php
@@ -33,6 +33,11 @@ class Calendar_Frontend_WebDAV_Event extends Sabre_DAV_File implements Sabre_Cal
     protected $_vevent;
     
     /**
+     * @var Calendar_Convert_Event_VCalendar
+     */
+    protected $_converter;
+    
+    /**
      * Constructor 
      * 
      * @param  string|Calendar_Model_Event  $_event  the id of a event or the event itself 
@@ -40,6 +45,7 @@ class Calendar_Frontend_WebDAV_Event extends Sabre_DAV_File implements Sabre_Cal
     public function __construct($_event = null) 
     {
         $this->_event = $_event;
+        $this->_converter = new Calendar_Convert_Event_VCalendar();
     }
     
     /**
@@ -52,7 +58,7 @@ class Calendar_Frontend_WebDAV_Event extends Sabre_DAV_File implements Sabre_Cal
      */
     public static function create(Tinebase_Model_Container $container, $vobjectData)
     {
-        $event = self::convertToCalendarModelEvent($vobjectData);
+        $event = $this->_converter->toTine20Model($vobjectData);
         $event->container_id = $container->getId();
         
         $event = Calendar_Controller_MSEventFacade::getInstance()->create($event);
@@ -208,7 +214,7 @@ class Calendar_Frontend_WebDAV_Event extends Sabre_DAV_File implements Sabre_Cal
      */
     public function put($cardData) 
     {
-        $contact = self::convertToCalendarModelEvent($cardData, $this->_getEvent());
+        $contact = $this->_converter->toTine20Model($cardData, $this->_getEvent());
         
         $this->_event = Calendar_Controller_MSEventFacade::getInstance()->update($contact);
         
@@ -227,201 +233,6 @@ class Calendar_Frontend_WebDAV_Event extends Sabre_DAV_File implements Sabre_Cal
     public function setACL(array $acl) 
     {
         throw new Sabre_DAV_Exception_MethodNotAllowed('Changing ACL is not yet supported');
-    }
-    
-    /**
-     * converts vcard to Calendar_Model_Event
-     * 
-     * @param  Sabre_VObject_Component|stream|string  $_vevent    the vcard to parse
-     * @param  Calendar_Model_Event              $_event  supply $_event to update
-     * @return Calendar_Model_Event
-     */
-    public static function convertToCalendarModelEvent($_vevent, Calendar_Model_Event $_event = null)
-    {
-        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' cardData ' . print_r($_vevent, true));
-        
-        if ($_vevent instanceof Sabre_VObject_Component) {
-            $vcard = $_vevent;
-        } else {
-            if (is_resource($_vevent)) {
-                $_vevent = stream_get_contents($_vevent);
-            }
-            if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' cardData ' . print_r($_vevent, true));
-            $vcard = Sabre_VObject_Reader::read($_vevent);
-        }
-        
-        if ($_event instanceof Calendar_Model_Event) {
-            $contact = $_event;
-        } else {
-            $contact = new Calendar_Model_Event(null, false);
-        }
-        
-        $data = array();
-        
-        foreach($vcard->children() as $property) {
-            
-            switch($property->name) {
-                case 'VERSION':
-                case 'PRODID':
-                case 'UID':
-                    // do nothing
-                    break;
-                
-                case 'ADR':
-                    $components = Sabre_VObject_Property::splitCompoundValues($property->value);
-                    
-                    if (isset($property['TYPE']) && $property['TYPE'] == 'home') {
-                        // home address
-                        $data['adr_two_street2']     = $components[1];
-                        $data['adr_two_street']      = $components[2];
-                        $data['adr_two_locality']    = $components[3];
-                        $data['adr_two_region']      = $components[4];
-                        $data['adr_two_postalcode']  = $components[5];
-                        $data['adr_two_countryname'] = $components[6];
-                    } else {
-                        // work address
-                        $data['adr_one_street2']     = $components[1];
-                        $data['adr_one_street']      = $components[2];
-                        $data['adr_one_locality']    = $components[3];
-                        $data['adr_one_region']      = $components[4];
-                        $data['adr_one_postalcode']  = $components[5];
-                        $data['adr_one_countryname'] = $components[6];
-                    }
-                    break;
-                    
-                case 'CATEGORIES':
-                    $tags = Sabre_VObject_Property::splitCompoundValues($property->value, ',');
-                    
-                    if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' cardData ' . print_r($tags, true));
-                    break;
-                    
-                case 'EMAIL':
-                    switch ($property['TYPE']) {
-                        case 'home':
-                            $data['email_home'] = $property->value;
-                            break;
-                        case 'work':
-                        default:
-                            $data['email'] = $property->value;
-                            break;
-                    }
-                    break;
-                    
-                case 'FN':
-                    $data['n_fn'] = $property->value;
-                    break;
-                    
-                case 'N':
-                    $components = Sabre_VObject_Property::splitCompoundValues($property->value);
-                    
-                    $data['n_family'] = $components[0];
-                    $data['n_given']  = $components[1];
-                    $data['n_middle'] = isset($components[2]) ? $components[2] : null;
-                    $data['n_prefix'] = isset($components[3]) ? $components[3] : null;
-                    $data['n_suffix'] = isset($components[4]) ? $components[4] : null;
-                    break;
-                    
-                case 'NOTE':
-                    $data['note'] = $property->value;
-                    break;
-                    
-                case 'ORG':
-                    $components = Sabre_VObject_Property::splitCompoundValues($property->value);
-                    
-                    $data['org_name'] = $components[0];
-                    $data['org_unit'] = isset($components[1]) ? $components[1] : null;
-                    break;
-                
-                case 'PHOTO':
-                    $data['jpegphoto'] = base64_decode($property->value);
-                    break;
-                    
-                case 'TEL':
-                    switch ($property['TYPE']) {
-                        case 'cell':
-                            $data['tel_cell'] = $property->value;
-                            break;
-                        case 'fax':
-                            $data['tel_fax'] = $property->value;
-                            break;
-                        case 'home':
-                            $data['tel_home'] = $property->value;
-                            break;
-                        case 'work':
-                            $data['tel_work'] = $property->value;
-                            break;
-                    }
-                    break;
-                    
-                case 'URL':
-                    switch ($property['TYPE']) {
-                        case 'home':
-                            $data['url_home'] = $property->value;
-                            break;
-                        case 'work':
-                        default:
-                            $data['url'] = $property->value;
-                            break;
-                    }
-                    break;
-                    
-                case 'TITLE':
-                    $data['title'] = $property->value;
-                    break;
-                    
-                default:
-                    if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' cardData ' . $property->name);
-                    break;
-            }
-        }
-        
-        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' data ' . print_r($data, true));
-        
-        if (empty($data['n_family'])) {
-            $parts = explode(' ', $data['n_fn']);
-            $data['n_family'] = $parts[count($parts) - 1];
-            $data['n_given'] = (count($parts) > 1) ? $parts[0] : null;
-        }
-        
-        $contact->setFromArray($data);
-        
-        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' data ' . print_r($contact->toArray(), true));
-        
-        return $contact;
-    }
-    
-    /**
-     * convert Calendar_Model_Event to Sabre_VObject_Component
-     * 
-     * @param  Calendar_Model_Event  $_event
-     * @return Sabre_VObject_Component
-     */
-    public static function convertToVEvent(Calendar_Model_Event $_event)
-    {
-        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' contact ' . print_r($_event->toArray(), true));
-        
-        $eventId = $_event->getId();
-        $lastModified = $_event->last_modified_time ? $_event->last_modified_time : $_event->creation_time;
-        
-        // we always use a event set to return exdates at once
-        $eventSet = new Tinebase_Record_RecordSet('Calendar_Model_Event', array($_event));
-        
-        if ($_event->rrule) {
-            foreach($_event->exdate as $exEvent) {
-                if (! $exEvent->is_deleted) {
-                    $eventSet->addRecord($exEvent);
-                    $_event->exdate->removeRecord($exEvent);
-                }
-            }
-            
-            // remaining exdates are fallouts
-            $_event->exdate = $_event->exdate->getOriginalDtStart();
-        }
-        
-        $exporter = new Calendar_Export_Ical();
-        $ics = $exporter->eventToIcal($eventSet);
-        
-        return $ics;
     }
     
     /**
@@ -447,7 +258,7 @@ class Calendar_Frontend_WebDAV_Event extends Sabre_DAV_File implements Sabre_Cal
     protected function _getVEvent()
     {
         if ($this->_vevent == null) {
-            $this->_vevent = self::convertToVEvent($this->_getEvent());
+            $this->_vevent = $this->_converter->fromTine20Model($this->_getEvent());
         }
         
         return $this->_vevent;
