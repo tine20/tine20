ac345b2061d948ea3a55c35063ca255127c68b0b
Philipp Schüle <p.schuele@metaways.de>
2011-09-27 09:49:04 +0200

    added role/status filters (#4854)

--- tests/tine20/Addressbook/JsonTest.php
+++ tests/tine20/Addressbook/JsonTest.php
@@ -494,7 +494,8 @@ class Addressbook_JsonTest extends PHPUnit_Framework_TestCase
      */
     public function testAttenderForeignIdFilter()
     {
-        $event = $this->_getEvent();
+        $contact = $this->_addContact();
+        $event = $this->_getEvent($contact);
         Calendar_Controller_Event::getInstance()->create($event);
         
         $filter = array(
@@ -510,11 +511,11 @@ class Addressbook_JsonTest extends PHPUnit_Framework_TestCase
                         'from'  => '2009-01-01 00:00:00',
                         'until' => '2010-12-31 23:59:59',
                     )),
-                    array('field' => "attender_status",   "operator" => "in",  "value" => array('NEEDS-ACTION', 'ACCEPTED')),
-                    array('field' => "attender_role",     "operator" => "in",  "value" => array('REQ')),
+                    array('field' => Calendar_Model_ContactFilter::STATUS_FIELD,   "operator" => "in",  "value" => array('NEEDS-ACTION', 'ACCEPTED')),
+                    array('field' => Calendar_Model_ContactFilter::ROLE_FIELD,     "operator" => "in",  "value" => array('REQ')),
                 )
             ),
-            array('field' => 'id', 'operator' => 'equals', 'value' => Tinebase_Core::getUser()->contact_id)
+            array('field' => 'id', 'operator' => 'in', 'value' => array(Tinebase_Core::getUser()->contact_id, $contact['id']))
         );
         $result = $this->_instance->searchContacts($filter, array());
         
@@ -525,9 +526,10 @@ class Addressbook_JsonTest extends PHPUnit_Framework_TestCase
     /**
      * returns a simple event
      *
+     * @param array $_contact
      * @return Calendar_Model_Event
      */
-    protected function _getEvent()
+    protected function _getEvent($_contact)
     {
         $testCalendar = Tinebase_Container::getInstance()->addContainer(new Tinebase_Model_Container(array(
             'name'           => 'PHPUnit test calendar',
@@ -546,6 +548,12 @@ class Addressbook_JsonTest extends PHPUnit_Framework_TestCase
                     'user_id'        => Tinebase_Core::getUser()->contact_id,
                     'user_type'      => Calendar_Model_Attender::USERTYPE_USER,
                     'role'           => Calendar_Model_Attender::ROLE_REQUIRED,
+                    'status_authkey' => Tinebase_Record_Abstract::generateUID(),
+                ),
+                array(
+                    'user_id'        => $_contact['id'],
+                    'user_type'      => Calendar_Model_Attender::USERTYPE_USER,
+                    'role'           => Calendar_Model_Attender::ROLE_OPTIONAL,
                     'status_authkey' => Tinebase_Record_Abstract::generateUID(),
                 ),
             )),
--- tine20/Calendar/Model/ContactFilter.php
+++ tine20/Calendar/Model/ContactFilter.php
@@ -19,6 +19,14 @@
 class Calendar_Model_ContactFilter extends Tinebase_Model_Filter_Abstract 
 {
     /**
+     * filter fields for role and status
+     * 
+     * @var string
+     */
+    const STATUS_FIELD = 'attender_status';
+    const ROLE_FIELD   = 'attender_role';
+    
+    /**
      * @var array list of allowed operators
      */
     protected $_operators = array(
@@ -30,10 +38,21 @@ class Calendar_Model_ContactFilter extends Tinebase_Model_Filter_Abstract
      *
      * @param  Zend_Db_Select                    $_select
      * @param  Tinebase_Backend_Sql_Abstract     $_backend
+     * 
+     * @todo allow multiple role/status filters?
      */
     public function appendFilterSql($_select, $_backend)
     {
-        // @todo get role and status from filter and remove all non-matching attendee
+        // get role and status from filter to remove all non-matching attendee
+        $roleFilter = NULL;
+        $statusFilter = NULL;
+        foreach ($this->_value as $filterData) {
+            if ($filterData['field'] === self::STATUS_FIELD) {
+                $statusFilter = $filterData;
+            } else if ($filterData['field'] === self::ROLE_FIELD) {
+                $roleFilter = $filterData;
+            }
+        }
         
         $eventFilter = new Calendar_Model_EventFilter($this->_value);
         $events = Calendar_Controller_Event::getInstance()->search($eventFilter);
@@ -42,8 +61,12 @@ class Calendar_Model_ContactFilter extends Tinebase_Model_Filter_Abstract
         $contactIds = array();
         foreach ($events as $event) {
             foreach ($event->attendee as $attender) {
-                if ($attender->user_type === Calendar_Model_Attender::USERTYPE_GROUPMEMBER || $attender->user_type === Calendar_Model_Attender::USERTYPE_USER) {
-                    $contactIds[] = $attender->user_id;
+                if (   $attender->user_type === Calendar_Model_Attender::USERTYPE_GROUPMEMBER 
+                    || $attender->user_type === Calendar_Model_Attender::USERTYPE_USER
+                ) {
+                    if ($this->_matchFilter($attender, $roleFilter) && $this->_matchFilter($attender, $statusFilter)) {
+                        $contactIds[] = $attender->user_id;
+                    }
                 }
             }
         }
@@ -52,5 +75,34 @@ class Calendar_Model_ContactFilter extends Tinebase_Model_Filter_Abstract
         // this is supposed to run in ContactFilter context
         $contactIdFilter = new Addressbook_Model_ContactIdFilter('id', 'in', $contactIds);
         $contactIdFilter->appendFilterSql($_select, $_backend);
+    }
+    
+    /**
+     * check if attender matches role/status filter
+     * 
+     * @param Calendar_Model_Attender $_attender
+     * @param array $_filterData
+     * @return boolean
+     */
+    protected function _matchFilter(Calendar_Model_Attender $_attender, $_filterData = NULL)
+    {
+        if ($_filterData === NULL) {
+            return TRUE;
+        }
+        
+        $attenderField = ($_filterData['field'] === self::STATUS_FIELD) ? 'status' : 'role';
+        
+        switch ($_filterData['operator']) {
+            case 'equals':
+                $result = ($_attender->{$attenderField} === $_filterData['value']);
+                break;
+            case 'in':
+                $result = in_array($_attender->{$attenderField}, $_filterData['value']);
+                break;
+            default:
+                $result = FALSE;
+        }
+        
+        return $result;
     }
 }
