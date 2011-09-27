2e08d2f1df27f413291c96f8869cea6a70cade66
Philipp Schüle <p.schuele@metaways.de>
2011-09-27 20:32:16 +0200

    improved async job fencing by adding a sequence number (#4878)

--- tine20/Tinebase/Model/AsyncJob.php
+++ tine20/Tinebase/Model/AsyncJob.php
@@ -5,8 +5,8 @@
  * @package     Tinebase
  * @subpackage  Record
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
- * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
- * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2009-2011 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schüle <p.schuele@metaways.de>
  * 
  */
 
@@ -66,6 +66,7 @@ class Tinebase_Model_AsyncJob extends Tinebase_Record_Abstract
             self::STATUS_SUCCESS,
         )),
         'message'           => array('allowEmpty' => TRUE),
+        'seq'               => array('allowEmpty' => TRUE),
     );
     
     /**
