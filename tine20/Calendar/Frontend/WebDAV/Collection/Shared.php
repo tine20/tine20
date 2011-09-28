aed8370727916937e61aafe63aac0dd081d0f945
Lars Kneschke <l.kneschke@metaways.de>
2011-09-28 18:01:23 +0200

    classes to handle personal and shared folders

---
+++ tine20/Calendar/Frontend/WebDAV/Collection/Shared.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Tinebase
+ * @subpackage  WebDAV
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2011-2011 Metaways Infosystems GmbH (http://www.metaways.de)
+ *
+ */
+
+/**
+ * class to handle generic folders in WebDAV tree
+ *
+ * @package     Tinebase
+ * @subpackage  WebDAV
+ */
+class Calendar_Frontend_WebDAV_Collection_Shared extends Tinebase_WebDav_Collection_Shared_Abstract
+{
+    protected $_applicationName = 'Calendar';
+    
+    protected $_model = 'Event';    
+}
