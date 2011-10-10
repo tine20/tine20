3d48ae6aba5917ebfcf58d0c058ba656100dc463
Cornelius Weiß <mail@corneliusweiss.de>
2011-10-10 11:25:21 +0200

    rework timeaccount filtermodel

---
+++ tine20/Timetracker/js/TimeAccountStatusFilterModel.js
@@ -0,0 +1,61 @@
+/**
+ * Tine 2.0
+ * 
+ * @package     Timetracker
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2007-2011 Metaways Infosystems GmbH (http://www.metaways.de)
+ */
+ 
+Ext.ns('Tine.Timetracker');
+
+Tine.Timetracker.TimeAccountStatusFilterModel = Ext.extend(Tine.widgets.grid.FilterModel, {
+    field: 'timeaccount_status',
+    valueType: 'string',
+    defaultValue: 'to bill',
+    
+    /**
+     * @private
+     */
+    initComponent: function() {
+        Tine.Timetracker.TimeAccountStatusFilterModel.superclass.initComponent.call(this);
+        
+        this.app = Tine.Tinebase.appMgr.get('Timetracker');
+        this.label = this.label ? this.label : this.app.i18n._("Time Account - Status");
+        this.operators = ['equals'];
+    },
+   
+    /**
+     * value renderer
+     * 
+     * @param {Ext.data.Record} filter line
+     * @param {Ext.Element} element to render to 
+     */
+    valueRenderer: function(filter, el) {
+        // value
+        var value = new Ext.form.ComboBox({
+            filter: filter,
+            width: 200,
+            id: 'tw-ftb-frow-valuefield-' + filter.id,
+            value: filter.data.value ? filter.data.value : this.defaultValue,
+            renderTo: el,
+            mode: 'local',
+            forceSelection: true,
+            blurOnSelect: true,
+            triggerAction: 'all',
+            store: [
+                ['not yet billed', this.app.i18n._('not yet billed')], 
+                ['to bill', this.app.i18n._('to bill')],
+                ['billed', this.app.i18n._('billed')]
+            ]
+        });
+        value.on('specialkey', function(field, e){
+             if(e.getKey() == e.ENTER){
+                 this.onFiltertrigger();
+             }
+        }, this);
+        
+        return value;
+    }
+});
+Tine.widgets.grid.FilterToolbar.FILTERS['timetracker.timeaccountstatus'] = Tine.Timetracker.TimeAccountStatusFilterModel;
