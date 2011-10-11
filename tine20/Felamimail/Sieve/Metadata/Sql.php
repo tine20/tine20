3ca0f6107d6f2ffdafc4238a5381da5c8eafcb33
Philipp Schüle <p.schuele@metaways.de>
2011-10-11 11:25:19 +0200

    generalized sieve metadata functionality and added stub of sql backend (#4922)
    this are the missing classes of the last commit ;)

---
+++ tine20/Felamimail/Sieve/Metadata/Sql.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Felamimail
+ * @subpackage  Sieve
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schüle <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2010-2011 Metaways Infosystems GmbH (http://www.metaways.de)
+ * 
+ * @todo        make it work
+ */
+
+/**
+ * class read and write sieve data from / to a sql database table
+ * 
+ * This class does not really parse the Sieve script, but either parses SmartSieves pseudo code
+ * or reads the serialized rules and vacation objects.
+ *
+ * @package     Felamimail
+ * @subpackage  Sieve
+ */
+class Felamimail_Sieve_Metadata_Sql extends Felamimail_Sieve_Metadata_Abstract
+{
+}
