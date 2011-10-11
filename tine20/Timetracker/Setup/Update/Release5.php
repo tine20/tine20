a0ab32be8d24f431489eabff7911f947e499dc55
Philipp Schüle <p.schuele@metaways.de>
2011-10-11 16:02:45 +0200

    added update script for timesheet persistent filters (#4948)
    they now should use the correct 'left-hand' filter syntax

--- tine20/Timetracker/Setup/Update/Release5.php
+++ tine20/Timetracker/Setup/Update/Release5.php
@@ -29,4 +29,34 @@ class Timetracker_Setup_Update_Release5 extends Setup_Update_Abstract
         $this->setTableVersion('timetracker_timeaccount', 7);
         $this->setApplicationVersion('Timetracker', '5.1');
     }
+    
+    /**
+     * update favorites to new filter syntax and update to 5.2
+     * 
+     * @return void
+     */
+    public function update_1()
+    {
+        $filters = Tinebase_PersistentFilter::getInstance()->getAll();
+        $timetrackerFilters = $filters->filter('application_id', Tinebase_Application::getInstance()->getApplicationByName('Timetracker')->getId());
+        $pfBackend = new Tinebase_PersistentFilter_Backend_Sql();
+        
+        foreach ($timetrackerFilters as $pfilter) {
+            foreach ($pfilter->filters as $filter) {
+                if (in_array($filter->getField(), array('timeaccount_id')) && $filter instanceof Tinebase_Model_Filter_ForeignId) {
+                    $values = array();
+                    foreach ($filter->getValue() as $idx => $subfilter) {
+                        $values[$idx] = $subfilter;
+                        if (in_array($subfilter['field'], array('id'))) {
+                            $values[$idx]['field'] = ':' . $subfilter['field'];
+                        }
+                    }
+                    $filter->setValue($values);
+                    $pfBackend->update($pfilter);
+                }
+            }
+        }
+        
+        $this->setApplicationVersion('Timetracker', '5.2');
+    }
 }
