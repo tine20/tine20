7d189951138307ad7ef27ab6c706358b72916afe
Lars Kneschke <l.kneschke@metaways.de>
2011-10-11 11:40:45 +0200

    codename is at least Maischa

--- scripts/packaging/build-tine20-packages.sh
+++ scripts/packaging/build-tine20-packages.sh
@@ -12,7 +12,7 @@ TEMPDIR="$BASEDIR/temp"
 MISCPACKAGESDIR="$BASEDIR/packages/misc"
 
 RELEASE=""
-CODENAME="Neele"
+CODENAME="Maischa"
 GITURL="http://git.tine20.org/git/tine20"
 GITBRANCH="master"
 PACKAGEDIR=""
