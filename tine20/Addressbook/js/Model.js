36a48c602ac4591e29bb60660554bf17b1f0d795
Cornelius Weiß <mail@corneliusweiss.de>
2011-10-14 13:06:07 +0200

    ASSIGNED - # 4962: implement conflict resolving assistent http://forge.tine20.org/mantisbt/view.php?id=4962
    classify fields

--- tine20/Addressbook/js/Model.js
+++ tine20/Addressbook/js/Model.js
@@ -12,24 +12,24 @@ Ext.ns('Tine.Addressbook.Model');
 
 // TODO: move this into model definition and replace uscases (?) with getter fn
 Tine.Addressbook.Model.ContactArray = Tine.Tinebase.Model.genericFields.concat([
-    {name: 'id'},
-    {name: 'tid'},
-    {name: 'private'},
-    {name: 'cat_id'},
+    {name: 'id', ommitDuplicateResolveing: true},
+    {name: 'tid', ommitDuplicateResolveing: true},
+    {name: 'private', ommitDuplicateResolveing: true},
+    {name: 'cat_id', ommitDuplicateResolveing: true},
     {name: 'n_family', label: 'Last Name' },//_('Last Name')
     {name: 'n_given', label: 'First Name' }, //_('First Name')
     {name: 'n_middle', label: 'Middle Name' }, //_('Middle Name')
     {name: 'n_prefix', label: 'Title' }, //_('Title')
-    {name: 'n_suffix', label: 'Suffix' }, //_('Suffix')
-    {name: 'n_fn', label: 'Display Name' }, //_('Display Name')
-    {name: 'n_fileas' },
+    {name: 'n_suffix', label: 'Suffix'}, //_('Suffix')
+    {name: 'n_fn', label: 'Display Name', ommitDuplicateResolveing: true }, //_('Display Name')
+    {name: 'n_fileas', ommitDuplicateResolveing: true },
     {name: 'bday', label: 'Birthday', type: 'date', dateFormat: Date.patterns.ISO8601Long }, //_('Birthday')
     {name: 'org_name', label: 'Company' }, //_('Company')
     {name: 'org_unit', label: 'Unit' }, //_('Unit')
     {name: 'salutation_id', label: 'Salutation' }, //_('Salutation')
     {name: 'title', label: 'Job Title' }, //_('Job Title')
     {name: 'role', label: 'Job Role' }, //_('Job Role')
-    {name: 'assistent'},
+    {name: 'assistent', ommitDuplicateResolveing: true},
     {name: 'room', label: 'Room' }, //_('Room')
     {name: 'adr_one_street', label: 'Street (Company Address)' }, //_('Street (Company Address)')
     {name: 'adr_one_street2', label: 'Street 2 (Company Address)' }, //_('Street 2 (Company Address)')
@@ -37,44 +37,44 @@ Tine.Addressbook.Model.ContactArray = Tine.Tinebase.Model.genericFields.concat([
     {name: 'adr_one_region', label: 'Region (Company Address)' }, //_('Region (Company Address)')
     {name: 'adr_one_postalcode', label: 'Postal Code (Company Address)' }, //_('Postal Code (Company Address)')
     {name: 'adr_one_countryname', label: 'Country (Company Address)' }, //_('Country (Company Address)')
-    {name: 'adr_one_lon'},
-    {name: 'adr_one_lat'},
-    {name: 'label'},
+    {name: 'adr_one_lon', ommitDuplicateResolveing: true},
+    {name: 'adr_one_lat', ommitDuplicateResolveing: true},
+    {name: 'label', ommitDuplicateResolveing: true},
     {name: 'adr_two_street', label: 'Street (Private Address)' }, //_('Street (Private Address)')
     {name: 'adr_two_street2', label: 'Street 2 (Private Address)' }, //_('Street 2 (Private Address)')
     {name: 'adr_two_locality', label: 'City (Private Address)' }, //_('City (Private Address)')
     {name: 'adr_two_region', label: 'Region (Private Address)' }, //_('Region (Private Address)')
     {name: 'adr_two_postalcode', label: 'Postal Code (Private Address)' }, //_('Postal Code (Private Address)')
     {name: 'adr_two_countryname', label: 'Country (Private Address)' }, //_('Country (Private Address)')
-    {name: 'adr_two_lon'},
-    {name: 'adr_two_lat'},
+    {name: 'adr_two_lon', ommitDuplicateResolveing: true},
+    {name: 'adr_two_lat', ommitDuplicateResolveing: true},
     {name: 'tel_work', label: 'Phone' }, //_('Phone')
     {name: 'tel_cell', label: 'Mobile' }, //_('Mobile')
     {name: 'tel_fax', label: 'Fax' }, //_('Fax')
-    {name: 'tel_assistent' },
-    {name: 'tel_car' },
-    {name: 'tel_pager' },
+    {name: 'tel_assistent', ommitDuplicateResolveing: true },
+    {name: 'tel_car', ommitDuplicateResolveing: true },
+    {name: 'tel_pager', ommitDuplicateResolveing: true },
     {name: 'tel_home', label: 'Phone (private)' }, //_('Phone (private)')
     {name: 'tel_fax_home', label: 'Fax (private)'}, //_('Fax (private)')
     {name: 'tel_cell_private', label: 'Mobile (private)' }, //_('Mobile (private)')
-    {name: 'tel_other' },
-    {name: 'tel_prefer'},
+    {name: 'tel_other', ommitDuplicateResolveing: true },
+    {name: 'tel_prefer', ommitDuplicateResolveing: true},
     {name: 'email', label: 'E-Mail' }, //_('E-Mail')
     {name: 'email_home', label: 'E-Mail (private)' }, //_('E-Mail (private)')
     {name: 'url', label: 'Web'}, //_('Web')
     {name: 'url_home', label: 'Web (private)' }, //_('Web (private)')
-    {name: 'freebusy_uri'},
-    {name: 'calendar_uri'},
+    {name: 'freebusy_uri', ommitDuplicateResolveing: true},
+    {name: 'calendar_uri', ommitDuplicateResolveing: true},
     {name: 'note', label: 'Description' }, //_('Description')
-    {name: 'tz'},
-    {name: 'pubkey'},
-    {name: 'jpegphoto'},
-    {name: 'account_id'},
+    {name: 'tz', ommitDuplicateResolveing: true},
+    {name: 'pubkey', ommitDuplicateResolveing: true},
+    {name: 'jpegphoto', ommitDuplicateResolveing: true},
+    {name: 'account_id', isMetaField: true},
     {name: 'tags'},
-    {name: 'notes'},
-    {name: 'relations'},
-    {name: 'customfields'},
-    {name: 'type'}
+    {name: 'notes', ommitDuplicateResolveing: true},
+    {name: 'relations', ommitDuplicateResolveing: true},
+    {name: 'customfields', isMetaField: true},
+    {name: 'type', ommitDuplicateResolveing: true}
 ]);
 
 /**
