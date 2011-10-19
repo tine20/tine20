599e932ce9f1bc0b183660cba8d34ed8dad50a61
Philipp Schüle <p.schuele@metaways.de>
2011-10-19 14:25:59 +0200

    adopt to new exception data structure
    ASSIGNED - # 4958: add configurable duplicate check to create actions 
    http://forge.tine20.org/mantisbt/view.php?id=4958

--- tests/tine20/Addressbook/JsonTest.php
+++ tests/tine20/Addressbook/JsonTest.php
@@ -711,9 +711,9 @@ class Addressbook_JsonTest extends PHPUnit_Framework_TestCase
         } catch (Tinebase_Exception_Duplicate $ted) {
             $this->assertTrue($_duplicateCheck, 'force creation failed');
             $exceptionData = $ted->toArray();
-            $this->assertEquals(1, $exceptionData['duplicates']['totalcount'], print_r($exceptionData['duplicates'], TRUE));
-            $this->assertEquals($contact['n_given'], $exceptionData['duplicates']['results'][0]['n_given']);
-            $this->assertEquals($contact['org_name'], $exceptionData['duplicates']['results'][0]['org_name']);
+            $this->assertEquals(1, count($exceptionData['duplicates']), print_r($exceptionData['duplicates'], TRUE));
+            $this->assertEquals($contact['n_given'], $exceptionData['duplicates'][0]['n_given']);
+            $this->assertEquals($contact['org_name'], $exceptionData['duplicates'][0]['org_name']);
         }
     }
 
@@ -734,8 +734,8 @@ class Addressbook_JsonTest extends PHPUnit_Framework_TestCase
             $this->assertTrue(FALSE, 'no duplicate exception');
         } catch (Tinebase_Exception_Duplicate $ted) {
             $exceptionData = $ted->toArray();
-            $this->assertEquals(1, $exceptionData['duplicates']['totalcount']);
-            $this->assertEquals($contact['email'], $exceptionData['duplicates']['results'][0]['email']);
+            $this->assertEquals(1, count($exceptionData['duplicates']));
+            $this->assertEquals($contact['email'], $exceptionData['duplicates'][0]['email']);
         }
     }
 
