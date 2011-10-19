e8ea189fd697e8d1fa40553c66df376f0c55eb90
Lars Kneschke <l.kneschke@metaways.de>
2011-10-19 04:28:06 +0200

    removed Alnum Filter

--- tine20/Calendar/Model/Attender.php
+++ tine20/Calendar/Model/Attender.php
@@ -89,7 +89,7 @@ class Calendar_Model_Attender extends Tinebase_Record_Abstract
         'seq'                  => array('allowEmpty' => true,  'Int'  ),
         */
         
-        'cal_event_id'         => array('allowEmpty' => true,  'Alnum'),
+        'cal_event_id'         => array('allowEmpty' => true/*,  'Alnum'*/),
         'user_id'              => array('allowEmpty' => false,        ),
         'user_type'            => array('allowEmpty' => true,  'InArray' => array(self::USERTYPE_USER, self::USERTYPE_GROUP, self::USERTYPE_GROUPMEMBER, self::USERTYPE_RESOURCE)),
         'role'                 => array('allowEmpty' => true,  'InArray' => array(self::ROLE_OPTIONAL, self::ROLE_REQUIRED)),
