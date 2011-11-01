425e0399177f5e488714b54feba1750a6907f6ce
Philipp Schüle <p.schuele@metaways.de>
2011-11-01 11:16:22 +0100

    splitted lengthy function getCompleteMessage()

--- tine20/Felamimail/Controller/Message.php
+++ tine20/Felamimail/Controller/Message.php
@@ -145,10 +145,11 @@ class Felamimail_Controller_Message extends Tinebase_Controller_Record_Abstract
      * get complete message by id
      *
      * @param string|Felamimail_Model_Message  $_id
+     * @param string 						   $_partId
      * @param boolean                          $_setSeen
      * @return Felamimail_Model_Message
      */
-    public function getCompleteMessage($_id, $_partId = null, $_setSeen = FALSE)
+    public function getCompleteMessage($_id, $_partId = NULL, $_setSeen = FALSE)
     {
         if ($_id instanceof Felamimail_Model_Message) {
             $message = $_id;
@@ -157,57 +158,64 @@ class Felamimail_Controller_Message extends Tinebase_Controller_Record_Abstract
         }
         
         if (Tinebase_Core::isLogLevel(Zend_Log::INFO)) Tinebase_Core::getLogger()->info(__METHOD__ . '::' . __LINE__ . 
-            ' Getting message ' . $message->messageuid 
+            ' Getting message content ' . $message->messageuid 
         );
         
-        // get account
         $folder = Felamimail_Controller_Folder::getInstance()->get($message->folder_id);
         $account = Felamimail_Controller_Account::getInstance()->get($folder->account_id);
-        $mimeType = ($account->display_format == Felamimail_Model_Account::DISPLAY_HTML || $account->display_format == Felamimail_Model_Account::DISPLAY_CONTENT_TYPE) 
-            ? Zend_Mime::TYPE_HTML 
-            : Zend_Mime::TYPE_TEXT;
         
-        $headers     = $this->getMessageHeaders($message, $_partId, true);
-        $body        = $this->getMessageBody($message, $_partId, $mimeType, $account, true);
-        $attachments = $this->getAttachments($message, $_partId);
+        $this->_getCompleteMessageContent($message, $account, $_partId);
         
-        // set \Seen flag
-        if ($_setSeen && !in_array(Zend_Mail_Storage::FLAG_SEEN, $message->flags)) {
-            if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . 
-                ' Add \Seen flag to msg uid ' . $message->messageuid
-            );
-            Felamimail_Controller_Message_Flags::getInstance()->addFlags($message, Zend_Mail_Storage::FLAG_SEEN);
-            $message->flags[] = Zend_Mail_Storage::FLAG_SEEN;
+        if ($_setSeen) {
+            Felamimail_Controller_Message_Flags::getInstance()->setSeenFlag($message);
         }
         
+        //if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' ' . print_r($message->toArray(), true));
+        
+        return $message;
+    }
+    
+    /**
+     * get message content (body, headers and attachments)
+     * 
+     * @param Felamimail_Model_Message $_message
+     * @param Felamimail_Model_Account $_account
+     * @param string $_partId
+     */
+    protected function _getCompleteMessageContent(Felamimail_Model_Message $_message, Felamimail_Model_Account $_account, $_partId = NULL)
+    {
+        $mimeType = ($_account->display_format == Felamimail_Model_Account::DISPLAY_HTML || $_account->display_format == Felamimail_Model_Account::DISPLAY_CONTENT_TYPE)
+        ? Zend_Mime::TYPE_HTML
+        : Zend_Mime::TYPE_TEXT;
+        
+        $headers     = $this->getMessageHeaders($_message, $_partId, true);
+        $body        = $this->getMessageBody($_message, $_partId, $mimeType, $_account, true);
+        $attachments = $this->getAttachments($_message, $_partId);
+        
         if ($_partId === null) {
-            $message->body        = $body;
-            $message->headers     = $headers;
-            $message->attachments = $attachments;
+            $_message->body        = $body;
+            $_message->headers     = $headers;
+            $_message->attachments = $attachments;
         } else {
             // create new object for rfc822 message
-            $structure = $message->getPartStructure($_partId, FALSE);
-            
-            $message = new Felamimail_Model_Message(array(
-                'messageuid'  => $message->messageuid,
-                'folder_id'   => $message->folder_id,
-                'received'    => $message->received,
+            $structure = $_message->getPartStructure($_partId, FALSE);
+        
+            $_message = new Felamimail_Model_Message(array(
+                'messageuid'  => $_message->messageuid,
+                'folder_id'   => $_message->folder_id,
+                'received'    => $_message->received,
                 'size'        => (array_key_exists('size', $structure)) ? $structure['size'] : 0,
                 'partid'      => $_partId,
                 'body'        => $body,
                 'headers'     => $headers,
                 'attachments' => $attachments
             ));
-
-            $message->parseHeaders($headers);
-            
+        
+            $_message->parseHeaders($headers);
+        
             $structure = array_key_exists('messageStructure', $structure) ? $structure['messageStructure'] : $structure;
-            $message->parseStructure($structure);
+            $_message->parseStructure($structure);
         }
-        
-        //if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' ' . print_r($message->toArray(), true));
-        
-        return $message;
     }
     
     /**
