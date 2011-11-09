96052626ce8445a0a640a0b206b2e6738587e5b8
Lars Kneschke <l.kneschke@metaways.de>
2011-11-09 17:54:31 +0100

    return calendar timezone and changed log level to trace
    ASSIGNED - # 4896: Implement a CalDAV frontend for the calendar 
    http://forge.tine20.org/mantisbt/view.php?id=4896

--- tine20/Calendar/Frontend/WebDAV/Container.php
+++ tine20/Calendar/Frontend/WebDAV/Container.php
@@ -32,23 +32,29 @@ class Calendar_Frontend_WebDAV_Container extends Tinebase_WebDav_Container_Abstr
      */
     public function getProperties($requestedProperties) 
     {
+        $displayName = $this->_container->type == Tinebase_Model_Container::TYPE_SHARED ? $this->_container->name . ' (shared)' : $this->_container->name;
+        
         $properties = array(
-            '{http://calendarserver.org/ns/}getctag' => round(time()/60),
+            '{http://calendarserver.org/ns/}getctag' => round(time()),
             'id'                => $this->_container->getId(),
             'uri'               => $this->_useIdAsName == true ? $this->_container->getId() : $this->_container->name,
         	'{DAV:}resource-id'	=> 'urn:uuid:' . $this->_container->getId(),
         	'{DAV:}owner'       => new Sabre_DAVACL_Property_Principal(Sabre_DAVACL_Property_Principal::HREF, 'principals/users/' . Tinebase_Core::getUser()->contact_id),
             #'principaluri'      => $principalUri,
-            '{DAV:}displayname' => $this->_container->type == Tinebase_Model_Container::TYPE_SHARED && $this->_useIdAsName == true ? $this->_container->name . ' (shared)' : $this->_container->name,
-            '{http://apple.com/ns/ical/}calendar-color' => $this->_container->color,
+            '{DAV:}displayname' => $displayName,
+            '{http://apple.com/ns/ical/}calendar-color' => (empty($this->_container->color)) ? '#000000' : $this->_container->color,
+            
             '{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}supported-calendar-component-set' => new Sabre_CalDAV_Property_SupportedCalendarComponentSet(array('VEVENT')),
-        	'{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}supported-calendar-data'          => new Sabre_CalDAV_Property_SupportedCalendarData(/*'3.0'*/),
-        	'{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}calendar-description'		       => 'Tine 2.0 Calendar',
+        	'{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}supported-calendar-data'          => new Sabre_CalDAV_Property_SupportedCalendarData(),
+        	'{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}calendar-description'		       => 'Calendar ' . $displayName,
+    		'{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}calendar-timezone'                => $this->_getCalendarVTimezone()
         );
         
         if (!empty(Tinebase_Core::getUser()->accountEmailAddress)) {
             $properties['{' . Sabre_CalDAV_Plugin::NS_CALDAV . '}calendar-user-address-set'	] = new Sabre_DAV_Property_HrefList(array('mailto:' . Tinebase_Core::getUser()->accountEmailAddress), false); 
         }
+        
+        if (Tinebase_Core::isLogLevel(Zend_Log::TRACE)) Tinebase_Core::getLogger()->trace(__METHOD__ . '::' . __LINE__ . print_r($properties, true));
         
         $response = array();
     
@@ -58,7 +64,7 @@ class Calendar_Frontend_WebDAV_Container extends Tinebase_WebDav_Container_Abstr
             }
         }
         
-        Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . print_r($response, true));
+        if (Tinebase_Core::isLogLevel(Zend_Log::TRACE)) Tinebase_Core::getLogger()->trace(__METHOD__ . '::' . __LINE__ . print_r($response, true));
         
         return $response;
     }
@@ -70,5 +76,17 @@ class Calendar_Frontend_WebDAV_Container extends Tinebase_WebDav_Container_Abstr
         }
         
         return $this->_controller;
+    }
+    
+    protected function _getCalendarVTimezone()
+    {
+        $timezone = Tinebase_Core::getPreference()->getValueForUser(Tinebase_Preference::TIMEZONE, Tinebase_Core::getUser()->getId());
+
+        // create vcalendar object with timezone information
+        $vcalendar = new Sabre_VObject_Component('CALENDAR');
+        $vcalendar->add(new Sabre_VObject_Component_VTimezone($timezone));
+        
+        // Taking out \r to not screw up the xml output
+        return str_replace("\r","", $vcalendar->serialize());
     }
 }
