b25b12c66ffbcb805366ce0f67f1ab9068bd53ac
Alexander Stintzing <alex@stintzing.net>
2011-11-17 21:15:08 +0100

    multiple edit dialog backend function still missing

--- tine20/Tinebase/js/widgets/dialog/MultipleEditDialogPlugin.js
+++ tine20/Tinebase/js/widgets/dialog/MultipleEditDialogPlugin.js
@@ -15,6 +15,8 @@ Tine.widgets.dialog.MultipleEditDialog.prototype = {
     
     disabledFields: [],
     
+    changes: null,
+    
     init: function(editDialog) {
         
         this.disabledFields = {};
@@ -24,22 +26,98 @@ Tine.widgets.dialog.MultipleEditDialog.prototype = {
         
         this.editDialog.selections = Ext.decode(this.editDialog.selections);
         this.form = this.editDialog.getForm();
-       
-        this.editDialog.on('load', this.loadCombinedRecord, this);
+        
+        this.editDialog.on('load', this.disableFields, this);
+        
+        this.editDialog.onRecordUpdate = this.editDialog.onRecordUpdate.createInterceptor(this.onRecordUpdate, this);
+        this.editDialog.onApplyChanges = function(button, event, closeWindow) { this.onRecordUpdate(); }       
     },
     
-    loadCombinedRecord: function() {
+    onRecordUpdate: function() {
+        this.changes = [];
+        this.form.items.each(function(item){
+            if(item.edited) {
+                this.changes.push({ name: item.getName(),  value: item.getValue() });
+            }
+        },this);
+        
+        Ext.MessageBox.confirm(_('Confirm'), String.format(_('Do you really want to change these {0} records?'), this.editDialog.selections.length), function(_btn) {
+            if (_btn == 'yes') {
+                Ext.MessageBox.wait(_('Please wait'), _('Applying changes'));
+                
+                Tine.log.debug('RClass',this.editDialog.recordClass);
+                Tine.log.debug('SELECTIONS',this.editDialog.selections);
+                Tine.log.debug('CHANGES',this.changes);
+        
+                // here comes the backend call
+                Ext.Ajax.request({
+                    url : 'index.php',
+                    params : { method : 'Tinebase.updateMultipleRecords', recordModel: this.editDialog.recordClass.getMeta('appName') + '_Model_' + this.editDialog.recordClass.getMeta('modelName'), changes: this.changes, selections: this.editDialog.selections },
+                    success : function(_result, _request) {
+                        Ext.MessageBox.hide();
+                        this.editDialog.purgeListeners();
+                        this.editDialog.window.close();                       
+                    }
+                });
+            }
+        }, this);
+        
+        return false;
+    },
+    
+    disableFields: function() {
         for(fieldName in this.editDialog.record.data) {
             if(typeof(this.editDialog.record.data[fieldName]) == 'object') continue;
-            Ext.each(this.editDialog.selections, function(selection,index){
-                if(selection[fieldName] != this.editDialog.record.data[fieldName]) {
-                    if(field = this.form.findField(fieldName)) {
-                        Tine.log.debug('FIELD:',field);
-                        field.disable();
-                    }
+            Ext.each(this.editDialog.selections, function(selection,index) {
+                var field = this.form.findField(fieldName);
+                if(field) {
+                    field.originalValue = this.editDialog.record.data[fieldName];
                     
+                    // set back??
+//                    field.on('dblclick',function(){
+//                        this.setValue(this.originalValue);
+//                        this.edited = false;
+//                        this.removeClass('applyToAll');
+//                        if(this.multi) {
+//                            this.addClass('notEdited');
+//                        }
+//                    });
+                    
+                if(selection[fieldName] != this.editDialog.record.data[fieldName]) {
+              
+                        
+                        field.setReadOnly(true);
+                        field.addClass('notEdited');
+                        field.multi = true;
+                        field.edited = false;
+                        field.on('focus',function() {
+                            this.setReadOnly(false);
+                            this.on('blur',function() {
+                                if(this.originalValue != this.getValue()) {
+                                    this.removeClass('notEdited');
+                                    this.addClass('applyToAll');
+                                    this.edited = true;
+                                }
+                            });
+                        });
+                                       
+                } else {
+                  
+                  field.on('blur',function() {
+                                if(this.originalValue != this.getValue()) {
+                                    this.addClass('applyToAll');
+                                    this.edited = true;
+                                } else {
+                                    this.edited = false;
+                                    this.removeClass('applyToAll');
+                                    if(this.multi) {
+                                        this.addClass('notEdited');
+                                    }
+                                }
+                            });  
+                }
                 }
             },this);   
-        }
+        } 
     }
 }
