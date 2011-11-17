0cb2be0cbfd9737388a8f61aba186f08ce5158f6
Alexander Stintzing <alex@stintzing.net>
2011-11-17 16:39:14 +0100

    multiple edit dialog

--- tine20/Tinebase/js/widgets/dialog/MultipleEditDialogPlugin.js
+++ tine20/Tinebase/js/widgets/dialog/MultipleEditDialogPlugin.js
@@ -1,5 +1,5 @@
 
-Ext.ns('Tine.widgets.dialog');
+Ext.ns('Tine.widgets.editDialog');
 
 Tine.widgets.dialog.MultipleEditDialog = function (config) {
     Ext.apply(this, config);
@@ -7,20 +7,39 @@ Tine.widgets.dialog.MultipleEditDialog = function (config) {
 
 Tine.widgets.dialog.MultipleEditDialog.prototype = {
     
-    init: function(dialog) {
-//        dialog.onEnable = dialog.onEnable.createSequence(this.onEnable, dialog);
-        dialog.onRender = dialog.onRender.createSequence(this.onRender, dialog);
-    },
-//    
-//    onEnable: function() {
-//          alert('asd');
-//          return;
-//          
-//    },
+    app: null,
     
-    onRender: function() {
-        Tine.log.debug('SEL',this.selections);
-        Tine.log.debug('REC',this.record);
-        Tine.log.debug('MODE',this.mode);
+    editDialog: null,
+    
+    form: null,
+    
+    disabledFields: [],
+    
+    init: function(editDialog) {
+        
+        this.disabledFields = {};
+        
+        this.editDialog = editDialog;
+        this.app = Tine.Tinebase.appMgr.get(this.editDialog.app);
+        
+        this.editDialog.selections = Ext.decode(this.editDialog.selections);
+        this.form = this.editDialog.getForm();
+       
+        this.editDialog.on('load', this.loadCombinedRecord, this);
+    },
+    
+    loadCombinedRecord: function() {
+        for(fieldName in this.editDialog.record.data) {
+            if(typeof(this.editDialog.record.data[fieldName]) == 'object') continue;
+            Ext.each(this.editDialog.selections, function(selection,index){
+                if(selection[fieldName] != this.editDialog.record.data[fieldName]) {
+                    if(field = this.form.findField(fieldName)) {
+                        Tine.log.debug('FIELD:',field);
+                        field.disable();
+                    }
+                    
+                }
+            },this);   
+        }
     }
 }
