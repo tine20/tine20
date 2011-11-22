a100fdec212725620c2c4ec2eaf2457e2e9c5de0
Philipp Schüle <p.schuele@metaways.de>
2011-11-22 15:38:54 +0100

    added testInternalInvitationRequestAutoProcess()
    ASSIGNED - # 5108: implement iMIP Frontend for Calendar 
    http://forge.tine20.org/mantisbt/view.php?id=5108

--- tests/tine20/Calendar/Frontend/iMIPTest.php
+++ tests/tine20/Calendar/Frontend/iMIPTest.php
@@ -21,6 +21,13 @@ require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'TestHe
 class Calendar_Frontend_iMIPTest extends PHPUnit_Framework_TestCase
 {
     /**
+     * event ids that should be deleted in tearDown
+     * 
+     * @var unknown_type
+     */
+    protected $_eventIdsToDelete = array();
+    
+    /**
      * iMIP frontent to be tested
      * 
      * @var Calendar_Frontend_iMIP
@@ -58,6 +65,9 @@ class Calendar_Frontend_iMIPTest extends PHPUnit_Framework_TestCase
      */
     protected function tearDown()
     {
+        if (! empty($this->_eventIdsToDelete)) {
+            Calendar_Controller_Event::getInstance()->delete($this->_eventIdsToDelete);
+        }
     }
     
     /**
@@ -73,14 +83,88 @@ class Calendar_Frontend_iMIPTest extends PHPUnit_Framework_TestCase
             'originator'     => 'l.kneschke@caldav.org',
         ));
         
-        $event = $iMIP->getEvent();
-        $this->assertEquals(3, count($event->attendee));
-        $this->assertEquals('test mit extern', $event->summary);
-        
         $this->_iMIPFrontend->autoProcess($iMIP);
-        return $iMIP;
+        $prepared = $this->_iMIPFrontend->prepareComponent($iMIP);
+
+        $this->assertEquals(3, count($prepared->event->attendee));
+        $this->assertEquals('test mit extern', $prepared->event->summary);
     }
     
+    /**
+     * testInternalInvitationRequestAutoProcess
+     */
+    public function testInternalInvitationRequestAutoProcess()
+    {
+        $event = $this->_getEvent();
+        $event = Calendar_Controller_Event::getInstance()->create($event);
+        $this->_eventIdsToDelete[] = $event->getId();
+        
+        // get iMIP invitation for event
+        $converter = Calendar_Convert_Event_VCalendar_Factory::factory(Calendar_Convert_Event_VCalendar_Factory::CLIENT_GENERIC);
+        $vevent = $converter->fromTine20Model($event);
+        //$vevent->METHOD = $method;
+        $ics = $vevent->serialize();
+        
+        $iMIP = new Calendar_Model_iMIP(array(
+            'id'             => Tinebase_Record_Abstract::generateUID(),
+        	'ics'            => $ics,
+            'method'         => 'REQUEST',
+            'originator'     => 'unittest@tine20.org',
+        ));
+        
+        $this->_iMIPFrontend->autoProcess($iMIP);
+        $prepared = $this->_iMIPFrontend->prepareComponent($iMIP);
+        
+        $this->assertEquals(2, count($prepared->event->attendee));
+        $this->assertEquals('Sleep very long', $prepared->event->summary);
+        $this->assertTrue(empty($prepared->preconditions));
+    }
+    
+    /**
+    * returns a simple event
+    *
+    * @return Calendar_Model_Event
+    */
+    protected function _getEvent()
+    {
+        return new Calendar_Model_Event(array(
+            'summary'     => 'Sleep very long',
+            'dtstart'     => '2012-03-25 01:00:00',
+            'dtend'       => '2012-03-25 11:15:00',
+            'description' => 'Early to bed and early to rise, makes a men healthy, wealthy and wise ... not.',
+            'attendee'    => $this->_getAttendee(),
+            'organizer'    => Tinebase_Core::getUser()->contact_id,
+            'uid'          => Calendar_Model_Event::generateUID(),
+        ));
+    }
+    
+    /**
+     * get test attendee
+     *
+     * @return Tinebase_Record_RecordSet
+     */
+    protected function _getAttendee()
+    {
+        $personas = Zend_Registry::get('personas');
+        $sclever = $personas['sclever'];
+        
+        return new Tinebase_Record_RecordSet('Calendar_Model_Attender', array(
+            array(
+                'user_id'        => Tinebase_Core::getUser()->contact_id,
+                'user_type'      => Calendar_Model_Attender::USERTYPE_USER,
+                'role'           => Calendar_Model_Attender::ROLE_REQUIRED,
+                'status_authkey' => Tinebase_Record_Abstract::generateUID(),
+            ),
+            array(
+                'user_id'        => $sclever->contact_id,
+                'user_type'      => Calendar_Model_Attender::USERTYPE_USER,
+                'role'           => Calendar_Model_Attender::ROLE_REQUIRED,
+                'status_authkey' => Tinebase_Record_Abstract::generateUID(),
+            ),
+        ));
+    }
+    
+
     /**
      * testExternalInvitationRequestProcess
      * 
@@ -91,18 +175,6 @@ class Calendar_Frontend_iMIPTest extends PHPUnit_Framework_TestCase
         // -- handle message with fmail (add to cache)
         // -- get $iMIP from message
         // -- test this->_iMIPFrontend->process($iMIP, $status);
-    }
-
-    /**
-     * testInternalInvitationRequest
-     * 
-     * @todo implement
-     */
-    public function testInternalInvitationRequest()
-    {
-        // -- create event
-        // -- get iMIP invitation for event
-        // -- autoProcess
     }
 
     /**
