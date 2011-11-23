tine20/tine20

commit 12c61ac20f48ebead83737ea17ec7f1bf9c6849c

added application name option to tag filter  (#4834)

tine20/Tinebase/Model/Filter/Tag.php
@@ -5,7 +5,7 @@
  * @package     Tinebase
  * @subpackage  Filter
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
- * @copyright   Copyright (c) 2007-2008 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2007-2011 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Cornelius Weiss <c.weiss@metaways.de>
  */
 
@@ -44,6 +44,10 @@ class Tinebase_Model_Filter_Tag extends Tinebase_Model_Filter_Abstract
      */
     protected function _setOptions(array $_options)
     {
+        if (! isset($_options['applicationName'])) {
+            throw new Tinebase_Exception_InvalidArgument('Tag filter needs the applicationName option');
+        }
+        
         $_options['idProperty'] = isset($_options['idProperty']) ? $_options['idProperty'] : 'id';
         
         $this->_options = $_options;
@@ -79,6 +83,9 @@ class Tinebase_Model_Filter_Tag extends Tinebase_Model_Filter_Abstract
             /* select */  array());
         
         $_select->where($db->quoteIdentifier("{$correlationName}.tag_id") .  $this->_opSqlMap[$this->_operator]['sqlop']);
+        
+        $app = Tinebase_Application::getInstance()->getApplicationByName($this->_options['applicationName']);
+        $_select->where($db->quoteIdentifier("{$correlationName}.application_id") . ' = ?', $app->getId());
     }
     
     /**

tests/tine20/Crm/JsonTest.php
@@ -4,7 +4,7 @@
  * 
  * @package     Crm
  * @license     http://www.gnu.org/licenses/agpl.html
- * @copyright   Copyright (c) 2008-2010 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2008-2011 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Philipp Schüle <p.schuele@metaways.de>
  * 
  */
@@ -13,10 +13,6 @@
  * Test helper
  */
 require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'TestHelper.php';
-
-if (!defined('PHPUnit_MAIN_METHOD')) {
-    define('PHPUnit_MAIN_METHOD', 'Crm_JsonTest::main');
-}
 
 /**
  * Test class for Crm_Json
@@ -220,6 +216,38 @@ class Crm_JsonTest extends Crm_AbstractTest
     }
     
     /**
+     * test tag filter (adds a contact with the same id + tag)
+     * 
+     * see bug #4834 (http://forge.tine20.org/mantisbt/view.php?id=4834)
+     */
+    public function testTagFilter()
+    {
+        $lead       = $this->_getLead();
+        $savedLead = $this->_instance->saveLead($lead->toArray());
+        
+        $sharedTagName = Tinebase_Record_Abstract::generateUID();
+        $tag = new Tinebase_Model_Tag(array(
+            'type'  => Tinebase_Model_Tag::TYPE_SHARED,
+            'name'  => $sharedTagName,
+            'description' => 'testTagFilter',
+            'color' => '#009B31',
+        ));
+        $contact    = $this->_getContact();
+        $contact->setId($savedLead['id']);
+        
+        $contact->tags = array($tag);
+        $savedContact = Addressbook_Controller_Contact::getInstance()->create($contact);
+        $tag = $savedContact->tags->getFirstRecord();
+        
+        $filter = array(
+            array('field' => 'tag',           'operator' => 'equals',       'value' => $tag->getId()),
+        );
+        
+        $result = $this->_instance->searchLeads($filter, array());
+        $this->assertEquals(0, $result['totalcount'], 'Should not find the lead!');
+    }    
+    
+    /**
      * get contact
      * 
      * @return Addressbook_Model_Contact
@@ -329,6 +357,5 @@ class Crm_JsonTest extends Crm_AbstractTest
         return array(
             array('field' => 'query',           'operator' => 'contains',       'value' => 'PHPUnit'),
         );
-        
     }
 }		

tine20/Addressbook/Model/ContactFilter.php
@@ -101,7 +101,10 @@ class Addressbook_Model_ContactFilter extends Tinebase_Model_Filter_FilterGroup
         'role'                 => array('filter' => 'Tinebase_Model_Filter_Text'),
         'pubkey'               => array('filter' => 'Tinebase_Model_Filter_Text'),
         'assistent'		       => array('filter' => 'Tinebase_Model_Filter_Text'),
-        'tag'                  => array('filter' => 'Tinebase_Model_Filter_Tag', 'options' => array('idProperty' => 'addressbook.id')),
+        'tag'                  => array('filter' => 'Tinebase_Model_Filter_Tag', 'options' => array(
+            'idProperty' => 'addressbook.id',
+            'applicationName' => 'Addressbook',
+        )),
         //'bday'               => array('filter' => 'Tinebase_Model_Filter_Date'),
         'last_modified_time'   => array('filter' => 'Tinebase_Model_Filter_Date'),
         'deleted_time'         => array('filter' => 'Tinebase_Model_Filter_DateTime'),

tine20/Addressbook/Model/ListFilter.php
@@ -44,7 +44,10 @@ class Addressbook_Model_ListFilter extends Tinebase_Model_Filter_FilterGroup
         ),
         'email'                => array('filter' => 'Tinebase_Model_Filter_Text'),
         'name'                 => array('filter' => 'Tinebase_Model_Filter_Text'),
-        'tag'                  => array('filter' => 'Tinebase_Model_Filter_Tag', 'options' => array('idProperty' => 'addressbook.id')),
+        'tag'                  => array('filter' => 'Tinebase_Model_Filter_Tag', 'options' => array(
+            'idProperty' => 'addressbook.id',
+            'applicationName' => 'Addressbook',
+        )),
         'last_modified_time'   => array('filter' => 'Tinebase_Model_Filter_Date'),
         'deleted_time'         => array('filter' => 'Tinebase_Model_Filter_DateTime'),
         'creation_time'        => array('filter' => 'Tinebase_Model_Filter_Date'),

tine20/Calendar/Model/EventFilter.php
@@ -47,7 +47,10 @@ class Calendar_Model_EventFilter extends Tinebase_Model_Filter_FilterGroup
         'organizer'             => array('filter' => 'Addressbook_Model_ContactIdFilter'),
         //'class'               => array('filter' => 'Tinebase_Model_Filter_Text'),
         //'status'              => array('filter' => 'Tinebase_Model_Filter_Text'),
-        'tag'                   => array('filter' => 'Tinebase_Model_Filter_Tag', 'options' => array('idProperty' => 'cal_events.id')),
+        'tag'                   => array('filter' => 'Tinebase_Model_Filter_Tag', 'options' => array(
+            'idProperty' => 'cal_events.id',
+            'applicationName' => 'Calendar',
+        )),
         'grants'                => array('filter' => 'Calendar_Model_GrantFilter'),
         // NOTE using dtdstart and dtend filters may not lead to the desired result. 
         //      you need to use the period filter to filter for events in a given period

tine20/Courses/Model/CourseFilter.php
@@ -33,7 +33,10 @@ class Courses_Model_CourseFilter extends Tinebase_Model_Filter_FilterGroup
         'group_id'       => array('filter' => 'Tinebase_Model_Filter_Id'),
         'query'          => array('filter' => 'Tinebase_Model_Filter_Query', 'options' => array('fields' => array('name', 'type'))),
         'name'           => array('filter' => 'Tinebase_Model_Filter_Text'),
-        'tag'            => array('filter' => 'Tinebase_Model_Filter_Tag', 'options' => array('idProperty' => 'courses.id')),
+        'tag'            => array('filter' => 'Tinebase_Model_Filter_Tag', 'options' => array(
+            'idProperty' => 'courses.id',
+            'applicationName' => 'Courses',
+        )),
         'type'           => array('filter' => 'Tinebase_Model_Filter_ForeignId', 'options' => array('filtergroup' => 'Tinebase_Model_DepartmentFilter', 'controller' => 'Tinebase_Department')),
         //'group_id'       => array('filter' => 'Tinebase_Model_Filter_ForeignId', 'options' => array('filtergroup' => 'Tinebase_Model_GroupFilter', 'controller' => 'Tinebase_Group')),
     );

tine20/Crm/Model/LeadFilter.php
@@ -40,7 +40,10 @@ class Crm_Model_LeadFilter extends Tinebase_Model_Filter_FilterGroup
         'query'                 => array('filter' => 'Crm_Model_LeadQueryFilter'),
         'description'           => array('filter' => 'Tinebase_Model_Filter_Text'),
         'lead_name'             => array('filter' => 'Tinebase_Model_Filter_Text'),
-        'tag'                   => array('filter' => 'Tinebase_Model_Filter_Tag', 'options' => array('idProperty' => 'metacrm_lead.id')),
+        'tag'                   => array('filter' => 'Tinebase_Model_Filter_Tag', 'options' => array(
+            'idProperty' => 'metacrm_lead.id',
+            'applicationName' => 'Crm',
+        )),
         'probability'           => array('filter' => 'Tinebase_Model_Filter_Int'),
         'turnover'              => array('filter' => 'Tinebase_Model_Filter_Int'),
         'leadstate_id'          => array('filter' => 'Tinebase_Model_Filter_Int'),

tine20/ExampleApplication/Model/ExampleRecordFilter.php
@@ -32,7 +32,10 @@ class ExampleApplication_Model_ExampleRecordFilter extends Tinebase_Model_Filter
         'query'          => array('filter' => 'Tinebase_Model_Filter_Query', 'options' => array('fields' => array('name', /*'...'*/))),
         'container_id'   => array('filter' => 'Tinebase_Model_Filter_Container', 'options' => array('applicationName' => 'ExampleApplication')),
         'id'             => array('filter' => 'Tinebase_Model_Filter_Id'),
-        'tag'            => array('filter' => 'Tinebase_Model_Filter_Tag', 'options' => array('idProperty' => 'example_application_record.id')),
+        'tag'            => array('filter' => 'Tinebase_Model_Filter_Tag', 'options' => array(
+            'idProperty' => 'example_application_record.id',
+            'applicationName' => 'ExampleApplication',
+        )),
         
         // @todo add filters
         /*

commit 9fadc162426ac4afc192ac7b53135e31de3c74e3

fixed not operator in tags filter (was broken by #4834) + added test
ASSIGNED - # 5128: tag filter 'is not' operator is not working 
http://forge.tine20.org/mantisbt/view.php?id=5128

tine20/Tinebase/Model/Filter/Tag.php
@@ -73,19 +73,19 @@ class Tinebase_Model_Filter_Tag extends Tinebase_Model_Filter_Abstract
         $db = Tinebase_Core::getDb();
         $idProperty = $db->quoteIdentifier($this->_options['idProperty']);
         
+        $app = Tinebase_Application::getInstance()->getApplicationByName($this->_options['applicationName']);
+
         $correlationName = Tinebase_Record_Abstract::generateUID() . $this->_value . 'tag';
-        
         // per left join we add a tag column named as the tag and filter this joined column
         // NOTE: we name the column we join like the tag, to be able to join multiple tag criteria (multiple invocations of this function)
         $_select->joinLeft(
             /* what */    array($correlationName => SQL_TABLE_PREFIX . 'tagging'), 
-            /* on   */    $db->quoteIdentifier("{$correlationName}.record_id") . " = $idProperty AND " . $db->quoteIdentifier("{$correlationName}.tag_id") . " = " . $db->quote($this->_value),
-            /* select */  array());
+            /* on   */    $db->quoteIdentifier("{$correlationName}.record_id") . " = $idProperty " .
+                " AND " . $db->quoteIdentifier("{$correlationName}.application_id") . " = " . $db->quote($app->getId()) .
+                " AND " . $db->quoteIdentifier("{$correlationName}.tag_id") . " = " . $db->quote($this->_value),
+        /* select */  array());
         
         $_select->where($db->quoteIdentifier("{$correlationName}.tag_id") .  $this->_opSqlMap[$this->_operator]['sqlop']);
-        
-        $app = Tinebase_Application::getInstance()->getApplicationByName($this->_options['applicationName']);
-        $_select->where($db->quoteIdentifier("{$correlationName}.application_id") . ' = ?', $app->getId());
     }
     
     /**

tests/tine20/Addressbook/JsonTest.php
@@ -390,5 +390,41 @@ class Addressbook_JsonTest extends PHPUnit_Framework_TestCase
         $sharedTagToDelete = Tinebase_Tags::getInstance()->getTagByName($sharedTagName);
         $personalTagToDelete = Tinebase_Tags::getInstance()->getTagByName($personalTagName);
         Tinebase_Tags::getInstance()->deleteTags(array($sharedTagToDelete->getId(), $personalTagToDelete->getId()));
-    }    
-}		
+    }
+    
+    /**
+     * testSearchContactsWithTagIsNotFilter
+     */
+    public function testSearchContactsWithTagIsNotFilter()
+    {
+        $allContacts = $this->_instance->searchContacts(array(), array());
+        
+        $filter = new Addressbook_Model_ContactFilter(array(
+            array(
+                'field'    => 'n_fileas',
+                'operator' => 'equals',
+                'value'    =>  Tinebase_Core::getUser()->accountDisplayName
+            )
+        ));
+        $sharedTagName = Tinebase_Record_Abstract::generateUID();
+        $tag = new Tinebase_Model_Tag(array(
+            'type'  => Tinebase_Model_Tag::TYPE_SHARED,
+            'name'  => $sharedTagName,
+            'description' => 'testImport',
+            'color' => '#009B31',
+        ));
+        $tag = Tinebase_Tags::getInstance()->attachTagToMultipleRecords($filter, $tag);
+        
+        $filter = array(array(
+            'field'    => 'tag',
+            'operator' => 'not',
+            'value'    => $tag->getId()
+        ));
+        $allContactsWithoutTheTag = $this->_instance->searchContacts($filter, array());
+        
+        $this->assertTrue(count($allContactsWithoutTheTag['totalcount']) > 0);
+        $this->assertEquals($allContacts['totalcount']-1, $allContactsWithoutTheTag['totalcount']);
+        
+        $sharedTagToDelete = Tinebase_Tags::getInstance()->getTagByName($sharedTagName);
+    }
+}