f3f7f2d6f366586396506ac05d11d66d6924ab30
Lars Kneschke <l.kneschke@metaways.de>
2011-12-13 22:39:40 +0100

    clients must always authenticate now, before we return some data

--- tine20/ActiveSync/Server/Http.php
+++ tine20/ActiveSync/Server/Http.php
@@ -79,22 +79,23 @@ class ActiveSync_Server_Http implements Tinebase_Server_Interface
         
         $syncFrontend = new ActiveSync_Frontend_Http();
         
+        if($syncFrontend->authenticate($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'], $_SERVER['REMOTE_ADDR']) !== true) {
+            header('WWW-Authenticate: Basic realm="ActiveSync for Tine 2.0"');
+            header('HTTP/1.1 401 Unauthorized');
+            return;
+        }
+        if(Tinebase_Core::getUser()->hasRight('ActiveSync', Tinebase_Acl_Rights::RUN) !== true) {
+            header('HTTP/1.1 403 ActiveSync not enabled for account ' . $_SERVER['PHP_AUTH_USER']);
+            echo "<b>ERROR</b>!<br>ActiveSync is not enabled for account {$_SERVER['PHP_AUTH_USER']}.";
+            return;
+        }
+        
         switch($_SERVER['REQUEST_METHOD']) {
             case 'OPTIONS':
                 $syncFrontend->handleOptions();
                 break;
                 
             case 'POST':
-                if($syncFrontend->authenticate($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'], $_SERVER['REMOTE_ADDR']) !== true) {
-                    header('WWW-Authenticate: Basic realm="ActiveSync for Tine 2.0"');
-                    header('HTTP/1.1 401 Unauthorized');
-                    return;                            
-                }
-                if(Tinebase_Core::getUser()->hasRight('ActiveSync', Tinebase_Acl_Rights::RUN) !== true) {
-                    header('HTTP/1.1 403 ActiveSync not enabled for account ' . $_SERVER['PHP_AUTH_USER']);
-                    Tinebase_Core::getLogger()->warn(__METHOD__ . '::' . __LINE__ . ' ActiveSync not enabled for account ' . $_SERVER['PHP_AUTH_USER']);
-                    return;                            
-                }
                 if(count($_GET) == 1) {
                     $arrayKeys = array_keys($_GET);
                     $parameters = $this->decodeRequestParameters($arrayKeys[0]);
@@ -108,16 +109,6 @@ class ActiveSync_Server_Http implements Tinebase_Server_Interface
                 break;
                 
             case 'GET':
-                if($syncFrontend->authenticate($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'], $_SERVER['REMOTE_ADDR']) !== true) {
-                    header('WWW-Authenticate: Basic realm="ActiveSync for Tine 2.0"');
-                    header('HTTP/1.1 401 Unauthorized');
-                    return;                            
-                }
-                if(Tinebase_Core::getUser()->hasRight('ActiveSync', Tinebase_Acl_Rights::RUN) !== true) {
-                    header('HTTP/1.1 403 ActiveSync not enabled for account ' . $_SERVER['PHP_AUTH_USER']);
-                    echo "<b>ERROR</b>!<br>ActiveSync is not enabled for account {$_SERVER['PHP_AUTH_USER']}.";
-                    return;                            
-                }
                 echo "It works!<br>Your username is: {$_SERVER['PHP_AUTH_USER']} and your IP address is: {$_SERVER['REMOTE_ADDR']}.";
                 break;
         }
@@ -130,10 +121,12 @@ class ActiveSync_Server_Http implements Tinebase_Server_Interface
      */
     protected function _getRequestParameters()
     {
-        $result['protocolVersion']  = $_SERVER['HTTP_MS_ASPROTOCOLVERSION'];
-        $result['command']          = $_GET['Cmd'];
-        $result['deviceId']         = $_GET['DeviceId'];
-        $result['deviceType']       = $_GET['DeviceType'];
+        $result = array(
+            'protocolVersion'  => $_SERVER['HTTP_MS_ASPROTOCOLVERSION'],
+            'command'          => $_GET['Cmd'],
+            'deviceId'         => $_GET['DeviceId'],
+            'deviceType'       => $_GET['DeviceType']
+        );
         
         return $result;
     }
