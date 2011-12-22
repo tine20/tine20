717ca0fdceddd8e79e65ebd58522bc103cb40c77
Philipp Schüle <p.schuele@metaways.de>
2011-12-22 12:46:15 +0100

    added some more logging
    ASSIGNED - # 4920: unreadcount in folder tree is not updated sometimes
    when messages are read 
    http://forge.tine20.org/mantisbt/view.php?id=4920

--- tine20/Felamimail/js/GridPanel.js
+++ tine20/Felamimail/js/GridPanel.js
@@ -1083,6 +1083,8 @@ Tine.Felamimail.GridPanel = Ext.extend(Tine.widgets.grid.GridPanel, {
     onStoreLoad: function(store, records, options) {
         this.supr().onStoreLoad.apply(this, arguments);
         
+        Tine.log.debug('Tine.Felamimail.GridPanel::onStoreLoad(): store loaded new records.');
+        
         this.updateQuotaBar(options.params.filter);
     },
     
