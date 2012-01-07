f7dfc8361e8b1a5b4baa96aad16c069e0cb436c4
Lars Kneschke <l.kneschke@metaways.de>
2012-01-07 23:13:50 +0100

    removed old code

--- tine20/Filemanager/Frontend/WebDAV/Directory.php
+++ tine20/Filemanager/Frontend/WebDAV/Directory.php
@@ -16,19 +16,6 @@
  */
 class Filemanager_Frontend_WebDAV_Directory extends Filemanager_Frontend_WebDAV_Node implements Sabre_DAV_ICollection
 {
-    #public function __construct($_path) 
-    #{
-        #parent::__construct($_path);
-                
-        #if ($this->_container == null) {
-        #    throw new Sabre_DAV_Exception_FileNotFound('The file with name: ' . $this->_path . ' could not be found');
-        #}
-        
-        #if (!Tinebase_Core::getUser()->hasGrant($this->_container, Tinebase_Model_Grants::GRANT_READ)) {
-        #    throw new Sabre_DAV_Exception_FileNotFound('The file with name: ' . $this->_path . ' could not be found');
-        #}
-    #}
-    
     /**
      * return list of children
      * @return array list of children
