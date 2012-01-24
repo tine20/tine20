fdffd9874ef01751aa9435e8b76c44d08a35ebb1
Lars Kneschke <l.kneschke@metaways.de>
2012-01-24 05:02:24 +0100

    new headers for current ActiveSync version

--- tine20/ActiveSync/Command/Options.php
+++ tine20/ActiveSync/Command/Options.php
@@ -31,11 +31,12 @@ class ActiveSync_Command_Options
         // same header like Exchange 2003
         header("MS-Server-ActiveSync: 8.3");
         header("MS-ASProtocolVersions: 2.5,12.0");
-        header("MS-ASProtocolCommands: CreateCollection,DeleteCollection,FolderCreate,FolderDelete,FolderSync,FolderUpdate,GetAttachment,GetItemEstimate,MeetingResponse,MoveCollection,MoveItems,Provision,ResolveRecipients,Ping,SendMail,Search,Settings,SmartForward,SmartReply,Sync");
+        header("MS-ASProtocolCommands: CreateCollection,DeleteCollection,FolderCreate,FolderDelete,FolderSync,FolderUpdate,GetAttachment,GetItemEstimate,ItemOperations,MeetingResponse,MoveCollection,MoveItems,Provision,ResolveRecipients,Ping,SendMail,Search,Settings,SmartForward,SmartReply,Sync");
         
         // same header like Exchange 2xxx???
-        #header("MS-Server-ActiveSync: 8.3");
+        #header('MS-Server-ActiveSync:  14.00.0536.000');
         #header("MS-ASProtocolVersions: 2.5,12.0,12.1,14.0,14.1");
         #header("MS-ASProtocolCommands: CreateCollection,DeleteCollection,FolderCreate,FolderDelete,FolderSync,FolderUpdate,GetAttachment,GetHierarchy,GetItemEstimate,ItemOperations,MeetingResponse,MoveCollection,MoveItems,Provision,ResolveRecipients,Ping,SendMail,Search,Settings,SmartForward,SmartReply,Sync,ValidateCert");
+        #header('MS-ASProtocolRevisions: 12.1r1');
     }    
 }
