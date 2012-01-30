abdb9c71216a45a39ffd0432df34f95895e867ac
Lars Kneschke <l.kneschke@metaways.de>
2012-01-30 08:53:51 +0100

    improved MoveItems test

--- tests/Syncope/Command/MoveItemsTests.php
+++ tests/Syncope/Command/MoveItemsTests.php
@@ -27,9 +27,25 @@ class Syncope_Command_MoveItemsTests extends Syncope_Command_ATestCase
         PHPUnit_TextUI_TestRunner::run($suite);
     }
     
+    protected function setUp()
+    {
+        parent::setUp();
+        
+        Syncope_Data_AData::$entries['Syncope_Data_Contacts']['addressbookFolderId']['moveItem'] = array(
+        	'FirstName' => 'Lars', 
+        	'LastName'  => 'Kneschke'
+        );
+    }
+    
+    protected function tearDown()
+    {
+        parent::tearDown();
+        unset(Syncope_Data_AData::$entries['Syncope_Data_Contacts']['addressbookFolderId']['moveItem']);
+    }
+    
     /**
      */
-    public function testMove()
+    public function testMoveInvalidSrcFolder()
     {
         // do initial sync first
         $doc = new DOMDocument();
@@ -57,21 +73,51 @@ class Syncope_Command_MoveItemsTests extends Syncope_Command_ATestCase
         $xpath = new DomXPath($responseDoc);
         $xpath->registerNamespace('Move', 'uri:Move');
         
-        // @todo improve test
-        return;
+        $nodes = $xpath->query('//Move:Moves/Move:Response/Move:Status');
+        $this->assertEquals(1, $nodes->length, $responseDoc->saveXML());
+        $this->assertEquals(Syncope_Command_MoveItems::STATUS_INVALID_SOURCE, $nodes->item(0)->nodeValue, $responseDoc->saveXML());
+    }
         
-        $nodes = $xpath->query('//ItemOperations:ItemOperations/ItemOperations:Status');
+    /**
+     */
+    public function testMove()
+    {
+        // do initial sync first
+        $doc = new DOMDocument();
+        $doc->loadXML('<?xml version="1.0" encoding="utf-8"?>
+            <!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/">
+            <FolderSync xmlns="uri:ItemOperations"><SyncKey>0</SyncKey></FolderSync>'
+        );
+        
+        $folderSync = new Syncope_Command_FolderSync($doc, $this->_device, null);
+        $folderSync->handle();
+        $responseDoc = $folderSync->getResponse();
+        #$responseDoc->formatOutput = true; echo $responseDoc->saveXML();
+        
+        $doc = new DOMDocument();
+        $doc->loadXML('<?xml version="1.0" encoding="utf-8"?>
+            <!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/">
+            <Moves xmlns="uri:Move"><Move><SrcMsgId>moveItem</SrcMsgId><SrcFldId>addressbookFolderId</SrcFldId><DstFldId>anotherAddressbookFolderId</DstFldId></Move></Moves>'
+        );
+        
+        $moveItems = new Syncope_Command_MoveItems($doc, $this->_device, null);
+        $moveItems->handle();
+        $responseDoc = $moveItems->getResponse();
+        #$responseDoc->formatOutput = true; echo $responseDoc->saveXML();
+        
+        $xpath = new DomXPath($responseDoc);
+        $xpath->registerNamespace('Move', 'uri:Move');
+        
+        $nodes = $xpath->query('//Move:Moves/Move:Response/Move:Status');
         $this->assertEquals(1, $nodes->length, $responseDoc->saveXML());
         $this->assertEquals(Syncope_Command_MoveItems::STATUS_SUCCESS, $nodes->item(0)->nodeValue, $responseDoc->saveXML());
         
-        $nodes = $xpath->query('//ItemOperations:ItemOperations/ItemOperations:Response/ItemOperations:Fetch/ItemOperations:Status');
+        $nodes = $xpath->query('//Move:Moves/Move:Response/Move:SrcMsgId');
         $this->assertEquals(1, $nodes->length, $responseDoc->saveXML());
-        $this->assertEquals(Syncope_Command_MoveItems::STATUS_SUCCESS, $nodes->item(0)->nodeValue, $responseDoc->saveXML());
+        $this->assertEquals('moveItem', $nodes->item(0)->nodeValue, $responseDoc->saveXML());
         
-        $nodes = $xpath->query('//ItemOperations:ItemOperations/ItemOperations:Response/ItemOperations:Fetch/ItemOperations:Properties/Email:Subject');
+        $nodes = $xpath->query('//Move:Moves/Move:Response/Move:DstMsgId');
         $this->assertEquals(1, $nodes->length, $responseDoc->saveXML());
-        $this->assertEquals('Subject of the email', $nodes->item(0)->nodeValue, $responseDoc->saveXML());
-
-        $this->assertEquals("uri:Email", $responseDoc->lookupNamespaceURI('Email'), $responseDoc->saveXML());
+        $this->assertEquals('moveItem', $nodes->item(0)->nodeValue, $responseDoc->saveXML());
     }    
 }
