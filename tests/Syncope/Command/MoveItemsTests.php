170073cd30965e0917ae32035ca80e756d92d563
Lars Kneschke <l.kneschke@metaways.de>
2012-01-30 09:01:12 +0100

    improve test coverage

--- tests/Syncope/Command/MoveItemsTests.php
+++ tests/Syncope/Command/MoveItemsTests.php
@@ -80,6 +80,41 @@ class Syncope_Command_MoveItemsTests extends Syncope_Command_ATestCase
         
     /**
      */
+    public function testMoveInvalidDstFolder()
+    {
+        // do initial sync first
+        $doc = new DOMDocument();
+        $doc->loadXML('<?xml version="1.0" encoding="utf-8"?>
+            <!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/">
+            <FolderSync xmlns="uri:ItemOperations"><SyncKey>0</SyncKey></FolderSync>'
+        );
+        
+        $folderSync = new Syncope_Command_FolderSync($doc, $this->_device, null);
+        $folderSync->handle();
+        $responseDoc = $folderSync->getResponse();
+        
+        
+        $doc = new DOMDocument();
+        $doc->loadXML('<?xml version="1.0" encoding="utf-8"?>
+            <!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/">
+            <Moves xmlns="uri:Move"><Move><SrcMsgId>2246b0b87ee914e283d6c53717cc36c68cacd187</SrcMsgId><SrcFldId>addressbookFolderId</SrcFldId><DstFldId>cf11782725c1e132d05fec5a7cd9862694933003</DstFldId></Move></Moves>'
+        );
+        
+        $moveItems = new Syncope_Command_MoveItems($doc, $this->_device, null);
+        $moveItems->handle();
+        $responseDoc = $moveItems->getResponse();
+        #$responseDoc->formatOutput = true; echo $responseDoc->saveXML();
+        
+        $xpath = new DomXPath($responseDoc);
+        $xpath->registerNamespace('Move', 'uri:Move');
+        
+        $nodes = $xpath->query('//Move:Moves/Move:Response/Move:Status');
+        $this->assertEquals(1, $nodes->length, $responseDoc->saveXML());
+        $this->assertEquals(Syncope_Command_MoveItems::STATUS_INVALID_DESTINATION, $nodes->item(0)->nodeValue, $responseDoc->saveXML());
+    }
+        
+    /**
+     */
     public function testMove()
     {
         // do initial sync first
