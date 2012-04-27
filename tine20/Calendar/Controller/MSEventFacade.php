0a303d0ffe64a89ccaa6588bf98218c8fb11e8e1
Philipp Schüle <p.schuele@metaways.de>
2012-04-27 21:38:30 +0200

    #6256: Log-Messages: filtering field without indices
    - added index for 'is_deleted' before filtering
    
    https://forge.tine20.org/mantisbt/view.php?id=6256
    Change-Id: I48174577bef8f15eb9e91c8dd2da4c0a8195e640
    Reviewed-on: https://gerrit.tine20.org/tine20/498
    Tested-by: jenkins user
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Calendar/Controller/MSEventFacade.php
+++ tine20/Calendar/Controller/MSEventFacade.php
@@ -268,6 +268,7 @@ class Calendar_Controller_MSEventFacade implements Tinebase_Controller_Record_In
         }
         
         $exceptions = $_event->exdate instanceof Tinebase_Record_RecordSet ? $_event->exdate : new Tinebase_Record_RecordSet('Calendar_Model_Event');
+        $exceptions->addIndices(array('is_deleted'));
         $_event->exdate = $exceptions->getOriginalDtStart();
         
         $currentPersistentExceptions = $_event->rrule ? $this->_eventController->getRecurExceptions($_event, FALSE) : new Tinebase_Record_RecordSet('Calendar_Model_Event');
@@ -394,6 +395,7 @@ class Calendar_Controller_MSEventFacade implements Tinebase_Controller_Record_In
     {
         $events = new Tinebase_Record_RecordSet('Calendar_Model_Event', array($_record));
         if ($_record->exdate instanceof Tinebase_Record_RecordSet) {
+            $_record->exdate->addIndices(array('is_deleted'));
             $events->merge($_record->exdate->filter('is_deleted', 0));
         }
         
