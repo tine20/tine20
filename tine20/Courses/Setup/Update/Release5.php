87e93da68f836b88d2fa1c9f014fda0373b7bc92
Philipp Schüle <p.schuele@metaways.de>
2012-05-03 12:20:03 +0200

    #5884: internet / fileserver activation in edit dialog
    - converted old internet column to keyfield
    
    https://forge.tine20.org/mantisbt/view.php?id=5884
    Change-Id: I4b25617a4da7d0a766cbfd1031bf02d4e4c038ec
    Reviewed-on: https://gerrit.tine20.org/tine20/517
    Tested-by: jenkins user
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Courses/Setup/Update/Release5.php
+++ tine20/Courses/Setup/Update/Release5.php
@@ -5,8 +5,8 @@
  * @package     Courses
  * @subpackage  Setup
  * @license     http://www.gnu.org/licenses/agpl.html AGPL3
- * @copyright   Copyright (c) 2011 Metaways Infosystems GmbH (http://www.metaways.de)
- * @author      Philipp Schuele <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2011-2012 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schüle <p.schuele@metaways.de>
  */
 
 class Courses_Setup_Update_Release5 extends Setup_Update_Abstract
@@ -20,26 +20,56 @@ class Courses_Setup_Update_Release5 extends Setup_Update_Abstract
         $pfe = new Tinebase_PersistentFilter_Backend_Sql();
         
         $commonValues = array(
-                'account_id'        => NULL,
-                    'application_id'    => Tinebase_Application::getInstance()->getApplicationByName('Courses')->getId(),
-                    'model'             => 'Courses_Model_CourseFilter',
+            'account_id'        => NULL,
+            'application_id'    => Tinebase_Application::getInstance()->getApplicationByName('Courses')->getId(),
+            'model'             => 'Courses_Model_CourseFilter',
         );
         
         $pfe->create(new Tinebase_Model_PersistentFilter(array_merge($commonValues, array(
-                'name'              => Courses_Preference::DEFAULTPERSISTENTFILTER_NAME,
-                'description'       => "All courses", // _("All courses")
-                    'filters'           => array(
-                        array(
-                            'field'     => 'is_deleted',
-        'operator'  => 'equals',
-        'value'=> '0'
-
-                    )),
-            
-            )                
+            'name'              => Courses_Preference::DEFAULTPERSISTENTFILTER_NAME,
+            'description'       => "All courses", // _("All courses")
+                'filters'           => array(
+                    array(
+                        'field'     => 'is_deleted',
+                        'operator'  => 'equals',
+                        'value'=> '0'
+                    )
+                ),
+            )
         )));
         
         $this->setApplicationVersion('Courses', '5.1');
-    }    
+    }
     
+    /**
+    * update to 5.2
+    * - internet column now is a textfield / keyfield
+    * 
+    * @return void
+    */
+    public function update_1()
+    {
+        Courses_Setup_Initialize::createInternetAccessKeyfield();
+        
+        // fetch old values and update courses
+        $stmt = $this->_db->query("SELECT id,internet FROM `" . SQL_TABLE_PREFIX . "courses`");
+        $internetAccess = $stmt->fetchAll(Zend_Db::FETCH_ASSOC);
+        
+        $declaration = new Setup_Backend_Schema_Field_Xml('
+            <field>
+                <name>internet</name>
+                <type>text</type>
+                <length>64</length>
+            </field>');
+        $this->_backend->alterCol('courses', $declaration);
+        
+        foreach ($internetAccess as $data) {
+            $this->_db->update(SQL_TABLE_PREFIX . 'courses', array(
+                'internet' => ($data['internet']) ? 'ON' : 'OFF',
+            ), "`id` = '{$data['id']}'");
+        }
+        
+        $this->setTableVersion('courses', 5);
+        $this->setApplicationVersion('Courses', '5.2');
+    }
 }
