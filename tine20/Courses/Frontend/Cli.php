93dc3b5ad62674a604f3783de017bb0850a58844
Philipp Schüle <p.schuele@metaways.de>
2012-05-04 15:16:22 +0200

    #6370: add cli function for setting filtered internet
    - added CLI fn and test
    - enable Courses app for CLI test
    
    https://forge.tine20.org/mantisbt/view.php?id=6370
    
    Change-Id: I24ccd817175a831e900fcae31ecb5b769e3971ab
    Reviewed-on: https://gerrit.tine20.org/tine20/530
    Tested-by: jenkins user
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Courses/Frontend/Cli.php
+++ tine20/Courses/Frontend/Cli.php
@@ -5,18 +5,18 @@
  * @subpackage  Frontend
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Cornelius Weiss <c.weiss@metaways.de>
- * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2009-2012 Metaways Infosystems GmbH (http://www.metaways.de)
  */
 
 /**
  * cli server for timetracker
  *
- * This class handles cli requests for the timetracker
+ * This class handles cli requests for the Courses app
  *
  * @package     Courses
  * @subpackage  Frontend
  */
-class Courses_Frontend_Cli
+class Courses_Frontend_Cli extends Tinebase_Frontend_Cli_Abstract
 {
     /**
      * the internal name of the application
@@ -39,17 +39,29 @@ class Courses_Frontend_Cli
     );
     
     /**
-     * echos usage information
-     *
+     * set all courses to internet = FILTERED
+     * 
+     * @return integer
      */
-    public function getHelp()
+    public function resetCoursesInternetAccess()
     {
-        foreach ($this->_help as $functionHelp) {
-            echo $functionHelp['description']."\n";
-            echo "parameters:\n";
-            foreach ($functionHelp['params'] as $param => $description) {
-                echo "$param \t $description \n";
-            }
+        if (! $this->_checkAdminRight()) {
+            return 2;
         }
+        
+        $config = Courses_Config::getInstance();
+        if (! isset($config->{Courses_Config::INTERNET_ACCESS_GROUP_ON}) || ! isset($config->{Courses_Config::INTERNET_ACCESS_GROUP_FILTERED})) {
+            echo "No internet groups defined in config. Exiting\n";
+            return 2;
+        }
+        
+        $filter = new Courses_Model_CourseFilter(array(
+            array('field' => 'internet', 'operator' => 'not', 'value' => 'FILTERED')
+        ));
+        $data = array('internet' => 'FILTERED');
+        $result = Courses_Controller_Course::getInstance()->updateMultiple($filter, $data);
+        
+        echo "Updated " . $result['totalcount'] . " Course(s)\n";
+        return 0;
     }
 }
