c144dc641e7a46a2c1d407de2684cc795b4ad2ae
Alexander Stintzing <alex@stintzing.net>
2012-05-11 13:18:06 +0200

    added relation property to timesheet model
    Change-Id: If90faecdae39b39c4365383f8ce864113de097ef
    Reviewed-on: https://gerrit.tine20.org/tine20/605
    Tested-by: jenkins user
    Reviewed-by: Cornelius Weiß <c.weiss@metaways.de>
    Tested-by: Cornelius Weiß <c.weiss@metaways.de>

--- tine20/Timetracker/Model/Timesheet.php
+++ tine20/Timetracker/Model/Timesheet.php
@@ -64,6 +64,7 @@ class Timetracker_Model_Timesheet extends Tinebase_Record_Abstract
         'deleted_time'          => array(Zend_Filter_Input::ALLOW_EMPTY => true),
         'deleted_by'            => array(Zend_Filter_Input::ALLOW_EMPTY => true),
     // other related data
+        'relations'             => array(Zend_Filter_Input::ALLOW_EMPTY => true, Zend_Filter_Input::DEFAULT_VALUE => NULL),
         'notes'                 => array(Zend_Filter_Input::ALLOW_EMPTY => true),
         'tags'                  => array(Zend_Filter_Input::ALLOW_EMPTY => true),   
     );
