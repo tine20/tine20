f433400caa6ad76bd1f3da0e934f21393bca2403
Alexander Stintzing <a.stintzing@metaways.de>
2012-05-18 14:54:14 +0200

    multiedit fix - customfields not in cfTab
    Change-Id: Id0fdb4c6546cdd44fd510650ca1c0f11b0e53d01
    Reviewed-on: https://gerrit.tine20.org/tine20/628
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>
    
    Conflicts:
    
    	tine20/Tinebase/js/widgets/dialog/MultipleEditDialogPlugin.js

--- tine20/Tinebase/js/widgets/dialog/MultipleEditDialogPlugin.js
+++ tine20/Tinebase/js/widgets/dialog/MultipleEditDialogPlugin.js
@@ -149,12 +149,15 @@ Tine.widgets.dialog.MultipleEditDialogPlugin.prototype = {
             keys.push({key: key, type: 'default', formField: field, recordKey: key});
         }, this);
         
-        this.editDialog.cfConfigs.each(function(config) {
-            var field = this.form.findField('customfield_' + config.data.name);
-            if (!field) return true;
-            keys.push({key: config.data.name, type: 'custom', formField: field, recordKey: '#' + config.data.name});
-        }, this);
-
+        var cfConfigs = Tine.widgets.customfields.ConfigManager.getConfigs(this.app, this.editDialog.recordClass);
+        if(cfConfigs) {
+            Ext.each(cfConfigs, function(config) {
+                var field = this.form.findField('customfield_' + config.data.name);
+                if (!field) return true;
+                keys.push({key: config.data.name, type: 'custom', formField: field, recordKey: '#' + config.data.name});
+            }, this);
+        }
+        
         Ext.each(keys, function(field) {
             var ff = field.formField;
             if ((!(ff.isXType('textfield'))) && (!(ff.isXType('checkbox'))) || ff.multiEditable === false) {
