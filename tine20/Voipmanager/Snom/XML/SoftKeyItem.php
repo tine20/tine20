248dbb239f9d74020cec2d0c3b1f38b7e982f1c7
Lars Kneschke <l.kneschke@metaways.de>
2012-05-23 13:16:23 +0200

    add missing files
    Change-Id: Ib1749c8776e8f0c8291973ebf04c73618ae6b761

---
+++ tine20/Voipmanager/Snom/XML/SoftKeyItem.php
@@ -0,0 +1,28 @@
+<?php
+
+class Voipmanager_Snom_XML_SoftKeyItem extends DOMElement
+{
+    public function setLabel($text)
+    {
+        $text = $this->appendChild($this->ownerDocument->createElement('Label', $text));
+
+        return $text;
+    }
+
+    public function setSoftKey($keyName)
+    {
+        $softKeyElement = $this->appendChild($this->ownerDocument->createElement('Softkey', $keyName));
+        
+        return $softKeyElement;
+    }
+
+    public function setURL($url)
+    {
+        $urlElement = $this->ownerDocument->createElement('URL');
+        
+        $urlElement->appendChild($this->ownerDocument->createTextNode($url));
+        $this->appendChild($urlElement);
+        
+        return $urlElement;
+    }
+}
