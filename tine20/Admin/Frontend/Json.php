8f96094f18f58d40d1668fb5dbd4251aa780f7cd
Philipp Schüle <p.schuele@metaways.de>
2012-07-25 15:40:35 +0200

    catch exceptions on user/group not found
    @2012-03
    
    Change-Id: I067572053f395e8575aeae3ff7ca289dade5ece9
    Reviewed-on: https://gerrit.tine20.org/tine20/860
    Tested-by: jenkins user
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Admin/Frontend/Json.php
+++ tine20/Admin/Frontend/Json.php
@@ -477,10 +477,18 @@ class Admin_Frontend_Json extends Tinebase_Frontend_Json_Abstract
         foreach ($_items as $num => $item) {
             switch ($item[$prefix . 'type']) {
                 case Tinebase_Acl_Rights::ACCOUNT_TYPE_USER:
-                    $item[$prefix . 'name'] = Tinebase_User::getInstance()->getUserById($item[$prefix . 'id'])->accountDisplayName;
+                    try {
+                        $item[$prefix . 'name'] = Tinebase_User::getInstance()->getUserById($item[$prefix . 'id'])->accountDisplayName;
+                    } catch (Tinebase_Exception_NotFound $tenf) {
+                        $item[$prefix . 'name'] = 'Unknown user';
+                    }
                     break;
                 case Tinebase_Acl_Rights::ACCOUNT_TYPE_GROUP:
-                    $item[$prefix . 'name'] = Tinebase_Group::getInstance()->getGroupById($item[$prefix . 'id'])->name;
+                    try {
+                        $item[$prefix . 'name'] = Tinebase_Group::getInstance()->getGroupById($item[$prefix . 'id'])->name;
+                    } catch (Tinebase_Exception_Record_NotDefined $ternd) {
+                        $item[$prefix . 'name'] = 'Unknown group';
+                    }
                     break;
                 case Tinebase_Acl_Rights::ACCOUNT_TYPE_ANYONE:
                     $item[$prefix . 'name'] = 'Anyone';
