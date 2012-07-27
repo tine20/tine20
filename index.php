26eff8b8963b4bcea61be5a87ac83fb12b461f8c
Lars Kneschke <l.kneschke@metaways.de>
2012-07-27 12:31:14 +0200

    moved exampel server code to index.php and removed file from docs
    Change-Id: I993ed14de3df6d1b2a537e2ee5c192416dfa86c2

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Syncroton
+ *
+ * Example server file
+ *
+ * @package     doc
+ * @license     http://www.tine20.org/licenses/lgpl.html LGPL Version 3
+ * @copyright   Copyright (c) 2012-2012 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ */
 
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
     header('WWW-Authenticate: Basic realm="Syncroton"');
