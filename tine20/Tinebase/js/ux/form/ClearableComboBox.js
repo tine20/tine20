161fe29fcf9d4df271f6392a3670e713c62c31d1
AirMike <airmike23@gmail.com>
2012-08-03 16:28:28 +0200

    Fix IE invalid argument error Ignore trigger click if field is disabled
    Change-Id: Ie2fc632a432ddbe221668dce16de21cf14b510af
    Reviewed-on: https://gerrit.tine20.org/tine20/876
    Tested-by: jenkins user
    Reviewed-by: Alexander Stintzing <a.stintzing@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/js/ux/form/ClearableComboBox.js
+++ tine20/Tinebase/js/ux/form/ClearableComboBox.js
@@ -16,8 +16,7 @@ Ext.ns('Ext.ux', 'Ext.ux.form');
  * @class       Ext.ux.form.ClearableComboBox
  * @extends     Ext.form.ComboBox
  */
-Ext.ux.form.ClearableComboBox = Ext.extend(Ext.form.ComboBox, {  
-    
+Ext.ux.form.ClearableComboBox = Ext.extend(Ext.form.ComboBox, {     
     /**
      * @cfg {bool} disableClearer
      * disables the clearer
@@ -27,86 +26,83 @@ Ext.ux.form.ClearableComboBox = Ext.extend(Ext.form.ComboBox, {
     initComponent : function(){
         Ext.ux.form.ClearableComboBox.superclass.initComponent.call(this);
         this.triggerConfig = {
-            tag:'span', cls:'x-form-twin-triggers', style:'padding-right:2px',  // padding needed to prevent IE from clipping 2nd trigger button
-            cn:[
+            tag: 'span', cls: 'x-form-twin-triggers', style: 'padding-right:2px',  // padding needed to prevent IE from clipping 2nd trigger button
+            cn: [
                 {tag: "img", src: Ext.BLANK_IMAGE_URL, cls: "x-form-trigger x-form-clear-trigger"},
                 {tag: "img", src: Ext.BLANK_IMAGE_URL, cls: "x-form-trigger"}
             ]
         };
-        
-        this.on('afterrender', function() {
-            this.initialWidth = this.wrap.getWidth();
-        }, this);
-        
-        this.on('resize', function() {
-            this.initialWidth = this.wrap.getWidth() + 2;
-        }, this);
-        
     },
 
-    getTrigger : function(index){
+    getTrigger: function (index) {
         return this.triggers[index];
     },
 
-    initTrigger : function(){
+    initTrigger: function () {
         var ts = this.trigger.select('.x-form-trigger', true);
         this.wrap.setStyle('overflow', 'hidden');
         var triggerField = this;
-        ts.each(function(t, all, index){
-            t.hide = function(){
+        ts.each(function (t, all, index) {
+            t.hide = function () {
                 this.dom.style.display = 'none';
-                triggerField.el.setWidth(triggerField.initialWidth - 21);
+                triggerField.el.setWidth(triggerField.wrap.getWidth() - 21);
             };
-            t.show = function(){
+            t.show = function () {
                 this.dom.style.display = '';
-                triggerField.el.setWidth(triggerField.initialWidth - 38);
+                triggerField.el.setWidth(triggerField.wrap.getWidth() - 38);
             };
-            var triggerIndex = 'Trigger'+(index+1);
+            var triggerIndex = 'Trigger' + (index + 1);
 
-            if(this['hide'+triggerIndex]){
+            if (this['hide' + triggerIndex]) {
                 t.dom.style.display = 'none';
             }
-            t.on("click", this['on'+triggerIndex+'Click'], this, {preventDefault:true});
+            
+            t.on("click", this['on' + triggerIndex + 'Click'], this, {preventDefault: true});
             t.addClassOnOver('x-form-trigger-over');
             t.addClassOnClick('x-form-trigger-click');
         }, this);
+        
         this.triggers = ts.elements;
         this.triggers[0].hide();
     },
     
     // clear contents of combobox
-    onTrigger1Click : function() {
+    onTrigger1Click: function () {
+        if (this.disabled) {
+           return;
+        }
         this.clearValue();
+    },
+    
+    // pass to original combobox trigger handler
+    onTrigger2Click: function () {
+        this.onTriggerClick();
     },
     
     /**
      * clear value
      */
-    clearValue: function() {
+    clearValue: function () {
         Ext.ux.form.ClearableComboBox.superclass.clearValue.apply(this, arguments);
         this.fireEvent('select', this, this.getRawValue(), this.startValue);
         this.startValue = this.getRawValue();
-        if(this.disableClearer !== true) {
+        if (this.triggers && this.disableClearer !== true) {
             this.triggers[0].hide();
         }
     },
     
-    // pass to original combobox trigger handler
-    onTrigger2Click : function() {
-        this.onTriggerClick();
-    },
     // show clear trigger when item got selected
-    onSelect: function(combo, record, index) {
-        if(this.disableClearer !== true) {
+    onSelect: function (combo, record, index) {
+        if (this.triggers && this.disableClearer !== true) {
             this.triggers[0].show();
         }
         Ext.ux.form.ClearableComboBox.superclass.onSelect.call(this, combo, record, index);
         this.startValue = this.getValue();
     },
     
-    setValue: function(value) {
+    setValue: function (value) {
         Ext.ux.form.ClearableComboBox.superclass.setValue.call(this, value);
-        if (value && (this.disableClearer !== true)) {
+        if (value && (this.triggers && this.disableClearer !== true)) {
             this.triggers[0].show();
         }
     }
