9b2388b53fd4ca27e9aa30af064cc81d8ae20ce3
zueleyha01 <praktikant@metaways.de>
2012-09-06 15:31:06 +0200

    All the functions can be executed separately now.
    Change-Id: I84cbbec55857f3963195655c4e665de7b563ed3b
    Reviewed-on: https://gerrit.tine20.org/tine20/1039
    Tested-by: jenkins user
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tests/tine20/Addressbook/Backend/SqlTest.php
+++ tests/tine20/Addressbook/Backend/SqlTest.php
@@ -8,6 +8,7 @@
  * @author      Lars Kneschke <l.kneschke@metaways.de>
  */
 
+
 /**
  * Test helper
  */
@@ -23,21 +24,17 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
 class Addressbook_Backend_SqlTest extends PHPUnit_Framework_TestCase
 {
     /**
-     * @var array test objects
-     */
-    protected $objects = array();
-
-    /**
-     * @var bool allow the use of GLOBALS to exchange data between tests
-     */
-    protected $backupGlobals = false;
-    
-    /**
      * Backend
      *
      * @var Addressbook_Backend_Sql
      */
     protected $_backend;
+    
+    /**
+     * 
+     * @var Tinebase_Model_Container
+     */
+    protected $_container;
     
     /**
      * Runs the test methods of this class.
@@ -59,7 +56,7 @@ class Addressbook_Backend_SqlTest extends PHPUnit_Framework_TestCase
      */
     protected function setUp()
     {
-        $GLOBALS['Addressbook_Backend_SqlTest'] = array_key_exists('Addressbook_Backend_SqlTest', $GLOBALS) ? $GLOBALS['Addressbook_Backend_SqlTest'] : array();
+        Tinebase_TransactionManager::getInstance()->startTransaction(Tinebase_Core::getDb());
         
         $this->_backend = Addressbook_Backend_Factory::factory(Addressbook_Backend_Factory::SQL);
         
@@ -70,9 +67,174 @@ class Addressbook_Backend_SqlTest extends PHPUnit_Framework_TestCase
             Tinebase_Model_Grants::GRANT_EDIT
         );
         
-        $container = $personalContainer[0];
+        $this->_container = $personalContainer[0];
+
+    }
+
+    /**
+     * Tears down the fixture
+     * This method is called after a test is executed.
+     *
+     * @access protected
+     */
+    protected function tearDown()
+    {
+        Tinebase_TransactionManager::getInstance()->rollBack();
+    }
+    
+
+    /**
+     * try to add a contact
+     * 
+     * @return Addressbook_Model_Contact
+     */
+    public function testCreateContact()
+    {
+        $contact = $this->_backend->create(self::getTestContact($this->_container));
         
-        $this->objects['initialContact'] = new Addressbook_Model_Contact(array(
+        $this->assertTrue(!empty($contact->id));
+        
+        return $contact;
+    }
+
+    /**
+     * try to get a contact
+     * 
+     * @return Addressbook_Model_Contact
+     */
+    public function testGetContact()
+    {
+        $contact = $this->testCreateContact();
+       
+        $updateContact = $this->_backend->get($contact->getId());
+        
+        $this->assertTrue($updateContact instanceof Addressbook_Model_Contact);
+        $this->assertEquals($contact->getId(), $updateContact->getId());
+        $this->assertEquals($contact->adr_one_locality, $updateContact->adr_one_locality);
+        
+        return $contact;
+    }
+    
+    
+    /**
+     * try to check the id of the contact
+     */
+    public function testgetByUserId()
+    {
+        Tinebase_Core::getUser()->accountId;
+         
+        $contact = $this->_backend->getByUserId(Tinebase_Core::getUser()->accountId);
+    
+        $this->assertTrue($contact instanceof Addressbook_Model_Contact);
+    
+        $this->setExpectedException('Addressbook_Exception_NotFound');
+        
+        $this->_backend->getByUserId('invalid_id');
+        
+    }
+     
+    
+    /**
+     * test search results
+     * 
+     * @return Addressbook_Model_Contact
+     */
+    public function testSearchContact()
+    {
+        $contact = $this->testCreateContact();
+        
+        $filter = new Addressbook_Model_ContactFilter(array(
+            array(
+                'field' => 'container_id', 
+                'operator' => 'equals', 
+                'value' => $contact->container_id
+                    
+        )));
+        
+        $contacts = $this->_backend->search($filter);
+        
+        
+        $this->assertTrue(count($contacts) >= 1, 'empty search');
+        $this->assertTrue( in_array($contact->getId(), $contacts->getId()) );
+        
+        $this->assertTrue((bool) $contact->jpegphoto, 'contact image is not detected');
+        
+        return $contact;
+    }
+    
+    /**
+     * test if image is in contact
+     * 
+     * 
+     */
+    public function testImage()
+    {
+        $contact = $this->testCreateContact();
+        
+        //$contact = $this->_backend->get($GLOBALS['Addressbook_ControllerTest']['contactId']);
+        $image = $this->_backend->getImage($contact->getId());
+        $tmpPath = tempnam(Tinebase_Core::getTempDir(), 'tine20_tmp_gd');
+        file_put_contents($tmpPath, $image);
+        $this->assertFileEquals(dirname(__FILE__) . '/../../Tinebase/ImageHelper/phpunit-logo.gif', $tmpPath);
+        unset($tmpPath);
+    }
+    
+    /**
+     * try to update a contact
+     * 
+     *@ return Addressbook_Model_Contact
+     */
+    public function testUpdateContact()
+    {
+        $contact = $this->testCreateContact();
+           
+        $contact->n_family = 'Toptas';
+        
+        $contact = $this->_backend->update($contact);
+        
+        $this->assertEquals('Toptas', $contact->n_family, 'family name mismatch');
+  
+        
+        return $contact;
+    }
+    
+    /**
+     * try to remove image
+     *
+     * API change 2009-04-26, image must now be queried separatly
+     */
+    public function testRemoveImage()
+    {
+        $contact = $this->testCreateContact();
+        
+        $contact->jpegphoto = '';
+        $updatedContact = $this->_backend->update($contact);
+        
+        $this->assertEquals('', $updatedContact->jpegphoto);
+    }
+    
+    /**
+     * try to delete a contact and its id
+     *
+     */
+    public function testDeleteContact()
+    {
+        $contact = $this->testCreateContact();
+        
+        $this->_backend->delete($contact->getId());
+        
+        $this->setExpectedException('Tinebase_Exception_NotFound');   
+        $contact = $this->_backend->get($contact->getId());
+    }
+    
+    /**
+     * create test contact
+     * 
+     * @return Addressbook_Model_Contact
+     */
+    public static function getTestContact(Tinebase_Model_Container $container)
+    {
+        $contact = new Addressbook_Model_Contact(array(
             'adr_one_countryname'   => 'DE',
             'adr_one_locality'      => 'Hamburg',
             'adr_one_postalcode'    => '24xxx',
@@ -114,192 +276,11 @@ class Addressbook_Backend_SqlTest extends PHPUnit_Framework_TestCase
             'tel_pager'             => '+49TELPAGER',
             'tel_work'              => '+49TELWORK',
         ));
-        
-        $this->objects['updatedContact'] = new Addressbook_Model_Contact(array(
-            'adr_one_countryname'   => 'DE',
-            'adr_one_locality'      => 'Hamburg',
-            'adr_one_postalcode'    => '24xxx',
-            'adr_one_region'        => 'Hamburg',
-            'adr_one_street'        => 'Pickhuben 4',
-            'adr_one_street2'       => 'no second street',
-            'adr_two_countryname'   => 'DE',
-            'adr_two_locality'      => 'Hamburg',
-            'adr_two_postalcode'    => '24xxx',
-            'adr_two_region'        => 'Hamburg',
-            'adr_two_street'        => 'Pickhuben 4',
-            'adr_two_street2'       => 'no second street2',
-            'assistent'             => 'Cornelius Weiß',
-            'bday'                  => '1975-01-02 03:04:05', // new Tinebase_DateTime???
-            'email'                 => 'unittests@tine20.org',
-            'email_home'            => 'unittests@tine20.org',
-            'note'                  => 'Bla Bla Bla',
-            'container_id'          => $container->id,
-            'role'                  => 'Role',
-            'title'                 => 'Title',
-            'url'                   => 'http://www.tine20.org',
-            'url_home'              => 'http://www.tine20.com',
-            'n_family'              => 'Kneschke',
-            'n_fileas'              => 'Kneschke, Lars',
-            'n_given'               => 'Lars',
-            'n_middle'              => 'no middle name',
-            'n_prefix'              => 'no prefix',
-            'n_suffix'              => 'no suffix',
-            'org_name'              => 'Metaways Infosystems GmbH',
-            'org_unit'              => 'Tine 2.0',
-            'tel_assistent'         => '+49TELASSISTENT',
-            'tel_car'               => '+49TELCAR',
-            'tel_cell'              => '+49TELCELL',
-            'tel_cell_private'      => '+49TELCELLPRIVATE',
-            'tel_fax'               => '+49TELFAX',
-            'tel_fax_home'          => '+49TELFAXHOME',
-            'tel_home'              => '+49TELHOME',
-            'tel_pager'             => '+49TELPAGER',
-            'tel_work'              => '+49TELWORK',
-        ));
-        
-        return;
-        
-        $this->expectFailure['TestRecord']['testSetId'][] = array('2','3');
-        $this->expectFailure['TestRecord']['testSetId'][] = array('30000000','3000000000000000000000000000');
-        $this->expectSuccess['TestRecord']['testSetId'][] = array('2','2');
-        
-        $this->expectFailure['TestRecordBypassFilters']['testSetIdBypassFilters'][] = array('2','3');
-        $this->expectFailure['TestRecordBypassFilters']['testSetIdBypassFilters'][] = array('30000000','3000000000000000000000000000');
-        $this->expectSuccess['TestRecordBypassFilters']['testSetIdBypassFilters'][] = array('2','2');
-        
-        $this->expectSuccess['TestRecord']['testSetFromArray'][] = array(array('test_1'=>'2', 'test_2'=>NULL), 'test_1');
-        $this->expectFailure['TestRecord']['testSetFromArrayException'][] = array('Tinebase_Exception_Record_Validation', array('test_2' => 'string'), );
-        $this->expectFailure['TestRecord']['testSetTimezoneException'][] = array('Exception', 'UTC', );
-        
-        $dummy = array(
-                    'test_id'=>2, 
-                    'test_2'=>'',
-                    'date_single' => $date->get(Tinebase_Record_Abstract::ISO8601LONG), 
-                    'date_multiple'=>'');
-            $this->expectSuccess['TestRecord']['testToArray'][] = array($dummy);
-            
-            
-            $this->expectSuccess['TestRecord']['__set'][] = array('test_3', 4 );
-            
-            $this->expectSuccess['TestRecord']['__get'][] = array('test_3', 4 );
-            
-            $this->expectSuccess['TestRecord']['test__isset'][] = array('test_id');
-            
-            $this->expectFailure['TestRecord']['test__isset'][] = array('string');
-            
-            
-            $this->expectFailure['TestRecord']['test__setException'][] = array( 'UnexpectedValueException', 'test_100',);
-        $this->expectFailure['TestRecord']['test__getException'][] = array( 'UnexpectedValueException', 'test_100',);
-        
-            
-            $this->expectFailure['TestRecord']['testOffsetUnset'][] = array( 'Tinebase_Exception_Record_NotAllowed', 'test_2',);
-    }
 
-    /**
-     * Tears down the fixture
-     * This method is called after a test is executed.
-     *
-     * @access protected
-     */
-    protected function tearDown()
-    {
-    
-    }
-    
-    /**
-     * try to add a contact
-     *
-     */
-    public function testAddContact()
-    {
-        $contact = $this->_backend->create($this->objects['initialContact']);
-        $GLOBALS['Addressbook_ControllerTest']['contactId'] = $contact->getId();
-        
-        $this->assertEquals($this->objects['initialContact']->adr_one_locality, $contact->adr_one_locality);
-    }
-
-    /**
-     * try to get a contact
-     *
-     */
-    public function testGetContact()
-    {
-        $contact = $this->_backend->get($GLOBALS['Addressbook_ControllerTest']['contactId']);
-        
-        $this->assertEquals($this->objects['initialContact']->adr_one_locality, $contact->adr_one_locality);
-    }
-    
-    /**
-     * test search results
-     * 
-     */
-    public function testSearchContact()
-    {
-        $filter = new Addressbook_Model_ContactFilter(array(
-            array('field' => 'container_id', 'operator' => 'equals', 'value' => $this->objects['initialContact']['container_id'])
-        ));
-        
-        $contacts = $this->_backend->search($filter);
-        $this->assertTrue(count($contacts) >= 1, 'empty search');
-        $this->assertTrue(in_array($GLOBALS['Addressbook_ControllerTest']['contactId'], $contacts->getId()), 'contact not found');
-        
-        $contact = $contacts[$contacts->getIndexById($GLOBALS['Addressbook_ControllerTest']['contactId'])];
-        $this->assertTrue((bool) $contact->jpegphoto, 'contact image is not detected');
-    }
-    
-    /**
-     * test if image is in contact
-     * 
-     */
-    public function testImage()
-    {
-        //$contact = $this->_backend->get($GLOBALS['Addressbook_ControllerTest']['contactId']);
-        $image = $this->_backend->getImage($GLOBALS['Addressbook_ControllerTest']['contactId']);
-        $tmpPath = tempnam(Tinebase_Core::getTempDir(), 'tine20_tmp_gd');
-        file_put_contents($tmpPath, $image);
-        $this->assertFileEquals(dirname(__FILE__) . '/../../Tinebase/ImageHelper/phpunit-logo.gif', $tmpPath);
-        unset($tmpPath);
-    }
-    
-    /**
-     * try to update a contact
-     *
-     */
-    public function testUpdateContact()
-    {
-        $this->objects['updatedContact']->setId($GLOBALS['Addressbook_ControllerTest']['contactId']);
-        $contact = $this->_backend->update($this->objects['updatedContact']);
-        
-        $this->assertEquals($this->objects['updatedContact']->adr_one_locality, $contact->adr_one_locality);
-    }
-    
-    /**
-     * try to remove image
-     *
-     * API change 2009-04-26, image must now be queried separatly
-     */
-    public function testRemoveImage()
-    {
-        $contact = $this->_backend->get($GLOBALS['Addressbook_ControllerTest']['contactId']);
-        $contact->jpegphoto = '';
-        $updatedContact = $this->_backend->update($contact);
-        $this->assertEquals('', $updatedContact->jpegphoto);
-    }
-    
-    /**
-     * try to delete a contact
-     *
-     */
-    public function testDeleteContact()
-    {
-        $this->_backend->delete($GLOBALS['Addressbook_ControllerTest']['contactId']);
-        
-        $this->setExpectedException('Tinebase_Exception_NotFound');
-        
-        $contact = $this->_backend->get($GLOBALS['Addressbook_ControllerTest']['contactId']);
+        return $contact;
     }
 }        
-    
+
 
 if (PHPUnit_MAIN_METHOD == 'Addressbook_Backend_SqlTest::main') {
     Addressbook_Backend_SqlTest::main();
