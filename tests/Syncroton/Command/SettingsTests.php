2043866add1e6a8885a6d419e428f04a8722a02f
Lars Kneschke <l.kneschke@metaways.de>
2012-09-10 09:38:02 +0200

    improved handling of empty policy and add support for updating device information
    Change-Id: I75b3ec3f9f076cceebb91c32f34c057255a680e5

--- tests/Syncroton/Command/SettingsTests.php
+++ tests/Syncroton/Command/SettingsTests.php
@@ -40,7 +40,7 @@ class Syncroton_Command_SettingsTests extends Syncroton_Command_ATestCase
         $doc = new DOMDocument();
         $doc->loadXML('<?xml version="1.0" encoding="utf-8"?>
             <!DOCTYPE AirSync PUBLIC "-//AIRSYNC//DTD AirSync//EN" "http://www.microsoft.com/">
-            <Settings xmlns="uri:Settings"><DeviceInformation><Set><Model>iPhone</Model></Set></DeviceInformation></Settings>'
+            <Settings xmlns="uri:Settings"><DeviceInformation><Set><Model>iPhone</Model><IMEI>086465r87697</IMEI></Set></DeviceInformation></Settings>'
         );
         $folderDelete = new Syncroton_Command_Settings($doc, $this->_device, null);
         $folderDelete->handle();
@@ -54,8 +54,8 @@ class Syncroton_Command_SettingsTests extends Syncroton_Command_ATestCase
         $this->assertEquals(1, $nodes->length, $responseDoc->saveXML());
         $this->assertEquals(Syncroton_Command_Settings::STATUS_SUCCESS, $nodes->item(0)->nodeValue, $responseDoc->saveXML());
         
-        #$nodes = $xpath->query('//Settings:Settings/Settings:SyncKey');
-        #$this->assertEquals(1, $nodes->length, $responseDoc->saveXML());
-        #$this->assertEquals(3, $nodes->item(0)->nodeValue, $responseDoc->saveXML());
+        $updatedDevice = Syncroton_Registry::getDeviceBackend()->get($this->_device->id);
+        
+        $this->assertEquals('086465r87697', $updatedDevice->imei);
     }    
 }
