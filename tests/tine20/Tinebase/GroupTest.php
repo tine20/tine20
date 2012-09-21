9eb0d92e22bd985e984f45fb5a718d1531ecdcfe
Philipp Schüle <p.schuele@metaways.de>
2012-09-21 09:49:29 +0200

    0007122: hide hidden users from lists
    - fixed array sort in groups test
    
    https://forge.tine20.org/mantisbt/view.php?id=7122

--- tests/tine20/Tinebase/GroupTest.php
+++ tests/tine20/Tinebase/GroupTest.php
@@ -198,7 +198,8 @@ class Tinebase_GroupTest extends PHPUnit_Framework_TestCase
 
         $getGroupMembersArray = Tinebase_Group::getInstance()->getGroupMembers($this->objects['initialGroup']->id);
         
-        $this->assertEquals(sort(array($this->objects['account1']->accountId, $this->objects['account2']->accountId, $this->objects['account3']->accountId)), sort($getGroupMembersArray));
+        $expectedValues = array($this->objects['account1']->accountId, $this->objects['account2']->accountId, $this->objects['account3']->accountId);
+        $this->assertEquals(sort($expectedValues), sort($getGroupMembersArray));
     }
     
     /**
