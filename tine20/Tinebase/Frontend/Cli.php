11e8d8b540a5595a80772098c58caa6db0b3f611
Philipp Schüle <p.schuele@metaways.de>
2012-10-02 14:29:49 +0200

    allow to remove async_job records prior given date
    @2012-03
    
    Change-Id: If4480b820998ad1583040749617e77b491d03aba
    Reviewed-on: https://gerrit.tine20.org/tine20/1141
    Tested-by: jenkins user
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/Frontend/Cli.php
+++ tine20/Tinebase/Frontend/Cli.php
@@ -227,9 +227,14 @@ class Tinebase_Frontend_Cli extends Tinebase_Frontend_Cli_Abstract
                     }
                     break;
                 case 'async_job':
-                    $db->query(
-                        'delete FROM ' . SQL_TABLE_PREFIX . 'async_job' .
-                        " WHERE status='success'");
+                    $where = ($dateString) ? array(
+                        $db->quoteInto($db->quoteIdentifier('end_time') . ' < ?', $dateString)
+                    ) : array();
+                    $where[] = $db->quoteInto($db->quoteIdentifier('status') . ' < ?', 'success');
+                    
+                    echo "\nRemoving all successful async_job entries " . ($dateString ? "before $dateString " : "") . "...";
+                    $deleteCount = $db->delete(SQL_TABLE_PREFIX . $table, $where);
+                    echo "\nRemoved $deleteCount records.";
                     break;
                 case 'credential_cache':
                     Tinebase_Auth_CredentialCache::getInstance()->clearCacheTable($dateString);
