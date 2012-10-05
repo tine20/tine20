c0a1f26509d6503e2768fd97c9538f91bcef687c
Lars Kneschke <l.kneschke@metaways.de>
2012-10-05 23:13:47 +0200

    refactored Http_Server for better handling of session problems
    Change-Id: I94e9065209011877dd99cdf25bc4dae770ee4fb1
    Reviewed-on: https://gerrit.tine20.org/tine20/1166
    Tested-by: jenkins user
    Reviewed-by: Cornelius Weiß <c.weiss@metaways.de>

--- tine20/Tinebase/Server/Http.php
+++ tine20/Tinebase/Server/Http.php
@@ -33,15 +33,18 @@ class Tinebase_Server_Http implements Tinebase_Server_Interface
      */
     public function handle()
     {
+        $server = new Tinebase_Http_Server();
+        $server->setClass('Tinebase_Frontend_Http', 'Tinebase');
+        
         try {
-            Tinebase_Core::initFramework();
+            try {
+                Tinebase_Core::initFramework();
+            } catch (Zend_Session_Exception $zse) {
+                // expire session cookie on client
+                Zend_Session::expireSessionCookie();
+            }
+            
             Tinebase_Core::getLogger()->INFO(__METHOD__ . '::' . __LINE__ .' Is HTTP request. method: ' . $this->getRequestMethod());
-            //Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ .' Rawdata: ' . print_r($_REQUEST, true));
-            
-            $server = new Tinebase_Http_Server();
-            
-            //NOTE: auth check for Tinebase HTTP api is done via Tinebase_Http::checkAuth  
-            $server->setClass('Tinebase_Frontend_Http', 'Tinebase');
             
             // register addidional HTTP apis only available for authorised users
             if (Zend_Session::isStarted() && Zend_Auth::getInstance()->hasIdentity()) {
@@ -67,49 +70,51 @@ class Tinebase_Server_Http implements Tinebase_Server_Interface
                 
                 // sessionId got send by client, but we don't use sessions for non authenticated users
                 if (Zend_Session::sessionExists()) {
+                    // expire session cookie on client
                     Zend_Session::expireSessionCookie();
                 }
             }
             
-            Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . " session status: " . (int) Zend_Session::isStarted());
-            
             $this->_method = $_REQUEST['method'];
+            
             $server->handle($_REQUEST);
+            
+        } catch (Zend_Json_Server_Exception $zjse) {
+            // invalid method requested or not authenticated
+            Tinebase_Core::getLogger()->INFO(__METHOD__ . '::' . __LINE__ .' Attempt to request a privileged Http-API method without valid session from "' . $_SERVER['REMOTE_ADDR']);
+            
+            header('HTTP/1.0 403 Forbidden');
+
+            exit;
+            
         } catch (Exception $exception) {
             if (! is_object(Tinebase_Core::getLogger())) {
-                // no logger -> exception happened very early, just rethrow it
+                // no logger -> exception happened very early
                 error_log($exception);
                 header('HTTP/1.0 503 Service Unavailable');
                 die('Service Unavailable');
             }
-            Tinebase_Core::getLogger()->INFO($exception);
             
-            $server = new Tinebase_Http_Server();
-            $server->setClass('Tinebase_Frontend_Http', 'Tinebase');
-            if ($exception instanceof Zend_Session_Exception) {
-                Tinebase_Core::getLogger()->INFO(__METHOD__ . '::' . __LINE__ .' Attempt to request a privileged Http-API method without valid session from "' . $_SERVER['REMOTE_ADDR']);
-                
-                // expire session cookie for client
-                Zend_Session::expireSessionCookie();
+            Tinebase_Core::getLogger()->info($exception);
             
-                header('HTTP/1.0 403 Forbidden');
-                exit;
-            } else {
-                try {
-                    // check if setup is required
-                    $setupController = Setup_Controller::getInstance();
-                    if ($setupController->setupRequired()) {
-                        $this->_method = 'Tinebase.setupRequired';
-                    } else {
-                        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->DEBUG(__CLASS__ . '::' . __METHOD__ . ' (' . __LINE__ .') Http-Api exception: ' . print_r($exception, true));
-                        $this->_method = 'Tinebase.exception';
-                    }
-                    $server->handle(array('method' => $this->_method));
-                } catch (Exception $e) {
-                    error_log($exception);
-                    header('HTTP/1.0 503 Service Unavailable');
-                    die('Service Unavailable');
+            try {
+                // check if setup is required
+                $setupController = Setup_Controller::getInstance();
+                if ($setupController->setupRequired()) {
+                    $this->_method = 'Tinebase.setupRequired';
+                } else {
+                    if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->DEBUG(
+                        __METHOD__ . '::' . __LINE__ . ' (' . __LINE__ .') Http-Api exception: ' . print_r($exception, true));
+                    
+                    $this->_method = 'Tinebase.exception';
                 }
+                
+                $server->handle(array('method' => $this->_method));
+                
+            } catch (Exception $e) {
+                error_log($exception);
+                header('HTTP/1.0 503 Service Unavailable');
+                die('Service Unavailable');
             }
         }
     }
