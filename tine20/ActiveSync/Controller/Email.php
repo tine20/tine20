a9df8d614bf7a98e75f50ca1757f16ba9c38658c
Lars Kneschke <l.kneschke@metaways.de>
2012-10-31 11:50:30 +0100

    fix spelling of variable name
    Change-Id: Ic5c4d6fcce8629a86909a7b5f2a86df5796889ed
    Reviewed-on: https://gerrit.tine20.org/tine20/1265
    Tested-by: jenkins user
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/ActiveSync/Controller/Email.php
+++ tine20/ActiveSync/Controller/Email.php
@@ -262,7 +262,7 @@ class ActiveSync_Controller_Email extends ActiveSync_Controller_Abstract impleme
         );
         
         if ($replaceMime === false) {
-            $messageId = is_array($source) ? $source['itemdId'] : $source;
+            $messageId = is_array($source) ? $source['itemId'] : $source;
             
             if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(
                 __METHOD__ . '::' . __LINE__ . " source: " . $messageId . "saveInSent: " . $saveInSent);
