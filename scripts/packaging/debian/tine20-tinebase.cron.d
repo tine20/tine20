bb144db4ad4eb504d74573c705c03215328aec30
Lars Kneschke <l.kneschke@metaways.de>
2013-01-02 00:31:05 +0100

    minor debian packaging improvements
    Change-Id: If2a8ed7fa7183f8bb5af22d20f38014bb4e92392
    Reviewed-on: https://gerrit.tine20.org/tine20/1467
    Reviewed-by: Lars Kneschke <l.kneschke@metaways.de>
    Tested-by: Lars Kneschke <l.kneschke@metaways.de>

--- scripts/packaging/debian/tine20-tinebase.cron.d
+++ scripts/packaging/debian/tine20-tinebase.cron.d
@@ -1,4 +1,5 @@
-SHELL=/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+MAILTO=root
+SHELL=/bin/bash
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 * * * * *	www-data	/usr/bin/php -f /usr/share/tine20/tine20.php -- --config=/etc/tine20/config.inc.php --method=Tinebase.triggerAsyncEvents
