1b5697ec57dbefbff3ce3fd8746c8f419522f832
Philipp Schüle <p.schuele@metaways.de>
2013-04-12 13:15:03 +0200

    #8196: Preferences values contains translated value
    - fixed integer preference field validation
    - added update script for broken default alarm prefs
    
    https://forge.tine20.org/mantisbt/view.php?id=8196
    
    Change-Id: I331707b5b7239fffc677df64c8166d7a57d0a3be
    Reviewed-on: http://gerrit.tine20.com/customers/80
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Reviewed-by: Cornelius Weiss <c.weiss@metaways.de>

--- tine20/Calendar/Setup/Update/Release6.php
+++ tine20/Calendar/Setup/Update/Release6.php
@@ -5,7 +5,7 @@
  * @package     Calendar
  * @subpackage  Setup
  * @license     http://www.gnu.org/licenses/agpl.html AGPL3
- * @copyright   Copyright (c) 2012 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2012-2013 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Cornelius Weiß <c.weiss@metaways.de>
  */
 class Calendar_Setup_Update_Release6 extends Setup_Update_Abstract
@@ -22,5 +22,34 @@ class Calendar_Setup_Update_Release6 extends Setup_Update_Abstract
         }
         
         $this->setApplicationVersion('Calendar', '6.1');
+    }
+    
+
+    /**
+     * update to 6.2
+     * 
+     * @see 0008196: Preferences values contains translated value
+     */
+    public function update_1()
+    {
+        $prefBackend = new Tinebase_Backend_Sql(array(
+            'modelName' => 'Tinebase_Model_Preference', 
+            'tableName' => 'preferences',
+        ));
+        $alarmPrefs = $prefBackend->search(new Tinebase_Model_PreferenceFilter(array(array(
+            'field'    => 'name',
+            'operator' => 'equals',
+            'value'    => Calendar_Preference::DEFAULTALARM_MINUTESBEFORE
+        ))));
+        foreach ($alarmPrefs as $pref) {
+            if (preg_match("/\((\d+)\)/", $pref->value, $matches)) {
+                if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__
+                    . ' Updating DEFAULTALARM_MINUTESBEFORE from ' . $pref->value . ' to ' . $matches[1]);
+                $pref->value = $matches[1];
+                $prefBackend->update($pref);
+            }
+        }
+        
+        $this->setApplicationVersion('Calendar', '6.2');
     }
 }
