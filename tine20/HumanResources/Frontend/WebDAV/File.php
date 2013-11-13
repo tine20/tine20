79dbcd44bb48a79c1be6edf78da9e54d71380fd6
Alexander Stintzing <a.stintzing@metaways.de>
2013-11-13 15:44:21 +0100

    prepare reporting
    Change-Id: I32193cedb20d6c66d400987bac0e5f2b08cb6395
    Reviewed-on: https://gerrit.tine20.org/tine20/2529
    Tested-by: jenkins user
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

---
+++ tine20/HumanResources/Frontend/WebDAV/File.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     HumanResources
+ * @subpackage  Frontend
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Alexander Stintzing <a.stintzing@metaways.de>
+ * @copyright   Copyright (c) 2013 Metaways Infosystems GmbH (http://www.metaways.de)
+ */
+
+/**
+ * class to handle webdav requests for HumanResources
+ * 
+ * @package     HumanResources
+ * @subpackage  Frontend
+ */
+class HumanResources_Frontend_WebDAV_File extends Tinebase_Frontend_WebDAV_File
+{
+}
