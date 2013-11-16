35bb8cbb56468609ec0b1260eb5fe1413b4bde28
Lars Kneschke <l.kneschke@metaways.de>
2013-11-16 07:17:32 +0100

    make _getAttendee compatible with parrent again
    Change-Id: I1824afb86de1918bc3b60ea472fbcf2452b68665
    Reviewed-on: https://gerrit.tine20.org/tine20/2550
    Tested-by: jenkins user
    Reviewed-by: Lars Kneschke <l.kneschke@metaways.de>

--- tine20/Calendar/Convert/Event/VCalendar/MacOSX.php
+++ tine20/Calendar/Convert/Event/VCalendar/MacOSX.php
@@ -46,21 +46,21 @@ class Calendar_Convert_Event_VCalendar_MacOSX extends Calendar_Convert_Event_VCa
     );
     
     /**
-     * get attendee object for given contact
+     * get attendee array for given contact
      * 
-     * @param Sabre_VObject_Property     $_attendee  the attendee row from the vevent object
+     * @param  \Sabre\VObject\Property\ICalendar\CalAddress  $calAddress  the attendee row from the vevent object
      * @return array
      */
-    protected function _getAttendee(Sabre_VObject_Property $_attendee)
+    protected function _getAttendee(\Sabre\VObject\Property\ICalendar\CalAddress $calAddress)
     {
         
-        $newAttendee = parent::_getAttendee($_attendee);
+        $newAttendee = parent::_getAttendee($calAddress);
         
         // beginning with mavericks iCal adds organiser as attedee without role
         // so we remove attendee without role 
         // @TODO check if this attendee is currentuser & organizer?
         if (version_compare($this->_version, '10.9', '>=')) {
-            if (! isset($_attendee['ROLE'])) {
+            if (! isset($calAddress['ROLE'])) {
                 return NULL;
             }
         }
