c493199ae0b82a9e0c5ffc9fb2b484971417ae07
Alexander Stintzing <a.stintzing@metaways.de>
2013-12-09 14:24:19 +0100

    whitespace changes - tine20/Sales/Setup/Initialize.php
    Change-Id: I8df3f39ce2db3ff7b751c2944d4c4446cc536497
    Reviewed-on: https://gerrit.tine20.org/tine20/2640
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: jenkins user

--- tine20/Sales/Setup/Initialize.php
+++ tine20/Sales/Setup/Initialize.php
@@ -5,7 +5,7 @@
  * @package     Sales
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Jonas Fischer <j.fischer@metaways.de>
- * @copyright   Copyright (c) 2011 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2011-2013 Metaways Infosystems GmbH (http://www.metaways.de)
  *
  */
 
@@ -22,46 +22,42 @@ class Sales_Setup_Initialize extends Setup_Initialize
     protected function _initializeFavorites() {
         // Products
         $commonValues = array(
-                            'account_id'        => NULL,
-                            'application_id'    => Tinebase_Application::getInstance()->getApplicationByName('Sales')->getId(),
-                            'model'             => 'Sales_Model_ProductFilter',
+            'account_id'        => NULL,
+            'application_id'    => Tinebase_Application::getInstance()->getApplicationByName('Sales')->getId(),
+            'model'             => 'Sales_Model_ProductFilter',
         );
         
         $pfe = new Tinebase_PersistentFilter_Backend_Sql();
         
         $pfe->create(new Tinebase_Model_PersistentFilter(
-        array_merge($commonValues, array(
-                                'name'              => "My Products", // _('My Products')
-                                'description'       => "Products created by me", // _('Products created by myself')
-                                'filters'           => array(
-        array(
-                                        'field'     => 'created_by',
-                                        'operator'  => 'equals',
-                                        'value'     => Tinebase_Model_User::CURRENTACCOUNT
-        )
-        ),
-        ))
+            array_merge($commonValues, array(
+                'name'              => "My Products", // _('My Products')
+                'description'       => "Products created by me", // _('Products created by myself')
+                'filters'           => array(
+                    array(
+                        'field'     => 'created_by',
+                        'operator'  => 'equals',
+                        'value'     => Tinebase_Model_User::CURRENTACCOUNT
+                    )
+                ),
+            ))
         ));
         
         // Contracts
-        $commonValues = array(
-                                'account_id'        => NULL,
-                                'application_id'    => Tinebase_Application::getInstance()->getApplicationByName('Sales')->getId(),
-                                'model'             => 'Sales_Model_ContractFilter',
-        );
+        $commonValues['model'] = 'Sales_Model_ContractFilter';
         
         $pfe->create(new Tinebase_Model_PersistentFilter(
-        array_merge($commonValues, array(
-                                    'name'              => "My Contracts", // _('My Contracts')
-                                    'description'       => "Contracts created by me", // _('Contracts created by myself')
-                                    'filters'           => array(
-        array(
-                                            'field'     => 'created_by',
-                                            'operator'  => 'equals',
-                                            'value'     => Tinebase_Model_User::CURRENTACCOUNT
-        )
-        ),
-        ))
+            array_merge($commonValues, array(
+                'name'              => "My Contracts", // _('My Contracts')
+                'description'       => "Contracts created by me", // _('Contracts created by myself')
+                'filters'           => array(
+                    array(
+                        'field'     => 'created_by',
+                        'operator'  => 'equals',
+                        'value'     => Tinebase_Model_User::CURRENTACCOUNT
+                    )
+                ),
+            ))
         ));
     }
     
