36e4cc633dd0c72375e784f3408b9a251b99ac8a
Alexander Stintzing <a.stintzing@metaways.de>
2013-12-11 17:43:17 +0100

    refactor sales
     - use model instead of type in numbers to allow
       use of different models, will be more general later
    
     - Parent class of Sales_Exception changed from
       Exception to Tinebase_Exception
    
     - create abstract numberable class
    
     - whitespace changes and using php 5.3 style
     
     - added abstract ods export class
     
     - changed Sales_Model_Number + tests
    
    Change-Id: Ie63362a4b93355f93fdd2640f18fad5bcf0cff58
    Reviewed-on: https://gerrit.tine20.org/tine20/2651
    Tested-by: jenkins user
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Sales/Backend/Number.php
+++ tine20/Sales/Backend/Number.php
@@ -6,7 +6,7 @@
  * @subpackage  Backend
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Philipp Schuele <p.schuele@metaways.de>
- * @copyright   Copyright (c) 2007-2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2007-2013 Metaways Infosystems GmbH (http://www.metaways.de)
  */
 
 
@@ -40,30 +40,39 @@ class Sales_Backend_Number extends Tinebase_Backend_Sql_Abstract
     protected $_defaultCountCol = 'id';
 
     /**
-     * get next number identified by $_type (i.e. contract) and update db
+     * get next number identified by $model (e.g. Sales_Model_Contract) and update db
      *
-     * @param   string $_type
-     * @param   integer $_userId
+     * @param   string $model
+     * @param   integer $userId
      * @return  integer
      * 
      * @todo    lock db
      */
-    public function getNext($_type, $_userId)
+    public function getNext($model, $userId)
     {
-        // get number for type
+        // get number for model
         $select = $this->_getSelect();
-        $select->where($this->_db->quoteIdentifier('type') . ' = ?', $_type);
+        $select->where($this->_db->quoteInto($this->_db->quoteIdentifier('model') . ' = ? ', $model));
         $stmt = $this->_db->query($select);
         $queryResult = $stmt->fetch();
         $stmt->closeCursor();
-                
-        if (!$queryResult) {
+        
+        if (! $queryResult) {
+            // find latest used number if there is already one or more numbers has been created manually
+            
+            $c = explode('_Model_', $model);
+            $cName = $c[0] . '_Controller_' . $c[1];
+            $controller = $cName::getInstance();
+            $latestRecord = $controller->getAll('number', 'DESC')->getFirstRecord();
+            
+            $nextNumber = $latestRecord ? intval($latestRecord->number) + 1 : 1;
+            
             // create new number circle
             $number = new Sales_Model_Number(array(
-                'type'          => $_type,
-                'number'        => 1,
-                'account_id'    => $_userId,
-                'update_time'   => Tinebase_DateTime::now()
+                'model'       => $model,
+                'number'      => $nextNumber,
+                'account_id'  => $userId,
+                'update_time' => Tinebase_DateTime::now()
             ), TRUE);
             
             $number = $this->create($number);
@@ -72,7 +81,7 @@ class Sales_Backend_Number extends Tinebase_Backend_Sql_Abstract
             $number = new Sales_Model_Number($queryResult);
             
             $number->number++;
-            $number->account_id = $_userId;
+            $number->account_id = $userId;
             $number->update_time = Tinebase_DateTime::now();
             
             $number = $this->update($number);
