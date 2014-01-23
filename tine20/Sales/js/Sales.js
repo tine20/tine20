2fabb38e52a971fb85459cdfb7fc637f31fe6ff1
Alexander Stintzing <a.stintzing@metaways.de>
2014-01-23 12:35:52 +0100

    0009572: Apply Modelconfig to Sales
     - needs: https://gerrit.tine20.org/tine20/#/c/2759/
    
     - apply MC to the Sales App
     - Divisions: make module
     - Divisions: add history
     - Contracts: add start- and enddate
     - Contracts: remove fields: status, cleared, cleared_in
    
    https://forge.tine20.org/mantisbt/view.php?id=9572
    
    Change-Id: I68a10452ca844e710091f86bb736af64c05fef04
    Reviewed-on: https://gerrit.tine20.org/tine20/2755
    Tested-by: jenkins user
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Sales/js/Sales.js
+++ tine20/Sales/js/Sales.js
@@ -4,7 +4,7 @@
  * @package     Sales
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Philipp Schuele <p.schuele@metaways.de>
- * @copyright   Copyright (c) 2007-2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2007-2014 Metaways Infosystems GmbH (http://www.metaways.de)
  *
  */
  
@@ -25,87 +25,12 @@ Ext.namespace('Tine.Sales');
  * Constructs mainscreen of the Sales application
  */
 Tine.Sales.MainScreen = Ext.extend(Tine.widgets.MainScreen, {
+    appName: 'Sales',
     activeContentType: 'Product',
     contentTypes: [
-        {modelName: 'Product', requiredRight: null, singularContainerMode: true},
+        {modelName: 'Product', requiredRight: 'manage_products', singularContainerMode: true},
         {modelName: 'Contract', requiredRight: null, singularContainerMode: true, genericCtxActions: ['grants']},
-        {modelName: 'CostCenter', requiredRight: 'manage_costcenters', singularContainerMode: true}
+        {modelName: 'CostCenter', requiredRight: 'manage_costcenters', singularContainerMode: true},
+        {modelName: 'Division', requiredRight: null, singularContainerMode: true}
     ]
-});
-
-/**
- * @namespace Tine.Sales
- * @class Tine.Sales.FilterPanel
- * @extends Tine.widgets.persistentfilter.PickerPanel
- * Sales Filter Panel<br>
- * 
- * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
- * @author      Alexander Stintzing <a.stintzing@metaways.de>
- * @copyright   Copyright (c) 2007-2012 Metaways Infosystems GmbH (http://www.metaways.de)
- */
-Tine.Sales.ProductFilterPanel = function(config) {
-    Ext.apply(this, config);
-    Tine.Sales.ProductFilterPanel.superclass.constructor.call(this);
-};
-
-Ext.extend(Tine.Sales.ProductFilterPanel, Tine.widgets.persistentfilter.PickerPanel, {
-    filter: [{field: 'model', operator: 'equals', value: 'Sales_Model_ProductFilter'}]
-});
-
-Tine.Sales.ContractFilterPanel = function(config) {
-    Ext.apply(this, config);
-    Tine.Sales.ContractFilterPanel.superclass.constructor.call(this);
-};
-
-Ext.extend(Tine.Sales.ContractFilterPanel, Tine.widgets.persistentfilter.PickerPanel, {
-    filter: [{field: 'model', operator: 'equals', value: 'Sales_Model_ContractFilter'}]
-});
-
-Tine.Sales.CostCenterFilterPanel = function(config) {
-    Ext.apply(this, config);
-    Tine.Sales.CostCenterFilterPanel.superclass.constructor.call(this);
-};
-
-Ext.extend(Tine.Sales.CostCenterFilterPanel, Tine.widgets.persistentfilter.PickerPanel, {
-    filter: [{field: 'model', operator: 'equals', value: 'Sales_Model_CostCenterFilter'}]
-});
-
-/**
- * default contracts backend
- */
-Tine.Sales.contractBackend = new Tine.Tinebase.data.RecordProxy({
-    appName: 'Sales',
-    modelName: 'Contract',
-    recordClass: Tine.Sales.Model.Contract
-});
-
-/**
- * @namespace Tine.Sales
- * @class Tine.Sales.productBackend
- * @extends Tine.Tinebase.data.RecordProxy
- * 
- * Product Backend
- */ 
-Tine.Sales.productBackend = new Tine.Tinebase.data.RecordProxy({
-    appName: 'Sales',
-    modelName: 'Product',
-    recordClass: Tine.Sales.Model.Product
-});
-
-/**
- * default costcenter backend
- */
-Tine.Sales.costcenterBackend = new Tine.Tinebase.data.RecordProxy({
-    appName: 'Sales',
-    modelName: 'CostCenter',
-    recordClass: Tine.Sales.Model.CostCenter
-});
-
-/**
- * default division backend
- */
-Tine.Sales.divisionBackend = new Tine.Tinebase.data.RecordProxy({
-    appName: 'Sales',
-    modelName: 'Division',
-    recordClass: Tine.Sales.Model.Division
 });
