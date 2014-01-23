52de12d1be3981c17f1db61fe0808394d3c49654
Alexander Stintzing <a.stintzing@metaways.de>
2014-01-23 12:35:52 +0100

    0009572: Apply Modelconfig to Sales
     - needs: https://gerrit.tine20.org/tine20/#/c/2759/
    
     - apply MC to the Sales App
     - Divisions: make module
     - Divisions: add history
     - Contracts: add start- and enddate
     - Contracts: remove fields: status, cleared, cleared_in
    
    https://forge.tine20.org/mantisbt/view.php?id=9572
    
    Change-Id: I68a10452ca844e710091f86bb736af64c05fef04
    Reviewed-on: https://gerrit.tine20.org/tine20/2755
    Tested-by: jenkins user
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Sales/Model/DivisionFilter.php
+++ tine20/Sales/Model/DivisionFilter.php
@@ -5,7 +5,7 @@
  * @package     Sales
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Alexander Stintzing <a.stintzing@metaways.de>
- * @copyright   Copyright (c) 2012 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2012-2014 Metaways Infosystems GmbH (http://www.metaways.de)
  *
  */
 
@@ -15,22 +15,5 @@
  */
 class Sales_Model_DivisionFilter extends Tinebase_Model_Filter_FilterGroup
 {
-    /**
-     * @var string application of this filter group
-     */
-    protected $_applicationName = 'Sales';
-
-    /**
-     * @var string name of model this filter group is designed for
-     */
-    protected $_modelName = 'Sales_Model_Division';
-
-    /**
-     * @var array filter model fieldName => definition
-     */
-    protected $_filterModel = array(
-        'id'                  => array('filter' => 'Tinebase_Model_Filter_Id'),
-        'title'               => array('filter' => 'Tinebase_Model_Filter_Text'),
-        'query'               => array('filter' => 'Tinebase_Model_Filter_Query', 'options' => array('fields' => array('name'))),
-    );
+    protected $_configuredModel = 'Sales_Model_Division';
 }
