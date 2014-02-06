0d6d953027d651a610359d77a74fccd45e3b05a7
Alexander Stintzing <a.stintzing@metaways.de>
2014-02-06 19:17:58 +0100

    0009648: Create more DemoData
     - Create some Sales.Products
     - Create more Crm.Leads
    
    https://forge.tine20.org/mantisbt/view.php?id=9648
    
    Change-Id: I9a48ab9e3df7c2518ed2c2f454ad083d79ab076a
    Reviewed-on: https://gerrit.tine20.org/tine20/2782
    Tested-by: jenkins user
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Sales/Setup/DemoData.php
+++ tine20/Sales/Setup/DemoData.php
@@ -104,7 +104,69 @@ class Sales_Setup_DemoData extends Tinebase_Setup_DemoData_Abstract
      */
     protected function _createSharedProducts()
     {
-        // TODO: create some products
+        $products = array(
+            array(
+                'name' => '10 Port 100 MBit Ethernet Switch',
+                'description' => '10 Port Fast Ethernet Switch, RJ45',
+                'price' => '28.13',
+            ),
+            array(
+                'name' => '28 Port 100 MBit Ethernet Switch PoE',
+                'description' => '28 Port Fast Ethernet Switch, PoE, RJ45',
+                'price' => '1029.99',
+            ),
+            array(
+                'name' => '10 Port Gigabit Ethernet Switch',
+                'description' => '10 Port 1 Gigabit Switch, RJ45',
+                'price' => '78.87',
+            ),
+            array(
+                'name' => '28 Port Gigabit Ethernet Switch PoE',
+                'description' => '28 Port 1 Gigabit Ethernet Switch PoE',
+                'price' => '3496.45',
+            )
+        );
+        
+        $default = array(
+            'manufacturer' => 'SwitchCo',
+            'category' => self::$_en ? 'LAN Equipment' : 'Netzwerkausrüstung'
+        );
+        
+        foreach($products as $product) {
+            $this->_productController->create(new Sales_Model_Product(array_merge($product, $default)));
+        }
+        
+        $products = array(
+            array(
+                'name' => self::$_en ? '10m Cat. 5a red' : '10m Kat. 5a rot',
+                'description' => self::$_en ? '10m Cat. 5a red cable up to 100MBit.' : '10m Kat. 5a rotes Kabel. Erlaubt Übertragungsraten von bis zu 100MBit.',
+                'price' => '5.99',
+            ),
+            array(
+                'name' => self::$_en ? '10m Cat. 5a blue' : '10m Kat. 5a blau',
+                'description' => self::$_en ? '10m Cat. 5a blue cable up to 100MBit.' : '10m Kat. 5a blaues Kabel. Erlaubt Übertragungsraten von bis zu 100MBit.',
+                'price' => '5.99',
+            ),
+            array(
+                'name' => self::$_en ? '10m Cat. 6 red' : '10m Kat. 6 rot',
+                'description' => self::$_en ? '10m Cat. 6 red cable up to 1000MBit.' : '10m Kat. 5a rotes Kabel. Erlaubt Übertragungsraten von bis zu 1000MBit.',
+                'price' => '9.99',
+            ),
+            array(
+                'name' => self::$_en ? '10m Cat. 6 blue' : '10m Kat. 6 blau',
+                'description' => self::$_en ? '10m Cat. 6 blue cable up to 1000MBit.' : '10m Kat. 5a blaues Kabel. Erlaubt Übertragungsraten von bis zu 1000MBit.',
+                'price' => '9.99',
+            ),
+        );
+        
+        $default = array(
+            'manufacturer' => self::$_en ? 'Salad Cabels' : 'Salat Kabel & Co.',
+            'category' => self::$_en ? 'LAN Equipment' : 'Netzwerkausrüstung'
+        );
+        
+        foreach($products as $product) {
+            $this->_productController->create(new Sales_Model_Product(array_merge($product, $default)));
+        }
     }
     
     /**
