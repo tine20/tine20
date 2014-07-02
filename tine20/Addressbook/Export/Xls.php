52710595e31cde5ea79c190bb262e0c34cae1a11
Alexander Stintzing <a.stintzing@metaways.de>
2014-07-02 12:37:41 +0200

    0010020: Allow to define the default export definition
    the default export definition should be configurable in preferences
    
    https://forge.tine20.org/mantisbt/view.php?id=10020
    
    Change-Id: I729c4e5dbc0cacef7090922c102439f4dc5acbe7
    Reviewed-on: http://gerrit.tine20.com/customers/815
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Addressbook/Export/Xls.php
+++ tine20/Addressbook/Export/Xls.php
@@ -19,6 +19,19 @@
 class Addressbook_Export_Xls extends Tinebase_Export_Spreadsheet_Xls
 {
     /**
+     * the constructor
+     *
+     * @param Tinebase_Model_Filter_FilterGroup $_filter
+     * @param Tinebase_Controller_Record_Interface $_controller (optional)
+     * @param array $_additionalOptions (optional) additional options
+     */
+    public function __construct(Tinebase_Model_Filter_FilterGroup $_filter, Tinebase_Controller_Record_Interface $_controller = NULL, $_additionalOptions = array())
+    {
+        $this->_prefKey = Addressbook_Preference::DEFAULT_CONTACT_XLS_EXPORTCONFIG;
+        parent::__construct($_filter, $_controller, $_additionalOptions);
+    }
+    
+    /**
      * @var string $_applicationName
      */
     protected $_applicationName = 'Addressbook';
