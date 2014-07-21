ac95e8461aaed2273e64bc895f2b2dc4e8b721e9
Philipp Schüle <p.schuele@metaways.de>
2014-07-21 17:26:07 +0200

    adds maxLength for number and title fields in TA edit dlg
    Change-Id: I77811dcad9f229506c2dcc67a36cfb99478d72c7
    Reviewed-on: http://gerrit.tine20.com/customers/886
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Timetracker/js/TimeaccountEditDialog.js
+++ tine20/Timetracker/js/TimeaccountEditDialog.js
@@ -184,11 +184,13 @@ Tine.Timetracker.TimeaccountEditDialog = Ext.extend(Tine.widgets.dialog.EditDial
                     items: [[{
                         fieldLabel: this.app.i18n._('Number'),
                         name: 'number',
-                        allowBlank: false
+                        allowBlank: false,
+                        maxLength: 127
                         }, {
                         columnWidth: .666,
                         fieldLabel: this.app.i18n._('Title'),
                         name: 'title',
+                        maxLength: 255,
                         allowBlank: false
                         }], [{
                         columnWidth: 1,
