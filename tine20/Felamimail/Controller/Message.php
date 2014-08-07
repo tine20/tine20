03656ef510a4d12314ec5be444549380c886e9c4
Alexander Stintzing <a.stintzing@metaways.de>
2014-08-07 10:36:47 +0200

    0010126: Text gets removed in invalid tags
    In the invalid HTML Structure like
    "<pre><font>Text .." the "<font>Text .." content is removed completely.
    
    Change-Id: Iec3c6f986afb302071395921a1b9e1e5cb2d1952
    Reviewed-on: http://gerrit.tine20.com/customers/942
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Felamimail/Controller/Message.php
+++ tine20/Felamimail/Controller/Message.php
@@ -670,6 +670,9 @@ class Felamimail_Controller_Message extends Tinebase_Controller_Record_Abstract
         $config->set('HTML.DefinitionID', 'purify message body contents');
         $config->set('HTML.DefinitionRev', 1);
         
+        // @see: http://htmlpurifier.org/live/configdoc/plain.html#HTML.TidyLevel
+        $config->set('HTML.TidyLevel', 'heavy');
+        
         // some config values to consider
         /*
         $config->set('Attr.EnableID', true);
