eb90d28b2a38ea20f30d511a0543c328a86487b3
sstamer <s.stamer@metaways.de>
2014-10-10 11:40:06 +0200

    Add helper clifunction to repair/set samba options
    Change-Id: I2e691ce494567e999faa21a91062b1654e0bd836
    Reviewed-on: http://gerrit.tine20.com/customers/1250
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Admin/Frontend/Cli.php
+++ tine20/Admin/Frontend/Cli.php
@@ -48,4 +48,48 @@ class Admin_Frontend_Cli extends Tinebase_Frontend_Cli_Abstract
     {
         parent::_import($_opts);
     }
+    
+    /**
+     * overwirte Samba options for users
+     *
+     */
+    public function repairUserSambaoptions($_opts)
+    {
+        $args = $_opts->getRemainingArgs();
+        if ($_opts->d) {
+            array_push($args, '--dry');
+        }
+        $_opts->setArguments($args);
+        $blacklist = array(); // List of Loginnames
+        $count = 0;
+        $tinebaseUser  = Tinebase_User::getInstance();
+        $users = $tinebaseUser->getUsers();
+        
+        foreach ($users as $id) {
+            $user = $tinebaseUser->getFullUserById($id->getId());
+            
+            if (isset($user['sambaSAM']) && empty($user['sambaSAM']['homeDrive']) && !in_array($user->accountLoginName, $blacklist)) {
+                echo($user->getId() . ' : ' . $user->accountLoginName);
+                echo("\n");
+                
+                //This must be adjusted
+                $samUser = new Tinebase_Model_SAMUser(array(
+                    'homePath'    => '\\\\fileserver\\' . $user->accountLoginName,
+                    'homeDrive'   => 'H:',
+                    'logonScript' => 'script.cmd',
+                    'profilePath' => '\\\\fileserver\\profiles\\' . $user->accountLoginName
+                ));
+                $user->sambaSAM = $samUser;
+                
+                if ($_opts->d) {
+                    print_r($user);
+                } else {
+                    $tinebaseUser->updateUser($user);
+                }
+                $count++;
+            };
+        }
+        echo('Found ' . $count . ' users!');
+        echo("\n");
+    }
 }
