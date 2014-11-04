94be7d691b62d2245fcca761061c31e7ef996484
Alexander Stintzing <a.stintzing@metaways.de>
2014-11-04 11:15:46 +0100

    Sales.Offer: Switch number prefix to ANG-
     - see: rt133834
    
    Change-Id: I485382678ae8aa1621429fc5a11a4a4fc077f9f9
    Reviewed-on: http://gerrit.tine20.com/customers/1330
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Tested-by: sstamer <s.stamer@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tests/tine20/Sales/OfferControllerTests.php
+++ tests/tine20/Sales/OfferControllerTests.php
@@ -35,22 +35,22 @@ class Sales_OfferControllerTests extends TestCase
         
         $record = $controller->create(new Sales_Model_Offer(array('title' => 'auto1')));
         
-        $this->assertEquals('AN-00001', $record->number);
+        $this->assertEquals('ANG-00001', $record->number);
         
         $record = $controller->create(new Sales_Model_Offer(array('title' => 'auto2')));
         
-        $this->assertEquals('AN-00002', $record->number);
+        $this->assertEquals('ANG-00002', $record->number);
         
         // set number to 4, should return the formatted number
         $record = $controller->create(new Sales_Model_Offer(array('title' => 'manu1', 'number' => 4)));
-        $this->assertEquals('AN-00004', $record->number);
+        $this->assertEquals('ANG-00004', $record->number);
         
         // the next number should be a number after the manual number
         $record = $controller->create(new Sales_Model_Offer(array('title' => 'auto3')));
-        $this->assertEquals('AN-00005', $record->number);
+        $this->assertEquals('ANG-00005', $record->number);
         
         // the user manually set this numer, so this should be corrected
-        $record = $controller->create(new Sales_Model_Offer(array('title' => 'manu1', 'number' => 'AN-100')));
-        $this->assertEquals('AN-00100', $record->number);
+        $record = $controller->create(new Sales_Model_Offer(array('title' => 'manu1', 'number' => 'ANG-100')));
+        $this->assertEquals('ANG-00100', $record->number);
     }
 }
