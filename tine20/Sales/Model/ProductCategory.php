5b2ae8656c74cc8cd074ad2de5e262c74be64ae8
Lars Kneschke <l.kneschke@metaways.de>
2015-04-11 07:17:12 +0200

    sales product improvements
    * added more columns to sales
      * product number
      * purchase price
      * sales price
      * gtin
    * made categories working
    * improved layout of edit dialogue
    * improved handling of app config settings
    
    Change-Id: I7f18eef605c2d7d05a348a4d6553b61c3e0b9b04
    Reviewed-on: http://gerrit.tine20.com/customers/1816
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Reviewed-by: Lars Kneschke <l.kneschke@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

---
+++ tine20/Sales/Model/ProductCategory.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Tine 2.0
+ * 
+ * @package     Sales
+ * @subpackage  Model
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ * @copyright   Copyright (c) 2015-2015 Metaways Infosystems GmbH (http://www.metaways.de)
+ *
+ */
+
+/**
+ * Product Category Record Class
+ * 
+ * @package     Sales
+ * @subpackage  Model
+ */
+class Sales_Model_ProductCategory extends Tinebase_Config_KeyFieldRecord
+{
+    /**
+     * application the record belongs to
+     *
+     * @var string
+     */
+    protected $_application = 'Sales';
+}
