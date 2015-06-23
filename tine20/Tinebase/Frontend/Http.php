38f0850425605589d66232039b66935cd3c8b2d7
Cornelius Weiß <mail@corneliusweiss.de>
2015-06-23 10:16:40 +0200

    cope with jsonKey for SSO logins
    Change-Id: Iad14ed1d112a09fda7b1a05c234db7150354bd8d
    Reviewed-on: http://gerrit.tine20.com/customers/1987
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Tested-by: sstamer <s.stamer@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/Frontend/Http.php
+++ tine20/Tinebase/Frontend/Http.php
@@ -116,6 +116,7 @@ class Tinebase_Frontend_Http extends Tinebase_Frontend_Http_Abstract
         if (isset($_POST['openid_action']) && $_POST['openid_action'] === 'login') {
             $server->login($_POST['openid_identifier'], $_POST['password'], $_POST['username']);
             unset($_GET['openid_action']);
+            $this->_setJsonKeyCookie();
             Zend_OpenId::redirect(dirname(Zend_OpenId::selfUrl()) . '/index.php', $_GET);
 
         // display openId login form
@@ -320,7 +321,7 @@ class Tinebase_Frontend_Http extends Tinebase_Frontend_Http_Abstract
         echo $view->render('update.php');
         exit();
     }
-    
+
     /**
      * login from HTTP post 
      * 
@@ -344,6 +345,8 @@ class Tinebase_Frontend_Http extends Tinebase_Frontend_Http_Abstract
         }
         
         if ($success === TRUE) {
+            $this->_setJsonKeyCookie();
+
             $ccAdapter = Tinebase_Auth_CredentialCache::getInstance()->getCacheAdapter();
             if (Tinebase_Core::isRegistered(Tinebase_Core::USERCREDENTIALCACHE)) {
                 $ccAdapter->setCache(Tinebase_Core::getUserCredentialCache());
@@ -371,14 +374,36 @@ class Tinebase_Frontend_Http extends Tinebase_Frontend_Http_Abstract
 
         $this->_renderMainScreen();
     }
-    
+
+    /**
+     * put jsonKey into separate cookie
+     *
+     * this is needed if login is not done by the client itself
+     */
+    protected function _setJsonKeyCookie()
+    {
+        // SSO Login
+        $cookie_params = session_get_cookie_params();
+
+        // don't issue errors in unit tests
+        @setcookie(
+            'TINE20JSONKEY',
+            Tinebase_Core::get('jsonKey'),
+            0,
+            $cookie_params['path'],
+            $cookie_params['domain'],
+            $cookie_params['secure']
+        );
+    }
+
     /**
      * checks authentication and display Tine 2.0 main screen 
      */
     public function mainScreen()
     {
         $this->checkAuth();
-        
+
+        $this->_setJsonKeyCookie();
         $this->_renderMainScreen();
     }
     
