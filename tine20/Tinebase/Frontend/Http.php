b202109a2867280b443ac3812784e4923672d1b4
sstamer <s.stamer@metaways.de>
2015-09-16 11:23:26 +0200

    Do not always set a cookie
    - partial reverts charge Iad14ed1d112a09fda7b1a05c234db7150354bd8d
    
    Change-Id: I43ba4c279734844c7e3ff691505525e5e1f8b345
    Reviewed-on: http://gerrit.tine20.com/customers/2201
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/Frontend/Http.php
+++ tine20/Tinebase/Frontend/Http.php
@@ -402,8 +402,6 @@ class Tinebase_Frontend_Http extends Tinebase_Frontend_Http_Abstract
     public function mainScreen()
     {
         $this->checkAuth();
-
-        $this->_setJsonKeyCookie();
         $this->_renderMainScreen();
     }
     
