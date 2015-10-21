fc1c185c662d3020c55e760a2160cf6fade3403f
Philipp Schüle <p.schuele@metaways.de>
2015-10-21 11:45:34 +0200

    increase timeout for update folder cache to 5 mins
    Change-Id: I37f21a2fb2434dea8deb13436c76cf35dd2dfa96
    Reviewed-on: http://gerrit.tine20.com/customers/2296
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Felamimail/Frontend/Json.php
+++ tine20/Felamimail/Frontend/Json.php
@@ -123,6 +123,9 @@ class Felamimail_Frontend_Json extends Tinebase_Frontend_Json_Abstract
      */
     public function updateFolderCache($accountId, $folderName)
     {
+        // this may take longer
+        $this->_longRunningRequest(300);
+
         $result = Felamimail_Controller_Cache_Folder::getInstance()->update($accountId, $folderName, TRUE);
         return $this->_multipleRecordsToJson($result);
     }
