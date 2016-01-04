45f1c45290d1ce797efd784d9a501569d20cc929
Philipp Schüle <p.schuele@metaways.de>
2016-01-04 15:46:22 +0100

    deactivate year view by default
    see https://forge.tine20.org/view.php?id=11304#c17250
    
    Change-Id: I147f288d7ca2c01b318f608bad3ba440fc6cf1a4
    Reviewed-on: https://gerrit.tine20.org/tine20/3272
    Tested-by: jenkins user
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Calendar/Config.php
+++ tine20/Calendar/Config.php
@@ -346,7 +346,7 @@ class Calendar_Config extends Tinebase_Config_Abstract
             ),
             'default'               => array(
                 self::FEATURE_SPLIT_VIEW                        => true,
-                self::FEATURE_YEAR_VIEW                         => true,
+                self::FEATURE_YEAR_VIEW                         => false,
                 self::FEATURE_EXTENDED_EVENT_CONTEXT_ACTIONS    => true,
                 self::FEATURE_COLOR_BY    => true,
             ),
