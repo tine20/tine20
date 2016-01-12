d40012ab0425a9c9b76036c3e559c55be55f1412
Philipp Schüle <p.schuele@metaways.de>
2016-01-12 09:57:43 +0100

    normalizes test user mail and credentials fetching
    Change-Id: I5976b68f63f13b512520edf5d1609dec88e8e6ba
    Reviewed-on: http://gerrit.tine20.com/customers/2573
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tests/tine20/Expressomail/Controller/AccountTest.php
+++ tests/tine20/Expressomail/Controller/AccountTest.php
@@ -172,23 +172,23 @@ class Expressomail_Controller_AccountTest extends PHPUnit_Framework_TestCase
      */
     public function testChangePasswordAndUpdateCredentialCache()
     {
-        $testConfig = Zend_Registry::get('testConfig');
+        $testCredentials = TestServer::getInstance()->getTestCredentials();
         
         $account = clone($this->_account);
         unset($account->id);
         $account->type = Expressomail_Model_Account::TYPE_USER;
-        $account->user = $testConfig->username;
+        $account->user = $testCredentials['username'];
         $imapConfig = Tinebase_Config::getInstance()->get(Tinebase_Config::IMAP, new Tinebase_Config_Struct())->toArray();
         if (isset($imapConfig['domain']) && ! empty($imapConfig['domain'])) {
             $account->user .= '@' . $imapConfig['domain'];
         }
-        $account->password = $testConfig->password;
+        $account->password = $testCredentials['password'];
         $account = $this->_controller->create($account);
         
         $testPw = 'testpwd';
         
         // change pw & update credential cache
-        $this->_setCredentials($testConfig->username, $testPw);
+        $this->_setCredentials($testCredentials['password'], $testPw);
         $account = $this->_controller->get($account->getId());
 
         // try to connect to imap
