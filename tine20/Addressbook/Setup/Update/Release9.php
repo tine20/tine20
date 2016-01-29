f6a64a60ae40292f6b152fd2779d166b6818715f
Philipp Schüle <p.schuele@metaways.de>
2016-01-29 16:48:19 +0100

    adds basic table schema for SimpleRecords
    Change-Id: Ie1b51ca913bcffb470c1c4533aa93cfd3af54aa4
    Reviewed-on: http://gerrit.tine20.com/customers/2643
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Addressbook/Setup/Update/Release9.php
+++ tine20/Addressbook/Setup/Update/Release9.php
@@ -12,76 +12,12 @@ class Addressbook_Setup_Update_Release9 extends Setup_Update_Abstract
 {
     /**
      * update to 9.1: add list_roles table
-     * 
+     *
      * @return void
      */
     public function update_0()
     {
-        $table = Setup_Backend_Schema_Table_Factory::factory('String', '
-         <table>
-            <name>addressbook_list_role</name>
-            <engine>InnoDB</engine>
-            <charset>utf8</charset>
-            <version>1</version>
-            <declaration>
-                <field>
-                    <name>id</name>
-                    <type>text</type>
-                    <length>40</length>
-                    <notnull>true</notnull>
-                </field>
-                <field>
-                    <name>name</name>
-                    <type>text</type>
-                    <length>256</length>
-                    <notnull>false</notnull>
-                </field>
-                <field>
-                    <name>description</name>
-                    <type>text</type>
-                    <notnull>false</notnull>
-                </field>
-                <field>
-                    <name>created_by</name>
-                    <type>text</type>
-                    <length>40</length>
-                </field>
-                <field>
-                    <name>creation_time</name>
-                    <type>datetime</type>
-                </field>
-                <field>
-                    <name>last_modified_by</name>
-                    <type>text</type>
-                    <length>40</length>
-                </field>
-                <field>
-                    <name>last_modified_time</name>
-                    <type>datetime</type>
-                </field>
-                <field>
-                    <name>is_deleted</name>
-                    <type>boolean</type>
-                    <default>false</default>
-                </field>
-                <field>
-                    <name>deleted_by</name>
-                    <type>text</type>
-                    <length>40</length>
-                </field>
-                <field>
-                    <name>deleted_time</name>
-                    <type>datetime</type>
-                </field>
-                <field>
-                    <name>seq</name>
-                    <type>integer</type>
-                    <notnull>true</notnull>
-                    <default>0</default>
-                </field>
-            </declaration>
-        </table>
-        ');
+        $table = Setup_Backend_Schema_Table_Factory::getSimpleRecordTable('addressbook_list_role');
         $this->_backend->createTable($table, 'Addressbook', 'addressbook_list_role');
         $this->setApplicationVersion('Addressbook', '9.1');
     }
