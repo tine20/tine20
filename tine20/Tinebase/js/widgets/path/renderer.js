4c6296ff3b3815cd5ba4de83741bcf5eea1d9ef5
Cornelius Weiß <c.weiss@metaways.de>
2016-02-23 15:15:51 +0100

    path UI: cope with types
    Change-Id: Ie173545321888c822c92f7010ed8821fd5b277ad

--- tine20/Tinebase/js/widgets/path/renderer.js
+++ tine20/Tinebase/js/widgets/path/renderer.js
@@ -42,10 +42,9 @@ Tine.widgets.path.pathRenderer = function(path, queryString) {
 
     pathName = pathName
         .replace(/^\//, '')
-        .replace(/\//g, ' \u0362 ');
+        .replace(/\//g, '\u0362');
 
     pathName = Ext.util.Format.htmlEncode(pathName);
-    console.log(pathName);
 
     if (queryParts.length) {
         var hasQueryMatches = false,
@@ -66,8 +65,13 @@ Tine.widgets.path.pathRenderer = function(path, queryString) {
         }
     }
 
-    var qtip = pathName.replace(/\u0362/g, "<br/>&nbsp;" + Ext.util.Format.htmlEncode('\u00BB'));
-    pathName = pathName.replace(/\u0362/g, Ext.util.Format.htmlEncode('\u00BB'));
+    var qtip = pathName.replace(/(?:{(.*)}){0,1}\u0362/g, function(all, type) {
+        return "<br/>&nbsp;" + (type ? type + ' ' : '') + Ext.util.Format.htmlEncode('\u00BB') + '&nbsp;';
+    });
+
+    pathName = pathName.replace(/(?:{(.*)}){0,1}\u0362/g, function(all, type) {
+        return "&nbsp;" + (type ? '<span class="tb-widgets-path-pathitem-type">' + type[0] + '</span>' : '') + Ext.util.Format.htmlEncode('\u00BB') + '&nbsp;';
+    });
 
 
     return pathName ? '<div class="tb-widgets-path-pathitem" ext:qtip="' + Ext.util.Format.htmlEncode(qtip) + '">' + pathName + '</div>' : pathName;
