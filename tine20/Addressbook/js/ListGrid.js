fa8315c0fa7fe8b9f3fa7bf60c95f415910d556c
Philipp Schüle <p.schuele@metaways.de>
2016-02-24 13:27:03 +0100

    remove leading comma in email grid column of lists
    Change-Id: I3c903a4898ec80282b8a9a66bad744b92e842504
    Reviewed-on: http://gerrit.tine20.com/customers/2795
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Addressbook/js/ListGrid.js
+++ tine20/Addressbook/js/ListGrid.js
@@ -99,7 +99,14 @@ Tine.Addressbook.ListGridPanel = Ext.extend(Tine.widgets.grid.GridPanel, {
         return [
             { id: 'type', header: this.app.i18n._('Type'), dataIndex: 'type', width: 30, renderer: Tine.Addressbook.ListGridPanel.listTypeRenderer, hidden: false },
             { id: 'name', header: this.app.i18n._('Name'), dataIndex: 'name', width: 30, hidden: false },
-            { id: 'emails', header: this.app.i18n._('Emails'), dataIndex: 'emails', hidden: false },
+            { id: 'emails', header: this.app.i18n._('Emails'), dataIndex: 'emails', hidden: false, renderer: function(value) {
+                if (! value) {
+                    return '';
+                }
+                // TODO should be fixed on server side
+                // remove leading comma
+                return value.replace(/^,/, '');
+            }},
         ].concat(this.getModlogColumns().concat(this.getCustomfieldColumns()));
     },
     
