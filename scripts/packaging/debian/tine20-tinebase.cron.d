46c0c8bebd9cf2ffb149fa2b3161f0c37b048e46
Philipp Schüle <p.schuele@metaways.de>
2016-02-25 12:13:53 +0100

    distro packaging has moved to separate repo
    Change-Id: I1bf4fa6b99b74a4f364e007a2f0aa2c5e97355ef
    Reviewed-on: http://gerrit.tine20.com/customers/2801
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- scripts/packaging/debian/tine20-tinebase.cron.d
+++
@@ -1,4 +0,0 @@
-SHELL=/bin/bash
-PATH=/sbin:/bin:/usr/sbin:/usr/bin
-
-* * * * *	www-data	/usr/bin/php -f /usr/share/tine20/tine20.php -- --config=/etc/tine20/config.inc.php --method=Tinebase.triggerAsyncEvents | logger -p daemon.notice -t "Tine 2.0 scheduler"
