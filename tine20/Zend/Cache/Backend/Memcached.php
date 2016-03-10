e1a5ac829314b041c6bbd3ec9829cac0b5f46f7d
Paul Mehrer <p.mehrer@metaways.de>
2016-03-10 11:31:16 +0100

    removed Zend_Cache from tine20/library to use composer maintained version
    0011160: Update to latest ZF1
    
    https://forge.tine20.org/view.php?id=11160
    
    Change-Id: I06e87774d7e2646e9e760bbec59ad7506f5b8c05
    Reviewed-on: https://gerrit.tine20.org/tine20/3286
    Tested-by: jenkins user
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Zend/Cache/Backend/Memcached.php
+++ tine20/Zend/Cache/Backend/Memcached.php
@@ -18,7 +18,7 @@
  * @copyright  Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  * @version    $Id$
- * 
+ *
  * NOTE: overwritten to always clear the cache even if a CLEANING_MODE_TAG_* mode is used (-> @see clean())
  * @todo check if still needed / clearing by tag should not be used any more
  */
@@ -266,7 +266,7 @@ class Zend_Cache_Backend_Memcached extends Zend_Cache_Backend implements Zend_Ca
             case Zend_Cache::CLEANING_MODE_NOT_MATCHING_TAG:
             case Zend_Cache::CLEANING_MODE_MATCHING_ANY_TAG:
                 $this->_log(self::TAGS_UNSUPPORTED_BY_CLEAN_OF_MEMCACHED_BACKEND);
-                // fall through / clear all entries
+            // fall through / clear all entries
             case Zend_Cache::CLEANING_MODE_ALL:
                 return $this->_memcache->flush();
                 break;
