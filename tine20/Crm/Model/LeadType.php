33982dadd76896d460d4d857105d5c582b007534
sstamer <s.stamer@metaways.de>
2016-04-11 15:18:33 +0200

    the LeadType model is still needed for the new config
    Change-Id: Ie0af9d3ca5123884ca7a64493806079a707515a3
    Reviewed-on: http://gerrit.tine20.com/customers/3038
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

---
+++ tine20/Crm/Model/LeadType.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package     Crm
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiss <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2015 Metaways Infosystems GmbH (http://www.metaways.de)
+ */
+
+/**
+ * lead type record
+ * 
+ * @package     Crm
+ */
+class Crm_Model_LeadType extends Tinebase_Config_KeyFieldRecord
+{
+
+}
