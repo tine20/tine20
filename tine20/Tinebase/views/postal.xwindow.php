f8e03873e0fe878c628ec82a29c6b486a88141ad
Cornelius Weiß <c.weiss@metaways.de>
2016-05-13 17:36:31 +0200

    0011830: integrate API for cross window communication
    integrate postal.js as pub/sub library
    * integrate postal.xwindow for cross window communication
    * have iframe service from server to ease third party app integration
     https://tine.example.com/?method=Tinebase.getPostalXWindow
    * integrate postal.request-response for rpc style communication
    
    https://forge.tine20.org/view.php?id=11830
    
    Change-Id: I87592e8756f3608e31d82f3659dcffff004ccfbd
    Reviewed-on: http://gerrit.tine20.com/customers/3162
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Reviewed-by: sstamer <s.stamer@metaways.de>
    Tested-by: sstamer <s.stamer@metaways.de>

---
+++ tine20/Tinebase/views/postal.xwindow.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * postal.xwindow view
+ *
+ * @package     Tinebase
+ * @subpackage  Views
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Cornelius Weiß <c.weiss@metaways.de>
+ * @copyright   Copyright (c) 2016 Metaways Infosystems GmbH (http://www.metaways.de)
+ */
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <div>
+      <input type="text" value="data.changed" id="topic"> <br />
+      <textarea rows="4" cols="50" id="message">This message was send with love from an external thirdparty Tine 2.0 xwindow client</textarea><br />
+      <br />
+      <input type="button" value="Send Message" id="sendMessage">
+      <input type="button" value="Clear" id="clear">
+      <input type="button" value="Disconnect" id="disconnect">
+    </div>
+
+    <div id="messages"></div>
+
+    <script src="Tinebase/js/node_modules/babel-core/browser-polyfill.js"></script>
+    <script src="Tinebase/js/node_modules/lodash/index.js"></script>
+    <script src="Tinebase/js/node_modules/jquery/dist/jquery.js"></script>
+    <script src="Tinebase/js/node_modules/postal/lib/postal.js"></script>
+    <script src="Tinebase/js/node_modules/postal.federation/lib/postal.federation.js"></script>
+    <script src="Tinebase/js/node_modules/postal.request-response/lib/postal.request-response.js"></script>
+    <script src="library/Store/store2.js"></script>
+    <script src="library/Store/store.bind.js"></script>
+    <script src="Tinebase/js/node_modules/postal.xwindow/lib/postal.xwindow.js"></script>
+    <script>
+        var config = postal.fedx.transports.xwindow.configure();
+        postal.fedx.transports.xwindow.configure( {
+            localStoragePrefix: "<?php echo Tinebase_Core::getUrl('path') . '/Tine.'; ?>" + config.localStoragePrefix
+        } );
+
+        // We need to tell postal how to get a deferred instance
+        postal.configuration.promise.createDeferred = function() {
+            return new $.Deferred();
+        };
+        // We need to tell postal how to get a "public-facing"/safe promise instance
+        postal.configuration.promise.getPromise = function(dfd) {
+            return dfd.promise();
+        };
+
+        postal.instanceId('xwindow-' + _.random(0,1000));
+        postal.fedx.addFilter([
+            { channel: 'thirdparty',   topic: '#', direction: 'both' },
+//            { channel : 'postal.request-response', topic : '#', direction : 'both'}
+        ]);
+        postal.subscribe( {
+            channel: "thirdparty",
+            topic: "#",
+            callback : function ( d, e ) {
+                $( "#messages" ).append( "<div><pre>" + JSON.stringify( e, null, 4 ) + "</pre></div>" );
+            }
+        } );
+
+        $(function() {
+            $( "#clear" ).on( "click", function() {
+                $( "#messages" ).html( "" );
+                postal.publish( {
+                    channel: "thirdparty",
+                    topic: "clear"
+                } );
+            } );
+            $("#sendMessage").on('click', function(){
+                postal.publish({
+                    channel: "thirdparty",
+                    topic: $("#topic").val(),
+                    data: $("#message").val()
+                });
+            });
+            $( "#disconnect" ).on( "click", function() {
+                postal.fedx.disconnect( { } );
+            } );
+
+            $("#msg3").on('click', function(){
+                postal.fedx.disconnect();
+            });
+            postal.fedx.signalReady();
+        });
+    </script>
+  </body>
+</html>
