ce5c8d70404fe830049ffa40e04f13e1271a1d85
Philipp Schüle <p.schuele@metaways.de>
2016-06-09 12:20:29 +0200

    Setup needs Tinebase libs (webpack), too
    Change-Id: Iaec7e2fd8bb28019c15d5c77c9b0dd7b6ebd7442
    Reviewed-on: http://gerrit.tine20.com/customers/3220
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Jenkins CI (http://ci.tine20.com/)

--- tine20/Setup/views/jsclient.php
+++ tine20/Setup/views/jsclient.php
@@ -42,6 +42,7 @@
                 echo "\n    <link rel='stylesheet' type='text/css' href='Tinebase/css/Tinebase-FAT.css' />";
                 echo "\n    <link rel='stylesheet' type='text/css' href='Setup/css/Setup-FAT.css' />";
                 echo "\n    <script type='text/javascript' language='javascript' src='Tinebase/js/Tinebase-FAT.js'></script>";
+                echo "\n    <script type='text/javascript' language='javascript' src='Tinebase/js/Tinebase-libs-FAT.js'></script>";
                 echo "\n    <script type='text/javascript' language='javascript' src='Setup/js/Setup-FAT.js'></script>";
                 echo "\n    <script type='text/javascript' language='javascript' src='Tinebase/js/Tinebase-lang-" . (string)$locale . ".js'></script>";
                 echo "\n    <script type='text/javascript' language='javascript' src='Setup/js/Setup-lang-" . (string)$locale . ".js'></script>";
@@ -51,6 +52,7 @@
                 echo "\n    <link rel='stylesheet' type='text/css' href='Tinebase/css/Tinebase-FAT.css' />";
                 echo "\n    <link rel='stylesheet' type='text/css' href='Setup/css/Setup-FAT.css' />";
                 echo "\n    <script type='text/javascript' language='javascript' src='Tinebase/js/Tinebase-FAT-debug.js'></script>";
+                echo "\n    <script type='text/javascript' language='javascript' src='Tinebase/js/Tinebase-libs-FAT-debug.js'></script>";
                 echo "\n    <script type='text/javascript' language='javascript' src='Setup/js/Setup-FAT-debug.js'></script>";
                 echo "\n    <script type='text/javascript' language='javascript' src='Tinebase/js/Tinebase-lang-" . (string)$locale . "-debug.js'></script>";
                 echo "\n    <script type='text/javascript' language='javascript' src='Setup/js/Setup-lang-" . (string)$locale . "-debug.js'></script>";
