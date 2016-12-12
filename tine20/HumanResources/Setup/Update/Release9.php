58d2257ba6a251c78a6edbdcc0172b1db1c454ca
sstamer <s.stamer@metaways.de>
2016-12-12 15:11:41 +0100

    0012418: Extrafreetime days can be negative
    https://forge.tine20.org/view.php?id=12418
    
    Change-Id: Ia1fd79ab07ef1d0af36d8a324cbd1ec8c763038a
    Reviewed-on: http://gerrit.tine20.com/customers/3950
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

---
+++ tine20/HumanResources/Setup/Update/Release9.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     HumanResources
+ * @subpackage  Setup
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL3
+ * @copyright   Copyright (c) 2016 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Stefanie Stamer <s.stamer@metaways.de>
+ */
+class HumanResources_Setup_Update_Release9 extends Setup_Update_Abstract
+{
+    /**
+     * update to 9.1
+     * 
+     *  - Extrafreetime days can be negative
+     */
+    public function update_0()
+    {
+        $declaration = new Setup_Backend_Schema_Field_Xml('
+                <field>
+                    <name>days</name>
+                    <type>integer</type>
+                    <notnull>true</notnull>
+                    <unsigned>false</unsigned>
+                    <length>4</length>
+                </field>');
+        $this->_backend->alterCol('humanresources_extrafreetime', $declaration);
+
+        $this->setTableVersion('humanresources_extrafreetime', '3');
+        $this->setApplicationVersion('HumanResources', '9.1');
+    }
+}
