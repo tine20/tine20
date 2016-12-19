40c53f8db8724e00136bf5ecee900b9c28e332ed
Philipp Schüle <p.schuele@metaways.de>
2016-12-19 16:34:02 +0100

    0012442: show record in import failure grid
    https://forge.tine20.org/view.php?id=12442
    
    Change-Id: If45b3cb6a34abcdb97012b58610c21079ec90446
    Reviewed-on: http://gerrit.tine20.com/customers/3981
    Tested-by: Philipp Schüle <p.schuele@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/js/widgets/dialog/ImportDialog.js
+++ tine20/Tinebase/js/widgets/dialog/ImportDialog.js
@@ -660,7 +660,11 @@ Tine.widgets.dialog.ImportDialog = Ext.extend(Tine.widgets.dialog.WizardPanel, {
                     plugins: exceptionExpander,
                     columns: [
                         exceptionExpander,
-                        { id: 'index', header: _('Index'), width: 60, sortable: false, dataIndex: 'index'}, 
+                        { id: 'index', header: _('Index'), width: 60, sortable: false, dataIndex: 'index'},
+                        { id: 'exception', header: _('Record'), width: 120, sortable: false, dataIndex: 'exception', renderer: function(exception) {
+                            // TODO find a way to show record more pretty
+                            return exception.clientRecord ? Ext.util.JSON.encode(exception.clientRecord) : '';
+                        }},
                         { id: 'failure', header: _('Failure'), width: 60, sortable: false, dataIndex: 'message'}
                     ],
                     autoExpandColumn: 'failure'
