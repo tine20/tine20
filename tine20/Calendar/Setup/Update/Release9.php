35a2153a84b4c1c997e38de07a1324414866dae4
Philipp Schüle <p.schuele@metaways.de>
2017-01-19 17:30:54 +0100

    0012538: fix displaycontainer calendar update script broken
    fixes update script Calendar 9.4
    
    https://forge.tine20.org/view.php?id=12538
    
    Change-Id: Idee8d7f958dd200371fc813f77169e81b6041b23
    Reviewed-on: http://gerrit.tine20.com/customers/4117
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Calendar/Setup/Update/Release9.php
+++ tine20/Calendar/Setup/Update/Release9.php
@@ -137,7 +137,7 @@ class Calendar_Setup_Update_Release9 extends Setup_Update_Abstract
         }
 
         // if called out of normal update process
-        if ($this->getApplicationVersion == '9.4') {
+        if ($this->getApplicationVersion('Calendar') == '9.4') {
             $this->setApplicationVersion('Calendar', '9.5');
         }
     }
