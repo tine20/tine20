6c30369be485d9521509fc31ebd7ae0d61032d00
Philipp Schüle <p.schuele@metaways.de>
2017-02-08 17:11:58 +0100

    0012622: filter non-ascii chars from mail filenames
    https://forge.tine20.org/view.php?id=12622
    
    Change-Id: If9776921e3fbcf96c32a14f6a3f3a2f0301bc732
    Reviewed-on: http://gerrit.tine20.com/customers/4181
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Filemanager/Controller/Node.php
+++ tine20/Filemanager/Controller/Node.php
@@ -1308,6 +1308,9 @@ class Filemanager_Controller_Node extends Tinebase_Controller_Record_Abstract
 
         // remove '/' from name as this might break paths
         $subject = preg_replace('/[\/]+/', '', $message->subject);
+        // remove possible harmful utf-8 chars
+        // TODO should not be enabled by default (configurable?)
+        $subject = Tinebase_Helper::mbConvertTo($subject, 'ASCII');
         $name = mb_substr($subject, 0, 245) . '_' . substr(md5($message->messageuid . $message->folder_id), 0, 10) . '.eml';
 
         return $name;
