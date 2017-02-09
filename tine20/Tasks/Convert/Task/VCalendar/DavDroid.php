6dc1a0a7c5727b1a1c2d0b832240a8bca77a45d4
Ingo Ratsdorf <ingo@envirology.co.nz>
2017-02-10 00:04:54 +1300

    Task write support for DAVDroid
    This enables write support via CalDAV for DAVDroid, see
    https://forge.tine20.org/view.php?id=10980

---
+++ tine20/Tasks/Convert/Task/VCalendar/DavDroid.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Calendar
+ * @subpackage  Convert
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Ingo Ratsdorf <ingo@envirology.co.nz>
+ * @copyright   Copyright (c) 2011-2013 Metaways Infosystems GmbH (http://www.metaways.de)
+ *
+ */
+
+/**
+ * class to convert a iphone vcalendar to event model and back again
+ *
+ * @package     Tasks
+ * @subpackage  Convert
+ */
+class Tasks_Convert_Task_VCalendar_DavDroid extends Tasks_Convert_Task_VCalendar_Abstract
+{
+    // DAVdroid/0.7.2
+    const HEADER_MATCH = '/DAVdroid\/(?P<version>.*)/';
+    
+    protected $_supportedFields = array(
+        'seq',
+        'dtstart',
+        #'transp',
+        'class',
+        'description',
+        'geo',
+        'location',
+        'priority',
+        'summary',
+        'url',
+        'alarms',
+        'tags',
+        'status',
+        'due',
+        'percent',
+        'completed',
+        'originator_tz'
+    );
+}
