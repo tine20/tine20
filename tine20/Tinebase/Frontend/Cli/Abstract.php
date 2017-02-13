2c7c11ca752c7611cf99126dc578059ecfc9888a
Cornelius Weiß <c.weiss@metaways.de>
2017-02-13 12:13:58 +0100

    0012802: introduce abstract calendar import class
    * and improve cli import result output
    
    https://forge.tine20.org/view.php?id=12802
    
    Change-Id: I05375ce0b980cce12162f7fd37ff8177a321a937
    Reviewed-on: http://gerrit.tine20.com/customers/4200
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Reviewed-on: http://gerrit.tine20.com/customers/4387
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/Frontend/Cli/Abstract.php
+++ tine20/Tinebase/Frontend/Cli/Abstract.php
@@ -389,19 +389,7 @@ class Tinebase_Frontend_Cli_Abstract
                 continue;
             }
 
-            // TODO use a loop here
-            if (isset($result[$filename]['totalcount']) && ! empty($result[$filename]['totalcount'])) {
-                echo "Imported " . $result[$filename]['totalcount'] . " records.\n";
-            }
-            if (isset($result[$filename]['failcount']) && ! empty($result[$filename]['failcount'])) {
-                echo "Import failed for " . $result[$filename]['failcount'] . " records.\n";
-            }
-            if (isset($result[$filename]['duplicatecount']) && ! empty($result[$filename]['duplicatecount'])) {
-                echo "Found " . $result[$filename]['duplicatecount'] . " duplicates.\n";
-            }
-            if (isset($result[$filename]['updatecount']) && ! empty($result[$filename]['updatecount'])) {
-                echo "Updated " . $result[$filename]['updatecount'] . " records.\n";
-            }
+            $this->_echoImportResult($result[$filename]);
 
             // import (check if dry run)
             if ($_opts->d && $_opts->v) {
@@ -414,6 +402,28 @@ class Tinebase_Frontend_Cli_Abstract
         }
         
         return $result;
+    }
+
+    /**
+     * echos import result
+     *
+     * @param array $result
+     */
+    protected function _echoImportResult($result)
+    {
+        // TODO use a loop here
+        if (isset($result['totalcount']) && ! empty($result['totalcount'])) {
+            echo "Imported " . $result['totalcount'] . " records.\n";
+        }
+        if (isset($result['failcount']) && ! empty($result['failcount'])) {
+            echo "Import failed for " . $result['failcount'] . " records.\n";
+        }
+        if (isset($result['duplicatecount']) && ! empty($result['duplicatecount'])) {
+            echo "Found " . $result['duplicatecount'] . " duplicates.\n";
+        }
+        if (isset($result['updatecount']) && ! empty($result['updatecount'])) {
+            echo "Updated " . $result['updatecount'] . " records.\n";
+        }
     }
 
     /**
