2522dc65644a6487a5a88f9d68399fa457b92eb4
Philipp Schüle <p.schuele@metaways.de>
2017-03-24 11:49:51 +0100

    0012850: '#' in urls should not be replaced
    https://forge.tine20.org/view.php?id=12850
    
    Change-Id: Ic370d2e4148d36f7276daeab3d73dd0aa260d836
    Reviewed-on: http://gerrit.tine20.com/customers/4413
    Tested-by: Jenkins CI (http://ci.tine20.com/)
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Felamimail/HTMLPurifier/AttrTransform/AValidator.php
+++ tine20/Felamimail/HTMLPurifier/AttrTransform/AValidator.php
@@ -8,7 +8,7 @@
  * @subpackage  HTMLPurifier
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Philipp Schüle <p.schuele@metaways.de>
- * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2009-2017 Metaways Infosystems GmbH (http://www.metaways.de)
  */
 
 /**
@@ -23,12 +23,12 @@ class Felamimail_HTMLPurifier_AttrTransform_AValidator extends HTMLPurifier_Attr
      * (non-PHPdoc)
      * @see HTMLPurifier_AttrTransform::transform()
      */
-    function transform($attr, $config, $context)
+    public function transform($attr, $config, $context)
     {
         $attr['target'] = '_blank';
         
         if (isset($attr['href'])) {
-            $href = preg_replace('/#(.*)/', '#felamimail_inline_$1', $attr['href']);
+            $href = preg_replace('/^#([^\/.]+)$/', '#felamimail_inline_$1', $attr['href']);
             
             if ($attr['href'] != $href) {
                 $attr['target'] = '_self';
