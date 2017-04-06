d2b682c2b16ec0aafac87958971dd5949f1867f4
Philipp Schüle <p.schuele@metaways.de>
2017-04-06 14:07:00 +0200

    fix logger config access for query tracing
    Change-Id: I389a010746ed64467f397c734d785b67b471bafa
    Reviewed-on: http://gerrit.tine20.com/customers/4517
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/Backend/Sql/Abstract.php
+++ tine20/Tinebase/Backend/Sql/Abstract.php
@@ -733,8 +733,9 @@ abstract class Tinebase_Backend_Sql_Abstract extends Tinebase_Backend_Abstract i
     protected function _checkTracing(Zend_Db_Select $select)
     {
         /** @noinspection PhpUndefinedFieldInspection */
-        if (Tinebase_Core::isLogLevel(Zend_Log::TRACE) && $config = Tinebase_Core::getConfig()->logger) {
-            if ($config->traceQueryOrigins) {
+        $config = Tinebase_Core::getConfig();
+        if ( Tinebase_Core::isLogLevel(Zend_Log::TRACE) && $config && isset($config->logger)) {
+            if ($config->logger->traceQueryOrigins) {
                 $e = new Exception();
                 Tinebase_Core::getLogger()->trace(__METHOD__ . '::' . __LINE__ . "\n" . 
                     "BACKTRACE: \n" . $e->getTraceAsString() . "\n" . 
