c34014a702951045ac2d6e46925fb17aabd1dd5e
Paul Mehrer <p.mehrer@metaways.de>
2017-05-08 13:06:11 +0200

    0013042: export: add handling for multiday whole day events
    add new option to split up a multiday whole day event into
    multiple one day lasting whole day events
    
    https://forge.tine20.org/view.php?id=13042
    
    Change-Id: I50d99b170beb07a105456e102f2f4b47335d0161
    Reviewed-on: http://gerrit.tine20.com/customers/4634
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Calendar/Export/GenericTrait.php
+++ tine20/Calendar/Export/GenericTrait.php
@@ -19,6 +19,7 @@
  * @property Tinebase_Controller_Record_Abstract    $_controller
  * @property Tinebase_Model_Filter_FilterGroup      $_filter
  * @property boolen                                 $_getRelations
+ * @property Zend_Config_Xml                        $_config
  *
  * TODO rename trait to hint functionality? GenericTrait is not a good name ;)
  */
@@ -33,6 +34,13 @@ trait Calendar_Export_GenericTrait
      * @var Tinebase_DateTime
      */
     protected $_until = NULL;
+
+    /**
+     * contains the original dtstart and dtend of events that are splitted multi day whole day events ($id => array('dtstart' => dt, 'dtend' => dt))
+     *
+     * @var array
+     */
+    protected $_multiDayWholDayEvents = array();
 
     /**
      * the constructor
@@ -61,6 +69,60 @@ trait Calendar_Export_GenericTrait
         // to support rrule & sorting we can't do pagination in calendar exports
         $records = $this->_controller->search($this->_filter, NULL, $this->_getRelations, false, 'export');
         Calendar_Model_Rrule::mergeAndRemoveNonMatchingRecurrences($records, $this->_filter);
+
+        // explode multiday whole day events to multiple, one day whole day events
+        $multiDayConfig = $this->_config->get('multiDay', NULL);
+        if (null !== $multiDayConfig && mb_strtolower($multiDayConfig) === 'daily') {
+            $newEvents = new Tinebase_Record_RecordSet('Calendar_Model_Event');
+            $removeIds = array();
+
+            /** @var Calendar_Model_Event $record */
+            foreach($records as $record) {
+                if ($record->is_all_day_event) {
+
+                    $orgDtStart = clone $record->dtstart;
+                    $orgDtEnd = clone $record->dtend;
+                    $dtstart = clone $record->dtstart;
+                    $dtstart->addDay(1);
+
+                    $isSplitted = false;
+
+                    while ($record->dtend->isLater($dtstart)) {
+                        $isSplitted = true;
+                        $newEvent = clone $record;
+                        $newEvent->setId(Tinebase_Record_Abstract::generateUID());
+                        $newEvent->dtend = clone $newEvent->dtstart;
+                        $newEvent->dtend->setTime(23, 59, 59);
+
+                        if ($newEvent->dtend->isLater($this->_from) && $newEvent->dtstart->isEarlier($this->_until)) {
+                            $newEvents->addRecord($newEvent);
+                            $this->_multiDayWholDayEvents[$newEvent->getId()] = array(
+                                'dtstart' => $orgDtStart,
+                                'dtend'   => $orgDtEnd
+                            );
+                        }
+
+                        $record->dtstart = clone $dtstart;
+                        $dtstart->addDay(1);
+                    }
+
+                    if (!$record->dtend->isLater($this->_from) || !$record->dtstart->isEarlier($this->_until)) {
+                        $removeIds[] = $record->getId();
+                    } elseif(true === $isSplitted) {
+                        $this->_multiDayWholDayEvents[$record->getId()] = array(
+                            'dtstart' => $orgDtStart,
+                            'dtend'   => $orgDtEnd
+                        );
+                    }
+                }
+            }
+            if (count($removeIds) > 0) {
+                foreach($removeIds as $id) {
+                    $records->removeById($id);
+                }
+            }
+            $records->merge($newEvents);
+        }
 
         $this->_sortRecords($records);
 
