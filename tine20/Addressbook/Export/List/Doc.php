7a4fb4aac507586115b6c847b8bb59c044a403b6
Paul Mehrer <p.mehrer@metaways.de>
2017-05-22 15:00:38 +0200

    Export - added definitions and templates
    Change-Id: I8283fbe87b5be768a7bb55de72fe7d57f31938a2
    Reviewed-on: http://gerrit.tine20.com/customers/4722
    Reviewed-by: Paul Mehrer <p.mehrer@metaways.de>
    Tested-by: Paul Mehrer <p.mehrer@metaways.de>

---
+++ tine20/Addressbook/Export/List/Doc.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Addressbook List Doc generation class
+ *
+ * @package     Addressbook
+ * @subpackage  Export
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Paul Mehrer <p.mehrer@metaways.de>
+ * @copyright   Copyright (c) 2017-2017 Metaways Infosystems GmbH (http://www.metaways.de)
+ *
+ */
+
+/**
+ * Addressbook List Doc generation class
+ *
+ * @package     Addressbook
+ * @subpackage  Export
+ *
+ */
+class Addressbook_Export_List_Doc extends Tinebase_Export_Doc
+{
+    protected $_defaultExportname = 'adb_list_doc';
+}
