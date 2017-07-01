b3f1565cceb101325fea692fa4984fa34213be04
Xristoph <christoph@out-world.com>
2017-07-02 00:04:10 +0200

    Support for Gnome Evolution tasks
    In current version of Tine20 it is not possible to add or update task
    using the Gnome Evolution client.
    
    Adds converter for Gnome Evolution client, which was tested on Gnome
    Evolution 3.10.04.
    
    For new tasks by Gnome Evoultution (at least 3.10.04 and 3.18.5.2) uses the following
    patterns to generated uid and
    the url of the .ics file.
       -> uid : <creattiontimestamp><identifier>@<hostname>
       -> .ics: <uid>-<modificationtimestamp>
    
    <creationtimestpamp>: <timestamp>
    <modificationtimestamp>: <timestamp>
    <timestamp>: <YYYY><MM><DD>T<hh><mm><ss>Z
    <YYYY>: four digit year number
    <MM>: two digit month number
    <DD>: two digit month day number
    <hh>: two digit hour
    <mm>: two digit minutes
    <ss>: two digit seconds
    <hostname>: hostname as returned by `hostname` command
    
    The resulting record id is longer than 40 char, and it will be in many
    cases be unique causing multiple instances of the same record be created,
    as urls will differ by <modificationtimestamp>.
    
    In order to get consistent ids the converter checks the uid if it contains
    the host name it strips off the host name and removes any '_' and '-'
    characters from the uid string and explicitly sets the id of the task
    record to this newly created id string. The
    Tasks_Frontend_WebDAV_Task::createFile function checks whether the
    converter has set an id for the task record if it is set than it does not
    use the url of the .ics file to set the id of the task record.
    
    Related-Bug: #0011010

---
+++ tine20/Tasks/Convert/Task/VCalendar/Evolution.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Calendar
+ * @subpackage  Frontend
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Thomas Pawassarat <tomp@topanet.de>
+ * @copyright   Copyright (c) 2011-2013 Metaways Infosystems GmbH (http://www.metaways.de)
+ *
+ */
+
+/**
+ * class to convert single event to/from VCalendar
+ *
+ * @package     Tasks
+ * @subpackage  Convert
+ */
+class Tasks_Convert_Task_VCalendar_Evolution extends Tasks_Convert_Task_VCalendar_Abstract
+{
+
+    // eM Client/5.0.17595.0
+    const HEADER_MATCH = '/Evolution\/(?P<version>.*)/';
+    
+    protected $_supportedFields = array(
+        'seq',#sequence
+        'dtstart',
+        #'transp',
+        'class',
+        'description',
+        #'geo',
+        #'location',
+        'priority',
+        'summary',
+        'url',
+        #'alarms',
+        'tags',
+        'status',
+        'due',
+        'percent', # percent-complete
+        'completed',
+        #'exdate',
+        #'rrule',
+        #'recurid',
+        #'is_all_day_event',
+        #'rrule_until',
+        'originator_tz'
+    );
+    
+    /**
+     * converts vcalendar to Tasks_Model_Task
+     * 
+     * @param  mixed                 $_blob   the vcalendar to parse
+     * @param  Calendar_Model_Event  $_record  update existing event
+     * @param  array                 $options
+     * @return Calendar_Model_Event
+     */
+    public function toTine20Model($_blob, Tinebase_Record_Abstract $_record = null, $options = array())
+    {
+        $task = parent::toTine20Model($_blob,$_record,$options);
+        /**
+         * TODO remove if tine core can better handle ical filename and uid extended by hostname task was created by and timestamp when change
+         *      occured:
+         **
+         * Evolution 3.10.04 creates the names of the event files by replacing any non url characters by _ and appending a timestamp to it
+         * set id of task to UID without name of computer created and time stamp of change, may result in conflicts if changes ond serveral computers
+         * at the same time but for now this would fix things
+         */
+        
+        // bypass filters until end of this funtion
+        $task->bypassFilters = true;
+        if ( isset($task->uid) ) {
+             if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(
+                       __METHOD__ . '::' . __LINE__ . ' Fixing Evolution TaskID: trip timestamp, if present, and hostname from uid ');
+             $evolutionid = preg_replace('/[-_]/','',preg_replace('/@.*$/','',$task->uid ));
+             if ( $evolutionid !== $task->uid ) {
+                 if ( strlen($evolutionid) <= 40 ) {
+                     $task->setId($evolutionid);
+                     if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(
+                         __METHOD__ . '::' . __LINE__ . ' Fixing Evolution TaskID: ' . $task->getId() );
+                 }
+                 else if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(
+                       __METHOD__ . '::' . __LINE__ . ' Fixing Evolution TaskID: failed! id to long (>40 char) after striping any non relevant character (' . $evolutionid . ')' );
+             }
+             else if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(
+                       __METHOD__ . '::' . __LINE__ . ' Fixing Evolution TaskID: not necessary as not created by evolution ' );
+ 
+        }
+        else if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(
+                   __METHOD__ . '::' . __LINE__ . ' Fixing Evolution TaskID: not necessary or possible as uid empty' );
+        
+        // enable filters again
+        $task->bypassFilters = false;
+        
+        $task->isValid(true);
+        return $task;
+    }
+}
+
+
