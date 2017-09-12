d824b5ed4cc37d709472c90f3c186b44f011558d
Paul Mehrer <p.mehrer@metaways.de>
2017-09-12 11:15:47 +0200

    Addressbook - List - resolve created_by / last_modified_by in json FE
    * added assertion in test
    
    Change-Id: I88d4ffdceb71a6594011be93dc6e1c7fc2b0a031
    Reviewed-on: http://gerrit.tine20.com/customers/5637
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Paul Mehrer <p.mehrer@metaways.de>
    Tested-by: Paul Mehrer <p.mehrer@metaways.de>

--- tine20/Addressbook/Model/List.php
+++ tine20/Addressbook/Model/List.php
@@ -120,6 +120,23 @@ class Addressbook_Model_List extends Tinebase_Record_Abstract
     );
 
     /**
+     * if foreign Id fields should be resolved on search and get from json
+     * should have this format:
+     *     array('Calendar_Model_Contact' => 'contact_id', ...)
+     * or for more fields:
+     *     array('Calendar_Model_Contact' => array('contact_id', 'customer_id), ...)
+     * (e.g. resolves contact_id with the corresponding Model)
+     *
+     * @var array
+     */
+    protected static $_resolveForeignIdFields = array(
+        Tinebase_Model_User::class => array(
+            'created_by',
+            'last_modified_by'
+        )
+    );
+
+    /**
      * @return array
      */
     static public function getManageAccountFields()
