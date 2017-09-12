09b6c5a29b3b2f2df50b721df7f87e88819d4a5f
Cornelius Weiß <c.weiss@metaways.de>
2017-09-12 14:10:40 +0200

    add screenshot shortcut
    Change-Id: Ife906125d8d423a069b57f3fcc6bfaab6e42305c
    Reviewed-on: http://gerrit.tine20.com/customers/5641
    Reviewed-by: Cornelius Weiss <c.weiss@metaways.de>
    Tested-by: Cornelius Weiss <c.weiss@metaways.de>

--- tine20/Tinebase/js/tineInit.js
+++ tine20/Tinebase/js/tineInit.js
@@ -117,6 +117,8 @@ Tine.Tinebase.tineInit = {
                 Tine.Tinebase.common.reload({
                     clearCache: true
                 });
+            } else if (e.ctrlKey && e.getKey() === e.S ) {
+                Ext.ux.screenshot.ux(window, {download: true, grabMouse: !e.shiftKey});
             }
         });
         
---
+++ tine20/Tinebase/js/ux/util/screenshot.js
@@ -0,0 +1,84 @@
+Ext.ns('Ext.ux.screenshot');
+
+/**
+ *
+ * @param {Window} win
+ * @param {Object} options
+ *      type: 'png',                // png|jpeg|webp(chrome only)
+ *      dpi: 300,
+ *      download: false,
+ *      filename: 'screenshot',     // without extension
+ *      grabMouse: false,
+ * @return {Promise}
+ */
+Ext.ux.screenshot.get = function(win, options) {
+    var _ = window.lodash;
+
+    // defaults
+    options = _.assign({}, {
+        type: 'png',
+        dpi: 300,
+        download: false,
+        filename: 'screenshot',
+        grabMouse: false,
+    }, options);
+
+    return new Promise(function (fulfill, reject) {
+            require.ensure(["html2canvas"], function() {
+                var html2canvas = require ("html2canvas");
+                var canvas = win.document.createElement("canvas");
+                canvas.width = win.innerWidth;
+                canvas.height = win.innerHeight;
+
+                Ext.ux.screenshot.setDPI(canvas, options.dpi);
+
+                html2canvas(win.document.body, {
+                    canvas: canvas,
+                    grabMouse: options.grabMouse,
+                    onrendered: function (canvas) {
+                        var mimeType = 'image/' + options.type,
+                            dataUrl = canvas.toDataURL(mimeType);
+
+                        if (options.download) {
+                            var downloadLink = document.createElement("a");
+
+                            downloadLink.href = dataUrl.replace("image/png", "image/octet-stream");
+                            downloadLink.download = options.filename + '.' + options.type;
+                            win.document.body.appendChild(downloadLink);
+                            downloadLink.click();
+                            win.document.body.removeChild(downloadLink);
+                        }
+
+                        fulfill(dataUrl);
+                    }
+                });
+            }, 'Tinebase/js/html2canvas');
+    });
+};
+
+Ext.ux.screenshot.ux = function(win, options) {
+    // display countdown
+    var secondsLeft = Ext.isNumber(options.countdown) ? options.countdown : 3;
+    if (secondsLeft) {
+        Ext.getBody().mask(String.format('Taking Screenshot in {0} seconds', secondsLeft));
+        options.countdown = --secondsLeft;
+        return Ext.ux.screenshot.ux.defer(1000, win, [win, options]);
+    }
+
+    Ext.getBody().unmask();
+    Ext.ux.screenshot.get(win, options);
+};
+
+Ext.ux.screenshot.setDPI = function (canvas, dpi) {
+    // Set up CSS size if it's not set up already
+    if (!canvas.style.width)
+        canvas.style.width = canvas.width + 'px';
+    if (!canvas.style.height)
+        canvas.style.height = canvas.height + 'px';
+
+    var scaleFactor = dpi / 96;
+    canvas.width = Math.ceil(canvas.width * scaleFactor);
+    canvas.height = Math.ceil(canvas.height * scaleFactor);
+    var ctx = canvas.getContext('2d');
+    ctx.scale(scaleFactor, scaleFactor);
+};
