tine20/tine20

commit dbc19d11cb5323bf94ec86bc3c7418c807a2c0d1

fix: clear combo and press ctrl+z
Change-Id: I093ee99c3185264a3b447e8b57a49385a1d9b347
Reviewed-on: http://gerrit.tine20.com/customers/5627
Reviewed-by: sstamer <s.stamer@metaways.de>
Tested-by: sstamer <s.stamer@metaways.de>

tine20/Tinebase/js/widgets/form/RecordPickerComboBox.js
@@ -336,6 +336,16 @@ Tine.Tinebase.widgets.form.RecordPickerComboBox = Ext.extend(Ext.ux.form.Clearab
         
         this.value = value;
         return this;
+    },
+
+    validateValue : function(value){
+        // happens when removing contents and pressing ctrl+z
+        if (Ext.isString(value) && ! this.selectedRecord) {
+            value = null;
+            this.setRawValue('');
+        }
+
+        return this.supr().validateValue(value);
     }
 });
 Ext.reg('tinerecordpickercombobox', Tine.Tinebase.widgets.form.RecordPickerComboBox);

tine20/Timetracker/js/TimesheetEditDialog.js
@@ -257,7 +257,11 @@ Tine.Timetracker.TimesheetEditDialog = Ext.extend(Tine.widgets.dialog.EditDialog
                         allowBlank: false,
                         forceSelection: true,
                         name: 'timeaccount_id',
-                        lazyInit: false
+                        lazyInit: false,
+                        listeners: {
+                            scope: this,
+                            select: this.onTimeaccountUpdate
+                        }
                     })], [{
                         fieldLabel: this.app.i18n._('Duration'),
                         name: 'duration',

commit ff902dba8bbc67e45a3246da4c6b696007e8980d

fix typeahead combos #2
this still breaks FMail compose dlg recipient select

this has been broken by dbc19d11c + daf709f46c

Change-Id: I0c9f4945b07253a6ad5e2bae2ecf668d794ee9a1
Reviewed-on: http://gerrit.tine20.com/customers/5691
Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
Tested-by: Philipp Schüle <p.schuele@metaways.de>

tine20/Tinebase/js/widgets/form/RecordPickerComboBox.js
@@ -339,17 +339,18 @@ Tine.Tinebase.widgets.form.RecordPickerComboBox = Ext.extend(Ext.ux.form.Clearab
     },
 
     // otherwise we can't distinguish between typeAhead and ctrl+z
-    filterValidation : Ext.emptyFn,
+    filterValidation : Ext.emptyFn
 
-    validateValue : function(value){
-        // happens when removing contents and pressing ctrl+z
-        if (Ext.isString(value) && ! this.selectedRecord) {
-            value = null;
-            this.setRawValue('');
-        }
-
-        return this.supr().validateValue(value);
-    }
+    // FIXME: this still breaks FMail compose dlg recipient select
+    // validateValue : function(value){
+    //     // happens when removing contents and pressing ctrl+z
+    //     if (Ext.isString(value) && ! this.selectedRecord) {
+    //         value = null;
+    //         this.setRawValue('');
+    //     }
+    //
+    //     return this.supr().validateValue(value);
+    // }
 
 });
 Ext.reg('tinerecordpickercombobox', Tine.Tinebase.widgets.form.RecordPickerComboBox);