38d11c9dcbed6db5358dde366d8d1de9dda983f7
Philipp Schüle <p.schuele@metaways.de>
2017-09-18 18:08:37 +0200

    customfield filter now extends foreign record filter
    * because frontend also uses foreign record logic for
     customfields
    
    * this should be refactored as there is lots of code duplication
     and messy code
    
    Change-Id: I514869b43ac470c189be41a2584eabd1c2160278
    Reviewed-on: http://gerrit.tine20.com/customers/5725
    Tested-by: Paul Mehrer <p.mehrer@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/Model/Filter/Relation.php
+++ tine20/Tinebase/Model/Filter/Relation.php
@@ -5,7 +5,7 @@
  * @package     Tinebase
  * @subpackage  Filter
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
- * @copyright   Copyright (c) 2009 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2009-2017 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Cornelius Weiss <c.weiss@metaways.de>
  */
 
