b2b76f744be955cc079c3e5d9b485175d0e926d6
Philipp Schüle <p.schuele@metaways.de>
2017-09-21 10:49:31 +0200

    0013462: Filemanager needed to open contacts
    https://forge.tine20.org/view.php?id=13462
    
    Change-Id: Ic0f5099b141a2f0763ab7e814941642ed7eee7a0
    Reviewed-on: http://gerrit.tine20.com/customers/5761
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/js/widgets/dialog/AttachmentsGridPanel.js
+++ tine20/Tinebase/js/widgets/dialog/AttachmentsGridPanel.js
@@ -141,7 +141,10 @@ Tine.widgets.dialog.AttachmentsGridPanel = Ext.extend(Tine.widgets.grid.FileUplo
             disabled:true
         });
 
-        this.action_preview = Tine.Filemanager.nodeActionsMgr.get('preview', {initialApp: this.app});
+        // TODO: does user need rights for Filemanager?
+        if (Tine.Tinebase.appMgr.isEnabled('Filemanager')) {
+            this.action_preview = Tine.Filemanager.nodeActionsMgr.get('preview', {initialApp: this.app});
+        }
 
         this.actionUpdater.addActions([this.action_download, this.action_preview]);
         this.getTopToolbar().addItem(this.action_download);
@@ -153,7 +156,8 @@ Tine.widgets.dialog.AttachmentsGridPanel = Ext.extend(Tine.widgets.grid.FileUplo
     onRowDbClick: function () {
         var _ = window.lodash;
 
-        if (!Tine.Tinebase.appMgr.isEnabled('Filemanager')) {
+        // TODO: does user need rights for Filemanager?
+        if (! Tine.Tinebase.appMgr.isEnabled('Filemanager')) {
             this.onDownload();
             return;
         }
