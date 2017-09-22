e3807c02e794cec5284edde93c5e72893983be31
Paul Mehrer <p.mehrer@metaways.de>
2017-09-22 08:13:02 +0200

    Filemanager may not be installed, it's true, added check
    Change-Id: Ic04b71233ef230cc1c34d85a0fc3fb2e91c13eeb
    Reviewed-on: http://gerrit.tine20.com/customers/5783
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Paul Mehrer <p.mehrer@metaways.de>

--- tine20/Tinebase/Convert/FullUser/Json.php
+++ tine20/Tinebase/Convert/FullUser/Json.php
@@ -43,19 +43,21 @@ class Tinebase_Convert_FullUser_Json extends Tinebase_Convert_Json
      */
     protected function _resolveAfterToArray($result, $modelConfiguration, $multiple = false)
     {
-        $fileSystem = Tinebase_FileSystem::getInstance();
-        $personalPath = $fileSystem->getApplicationBasePath(
-            'Filemanager',
-            Tinebase_FileSystem::FOLDER_TYPE_PERSONAL
-        ) . '/';
-        foreach (false === $multiple ? array(&$result) : $result as &$val) {
-            $val['effectiveAndLocalQuota'] = null;
-            try {
-                $val['effectiveAndLocalQuota'] = $fileSystem->getEffectiveAndLocalQuota(
-                    $fileSystem->stat($personalPath . $val['accountId']));
-            } catch (Tinebase_Exception_NotFound $tenf) {
-                if (Tinebase_Core::isLogLevel(Zend_Log::INFO)) Tinebase_Core::getLogger()->info(__METHOD__ . '::' . __LINE__
-                    . ' did not find personal folder for account ' . $val['accountId']);
+        if (Tinebase_Application::getInstance()->isInstalled('Filemanager')) {
+            $fileSystem = Tinebase_FileSystem::getInstance();
+            $personalPath = $fileSystem->getApplicationBasePath(
+                'Filemanager',
+                Tinebase_FileSystem::FOLDER_TYPE_PERSONAL
+            ) . '/';
+            foreach (false === $multiple ? array(&$result) : $result as &$val) {
+                $val['effectiveAndLocalQuota'] = null;
+                try {
+                    $val['effectiveAndLocalQuota'] = $fileSystem->getEffectiveAndLocalQuota(
+                        $fileSystem->stat($personalPath . $val['accountId']));
+                } catch (Tinebase_Exception_NotFound $tenf) {
+                    if (Tinebase_Core::isLogLevel(Zend_Log::INFO)) Tinebase_Core::getLogger()->info(__METHOD__ . '::' . __LINE__
+                        . ' did not find personal folder for account ' . $val['accountId']);
+                }
             }
         }
 
