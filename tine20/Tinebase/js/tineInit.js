816241dfb211373e8b425ac772b7d9f2b7f28ae4
Michael Spahn <m.spahn@metaways.de>
2017-10-27 13:12:09 +0200

    fix system links opened from dialogs
    Change-Id: Ie4abfc6b411da4b3b65a4ad7a787fb936148671f
    Reviewed-on: http://gerrit.tine20.com/customers/6147
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Michael Spahn <m.spahn@metaways.de>

--- tine20/Tinebase/js/tineInit.js
+++ tine20/Tinebase/js/tineInit.js
@@ -145,17 +145,31 @@ Tine.Tinebase.tineInit = {
             Tine.Tinebase.MainContextMenu.showIf(e);
         }, this);
 
-        // open internal links in same window (use router)
         Ext.getBody().on('click', function(e) {
-            var target = e.getTarget('a',1 ,true);
-            if (target && target.getAttribute('target') == '_blank') {
-                var href = String(target.getAttribute('href'));
+            var target = e.getTarget('a', 1, true);
+
+            // open internal links in same window (use router)
+            if(window.isMainWindow === true) {
+                if (target && target.getAttribute('target') === '_blank') {
+                    var href = String(target.getAttribute('href'));
+
                     if (href.match(new RegExp('^' + window.lodash.escapeRegExp(Tine.Tinebase.common.getUrl())))) {
                         target.set({
                             href: decodeURI(href),
                             target: "_self"
                         });
                     }
+                }
+            } else {
+                e.preventDefault();
+
+                if (e.ctrlKey || e.metaKey) {
+                    var window = window.open(target.getAttribute('href'), '_blank', null, true);
+                    window.opener = undefined;
+                } else {
+                    window.opener.location.href = target.getAttribute('href');
+                    window.close();
+                }
             }
         }, this);
     },
@@ -283,7 +297,7 @@ Tine.Tinebase.tineInit = {
 
         Tine.Tinebase.router = new director.Router().init();
         Tine.Tinebase.router.configure({notfound: function () {
-            var defaultApp = Tine.Tinebase.appMgr.getDefault()
+            var defaultApp = Tine.Tinebase.appMgr.getDefault();
             Tine.Tinebase.router.setRoute('/' + defaultApp.appName);
         }});
 
