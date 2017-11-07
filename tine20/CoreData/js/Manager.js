4729a126c7f4683dd7705743135beef5b83afaa4
Philipp Schüle <p.schuele@metaways.de>
2017-11-07 17:07:20 +0100

    fixes scope of "constr" var in CoreData manager
    Change-Id: I83f46bc496e648423b71bbe585adfe271f98a095
    Reviewed-on: http://gerrit.tine20.com/customers/6267
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/CoreData/js/Manager.js
+++ tine20/CoreData/js/Manager.js
@@ -29,6 +29,7 @@ Tine.CoreData.Manager = function() {
          * @returns Ext.form.Field|null
          */
         getGrid: function (id, options) {
+            var constr = null;
             if (grids.hasOwnProperty(id)) {
                 constr = grids[id].constr;
                 if (! options) {
@@ -49,6 +50,7 @@ Tine.CoreData.Manager = function() {
          * @returns Ext.form.Field|null
          */
         getEditDialog: function (id, options) {
+            var constr = null;
             if (editDialogs.hasOwnProperty(id)) {
                 constr = editDialogs[type];
             }
@@ -65,8 +67,9 @@ Tine.CoreData.Manager = function() {
          * TODO is this needed?
          */
         getToolbar: function (id, options) {
+            var constr = null;
             if (toolbars.hasOwnProperty(id)) {
-                var constr = toolbars[type];
+                constr = toolbars[type];
                 if (! options) {
                     options = toolbars[id].options;
                 }
