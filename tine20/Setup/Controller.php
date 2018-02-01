6bc2421352a054433bbb7f68daf61ff522399fb5
Philipp Schüle <p.schuele@metaways.de>
2018-02-01 09:08:16 +0100

    clear cache after update
    Change-Id: I6c113920cd883e0a1210b29ee1bee6efdf26124c
    Reviewed-on: http://gerrit.tine20.com/customers/7879
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Setup/Controller.php
+++ tine20/Setup/Controller.php
@@ -417,6 +417,7 @@ class Setup_Controller
      *
      * @param Tinebase_Record_RecordSet $_applications
      * @return  array   messages
+     * @throws Tinebase_Exception
      */
     public function updateApplications(Tinebase_Record_RecordSet $_applications = null)
     {
@@ -469,6 +470,8 @@ class Setup_Controller
                 }
             }
         }
+
+        $this->_clearCache();
         
         return array(
             'messages' => $messages,
@@ -612,10 +615,8 @@ class Setup_Controller
                 break;
         }
         
-        Setup_Core::getLogger()->info(__METHOD__ . '::' . __LINE__ . ' Clearing cache after update ...');
-        $this->_enableCaching();
-        Tinebase_Core::getCache()->clean(Zend_Cache::CLEANING_MODE_ALL);
-        
+        $this->_clearCache();
+
         return $messages;
     }
 
