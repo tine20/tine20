b4aa81cd75b8eca5f51025c4d75e194c431ae517
Philipp Schüle <p.schuele@metaways.de>
2018-05-03 10:58:48 +0200

    adds a test for Record.set('field', 'null')
    Change-Id: Ic64b6b73b2b6a8dc4ce3c3893cec40c9fad51129
    Reviewed-on: http://gerrit.tine20.com/customers/9115
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

---
+++ tests/js/unit/Tinebase/data/Record.spec.js
@@ -0,0 +1,42 @@
+/*
+ * Tine 2.0
+ *
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Philipp Schüle <p.schuele@metaways.de>
+ * @copyright   Copyright (c) 2018 Metaways Infosystems GmbH (http://www.metaways.de)
+ */
+
+import * as global from 'globalFakes'
+
+require('data/Record')
+
+describe('Record', () => {
+  global.log()
+
+  // needs to be uppercase because we want to create a new record with "new Uit()"
+  let Uit
+
+  beforeEach(() => {
+    Uit = Tine.Tinebase.data.Record.create([
+      {name: 'id'},
+      {name: 'name'}
+    ], {
+      appName: 'Tinebase',
+      modelName: 'TestModel',
+      idProperty: 'id',
+      titleProperty: 'name',
+      recordName: 'TestModel',
+      recordsName: 'TestModels'
+    })
+  })
+
+  it('sets null values correctly', () => {
+    var record = new Uit({
+      'name': null
+    })
+    record.set('name', 'null')
+    expect(record.data.name).to.equal('null')
+    record.set('name', null)
+    expect(record.data.name).to.equal(null)
+  })
+})
