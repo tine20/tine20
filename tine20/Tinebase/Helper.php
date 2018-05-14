56cb9ddc917e6c9008caae3d017e796817aba789
Cornelius Weiß <c.weiss@metaways.de>
2018-05-14 18:12:39 +0200

    beautify anon download page
    Change-Id: I94604210fafc6cdd0f9fa9dc1b99e4757df34f4a
    Reviewed-on: http://gerrit.tine20.com/customers/9189
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Cornelius Weiss <c.weiss@metaways.de>

--- tine20/Tinebase/Helper.php
+++ tine20/Tinebase/Helper.php
@@ -87,7 +87,43 @@ class Tinebase_Helper
         
         return $result;
     }
-    
+
+    /**
+     * converts value to human readable unit
+     *
+     * @param integer $bytes        value to format
+     * @param string  $force_unit   force given unit
+     * @param string  $format       formatstring for sprintf
+     * @param bool    $si           use SI unit
+     * @return string
+     */
+    public static function formatBytes($bytes, $force_unit = NULL, $format = NULL, $si = TRUE)
+    {
+        // Format string
+        $format = ($format === NULL) ? '%01.2f %s' : (string) $format;
+
+        // IEC prefixes (binary)
+        if ($si == FALSE OR strpos($force_unit, 'i') !== FALSE)
+        {
+            $units = array('B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB');
+            $mod   = 1024;
+        }
+        // SI prefixes (decimal)
+        else
+        {
+            $units = array('B', 'kB', 'MB', 'GB', 'TB', 'PB');
+            $mod   = 1000;
+        }
+
+        // Determine unit to use
+        if (($power = array_search((string) $force_unit, $units)) === FALSE)
+        {
+            $power = ($bytes > 0) ? floor(log($bytes, $mod)) : 0;
+        }
+
+        return sprintf($format, $bytes / pow($mod, $power), $units[$power]);
+    }
+
     /**
      * get svn revision info
      *
