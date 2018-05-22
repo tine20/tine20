41182228b24b9e33e76ed379276521fa2c8c8ace
Philipp Schüle <p.schuele@metaways.de>
2018-05-22 11:00:37 +0200

    0013824: Unable to add new contact
    * only add event tab panel if Calendar app is available
    * "null" panel breaks extjs ...
    
    https://forge.tine20.org/view.php?id=13824
    
    Change-Id: I3200ae39eae3904add238efc88e0bb03b4bcb115
    Reviewed-on: http://gerrit.tine20.com/customers/9253
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Addressbook/js/ContactEditDialog.js
+++ tine20/Addressbook/js/ContactEditDialog.js
@@ -96,92 +96,81 @@ Tine.Addressbook.ContactEditDialog = Ext.extend(Tine.widgets.dialog.EditDialog, 
             this.contactEventPanel = null;
         }
 
-        return {
-            xtype: 'tabpanel',
+        var tabs = [{
+            title: this.app.i18n.n_('Contact', 'Contacts', 1),
             border: false,
-            plain: true,
-            activeTab: 0,
-            plugins: [{
-                ptype : 'ux.tabpanelkeyplugin'
-            }],
-            defaults: {
-                hideMode: 'offsets'
-            },
+            frame: true,
+            layout: 'border',
             items: [{
-                title: this.app.i18n.n_('Contact', 'Contacts', 1),
-                border: false,
-                frame: true,
+                region: 'center',
                 layout: 'border',
                 items: [{
-                    region: 'center',
-                    layout: 'border',
+                    xtype: 'fieldset',
+                    region: 'north',
+                    autoHeight: true,
+                    title: this.app.i18n._('Personal Information'),
                     items: [{
-                        xtype: 'fieldset',
-                        region: 'north',
-                        autoHeight: true,
-                        title: this.app.i18n._('Personal Information'),
+                        xtype: 'panel',
+                        layout: 'hbox',
+                        align: 'stretch',
+                        plugins: [{
+                            ptype: 'ux.itemregistry',
+                            key:   'Tine.Addressbook.editDialog.northPanel'
+                        }],
                         items: [{
-                            xtype: 'panel',
-                            layout: 'hbox',
-                            align: 'stretch',
-                            plugins: [{
-                                ptype: 'ux.itemregistry',
-                                key:   'Tine.Addressbook.editDialog.northPanel'
-                            }],
-                            items: [{
-                                flex: 1,
-                                xtype: 'columnform',
-                                autoHeight: true,
-                                style:'padding-right: 5px;',
-                                items: [[
-                                    new Tine.Tinebase.widgets.keyfield.ComboBox({
-                                        fieldLabel: this.app.i18n._('Salutation'),
-                                        name: 'salutation',
-                                        app: 'Addressbook',
-                                        keyFieldName: 'contactSalutation',
-                                        value: '',
-                                        columnWidth: 0.35,
-                                        listeners: {
-                                            scope: this,
-                                            'select': function (combo, record, index) {
-                                                var jpegphoto = this.getForm().findField('jpegphoto');
-                                                // set new empty photo depending on chosen salutation only if user doesn't have own image
-                                                jpegphoto.setDefaultImage(record.json.image || 'images/empty_photo_blank.png');
-                                            }
+                            flex: 1,
+                            xtype: 'columnform',
+                            autoHeight: true,
+                            style:'padding-right: 5px;',
+                            items: [[
+                                new Tine.Tinebase.widgets.keyfield.ComboBox({
+                                    fieldLabel: this.app.i18n._('Salutation'),
+                                    name: 'salutation',
+                                    app: 'Addressbook',
+                                    keyFieldName: 'contactSalutation',
+                                    value: '',
+                                    columnWidth: 0.35,
+                                    listeners: {
+                                        scope: this,
+                                        'select': function (combo, record, index) {
+                                            var jpegphoto = this.getForm().findField('jpegphoto');
+                                            // set new empty photo depending on chosen salutation only if user doesn't have own image
+                                            jpegphoto.setDefaultImage(record.json.image || 'images/empty_photo_blank.png');
                                         }
+                                    }
                                 }), {
                                     columnWidth: 0.65,
                                     fieldLabel: this.app.i18n._('Title'),
                                     name: 'n_prefix',
                                     maxLength: 64
                                 }], [{
-                                    columnWidth: 0.35,
-                                    fieldLabel: this.app.i18n._('First Name'),
-                                    name: 'n_given',
-                                    maxLength: 64
-                                }, {
-                                    columnWidth: 0.30,
-                                    fieldLabel: this.app.i18n._('Middle Name'),
-                                    name: 'n_middle',
-                                    maxLength: 64
-                                }, {
-                                    columnWidth: 0.35,
-                                    fieldLabel: this.app.i18n._('Last Name'),
-                                    name: 'n_family',
-                                    maxLength: 255
-                                }], [{
-                                    columnWidth: 0.65,
-                                    xtype: 'mirrortextfield',
-                                    fieldLabel: this.app.i18n._('Company'),
-                                    name: 'org_name',
-                                    maxLength: 255
-                                }, {
-                                    columnWidth: 0.35,
-                                    fieldLabel: this.app.i18n._('Unit'),
-                                    name: 'org_unit',
-                                    maxLength: 64
-                                }
-                                ]/* move to seperate tab, [{
+                                columnWidth: 0.35,
+                                fieldLabel: this.app.i18n._('First Name'),
+                                name: 'n_given',
+                                maxLength: 64
+                            }, {
+                                columnWidth: 0.30,
+                                fieldLabel: this.app.i18n._('Middle Name'),
+                                name: 'n_middle',
+                                maxLength: 64
+                            }, {
+                                columnWidth: 0.35,
+                                fieldLabel: this.app.i18n._('Last Name'),
+                                name: 'n_family',
+                                maxLength: 255
+                            }], [{
+                                columnWidth: 0.65,
+                                xtype: 'mirrortextfield',
+                                fieldLabel: this.app.i18n._('Company'),
+                                name: 'org_name',
+                                maxLength: 255
+                            }, {
+                                columnWidth: 0.35,
+                                fieldLabel: this.app.i18n._('Unit'),
+                                name: 'org_unit',
+                                maxLength: 64
+                            }
+                            ]/* move to seperate tab, [{
                                     columnWidth: .4,
                                     fieldLabel: this.app.i18n._('Suffix'),
                                     name:'n_suffix'
@@ -201,10 +190,10 @@ Tine.Addressbook.ContactEditDialog = Ext.extend(Tine.widgets.dialog.EditDialog, 
                                 height: 120
                             })
                         ]
-                        }, {
-                            xtype: 'columnform',
-                            items: [[
-                                !Tine.Tinebase.appMgr.get('Addressbook').featureEnabled('featureIndustry') ?
+                    }, {
+                        xtype: 'columnform',
+                        items: [[
+                            !Tine.Tinebase.appMgr.get('Addressbook').featureEnabled('featureIndustry') ?
                                 {
                                     columnWidth: 0.64,
                                     xtype: 'combo',
@@ -214,9 +203,9 @@ Tine.Addressbook.ContactEditDialog = Ext.extend(Tine.widgets.dialog.EditDialog, 
                                 } :
                                 (
                                     new Tine.Addressbook.IndustrySearchCombo({
-                                    fieldLabel: this.app.i18n._('Industry'),
-                                    columnWidth: 0.64,
-                                    name: 'industry'
+                                        fieldLabel: this.app.i18n._('Industry'),
+                                        columnWidth: 0.64,
+                                        name: 'industry'
                                     })
                                 ), {
                                      columnWidth: 0.36,
@@ -444,13 +433,29 @@ Tine.Addressbook.ContactEditDialog = Ext.extend(Tine.widgets.dialog.EditDialog, 
                     ]
                 }]
             }, this.mapPanel,
-                new Tine.widgets.activities.ActivitiesTabPanel({
-                    app: this.appName,
-                    record_id: (this.record && ! this.copyRecord) ? this.record.id : '',
-                    record_model: this.appName + '_Model_' + this.recordClass.getMeta('modelName')
-                }),
-                this.contactEventPanel
-            ]
+            new Tine.widgets.activities.ActivitiesTabPanel({
+                app: this.appName,
+                record_id: (this.record && ! this.copyRecord) ? this.record.id : '',
+                record_model: this.appName + '_Model_' + this.recordClass.getMeta('modelName')
+            }),
+        ];
+
+        if (this.contactEventPanel) {
+            tabs.push(this.contactEventPanel);
+        }
+
+        return {
+            xtype: 'tabpanel',
+            border: false,
+            plain: true,
+            activeTab: 0,
+            plugins: [{
+                ptype : 'ux.tabpanelkeyplugin'
+            }],
+            defaults: {
+                hideMode: 'offsets'
+            },
+            items: tabs
         };
     },
 
@@ -458,14 +463,12 @@ Tine.Addressbook.ContactEditDialog = Ext.extend(Tine.widgets.dialog.EditDialog, 
      * init component
      */
     initComponent: function () {
-        var relatedRecords = {};
-
         this.initToolbar();
         this.groupsPanel = new Tine.Addressbook.contactListsGridPanel({
             initialLoadAfterRender: false,
             frame: false
         });
-        this.supr().initComponent.apply(this, arguments);
+        Tine.Addressbook.ContactEditDialog.superclass.initComponent.apply(this, arguments);
     },
 
     /**
