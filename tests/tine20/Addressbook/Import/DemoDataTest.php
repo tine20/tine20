3e0bfe5d1430921ae1045607df8a9f3d7213f787
Philipp Schüle <p.schuele@metaways.de>
2018-07-19 10:18:01 +0200

    feature(Addressbook): add contact demo data csv
    * Add Testcase for import csv data
    
    Change-Id: I29688b91bb5f847e3371ecffa16bc0d3fc74b7d5
    Reviewed-on: http://gerrit.tine20.com/customers/9891
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

---
+++ tests/tine20/Addressbook/Import/DemoDataTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Tine 2.0 - http://www.tine20.org
+ *
+ * @package     Inventory
+ * @license     http://www.gnu.org/licenses/agpl.html
+ * @copyright   Copyright (c) 2018 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Christian Feitl <c.feitl@metaways.de>
+ */
+
+/**
+ * Test class for Inventory
+ */
+class Addressbook_Import_DemoDataTest extends TestCase
+{
+    /**
+     * @var Tinebase_Model_Container
+     */
+    protected $_importContainer = null;
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testImportDemoData()
+    {
+        $this->_importContainer = $this->_getTestContainer('Addressbook', 'Addressbook_Model_Contact');
+        $importer = new Tinebase_Setup_DemoData_Import('Addressbook_Model_Contact', [
+            'container_id' => $this->_importContainer->getId(),
+            'definition' => 'adb_tine_import_csv',
+        ]);
+        $importer->importDemodata();
+
+        $filter = Tinebase_Model_Filter_FilterGroup::getFilterForModel('Addressbook_Model_Contact', [
+            ['field' => 'container_id', 'operator' => 'equals', 'value' => $this->_importContainer->getId()]
+        ]);
+        $result = Addressbook_Controller_Contact::getInstance()->search($filter);
+        self::assertEquals(7, count($result));
+    }
+
+
+    public function testImportDemoDataViaCli()
+    {
+        $this->_cli = new Addressbook_Frontend_Cli();
+        $out = $this->_appCliHelper('Addressbook', 'createDemoData', []);
+        echo $out;
+
+        $filter = Tinebase_Model_Filter_FilterGroup::getFilterForModel('Addressbook_Model_Contact', [
+            ['field' => 'creation_time', 'operator' => 'within', 'value' => 'dayThis']
+        ]);
+        $result = Addressbook_Controller_Contact::getInstance()->search($filter);
+        self::assertGreaterThanOrEqual(18, count($result));
+    }
+}
