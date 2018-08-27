25d7174abd84e62d4a99ec3f52977612b99852e2
Paul Mehrer <p.mehrer@metaways.de>
2018-08-27 03:46:23 +0200

    fix(Tinebase test) - weekThis test issue fixed
    Change-Id: Ief4aed9f1b4040feabc15179e09a28b1c45244dd
    Reviewed-on: http://gerrit.tine20.com/customers/10219
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Paul Mehrer <p.mehrer@metaways.de>

--- tests/tine20/Tinebase/CustomFieldTest.php
+++ tests/tine20/Tinebase/CustomFieldTest.php
@@ -414,6 +414,7 @@ class Tinebase_CustomFieldTest extends TestCase
     public function testSearchByDate()
     {
         $date = new Tinebase_DateTime();
+        $date->setTimezone(Tinebase_Core::getUserTimezone());
         $cf = self::getCustomField(array('application_id' => Tinebase_Application::getInstance()->getApplicationByName('Addressbook')->getId(), 'model' => 'Addressbook_Model_Contact', 'definition' => array('type' => 'date')));
         $this->_instance->addCustomField($cf);
         
