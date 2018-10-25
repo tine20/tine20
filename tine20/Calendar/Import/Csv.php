1cda38171a60421d04e37af78788b21681a52c3f
Christian Feitl <c.feitl@metaways.de>
2018-10-25 12:30:02 +0200

    fix(Calendar): if(!empty) by Contact etc...
    * forgot if(!empty) by Contact etc in the csv import
    
    Change-Id: I13c77ba953ae5f47295ebabfc535d9f8633cc15f
    Reviewed-on: http://gerrit.tine20.com/customers/10727
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Calendar/Import/Csv.php
+++ tine20/Calendar/Import/Csv.php
@@ -22,13 +22,11 @@ class Calendar_Import_Csv extends Tinebase_Import_Csv_Generic
     protected $_members;
 
     protected $_resources;
-    
+
     protected $_groups;
     
-    protected $_test;
-
     protected $_secretaryGrants = array(Tinebase_Model_Grants::GRANT_READ, Tinebase_Model_Grants::GRANT_ADD);
-    
+
 
     /**
      * additional config options
@@ -82,13 +80,13 @@ class Calendar_Import_Csv extends Tinebase_Import_Csv_Generic
         $time = explode(':', $result['time']);
         $date->addDay($result['date']);
         $date->setTime(
-            (integer) $time['0'],
-            isset($time['1']) ? (integer) $time['1'] : 0,
-            isset($time['2']) ? (integer) $time['2'] : 0
+            (integer)$time['0'],
+            isset($time['1']) ? (integer)$time['1'] : 0,
+            isset($time['2']) ? (integer)$time['2'] : 0
         );
 
         $result['dtstart'] = $date->toString();
-        $duration = isset($result['duration']) && ! empty($result['duration']) ? (integer) $result['duration'] : 1;
+        $duration = isset($result['duration']) && !empty($result['duration']) ? (integer)$result['duration'] : 1;
         $date->addHour($duration);
         $result['dtend'] = $date->toString();
 
@@ -108,73 +106,76 @@ class Calendar_Import_Csv extends Tinebase_Import_Csv_Generic
         $this->_createMemberAttender($importedRecord);
         $this->_createGroupAttender($importedRecord);
     }
-    
+
 
     protected function _createResourceAttender($importedRecord)
     {
+        if (empty($this->_resources)) {
+            return;
+        }
+
         $resource_Controller = Calendar_Controller_Resource::getInstance();
 
         $cal_Backend_Sql = new Calendar_Backend_Sql();
 
         $name_check = [];
         foreach (explode(';', $this->_resources) as $resource) {
-            if (!empty($resource)) {
 
-                //$resource_data_all = $resource_Controller->getAll();
-                foreach ($resource_Controller->getAll() as $resource_data) {
 
-                    if ($resource == $resource_data['name']) {
-                        $resource_id = $resource_data['id'];
-                        $display_id = $resource_data['container_id'];
-                    }
-                    $name_check[] = $resource_data['name'];
+            //$resource_data_all = $resource_Controller->getAll();
+            foreach ($resource_Controller->getAll() as $resource_data) {
+
+                if ($resource == $resource_data['name']) {
+                    $resource_id = $resource_data['id'];
+                    $display_id = $resource_data['container_id'];
                 }
-                if (!in_array($resource, $name_check)) {
-                    $resource_Model = new Calendar_Model_Resource(array(
-                        'name' => $resource,
-                        'description' => 'Import Csv resource',
-                        'email' => 'csv@resource.com',
-                        'is_location' => TRUE,
-                        'grants' => [
-                            array_merge([
-                                'account_id' => Tinebase_Core::getUser()->getId(),
-                                'account_type' => Tinebase_Acl_Rights::ACCOUNT_TYPE_USER,
-                            ], [
-                                Calendar_Model_ResourceGrants::RESOURCE_ADMIN => true,
-                                Calendar_Model_ResourceGrants::EVENTS_READ => true,
-                                Calendar_Model_ResourceGrants::EVENTS_SYNC => true,
-                                Calendar_Model_ResourceGrants::EVENTS_FREEBUSY => true,
-                                Calendar_Model_ResourceGrants::EVENTS_EDIT => true,
-                            ]),
-                        ]
-                    ));
-                    $cal_Controller = Calendar_Controller_Resource::getInstance()->create($resource_Model);
-                    $resource_id = $cal_Controller->getId();
-                    $display_id = $cal_Controller->container_id;
-                }
-
-                $attender = new Calendar_Model_Attender();
-
-                $attender->user_id = $resource_id;
-                $attender->displaycontainer_id = $display_id;
-                $attender->user_type = Calendar_Model_Attender::USERTYPE_RESOURCE;
-                $attender->cal_event_id = $importedRecord['id'];
-                $attender->status_authkey = Tinebase_Record_Abstract::generateUID();
-                
-
-                Tinebase_Timemachine_ModificationLog::getInstance()->setRecordMetaData($attender, 'create');
-
-                $cal_Backend_Sql->createAttendee($attender);
-
+                $name_check[] = $resource_data['name'];
             }
+            if (!in_array($resource, $name_check)) {
+                $resource_Model = new Calendar_Model_Resource(array(
+                    'name' => $resource,
+                    'description' => 'Import Csv resource',
+                    'email' => 'csv@resource.com',
+                    'is_location' => TRUE,
+                    'grants' => [
+                        array_merge([
+                            'account_id' => Tinebase_Core::getUser()->getId(),
+                            'account_type' => Tinebase_Acl_Rights::ACCOUNT_TYPE_USER,
+                        ], [
+                            Calendar_Model_ResourceGrants::RESOURCE_ADMIN => true,
+                            Calendar_Model_ResourceGrants::EVENTS_READ => true,
+                            Calendar_Model_ResourceGrants::EVENTS_SYNC => true,
+                            Calendar_Model_ResourceGrants::EVENTS_FREEBUSY => true,
+                            Calendar_Model_ResourceGrants::EVENTS_EDIT => true,
+                        ]),
+                    ]
+                ));
+                $cal_Controller = Calendar_Controller_Resource::getInstance()->create($resource_Model);
+                $resource_id = $cal_Controller->getId();
+                $display_id = $cal_Controller->container_id;
+            }
+
+            $attender = new Calendar_Model_Attender();
+
+            $attender->user_id = $resource_id;
+            $attender->displaycontainer_id = $display_id;
+            $attender->user_type = Calendar_Model_Attender::USERTYPE_RESOURCE;
+            $attender->cal_event_id = $importedRecord['id'];
+            $attender->status_authkey = Tinebase_Record_Abstract::generateUID();
+
+
+            Tinebase_Timemachine_ModificationLog::getInstance()->setRecordMetaData($attender, 'create');
+
+            $cal_Backend_Sql->createAttendee($attender);
+
         }
     }
 
     protected $_calendars = array();
-    
+
     protected function _createMemberAttender($importedRecord)
     {
-        if (strpos($this->_members, ';') === false) {
+        if (empty($this->_members)) {
             return;
         }
 
@@ -195,7 +196,7 @@ class Calendar_Import_Csv extends Tinebase_Import_Csv_Generic
                 }
 
                 if (!in_array($member, $member_check)) {
-                    if(strpbrk($member,",") === FALSE) {
+                    if (strpbrk($member, ",") === FALSE) {
                         $user_Model = new Tinebase_Model_FullUser(array(
                             'accountDisplayName' => $member,
                             'accountLastName' => $member,
@@ -203,19 +204,17 @@ class Calendar_Import_Csv extends Tinebase_Import_Csv_Generic
                             'accountLoginName' => $member,
                             'accountPrimaryGroup' => 'Users'
                         ));
-                        $user_id = Admin_Controller_User::getInstance()->create($user_Model,1456,1456)->getId();
-                        
+                        $user_id = Admin_Controller_User::getInstance()->create($user_Model, 1456, 1456)->getId();
+
                         $tine_Container_Data = Tinebase_Container::getInstance()->getAll();
                         foreach ($tine_Container_Data as $container) {
-                            if($container['owner_id'] == $user_id && $container['model'] == 'Calendar_Model_Event')
-                            {
+                            if ($container['owner_id'] == $user_id && $container['model'] == 'Calendar_Model_Event') {
                                 $container_id = $container['id'];
                             }
                         }
-                        Tinebase_Container::getInstance()->addGrants($container_id,'group', Tinebase_Group::getInstance()->getGroupByName('users')->getId(), $this->_secretaryGrants, true );                  
-                        
-                    }else
-                    {
+                        Tinebase_Container::getInstance()->addGrants($container_id, 'group', Tinebase_Group::getInstance()->getGroupByName('users')->getId(), $this->_secretaryGrants, true);
+
+                    } else {
                         $member_Model = new Addressbook_Model_Contact();
                         $member_name = explode(',', $member);
                         $member_Model->n_fileas = $member;
@@ -238,36 +237,39 @@ class Calendar_Import_Csv extends Tinebase_Import_Csv_Generic
             $cal_Backend_Sql = new Calendar_Backend_Sql();
             $cal_Backend_Sql->createAttendee($attender);
         }
+
     }
-    
+
     protected function _createGroupAttender($importedRecord)
     {
+        if (empty($this->_groups)) {
+            return;
+        }
         $groups = explode(';', $this->_groups);
         $tinebase_Group = Tinebase_Group::getInstance();
-        foreach($groups as $group) {
-            if (!empty($group)) {
-                try {
-                    $group_Data = $tinebase_Group->getGroupByName($group);
-                    $group_id = $group_Data['id'];
-                } catch (Exception $e) {
-                    $group_Model = new Tinebase_Model_Group();
-                    $group_Model->name = $group;
-                    $group_Model->description = "import DemoData Csv";
-                    $group_id = Admin_Controller_Group::getInstance()->create($group_Model)->getId();
-                }
-
-                $attender = new Calendar_Model_Attender();
-
-                $attender->user_id = $group_id;
-                $attender->user_type = Calendar_Model_Attender::USERTYPE_GROUP;
-                $attender->cal_event_id = $importedRecord['id'];
-                $attender->status_authkey = Tinebase_Record_Abstract::generateUID();
-
-                Tinebase_Timemachine_ModificationLog::getInstance()->setRecordMetaData($attender, 'create');
-
-                $cal_Backend_Sql = new Calendar_Backend_Sql();
-                $cal_Backend_Sql->createAttendee($attender);
+        foreach ($groups as $group) {
+            try {
+                $group_Data = $tinebase_Group->getGroupByName($group);
+                $group_id = $group_Data['id'];
+            } catch (Exception $e) {
+                $group_Model = new Tinebase_Model_Group();
+                $group_Model->name = $group;
+                $group_Model->description = "import DemoData Csv";
+                $group_id = Admin_Controller_Group::getInstance()->create($group_Model)->getId();
             }
+
+            $attender = new Calendar_Model_Attender();
+
+            $attender->user_id = $group_id;
+            $attender->user_type = Calendar_Model_Attender::USERTYPE_GROUP;
+            $attender->cal_event_id = $importedRecord['id'];
+            $attender->status_authkey = Tinebase_Record_Abstract::generateUID();
+
+            Tinebase_Timemachine_ModificationLog::getInstance()->setRecordMetaData($attender, 'create');
+
+            $cal_Backend_Sql = new Calendar_Backend_Sql();
+            $cal_Backend_Sql->createAttendee($attender);
         }
+
     }
 }
