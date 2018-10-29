335e2797b5e8c2f0da41188581273132e50f8e18
Philipp Schüle <p.schuele@metaways.de>
2018-10-29 11:14:49 +0100

    fix(container): don't delete container contents if model has no container
    Change-Id: I19104e540514e5607d09f8d5f75cf22b1b80998d
    Reviewed-on: http://gerrit.tine20.com/customers/10749
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/Controller/Record/Abstract.php
+++ tine20/Tinebase/Controller/Record/Abstract.php
@@ -2739,11 +2739,23 @@ HumanResources_CliTests.testSetContractsEndDate */
 
             if (null === $_filter) {
                 /** @var Tinebase_Model_Filter_FilterGroup $_filter */
-                $_filter = new $filterName(array(), Tinebase_Model_Filter_FilterGroup::CONDITION_AND, array('ignoreAcl' => $_ignoreAcl));
+                $_filter = Tinebase_Model_Filter_FilterGroup::getFilterForModel(
+                    $model,
+                    [],
+                    Tinebase_Model_Filter_FilterGroup::CONDITION_AND,
+                    ['ignoreAcl' => $_ignoreAcl]
+                );
+
                 // we add the container_id filter like this because Calendar Filters have special behaviour that we want to avoid
                 // alternatively the calender event controller would have to overwrite this method and deal with this application
                 // specifics itself. But for the time being, this seems like a good generic solution
                 $_filter->addFilter(new Tinebase_Model_Filter_Id('container_id', 'equals', $_container->id));
+            }
+
+            if ($_filter->getFilter('container_id', false, true) === null) {
+                Tinebase_Core::getLogger()->notice(__METHOD__ . '::' . __LINE__
+                    . ' no container filter in model -> skip');
+                return;
             }
 
             if ($_ignoreAcl) {
