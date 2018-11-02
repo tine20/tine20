22808f831bf5030c429b15cd1c6b650679494d3b
Cornelius Weiß <c.weiss@metaways.de>
2018-11-02 15:31:33 +0100

    fix(Tinebase JS): container selector somtimes disabled
    * for new records w.o. default container
    
    Change-Id: Iad572f8054cc0bbdfd6a0adb6496f9646a422183
    Reviewed-on: http://gerrit.tine20.com/customers/10803
    Reviewed-by: Cornelius Weiss <c.weiss@metaways.de>
    Tested-by: Cornelius Weiss <c.weiss@metaways.de>

--- tine20/Tinebase/js/widgets/dialog/EditDialog.js
+++ tine20/Tinebase/js/widgets/dialog/EditDialog.js
@@ -833,6 +833,7 @@ Tine.widgets.dialog.EditDialog = Ext.extend(Ext.FormPanel, {
         // unset container if user is not allowed to add record in original container
         if(this.evalGrants && ! hasRequiredGrant) {
             _.unset(this.record, 'data.' + this.recordClass.getMeta('containerProperty'));
+            _.set(this.record, this.recordClass.getMeta('grantsPath') + '.deleteGrant', true);
             _.set(this.record, this.recordClass.getMeta('grantsPath') + '.addGrant', true);
             _.set(this.record, this.recordClass.getMeta('grantsPath') + '.editGrant', true);
         }
@@ -914,6 +915,7 @@ Tine.widgets.dialog.EditDialog = Ext.extend(Ext.FormPanel, {
 
 
         if (!this.record.id || !_.get(this.record, 'data.' + this.recordClass.getMeta('containerProperty'), false)) {
+            _.set(this.record, this.recordClass.getMeta('grantsPath') + '.deleteGrant', true);
             _.set(this.record, this.recordClass.getMeta('grantsPath') + '.addGrant', true);
             _.set(this.record, this.recordClass.getMeta('grantsPath') + '.editGrant', true);
         }
