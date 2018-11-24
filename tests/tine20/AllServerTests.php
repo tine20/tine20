019f959b7ee065ec786483a996da7f4c6344e24c
Paul Mehrer <p.mehrer@metaways.de>
2018-11-24 16:53:43 +0100

    fix(ServerTests) - just cleanup
    Change-Id: I3ec5d0bc74b1e9ba8496adf952a6bee47b95d6f0
    Reviewed-on: http://gerrit.tine20.com/customers/11047
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Paul Mehrer <p.mehrer@metaways.de>

--- tests/tine20/AllServerTests.php
+++ tests/tine20/AllServerTests.php
@@ -4,7 +4,7 @@
  * 
  * @package     Tinebase
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
- * @copyright   Copyright (c) 2014-2014 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2014-2018 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Lars Kneschke <l.kneschke@metaways.de>
  */
 
@@ -19,11 +19,11 @@ class AllServerTests
     {
         $suite = new PHPUnit_Framework_TestSuite('Tine 2.0 all server tests');
         
-        $suite->addTestSuite('ActiveSync_Server_HttpTests');
-        $suite->addTestSuite('Tinebase_ControllerServerTest');
-        $suite->addTestSuite('Tinebase_Server_WebDAVTests');
-        $suite->addTestSuite('Tinebase_Server_JsonTests');
-        $suite->addTestSuite('Tinebase_Server_HttpTests');
+        $suite->addTestSuite(ActiveSync_Server_HttpTests::class);
+        $suite->addTestSuite(Tinebase_ControllerServerTest::class);
+        $suite->addTestSuite(Tinebase_Server_WebDAVTests::class);
+        $suite->addTestSuite(Tinebase_Server_JsonTests::class);
+        $suite->addTestSuite(Tinebase_Server_HttpTests::class);
 
         return $suite;
     }
