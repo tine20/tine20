d4bb4c0556f7f620b5923e9f2a967d9c6d5e7398
Philipp Schüle <p.schuele@metaways.de>
2018-11-27 14:05:52 +0100

    Revert "fix(Setup Update) - add clear cache after legacy update run"
    This reverts commit 7a46650094b05114581c76dde38b4c290bbd4928.

--- tine20/Setup/Controller.php
+++ tine20/Setup/Controller.php
@@ -454,9 +454,6 @@ class Setup_Controller
         //            'updated'  => $this->_updatedApplications,
         //        );
         $result = $this->_legacyUpdateApplications($_applications);
-        Tinebase_Core::getCache()->clean();
-        Tinebase_Cache_PerRequest::getInstance()->reset();
-        Tinebase_Application::destroyInstance();
         $iterationCount = 0;
 
         do {
