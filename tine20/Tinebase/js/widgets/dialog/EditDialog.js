11057ad0692270861c454cedbe133b9bd3b52894
Cornelius Weiß <c.weiss@metaways.de>
2019-01-17 18:26:55 +0100

    Tinebase(Feature): new extjs plugin registry
    allow components to query for registered plugins
    
    Change-Id: I6c335532a0db4fbaf6013b6aa6b537357da45892
    Reviewed-on: http://gerrit.tine20.com/customers/11531
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Cornelius Weiss <c.weiss@metaways.de>

--- tine20/Tinebase/js/widgets/dialog/EditDialog.js
+++ tine20/Tinebase/js/widgets/dialog/EditDialog.js
@@ -3,7 +3,7 @@
  * 
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Cornelius Weiss <c.weiss@metaways.de>
- * @copyright   Copyright (c) 2007-2013 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2007-2019 Metaways Infosystems GmbH (http://www.metaways.de)
  */
 Ext.ns('Tine.widgets.dialog');
 
@@ -361,7 +361,8 @@ Tine.widgets.dialog.EditDialog = Ext.extend(Ext.FormPanel, {
         if (this.disableCfs !== true) {
             this.plugins.push(new Tine.widgets.customfields.EditDialogPlugin({}));
         }
-        
+        Ext.ux.pluginRegistry.addRegisteredPlugins(this);
+
         // init actions
         this.initActions();
         // init buttons and tbar
