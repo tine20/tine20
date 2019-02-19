36b1b82f5ef4a654b35f2f954e8a697659e091cf
Cornelius Weiß <c.weiss@metaways.de>
2019-02-19 09:51:22 +0100

    fix(Tinebase): remain timefield size after expand
    Change-Id: Id456fd87613947b40bbb21d8a4c0faef41cbbec2
    Reviewed-on: http://gerrit.tine20.com/customers/11883
    Reviewed-by: Cornelius Weiss <c.weiss@metaways.de>
    Tested-by: Cornelius Weiss <c.weiss@metaways.de>

--- tine20/Tinebase/js/extFixes.js
+++ tine20/Tinebase/js/extFixes.js
@@ -644,7 +644,7 @@ Ext.form.TriggerField.prototype.taskForResize = new Ext.util.DelayedTask(functio
 
         var visible = !!window.lodash.get(cmp, 'el.dom.offsetParent', false);
 
-        if (visible !== cmp.wasVisible && cmp.el.dom) {
+        if (visible !== cmp.wasVisible && cmp.el.dom && !cmp.noFix) {
             cmp.setWidth(cmp.width);
             if (cmp.wrap && cmp.wrap.dom) {
                 cmp.wrap.setWidth(cmp.width);
