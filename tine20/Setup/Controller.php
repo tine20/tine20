e759bf5712cbdf442737def721311982c91c7f76
Paul Mehrer <p.mehrer@metaways.de>
2019-02-25 13:41:46 +0100

    tweak(Tinebase Update) - move legacy doctrine schema update to new update method
    so we can hook in before it gets excecuted
    
    Change-Id: I3f7b6f7dce03ce3375ea819a2f9a9876d7fc2dd1
    Reviewed-on: http://gerrit.tine20.com/customers/11949
    Reviewed-by: Paul Mehrer <p.mehrer@metaways.de>
    Tested-by: Paul Mehrer <p.mehrer@metaways.de>

--- tine20/Setup/Controller.php
+++ tine20/Setup/Controller.php
@@ -626,16 +626,6 @@ class Setup_Controller
         $adbRelease11 = new Addressbook_Setup_Update_Release11(Setup_Backend_Factory::factory());
         $adbRelease11->fixContactData();
 
-        try {
-            Setup_SchemaTool::updateAllSchema();
-        } catch (Exception $e) {
-            Tinebase_Exception::log($e);
-            Setup_Core::getLogger()->notice(__METHOD__ . '::' . __LINE__ . ' Schema update failed - retrying once ...');
-            $this->clearCache();
-            sleep(5);
-            Setup_SchemaTool::updateAllSchema();
-        }
-
         for ($majorVersion = $tinebase->getMajorVersion(); $majorVersion <= $major; $majorVersion++) {
             $messages = array_merge($messages, $this->updateApplication($tinebase, $majorVersion));
         }
--- tine20/Tinebase/Setup/Update/12.php
+++ tine20/Tinebase/Setup/Update/12.php
@@ -12,8 +12,15 @@
 class Tinebase_Setup_Update_12 extends Setup_Update_Abstract
 {
     const RELEASE012_UPDATE001 = __CLASS__ . '::update001';
+    const RELEASE012_UPDATE002 = __CLASS__ . '::update002';
 
     static protected $_allUpdates = [
+        self::PRIO_TINEBASE_BEFORE_STRUCT => [
+            self::RELEASE012_UPDATE002          => [
+                self::CLASS_CONST                   => self::class,
+                self::FUNCTION_CONST                => 'update002',
+            ]
+        ],
         self::PRIO_TINEBASE_UPDATE        => [
             self::RELEASE012_UPDATE001          => [
                 self::CLASS_CONST                   => self::class,
@@ -27,5 +34,22 @@ class Tinebase_Setup_Update_12 extends Setup_Update_Abstract
         $release11 = new Tinebase_Setup_Update_Release11($this->_backend);
         $release11->update_45();
         $this->addApplicationUpdate('Tinebase', '12.19', self::RELEASE012_UPDATE001);
+    }
+
+    public function update002()
+    {
+        // clear open transactions
+        Tinebase_TransactionManager::getInstance()->rollBack();
+        try {
+            Setup_SchemaTool::updateAllSchema();
+        } catch (Exception $e) {
+            Tinebase_Exception::log($e);
+            Setup_Core::getLogger()->notice(__METHOD__ . '::' . __LINE__ . ' Schema update failed - retrying once ...');
+            $this->clearCache();
+            sleep(5);
+            Setup_SchemaTool::updateAllSchema();
+        }
+
+        $this->addApplicationUpdate('Tinebase', '12.20', self::RELEASE012_UPDATE002);
     }
 }
