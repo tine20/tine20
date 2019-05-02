a02aadaf996d0d1478b54766a0522b0d8916fef6
Cornelius Weiß <c.weiss@metaways.de>
2019-05-02 09:57:47 +0200

    improve(Tinebase): use terser as js minifier
    * improve build speed
    * save some bytes
    * bring back sourceMaps
    
    Change-Id: Ia8b2057c2b2e9562c967e4994a1a31f3887656d9
    Reviewed-on: http://gerrit.tine20.com/customers/12701
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/js/webpack.prod.js
+++ tine20/Tinebase/js/webpack.prod.js
@@ -1,26 +1,26 @@
 const merge = require('webpack-merge');
 const UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
-const MinifyPlugin = require("babel-minify-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin')
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    // @see https://github.com/webpack/webpack/issues/5931
-    devtool: 'none',
+    devtool: 'source-map',
     mode: 'production',
     optimization:{
-        minimize: false, // disables uglify -> we use babel minify
+        minimizer: [new TerserPlugin({
+            sourceMap: true,
+            extractComments: true,
+            terserOptions: {
+                // twing problem @see https://github.com/ericmorand/twing/issues/314,
+                // this can be removed when https://github.com/ericmorand/twing/issues/336 is solved
+                keep_classnames: true,
+            },
+        })],
     },
     plugins: [
         new UnminifiedWebpackPlugin({
             postfix : 'debug'
         }),
-        new MinifyPlugin({
-            // mangle: false,
-            // keepFnName: true,
-            keepClassName: true, // twing problem @see https://github.com/ericmorand/twing/issues/314
-        }, {
-            comments: false,
-        })
     ],
 });
 
