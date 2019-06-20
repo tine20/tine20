242b1b081f3de4f36141d401b8d8304bb26b5d66
Christian Feitl <c.feitl@metaways.de>
2019-06-20 11:30:59 +0200

    fix(Calendar): updatescript for broken models in Calendar container
    * fixes #7040
    
    Change-Id: I30ceafade5f074c9f14b64f5ec38a11b05d6485d
    Reviewed-on: http://gerrit.tine20.com/customers/13221
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Calendar/Setup/Update/12.php
+++ tine20/Calendar/Setup/Update/12.php
@@ -14,6 +14,7 @@ class Calendar_Setup_Update_12 extends Setup_Update_Abstract
     const RELEASE012_UPDATE001 = __CLASS__ . '::update001';
     const RELEASE012_UPDATE002 = __CLASS__ . '::update002';
     const RELEASE012_UPDATE003 = __CLASS__ . '::update003';
+    const RELEASE012_UPDATE004 = __CLASS__ . '::update004';
 
     static protected $_allUpdates = [
         self::PRIO_NORMAL_APP_UPDATE        => [
@@ -28,6 +29,10 @@ class Calendar_Setup_Update_12 extends Setup_Update_Abstract
             self::RELEASE012_UPDATE003          => [
                 self::CLASS_CONST                   => self::class,
                 self::FUNCTION_CONST                => 'update003',
+            ],
+            self::RELEASE012_UPDATE004          => [
+                self::CLASS_CONST                   => self::class,
+                self::FUNCTION_CONST                => 'update004',
             ],
         ],
     ];
@@ -51,5 +56,25 @@ class Calendar_Setup_Update_12 extends Setup_Update_Abstract
         $release10 = new Calendar_Setup_Update_Release10($this->_backend);
         $release10->update_11();
         $this->addApplicationUpdate('Calendar', '12.9', self::RELEASE012_UPDATE003);
+    }
+
+    public function update004()
+    {
+        $controller = Tinebase_Container::getInstance();
+        $aclFilter = $controller->doSearchAclFilter(false);
+        $all_container = $controller->search(new Tinebase_Model_ContainerFilter([
+            ['field' => 'application_id', 'operator' => 'equals', 'value' =>
+                Tinebase_Application::getInstance()->getApplicationByName('Calendar')->getId()],
+            ['field' => 'model', 'operator' => 'equals', 'value' => 'Calendar_Model_']
+            ]));
+        $controller->doSearchAclFilter($aclFilter);
+
+        foreach ($all_container as $container)
+        {
+                $container['model'] = 'Calendar_Model_Event';
+                $controller->update($container);
+        }
+
+        $this->addApplicationUpdate('Calendar', '12.10', self::RELEASE012_UPDATE004);
     }
 }
