ab9bad0ca2f62cb0a84b2eafb46520d6f234b194
Christian Feitl <c.feitl@metaways.de>
2019-09-04 10:14:49 +0200

    fix(Tinebase): favorite grantbox search group/user/role
    * make enable/disable of grants grid work
    * change message
    * update translation
    * add e2etests
    
    Change-Id: I3e4a44c9a13299ee84835150cd5352490d4a3f0b
    Reviewed-on: http://gerrit.tine20.com/customers/13905
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

---
+++ tests/e2etests/src/test/Tinebase/saveFavorite.test.js
@@ -0,0 +1,64 @@
+const expect = require('expect-puppeteer');
+const lib = require('../../lib/browser');
+
+require('dotenv').config();
+
+beforeAll(async () => {
+    await lib.getBrowser('Adressbuch', 'Kontakte');
+});
+
+describe('Mainpage', () => {
+
+    test('save favorite', async () => {
+        try {
+            await expect(page).toClick('.t-app-addressbook button', {text: 'Details anzeigen'});
+        } catch (e) {
+            console.log('filterpanel is aktiv');
+        }
+        await page.waitForSelector('.t-app-addressbook .action_saveFilter');
+        await expect(page).toClick('.t-app-addressbook .action_saveFilter');
+        await page.waitForSelector('.x-window.x-resizable-pinned');
+        await page.type('.x-form-text.x-form-field.x-form-invalid', 'favorite');
+        await page.waitForSelector('.x-panel.x-wdgt-pickergrid.x-grid-panel.x-masked-relative.x-masked');
+        await expect(page).toClick('.x-btn-image.action_saveAndClose');
+    });
+
+    test('save shared favorite', async () => {
+        try {
+            await expect(page).toClick('.t-app-addressbook button', {text: 'Details anzeigen'});
+        } catch (e) {
+            console.log('filterpanel is aktiv');
+        }
+        await page.waitForSelector('.t-app-addressbook .action_saveFilter');
+        await expect(page).toClick('.t-app-addressbook .action_saveFilter');
+        await page.waitForSelector('.x-window.x-resizable-pinned');
+        await page.type('.x-form-text.x-form-field.x-form-invalid', 'shared favorite');
+        await page.click('.x-form-checkbox.x-form-field');
+        await page.waitFor(() => !document.querySelector('.x-panel.x-wdgt-pickergrid.x-grid-panel.x-masked-relative.x-masked'));
+        await expect(page).toClick('.x-btn-image.action_saveAndClose');
+        await page.waitFor(1000);
+    });
+
+    test('edit favorite', async () => {
+        await expect(page).toClick('span', {text: 'favorite', button:'right'});
+        await expect(page).toClick('.x-menu-item-icon.action_edit', {visible: true});
+        await page.waitForSelector('.x-window.x-resizable-pinned');
+        await page.screenshot({path: 'screenshots/openFavorite1.png'});
+        await page.waitForSelector('.x-panel.x-wdgt-pickergrid.x-grid-panel.x-masked-relative.x-masked');
+        await page.keyboard.press('Escape');
+        await page.waitFor(() => !document.querySelector('.x-window.x-resizable-pinned'));
+    });
+
+    test('edit shared favorite', async () => {
+        await expect(page).toClick('span', {text: 'shared favorite', button:'right'});
+        await expect(page).toClick('.x-menu-item-icon.action_edit', {visible: true});
+        await page.waitForSelector('.x-window.x-resizable-pinned');
+        await page.waitFor(() => !document.querySelector('.x-panel.x-wdgt-pickergrid.x-grid-panel.x-masked-relative.x-masked'));
+        await page.keyboard.press('Escape');
+        await page.waitFor(() => !document.querySelector('.x-window.x-resizable-pinned'));
+    });
+});
+
+afterAll(async () => {
+    browser.close();
+});
