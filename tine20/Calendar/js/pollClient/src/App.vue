0bc0b34392b85fb4b77aec520c355f95d839059e
Christian Feitl <c.feitl@metaways.de>
2019-12-05 11:22:34 +0100

    fix(PollClient): branding logo max size
    Change-Id: I9e311c8c284a9e508d3de24e47bd015c4b6a0261
    Reviewed-on: http://gerrit.tine20.com/customers/14927
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>
    Tested-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Calendar/js/pollClient/src/App.vue
+++ tine20/Calendar/js/pollClient/src/App.vue
@@ -20,7 +20,7 @@
           </div>
           <div class="col-md-4 col-sm-12 text-right">
             <a :href="poll.config.brandingWeburl">
-              <img :src="poll.config.installLogo" :alt="poll.config.brandingTitle" />
+              <img style="max-width: 300px; max-height: 80px" :src="poll.config.installLogo" :alt="poll.config.brandingTitle"/>
             </a>
           </div>
         </div>
