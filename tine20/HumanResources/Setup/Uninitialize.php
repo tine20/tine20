aab0743aa7f0a46345692507b68dc2260bdef08c
Philipp Schüle <p.schuele@metaways.de>
2020-01-13 08:59:39 +0100

    tweak(HumanResources/Uninit): catch problem with customfield
    ... as it might be missing / already been deleted
    
    Change-Id: I712fc2af96161545d3253a97a11d0189c9f4e38e
    Reviewed-on: http://gerrit.tine20.com/customers/15235
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/HumanResources/Setup/Uninitialize.php
+++ tine20/HumanResources/Setup/Uninitialize.php
@@ -5,14 +5,14 @@
  * @package     HumanResources
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Paul Mehrer <p.mehrer@metaways.de>
- * @copyright   Copyright (c) 2019 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2019-2020 Metaways Infosystems GmbH (http://www.metaways.de)
  *
  */
 
 /**
  * class for HumanResources uninitialization
  *
- * @package     Sales
+ * @package     HumanResources
  */
 class HumanResources_Setup_Uninitialize extends Setup_Uninitialize
 {
@@ -23,11 +23,11 @@ class HumanResources_Setup_Uninitialize extends Setup_Uninitialize
     {
         try {
             $appId = Tinebase_Application::getInstance()->getApplicationByName(Timetracker_Config::APP_NAME)->getId();
-
-            Tinebase_CustomField::getInstance()->deleteCustomField(
-                Tinebase_CustomField::getInstance()->getCustomFieldByNameAndApplication($appId,
-                    HumanResources_Model_FreeTimeType::TT_TS_SYSCF_CLOCK_OUT_REASON, null, true)
-            );
+            $customfield = Tinebase_CustomField::getInstance()->getCustomFieldByNameAndApplication($appId,
+                HumanResources_Model_FreeTimeType::TT_TS_SYSCF_CLOCK_OUT_REASON, null, true);
+            if ($customfield) {
+                Tinebase_CustomField::getInstance()->deleteCustomField($customfield);
+            }
         } catch (Tinebase_Exception_NotFound $tenf) {}
     }
 }
