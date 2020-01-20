b1fdb7aaebb0cc84c1f37c61c2aef4c0c529e443
Paul Mehrer <p.mehrer@metaways.de>
2020-01-20 17:47:12 +0100

    fix(php74) fix test fail 1-3
    Change-Id: Ic492c7b3d2503d3902a90b8d47aae1f115b60da2
    Reviewed-on: http://gerrit.tine20.com/customers/15377
    Reviewed-by: Paul Mehrer <p.mehrer@metaways.de>
    Tested-by: Paul Mehrer <p.mehrer@metaways.de>

--- tests/tine20/Tinebase/ConfigTest.php
+++ tests/tine20/Tinebase/ConfigTest.php
@@ -140,7 +140,7 @@ class Tinebase_ConfigTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($clientConfig->Calendar instanceof Tinebase_Config_Struct, 'calendar clientconfig is not a struct');
         $this->assertEquals(Calendar_Config::getInstance()->fixedCalendars, $clientConfig->Calendar->fixedCalendars->value, 'fixed calendar config not correct');
 
-        $this->assertFalse((isset($clientConfig->Tinebase['SMTP']) || array_key_exists('SMTP', $clientConfig->Tinebase)), 'SMTP is not a client config');
+        $this->assertFalse(isset($clientConfig->Tinebase['SMTP']), 'SMTP is not a client config');
     }
 
     /**
