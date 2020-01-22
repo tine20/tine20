04fee260a511f66ab09b5eed7d96c47f42b42a4d
Philipp Schüle <p.schuele@metaways.de>
2020-01-22 15:28:41 +0100

    text(Tinebase/Core): make tine useragent more readable
    Change-Id: Ib5d2d1ae6a56da0987aef88568c634d34dcec586
    Reviewed-on: http://gerrit.tine20.com/customers/15417
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/Core.php
+++ tine20/Tinebase/Core.php
@@ -2108,7 +2108,7 @@ class Tinebase_Core
      */
     public static function getTineUserAgent($submodule = '')
     {
-        return 'Tine 2.0 ' . $submodule . '(version ' . TINE20_CODENAME . ' - ' . TINE20_PACKAGESTRING . ')';
+        return 'Tine 2.0 ' . $submodule . ' (version ' . TINE20_CODENAME . ' - ' . TINE20_PACKAGESTRING . ')';
     }
 
     /**
