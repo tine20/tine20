be2a48b284adb3575289bebeb6c9c65fee206904
Philipp Schüle <p.schuele@metaways.de>
2020-01-28 09:54:58 +0100

    tweak(Felamimail/Account): don't log missing creds to sentry
    Change-Id: Ia300e1297d7b073738b20328633d064a6a94c486
    Reviewed-on: http://gerrit.tine20.com/customers/15499
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Felamimail/Controller/Account.php
+++ tine20/Felamimail/Controller/Account.php
@@ -954,8 +954,9 @@ class Felamimail_Controller_Account extends Tinebase_Controller_Record_Grants
             try {
                 $credentialsBackend->getCachedCredentials($credentials);
             } catch (Tinebase_Exception_NotFound $tenf) {
-                // create new credentials in this case
-                Tinebase_Exception::log($tenf);
+                if (Tinebase_Core::isLogLevel(Zend_Log::INFO)) Tinebase_Core::getLogger()->info(
+                    __METHOD__ . '::' . __LINE__ . ' Credentials could not be found/decrypted ... '
+                    . ' Creating new credentials.');
                 $credentials = null;
             }
         }
@@ -984,7 +985,8 @@ class Felamimail_Controller_Account extends Tinebase_Controller_Record_Grants
         
         if ($_record->smtp_user && $_record->smtp_password) {
             // create extra smtp credentials
-            if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' Update/create SMTP credentials.');
+            if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(
+                __METHOD__ . '::' . __LINE__ . ' Update/create SMTP credentials.');
             $_record->smtp_credentials_id = $this->_createCredentials($_record->smtp_user, $_record->smtp_password);
             
         } else if (
