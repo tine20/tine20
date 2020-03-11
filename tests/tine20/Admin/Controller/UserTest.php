caa557bcd4f883ead9ff59223733058ab68958e5
Paul Mehrer <p.mehrer@metaways.de>
2020-03-11 16:29:23 +0100

    added assertion to admin controller usertest
    Change-Id: I38bdb3f36952ff7401684223598c053a802211b0
    Reviewed-on: http://gerrit.tine20.com/customers/16067
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Paul Mehrer <p.mehrer@metaways.de>

--- tests/tine20/Admin/Controller/UserTest.php
+++ tests/tine20/Admin/Controller/UserTest.php
@@ -47,5 +47,7 @@ class Admin_Controller_UserTest extends TestCase
         $user = Admin_Controller_User::getInstance()->create($userToCreate, $pw, $pw);
 
         static::assertSame($container->getId(), $user->container_id);
+        static::assertSame($container->getId(),
+            Addressbook_Controller_Contact::getInstance()->get($user->contact_id)->container_id);
     }
 }
