ed665d59e9661e11d80d6f280da7cee1c6b75d9e
Christian Feitl <c.feitl@metaways.de>
2020-03-12 15:06:17 +0100

    feature(Admin/Demodata): add grants system for import
    * all grants files are load
    * add test
    
    Change-Id: I6df7ccc623f44ac0af808b4cd69960bcedc8df98
    Reviewed-on: http://gerrit.tine20.com/customers/16409
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Admin/Import/Role/Csv.php
+++ tine20/Admin/Import/Role/Csv.php
@@ -36,6 +36,10 @@ class Admin_Import_Role_Csv extends Tinebase_Import_Csv_Abstract
      */
     protected $_groups;
 
+    protected $_rights;
+
+    protected $_rightsFile;
+
 
     /**
      * add some more values (container id)
@@ -52,8 +56,8 @@ class Admin_Import_Role_Csv extends Tinebase_Import_Csv_Abstract
     {
         try {
             $role = Tinebase_Role::getInstance()->getRoleByName($_record['name']);
-            $this->_inspectAfterImport($role);
             $this->_importResult['duplicatecount']++;
+            return $role;
         } catch (Tinebase_Exception $e) {
             return parent::_importRecord($_record, $_resolveStrategy, $_recordData);
         }
@@ -71,9 +75,72 @@ class Admin_Import_Role_Csv extends Tinebase_Import_Csv_Abstract
         $result = parent::_doConversions($_data);
         $this->_members = $result['members'];
         $this->_groups = $result['groups'];
+        $this->_rights = $result['rights'];
 
         return $result;
     }
+
+    protected function addRights($rightsName)
+    {
+        if (!$rightsName) {
+            return null;
+        }
+        if (!extension_loaded('yaml')) {
+            throw new Tinebase_Exception('yaml extension required');
+        }
+
+        $importDir = dirname(dirname(dirname(__DIR__))) . DIRECTORY_SEPARATOR
+            . 'Admin' . DIRECTORY_SEPARATOR . 'Setup' . DIRECTORY_SEPARATOR . 'DemoData' . DIRECTORY_SEPARATOR . 'import' . DIRECTORY_SEPARATOR . 'Rights';
+
+        $files = array_diff(scandir($importDir), array('..', '.'));
+
+        foreach ($files as $file) {
+            $path = $importDir . DIRECTORY_SEPARATOR . $file;
+            if (file_exists($path)) {
+                if (Tinebase_Core::isLogLevel(Zend_Log::INFO)) Tinebase_Core::getLogger()->info(__METHOD__ . '::' . __LINE__
+                    . ' Importing DemoData set from file ' . $path);
+                $setData = yaml_parse_file($path);
+
+                if ($setData[$rightsName]) {
+                    $set = $setData[$rightsName];
+
+                    $roleRights = [];
+                    // resolve rights
+                    foreach ($set as $data) {
+                        if ($data == 'AllAdmin') {
+                            $enabledApps = Tinebase_Application::getInstance()->getApplicationsByState(Tinebase_Application::ENABLED);
+                            foreach ($enabledApps as $application) {
+                                $allRights = Tinebase_Application::getInstance()->getAllRights($application->getId());
+                                foreach ($allRights as $right) {
+                                    $roleRights[] = array(
+                                        'application_id' => $application->getId(),
+                                        'right' => $right,
+                                    );
+                                }
+                            }
+                            return $roleRights;
+                        }
+                        $data = explode('/', $data);
+                        try {
+                            $appId = Tinebase_Application::getInstance()->getApplicationByName($data[0])->getId();
+                            $rightsData = explode(',', $data[1]);
+                            foreach ($rightsData as $right) {
+                                $roleRights[] = array('application_id' => $appId, 'right' => $right);
+                            }
+
+                        } catch (Exception $e) {
+                            if (Tinebase_Core::isLogLevel(Zend_Log::NOTICE)) Tinebase_Core::getLogger()->notice
+                            (__METHOD__ . '::' . __LINE__ . 'Application "' . $data[0] .
+                                '" not installed. Skipping...' . PHP_EOL);
+
+                        }
+                    }
+                }
+            }
+            return $roleRights;
+        }
+    }
+
 
     /**
      *   add role-account for user and group
@@ -101,7 +168,9 @@ class Admin_Import_Role_Csv extends Tinebase_Import_Csv_Abstract
                 Tinebase_Role::getInstance()->addRoleMember($role_id, $ids);
             }
         }
-        // @ToDo Rights for the Roles
-        // Tinebase_Role::getInstance()->addRoleRight($role_id,'671a772cb6e802a579d61ed76fcd45e3cdcc87ec','run');
+
+        if(!empty($this->_rights)) {
+            Tinebase_Role::getInstance()->setRoleRights($role_id, $this->addRights($this->_rights));
+        }
     }
 }
