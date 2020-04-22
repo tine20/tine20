d5fb5f7b9b0666acca29a75f67b0fb2d8e236928
Philipp Schüle <p.schuele@metaways.de>
2020-04-22 08:32:14 +0200

    tweak(Tinebase/WebDAV): convert tenf to sabre exception
    Change-Id: I83e4ffb830e8af27cd215240404bcc15ccba6bbc
    Reviewed-on: http://gerrit.tine20.com/customers/16373
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/Frontend/WebDAV/File.php
+++ tine20/Tinebase/Frontend/WebDAV/File.php
@@ -16,6 +16,13 @@
  */
 class Tinebase_Frontend_WebDAV_File extends Tinebase_Frontend_WebDAV_Node implements Sabre\DAV\IFile
 {
+    /**
+     * @return bool|false|mixed|resource|null
+     * @throws Tinebase_Exception_AccessDenied
+     * @throws Tinebase_Exception_InvalidArgument
+     * @throws \Sabre\DAV\Exception\Forbidden
+     * @throws \Sabre\DAV\Exception\NotFound
+     */
     public function get() 
     {
         $pathRecord = Tinebase_Model_Tree_Node_Path::createFromStatPath($this->_path);
@@ -28,7 +35,11 @@ class Tinebase_Frontend_WebDAV_File extends Tinebase_Frontend_WebDAV_Node implem
             throw new Sabre\DAV\Exception\Forbidden('Forbidden to download file: ' . $this->_path);
         }
 
-        $node = $pathRecord->getNode();
+        try {
+            $node = $pathRecord->getNode();
+        } catch (Tinebase_Exception_NotFound $tenf) {
+            throw new Sabre\DAV\Exception\NotFound($tenf->getMessage());
+        }
         if ($node->is_quarantined) {
             throw new Sabre\DAV\Exception\Forbidden('File is quarantined: ' . $this->_path);
         }
