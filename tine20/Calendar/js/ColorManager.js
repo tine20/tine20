d06128e2e452e83f391c9d41f2fdbbaaf4bc68de
Jan Evers <j.evers@metaways.de>
2020-06-12 15:48:40 +0200

    feature(Calendar): adds color scheme generation for calendar
    Change-Id: If4f9fffcf762d537378979e6a7713e1c0046ecab
    Reviewed-on: http://gerrit.tine20.com/customers/16887
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Cornelius Weiss <c.weiss@metaways.de>

--- tine20/Calendar/js/ColorManager.js
+++ tine20/Calendar/js/ColorManager.js
@@ -113,20 +113,58 @@ Ext.apply(Tine.Calendar.ColorManager.prototype, {
     },
     
     getCustomSchema: function(color) {
-        var diffs = new Ext.util.MixedCollection();
-        for (var key in this.colorSchemata) {
-            if (this.colorSchemata.hasOwnProperty(key)) {
-                diffs.add(Tine.Calendar.ColorManager.compare(color, key, true), this.colorSchemata[key]);
-            }
+        let schema = {color: "#" + color}
+        schema.light = this.shadeColor(0.4, schema.color);
+        schema.text = this.getTextColor(schema.color);
+        schema.lighttext = this.getTextColor(schema.light);
+
+        return schema;
+    },
+
+    shadeColor: function(shade, color) {
+        // TODO: Move copied pSBC code out of here!
+
+        // pSBC - Shade Blend Convert - Version 4.0 - 02/18/2019
+        // https://github.com/PimpTrizkit/PJs/edit/master/pSBC.js
+        const pSBC=(p,c0,c1,l)=>{
+            let r,g,b,P,f,t,h,m=Math.round,a=typeof(c1)=="string";
+            if(typeof(p)!="number"||p<-1||p>1||typeof(c0)!="string"||(c0[0]!='r'&&c0[0]!='#')||(c1&&!a))return null;
+            h=c0.length>9,h=a?c1.length>9?true:c1=="c"?!h:false:h,f=pSBC.pSBCr(c0),P=p<0,t=c1&&c1!="c"?pSBC.pSBCr(c1):P?{r:0,g:0,b:0,a:-1}:{r:255,g:255,b:255,a:-1},p=P?p*-1:p,P=1-p;
+            if(!f||!t)return null;
+            if(l)r=m(P*f.r+p*t.r),g=m(P*f.g+p*t.g),b=m(P*f.b+p*t.b);
+            else r=m((P*f.r**2+p*t.r**2)**0.5),g=m((P*f.g**2+p*t.g**2)**0.5),b=m((P*f.b**2+p*t.b**2)**0.5);
+            a=f.a,t=t.a,f=a>=0||t>=0,a=f?a<0?t:t<0?a:a*P+t*p:0;
+            if(h)return"rgb"+(f?"a(":"(")+r+","+g+","+b+(f?","+m(a*1000)/1000:"")+")";
+            else return"#"+(4294967296+r*16777216+g*65536+b*256+(f?m(a*255):0)).toString(16).slice(1,f?undefined:-2)
         }
-        
-        //NOTE default is string sort
-        diffs.keySort('ASC', function(v1,v2) {return v1 > v2 ? 1 : (v1 < v2 ? -1 : 0);});
-        
-        // cache result
-        this.colorSchemata[color] = diffs.first();
-        
-        return this.colorSchemata[color];
+
+        pSBC.pSBCr=(d)=>{
+            const i=parseInt,m=Math.round;
+            let n=d.length,x={};
+            if(n>9){
+                const [r, g, b, a] = (d = d.split(','));
+                n = d.length;
+                if(n<3||n>4)return null;
+                x.r=i(r[3]=="a"?r.slice(5):r.slice(4)),x.g=i(g),x.b=i(b),x.a=a?parseFloat(a):-1
+            }else{
+                if(n==8||n==6||n<4)return null;
+                if(n<6)d="#"+d[1]+d[1]+d[2]+d[2]+d[3]+d[3]+(n>4?d[4]+d[4]:"");
+                d=i(d.slice(1),16);
+                if(n==9||n==5)x.r=d>>24&255,x.g=d>>16&255,x.b=d>>8&255,x.a=m((d&255)/0.255)/1000;
+                else x.r=d>>16,x.g=d>>8&255,x.b=d&255,x.a=-1
+            }return x
+        };
+        // end of pSBC
+
+        return pSBC(shade, color);
+    },
+
+    getTextColor: function (colorString) {
+        let values = Tine.Calendar.ColorManager.str2dec(colorString)
+        // constants see: https://stackoverflow.com/questions/596216/formula-to-determine-brightness-of-rgb-color
+        let Y = 0.299 * values[0] + 0.587 * values[1] + 0.114 * values[2];
+
+        return Y > 128 ? '#000000' : '#FFFFFF';
     }
 });
 
