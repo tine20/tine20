79b0b68f4cd744b96985a225d069e8bf878bc3a4
Philipp Schüle <p.schuele@metaways.de>
2020-06-15 13:29:09 +0200

    feature(Admin/EmailAccount): update script adds missing mail accounts
    ... for enabled and visible users
    ... if Felamimail and system accounts are enabled
    
    Change-Id: I69dc227f8346ceb232ea83c9246dbd780c2be3b1
    Reviewed-on: http://gerrit.tine20.com/customers/16901
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Admin/Setup/Update/13.php
+++ tine20/Admin/Setup/Update/13.php
@@ -12,12 +12,17 @@
 class Admin_Setup_Update_13 extends Setup_Update_Abstract
 {
     const RELEASE013_UPDATE000 = __CLASS__ . '::update000';
+    const RELEASE013_UPDATE001 = __CLASS__ . '::update001';
 
     static protected $_allUpdates = [
         self::PRIO_NORMAL_APP_UPDATE        => [
             self::RELEASE013_UPDATE000          => [
                 self::CLASS_CONST                   => self::class,
                 self::FUNCTION_CONST                => 'update000',
+            ],
+            self::RELEASE013_UPDATE001          => [
+                self::CLASS_CONST                   => self::class,
+                self::FUNCTION_CONST                => 'update001',
             ],
         ],
     ];
@@ -33,5 +38,29 @@ class Admin_Setup_Update_13 extends Setup_Update_Abstract
             }
         }
         $this->addApplicationUpdate('Admin', '13.0', self::RELEASE013_UPDATE000);
+    }
+
+    /**
+     * add missing Felamimail system accounts for current users
+     */
+    public function update001()
+    {
+        if (Tinebase_Config::getInstance()->{Tinebase_Config::IMAP}
+            ->{Tinebase_Config::IMAP_USE_SYSTEM_ACCOUNT} && Tinebase_Application::getInstance()->isInstalled('Felamimail')) {
+            /* @var Tinebase_Model_FullUser $user */
+            foreach (Admin_Controller_User::getInstance()->searchFullUsers('') as $user) {
+                if ($user->accountStatus !== Tinebase_Model_User::ACCOUNT_STATUS_ENABLED
+                    || $user->visibility !== Tinebase_Model_User::VISIBILITY_DISPLAYED
+                ) {
+                    // skip invisible and disabled users
+                    continue;
+                }
+                $account = Admin_Controller_EmailAccount::getInstance()->getSystemAccount($user);
+                if (! $account) {
+                    Felamimail_Controller_Account::getInstance()->createSystemAccount($user, null);
+                }
+            }
+        }
+        $this->addApplicationUpdate('Admin', '13.1', self::RELEASE013_UPDATE001);
     }
 }
