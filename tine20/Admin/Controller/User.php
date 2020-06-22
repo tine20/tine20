d91cb9813ca4069d9ee601ee7629659120c946fa
Philipp Schüle <p.schuele@metaways.de>
2020-06-22 18:27:51 +0200

    fix(Admin/User): only add quota info with enabled imap email config
    ... as we might run into strange problems otherwise
    
    Change-Id: I5e0daa3b1eac429cfdc4cf8a046047204ce2baf4
    Reviewed-on: http://gerrit.tine20.com/customers/16961
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Admin/Controller/User.php
+++ tine20/Admin/Controller/User.php
@@ -87,15 +87,18 @@ class Admin_Controller_User extends Tinebase_Controller_Abstract
         
         $result = $this->_userBackend->getUsers($_filter, $_sort, $_dir, $_start, $_limit, 'Tinebase_Model_FullUser');
 
-        $emailUser = null;
-        try {
-            $emailUser = Tinebase_EmailUser::getInstance();
-        } catch (Tinebase_Exception_NotFound $tenf) {}
+        if (Tinebase_EmailUser::manages(Tinebase_Config::IMAP)) {
+            $emailUser = null;
+            try {
+                $emailUser = Tinebase_EmailUser::getInstance();
+            } catch (Tinebase_Exception_NotFound $tenf) {
+            }
 
-        // FIXME if you want quota info: in Tinebase_User_Plugin_LdapInterface, inspectGetUserByProperty has a second param!
-        if (null !== $emailUser && ! $emailUser instanceof Tinebase_User_Plugin_LdapInterface ) {
-            foreach ($result as $user) {
-                $emailUser->inspectGetUserByProperty($user);
+            // FIXME if you want quota info: in Tinebase_User_Plugin_LdapInterface, inspectGetUserByProperty has a second param!
+            if (null !== $emailUser && !$emailUser instanceof Tinebase_User_Plugin_LdapInterface) {
+                foreach ($result as $user) {
+                    $emailUser->inspectGetUserByProperty($user);
+                }
             }
         }
         
