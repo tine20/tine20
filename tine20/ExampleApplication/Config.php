caf7d875abf99a810147e844d2ee9ae195ffe583
Paul Mehrer <p.mehrer@metaways.de>
2020-08-14 21:49:23 +0200

    tweak(ExampleApp) make use of TB_Controller_SingletonTrait
    Change-Id: I443c13cbcf74efe87f2872f4edef2ba9cf386940
    Reviewed-on: http://gerrit.tine20.com/customers/17525
    Reviewed-by: Paul Mehrer <p.mehrer@metaways.de>
    Tested-by: Paul Mehrer <p.mehrer@metaways.de>

--- tine20/ExampleApplication/Config.php
+++ tine20/ExampleApplication/Config.php
@@ -4,7 +4,7 @@
  * @subpackage  Config
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Philipp Schüle <p.schuele@metaways.de>
- * @copyright   Copyright (c) 2011-2018 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2011-2020 Metaways Infosystems GmbH (http://www.metaways.de)
  */
 
 /**
@@ -15,6 +15,8 @@
  */
 class ExampleApplication_Config extends Tinebase_Config_Abstract
 {
+    use Tinebase_Controller_SingletonTrait;
+
     const APP_NAME = 'ExampleApplication';
 
     /**
@@ -198,46 +200,6 @@ class ExampleApplication_Config extends Tinebase_Config_Abstract
      * @see tine20/Tinebase/Config/Abstract::$_appName
      */
     protected $_appName = 'ExampleApplication';
-    
-    /**
-     * holds the instance of the singleton
-     *
-     * @var Tinebase_Config
-     */
-    private static $_instance = NULL;
-    
-    /**
-     * the constructor
-     *
-     * don't use the constructor. use the singleton 
-     */    
-    private function __construct() {}
-    
-    /**
-     * the constructor
-     *
-     * don't use the constructor. use the singleton 
-     */    
-    private function __clone() {}
-    
-    /**
-     * Returns instance of Tinebase_Config
-     *
-     * @return Tinebase_Config
-     */
-    public static function getInstance() 
-    {
-        if (self::$_instance === NULL) {
-            self::$_instance = new self();
-        }
-        
-        return self::$_instance;
-    }
-
-    public static function destroyInstance()
-    {
-        self::$_instance = null;
-    }
     
     /**
      * (non-PHPdoc)
