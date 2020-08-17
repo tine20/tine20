755c91a829b534ffc3fed22fea4dba7140046e4b
Philipp Schüle <p.schuele@metaways.de>
2020-08-17 09:54:28 +0200

    tweak(Felamimail/Message): reduce maxAttachmentSize
    ... to 1/6 of php memory_limit
    
    as we still run into OOM on message send with big
     attachments
    
    Change-Id: Ie8d356e307cd6568700764f2354fa439a7fddca5
    Reviewed-on: http://gerrit.tine20.com/customers/17535
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Felamimail/Controller/Message/Send.php
+++ tine20/Felamimail/Controller/Message/Send.php
@@ -1116,7 +1116,7 @@ class Felamimail_Controller_Message_Send extends Felamimail_Controller_Message
     /**
      * get max attachment size for outgoing mails
      * 
-     * - currently it is set to memory_limit / 4
+     * - currently it is set to memory_limit / 6
      * - returns size in Bytes
      * 
      * @return integer
@@ -1130,7 +1130,7 @@ class Felamimail_Controller_Message_Send extends Felamimail_Controller_Message
             $configuredMemoryLimit = '512M';
         }
 
-        $result = round(Tinebase_Helper::convertToBytes($configuredMemoryLimit) / 4);
+        $result = round(Tinebase_Helper::convertToBytes($configuredMemoryLimit) / 6);
         if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__
             . ' memory_limit = ' . $configuredMemoryLimit . ' / max upload size: ' . $result);
 
