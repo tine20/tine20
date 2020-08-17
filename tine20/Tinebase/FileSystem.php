838acf9b9733556b28f28fa6fbd14f3f96258abe
Philipp Schüle <p.schuele@metaways.de>
2020-08-17 09:40:02 +0200

    tweak(Tinebase/FileSystem): prevent race condition
    ... move DEBUG log to prevent
     "failed to open stream: File exists"
    
    maybe we should catch the fopen() "file exists" error?
    
    Change-Id: Ie3be56b264faf789e81f2825c7e80f7a8a65b1eb
    Reviewed-on: http://gerrit.tine20.com/customers/17533
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/FileSystem.php
+++ tine20/Tinebase/FileSystem.php
@@ -2205,7 +2205,8 @@ class Tinebase_FileSystem implements
         
         $hashDirectory = $this->_basePath . '/' . substr($hash, 0, 3);
         if (!file_exists($hashDirectory)) {
-            Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' create hash directory: ' . $hashDirectory);
+            if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(
+                __METHOD__ . '::' . __LINE__ . ' create hash directory: ' . $hashDirectory);
             if (@mkdir($hashDirectory, 0700) === false) {
                 if (!is_dir($hashDirectory)) {
                     throw new Tinebase_Exception_UnexpectedValue('failed to create directory');
@@ -2213,12 +2214,13 @@ class Tinebase_FileSystem implements
             }
         }
         
-        $hashFile      = $hashDirectory . '/' . substr($hash, 3);
+        $hashFile = $hashDirectory . '/' . substr($hash, 3);
         $avResult = new Tinebase_FileSystem_AVScan_Result(Tinebase_FileSystem_AVScan_Result::RESULT_ERROR, null);
         $fileCreated = false;
 
+        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(
+            __METHOD__ . '::' . __LINE__ . ' create hash file: ' . $hashFile);
         while (!file_exists($hashFile)) {
-            Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' create hash file: ' . $hashFile);
             $hashHandle = fopen($hashFile, 'x');
             if (! $hashHandle) {
                 if (file_exists($hashFile)) {
