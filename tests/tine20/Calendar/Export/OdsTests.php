129b634d19fc0dfe305548bacbfaa3761f3d8afd
Christian Feitl <c.feitl@metaways.de>
2020-09-23 12:59:32 +0200

    test(Calendar/Export): add test for filter with attendee
    Change-Id: I12224768d3d07f541f53e15f9064bc4b6c2c4e40
    Reviewed-on: http://gerrit.tine20.com/customers/19325
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Paul Mehrer <p.mehrer@metaways.de>

--- tests/tine20/Calendar/Export/OdsTests.php
+++ tests/tine20/Calendar/Export/OdsTests.php
@@ -4,7 +4,7 @@
  *
  * @package     Calendar
  * @license     http://www.gnu.org/licenses/agpl.html
- * @copyright   Copyright (c) 2014 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @copyright   Copyright (c) 2014-2021 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Cornelius Weiss <c.weiss@metaways.de>
  *
  */
@@ -21,6 +21,31 @@ class Calendar_Export_OdsTests extends Calendar_TestCase
     {
         // export & check
         $csvExportClass = new Calendar_Export_Ods(new Calendar_Model_EventFilter(array()));
-        $result = $csvExportClass->generate();
+        $csvExportClass->generate();
+    }
+
+    public function testExportWithAttendeeOdsSheet()
+    {
+        // @TODO have some demodata to export here
+
+        $calendar = $this->_getTestCalendar();
+
+        $filter = new Calendar_Model_EventFilter([
+            ['field' =>	'query', 'operator' => 'contains', 'value' => ''],
+            ['field' => 'period', 'operator' => 'within', 'value' => [
+                'from' => '2020-09-21 00:00:00',
+                'until' => '2020-09-28 00:00:00',
+            ]],
+            ['condition' => 'OR', 'filters' => [
+                ['field' => 'attender_status', 'operator' => 'notin', 'value' => 'DECLINED'],
+                ['field' => 'container_id', 'operator' => 'equals', 'value' => $calendar->getId()],
+                ['field' => 'attender', 'operator' => 'in', 'value' => [
+                    'user_type' => Calendar_Model_Attender::USERTYPE_USER,
+                    'user_id'   => $this->_getPersonasContacts('sclever')->getId()
+                ]]] , 'id' => 'FilterPanel']
+        ]);
+
+        $doc = new Calendar_Export_Ods($filter);
+        $doc->generate();
     }
 }
