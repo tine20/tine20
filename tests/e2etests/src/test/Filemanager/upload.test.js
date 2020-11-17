328b56643de08358a7c8695bb3862cc53968f2be
Christian Feitl <c.feitl@metaways.de>
2020-11-17 14:00:06 +0100

    fix(Filemanager/e2etest): fix open editDialog
    Change-Id: I43a27e97c5d34f3cccffe664501fea3f0c65f5ac
    Reviewed-on: http://gerrit.tine20.com/customers/18487
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tests/e2etests/src/test/Filemanager/upload.test.js
+++ tests/e2etests/src/test/Filemanager/upload.test.js
@@ -18,41 +18,44 @@ describe('filemanager', () => {
             }
 
             await expect(page).toClick('.t-app-filemanager .tine-mainscreen-centerpanel-west-treecards span', {text: 'Tine 2.0 Admin Account\'s personal files'});
+            await page.waitFor(2000);
         });
         // gehen ins home verzeichnis
         describe('new folder', () => {
             let editDialog;
             test('create folder', async () => {
                 const folder = 'Test' + Math.round(Math.random() * 10000000);
-                await page.waitFor(1000);
                 await expect(page).toClick('.t-app-filemanager button', {text: 'Ordner anlegen'});
                 await page.waitForSelector('.x-window.x-window-plain.x-window-dlg');
                 await page.type('.ext-mb-fix-cursor input', folder);
                 await page.keyboard.press('Enter');
+                await page.waitFor(2000);
                 await expect(page).toClick('.x-grid3-cell-inner.x-grid3-col-name' ,{text:folder});
             });
             test('open editDialog', async () => {
                 await expect(page).toClick('.t-app-filemanager button', {text: 'Eigenschaften bearbeiten'});
                 editDialog = await lib.getNewWindow();
+                await editDialog.waitFor(2000);
                 await expect(editDialog).toClick('span',{text: 'Berechtigungen'});
             });
-            // test('add user in grantsPanel', async () => {
-            //     await expect(editDialog).toClick('.x-form-cb-label', {text:'Diese Ordner hat eigene Berechtigungen'});
-            //     let input = await editDialog.$$('.x-panel-tbar.x-panel-tbar-noheader');
-            //     await input[1].click();
-            //     await editDialog.keyboard.press('ArrowDown');
-            //     await expect(editDialog).toClick('.x-combo-list-item', {text:'Users'});
-            // });
-            // test('give new user rights', async () => {
-            //     await editDialog.waitForXPath('//div[contains(@class, "x-grid3-row ") and contains(., "Users")]');
-            //     await clickCheckBox(editDialog,'x-grid3-cc-add');
-            //     await clickCheckBox(editDialog,'x-grid3-cc-edit');
-            //     await clickCheckBox(editDialog,'x-grid3-cc-delete');
-            //     await clickCheckBox(editDialog,'x-grid3-cc-download');
-            //     await clickCheckBox(editDialog,'x-grid3-cc-publish');
-            // });
+            test('add user in grantsPanel', async () => {
+                await expect(editDialog).toClick('.x-form-cb-label', {text:'Diese Ordner hat eigene Berechtigungen'});
+                let input = await editDialog.$$('.x-panel-tbar.x-panel-tbar-noheader');
+                await input[1].click();
+                await editDialog.keyboard.press('ArrowDown');
+                await expect(editDialog).toClick('.x-combo-list-item', {text:'Users'});
+            });
+            test('give new user rights', async () => {
+                await editDialog.waitForXPath('//div[contains(@class, "x-grid3-row ") and contains(., "Users")]');;
+                await clickCheckBox(editDialog,'x-grid3-cc-add');
+                await clickCheckBox(editDialog,'x-grid3-cc-edit');
+                await clickCheckBox(editDialog,'x-grid3-cc-delete');
+                await clickCheckBox(editDialog,'x-grid3-cc-download');
+                await clickCheckBox(editDialog,'x-grid3-cc-publish');
+            });
             test('save folder', async () => {
                 await expect(editDialog).toClick('button', {text:'Ok'});
+                await page.waitFor(2000);
             });
             test('upload file', async () => {
                 //@todo upload file!
@@ -67,7 +70,7 @@ afterAll(async () => {
 
 
 async function clickCheckBox(page, checkbox) {
+    await page.waitFor(500);
     const elements = await page.$x('//div[contains(@class, "x-grid3-row") and contains(., "Users")] //div[contains(@class, "'+ checkbox +'")]');
-    console.log(elements);
     await elements[0].click();
 }
