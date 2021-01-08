08290857b0a94d36bf108aaf011b2fce4d6d9145
Christian Feitl <c.feitl@metaways.de>
2021-01-08 13:18:29 +0100

    tweak(Tinebase): add filterspec for records
    * load default mc2 value for checkbox
    
    Change-Id: Iea32c05937d2507563a14f7af277c8325da0f366
    Reviewed-on: http://gerrit.tine20.com/customers/18841
    Tested-by: Jenkins CI (http://ci.tine20.com/) <tine20-jenkins@metaways.de>
    Reviewed-by: Philipp Schüle <p.schuele@metaways.de>

--- tine20/Tinebase/js/widgets/form/FieldManager.js
+++ tine20/Tinebase/js/widgets/form/FieldManager.js
@@ -122,6 +122,7 @@ Tine.widgets.form.FieldManager = function() {
                     if (category === 'editDialog') {
                         field.xtype = 'checkbox';
                         field.boxLabel = field.fieldLabel;
+                        field.checked = field['default'];
                         field.hideLabel = true;
                         field.blurOnChange = true;
                     } else {
@@ -206,14 +207,16 @@ Tine.widgets.form.FieldManager = function() {
                     break;
                 case 'record':
                     if (fieldDefinition.config && fieldDefinition.config.appName && fieldDefinition.config.modelName) {
+                        if (fieldDefinition.config.additionalFilterSpec) {
+                            field.additionalFilterSpec = fieldDefinition.config.additionalFilterSpec;
+                        }
                         if (category === 'editDialog' && fieldDefinition.config.dependentRecords) {
                             field.xtype = 'tw-recordEditField';
                             field.appName = fieldDefinition.config.appName;
                             field.modelName = fieldDefinition.config.modelName;
                             field.fieldName = fieldDefinition.fieldName;
                             break;
-                        } 
-                        
+                        }
                         var picker = Tine.widgets.form.RecordPickerManager.get(
                             fieldDefinition.config.appName,
                             fieldDefinition.config.modelName,
@@ -225,6 +228,9 @@ Tine.widgets.form.FieldManager = function() {
                     break;
                 case 'records':
                     if (category === 'editDialog') {
+                        if (fieldDefinition.config.additionalFilterSpec) {
+                            field.additionalFilterSpec = fieldDefinition.config.additionalFilterSpec;
+                        }
                         field.xtype = 'wdgt.pickergrid';
                         field.recordClass = Tine[fieldDefinition.config.appName].Model[fieldDefinition.config.modelName];
                         field.allowCreateNew = _.get(fieldDefinition, 'config.dependentRecords', false);
