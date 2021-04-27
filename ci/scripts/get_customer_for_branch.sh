ba3688d4aeb49e3824e31972ecc7f8e31637e79d
Milan Mertens <m.mertens@metaways.de>
2021-04-27 10:23:26 +0200

    feature(ci): build source packages

---
+++ ci/scripts/get_customer_for_branch.sh
@@ -0,0 +1,22 @@
+#/bin/sh
+branch=$1
+
+if echo "${branch}" | grep -Eq '(pu/|feat/|change/)'; then
+	exit 1
+fi
+
+if ! echo "${branch}" | grep -q '/'; then
+	if ! echo "${branch}" | grep -Eq '20..\.11'; then
+    		exit 1
+	fi
+
+	echo tine20.org
+	exit 0
+else
+	if [ $(echo "${branch}" | awk -F"/" '{print NF-1}') != 1 ]; then
+    		exit 1
+	fi
+
+	echo "${branch}" | cut -d '/' -f0
+	exit 0
+fi
