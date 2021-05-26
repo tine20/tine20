9c949b517c64b23a4203658503cfd46dc6ccdfb4
Milan Mertens <m.mertens@metaways.de>
2021-05-26 10:02:43 +0200

    tweak(ci) move docker image build to script

---
+++ ci/scripts/reuse_or_build_image.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+
+TARGET=$1
+REUSE=$2
+CI_COMMIT_REF_NAME_ESCAPED=$(echo ${CI_COMMIT_REF_NAME} | sed sI/I-Ig)
+MAJOR_COMMIT_REF_NAME_ESCAPED=$(echo ${MAJOR_COMMIT_REF_NAME} | sed sI/I-Ig)
+
+if [ "${REUSE}" = "false" ]; then
+    echo "building image ..."
+    $CI_PROJECT_DIR/ci/scripts/build_image.sh $TARGET
+    exit 0
+fi
+
+echo "reusing image ..."
+if curl -s -f --user $REGISTRY_USER:$REGISTRY_PASSWORD -H "accept: application/vnd.docker.distribution.manifest.v2+json" "https://${REGISTRY}/v2/${TARGET}/manifests/${CI_COMMIT_REF_NAME_ESCAPED}-${PHP_VERSION}" > /dev/null; then
+    echo "using branch image ..."
+
+    $CI_PROJECT_DIR/ci/scripts/rename_remote_image.sh $REGISTRY_USER $REGISTRY_PASSWORD $REGISTRY ${TARGET} ${CI_COMMIT_REF_NAME_ESCAPED}-${PHP_VERSION} ${TARGET}-commit ${CI_PIPELINE_ID}-${PHP_VERSION}
+    exit 0
+fi
+echo "can not reuse branch image, trying major branch image ..."
+
+if curl -s -f --user $REGISTRY_USER:$REGISTRY_PASSWORD -H "accept: application/vnd.docker.distribution.manifest.v2+json" "https://${REGISTRY}/v2/${TARGET}/manifests/${MAJOR_COMMIT_REF_NAME_ESCAPED}-${PHP_VERSION}" > /dev/null; then
+    echo "using major branch image ..."
+    
+    $CI_PROJECT_DIR/ci/scripts/rename_remote_image.sh $REGISTRY_USER $REGISTRY_PASSWORD $REGISTRY ${TARGET} ${MAJOR_COMMIT_REF_NAME_ESCAPED}-${PHP_VERSION} ${TARGET}-commit ${CI_PIPELINE_ID}-${PHP_VERSION}
+    exit 0
+fi
+echo "can not reuse major branch image. building image ..."
+
+$CI_PROJECT_DIR/ci/scripts/build_image.sh $TARGET
+
