b2dadc874196f05334592da22f47bd8dad8e188b
Cornelius Weiß <c.weiss@metaways.de>
2021-06-14 11:12:22 +0200

    tweak(Tinebase) tell tika to use our tempdir

--- tests/tine20/Filemanager/Frontend/JsonTests.php
+++ tests/tine20/Filemanager/Frontend/JsonTests.php
@@ -99,8 +99,6 @@ class Filemanager_Frontend_JsonTests extends TestCase
         Tinebase_Container::getInstance()->getDefaultContainer('Filemanager_Model_Node');
 
         $this->_createdNodesJson = null;
-
-        copy(dirname(dirname(__FILE__)) . '/files/test.txt', dirname(dirname(__FILE__)) . '/files/test.txt.bkp');
     }
     
     /**
@@ -135,11 +133,6 @@ class Filemanager_Frontend_JsonTests extends TestCase
         $this->_otherUserContainer = null;
 
         clearstatcache();
-        if (!is_file(dirname(dirname(__FILE__)) . '/files/test.txt')) {
-            rename(dirname(dirname(__FILE__)) . '/files/test.txt.bkp', dirname(dirname(__FILE__)) . '/files/test.txt');
-        } else {
-            unlink(dirname(dirname(__FILE__)) . '/files/test.txt.bkp');
-        }
     }
 
     /**
@@ -986,13 +979,16 @@ class Filemanager_Frontend_JsonTests extends TestCase
         // create empty file first (like the js frontend does)
         $result = $this->_getUit()->createNode($filepath, Tinebase_Model_Tree_FileObject::TYPE_FILE, array(), false);
 
+        $tempFileFileFile = Tinebase_Core::getTempDir() . '/test.tmp';
+        copy(dirname(dirname(__FILE__)) . '/files/test.txt', $tempFileFileFile);
+        
         $tempFileBackend = new Tinebase_TempFile();
-        $tempFile = $tempFileBackend->createTempFile(dirname(dirname(__FILE__)) . '/files/test.txt');
+        $tempFile = $tempFileBackend->createTempFile($tempFileFileFile);
         $result = $this->_getUit()->createNode($filepath, Tinebase_Model_Tree_FileObject::TYPE_FILE, $tempFile->getId(), true);
         
         $this->assertEquals('text/plain', $result['contenttype'], print_r($result, true));
         $this->assertEquals(17, $result['size']);
-        $this->assertFalse(is_file(dirname(dirname(__FILE__)) . '/files/test.txt'));
+        $this->assertFalse(is_file($tempFileFileFile));
         
         return $result;
     }
