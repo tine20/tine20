ce2d797da96cf451a0a49f8dcc439e292a2db6bc
Philipp Schüle <p.schuele@metaways.de>
2021-07-07 17:43:05 +0200

    feature(Addressbook/ListRole): add max members field

--- tine20/Addressbook/Model/ListRole.php
+++ tine20/Addressbook/Model/ListRole.php
@@ -12,6 +12,7 @@ class Addressbook_Model_ListRole extends Tinebase_Record_NewAbstract
 {
     public const FLD_NAME = 'name';
     public const FLD_DESCRIPTION = 'description';
+    public const FLD_MAX_MEMBERS = 'max_members';
 
     public const MODEL_NAME_PART = 'ListRole';
     public const TABLE_NAME = 'addressbook_list_role';
@@ -54,6 +55,13 @@ class Addressbook_Model_ListRole extends Tinebase_Record_NewAbstract
                 self::LABEL => 'Description', //_('Description')
                 self::TYPE => self::TYPE_TEXT,
                 self::LENGTH => \Doctrine\DBAL\Platforms\MySqlPlatform::LENGTH_LIMIT_MEDIUMTEXT,
+                self::NULLABLE => true,
+                self::VALIDATORS => array(Zend_Filter_Input::ALLOW_EMPTY => true),
+                self::INPUT_FILTERS => [Zend_Filter_Empty::class => null],
+            ),
+            self::FLD_MAX_MEMBERS => array(
+                self::LABEL => 'Maximum Members', //_('Maximum Members')
+                self::TYPE => self::TYPE_INTEGER,
                 self::NULLABLE => true,
                 self::VALIDATORS => array(Zend_Filter_Input::ALLOW_EMPTY => true),
                 self::INPUT_FILTERS => [Zend_Filter_Empty::class => null],
