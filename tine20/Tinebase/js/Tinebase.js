55f56fd61c75c8f3d160dcf4034a872cc5ff11d7
Stefanie Stamer <s.stamer@metaways.de>
2021-07-22 09:12:46 +0200

    feature(Tinebase): community Identification Number picker

--- tine20/Tinebase/js/Tinebase.js
+++ tine20/Tinebase/js/Tinebase.js
@@ -22,6 +22,8 @@ require('../../library/ExtJS/ext-custom');
 // include traditional stuff as defined in jsb2
 require('./../../Tinebase/Tinebase.jsb2');
 require('./CommunityIdentNr/explainer');
+require('./CommunityIdentNr/picker');
+
 
 // UI style >= 2019
 require('node-waves');
