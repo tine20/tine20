0e996d05389f428a83db76e04a20a4f31a66bfb0
Paul Mehrer <p.mehrer@metaways.de>
2021-08-17 12:11:00 +0200

    added tine20version slug 2020.11 to update file 13.php

--- tine20/Projects/Setup/Update/13.php
+++ tine20/Projects/Setup/Update/13.php
@@ -7,6 +7,8 @@
  * @license     http://www.gnu.org/licenses/agpl.html AGPL3
  * @copyright   Copyright (c) 2020 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Philipp Schüle <p.schuele@metaways.de>
+ *
+ * this ist 2020.11 (ONLY!)
  */
 class Projects_Setup_Update_13 extends Setup_Update_Abstract
 {
