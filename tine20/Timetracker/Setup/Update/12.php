65e817cd758e27a9ec7b0f1fe624899b9d6d8b71
Paul Mehrer <p.mehrer@metaways.de>
2021-08-17 12:14:13 +0200

    added tine20version slug 2019.11 to update file 12.php

--- tine20/Timetracker/Setup/Update/12.php
+++ tine20/Timetracker/Setup/Update/12.php
@@ -8,6 +8,8 @@
  * @license     http://www.gnu.org/licenses/agpl.html AGPL3
  * @copyright   Copyright (c) 2020 Metaways Infosystems GmbH (http://www.metaways.de)
  * @author      Paul Mehrer <p.mehrer@metaways.de>
+ *
+ * this ist 2019.11 (ONLY!)
  */
 class Timetracker_Setup_Update_12 extends Setup_Update_Abstract
 {
