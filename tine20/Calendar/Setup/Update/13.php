1a7fabc3a04c637819825b556963be71f59f2382
Cornelius Weiß <c.weiss@metaways.de>
2021-08-17 11:17:43 +0200

    fix(Calendar): url field to short
    * also cleanup branch updatescript mess

--- tine20/Calendar/Setup/Update/13.php
+++ tine20/Calendar/Setup/Update/13.php
@@ -12,20 +12,26 @@
 class Calendar_Setup_Update_13 extends Setup_Update_Abstract
 {
     const RELEASE013_UPDATE001 = __CLASS__ . '::update001';
-    const RELEASE013_UPDATE002 = __CLASS__ . '::update002';
+    const RELEASE013_UPDATE002 = __CLASS__ . '::update002'; // burned number @see 14.1
+    const RELEASE013_UPDATE003 = __CLASS__ . '::update003';
+    const RELEASE013_UPDATE004 = __CLASS__ . '::update004';
 
 
     static protected $_allUpdates = [
-        self::PRIO_NORMAL_APP_STRUCTURE => [
-            self::RELEASE013_UPDATE002 => [
-                self::CLASS_CONST => self::class,
-                self::FUNCTION_CONST => 'update002',
-            ],
-        ],
         self::PRIO_NORMAL_APP_UPDATE        => [
             self::RELEASE013_UPDATE001          => [
                 self::CLASS_CONST                   => self::class,
                 self::FUNCTION_CONST                => 'update001',
+            ],
+        ],
+        self::PRIO_NORMAL_APP_STRUCTURE => [
+            self::RELEASE013_UPDATE003 => [
+                self::CLASS_CONST => self::class,
+                self::FUNCTION_CONST => 'update003',
+            ],
+            self::RELEASE013_UPDATE004 => [
+                self::CLASS_CONST => self::class,
+                self::FUNCTION_CONST => 'update004',
             ],
         ],
     ];
@@ -35,21 +41,43 @@ class Calendar_Setup_Update_13 extends Setup_Update_Abstract
         $this->addApplicationUpdate('Calendar', '13.0', self::RELEASE013_UPDATE001);
     }
 
-    public function update002()
+    public function update003()
     {
+        if (! $this->_backend->columnExists('mute', 'cal_events')) {
+            $declaration = new Setup_Backend_Schema_Field_Xml('
+                <field>
+                    <name>mute</name>
+                    <type>boolean</type>
+                </field>');
+            $this->_backend->addCol('cal_events', $declaration);
+        }
         if ($this->getTableVersion('cal_events') < 17) {
-            if (! $this->_backend->columnExists('mute', 'cal_events')) {
-                $declaration = new Setup_Backend_Schema_Field_Xml('
-            <field>
-                <name>mute</name>
-                <type>boolean</type>
-            </field>');
-                $this->_backend->addCol('cal_events', $declaration);
-            }
             $this->setTableVersion('cal_events', 17);
         }
 
-        $this->setTableVersion('cal_events', 13.1);
-        $this->addApplicationUpdate('Calendar', '13.1', self::RELEASE013_UPDATE002);
+        $this->setTableVersion('cal_events', 13.2);
+        $this->addApplicationUpdate('Calendar', '13.2', self::RELEASE013_UPDATE003);
     }
+
+    /**
+     * url field to short
+     * @throws Setup_Exception_NotFound
+     */
+    public function update004()
+    {
+        $this->_backend->alterCol('cal_events', new Setup_Backend_Schema_Field_Xml(
+            '<field>
+                <name>url</name>
+                <type>text</type>
+                <length>65535</length>
+                <notnull>false</notnull>
+            </field>'));
+
+        if ($this->getTableVersion('cal_events') < 18) {
+            $this->setTableVersion('cal_events', 18);
+        }
+
+        $this->addApplicationUpdate('Calendar', '13.3', self::RELEASE013_UPDATE004);
+    }
+
 }
