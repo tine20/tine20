fe64b61004febddd998178f77d32ee208bc319ab
Paul Mehrer <p.mehrer@metaways.de>
2021-08-25 18:29:50 +0200

    feature(Felamimail) mail attachment cache, openable by OOI

--- tests/tine20/Tinebase/ApplicationTest.php
+++ tests/tine20/Tinebase/ApplicationTest.php
@@ -327,6 +327,7 @@ class Tinebase_ApplicationTest extends TestCase
             'Felamimail' => array(
                 Felamimail_Model_Account::class,
                 Felamimail_Model_AccountGrants::class,
+                Felamimail_Model_AttachmentCache::class,
                 Felamimail_Model_Folder::class,
                 Felamimail_Model_Message::class,
                 Felamimail_Model_MessageFileLocation::class,
