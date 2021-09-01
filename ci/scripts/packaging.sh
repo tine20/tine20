2d9024b70bcc31c0bc1418f6ccf8b7e2b87088df
Milan Mertens <m.mertens@metaways.de>
2021-09-01 09:25:26 +0200

    tweak(ci): ci project path with namespace variable

--- ci/scripts/packaging.sh
+++ ci/scripts/packaging.sh
@@ -24,27 +24,27 @@ export RELEASE=$(echo "${VERSION}" | sed sI-I~Ig)
 export REVISION=0
 export CODENAME="${CODENAME}"
 
-cd ${CI_BUILDS_DIR}/tine20/tine20
+cd ${CI_BUILDS_DIR}/${CI_PROJECT_NAMESPACE}/tine20
 # create archives
-./ci/dockerimage/make.sh -o "${CI_BUILDS_DIR}/tine20/tine20/packages.tar" -c "${CACHE_IMAGE}" -c "${MAJOR_CACHE_IMAGE}" packages
+./ci/dockerimage/make.sh -o "${CI_BUILDS_DIR}/${CI_PROJECT_NAMESPACE}/tine20/packages.tar" -c "${CACHE_IMAGE}" -c "${MAJOR_CACHE_IMAGE}" packages
 
 # add current.map
 echo "currentPackage ${RELEASE}/tine20-allinone_${RELEASE}.zip" >> current.map
-tar -rf "${CI_BUILDS_DIR}/tine20/tine20/packages.tar" current.map
+tar -rf "${CI_BUILDS_DIR}/${CI_PROJECT_NAMESPACE}/tine20/packages.tar" current.map
 
 #push packages to gitlab
 
-customer=$(${CI_BUILDS_DIR}/tine20/tine20/ci/scripts/get_customer_for_branch.sh $MAJOR_COMMIT_REF_NAME)
+customer=$(${CI_BUILDS_DIR}/${CI_PROJECT_NAMESPACE}/tine20/ci/scripts/get_customer_for_branch.sh $MAJOR_COMMIT_REF_NAME)
 
 curl \
 	--header "JOB-TOKEN: ${CI_JOB_TOKEN}" \
-	--upload-file "${CI_BUILDS_DIR}/tine20/tine20/packages.tar" \
+	--upload-file "${CI_BUILDS_DIR}/${CI_PROJECT_NAMESPACE}/tine20/packages.tar" \
 	"${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/${customer}/${VERSION}/all.tar"
-echo "published packages to ${CI_API_V4_URL}/projects/${CI_BUILDS_DIR}/tine20/tine20/packages/generic/${customer}/${VERSION}/all.tar"
+echo "published packages to ${CI_API_V4_URL}/projects/${CI_BUILDS_DIR}/${CI_PROJECT_NAMESPACE}/tine20/packages/generic/${customer}/${VERSION}/all.tar"
 
-tar -xf "${CI_BUILDS_DIR}/tine20/tine20/packages.tar"
+tar -xf "${CI_BUILDS_DIR}/${CI_PROJECT_NAMESPACE}/tine20/packages.tar"
 
-cd ${CI_BUILDS_DIR}/tine20/tine20/${RELEASE}/
+cd ${CI_BUILDS_DIR}/${CI_PROJECT_NAMESPACE}/tine20/${RELEASE}/
 
 for f in *; do
 	curl \
