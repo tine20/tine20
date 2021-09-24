78adcfd8926ec8e0e5e621240b657a0961d5a07f
Paul Mehrer <p.mehrer@metaways.de>
2021-09-24 10:20:34 +0200

    tweak(SSO) SAML2 give login page RP data to display

---
+++ tine20/SSO/Setup/Update/14.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     SSO
+ * @subpackage  Setup
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL3
+ * @copyright   Copyright (c) 2021 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Paul Mehrer <p.mehrer@metaways.de>
+ *
+ * this is 2021.11 (ONLY!)
+ */
+class SSO_Setup_Update_14 extends Setup_Update_Abstract
+{
+    const RELEASE014_UPDATE001 = __CLASS__ . '::update001';
+
+    static protected $_allUpdates = [
+        self::PRIO_NORMAL_APP_STRUCTURE => [
+            self::RELEASE014_UPDATE001          => [
+                self::CLASS_CONST                   => self::class,
+                self::FUNCTION_CONST                => 'update001',
+            ],
+        ],
+    ];
+
+    public function update001()
+    {
+        Setup_SchemaTool::updateSchema([SSO_Model_RelyingParty::class]);
+        $this->addApplicationUpdate(SSO_Config::APP_NAME, '14.0', self::RELEASE014_UPDATE001);
+    }
+}
