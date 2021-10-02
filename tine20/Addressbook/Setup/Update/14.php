80a102328dcd55758cd789982a7223dad757e90e
Patrick <psieg@users.noreply.github.com>
2021-10-02 20:42:23 +0200

    Fix Addressbook update config part 2

--- tine20/Addressbook/Setup/Update/14.php
+++ tine20/Addressbook/Setup/Update/14.php
@@ -23,7 +23,7 @@ class Addressbook_Setup_Update_14 extends Setup_Update_Abstract
             ],
             self::RELEASE014_UPDATE002          => [
                 self::CLASS_CONST                   => self::class,
-                self::FUNCTION_CONST                => 'update001',
+                self::FUNCTION_CONST                => 'update002',
             ],
         ],
         self::PRIO_NORMAL_APP_UPDATE        => [
