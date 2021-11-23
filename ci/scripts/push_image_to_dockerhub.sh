09b427a3ac76da96b8186e17f4592550895e240a
Milan Mertens <m.mertens@metaways.de>
2021-11-23 13:12:35 +0100

    tweak(ci): publish images to tinegroupware/tine

--- ci/scripts/push_image_to_dockerhub.sh
+++ ci/scripts/push_image_to_dockerhub.sh
@@ -6,7 +6,7 @@ DOCKERHUB_NAME=$2
 docker login -u "${DOCKERHUB_USER}" -p "${DOCKERHUB_TOKEN}" "docker.io"
 
 FROM_IMAGE="${REGISTRY}/${NAME}-commit:${IMAGE_TAG}"
-DESTINATION_IMAGE="docker.io/tine20/${DOCKERHUB_NAME}:${DOCKERHUB_TAG}"
+DESTINATION_IMAGE="docker.io/tinegroupware/${DOCKERHUB_NAME}:${DOCKERHUB_TAG}"
 
 docker pull "${FROM_IMAGE}"
 docker tag "${FROM_IMAGE}" "${DESTINATION_IMAGE}"
