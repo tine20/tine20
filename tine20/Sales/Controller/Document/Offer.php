4d81b4d8e2816bd61f19c90635bb8a5268f7995e
Paul Mehrer <p.mehrer@metaways.de>
2022-01-04 11:36:02 +0100

    tweak(TB/Sales) refactor validate[Transition]State, add DocumentOffer controller constraints

--- tine20/Sales/Controller/Document/Offer.php
+++ tine20/Sales/Controller/Document/Offer.php
@@ -37,20 +37,66 @@ class Sales_Controller_Document_Offer extends Sales_Controller_Document_Abstract
         $this->_modelName = Sales_Model_Document_Offer::class;
         $this->_purgeRecords = false;
         $this->_doContainerACLChecks = false;
-
-        $this->_transitionStatusField = Sales_Model_Document_Offer::FLD_OFFER_STATUS;
-        $this->_transitionConfig = Sales_Config::DOCUMENT_OFFER_STATUS_TRANSITIONS;
     }
 
     protected function _inspectBeforeCreate(Tinebase_Record_Interface $_record)
     {
-        $this->_validateState($_record);
+        $this->_validateTransitionState(Sales_Model_Document_Offer::FLD_OFFER_STATUS,
+            Sales_Config::getInstance()->{Sales_Config::DOCUMENT_OFFER_STATUS_TRANSITIONS}, $_record);
+
+        // when booked and no document_date set, set it to now
+        if (Sales_Config::getInstance()->{Sales_Config::DOCUMENT_OFFER_STATUS}->records
+                ->getById($_record->{Sales_Model_Document_Offer::FLD_OFFER_STATUS})
+                    ->{Sales_Model_Document_OfferStatus::FLD_BOOKED} &&
+                    ! $_record->{Sales_Model_Document_Offer::FLD_DOCUMENT_DATE}) {
+            $_record->{Sales_Model_Document_Offer::FLD_DOCUMENT_DATE} = Tinebase_DateTime::now();
+        }
+
         parent::_inspectBeforeCreate($_record);
     }
 
     protected function _inspectBeforeUpdate($_record, $_oldRecord)
     {
-        $this->_validateState($_record, $_oldRecord);
+        $this->_validateTransitionState(Sales_Model_Document_Offer::FLD_OFFER_STATUS,
+            Sales_Config::getInstance()->{Sales_Config::DOCUMENT_OFFER_STATUS_TRANSITIONS}, $_record, $_oldRecord);
+
+        // when oldRecord is booked, enforce read only
+        if ($_oldRecord->{Sales_Model_Document_Offer::FLD_OFFER_STATUS} &&
+                Sales_Config::getInstance()->{Sales_Config::DOCUMENT_OFFER_STATUS}->records
+                    ->getById($_oldRecord->{Sales_Model_Document_Offer::FLD_OFFER_STATUS})
+                        ->{Sales_Model_Document_OfferStatus::FLD_BOOKED}) {
+            foreach ($_record->getConfiguration()->fields as $field => $fConf) {
+                if (in_array($field, [
+                    Sales_Model_Document_Offer::FLD_OFFER_STATUS,
+                    Sales_Model_Document_Offer::FLD_COST_CENTER_ID,
+                    Sales_Model_Document_Offer::FLD_COST_BEARER_ID,
+                    Sales_Model_Document_Offer::FLD_DESCRIPTION,
+                    'tags', 'attachments', 'relations',
+                ])) continue;
+                $_record->{$field} = $_oldRecord->{$field};
+            }
+        }
+        // when booked and no document_date set, set it to now
+        if (Sales_Config::getInstance()->{Sales_Config::DOCUMENT_OFFER_STATUS}->records
+                ->getById($_record->{Sales_Model_Document_Offer::FLD_OFFER_STATUS})
+                    ->{Sales_Model_Document_OfferStatus::FLD_BOOKED} &&
+                    ! $_record->{Sales_Model_Document_Offer::FLD_DOCUMENT_DATE}) {
+            $_record->{Sales_Model_Document_Offer::FLD_DOCUMENT_DATE} = Tinebase_DateTime::now();
+        }
+
         parent::_inspectBeforeUpdate($_record, $_oldRecord);
+    }
+
+    protected function _inspectDelete(array $_ids)
+    {
+        // do not deleted booked records
+        foreach ($this->getMultiple($_ids) as $record) {
+            if (Sales_Config::getInstance()->{Sales_Config::DOCUMENT_OFFER_STATUS}->records
+                    ->getById($record->{Sales_Model_Document_Offer::FLD_OFFER_STATUS})
+                    ->{Sales_Model_Document_OfferStatus::FLD_BOOKED}) {
+                unset($_ids[array_search($record->getId(), $_ids)]);
+            }
+        }
+        return parent::_inspectDelete($_ids);
     }
 }
