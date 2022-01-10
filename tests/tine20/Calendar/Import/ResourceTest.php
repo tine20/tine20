b5119505ebba33cf775c99312fad2851d5a52328
Christian Feitl <c.feitl@metaways.de>
2022-01-10 14:26:00 +0100

    test(Calendar): activate resource import test

--- tests/tine20/Calendar/Import/ResourceTest.php
+++ tests/tine20/Calendar/Import/ResourceTest.php
@@ -27,8 +27,6 @@ class Calendar_Import_ResourceTest extends TestCase
 
     public function testImportDemoData()
     {
-        self::markTestSkipped('FIXME: this fails in UPDATE test');
-
         $importer = new Tinebase_Setup_DemoData_Import('Calendar_Model_Resource', [
             'definition' => 'cal_import_resource_csv',
             'file' => 'resource.csv',
