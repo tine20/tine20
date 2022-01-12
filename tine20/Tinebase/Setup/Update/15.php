f6af0299c89af7b4077174d49dfe5da88e61e1f9
Paul Mehrer <p.mehrer@metaways.de>
2022-01-12 10:27:03 +0100

    tweak(TB Rights) add new default right mainscreen, migrate run to run+mainscreen

--- tine20/Tinebase/Setup/Update/15.php
+++ tine20/Tinebase/Setup/Update/15.php
@@ -15,6 +15,7 @@ class Tinebase_Setup_Update_15 extends Setup_Update_Abstract
 {
     const RELEASE015_UPDATE000 = __CLASS__ . '::update000';
     const RELEASE015_UPDATE001 = __CLASS__ . '::update001';
+    const RELEASE015_UPDATE002 = __CLASS__ . '::update002';
 
     static protected $_allUpdates = [
         self::PRIO_TINEBASE_STRUCTURE       => [
@@ -22,12 +23,18 @@ class Tinebase_Setup_Update_15 extends Setup_Update_Abstract
                 self::CLASS_CONST                   => self::class,
                 self::FUNCTION_CONST                => 'update001',
             ],
+            // as we do a raw query, we dont want that table to be changed before we do our query => prio struct
+            self::RELEASE015_UPDATE002          => [
+                self::CLASS_CONST                   => self::class,
+                self::FUNCTION_CONST                => 'update002',
+            ],
         ],
         self::PRIO_TINEBASE_UPDATE          => [
             self::RELEASE015_UPDATE000          => [
                 self::CLASS_CONST                   => self::class,
                 self::FUNCTION_CONST                => 'update000',
             ],
+
         ],
     ];
 
@@ -40,5 +47,19 @@ class Tinebase_Setup_Update_15 extends Setup_Update_Abstract
     {
         Setup_SchemaTool::updateSchema([Tinebase_Model_Tree_FileObject::class]);
         $this->addApplicationUpdate('Tinebase', '15.1', self::RELEASE015_UPDATE001);
+    }
+
+    // as we do a raw query, we dont want that table to be changed before we do our query => prio struct
+    public function update002()
+    {
+        $db = $this->getDb();
+        foreach ($db->query('SELECT role_id, application_id from ' . SQL_TABLE_PREFIX . 'role_rights WHERE `right` = "'
+                . Tinebase_Acl_Rights_Abstract::RUN . '"')->fetchAll(Zend_Db::FETCH_NUM) as $row) {
+            $db->query($db->quoteInto('INSERT INTO ' . SQL_TABLE_PREFIX . 'role_rights SET role_id = ?', $row[0]) .
+                $db->quoteInto(', application_id = ?', $row[1]) .
+                ', `right` = "' . Tinebase_Acl_Rights_Abstract::MAINSCREEN . '", `id` = "' .
+                Tinebase_Record_Abstract::generateUID() . '"');
+        }
+        $this->addApplicationUpdate('Tinebase', '15.2', self::RELEASE015_UPDATE002);
     }
 }
