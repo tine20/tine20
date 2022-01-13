f8ec8414282685da8fd822adeeec678624e5d06a
Cornelius Weiß <c.weiss@metaways.de>
2022-01-13 19:18:24 +0100

    feature(Sales): customer language & discout takeover

--- tine20/Sales/Setup/Update/15.php
+++ tine20/Sales/Setup/Update/15.php
@@ -15,12 +15,17 @@ class Sales_Setup_Update_15 extends Setup_Update_Abstract
 {
     const RELEASE015_UPDATE000 = __CLASS__ . '::update000';
     const RELEASE015_UPDATE001 = __CLASS__ . '::update001';
+    const RELEASE015_UPDATE002 = __CLASS__ . '::update002';
 
     static protected $_allUpdates = [
         self::PRIO_NORMAL_APP_STRUCTURE     => [
             self::RELEASE015_UPDATE001          => [
                 self::CLASS_CONST                   => self::class,
                 self::FUNCTION_CONST                => 'update001',
+            ],
+            self::RELEASE015_UPDATE002          => [
+                self::CLASS_CONST                   => self::class,
+                self::FUNCTION_CONST                => 'update002',
             ],
         ],
         self::PRIO_NORMAL_APP_UPDATE        => [
@@ -44,5 +49,22 @@ class Sales_Setup_Update_15 extends Setup_Update_Abstract
             Sales_Model_Document_Order::class, Sales_Model_DocumentPosition_Offer::class,
             Sales_Model_DocumentPosition_Order::class]);
         $this->addApplicationUpdate('Sales', '15.1', self::RELEASE015_UPDATE001);
+    }
+
+    public function update002()
+    {
+        if ($this->getTableVersion('sales_customers') < 4) {
+            $declaration = new Setup_Backend_Schema_Field_Xml('
+               <field>
+                    <name>language</name>
+                    <type>text</type>
+                    <length>40</length>
+                    <notnull>false</notnull>
+                </field>
+        ');
+            $this->_backend->addCol('sales_customers', $declaration, 6);
+            $this->setTableVersion('sales_customers', 4);
+        }
+        $this->addApplicationUpdate('Sales', '15.2', self::RELEASE015_UPDATE002);
     }
 }
