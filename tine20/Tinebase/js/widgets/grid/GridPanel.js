b11ce676fcd6980cfecd09240b48fa54bb0ed736
Cornelius Weiß <c.weiss@metaways.de>
2022-01-13 14:31:26 +0100

    tweak(Tinebase): allow to define cols by stirngs

--- tine20/Tinebase/js/widgets/grid/GridPanel.js
+++ tine20/Tinebase/js/widgets/grid/GridPanel.js
@@ -601,7 +601,11 @@ Ext.extend(Tine.widgets.grid.GridPanel, Ext.Panel, {
                 appName = this.recordClass.getMeta('appName'),
                 modelName = this.recordClass.getMeta('modelName');
 
-            Ext.each(this.modelConfig.fieldKeys, function(key) {
+            Ext.each(this.columns || this.modelConfig.fieldKeys, function(key) {
+                if (! Ext.isString(key)) {
+                    columns.push(key);
+                    return;
+                }
                 var config = Tine.widgets.grid.ColumnManager.get(appName, modelName, key, 'mainScreen');
                 
                 // @todo thats just a hotfix!
