a971ed1136ed22a86246207bf8e024888418becf
Philipp Schüle <p.schuele@metaways.de>
2022-01-18 10:03:49 +0100

    tweak(Addressbook/Contact): throw event on create contact

---
+++ tine20/Addressbook/Event/CreateContact.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Tine 2.0
+ *
+ * @package     Addressbook
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @copyright   Copyright (c) 2022 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Philipp Schüle <p.schuele@metaways.de>
+ */
+
+/**
+ * event class for changed list
+ *
+ * @package     Addressbook
+ */
+class Addressbook_Event_CreateContact extends Tinebase_Event_Abstract
+{
+    /**
+     * the list object
+     *
+     * @var Addressbook_Model_Contact
+     */
+    public $createdContact;
+
+    public $record;
+}
