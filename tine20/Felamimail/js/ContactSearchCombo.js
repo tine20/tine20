c8bcc57924c339b9e94f5e9f17101f047aa433a6
Philipp Schüle <p.schuele@metaways.de>
2022-02-01 15:21:06 +0100

    fix(Felamimail/Compose): deactivate broken duplicate removing
    ... this needs fixing - it removes too many valid email addresses

--- tine20/Felamimail/js/ContactSearchCombo.js
+++ tine20/Felamimail/js/ContactSearchCombo.js
@@ -211,28 +211,28 @@ Tine.Felamimail.ContactSearchCombo = Ext.extend(Tine.Addressbook.SearchCombo, {
             }
         });
     
-        // remove duplicated email addresses in mailingList , 
-        store.each(function(record) {
-            if (record.data.emails !== '') {
-                const idx = store.indexOf(record);
-                let emailArray = _.compact(_.split(record.data.emails, ','));
-                
-                duplicates = store.queryBy(function (contact) {
-                    if (contact.data.email !== '' ) {
-                        return record.id !== contact.id && _.includes(emailArray, contact.data.email);
-                    }
-                });
-    
-                emailArray = _.difference(emailArray, _.map(duplicates.items, 'data.email'));
-                record.data.emails = _.join(emailArray, ',');
-                Tine.log.debug('remove duplicate: ' + Tine.Felamimail.getEmailStringFromContact(record));
-                store.removeAt(idx);
-                
-                if (emailArray.length > 0) {
-                    store.insert(idx, record);
-                }
-            }
-        });
+        // FIXME remove duplicated email addresses in mailingList ,
+        // store.each(function(record) {
+        //     if (record.data.emails !== '') {
+        //         const idx = store.indexOf(record);
+        //         let emailArray = _.compact(_.split(record.data.emails, ','));
+        //
+        //         duplicates = store.queryBy(function (contact) {
+        //             if (contact.data.email !== '' ) {
+        //                 return record.id !== contact.id && _.includes(emailArray, contact.data.email);
+        //             }
+        //         });
+        //
+        //         emailArray = _.difference(emailArray, _.map(duplicates.items, 'data.email'));
+        //         record.data.emails = _.join(emailArray, ',');
+        //         Tine.log.debug('remove duplicate: ' + Tine.Felamimail.getEmailStringFromContact(record));
+        //         store.removeAt(idx);
+        //
+        //         if (emailArray.length > 0) {
+        //             store.insert(idx, record);
+        //         }
+        //     }
+        // });
     }    
 });
 Ext.reg('felamimailcontactcombo', Tine.Felamimail.ContactSearchCombo);
