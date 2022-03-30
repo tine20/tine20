72adef9d3f36feb07ed91e05a4e49895d2ed1151
Jan Evers <j.evers@metaways.de>
2022-03-30 10:35:51 +0200

    fix(PollClient): Allow poll name to be null

--- tine20/Calendar/js/pollClient/src/App.vue
+++ tine20/Calendar/js/pollClient/src/App.vue
@@ -16,7 +16,7 @@
         <div class="row">
           <div class="col-md-8 col-sm-12">
             <h1 class="poll-event">{{poll.event_summary}}</h1>
-            <h2 class="poll-name" v-if="poll.name.length > 0">{{poll.name}}</h2>
+            <h2 class="poll-name" v-if="poll.name && poll.name.length > 0">{{poll.name}}</h2>
           </div>
           <div class="col-md-4 col-sm-12 text-right">
             <a :href="poll.config.brandingWeburl">
