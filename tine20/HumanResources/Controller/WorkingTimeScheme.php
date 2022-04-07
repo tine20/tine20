e64c94922795a9e539b61ca9cc9eedb266a27441
Paul Mehrer <p.mehrer@metaways.de>
2022-04-07 16:45:34 +0200

    tweak(HR WTS) add grants to wts

--- tine20/HumanResources/Controller/WorkingTimeScheme.php
+++ tine20/HumanResources/Controller/WorkingTimeScheme.php
@@ -38,7 +38,7 @@ class HumanResources_Controller_WorkingTimeScheme extends Tinebase_Controller_Re
         $this->_modelName = HumanResources_Model_WorkingTimeScheme::class;
         $this->_purgeRecords = false;
         // activate this if you want to use containers
-        $this->_doContainerACLChecks = false;
+        $this->_doContainerACLChecks = true;
     }
 
     /**
@@ -54,11 +54,122 @@ class HumanResources_Controller_WorkingTimeScheme extends Tinebase_Controller_Re
             return;
         }
 
-        if (self::ACTION_GET !== $_action && !$this->checkRight(HumanResources_Acl_Rights::MANAGE_EMPLOYEE, FALSE)) {
-            throw new Tinebase_Exception_AccessDenied('You are not allowed to ' . $_action . ' free time type.');
+        if (self::ACTION_GET !== $_action && !Tinebase_Core::getUser()
+                ->hasRight(HumanResources_Config::APP_NAME, HumanResources_Acl_Rights::MANAGE_EMPLOYEE)) {
+            throw new Tinebase_Exception_AccessDenied('You are not allowed to ' . $_action . ' working time schemes.');
         }
         parent::_checkRight($_action);
     }
+
+    protected function _checkGrant($_record, $_action, $_throw = TRUE, $_errorMessage = 'No Permission.', $_oldRecord = NULL)
+    {
+        /** @var HumanResources_Model_FreeTime $_record */
+        if (!$this->_doContainerACLChecks) {
+            return true;
+        }
+
+        // if we have manage_employee right, we have all grants
+        if (Tinebase_Core::getUser()->hasRight(HumanResources_Config::APP_NAME, HumanResources_Acl_Rights::MANAGE_EMPLOYEE)) {
+            return true;
+        }
+
+        // if you dont have manage_employee, you may only GET
+        if (self::ACTION_GET !== $_action) {
+            if ($_throw) {
+                throw new Tinebase_Exception_AccessDenied($_errorMessage);
+            }
+            return false;
+        }
+
+        if (HumanResources_Model_WorkingTimeScheme::TYPES_INDIVIDUAL !==
+                $_record->{HumanResources_Model_WorkingTimeScheme::FLDS_TYPE}) {
+            // if we have update employee data on any division -> we see all shared / templates everything
+            $filter = Tinebase_Model_Filter_FilterGroup::getFilterForModel(HumanResources_Model_Division::class, [
+                ['field' => 'container_id', 'operator' => 'specialNode', 'value' => 'all'],
+            ]);
+            $filter->setRequiredGrants([HumanResources_Model_DivisionGrants::UPDATE_EMPLOYEE_DATA]);
+            $divisionCtrl = HumanResources_Controller_Division::getInstance();
+            $oldValue = $divisionCtrl->doContainerACLChecks(false);
+            try {
+                if (0 < $divisionCtrl->searchCount($filter)) {
+                    return true;
+                }
+            } finally {
+                $divisionCtrl->doContainerACLChecks($oldValue);
+            }
+        }
+
+        // if we see a contract with this working time scheme, we do see the working time scheme
+        if (0 < HumanResources_Controller_Contract::getInstance()->searchCount(
+                Tinebase_Model_Filter_FilterGroup::getFilterForModel(HumanResources_Model_Contract::class, [
+                    ['field' => HumanResources_Model_Contract::FLDS_WORKING_TIME_SCHEME, 'operator' => 'equals', 'value' => $_record->getId()],
+                ]))) {
+            return true;
+        }
+
+        if ($_throw) {
+            throw new Tinebase_Exception_AccessDenied($_errorMessage);
+        }
+        return false;
+    }
+
+    /**
+     * Removes containers where current user has no access to
+     *
+     * @param Tinebase_Model_Filter_FilterGroup $_filter
+     * @param string $_action get|update
+     */
+    public function checkFilterACL(Tinebase_Model_Filter_FilterGroup $_filter, $_action = self::ACTION_GET)
+    {
+        if (!$this->_doContainerACLChecks) {
+            return;
+        }
+
+        // DO NOT CALL PARENT, we do not have a container! this is a pure fake filter acl function!
+
+        // if we have manage_employee right, we need no acl filter
+        if (Tinebase_Core::getUser()->hasRight(HumanResources_Config::APP_NAME, HumanResources_Acl_Rights::MANAGE_EMPLOYEE)) {
+            return;
+        }
+
+        if (self::ACTION_GET !== $_action) {
+            throw new Tinebase_Exception_AccessDenied('You are not allowed to ' . $_action . ' working time schemes.');
+        }
+
+        if ($_filter->getCondition() !== Tinebase_Model_Filter_FilterGroup::CONDITION_AND) {
+            $_filter->andWrapItself();
+        }
+
+        $orWrapper = new Tinebase_Model_Filter_FilterGroup([], Tinebase_Model_Filter_FilterGroup::CONDITION_OR);
+
+        // if we have update employee data on any division -> we see all shared / templates
+        $filter = Tinebase_Model_Filter_FilterGroup::getFilterForModel(HumanResources_Model_Division::class, [
+            ['field' => 'container_id', 'operator' => 'specialNode', 'value' => 'all'],
+        ]);
+        $filter->setRequiredGrants([HumanResources_Model_DivisionGrants::UPDATE_EMPLOYEE_DATA]);
+        $divisionCtrl = HumanResources_Controller_Division::getInstance();
+        $oldValue = $divisionCtrl->doContainerACLChecks(false);
+        try {
+            if (0 < $divisionCtrl->searchCount($filter)) {
+                // add where type !== individual
+                $orWrapper->addFilter(
+                    $_filter->createFilter(['field' => HumanResources_Model_WorkingTimeScheme::FLDS_TYPE, 'operator' => 'not', 'value' => HumanResources_Model_WorkingTimeScheme::TYPES_INDIVIDUAL])
+                );
+            }
+        } finally {
+            $divisionCtrl->doContainerACLChecks($oldValue);
+        }
+
+        // add or where id in (?) <- distinct wts ids from contracts that we see
+        $wtsIds = array_keys(HumanResources_Controller_Contract::getInstance()->search(
+            Tinebase_Model_Filter_FilterGroup::getFilterForModel(HumanResources_Model_Contract::class),
+            /*Pagi*/null, /*Rels*/ false, [HumanResources_Model_Contract::FLDS_WORKING_TIME_SCHEME]
+        ));
+        $orWrapper->addFilter($_filter->createFilter(['field' => 'id', 'operator' => 'in', 'value' => $wtsIds]));
+
+        $_filter->addFilterGroup($orWrapper);
+    }
+
 
     /**
      * get working time account for given employee
