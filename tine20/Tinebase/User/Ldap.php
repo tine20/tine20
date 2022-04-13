5016be11f4f412181fd933acbe15a0b8915dee20
Christian Feitl <c.feitl@metaways.de>
2022-04-13 11:23:30 +0200

    fix(Tinebase/Ldap): update pw must change property
    ... tine would ask user to change pw again after login
     each time ...

--- tine20/Tinebase/User/Ldap.php
+++ tine20/Tinebase/User/Ldap.php
@@ -408,6 +408,7 @@ class Tinebase_User_Ldap extends Tinebase_User_Sql implements Tinebase_User_Inte
         // update last modify timestamp in sql backend too
         $values = array(
             'last_password_change' => Tinebase_DateTime::now()->get(Tinebase_Record_Abstract::ISO8601LONG),
+            'password_must_change' => 0,
         );
         
         $where = array(
