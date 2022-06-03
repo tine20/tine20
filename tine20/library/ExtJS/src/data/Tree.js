5f928ccb8a205bb65e6df28122e59b3e2534ddb0
Cornelius Weiß <c.weiss@metaways.de>
2022-06-03 09:59:39 +0200

    fix(Email): tree expanding icons on folder move

--- tine20/library/ExtJS/src/data/Tree.js
+++ tine20/library/ExtJS/src/data/Tree.js
@@ -392,6 +392,8 @@ Ext.extend(Ext.data.Node, Ext.util.Observable, {
             }
             index = this.childNodes.length;
             if(index === 0){
+                this.leaf = false;
+                this.attributes.expandable = true;
                 this.setFirstChild(node);
             }
             this.childNodes.push(node);
@@ -431,6 +433,10 @@ Ext.extend(Ext.data.Node, Ext.util.Observable, {
 
         // remove it from childNodes collection
         this.childNodes.splice(index, 1);
+        if(!this.childNodes.length) {
+            this.leaf = true;
+            this.attributes.expandable = false;
+        }
 
         // update siblings
         if(node.previousSibling){
