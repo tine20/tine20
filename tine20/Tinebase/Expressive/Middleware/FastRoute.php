43179e24c44e0231125a7109c27f77b263dee9b5
Paul Mehrer <p.mehrer@metaways.de>
2022-06-07 11:42:12 +0200

    fix(TB Expressiv) fix routing for subdir installations

--- tine20/Tinebase/Expressive/Middleware/FastRoute.php
+++ tine20/Tinebase/Expressive/Middleware/FastRoute.php
@@ -48,8 +48,9 @@ class Tinebase_Expressive_Middleware_FastRoute implements MiddlewareInterface
         $uri = preg_replace('/\/+$/', '', $uri);
 
         // if the tine20 server is located in a subdir, we need to remove the server path from the uri
-        $serverPath = Tinebase_Core::getUrl(Tinebase_Core::GET_URL_PATH);
-        $uri = preg_replace('/^' . preg_quote($serverPath, '/') . '/', '', $uri);
+        if ('/' !== ($serverPath = Tinebase_Core::getUrl(Tinebase_Core::GET_URL_PATH)) && '' !== $serverPath) {
+            $uri = preg_replace('/^' . preg_quote($serverPath, '/') . '/', '/', $uri);
+        }
 
         if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::'
             . __LINE__ . " FastRoute dispatching:\n" . $request->getMethod() . ' '. $uri . array_reduce(array_keys($request->getHeaders()), function($headers, $name) use ($request)  {
