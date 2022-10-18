e8a4e4d5d1e3a094aef9777d6d7daba71e01e415
Philipp Schüle <p.schuele@metaways.de>
2022-10-18 13:29:06 +0200

    tweak(Tinebase/Http): remove newlines from disposition header

--- tine20/Tinebase/Frontend/Http/Abstract.php
+++ tine20/Tinebase/Frontend/Http/Abstract.php
@@ -382,7 +382,9 @@ abstract class Tinebase_Frontend_Http_Abstract extends Tinebase_Frontend_Abstrac
         header("Pragma: cache");
 
         if ($disposition) {
-            header('Content-Disposition: ' . $disposition . '; filename="' . $filename . '"');
+            $headerLine = 'Content-Disposition: ' . $disposition . '; filename="' . $filename . '"';
+            $headerLine = str_replace(["\r\n", "\r", "\n"], '', $headerLine);
+            header($headerLine);
         }
         if (empty($contentType)) {
             $contentType = Tinebase_Model_Tree_FileObject::DEFAULT_CONTENT_TYPE;
