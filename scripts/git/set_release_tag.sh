6c40375057133671be43c72280e2a9cda5309c6a
Philipp Schüle <p.schuele@metaways.de>
2022-11-03 10:43:09 +0100

    tweak(scripts/git): backport set_release_tag script

---
+++ scripts/git/set_release_tag.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+# (c) copyright Metaways Infosystems GmbH 2016-2022
+# authors:  Philipp Schüle <p.schuele@metaways.de>
+#
+# USAGE: sh ~/Desktop/scripts/set_release_tag.sh 2018.11.1
+
+TAG=$1
+
+if [ "$TAG" = "" ]
+then
+  echo "Please enter release tag:"
+  read -r TAG
+fi
+
+git pull
+git tag -a $TAG -m "version $TAG"
+git push origin $TAG
+
