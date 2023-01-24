df80bae21d3954addf6a24d4e299d7c93658d5df
Cornelius Weiß <c.weiss@metaways.de>
2023-01-24 16:26:02 +0100

    fix(Tinebase): can't relate to filemanager

--- tests/e2etests/src/test/Tinebase/relationGridPanel.test.js
+++ tests/e2etests/src/test/Tinebase/relationGridPanel.test.js
@@ -7,7 +7,7 @@ beforeAll(async () => {
     await lib.getBrowser('Adressbuch', 'Kontakte');
 });
 
-describe.skip('Mainpage', () => {
+describe('Mainpage', () => {
 
     test('openEditDialog', async () => {
         let popupWindow = await lib.getEditDialog('Kontakt hinzufügen');
