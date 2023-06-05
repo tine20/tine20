40deb494c4b1ce9d743ba59f53d6105f5425cc87
Philipp Schüle <p.schuele@metaways.de>
2023-06-05 09:19:54 +0200

    tests(Felamimail/Json): recipient order does not matter

--- tests/tine20/Felamimail/Frontend/JsonTest.php
+++ tests/tine20/Felamimail/Frontend/JsonTest.php
@@ -1324,7 +1324,6 @@ class Felamimail_Frontend_JsonTest extends Felamimail_TestCase
 
     /**
      * testSaveMessageWithMixedRecipients
-     *
      */
     public function testSaveMessageWithMixedRecipients()
     {
@@ -1354,11 +1353,11 @@ class Felamimail_Frontend_JsonTest extends Felamimail_TestCase
         // check if message is in drafts folder and recipients are present
         $message = $this->_searchForMessageBySubject($messageToSave['subject'], $this->_account->drafts_folder);
         self::assertEquals($messageToSave['subject'], $message['subject']);
-        self::assertEquals($messageToSave['to'][0]['email'], $message['to'][0], 'recipient not found: '
+        self::assertTrue(in_array($messageToSave['to'][0]['email'], $message['to']), 'recipient not found: '
             . print_r($message, true));
-        self::assertEquals('teststring@mail.test', $message['to'][1], 'recipient not found');
-        self::assertEquals('teststring2@mail.test', $message['to'][2], 'recipient not found');
-        self::assertEquals('teststring3@mail.test', $message['to'][3], 'recipient not found');
+        self::assertTrue(in_array('teststring@mail.test', $message['to']), 'recipient not found');
+        self::assertTrue(in_array('teststring2@mail.test', $message['to']), 'recipient not found');
+        self::assertTrue(in_array('teststring3@mail.test', $message['to']), 'recipient not found');
         self::assertEquals(2, count($message['bcc']), 'bcc recipient not found: '
             . print_r($message, true));
         self::assertStringContainsString('bccaddress', $message['bcc'][0], 'bcc recipient not found');
